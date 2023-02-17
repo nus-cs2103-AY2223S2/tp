6e6005d73525469e8c2cec14a16bb4b6c9d94606
lywich <lywichcy@gmail.com>
2023-02-18 03:01:21 +0800

    Create Description.java
    Task will take Description as one of its paramters along with Name and Set<Tag>.

---
+++ src/main/java/seedu/address/model/task/Description.java
@@ -0,0 +1,53 @@
+package seedu.address.model.task;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents a Task's description in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidDescription(String)}
+ */
+public class Description {
+
+
+    public static final String MESSAGE_CONSTRAINTS =
+            "Description must contain at least one character";
+    public static final String VALIDATION_REGEX = ".*";
+    public final String value;
+
+    /**
+     * Constructs a {@code Description}.
+     *
+     * @param description A valid description.
+     */
+    public Description(String description) {
+        requireNonNull(description);
+        checkArgument(isValidDescription(description), MESSAGE_CONSTRAINTS);
+        value = description;
+    }
+
+    /**
+     * Returns true if a given string is a valid phone number.
+     */
+    public static boolean isValidDescription(String test) {
+        return !test.isEmpty();
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Description // instanceof handles nulls
+                && value.equals(((Description) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+}
