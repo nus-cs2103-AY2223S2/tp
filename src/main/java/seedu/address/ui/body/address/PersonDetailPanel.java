239dc34abc898d93796cb9b1f4cc7a25686c5eb2
hansstanley <nusstuhss@gmail.com>
2023-02-17 21:37:17 +0800

    Add PersonDetailCard for better readability

--- src/main/java/seedu/address/ui/body/address/PersonDetailPanel.java
+++ src/main/java/seedu/address/ui/body/address/PersonDetailPanel.java
@@ -2,8 +2,13 @@ package seedu.address.ui.body.address;
 
 import java.util.Comparator;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Control;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Region;
 import seedu.address.model.person.Person;
@@ -20,13 +25,9 @@ public class PersonDetailPanel extends UiPart<Region> {
     @FXML
     private Label id;
     @FXML
-    private Label phone;
-    @FXML
-    private Label address;
-    @FXML
-    private Label email;
-    @FXML
     private FlowPane tags;
+    @FXML
+    private ListView<PersonDetailCard.DetailCardData> dataList;
 
     /**
      * Creates a blank {@code PersonDetailPanel}.
@@ -45,13 +46,29 @@ public class PersonDetailPanel extends UiPart<Region> {
 
         id.setText(displayedIndex + ".");
         name.setText(person.getName().toString());
-        phone.setText(person.getPhone().toString());
-        address.setText(person.getAddress().toString());
-        email.setText(person.getEmail().toString());
         person.getTags()
                 .stream()
                 .sorted(Comparator.comparing(tag -> tag.tagName))
                 .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
+
+        ObservableList<PersonDetailCard.DetailCardData> detailCardDataList = FXCollections.observableArrayList(
+                new PersonDetailCard.DetailCardData("Phone", person.getPhone().toString()),
+                new PersonDetailCard.DetailCardData("Address", person.getAddress().toString()),
+                new PersonDetailCard.DetailCardData("Email", person.getEmail().toString())
+        );
+        dataList.setItems(detailCardDataList);
+        dataList.setCellFactory(listView -> {
+            DataListViewCell cell = new DataListViewCell();
+
+            /*
+             * @@author hansstanley-reused
+             * Reused from https://stackoverflow.com/questions/37130122 with minor modifications.
+             * This restricts the width of the cells so the horizontal scroll bar does not appear.
+             */
+            cell.prefWidthProperty().bind(dataList.widthProperty().subtract(20));
+            cell.setMaxWidth(Control.USE_PREF_SIZE);
+            return cell;
+        });
     }
 
     /**
@@ -60,9 +77,24 @@ public class PersonDetailPanel extends UiPart<Region> {
     public void clearPerson() {
         id.setText("Select a contact.");
         name.setText(null);
-        phone.setText(null);
-        address.setText(null);
-        email.setText(null);
         tags.getChildren().clear();
+    }
+
+    /**
+     * Custom {@code ListCell} that displays the graphics of a {@code PersonDetailCard.DetailCardData}
+     * using a {@code PersonDetailCard}.
+     */
+    static class DataListViewCell extends ListCell<PersonDetailCard.DetailCardData> {
+        @Override
+        protected void updateItem(PersonDetailCard.DetailCardData data, boolean empty) {
+            super.updateItem(data, empty);
+
+            if (empty || data == null) {
+                setGraphic(null);
+                setText(null);
+            } else {
+                setGraphic(new PersonDetailCard(data).getRoot());
+            }
+        }
     }
 }
