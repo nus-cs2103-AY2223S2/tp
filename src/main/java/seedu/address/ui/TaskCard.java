5a3467f4b01e2208362a4870e1881acf8ca14043
lywich <lywichcy@gmail.com>
2023-02-18 03:08:55 +0800

    Update FXML files
    GUI looks for PersonListCard/Panel.
    
    Edit the FXML file names to be TaskListCard/Panel to function.

--- src/main/java/seedu/address/ui/TaskCard.java
+++ src/main/java/seedu/address/ui/TaskCard.java
@@ -14,7 +14,7 @@ import seedu.address.model.task.Task;
  */
 public class TaskCard extends UiPart<Region> {
 
-    private static final String FXML = "PersonListCard.fxml";
+    private static final String FXML = "TaskListCard.fxml";
 
     /**
      * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
