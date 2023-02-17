cc6b39b8b59bfac6938b80fdcb39b4a3c1f8e44e
YF <e0773591@u.nus.edu>
2023-02-17 14:27:56 +0800

    Fix Styling issues

--- src/test/java/seedu/address/testutil/EditPersonDescriptorBuilder.java
+++ src/test/java/seedu/address/testutil/EditPersonDescriptorBuilder.java
@@ -8,8 +8,8 @@ import seedu.address.logic.commands.EditCommand.EditPersonDescriptor;
 import seedu.address.model.student.Address;
 import seedu.address.model.student.Email;
 import seedu.address.model.student.Name;
-import seedu.address.model.student.Student;
 import seedu.address.model.student.Phone;
+import seedu.address.model.student.Student;
 import seedu.address.model.tag.Tag;
 
 /**
