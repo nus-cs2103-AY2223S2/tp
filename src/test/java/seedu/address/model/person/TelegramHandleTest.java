cea315334e305284d4a6de99eca229ad57616df3
Hafeez <izzhafeez@gmail.com>
2023-02-19 01:36:29 +0800

    Create tests for TelegramHandle

--- src/test/java/seedu/address/model/person/TelegramHandleTest.java
+++ src/test/java/seedu/address/model/person/TelegramHandleTest.java
@@ -1,2 +1,34 @@
-package seedu.address.model.person;public class TelegramHandleTest {
+package seedu.address.model.person;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
+
+public class TelegramHandleTest {
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new TelegramHandle(null));
+    }
+
+    @Test
+    public void constructor_invalidTelegram_throwsIllegalArgumentException() {
+        String invalidTelegramHandle = "";
+        assertThrows(IllegalArgumentException.class, () -> new TelegramHandle(invalidTelegramHandle));
+    }
+
+    @Test
+    public void isValidTelegramHandle() {
+        // null email
+        assertThrows(NullPointerException.class, () -> TelegramHandle.isValidTelegramHandle(null));
+
+        // blank email
+        assertFalse(TelegramHandle.isValidTelegramHandle("")); // empty string
+        assertFalse(TelegramHandle.isValidTelegramHandle(" ")); // spaces only
+
+        // missing parts
+        assertFalse(TelegramHandle.isValidTelegramHandle("linusrichards")); // missing '@' symbol
+        assertFalse(TelegramHandle.isValidTelegramHandle("@")); // missing domain name
+    }
 }
