ca0bc5ba7531c262d58ccb3229e1e1411c96877f
Hafeez <izzhafeez@gmail.com>
2023-02-21 11:50:43 +0800

    Fix user stub to pass AddCommandTest case

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -140,6 +140,16 @@ public class AddCommandTest {
         }
 
         @Override
+        public User getUser() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setUser(User user) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
         public ObservableList<Person> getFilteredPersonList() {
             throw new AssertionError("This method should not be called.");
         }
