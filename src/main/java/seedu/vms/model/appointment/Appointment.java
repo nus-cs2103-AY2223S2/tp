fc1f94ab75ca43f107f313e2b390bfb943ada1f5
DESKTOP-GGI1K1E\kelvi <e0726844@u.nus.edu>
2023-02-21 08:54:00 +0800

    Update Appointment class
    Previously,
    * Appointment class: constructor accepts Person and String.
                                          String was to be parsed to LocalDate.
    * Appointment class: appointmentTime used LocalDate.
    * Appointment class: use DateParseException class to handle invalid dates.
    
    Now,
    * Appointment class: constructor accepts Person and LocalDateTime.
    * Appointment class: appointmentTime used LocalDateTime.
    * Appointment class: removed DateParseException class.
    * Appointment class: added isSamePatient() method.
    * Appointment class: added equals() method.
    * Appointment class: added hashCode() method.

--- src/main/java/seedu/vms/model/appointment/Appointment.java
+++ src/main/java/seedu/vms/model/appointment/Appointment.java
@@ -1,44 +1,75 @@
 package seedu.vms.model.appointment;
 
-import seedu.vms.model.appointment.exceptions.DateParseException;
-import seedu.vms.model.person.Person;
+import static seedu.vms.commons.util.CollectionUtil.requireAllNonNull;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeParseException;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import seedu.vms.model.person.Person;
 
 /**
  * Represents an Appointment in the vaccine management system.
  */
 public class Appointment {
     private final Person person;
-    private LocalDate appointmentTime;
+    private LocalDateTime appointmentTime;
 
-    public Appointment(Person person, String appointmentTime) throws DateParseException {
-        try {
-            this.person = person;
-            this.appointmentTime = LocalDate.parse(appointmentTime);
-        } catch (DateTimeParseException e) {
-            // If given appointment time cannot be parsed to local date
-            throw new DateParseException();
-        }
+    public Appointment(Person person, LocalDateTime appointmentTime) {
+        requireAllNonNull(person, appointmentTime);
+        this.person = person;
+        this.appointmentTime = appointmentTime;
     }
 
     public Person getPerson() {
         return person;
     }
 
-    public LocalDate getAppointmentTime() {
+    public LocalDateTime getAppointmentTime() {
         return appointmentTime;
     }
 
-    public boolean setAppointmentTime(String appointmentTime) {
-        try {
-            this.appointmentTime = LocalDate.parse(appointmentTime);
+    public boolean setAppointmentTime(LocalDateTime appointmentTime) {
+        requireAllNonNull(appointmentTime);
+        this.appointmentTime = appointmentTime;
+        return true;
+    }
+
+    /**
+     * Returns true if both appointments have the same patient.
+     * This defines a weaker notion of equality between two patients.
+     */
+    public boolean isSamePatient(Person otherPerson) {
+        if (otherPerson == person) {
             return true;
-        } catch (DateTimeParseException e) {
-            // If given appointment time cannot be parsed to local date
+        }
+
+        return otherPerson != null
+                && otherPerson.getName().equals(person.getName());
+    }
+
+    /**
+     * Returns true if both appointments have the same patient and data fields.
+     * This defines a stronger notion of equality between two appointments.
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof Appointment)) {
             return false;
         }
+
+        Appointment otherAppointment = (Appointment) other;
+        return otherAppointment.getPerson().equals(getPerson())
+                && otherAppointment.getAppointmentTime().equals(getAppointmentTime());
+    }
+
+    @Override
+    public int hashCode() {
+        // use this method for custom fields hashing instead of implementing your own
+        return Objects.hash(person, appointmentTime);
     }
 
     @Override
