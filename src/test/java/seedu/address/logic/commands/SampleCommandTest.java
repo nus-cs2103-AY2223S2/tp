e3dc58b215dd0774e2ce9e9d6021cebb4df25c47
Hafeez <izzhafeez@gmail.com>
2023-02-22 20:46:24 +0800

    Fix sample command

---
+++ src/test/java/seedu/address/logic/commands/SampleCommandTest.java
@@ -0,0 +1,11 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
+
+public class SampleCommandTest {
+
+}
