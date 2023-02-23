d9d523102d5e5a80df94b7bb45505f86dc6e0eba
Nevin <nevinlyx00@gmail.com>
2023-02-24 01:10:26 +0800

    updated DrugModel classes

--- src/main/java/seedu/address/model/drug/ExpiryDate.java
+++ src/main/java/seedu/address/model/drug/ExpiryDate.java
@@ -1,7 +1,56 @@
 package seedu.address.model.drug;
 
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
 /**
  * Represents a drug's expiry date in the drug inventory
  */
 public class ExpiryDate {
+    public static final String MESSAGE_CONSTRAINTS = "Expiry dates should follow the DD-MM-YYYY format and only have" +
+            "numerical digits separated by - as inputs";
+
+    /*
+     * the format of "dd-mm-yyyy", where "dd" represents the day,
+     * "mm" represents the month, and "yyyy" represents the year
+     * with all characters being numerical digits separated by hypens.
+     */
+    public static final String VALIDATION_REGEX = "\\d{2}-\\d{2}-\\d{4}";
+
+    public final String expiryDate;
+
+    /**
+     * Constructs an {@code ExpiryDate}.
+     *
+     * @param expiryDate A valid expiryDate.
+     */
+    public ExpiryDate(String expiryDate) {
+        requireNonNull(expiryDate);
+        checkArgument(isValidExpiryDate(expiryDate), MESSAGE_CONSTRAINTS);
+        this.expiryDate = expiryDate;
+    }
+
+    /**
+     * Returns true if a given string is a valid email.
+     */
+    public static boolean isValidExpiryDate(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return expiryDate;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof ExpiryDate // instanceof handles nulls
+                && expiryDate.equals(((ExpiryDate) other).expiryDate)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return expiryDate.hashCode();
+    }
 }
--- src/main/java/seedu/address/model/drug/Purpose.java
+++ src/main/java/seedu/address/model/drug/Purpose.java
@@ -1,7 +1,55 @@
 package seedu.address.model.drug;
 
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
 /**
  * Represents a drug's purpose in the drug inventory
  */
 public class Purpose {
+    public static final String MESSAGE_CONSTRAINTS = "Purposes should start with at least 3 alphabets " +
+            "and only contain alphabets or whitespace";
+
+    /*
+     * The first 3 consecutive characters are upper or lowercase alphabetical characters,
+     * followed by 0 or more alphabetical or whitespace characters.
+     */
+    public static final String VALIDATION_REGEX = "[A-Za-z]{3,}[A-Za-z\\s]*";
+
+    public final String purpose;
+
+    /**
+     * Constructs an {@code Purpose}.
+     *
+     * @param purpose A valid purpose.
+     */
+    public Purpose(String purpose) {
+        requireNonNull(purpose);
+        checkArgument(isValidPurpose(purpose), MESSAGE_CONSTRAINTS);
+        this.purpose = purpose;
+    }
+
+    /**
+     * Returns true if a given string is a valid email.
+     */
+    public static boolean isValidPurpose(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return purpose;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Purpose // instanceof handles nulls
+                && purpose.equals(((Purpose) other).purpose)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return purpose.hashCode();
+    }
 }
