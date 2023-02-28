605d26d219705c73dd766bb039ce338e9c0d9eed
jefrai <30693850+jefrai@users.noreply.github.com>
2023-03-01 02:39:48 +0800

    Fix Checkstyle errors

--- src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
+++ src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
@@ -10,7 +10,16 @@ import seedu.address.logic.commands.RemarkCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.Remark;
 
+/**
+ * Parses input arguments and creates a new RemarkCommand object
+ */
 public class RemarkCommandParser implements Parser<RemarkCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the RemarkCommand
+     * and returns a RemarkCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
     @Override
     public RemarkCommand parse(String args) throws ParseException {
         requireNonNull(args);
