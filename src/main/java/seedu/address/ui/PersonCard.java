b6409cc89a1a31e22b9830cb4d0d728f71450dc7
kohkaixun <e0726276@u.nus.edu>
2023-03-01 15:13:47 +0800

    Revert "Merge branch 'tutorial-adding-command'"
    This reverts commit 9325f892f12035244902a2299ea3daf34c0bebeb, reversing
    changes made to d1b55b20c13126341abda7f3811ce58f1a8e2287.

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -24,9 +24,6 @@ public class PersonCard extends UiPart<Region> {
      * @see <a href="https://github.com/se-edu/addressbook-level4/issues/336">The issue on AddressBook level 4</a>
      */
 
-    @FXML
-    private Label remark;
-
     public final Person person;
 
     @FXML
@@ -58,7 +55,6 @@ public class PersonCard extends UiPart<Region> {
         person.getTags().stream()
                 .sorted(Comparator.comparing(tag -> tag.tagName))
                 .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
-        remark.setText(person.getRemark().value);
     }
 
     @Override
