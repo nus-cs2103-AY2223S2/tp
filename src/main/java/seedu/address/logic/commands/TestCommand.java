b7a43174f5072a2bef9db462b6ce3674e703f40b
Wang Jiefan <wng.jiefan@gmail.com>
2023-03-01 13:48:25 +0800

    Checkstyle

--- src/main/java/seedu/address/logic/commands/TestCommand.java
+++ src/main/java/seedu/address/logic/commands/TestCommand.java
@@ -3,7 +3,10 @@ package seedu.address.logic.commands;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
-public class TestCommand extends Command{
+/**
+ * Test command please ignore
+ */
+public class TestCommand extends Command {
 
     public static final String COMMAND_WORD = "test";
 
