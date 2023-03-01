42aebb2ccd6da067a0c5c52875ec5b1a11e22a05
zhacatomn <zhacatomn@gmail.com>
2023-03-01 16:27:43 +0800

    Extend ReadOnlyFriendlyLink from Elderly and Volunteer

--- src/main/java/seedu/address/model/ReadOnlyFriendlyLink.java
+++ src/main/java/seedu/address/model/ReadOnlyFriendlyLink.java
@@ -2,30 +2,18 @@ package seedu.address.model;
 
 import javafx.collections.ObservableList;
 import seedu.address.model.pair.Pair;
-import seedu.address.model.person.Elderly;
 import seedu.address.model.person.Person;
-import seedu.address.model.person.Volunteer;
 
 /**
- * Unmodifiable view of an address book
+ * Unmodifiable view of FriendlyLink.
  */
-public interface ReadOnlyFriendlyLink {
+public interface ReadOnlyFriendlyLink extends ReadOnlyElderly, ReadOnlyVolunteer {
 
     /**
      * Returns an unmodifiable view of the persons list.
      * This list will not contain any duplicate persons.
      */
     ObservableList<Person> getPersonList();
-    /**
-     * Returns an unmodifiable view of the volunteer list.
-     * This list will not contain any duplicate persons.
-     */
-    ObservableList<Volunteer> getVolunteerList();
-    /**
-     * Returns an unmodifiable view of the elderly list.
-     * This list will not contain any duplicate persons.
-     */
-    ObservableList<Elderly> getElderlyList();
 
     /**
      * Returns an unmodifiable view of the pairs list.
