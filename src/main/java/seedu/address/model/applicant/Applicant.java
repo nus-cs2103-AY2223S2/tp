881d28a26156963aeed8bef394aae2133335192d
homuntan02 <homuntan02@gmail.com>
2023-03-01 20:16:27 +0800

    Creat Name class

--- src/main/java/seedu/address/model/applicant/Applicant.java
+++ src/main/java/seedu/address/model/applicant/Applicant.java
@@ -1,23 +1,58 @@
 package seedu.address.model.applicant;
 
-public class Applicant {
-    private String name;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Person;
+import seedu.address.model.tag.Tag;
 
-    /**
-     * constructs an Applicant.
-     *
-     * @param name the name of the applicant.
-     */
-    public Applicant(String name) {
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * Represents an Applicant in the address book.
+ * Guarantees: details are present and not null, field values are validated, immutable.
+ */
+public class Applicant {
+    // Identity fields
+    private final Name name;
+
+    public Applicant(Name name) {
         this.name = name;
     }
 
-    /**
-     * gets the name of an applicant.
-     *
-     * @return the name of the applicant.
-     */
-    public String getName() {
-        return this.name;
+    public Name getName() {
+        return name;
     }
+
+    /**
+     * Returns true if both applicants have the same identity and data fields.
+     * This defines a stronger notion of equality between two applicants.
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof Applicant)) {
+            return false;
+        }
+
+        Applicant otherApplicant = (Applicant) other;
+        return otherApplicant.getName().equals(getName());
+    }
+
+    @Override
+    public int hashCode() {
+        // use this method for custom fields hashing instead of implementing your own
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append(getName());
+
+        return builder.toString();
+    }
+
 }
