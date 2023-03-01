f576a0c745d0404c01dfc6a69a17f84fca7ce530
cydtseng <tsengchenyu@gmail.com>
2023-03-01 19:35:57 +0800

    Add implementation for UniquePatientList and two additional Exception classes for Patient

--- src/main/java/seedu/address/model/person/patient/UniquePatientList.java
+++ src/main/java/seedu/address/model/person/patient/UniquePatientList.java
@@ -1,4 +1,129 @@
 package seedu.address.model.person.patient;
 
-public class UniquePatientList {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.UniquePersonList;
+import seedu.address.model.person.exceptions.DuplicatePatientException;
+import seedu.address.model.person.exceptions.PatientNotFoundException;
+import seedu.address.model.person.exceptions.PersonNotFoundException;
+
+import java.util.Iterator;
+import java.util.List;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+public class UniquePatientList implements Iterable<Patient> {
+
+    private final ObservableList<Patient> internalList = FXCollections.observableArrayList();
+    private final ObservableList<Patient> internalUnmodifiableList =
+            FXCollections.unmodifiableObservableList(internalList);
+
+    /**
+     * Returns true if the list contains an equivalent person as the given argument
+     */
+    public boolean contains(Patient toCheck) {
+        requireNonNull(toCheck);
+        return internalList.stream().anyMatch(toCheck::isSamePerson);
+    }
+
+    /**
+     * Adds a patient to the list.
+     * The patient must not already exist in the list.
+     */
+    public void add(Patient toAdd) {
+        requireNonNull(toAdd);
+        if (contains(toAdd)) {
+            throw new DuplicatePatientException();
+        }
+        internalList.add(toAdd);
+    }
+
+    /**
+     * Replaces the patient {@code target} in the list with {@code editedPatient}
+     * {@code target} must exist in the list.
+     * The patient identity of {@code editedPatient} must not be the same as
+     * another existing patient in the list.
+     */
+    public void setPatient(Patient target, Patient editedPatient) {
+        requireAllNonNull(target, editedPatient);
+        int index = internalList.indexOf(target);
+        if (index == -1) {
+            throw new PatientNotFoundException();
+        }
+
+        if(!target.isSamePerson(editedPatient) && contains(editedPatient)) {
+            throw new DuplicatePatientException();
+        }
+        internalList.set(index, editedPatient);
+    }
+
+
+    /**
+     * Removes the equivalent person from the list.
+     * The person must exist in the list.
+     */
+    public void remove(Patient toRemove) {
+        requireNonNull(toRemove);
+        if (!internalList.remove(toRemove)) {
+            throw new PatientNotFoundException();
+        }
+    }
+
+    public void setPersons(UniquePatientList replacement) {
+        requireNonNull(replacement);
+        internalList.setAll(replacement.internalList);
+    }
+
+    /**
+     * Replaces the contents of this list with {@code patients}
+     * {@code patients} must not contain duplicate persons.
+     */
+    public void setPatients(List<Patient> patients) {
+        requireNonNull(patients);
+        if(!patientsAreUnique(patients)) {
+            throw new DuplicatePatientException();
+        }
+        internalList.setAll(patients);
+    }
+
+
+    /**
+     * Returns the backing list as an unmodifiable {@code ObservableList}.
+     */
+    public ObservableList<Patient> asUnmodifiableObservableList() {
+        return internalUnmodifiableList;
+    }
+
+    @Override
+    public Iterator<Patient> iterator() {
+        return internalList.iterator();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this
+                || (other instanceof UniquePatientList
+                    && internalList.equals(((UniquePatientList) other).internalList));
+    }
+
+    @Override
+    public int hashCode() {
+        return internalList.hashCode();
+    }
+
+    /**
+     * Returns true if {@code patients} contains only unique patients.
+     */
+    private boolean patientsAreUnique(List<Patient> patients) {
+        for (int i = 0; i < patients.size() - 1; i++) {
+            for (int j = i + 1; j < patients.size(); j++) {
+                if (patients.get(i).isSamePerson(patients.get(j))) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
