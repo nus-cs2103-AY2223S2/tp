99848a72cf62b6aaefeb1473fe6f21240d22e04b
daitenshionyan <100074448+daitenshionyan@users.noreply.github.com>
2023-03-02 22:41:07 +0800

    Add logical test for same brand dose requirement

--- src/test/java/seedu/vms/storage/vaccination/VaxTypeLoaderTest.java
+++ src/test/java/seedu/vms/storage/vaccination/VaxTypeLoaderTest.java
@@ -1,12 +1,19 @@
 package seedu.vms.storage.vaccination;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.vms.model.vaccination.VaxTestingUtil.assertVaxType;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.HashSet;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import seedu.vms.model.vaccination.VaxChecker;
+import seedu.vms.model.vaccination.VaxRecord;
 import seedu.vms.model.vaccination.VaxRequirement;
 import seedu.vms.model.vaccination.VaxType;
 
@@ -30,10 +37,18 @@ public class VaxTypeLoaderTest {
                     "Tromethamine",
                     "Tris(hydroxymethyl)aminoethane hydrochloride"))));
 
+    private static final String TYPE_2_NAME = "Dose 2 (Pfizer)";
+    private static final String TYPE_3_NAME = "Dose 1 (Moderna)";
+
+
+    @BeforeAll
+    public static void load() throws Exception {
+        VaxTypeLoader.load();
+    }
+
 
     @Test
     public void loadTest() throws Exception {
-        VaxTypeLoader.load();
         assertVaxType(VaxType.get(TYPE_1_NAME),
                 TYPE_1_NAME,
                 TYPE_1_GROUPS,
@@ -42,5 +57,33 @@ public class VaxTypeLoaderTest {
                 TYPE_1_MIN_SPACING,
                 TYPE_1_REQUIREMENTS,
                 TYPE_1_ALLERGY_REQS);
+    }
+
+
+    @Test
+    public void logicalTest() {
+        // first 2 doses must be the same brand
+        VaxType doseP1 = VaxType.get(TYPE_1_NAME);
+        VaxType doseP2 = VaxType.get(TYPE_2_NAME);
+        VaxType doseM1 = VaxType.get(TYPE_3_NAME);
+
+        LocalDateTime dose1Time = LocalDateTime.now();
+        LocalDateTime dose2Time = dose1Time.plus(TYPE_1_MIN_SPACING, ChronoUnit.DAYS);
+
+        List<VaxRecord> validRecs = List.of(new VaxRecord(doseP1, dose1Time));
+        List<VaxRecord> invalidRecs = List.of(new VaxRecord(doseM1, dose1Time));
+
+        assertTrue(VaxChecker.check(
+                doseP2,
+                TYPE_1_MIN_AGE,
+                new HashSet<>(),
+                validRecs,
+                dose2Time));
+        assertFalse(VaxChecker.check(
+            doseP2,
+            TYPE_1_MIN_AGE,
+            new HashSet<>(),
+            invalidRecs,
+            dose2Time));
     }
 }
