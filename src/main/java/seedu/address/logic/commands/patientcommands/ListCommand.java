2e90c6160c7f85902df1dc44e8dbb7ed7a3a2218
Nevin <nevinlyx00@gmail.com>
2023-03-03 15:57:10 +0800

    List Command for Patient

--- src/main/java/seedu/address/logic/commands/patientcommands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/patientcommands/ListCommand.java
@@ -5,14 +5,19 @@ import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.CareFlowModel;
 
+import static java.util.Objects.requireNonNull;
+
 /**
  * Lists all patients in the patient records
  */
 public class ListCommand extends Command {
     public static final String COMMAND_WORD = "list";
+    public static final String MESSAGE_SUCCESS = "Listed all patients";
 
     @Override
     public CommandResult execute(CareFlowModel model) throws CommandException {
-        return null;
+        requireNonNull(model);
+        model.updateFilteredPatientList(model.PREDICATE_SHOW_ALL_PATIENTS);
+        return new CommandResult(MESSAGE_SUCCESS);
     }
 }
