1cca2c2d47cf0ad13c84e9051cc4be23392efdc3
Aarnav Srinivasan <aarnav.srinivasan@gmail.com>
2023-03-03 10:45:35 +0800

    Rename main package to seedu.calidr

---
+++ src/test/java/seedu/calidr/logic/commands/ExitCommandTest.java
@@ -0,0 +1,20 @@
+package seedu.calidr.logic.commands;
+
+import static seedu.calidr.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.calidr.logic.commands.ExitCommand.MESSAGE_EXIT_ACKNOWLEDGEMENT;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.calidr.model.Model;
+import seedu.calidr.model.ModelManager;
+
+public class ExitCommandTest {
+    private Model model = new ModelManager();
+    private Model expectedModel = new ModelManager();
+
+    @Test
+    public void execute_exit_success() {
+        CommandResult expectedCommandResult = new CommandResult(MESSAGE_EXIT_ACKNOWLEDGEMENT, false, true);
+        assertCommandSuccess(new ExitCommand(), model, expectedCommandResult, expectedModel);
+    }
+}
