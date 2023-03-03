0c2ebea7a2627da47667f586388abc381db61fb9
Karen-Lam <karenycarrot@gmail.com>
2023-03-03 14:58:03 +0800

    Add Exceptions files related to Internship
    Required: Need to delete DuplicatePersonException.java and
    PersonNotFoundException.java after merge with master branch

--- src/main/java/seedu/address/model/person/exceptions/InternshipNotFoundException.java
+++ src/main/java/seedu/address/model/person/exceptions/InternshipNotFoundException.java
@@ -1,2 +1,6 @@
-package seedu.address.model.person.exceptions;public class InternshipNotFoundException {
-}
+package seedu.address.model.person.exceptions;
+
+/**
+ * Signals that the operation is unable to find the specified internship.
+ */
+public class InternshipNotFoundException extends RuntimeException {}
