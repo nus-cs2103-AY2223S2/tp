0fee27d08ab5a7117622d40950cc38c5e2d1dbbd
Merrick <merrickneo@gmail.com>
2023-03-03 23:16:02 +0800

    Change delete command to delete applicants based on their name and phone number

--- src/test/java/seedu/address/logic/parser/DeleteCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/DeleteCommandParserTest.java
@@ -1,13 +1,17 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_AMY;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
-import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.DeleteCommand;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.NamePhoneNumberPredicate;
+import seedu.address.model.person.Phone;
 
 /**
  * As we are only doing white-box testing, our test cases do not cover path variations
@@ -22,11 +26,21 @@ public class DeleteCommandParserTest {
 
     @Test
     public void parse_validArgs_returnsDeleteCommand() {
-        assertParseSuccess(parser, "1", new DeleteCommand(INDEX_FIRST_PERSON));
+        // no leading and trailing whitespaces
+        DeleteCommand expectedDeleteCommand = new DeleteCommand(new NamePhoneNumberPredicate(new Name(VALID_NAME_AMY), new Phone(VALID_PHONE_AMY)));
+        assertParseSuccess(parser, "n/AMY BEE p/11111111", expectedDeleteCommand);
+
+        // multiple whitespaces between keywords
+        assertParseSuccess(parser, " \n n/AMY BEE \n \t p/11111111  \t", expectedDeleteCommand);
     }
 
     @Test
     public void parse_invalidArgs_throwsParseException() {
         assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_emptyArg_throwsParseException() {
+        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
     }
 }
