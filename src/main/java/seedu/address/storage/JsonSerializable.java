59ad2e7ff25899faad28718df9b296d26895655d
Zong Xun <zxun2000@gmail.com>
2023-03-05 00:34:20 +0800

    Update JsonSerializable to accept FriendlyLink as parameter

--- src/main/java/seedu/address/storage/JsonSerializable.java
+++ src/main/java/seedu/address/storage/JsonSerializable.java
@@ -1,12 +1,13 @@
 package seedu.address.storage;
 
 import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.FriendlyLink;
 
 /**
  * Indicates that an Entity is serializable
  *
- * @param <T> The type to serialize to.
+ * @param <T> The model type to serialize to
  */
 public interface JsonSerializable<T> {
-    T toModelType() throws IllegalValueException;
+    T toModelType(FriendlyLink friendlyLink) throws IllegalValueException;
 }
