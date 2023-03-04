f1e9bfbc921f88478c1dafd35117964fe1be2f17
daitenshionyan <100074448+daitenshionyan@users.noreply.github.com>
2023-03-04 18:44:30 +0800

    Extract full sample VaxType data out

--- src/test/java/seedu/vms/storage/vaccination/VaxTypeLoaderTest.java
+++ src/test/java/seedu/vms/storage/vaccination/VaxTypeLoaderTest.java
@@ -3,14 +3,11 @@ package seedu.vms.storage.vaccination;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.vms.commons.exceptions.IllegalValueException;
-import seedu.vms.model.vaccination.Requirement;
-import seedu.vms.model.vaccination.Requirement.RequirementType;
+import seedu.vms.testutil.SampleVaxTypeData;
 import seedu.vms.model.vaccination.VaxTestingUtil;
 import seedu.vms.model.vaccination.VaxType;
 import seedu.vms.model.vaccination.VaxTypeManager;
@@ -34,39 +31,19 @@ public class VaxTypeLoaderTest {
     private static final String FILE_VALID_MULTIPLE = "ValidMultipleTypes.json";
     private static final String FILE_EMPTY = "ZeroTypes.json";
 
-    // Expected parameter values of dose 1 pfizer
-    private static final String NAME = "Dose 1 (Pfizer)";
-    private static final HashSet<String> GROUPS =
-            new HashSet<>(List.of("DOSE 1", "Pfizer", "Vaccination"));
-    private static final int MIN_AGE = 5;
-    private static final int MAX_AGE = Integer.MAX_VALUE;
-    private static final int MIN_SPACING = 56;
-    private static final List<Requirement> HISTORY_REQS = List.of(
-            new Requirement(RequirementType.NONE, new HashSet<>(List.of("DOSE 1"))));
-    private static final List<Requirement> ALLERGY_REQS = List.of(
-            new Requirement(RequirementType.NONE, new HashSet<>(List.of(
-                    "((4-Hydroxybutyl)azanediyl)bis(hexane-6,1-diyl)bis(2-hexyldecanoate)",
-                    "2-[(polyethylene glycol)-2000]-N,N-ditetradecylacetamide",
-                    "1,2-Distearoyl-sn-glycero-3-phosphocholine",
-                    "Cholesterol",
-                    "Sucrose",
-                    "Phosphate",
-                    "Tromethamine",
-                    "Tris(hydroxymethyl)aminoethane hydrochloride"))));
-
 
     @Test
     public void load_resource() throws Exception {
         VaxTypeManager storage = VaxTypeLoader.load();
-        VaxType vaxType = storage.get(NAME).get();
+        VaxType vaxType = storage.get(SampleVaxTypeData.NAME).get();
         VaxTestingUtil.assertVaxType(vaxType,
-                NAME,
-                GROUPS,
-                MIN_AGE,
-                MAX_AGE,
-                MIN_SPACING,
-                HISTORY_REQS,
-                ALLERGY_REQS);
+                SampleVaxTypeData.NAME,
+                SampleVaxTypeData.GROUPS,
+                SampleVaxTypeData.MIN_AGE,
+                SampleVaxTypeData.MAX_AGE,
+                SampleVaxTypeData.MIN_SPACING,
+                SampleVaxTypeData.HISTORY_REQS,
+                SampleVaxTypeData.ALLERGY_REQS);
     }
 
 
@@ -97,15 +74,15 @@ public class VaxTypeLoaderTest {
                 VaxType.DEFAULT_MIN_SPACING,
                 VaxType.DEFAULT_HISTORY_REQS,
                 VaxType.DEFAULT_ALLERGY_REQS);
-        vaxType = storage.get(NAME).get();
+        vaxType = storage.get(SampleVaxTypeData.NAME).get();
         VaxTestingUtil.assertVaxType(vaxType,
-                NAME,
-                GROUPS,
-                MIN_AGE,
-                MAX_AGE,
-                MIN_SPACING,
-                HISTORY_REQS,
-                ALLERGY_REQS);
+                SampleVaxTypeData.NAME,
+                SampleVaxTypeData.GROUPS,
+                SampleVaxTypeData.MIN_AGE,
+                SampleVaxTypeData.MAX_AGE,
+                SampleVaxTypeData.MIN_SPACING,
+                SampleVaxTypeData.HISTORY_REQS,
+                SampleVaxTypeData.ALLERGY_REQS);
     }
 
 
