73d7e9762f79b2278347daebc62fd7a5df97779e
Ai Bo <bo.ai@u.nus.edu>
2023-03-04 20:59:03 +0800

    add location object

---
+++ src/main/java/seedu/address/model/location/Location.java
@@ -0,0 +1,50 @@
+package seedu.address.model.location;
+
+import java.util.UUID;
+
+import seedu.address.model.item.Identifiable;
+
+/**
+ * Location is a unit place that the flight can travel to or
+ *  arrive at.
+ */
+public class Location implements Identifiable {
+    private final String name;
+    private final String id;
+
+    /**
+     * Creates a Location object with the given name.
+     * @param name: name of the location
+     */
+    public Location(String name) {
+        this(UUID.randomUUID().toString(), name);
+    }
+
+    /**
+     * Creates a Location object from the given id and name
+     * @param id a unique id assigned to the location
+     * @param name the name of the location
+     */
+    public Location(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    /**
+     * Returns the name of the location in string.
+     * @return the name of the location
+     */
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return getName();
+    }
+
+    @Override
+    public String getId() {
+        return id;
+    }
+}
