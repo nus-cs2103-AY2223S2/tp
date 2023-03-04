66f498e6befaa7c5b069fa21d1939f0efbed2ea0
Xiuxuan Wang <36021591+wxxedu@users.noreply.github.com>
2023-03-04 19:46:29 +0800

    fix imports

--- src/test/java/seedu/address/logic/core/WingmanParserTest.java
+++ src/test/java/seedu/address/logic/core/WingmanParserTest.java
@@ -6,7 +6,6 @@ import static seedu.address.testutil.Assert.assertThrows;
 
 import java.util.List;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
