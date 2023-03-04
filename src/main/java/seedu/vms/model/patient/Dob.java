b2a7974c122138711265eb7788a1fa2af4bf43c2
francisyzy <francisyzy@me.com>
2023-03-04 14:57:19 +0800

    Add dob
    Yet to implement into patient yet
    
    Related to #1

---
+++ src/main/java/seedu/vms/model/patient/Dob.java
@@ -0,0 +1,83 @@
+package seedu.vms.model.patient;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.vms.commons.util.AppUtil.checkArgument;
+import static seedu.vms.logic.parser.ParserUtil.parseDate;
+import static java.time.format.DateTimeFormatter.ISO_LOCAL_DATE;
+
+import java.time.LocalDateTime;
+
+import seedu.vms.logic.parser.exceptions.ParseException;
+
+/**
+ * Represents a Patient's dob in the dob book.
+ * It is represented as LocalDateTime with the time set to 0000.
+ * Guarantees: immutable; is valid as declared in {@link #isValidDob(String)}
+ */
+public class Dob {
+
+    public static final String MESSAGE_CONSTRAINTS = "Date of birth can take any date earlier than today, and it should not be blank";
+
+    public final LocalDateTime value;
+
+    /**
+     * Constructs an {@code Dob}.
+     *
+     * @param dob A valid date of birth.
+     * @throws ParseException
+     */
+    public Dob(String dob) throws ParseException {
+        requireNonNull(dob);
+        checkArgument(isValidDob(dob), MESSAGE_CONSTRAINTS);
+        value = parseDate(dob);
+    }
+
+    /**
+     * Constructs an {@code Dob}.
+     *
+     * @param dob A valid date of birth.
+     */
+    public Dob(LocalDateTime dob) {
+        requireNonNull(dob);
+        checkArgument(isValidDob(dob), MESSAGE_CONSTRAINTS);
+        value = dob;
+    }
+
+    /**
+     * Returns true if a given string is a valid date that is before than today
+     */
+    public static boolean isValidDob(String test) {
+        LocalDateTime testDate;
+        try {
+            testDate = parseDate(test);
+        } catch (ParseException e) {
+            return false;
+        }
+        return testDate.isBefore(LocalDateTime.now());
+    }
+
+    /**
+     * Returns true if a given string is a valid date that is before than today
+     */
+    public static boolean isValidDob(LocalDateTime test) {
+        return test.isBefore(LocalDateTime.now());
+    }
+
+    @Override
+    public String toString() {
+        return value.format(ISO_LOCAL_DATE);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Dob // instanceof handles nulls
+                        && value.equals(((Dob) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+}
---
+++ src/test/java/seedu/vms/model/patient/DobTest.java
@@ -0,0 +1,43 @@
+package seedu.vms.model.patient;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.vms.testutil.Assert.assertThrows;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.vms.logic.parser.exceptions.ParseException;
+
+public class DobTest {
+
+    @Test
+    public void constructor_invalidDob_throwsIllegalArgumentException() {
+        String invalidDob = "";
+        assertThrows(IllegalArgumentException.class, () -> new Dob(invalidDob));
+    }
+
+    @Test
+    public void isValidDob() {
+        // invalid dobs
+        assertFalse(Dob.isValidDob("")); // empty string
+        assertFalse(Dob.isValidDob(" ")); // spaces only
+        assertFalse(Dob.isValidDob(LocalDateTime.now().plusDays(1))); // future dob
+        assertFalse(Dob.isValidDob("2020-22-01")); // Invalid format
+
+        // valid dobs
+        assertTrue(Dob.isValidDob(LocalDateTime.now().minusDays(1))); // yesterday dob
+        assertTrue(Dob.isValidDob("2020-02-29")); // Leap
+        assertTrue(Dob.isValidDob("1903-05-29")); // Very old person
+        assertTrue(Dob.isValidDob("2000-01-01")); // Y2k
+    }
+    
+    @Test
+    public void isValidFormat() throws ParseException {
+        final String expected = "2020-03-19";
+
+        assertEquals(expected, new Dob(expected).toString());
+    }
+}
