1789676750981496b18cca2365dfa35a80159a31
Irminrics <irminrics@gmail.com>
2023-03-06 02:18:14 +0800

    Add test for isSameEvent method

--- src/test/java/seedu/address/model/event/EventTest.java
+++ src/test/java/seedu/address/model/event/EventTest.java
@@ -20,13 +20,16 @@ public class EventTest {
         Event carnivalCopy = new EventBuilder(CARNIVAL).build();
         assertTrue(CARNIVAL.equals(carnivalCopy));
         assertTrue(CARNIVAL.hashCode() == carnivalCopy.hashCode());
+        assertTrue(CARNIVAL.isSameEvent(carnivalCopy));
 
         // same object -> returns true
         assertTrue(CARNIVAL.equals(CARNIVAL));
         assertTrue(CARNIVAL.hashCode() == CARNIVAL.hashCode());
+        assertTrue(CARNIVAL.isSameEvent(CARNIVAL));
 
         // null -> returns false
         assertFalse(CARNIVAL.equals(null));
+        assertFalse(CARNIVAL.isSameEvent(null));
 
         // different type -> returns false
         assertFalse(CARNIVAL.equals(511));
@@ -34,6 +37,7 @@ public class EventTest {
         // different event -> returns false
         assertFalse(CARNIVAL.equals(SPORTS_DAY));
         assertFalse(CARNIVAL.hashCode() == SPORTS_DAY.hashCode());
+        assertFalse(CARNIVAL.isSameEvent(SPORTS_DAY));
 
         Event editedCarnival = new EventBuilder(CARNIVAL).withName(VALID_EVENT_NAME_CARNIVAL)
                 .withStartDateTime(VALID_START_DATE_TIME_CARNIVAL)
@@ -42,6 +46,6 @@ public class EventTest {
         // different event with same name -> returns false
         assertFalse(CARNIVAL.equals(editedCarnival));
         assertFalse(CARNIVAL.hashCode() == editedCarnival.hashCode());
-
+        assertFalse(CARNIVAL.isSameEvent(editedCarnival));
     }
 }
