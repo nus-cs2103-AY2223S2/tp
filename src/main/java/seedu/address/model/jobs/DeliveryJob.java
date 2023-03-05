e8fed46fd67ad384f9f385ffc11dc31254da107a
c0j0s <c0j0s@outlook.com>
2023-03-06 00:08:05 +0800

    Add delivery job and package model

---
+++ src/main/java/seedu/address/model/jobs/DeliveryJob.java
@@ -0,0 +1,76 @@
+package seedu.address.model.jobs;
+
+import java.util.List;
+import java.util.UUID;
+
+import seedu.address.model.person.Person;
+
+/**
+ * Represents delivery jobs entities.
+ */
+public class DeliveryJob {
+    // Identity fields
+    private final UUID jobId;
+
+    // Delivery informations
+    private Person recepient;
+    private String deliverSlot; // TODO: Update data type when confirmed
+    private List<DeliveryPackage> packages;
+    private double earning;
+
+    /**
+     * Constructs a job entity.
+     *
+     * @param recepient
+     * @param deliverSlot
+     * @param packages
+     * @param earning
+     */
+    public DeliveryJob(Person recepient, String deliverSlot, List<DeliveryPackage> packages, double earning) {
+        this(UUID.randomUUID(), recepient, deliverSlot, packages, earning);
+    }
+
+    private DeliveryJob(UUID jobId, Person recepient, String deliverSlot, List<DeliveryPackage> packages, double earning) {
+        this.jobId = jobId;
+        this.recepient = recepient;
+        this.deliverSlot = deliverSlot;
+        this.packages = packages;
+        this.earning = earning;
+    }
+
+    public Person getRecepient() {
+        return recepient;
+    }
+
+    public String getDeliverSlot() {
+        return deliverSlot;
+    }
+
+    public List<DeliveryPackage> getPackages() {
+        return packages;
+    }
+
+    public double getEarning() {
+        return earning;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        
+        String outString = "Job [%s]\n"
+            + "receipent: %s\n"
+            + "slot: %s\n"
+            + "earn: $%s\n";
+
+        builder.append(
+            String.format(outString, 
+                jobId, 
+                getRecepient(), 
+                getDeliverSlot(), 
+                getEarning())
+            );
+
+        return builder.toString();
+    }
+}
