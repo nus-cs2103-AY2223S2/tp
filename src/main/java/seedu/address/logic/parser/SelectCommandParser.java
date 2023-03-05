7da94a056fb45207560dc1defe2eca0ee92a2c4b
tituswe <tituslowesg@gmail.com>
2023-03-06 01:20:56 +0800

    Fix Javadoc bug

--- src/main/java/seedu/address/logic/parser/SelectCommandParser.java
+++ src/main/java/seedu/address/logic/parser/SelectCommandParser.java
@@ -12,8 +12,9 @@ import seedu.address.logic.parser.exceptions.ParseException;
 public class SelectCommandParser implements Parser<SelectCommand> {
 
     /**
-     * Parses the given {@code String} of arguments in the context of the DeleteCommand
+     * Parses the given {@code String} of arguments in the context of the SelectCommand
      * and returns a DeleteCommand object for execution.
+     *
      * @throws ParseException if the user input does not conform the expected format
      */
     public SelectCommand parse(String args) throws ParseException {
