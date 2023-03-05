33b6e07bc4835a9f5533ae95aefbc3d9ab87595d
dawg420 <88711624+dawg420@users.noreply.github.com>
2023-03-06 04:45:22 +0800

    Revert "Remark functionality"
    This reverts commit e40916ebe3e9f9b36979f733bc573827ba657389.

--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -1,12 +1,13 @@
 package seedu.address.testutil;
+
 import java.util.HashSet;
 import java.util.Set;
+
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
-import seedu.address.model.person.Remark;
 import seedu.address.model.tag.Tag;
 import seedu.address.model.util.SampleDataUtil;
 
@@ -14,25 +15,26 @@ import seedu.address.model.util.SampleDataUtil;
  * A utility class to help with building Person objects.
  */
 public class PersonBuilder {
-    public static final String DEFAULT_NAME = "Alice Pauline";
+
+    public static final String DEFAULT_NAME = "Amy Bee";
     public static final String DEFAULT_PHONE = "85355255";
-    public static final String DEFAULT_EMAIL = "alice@gmail.com";
+    public static final String DEFAULT_EMAIL = "amy@gmail.com";
     public static final String DEFAULT_ADDRESS = "123, Jurong West Ave 6, #08-111";
-    public static final String DEFAULT_REMARK = "She likes aardvarks.";
 
     private Name name;
     private Phone phone;
     private Email email;
     private Address address;
-    private Remark remark;
     private Set<Tag> tags;
 
+    /**
+     * Creates a {@code PersonBuilder} with the default details.
+     */
     public PersonBuilder() {
         name = new Name(DEFAULT_NAME);
         phone = new Phone(DEFAULT_PHONE);
         email = new Email(DEFAULT_EMAIL);
         address = new Address(DEFAULT_ADDRESS);
-        remark = new Remark(DEFAULT_REMARK);
         tags = new HashSet<>();
     }
 
@@ -44,7 +46,6 @@ public class PersonBuilder {
         phone = personToCopy.getPhone();
         email = personToCopy.getEmail();
         address = personToCopy.getAddress();
-        remark = personToCopy.getRemark();
         tags = new HashSet<>(personToCopy.getTags());
     }
 
@@ -55,6 +56,7 @@ public class PersonBuilder {
         this.name = new Name(name);
         return this;
     }
+
     /**
      * Parses the {@code tags} into a {@code Set<Tag>} and set it to the {@code Person} that we are building.
      */
@@ -62,6 +64,7 @@ public class PersonBuilder {
         this.tags = SampleDataUtil.getTagSet(tags);
         return this;
     }
+
     /**
      * Sets the {@code Address} of the {@code Person} that we are building.
      */
@@ -69,6 +72,7 @@ public class PersonBuilder {
         this.address = new Address(address);
         return this;
     }
+
     /**
      * Sets the {@code Phone} of the {@code Person} that we are building.
      */
@@ -76,6 +80,7 @@ public class PersonBuilder {
         this.phone = new Phone(phone);
         return this;
     }
+
     /**
      * Sets the {@code Email} of the {@code Person} that we are building.
      */
@@ -84,18 +89,8 @@ public class PersonBuilder {
         return this;
     }
 
-    /**
-     * Sets the {@code Remark} of the {@code Person} that we are building.
-     */
-    public PersonBuilder withRemark(String remark) {
-        this.remark = new Remark(remark);
-        return this;
-    }
-
-
-
     public Person build() {
-        return new Person(name, phone, email, address, remark, tags);
+        return new Person(name, phone, email, address, tags);
     }
 
 }
