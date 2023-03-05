669d5cb4d4f2d7d8f4c34836f5a2def95b7bdb49
hansstanley <nusstuhss@gmail.com>
2023-03-05 10:21:51 +0800

    Replace CalendarFX with custom calendar view

--- src/main/java/seedu/address/ui/body/calendar/CalendarPanel.java
+++ src/main/java/seedu/address/ui/body/calendar/CalendarPanel.java
@@ -21,38 +21,10 @@ import seedu.address.ui.UiPart;
 public class CalendarPanel extends UiPart<Region> {
     private static final String FXML = "body/calendar/CalendarPanel.fxml";
 
-    @FXML
-    private StackPane calendarPlaceholder;
-
-
     /**
      * Creates a {@code CalendarPanel}.
      */
     public CalendarPanel() {
         super(FXML);
-
-        calendarPlaceholder.getChildren().add(getCalendarNode());
-    }
-
-    private Node getCalendarNode() {
-        Calendar<String> calendar = new Calendar<>("Events");
-        calendar.setStyle(Calendar.Style.STYLE5);
-        calendar.setReadOnly(true); // disables entry modification
-
-        CalendarSource calendarSource = new CalendarSource("Calendars");
-        calendarSource.getCalendars().add(calendar);
-
-        DetailedWeekView weekView = new DetailedWeekView();
-        weekView.setContextMenuCallback(null); // disables the context menu
-        weekView.setEntryFactory(param -> null); // disables entry creation
-        weekView.getCalendarSources().add(calendarSource);
-
-        // Updates the current date and time every 10 seconds
-        Executors.newSingleThreadScheduledExecutor().scheduleWithFixedDelay(() -> {
-            weekView.setToday(LocalDate.now());
-            weekView.setTime(LocalTime.now());
-        }, 10, 10, TimeUnit.SECONDS);
-
-        return weekView;
     }
 }
