1e5888d20389f21aa5ce031b7aab727d477b1dc4
tituswe <tituslowesg@gmail.com>
2023-03-05 19:59:19 +0800

    Implement Select Command

---
+++ src/main/java/seedu/address/logic/commands/SelectCommand.java
@@ -0,0 +1,50 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+import java.util.List;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.exceptions.CommandException;
+
+import seedu.address.model.Model;
+import seedu.address.model.person.Person;
+
+public class SelectCommand extends Command {
+
+    public static final String COMMAND_WORD = "select";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Selects the person identified by the index number used in the displayed person list.\n"
+            + "Parameters: INDEX (must be a positive integer)\n"
+            + "Example: " + COMMAND_WORD + " 1";
+
+    public static final String MESSAGE_SELECT_PERSON_SUCCESS = "Selected Person: %1$s";
+
+    private final Index targetIndex;
+
+    public SelectCommand(Index targetIndex) {
+        this.targetIndex = targetIndex;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireAllNonNull(model);
+        List<Person> lastShownList = model.getFilteredPersonList();
+
+        if (targetIndex.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+
+        Person personToSelect = lastShownList.get(targetIndex.getZeroBased());
+        return new CommandResult(String.format(MESSAGE_SELECT_PERSON_SUCCESS, personToSelect));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof SelectCommand // instanceof handles nulls
+                && targetIndex.equals(((SelectCommand) other).targetIndex)); // state check
+    }
+}
---
+++ src/main/java/seedu/address/logic/parser/SelectCommandParser.java
@@ -0,0 +1,27 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.DeleteCommand;
+import seedu.address.logic.commands.SelectCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+public class SelectCommandParser implements Parser<SelectCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the DeleteCommand
+     * and returns a DeleteCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public SelectCommand parse(String args) throws ParseException {
+        try {
+            Index index = ParserUtil.parseIndex(args);
+            return new SelectCommand(index);
+        } catch (ParseException pe) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, SelectCommand.MESSAGE_USAGE), pe);
+        }
+    }
+
+}
