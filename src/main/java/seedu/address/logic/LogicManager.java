bdd46be3ce7b6f65138846a406e7b95950c34de3
MA-YUANXIN\yuanx <e0726041@u.nus.edu>
2023-03-06 02:31:54 +0800

    Update checkstyle errors

--- src/main/java/seedu/address/logic/LogicManager.java
+++ src/main/java/seedu/address/logic/LogicManager.java
@@ -66,7 +66,7 @@ public class LogicManager implements Logic {
     }
 
     @Override
-    public ObservableList<Student> getFilteredStudentList() {return model.getFilteredStudentList();}
+    public ObservableList<Student> getFilteredStudentList() { return model.getFilteredStudentList(); }
 
     @Override
     public Path getAddressBookFilePath() {
--- src/main/java/seedu/address/logic/commands/student/StudentAddCommand.java
+++ src/main/java/seedu/address/logic/commands/student/StudentAddCommand.java
@@ -1,16 +1,12 @@
 package seedu.address.logic.commands.student;
 
-import seedu.address.logic.commands.AddCommand;
-import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
-import seedu.address.model.person.Person;
 import seedu.address.model.person.student.Student;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.parser.CliSyntax.*;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 public class StudentAddCommand extends StudentCommand {
     public static final String COMMAND_WORD = "add";
