a14e04b2f4e8e870adc7d4146d3443676a6f0323
inas0ng <70128508+inas0ng@users.noreply.github.com>
2023-03-05 17:48:54 +0800

    Update DelEventCommandTest.java

--- src/test/java/seedu/address/logic/commands/DelEventCommandTest.java
+++ src/test/java/seedu/address/logic/commands/DelEventCommandTest.java
@@ -2,8 +2,8 @@ package seedu.address.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.CommandTestUtil.showEventAtIndex;
 import static seedu.address.testutil.TypicalEvents.getTypicalAddressBook;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_EVENT;
