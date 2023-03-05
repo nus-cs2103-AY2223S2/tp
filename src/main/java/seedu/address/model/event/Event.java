0fc166bde0d74431dafc4d354a28af6d55eb2f94
ARPspoofing <mohdreyaaz99@gmail.com>
2023-03-05 09:41:12 +0800

    Add basic templates for event classes

--- src/main/java/seedu/address/model/event/Event.java
+++ src/main/java/seedu/address/model/event/Event.java
@@ -6,6 +6,10 @@ import java.util.List;
 
 import seedu.address.model.person.Person;
 
+/**
+ * Represents an abstract event that tutorial, lab and consultation sessions will inherit from.
+ * It contains the standard getters and setters that is common among tutorial, lab and consultations
+ */
 public abstract class Event {
 
     private final List<Person> students;
@@ -16,40 +20,86 @@ public abstract class Event {
     //Add status tags in v 1.2
     private String status = "upcoming";
 
+    /**
+     * Constructor with no parameters sets an empty list of students enrolled in the event.
+     * The time of the event will be assumed to be the current time the constructor is executed
+     */
+    public Event() {
+        students = new ArrayList<>();
+        eventDate = LocalDate.now();
+    }
+
+    /**
+     * Sets the students to a new List of students. The date will be set to the current time of execution
+     * @param students
+     */
     public Event(List<Person> students) {
         this.students = students;
         this.eventDate = LocalDate.now();
     }
 
+    /**
+     * Sets the students to a new List of students. The date will be set to a specific date
+     * @param students
+     * @param eventDate
+     */
     public Event(List<Person> students, LocalDate eventDate) {
         this.students = new ArrayList<>();
         this.eventDate = eventDate;
     }
 
+    /**
+     * Gets the list of students
+     * @return students
+     */
     public List<Person> getStudents() {
         return students;
     }
 
+    /**
+     * Gets the date of the event
+     * @return localdate
+     */
     public LocalDate getDate() {
         return eventDate;
     }
 
+    /**
+     * Adds a specific student into the list of students
+     * @param student
+     */
     public void addStudent(Person student) {
         students.add(student);
     }
 
+    /**
+     * Removes a specific student from the list of students
+     * @param student
+     */
     public void removeStudent(Person student) {
         students.remove(student);
     }
 
+    /**
+     * Removes student based on his 0 based index in the list of students
+     * @param index
+     */
     public void removeIndexStudent(int index) {
         students.remove(index);
     }
 
+    /**
+     * Changes the date of the event
+     * @param date
+     */
     public void changeDate(LocalDate date) {
         eventDate = date;
     }
 
+    /**
+     * Get the total number of students enrolled in the event
+     * @return
+     */
     public int countStudents() {
         return students.size();
     }
--- src/main/java/seedu/address/model/event/Tutorial.java
+++ src/main/java/seedu/address/model/event/Tutorial.java
@@ -1,4 +1,85 @@
 package seedu.address.model.event;
 
-public class Tutorial {
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import seedu.address.model.person.Person;
+
+public class Tutorial extends Event {
+
+    public Tutorial(List<Person> students) {
+        super(students);
+    }
+
+    /**
+     * Sets the students to a new List of students. The date will be set to a specific date
+     * @param students
+     * @param eventDate
+     */
+    public Tutorial(List<Person> students, LocalDate eventDate) {
+        super(students, eventDate);
+    }
+
+    /**
+     * Gets the list of students
+     * @return students
+     */
+    public List<Person> getStudents() {
+        return super.getStudents();
+    }
+
+    /**
+     * Gets the date of the event
+     * @return localdate
+     */
+    public LocalDate getDate() {
+        return super.getDate();
+    }
+
+    /**
+     * Adds a specific student into the list of students
+     * @param student
+     */
+    public void addStudent(Person student) {
+        super.addStudent(student);
+    }
+
+    /**
+     * Removes a specific student from the list of students
+     * @param student
+     */
+    public void removeStudent(Person student) {
+        super.removeStudent(student);
+    }
+
+    /**
+     * Removes student based on his 0 based index in the list of students
+     * @param index
+     */
+    public void removeIndexStudent(int index) {
+        super.removeIndexStudent(index);
+    }
+
+    /**
+     * Changes the date of the event
+     * @param date
+     */
+    public void changeDate(LocalDate date) {
+        super.changeDate(date);
+    }
+
+    /**
+     * Get the total number of students enrolled in the event
+     * @return
+     */
+    public int countStudents() {
+        return super.countStudents();
+    }
+
+    @Override
+    public String toString() {
+        return "Tutorial";
+    }
+
 }
