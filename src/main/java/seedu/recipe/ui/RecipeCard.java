fe5e8f1ba47db98a26325261a17cc6cf523a012f
bernicetoh <bernicetoh2003@gmail.com>
2023-03-06 22:14:25 +0800

    Update Ui to follow CookHub agenda

--- src/main/java/seedu/recipe/ui/RecipeCard.java
+++ src/main/java/seedu/recipe/ui/RecipeCard.java
@@ -1,13 +1,11 @@
-package seedu.address.ui;
-
-import java.util.Comparator;
+package seedu.recipe.ui;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
-import seedu.address.model.person.Person;
+import seedu.recipe.model.recipe.Recipe;
 
 /**
  * An UI component that displays information of a {@code Recipe}.
