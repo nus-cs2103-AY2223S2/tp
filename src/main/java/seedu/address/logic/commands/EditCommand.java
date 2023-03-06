a5193a0670f4e4a0c22b32b9f1fd1943423f9bbf
engenhui1999 <79076045+engenhui1999@users.noreply.github.com>
2023-03-06 23:29:32 +0800

    Fix checkstyle errors

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -21,10 +21,8 @@ import seedu.address.commons.util.CollectionUtil;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.person.Address;
-import seedu.address.model.person.Email;
 import seedu.address.model.person.Event;
 import seedu.address.model.person.Name;
-import seedu.address.model.person.Phone;
 import seedu.address.model.person.Rate;
 import seedu.address.model.tag.Tag;
 
