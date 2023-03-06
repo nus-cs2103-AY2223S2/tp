a8e8c969be2b9629866b0f9a6f73c31957c8a54a
anunayajoshi <47421902+anunayajoshi@users.noreply.github.com>
2023-03-06 17:21:43 +0800

    Add UI Refactoring

--- src/main/java/seedu/address/ui/ClientCard.java
+++ src/main/java/seedu/address/ui/ClientCard.java
@@ -12,9 +12,9 @@ import seedu.address.model.person.Person;
 /**
  * An UI component that displays information of a {@code Person}.
  */
-public class PersonCard extends UiPart<Region> {
+public class ClientCard extends UiPart<Region> {
 
-    private static final String FXML = "PersonListCard.fxml";
+    private static final String FXML = "ClientListCard.fxml";
 
     /**
      * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
@@ -44,7 +44,7 @@ public class PersonCard extends UiPart<Region> {
     /**
      * Creates a {@code PersonCode} with the given {@code Person} and index to display.
      */
-    public PersonCard(Person person, int displayedIndex) {
+    public ClientCard(Person person, int displayedIndex) {
         super(FXML);
         this.person = person;
         id.setText(displayedIndex + ". ");
@@ -65,12 +65,12 @@ public class PersonCard extends UiPart<Region> {
         }
 
         // instanceof handles nulls
-        if (!(other instanceof PersonCard)) {
+        if (!(other instanceof ClientCard)) {
             return false;
         }
 
         // state check
-        PersonCard card = (PersonCard) other;
+        ClientCard card = (ClientCard) other;
         return id.getText().equals(card.id.getText())
                 && person.equals(card.person);
     }
--- src/main/java/seedu/address/ui/ClientListPanel.java
+++ src/main/java/seedu/address/ui/ClientListPanel.java
@@ -13,26 +13,26 @@ import seedu.address.model.person.Person;
 /**
  * Panel containing the list of persons.
  */
-public class PersonListPanel extends UiPart<Region> {
-    private static final String FXML = "PersonListPanel.fxml";
-    private final Logger logger = LogsCenter.getLogger(PersonListPanel.class);
+public class ClientListPanel extends UiPart<Region> {
+    private static final String FXML = "ClientListPanel.fxml";
+    private final Logger logger = LogsCenter.getLogger(ClientListPanel.class);
 
     @FXML
-    private ListView<Person> personListView;
+    private ListView<Person> clientListView;
 
     /**
      * Creates a {@code PersonListPanel} with the given {@code ObservableList}.
      */
-    public PersonListPanel(ObservableList<Person> personList) {
+    public ClientListPanel(ObservableList<Person> clientList) {
         super(FXML);
-        personListView.setItems(personList);
-        personListView.setCellFactory(listView -> new PersonListViewCell());
+        clientListView.setItems(clientList);
+        clientListView.setCellFactory(listView -> new ClientListViewCell());
     }
 
     /**
      * Custom {@code ListCell} that displays the graphics of a {@code Person} using a {@code PersonCard}.
      */
-    class PersonListViewCell extends ListCell<Person> {
+    class ClientListViewCell extends ListCell<Person> {
         @Override
         protected void updateItem(Person person, boolean empty) {
             super.updateItem(person, empty);
@@ -41,7 +41,7 @@ public class PersonListPanel extends UiPart<Region> {
                 setGraphic(null);
                 setText(null);
             } else {
-                setGraphic(new PersonCard(person, getIndex() + 1).getRoot());
+                setGraphic(new ClientCard(person, getIndex() + 1).getRoot());
             }
         }
     }
