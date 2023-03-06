6c1ef87ee4df0340bbf0b0058e7759068cae2b1a
DESKTOP-76Q8Q8L\Owner <e0772512@u.nus.edu>
2023-03-06 16:27:36 +0800

    Modified test file

--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -4,6 +4,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import seedu.address.model.person.Address;
+import seedu.address.model.person.BusinessSize;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
@@ -21,10 +22,13 @@ public class PersonBuilder {
     public static final String DEFAULT_EMAIL = "amy@gmail.com";
     public static final String DEFAULT_ADDRESS = "123, Jurong West Ave 6, #08-111";
 
+    public static final String DEFAULT_BUSINESSSIZE = "420";
+
     private Name name;
     private Phone phone;
     private Email email;
     private Address address;
+    private BusinessSize businessSize;
     private Set<Tag> tags;
 
     /**
@@ -35,6 +39,7 @@ public class PersonBuilder {
         phone = new Phone(DEFAULT_PHONE);
         email = new Email(DEFAULT_EMAIL);
         address = new Address(DEFAULT_ADDRESS);
+        businessSize = new BusinessSize(DEFAULT_BUSINESSSIZE);
         tags = new HashSet<>();
     }
 
@@ -46,6 +51,7 @@ public class PersonBuilder {
         phone = personToCopy.getPhone();
         email = personToCopy.getEmail();
         address = personToCopy.getAddress();
+        businessSize = personToCopy.getBusinessSize();
         tags = new HashSet<>(personToCopy.getTags());
     }
 
@@ -82,6 +88,14 @@ public class PersonBuilder {
     }
 
     /**
+     * Sets the {@code BusinessSize} of the {@code Person} that we are building.
+     */
+    public PersonBuilder withBusinessSize(String businessSize) {
+        this.businessSize = new BusinessSize(businessSize);
+        return this;
+    }
+
+    /**
      * Sets the {@code Email} of the {@code Person} that we are building.
      */
     public PersonBuilder withEmail(String email) {
@@ -90,7 +104,7 @@ public class PersonBuilder {
     }
 
     public Person build() {
-        return new Person(name, phone, email, address, tags);
+        return new Person(name, phone, email, address, businessSize, tags);
     }
 
 }
