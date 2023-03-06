cf3fd1c28950906657746072921766974ad99191
Joy Ng Jing Ru <e0943052@u.nus.edu>
2023-03-06 08:55:59 +0800

    Edit more style issues

--- src/test/java/seedu/task/testutil/TypicalTagComparator.java
+++ src/test/java/seedu/task/testutil/TypicalTagComparator.java
@@ -6,15 +6,16 @@ import seedu.task.model.tracker.TagComparator;
  * A utility class containing a list of {@code TagComparator} objects to be used in tests.
  */
 public class TypicalTagComparator {
-    private TypicalTagComparator() {} // prevents instantiation
 
     public static final TagComparator CODE_3 = new TagComparatorBuilder().withOccurrence(3).build();
-    public static final TagComparator SLEEP_5 = new TagComparatorBuilder()
-            .withTag("SLEEP")
-            .withOccurrence(5)
-            .build();
     public static final TagComparator EAT_1 = new TagComparatorBuilder()
             .withTag("EAT")
             .withOccurrence(1)
             .build();
+    public static final TagComparator SLEEP_5 = new TagComparatorBuilder()
+            .withTag("SLEEP")
+            .withOccurrence(5)
+            .build();
+
+    private TypicalTagComparator() {} // prevents instantiation
 }
