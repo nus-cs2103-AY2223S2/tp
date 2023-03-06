65b6a37a28fc877837d0596cd979d65dbc4fda43
bryansendeavour <bryanojj@hotmail.com>
2023-03-06 11:17:50 +0800

    no message

--- src/test/java/seedu/address/model/person/ContactTest.java
+++ src/test/java/seedu/address/model/person/ContactTest.java
@@ -1,7 +1,8 @@
 package seedu.address.model.person;
 
-import org.junit.jupiter.api.Test;
 import seedu.address.testutil.PersonBuilder;
+
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
