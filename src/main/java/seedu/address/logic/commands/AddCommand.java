ab74435e3409d2268034bdde49973ddc4d18c5fa
Hafeez <izzhafeez@gmail.com>
2023-03-06 21:56:34 +0800

    Fix checkstyle

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -2,13 +2,13 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import java.util.Set;
+
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.Prefix;
 import seedu.address.model.Model;
 import seedu.address.model.person.Person;
 import seedu.address.model.tag.ModuleTag;
-
-import java.util.Set;
 
 /**
  * Adds a person to the address book.
