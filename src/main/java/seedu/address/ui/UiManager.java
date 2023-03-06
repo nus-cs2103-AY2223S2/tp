34a55507a9e45359e58a86b08b2749a425b67918
bernicetoh <bernicetoh2003@gmail.com>
2023-03-06 21:11:08 +0800

    Resolve merge conflict in UiManager

--- src/main/java/seedu/address/ui/UiManager.java
+++ src/main/java/seedu/address/ui/UiManager.java
@@ -20,7 +20,6 @@ public class UiManager implements Ui {
     public static final String ALERT_DIALOG_PANE_FIELD_ID = "alertDialogPane";
 
     private static final Logger logger = LogsCenter.getLogger(UiManager.class);
-//  private static final String ICON_APPLICATION = "/images/cookHub.png";
     private static final String ICON_APPLICATION = "/images/address_book_32.png";
 
     private Logic logic;
