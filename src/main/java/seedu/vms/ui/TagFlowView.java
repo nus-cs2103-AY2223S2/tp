4b6112abbcc2356469280ccd8b334bd206ef0cf9
daitenshionyan <100074448+daitenshionyan@users.noreply.github.com>
2023-03-08 05:34:04 +0800

    Add addition of style classes to tags

--- src/main/java/seedu/vms/ui/TagFlowView.java
+++ src/main/java/seedu/vms/ui/TagFlowView.java
@@ -1,6 +1,7 @@
 package seedu.vms.ui;
 
 import java.util.Collection;
+import java.util.List;
 
 import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
@@ -11,19 +12,34 @@ import javafx.scene.layout.Region;
  * Graphical representation of a list of tags.
  */
 public class TagFlowView extends FlowPane {
-    private static final String TAG_STYLE_CLASS = "tag";
+    public static final String STYLE_CLASS_TAG_FLOW_PANE = "tag-flow-pane";
+    private static final String STYLE_CLASS_TAG = "tag";
+
+
+    /**
+     * Constructs a {@code TagFlowView} no additional style classes added to
+     * its tags.
+     *
+     * @param tags - the tags to display.
+     */
+    public TagFlowView(Collection<String> tags) {
+        this(tags, List.of());
+    }
 
 
     /**
      * Constructs a {@code TagFlowView}.
      *
      * @param tags - the tags to display.
+     * @param styleClasses - a list of additional style classes to add to the
+     *      tags.
      */
-    public TagFlowView(Collection<String> tags) {
-        setHgap(5);
-        setVgap(5);
+    public TagFlowView(Collection<String> tags, Collection<String> styleClasses) {
+        getStyleClass().add(STYLE_CLASS_TAG_FLOW_PANE);
         for (String tag : tags) {
-            getChildren().add(formLabel(tag));
+            Label label = formLabel(tag);
+            label.getStyleClass().addAll(styleClasses);
+            getChildren().add(label);
         }
     }
 
@@ -31,7 +47,7 @@ public class TagFlowView extends FlowPane {
     private Label formLabel(String text) {
         Label label = new Label(text);
         label.setWrapText(true);
-        label.getStyleClass().add(TAG_STYLE_CLASS);
+        label.getStyleClass().add(STYLE_CLASS_TAG);
         label.setMinHeight(Region.USE_PREF_SIZE);
         return label;
     }
