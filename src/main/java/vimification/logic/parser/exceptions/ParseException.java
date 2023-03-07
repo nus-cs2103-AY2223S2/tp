c24064bb6668918031625539b09e5f091507f01e
VietAnh1010 <vietanh101003@gmail.com>
2023-03-07 23:31:48 +0800

    Rename package from seedu.address to vimification
    The old package name is seedu.address.
    
    We need to change the name to make it clear that this is the code base for Vimification.
    Also, changing the package name removes 1 level of directories. The extra level of nested
    directory is not necessary.
    
    The package is renamed from seedu.address to vimification. Occurrences of seedu.address in all
    files are also replaced with vimification.
    
    This is a big change that affects the entire codebase. Testing is required to ensure that the
    code still works correctly after the renaming.

---
+++ src/main/java/vimification/logic/parser/exceptions/ParseException.java
@@ -0,0 +1,17 @@
+package vimification.logic.parser.exceptions;
+
+import vimification.commons.exceptions.IllegalValueException;
+
+/**
+ * Represents a parse error encountered by a parser.
+ */
+public class ParseException extends IllegalValueException {
+
+    public ParseException(String message) {
+        super(message);
+    }
+
+    public ParseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
