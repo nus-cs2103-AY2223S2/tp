9e8744fa37e671e889f985cd5b26e44b81099ffd
jiawei <b33b33o11@gmail.com>
2023-03-07 23:48:04 +0800

    Implement fields for `Meeting`

--- src/main/java/seedu/address/model/meeting/DateTime.java
+++ src/main/java/seedu/address/model/meeting/DateTime.java
@@ -1,4 +1,126 @@
 package seedu.address.model.meeting;
 
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+import java.time.DateTimeException;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Objects;
+
+/**
+ * Represents a Meetings's date/time in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidDateTime(String)}
+ *
+ * TODO: Include more acceptable formats for date/time instead of just dd/MM/yyyy HH:mm
+ */
 public class DateTime {
+    public static final String DATE_FORMAT = "dd/MM/yyyy";
+    public static final String TIME_FORMAT = "HH:mm";
+
+    public static final String MESSAGE_CONSTRAINTS =
+            "Dates/Times should only contain alphanumeric characters and spaces, and it should not be blank"
+            + "and adhere to the following constraints:\n"
+            + "1. Date must comply with the format: " + DATE_FORMAT + ".\n"
+            + "2. Time must comply with the format: " + TIME_FORMAT + " in 24-hour format.";
+
+    /**
+     * The first character of the date/time string must not be a whitespace,
+     * otherwise " " (a blank string) becomes a valid input.
+     * <p>
+     * Inputs in addition to formats like dd/MM/yyyy are allowed for semantics
+     * such as "next monday".
+     */
+    public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
+
+    private final LocalDateTime meetingDateTime;
+
+    /**
+     * This is used only for inputs that include specific durations.
+     * For example Thursday 2pm - Thursday 4pm implies duration of
+     * 2 hours.
+     */
+    private final Duration meetingDuration;
+
+    /**
+     * Constructs a {@code DateTime} without any duration specified.
+     *
+     * @param dateTime A valid date/time.
+     */
+    public DateTime(String dateTime) {
+        requireNonNull(dateTime);
+        checkArgument(isValidDateTime(dateTime), MESSAGE_CONSTRAINTS);
+        try {
+            meetingDateTime = LocalDateTime.parse(dateTime,
+                    DateTimeFormatter.ofPattern(String.format("%s %s", DATE_FORMAT, TIME_FORMAT)));
+            meetingDuration = Duration.ZERO;
+        } catch (DateTimeParseException e) {
+            throw new DateTimeException("Unable to recognise '" + dateTime + "' as valid date/time.");
+        }
+    }
+
+    /**
+     * Constructs a {@code DateTime} with a duration specified.
+     *
+     * @param startDateTime A valid start date/time.
+     * @param endDateTime A valid end date/time.
+     */
+    public DateTime(String startDateTime, String endDateTime) {
+        requireAllNonNull(startDateTime, endDateTime);
+        checkArgument(isValidDateTime(startDateTime), MESSAGE_CONSTRAINTS);
+        checkArgument(isValidDateTime(endDateTime), MESSAGE_CONSTRAINTS);
+
+        try {
+            meetingDateTime = LocalDateTime.parse(startDateTime,
+                    DateTimeFormatter.ofPattern(String.format("%s %s", DATE_FORMAT, TIME_FORMAT)));
+        } catch (DateTimeParseException e) {
+            throw new DateTimeException("Unable to recognise '" + startDateTime + "' as valid date/time.");
+        }
+
+        try {
+            LocalDateTime end = LocalDateTime.parse(startDateTime,
+                    DateTimeFormatter.ofPattern(String.format("%s %s", DATE_FORMAT, TIME_FORMAT)));
+            meetingDuration = Duration.between(meetingDateTime, end);
+        } catch (DateTimeParseException e) {
+            throw new DateTimeException("Unable to recognise '" + endDateTime + "' as valid date/time.");
+        } catch (DateTimeException | ArithmeticException e) {
+            throw new DateTimeException("Unable to calculate the duration between " + startDateTime
+                    + " and " + endDateTime);
+        }
+    }
+
+    /**
+     * Returns true if a given string is a valid title.
+     */
+    public static boolean isValidDateTime(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        String end = meetingDuration != null ? meetingDateTime.plus(meetingDuration).toString() : "";
+
+        if (!end.isEmpty()) {
+            return meetingDateTime + " to " + end;
+        }
+
+        return meetingDateTime.toString();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof DateTime // instanceof handles nulls
+                && meetingDateTime.equals(((DateTime) other).meetingDateTime) // state check
+                && meetingDuration.equals(((DateTime) other).meetingDuration)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        // use this method for custom fields hashing instead of implementing your own
+        return Objects.hash(meetingDateTime, meetingDuration);
+    }
 }
--- src/main/java/seedu/address/model/meeting/Location.java
+++ src/main/java/seedu/address/model/meeting/Location.java
@@ -1,4 +1,84 @@
 package seedu.address.model.meeting;
 
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/**
+ * Represents a Meeting's location in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidLocation(String)}
+ */
 public class Location {
+    public static final String MESSAGE_CONSTRAINTS =
+            "Descriptions should only contain alphanumeric characters and spaces, and it should not be blank";
+
+    /**
+     * The first character of the location must not be a whitespace,
+     * otherwise " " (a blank string) becomes a valid input.
+     */
+    public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
+
+    private final String meetingLocation;
+    private final boolean isVirtualLocation;
+
+    /**
+     * Constructs a {@code Location}.
+     *
+     * @param location A valid description.
+     */
+    public Location(String location) {
+        requireNonNull(location);
+        checkArgument(isValidLocation(location), MESSAGE_CONSTRAINTS);
+        meetingLocation = location;
+        isVirtualLocation = isValidUrl(location);
+    }
+
+    public boolean isVirtualLocation() {
+        return isVirtualLocation;
+    }
+
+    /**
+     * Returns true if a given string is a valid description.
+     *
+     * @param test A string to test for validity.
+     * @return True if string is valid, false otherwise.
+     */
+    public static boolean isValidLocation(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+    /**
+     * Returns true if a given string is a valid URL.
+     *
+     * @param url A string to test if it is a URL.
+     * @return True if string is a valid URL, false otherwise.
+     */
+    private boolean isValidUrl(String url) {
+        try {
+            new URI(url);
+        } catch (URISyntaxException e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return meetingLocation;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Location // instanceof handles nulls
+                && meetingLocation.equals(((Location) other).meetingLocation)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return meetingLocation.hashCode();
+    }
 }
