6a6da9fcda40479c4fb3051b5bef58cfa692b725
Hafeez <izzhafeez@gmail.com>
2023-03-07 22:12:15 +0800

    Implement LoadCommandParser to parse Load Commands

--- src/main/java/seedu/address/logic/parser/LoadCommandParser.java
+++ src/main/java/seedu/address/logic/parser/LoadCommandParser.java
@@ -1,2 +1,30 @@
-package seedu.address.logic.parser;public class LoadCommandParser {
+package seedu.address.logic.parser;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import seedu.address.logic.commands.LoadCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+/**
+ * Parses input arguments and creates a new LoadCommand object
+ */
+public class LoadCommandParser implements Parser<LoadCommand> {
+    @Override
+    public LoadCommand parse(String args) throws ParseException {
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args);
+        List<String> fileNameList = argMultimap.getAllValues(new Prefix(""));
+
+        // we only allow single unspaced strings as filenames
+        if (fileNameList.stream().allMatch(String::isEmpty)) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, LoadCommand.MESSAGE_USAGE));
+        }
+
+        Path filePath = Paths.get("data", fileNameList.get(0) + ".json");
+
+        return new LoadCommand(filePath);
+    }
 }
