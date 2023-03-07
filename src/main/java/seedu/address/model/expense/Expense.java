a7ad878f2d6dbee60c27fb9c86b0c7323733062a
Shirshajit Sen Gupta <shirshajit@gmail.com>
2023-03-08 01:34:16 +0800

    Create expense class

---
+++ src/main/java/seedu/address/model/expense/Expense.java
@@ -0,0 +1,95 @@
+package seedu.address.model.expense;
+
+import java.util.Date;
+
+public class Expense {
+    private String name;
+    private double amount;
+    private Date date;
+    // TODO: Change to Category class
+    private String category;
+
+    public Expense(String name, double amount, Date date, String category) {
+        this.name = name;
+        this.amount = amount;
+        this.date = date;
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    @Override
+    public String toString() {
+        return "Expense{" +
+                "name='" + name + '\'' +
+                ", amount=" + amount +
+                ", date=" + date +
+                ", category='" + category + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Expense expense = (Expense) o;
+
+        if (Double.compare(expense.amount, amount) != 0) {
+            return false;
+        }
+        if (name != null ? !name.equals(expense.name) : expense.name != null) {
+            return false;
+        }
+        if (date != null ? !date.equals(expense.date) : expense.date != null) {
+            return false;
+        }
+        return category != null ? category.equals(expense.category) : expense.category == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = name != null ? name.hashCode() : 0;
+        temp = Double.doubleToLongBits(amount);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        result = 31 * result + (date != null ? date.hashCode() : 0);
+        result = 31 * result + (category != null ? category.hashCode() : 0);
+        return result;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
