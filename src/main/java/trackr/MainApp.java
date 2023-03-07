87519fcc36756ebc04319e96c9b49b0745b8f2a5
Liu Muchen <liumuchen1@gmail.com>
2023-03-08 00:26:13 +0800

    Updated MainApp `AddressBook` to `Trackr`

--- src/main/java/trackr/MainApp.java
+++ src/main/java/trackr/MainApp.java
@@ -170,7 +170,7 @@ public class MainApp extends Application {
                     + "Using default user prefs");
             initializedPrefs = new UserPrefs();
         } catch (IOException e) {
-            logger.warning("Problem while reading from the file. Will be starting with an empty AddressBook");
+            logger.warning("Problem while reading from the file. Will be starting with an empty Trackr");
             initializedPrefs = new UserPrefs();
         }
 
@@ -186,13 +186,13 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        logger.info("Starting AddressBook " + MainApp.VERSION);
+        logger.info("Starting Trackr " + MainApp.VERSION);
         ui.start(primaryStage);
     }
 
     @Override
     public void stop() {
-        logger.info("============================ [ Stopping Address Book ] =============================");
+        logger.info("============================ [ Stopping Trackr ] =============================");
         try {
             storage.saveUserPrefs(model.getUserPrefs());
         } catch (IOException e) {
