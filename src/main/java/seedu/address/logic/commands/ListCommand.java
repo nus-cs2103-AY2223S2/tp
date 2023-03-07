b397e872b1860907f78549a5edd1310fe8078b76
Hafeez <izzhafeez@gmail.com>
2023-03-07 22:06:15 +0800

    Modify List Command to return Command Result

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -3,6 +3,7 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
+import seedu.address.logic.commands.results.CommandResult;
 import seedu.address.model.Model;
 
 /**
