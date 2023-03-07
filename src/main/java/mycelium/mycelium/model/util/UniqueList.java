7abfe93db8ccc4649dc275b0cd1b4740835565d1
junyi <47441677+immanuelhume@users.noreply.github.com>
2023-03-07 13:17:01 +0800

    Add methods to UniqueList

--- src/main/java/mycelium/mycelium/model/util/UniqueList.java
+++ src/main/java/mycelium/mycelium/model/util/UniqueList.java
@@ -1,9 +1,12 @@
 package mycelium.mycelium.model.util;
 
 import static java.util.Objects.requireNonNull;
+import static mycelium.mycelium.commons.util.CollectionUtil.requireAllNonNull;
 
 import java.util.Iterator;
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.IntStream;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -52,6 +55,53 @@ public class UniqueList<T extends IsSame<T>> implements Iterable<T> {
         assert filtered.size() == 1 : "Invariant violated: list contains duplicate items";
         assert internalList.remove(filtered.get(0)) : "Invariant violated: list does not contain item to remove";
     }
+
+    /**
+     * Replaces an item with another at the same index. The two items do not have to be the same.
+     */
+    public void setItem(T target, T newItem) {
+        requireAllNonNull(target, newItem);
+        int[] idxs = IntStream.range(0, internalList.size()).filter(i -> internalList.get(i).isSame(target)).toArray();
+        if (idxs.length == 0) {
+            throw new ItemNotFoundException(target);
+        }
+        assert idxs.length == 1 : "Invariant violated: list contains duplicate items";
+        if (!target.isSame(newItem) && contains(newItem)) {
+            throw new DuplicateItemException(newItem);
+        }
+        internalList.set(idxs[0], newItem);
+    }
+
+    /**
+     * Replaces the current list with another.
+     */
+    public void setItems(UniqueList<T> replacement) {
+        requireNonNull(replacement);
+        internalList.setAll(replacement.internalList);
+    }
+
+    /**
+     * Replaces the current list with another. Items in the list must be unique.
+     */
+    public void setItems(List<T> items) {
+        requireAllNonNull(items);
+        if (!itemsAreUnique(items)) {
+            throw new DuplicateItemException();
+        }
+        internalList.setAll(items);
+    }
+
+    private boolean itemsAreUnique(List<T> items) {
+        for (int i = 0; i < items.size() - 1; i++) {
+            for (int j = i + 1; j < items.size(); j++) {
+                if (items.get(i).isSame(items.get(j))) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
 
     /**
      * Returns an immutable reference to the internal list.
--- src/test/java/mycelium/mycelium/model/util/UniqueListTest.java
+++ src/test/java/mycelium/mycelium/model/util/UniqueListTest.java
@@ -3,6 +3,8 @@ package mycelium.mycelium.model.util;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.junit.jupiter.api.Test;
@@ -87,5 +89,151 @@ public class UniqueListTest {
         projects.add(defaultProject);
         projects.remove(new ProjectBuilder().withClientEmail("chungus@chungus.org").build());
         assertEquals(expectedProjects, projects);
+    }
+
+    @Test
+    public void setItem_nullTarget_throwsNullPointerException() {
+        UniqueList<Project> projects = new UniqueList<>();
+        assertThrows(NullPointerException.class, () -> projects.setItem(null, new ProjectBuilder().build()));
+    }
+
+    @Test
+    public void setItem_nullNewItem_throwsNullPointerException() {
+        UniqueList<Project> projects = new UniqueList<>();
+        assertThrows(NullPointerException.class, () -> projects.setItem(new ProjectBuilder().build(), null));
+    }
+
+    @Test
+    public void setItem_targetItemDoesNotExist_throwsItemNotFoundException() {
+        UniqueList<Project> projects = new UniqueList<>();
+        Project defaulProject = new ProjectBuilder().build();
+        assertThrows(ItemNotFoundException.class, ()
+            -> projects.setItem(defaulProject, defaulProject));
+    }
+
+    @Test
+    public void setItem_duplicateItem_throwsDuplicateItemException() {
+        UniqueList<Project> projects = new UniqueList<>();
+        Project projA = new ProjectBuilder().withName("A").build();
+        Project projB = new ProjectBuilder().withName("B").build();
+        projects.add(projA);
+        projects.add(projB);
+
+        assertThrows(DuplicateItemException.class, () -> projects.setItem(projA, projB));
+    }
+
+    @Test
+    public void setItem_diffNewItem_replacesItem() {
+        UniqueList<Project> projects = new UniqueList<>();
+        Project projA = new ProjectBuilder().withName("A").build();
+        Project projB = new ProjectBuilder().withName("B").build();
+        projects.add(projA);
+        projects.add(projB);
+
+        Project projC = new ProjectBuilder().withName("C").build();
+        projects.setItem(projA, projC);
+
+        UniqueList<Project> expectedProjects = new UniqueList<>();
+        expectedProjects.add(projC);
+        expectedProjects.add(projB);
+        assertEquals(expectedProjects, projects);
+    }
+
+    @Test
+    public void setItem_identicalNewItem_nothingChanges() {
+        UniqueList<Project> projects = new UniqueList<>();
+        Project projA = new ProjectBuilder().withName("A").build();
+        projects.add(projA);
+
+        projects.setItem(projA, projA);
+
+        UniqueList<Project> expectedProjects = new UniqueList<>();
+        expectedProjects.add(projA);
+        assertEquals(expectedProjects, projects);
+    }
+
+    @Test
+    public void setItem_sameNewItemNotEquals_replacesItem() {
+        UniqueList<Project> projects = new UniqueList<>();
+        Project projA = new ProjectBuilder().withName("A").build();
+        Project projB = new ProjectBuilder().withName("B").build();
+        projects.add(projA);
+        projects.add(projB);
+
+        Project newProjA = new ProjectBuilder().withName("A").withClientEmail("chungus@chungus.org").build();
+        projects.setItem(projA, newProjA);
+
+        UniqueList<Project> expectedProjects = new UniqueList<>();
+        expectedProjects.add(newProjA);
+        expectedProjects.add(projB);
+        assertEquals(expectedProjects, projects);
+    }
+
+    @Test
+    public void setItems_nullInput_throwsNullPointerException() {
+        UniqueList<Project> projects = new UniqueList<>();
+        assertThrows(NullPointerException.class, () -> projects.setItems((UniqueList<Project>) null));
+        assertThrows(NullPointerException.class, () -> projects.setItems((List<Project>) null));
+    }
+
+    @Test
+    public void setItems_uniqueList_replacesOwnListWithProvidedUniqueList() {
+        UniqueList<Project> projects = new UniqueList<>();
+        Project projA = new ProjectBuilder().withName("A").build();
+        Project projB = new ProjectBuilder().withName("B").build();
+        projects.add(projA);
+        projects.add(projB);
+
+        UniqueList<Project> newProjects = new UniqueList<>();
+        Project projC = new ProjectBuilder().withName("C").build();
+        Project projD = new ProjectBuilder().withName("D").build();
+        newProjects.add(projC);
+        newProjects.add(projD);
+
+        projects.setItems(newProjects);
+
+        assertEquals(newProjects, projects);
+    }
+
+    @Test
+    public void setItems_validList_replacesOwnListWithProvidedList() {
+        UniqueList<Project> projects = new UniqueList<>();
+        Project projA = new ProjectBuilder().withName("A").build();
+        Project projB = new ProjectBuilder().withName("B").build();
+        projects.add(projA);
+        projects.add(projB);
+
+        List<Project> newProjects = new ArrayList<>();
+        Project projC = new ProjectBuilder().withName("C").build();
+        Project projD = new ProjectBuilder().withName("D").build();
+        newProjects.add(projC);
+        newProjects.add(projD);
+
+        projects.setItems(newProjects);
+
+        UniqueList<Project> expectedProjects = new UniqueList<>();
+        expectedProjects.add(projC);
+        expectedProjects.add(projD);
+        assertEquals(expectedProjects, projects);
+    }
+
+    @Test
+    public void setItems_duplicateItems_throwsDuplicateItemException() {
+        UniqueList<Project> projects = new UniqueList<>();
+
+        List<Project> newProjects = new ArrayList<>();
+        Project projC = new ProjectBuilder().withName("C").build();
+        Project projD = new ProjectBuilder().withName("D").build();
+        newProjects.add(projC);
+        newProjects.add(projD);
+        newProjects.add(projC);
+
+        assertThrows(DuplicateItemException.class, () -> projects.setItems(newProjects));
+    }
+
+    @Test
+    public void asUnmodifiableObservableList_modifyList_throwsUnsupportedOperationException() {
+        assertThrows(UnsupportedOperationException.class, ()
+            -> new UniqueList<>().asUnmodifiableObservableList().remove(0));
     }
 }
