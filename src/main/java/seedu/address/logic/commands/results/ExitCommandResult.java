ccbafe879203bda7af77307717296a202a181e0d
Hafeez <izzhafeez@gmail.com>
2023-03-07 22:09:01 +0800

    Implement ExitCommandResult for code cleanliness

--- src/main/java/seedu/address/logic/commands/results/ExitCommandResult.java
+++ src/main/java/seedu/address/logic/commands/results/ExitCommandResult.java
@@ -1,2 +1,21 @@
-package seedu.address.logic.commands.results;public class ExitCommandResult {
+package seedu.address.logic.commands.results;
+
+/**
+ * CommandResult to be shown when executing an ExitCommand.
+ */
+public class ExitCommandResult extends CommandResult {
+
+    public ExitCommandResult(String feedbackToUser) {
+        super(feedbackToUser);
+    }
+
+    @Override
+    public boolean isExit() {
+        return true;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return super.equals(other) && other instanceof ExitCommandResult;
+    }
 }
