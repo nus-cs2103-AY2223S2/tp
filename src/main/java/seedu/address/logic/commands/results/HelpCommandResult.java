1c4a6dd38c72511b992dd53664e07c2c1bb41956
Hafeez <izzhafeez@gmail.com>
2023-03-07 22:10:11 +0800

    Implement HelpCommandResult for code cleanliness

--- src/main/java/seedu/address/logic/commands/results/HelpCommandResult.java
+++ src/main/java/seedu/address/logic/commands/results/HelpCommandResult.java
@@ -1,2 +1,21 @@
-package seedu.address.logic.commands.results;public class HelpCommandResult {
+package seedu.address.logic.commands.results;
+
+/**
+ * CommandResult to be shown when executing a HelpCommand.
+ */
+public class HelpCommandResult extends CommandResult {
+
+    public HelpCommandResult(String feedbackToUser) {
+        super(feedbackToUser);
+    }
+
+    @Override
+    public boolean isShowHelp() {
+        return true;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return super.equals(other) && other instanceof HelpCommandResult;
+    }
 }
