5a7ac3b01ec4b84d8f3c7c6273aabaf9434bb61f
OliviaJHL <305914126@qq.com>
2023-03-07 19:49:52 +0800

    Update Ui and list clients function.
    Current Ui and listClients has no changes.
    
    Require to update.
    
    Let's update the .fxml and listCommand file.
    
    Can only update manually.

--- src/test/java/seedu/address/logic/commands/ListCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ListCommandTest.java
@@ -28,12 +28,12 @@ public class ListCommandTest {
 
     @Test
     public void execute_listIsNotFiltered_showsSameList() {
-        assertCommandSuccess(new ListCommand(), model, ListCommand.MESSAGE_SUCCESS, expectedFitBookModel);
+        assertCommandSuccess(new ListClientsCommand(), model, ListClientsCommand.MESSAGE_SUCCESS, expectedFitBookModel);
     }
 
     @Test
     public void execute_listIsFiltered_showsEverything() {
         showClientAtIndex(model, INDEX_FIRST_PERSON);
-        assertCommandSuccess(new ListCommand(), model, ListCommand.MESSAGE_SUCCESS, expectedFitBookModel);
+        assertCommandSuccess(new ListClientsCommand(), model, ListClientsCommand.MESSAGE_SUCCESS, expectedFitBookModel);
     }
 }
