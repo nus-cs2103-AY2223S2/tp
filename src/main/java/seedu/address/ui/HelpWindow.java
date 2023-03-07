9ebbc5b34a40642d43e0d2a374f1d8480738b221
SeanChinJunKai <e0774037@u.nus.edu>
2023-03-07 23:43:59 +0800

    Add commands available with brief desciption in help window

--- src/main/java/seedu/address/ui/HelpWindow.java
+++ src/main/java/seedu/address/ui/HelpWindow.java
@@ -15,9 +15,16 @@ import seedu.address.commons.core.LogsCenter;
  */
 public class HelpWindow extends UiPart<Stage> {
 
-    public static final String USERGUIDE_URL = "https://se-education.org/addressbook-level3/UserGuide.html";
-    public static final String HELP_MESSAGE = "Refer to the user guide: " + USERGUIDE_URL;
-
+    public static final String USERGUIDE_URL = "https://ay2223s2-cs2103t-t11-4.github.io/tp/UserGuide.html";
+    public static final String HELP_MESSAGE = "Refer to the user guide for more in-depth information: " + USERGUIDE_URL;
+    public static final String ADD_COMMAND_MESSAGE = "1) Add a person to the patient records: \nFormat:add n/NAME i/NRIC p/PHONE_NUMBER e/EMAIL a/ADDRESS [d/DIAGNOSIS] [t/TAG]";
+    public static final String GET_COMMAND_MESSAGE = "2) Get a patient's record: \nFormat: get i/NRIC";
+    public static final String EDIT_COMMAND_MESSAGE = "3) Edit an existing person in patient records: \nFormat: edit INDEX [n/NAME] [i/NRIC] [p/PHONE] [e/EMAIL] [a/ADDRESS] [d/DIAGNOSIS] [t/TAG]";
+    public static final String DELETE_COMMAND_MESSAGE = "4) Deletes the patient with the specified NRIC from the address book: \nFormat: delete INDEX\n";
+    public static final String FILTER_COMMAND_MESSAGE = "5) Filters patients according to a particular attribute. Eg. Medicine usage or health conditions: \nFormat: filter KEYWORD [MORE_KEYWORDS]";
+    public static final String LIST_COMMAND_MESSAGE = "6) Lists all patients in the records system: \nFormat: list";
+    public static final String CLEAR_COMMAND_MESSAGE = "7) Purge all data in records system: \nFormat: clearAll";
+    public static final String NOTE_MESSAGE = "Note that square brackets signify optional arguments";
     private static final Logger logger = LogsCenter.getLogger(HelpWindow.class);
     private static final String FXML = "HelpWindow.fxml";
 
@@ -27,6 +34,31 @@ public class HelpWindow extends UiPart<Stage> {
     @FXML
     private Label helpMessage;
 
+    @FXML
+    private Label addMessage;
+
+    @FXML
+    private Label getMessage;
+
+    @FXML
+    private Label editMessage;
+
+    @FXML
+    private Label deleteMessage;
+
+    @FXML
+    private Label filterMessage;
+
+    @FXML
+    private Label listMessage;
+
+    @FXML
+    private Label noteMessage;
+
+    @FXML
+    private Label clearMessage;
+
+
     /**
      * Creates a new HelpWindow.
      *
@@ -34,6 +66,14 @@ public class HelpWindow extends UiPart<Stage> {
      */
     public HelpWindow(Stage root) {
         super(FXML, root);
+        addMessage.setText(ADD_COMMAND_MESSAGE);
+        getMessage.setText(GET_COMMAND_MESSAGE);
+        editMessage.setText(EDIT_COMMAND_MESSAGE);
+        deleteMessage.setText(DELETE_COMMAND_MESSAGE);
+        filterMessage.setText(FILTER_COMMAND_MESSAGE);
+        listMessage.setText(LIST_COMMAND_MESSAGE);
+        clearMessage.setText(CLEAR_COMMAND_MESSAGE);
+        noteMessage.setText(NOTE_MESSAGE);
         helpMessage.setText(HELP_MESSAGE);
     }
 
