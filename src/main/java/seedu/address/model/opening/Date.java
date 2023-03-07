20b4250ccae580424b69002934d23c2247b03641
Lanreath <tyfei00@gmail.com>
2023-03-07 22:47:22 +0800

    Update date class to contain two data attributes

--- src/main/java/seedu/address/model/opening/Date.java
+++ src/main/java/seedu/address/model/opening/Date.java
@@ -5,6 +5,7 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Objects;
 
 /**
  * Represents an Opening's date in the address book.
@@ -14,16 +15,19 @@ public class Date {
             "Dates should only be in the format yyyy-MM-dd e.g. 2023-01-01";
 
     public final String fullDate;
+    public final String fullName;
 
     /**
      * Constructs a {@code Date}.
      *
      * @param date A valid date.
      */
-    public Date(String date) {
+    public Date(String name, String date) {
         requireNonNull(date);
+        requireNonNull(name);
         checkArgument(isValidDate(date), MESSAGE_CONSTRAINTS);
         fullDate = date;
+        fullName = name;
     }
 
     /**
@@ -42,18 +46,19 @@ public class Date {
 
     @Override
     public String toString() {
-        return fullDate;
+        return fullName + ": " + fullDate;
     }
 
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof Date // instanceof handles nulls
+                && fullName.equals(((Date) other).fullName)
                 && fullDate.equals(((Date) other).fullDate)); // state check
     }
 
     @Override
     public int hashCode() {
-        return fullDate.hashCode();
+        return Objects.hash(fullName, fullDate);
     }
 }
