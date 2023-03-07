182cafb941b2c6117eac12ae2ca568a3e0241c07
Hafeez <izzhafeez@gmail.com>
2023-03-07 22:01:39 +0800

    Modify ClearCommand to return a Command result

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -2,6 +2,7 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import seedu.address.logic.commands.results.CommandResult;
 import seedu.address.model.EduMate;
 import seedu.address.model.Model;
 
@@ -12,7 +13,6 @@ public class ClearCommand extends Command {
 
     public static final String COMMAND_WORD = "clear";
     public static final String MESSAGE_SUCCESS = "Address book has been cleared!";
-
 
     @Override
     public CommandResult execute(Model model) {
