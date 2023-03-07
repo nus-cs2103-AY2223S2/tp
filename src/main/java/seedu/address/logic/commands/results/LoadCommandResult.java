293f9111ff5f205c86f9932a65a9a5e53bf10a12
Hafeez <izzhafeez@gmail.com>
2023-03-07 22:10:34 +0800

    Implement LoadCommandResult for code cleanliness

--- src/main/java/seedu/address/logic/commands/results/LoadCommandResult.java
+++ src/main/java/seedu/address/logic/commands/results/LoadCommandResult.java
@@ -1,2 +1,45 @@
-package seedu.address.logic.commands.results;public class LoadCommandResult {
+package seedu.address.logic.commands.results;
+
+import static java.util.Objects.requireNonNull;
+
+import java.nio.file.Path;
+import java.util.Objects;
+import java.util.Optional;
+
+/**
+ * CommandResult to be shown when executing a LoadCommand.
+ */
+public class LoadCommandResult extends CommandResult {
+
+    /** Path to save the EduMate */
+    private final Path filePath;
+
+    public LoadCommandResult(String feedbackToUser, Path filePath) {
+        super(feedbackToUser);
+        requireNonNull(filePath);
+
+        this.filePath = filePath;
+    }
+
+    @Override
+    public boolean isLoad() {
+        return true;
+    }
+
+    @Override
+    public Optional<Path> getFilePath() {
+        return Optional.of(filePath);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return super.equals(other)
+                && other instanceof LoadCommandResult
+                && filePath.equals(((LoadCommandResult) other).filePath);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), filePath);
+    }
 }
