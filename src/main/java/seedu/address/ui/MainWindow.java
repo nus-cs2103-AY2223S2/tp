c4a79278eee849a99852872b98e48e234890d666
StanleyNeoh <95394480+StanleyNeoh@users.noreply.github.com>
2023-03-07 17:08:01 +0800

    Add tabbing to Mycelium

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -1,13 +1,16 @@
 package seedu.address.ui;
 
+import java.util.ArrayList;
 import java.util.logging.Logger;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.TabPane;
 import javafx.scene.control.TextInputControl;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import seedu.address.commons.core.GuiSettings;
@@ -35,6 +38,9 @@ public class MainWindow extends UiPart<Stage> {
     private ResultDisplay resultDisplay;
     private HelpWindow helpWindow;
 
+    private TabPage projectTab;
+    private TabPage clientTab;
+
     @FXML
     private StackPane commandBoxPlaceholder;
 
@@ -42,10 +48,10 @@ public class MainWindow extends UiPart<Stage> {
     private MenuItem helpMenuItem;
 
     @FXML
-    private StackPane personListPanelPlaceholder;
+    private StackPane resultDisplayPlaceholder;
 
     @FXML
-    private StackPane resultDisplayPlaceholder;
+    private TabPane tabPane;
 
     @FXML
     private StackPane statusbarPlaceholder;
@@ -111,7 +117,10 @@ public class MainWindow extends UiPart<Stage> {
      */
     void fillInnerParts() {
         personListPanel = new PersonListPanel(logic.getFilteredPersonList());
-        personListPanelPlaceholder.getChildren().add(personListPanel.getRoot());
+        projectTab = new TabPage("Projects");
+        clientTab = new TabPage("Client");
+        clientTab.fillInnerContent(personListPanel.getRoot());
+        tabPane.getTabs().addAll(projectTab.getRoot(), clientTab.getRoot());
 
         resultDisplay = new ResultDisplay();
         resultDisplayPlaceholder.getChildren().add(resultDisplay.getRoot());
