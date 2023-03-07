e4d65eefbadfdb9998c43518ac5b2ad2ef164601
junyi <47441677+immanuelhume@users.noreply.github.com>
2023-03-07 11:22:38 +0800

    Add equals impl for Project

--- src/main/java/mycelium/mycelium/model/project/Project.java
+++ src/main/java/mycelium/mycelium/model/project/Project.java
@@ -119,5 +119,63 @@ public class Project {
         }
         return other != null && this.name.equals(other.name);
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((status == null) ? 0 : status.hashCode());
+        result = prime * result + ((clientEmail == null) ? 0 : clientEmail.hashCode());
+        result = prime * result + ((source == null) ? 0 : source.hashCode());
+        result = prime * result + ((description == null) ? 0 : description.hashCode());
+        result = prime * result + ((acceptedOn == null) ? 0 : acceptedOn.hashCode());
+        result = prime * result + ((deadline == null) ? 0 : deadline.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Project other = (Project) obj;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (status != other.status)
+            return false;
+        if (clientEmail == null) {
+            if (other.clientEmail != null)
+                return false;
+        } else if (!clientEmail.equals(other.clientEmail))
+            return false;
+        if (source == null) {
+            if (other.source != null)
+                return false;
+        } else if (!source.equals(other.source))
+            return false;
+        if (description == null) {
+            if (other.description != null)
+                return false;
+        } else if (!description.equals(other.description))
+            return false;
+        if (acceptedOn == null) {
+            if (other.acceptedOn != null)
+                return false;
+        } else if (!acceptedOn.equals(other.acceptedOn))
+            return false;
+        if (deadline == null) {
+            if (other.deadline != null)
+                return false;
+        } else if (!deadline.equals(other.deadline))
+            return false;
+        return true;
+    }
 }
 
