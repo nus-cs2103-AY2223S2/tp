26e798d84080bea1f448ed3572bb8a2293a612c4
StanleyNeoh <95394480+StanleyNeoh@users.noreply.github.com>
2023-03-08 01:17:23 +0800

    Add project stubs and improve generic listview

--- src/main/java/mycelium/mycelium/ui/common/ListPanel.java
+++ src/main/java/mycelium/mycelium/ui/common/ListPanel.java
@@ -1,9 +1,13 @@
 package mycelium.mycelium.ui.common;
 
+import static java.util.Objects.requireNonNull;
+
+import java.util.function.BiFunction;
 import java.util.logging.Logger;
 
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.Region;
@@ -15,7 +19,6 @@ import mycelium.mycelium.commons.core.LogsCenter;
 public class ListPanel<T> extends UiPart<Region> {
     private static final String FXML = "ListPanel.fxml";
     private final Logger logger = LogsCenter.getLogger(ListPanel.class);
-    private final Cardifier<T> cardifier;
 
     @FXML
     private ListView<T> listView;
@@ -23,29 +26,47 @@ public class ListPanel<T> extends UiPart<Region> {
     /**
      * Creates a {@code ListPanel} with the given {@code ObservableList}.
      */
-    public ListPanel(ObservableList<T> list, Cardifier<T> cardifier) {
+    public ListPanel(ObservableList<T> list) {
         super(FXML);
-        this.cardifier = cardifier;
-        if (list == null) {
-            return;
-        }
+        requireNonNull(list);
         listView.setItems(list);
-        listView.setCellFactory(listview -> new ListViewCell());
+    }
+
+    /**
+     * Creates a {@code ListPanel} with the given {@cpde list} after transforming with {@code biMap}
+     * @param list ObservableList of items
+     * @param biMap BiFunction that transform the list to the approproate {@code UiPart}
+     */
+    public ListPanel(
+        ObservableList<T> list,
+        BiFunction<
+            ? super T,
+            ? super Integer,
+            ? extends UiPart<? extends Node>> biMap
+    ) {
+        this(list);
+        listView.setCellFactory(listView -> new ListViewCell(biMap));
     }
 
     /**
      * Custom {@code ListCell} that displays the graphics of a {@code T}.
      */
     class ListViewCell extends ListCell<T> {
+        private BiFunction<? super T, ? super Integer, ? extends UiPart<? extends Node>> biMap;
+
+        ListViewCell(BiFunction<? super T, ? super Integer, ? extends UiPart<? extends Node>> biMap) {
+            super();
+            this.biMap = biMap;
+        }
+
         @Override
         protected void updateItem(T item, boolean empty) {
             super.updateItem(item, empty);
-
             if (empty || item == null) {
                 setGraphic(null);
                 setText(null);
             } else {
-                setGraphic(cardifier.cardify(item, getIndex() + 1).getRoot());
+                setGraphic(biMap.apply(item, getIndex() + 1).getRoot());
             }
         }
     }
--- src/main/java/mycelium/mycelium/ui/project/Project.java
+++ src/main/java/mycelium/mycelium/ui/project/Project.java
@@ -3,6 +3,8 @@ package mycelium.mycelium.ui.project;
 import java.util.Date;
 import java.util.Optional;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import mycelium.mycelium.model.person.Email;
 // Stub for Project
 
@@ -21,14 +23,59 @@ public class Project {
     /**
      * Creates a stub project
      */
-    public Project() {
-        name = "Bing";
-        status = ProjectStatus.NOT_STARTED;
-        clientEmail = new Email("johndoe@gmail.com");
-        source = "fiver";
-        description = "Create the next google AKA bing";
-        acceptedOn = new Date();
-        deadline = null;
+    public Project(
+        String name,
+        ProjectStatus status,
+        Email email,
+        String source,
+        String description,
+        Date acceptedOn,
+        Date deadline
+    ) {
+        this.name = name;
+        this.status = status;
+        this.clientEmail = email;
+        this.source = source;
+        this.description = description;
+        this.acceptedOn = acceptedOn;
+        this.deadline = Optional.ofNullable(deadline);
+    }
+
+    /**
+     * Generates a list of projects for testing
+     * @return observable list of projects
+     */
+    public static ObservableList<Project> generateStub() {
+        ObservableList<Project> projectStub = FXCollections.observableArrayList();
+        projectStub.addAll(
+            new Project(
+                "Bing",
+                ProjectStatus.NOT_STARTED,
+                new Email("johndoe@gmail.com"),
+                "fiver",
+                "Create the next google AKA bing",
+                new Date(),
+                null
+            ),
+            new Project(
+                "Havard2.0",
+                ProjectStatus.IN_PROGRESS,
+                new Email("EluidKipchoge@gmail.com"),
+                "Behind the alley",
+                "University on the streets",
+                new Date(),
+                new Date()
+            ),
+            new Project(
+                "Build Skynet",
+                ProjectStatus.DONE,
+                new Email("VladPutin@hotmale.com"),
+                "Russia",
+                "Conquer the world",
+                new Date(),
+                null
+            ));
+        return projectStub;
     }
 }
 
