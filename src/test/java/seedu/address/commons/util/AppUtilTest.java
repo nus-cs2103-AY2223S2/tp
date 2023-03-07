25ca2e01d415250e10ca5b4c48d617c25be8fca0
tyx021 <97380048+tyx021@users.noreply.github.com>
2023-03-07 21:13:24 +0800

    Update src/test/java/seedu/address/commons/util/AppUtilTest.java

--- src/test/java/seedu/address/commons/util/AppUtilTest.java
+++ src/test/java/seedu/address/commons/util/AppUtilTest.java
@@ -9,7 +9,7 @@ public class AppUtilTest {
 
     @Test
     public void getImage_exitingImage() {
-        assertNotNull(AppUtil.getImage("/images/clipboard.png"));
+        assertNotNull(AppUtil.getImage("/images/clipboard.PNG"));
     }
 
     @Test
