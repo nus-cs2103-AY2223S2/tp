19176aa8ee58724406933740e76295380ed5aaf6
praveenkrishna0512 <praveenkrishna0512@gmail.com>
2023-03-08 22:04:00 +0800

    Remove unused imports

--- src/main/java/seedu/address/storage/JsonAdaptedDoctor.java
+++ src/main/java/seedu/address/storage/JsonAdaptedDoctor.java
@@ -1,23 +1,15 @@
 package seedu.address.storage;
 
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.person.Doctor;
-import seedu.address.model.person.Email;
-import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
-import seedu.address.model.person.Phone;
 import seedu.address.model.person.Specialty;
 import seedu.address.model.person.Yoe;
-import seedu.address.model.tag.Tag;
 
 /**
  * Jackson-friendly version of {@link Doctor}.
