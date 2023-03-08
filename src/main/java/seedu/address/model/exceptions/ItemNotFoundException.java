b03991874ec40562ddcd40bb71f0396a0aeabed2
SPWwj <weiwenjie@u.nus.edu>
2023-03-08 11:57:44 +0800

    Add UniqueItemListTest and remove redundant exception class

---
+++ src/main/java/seedu/address/model/exceptions/ItemNotFoundException.java
@@ -0,0 +1,5 @@
+package seedu.address.model.exceptions;
+/**
+ * Signals that the operation is unable to find the specified personTask.
+ */
+public class ItemNotFoundException extends RuntimeException {}
--- src/main/java/seedu/address/model/mapping/exceptions/PersonTaskNotFoundException.java
+++
@@ -1,5 +0,0 @@
-package seedu.address.model.mapping.exceptions;
-/**
- * Signals that the operation is unable to find the specified personTask.
- */
-public class PersonTaskNotFoundException extends RuntimeException {}
