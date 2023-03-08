94ac782d24216d9fc1fc2cd8dd16ddc8a0b0887a
Lyndon Lim <hnghng127@gmail.com>
2023-03-08 17:02:47 +0800

    Fix checkstyle errors

--- src/test/java/seedu/address/model/group/UniqueGroupListTest.java
+++ src/test/java/seedu/address/model/group/UniqueGroupListTest.java
@@ -1,20 +1,18 @@
 package seedu.address.model.group;
 
-import org.junit.jupiter.api.Test;
-import seedu.address.model.person.Person;
-import seedu.address.model.person.UniquePersonList;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.HashSet;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
-import static seedu.address.testutil.Assert.assertThrows;
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.person.Person;
 
 public class UniqueGroupListTest {
 
     @Test
-    public void addGroupToList(){
+    public void addGroupToList() {
         Group group1 = new Group("2103T");
         UniqueGroupList ugl = new UniqueGroupList();
         ugl.add(group1);
@@ -22,7 +20,7 @@ public class UniqueGroupListTest {
     }
 
     @Test
-    public void removeGroupFromList(){
+    public void removeGroupFromList() {
         Group group1 = new Group("2103T");
         UniqueGroupList ugl = new UniqueGroupList();
         ugl.add(group1);
