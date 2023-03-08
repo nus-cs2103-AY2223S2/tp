0083880e2536563303f055960ce589a531cefc1c
Lim Diat Bbin <97030167+Diatbbin@users.noreply.github.com>
2023-03-08 18:13:09 +0800

    Update src/main/java/seedu/address/logic/parser/ParserUtil.java
    Co-authored-by: MrTwit99 <97277438+MrTwit99@users.noreply.github.com>

--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -10,7 +10,14 @@ import java.util.Set;
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.util.StringUtil;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.person.*;
+import seedu.address.model.person.Address;
+import seedu.address.model.person.Age;
+import seedu.address.model.person.Email;
+import seedu.address.model.person.Image;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.Phone;
+import seedu.address.model.person.Sex;
 import seedu.address.model.person.student.*;
 import seedu.address.model.tag.Tag;
 
