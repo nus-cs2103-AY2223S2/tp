517fae5d8be735bae7f54481d9b3e8ece7a47659
SeanChinJunKai <e0774037@u.nus.edu>
2023-03-08 21:41:50 +0800

    Fix line length more than 120 characters

--- src/main/java/seedu/address/ui/HelpWindow.java
+++ src/main/java/seedu/address/ui/HelpWindow.java
@@ -17,11 +17,18 @@ public class HelpWindow extends UiPart<Stage> {
 
     public static final String USERGUIDE_URL = "https://ay2223s2-cs2103t-t11-4.github.io/tp/UserGuide.html";
     public static final String HELP_MESSAGE = "Refer to the user guide for more in-depth information: " + USERGUIDE_URL;
-    public static final String ADD_COMMAND_MESSAGE = "1) Add a person to the patient records: \nFormat:add n/NAME i/NRIC p/PHONE_NUMBER e/EMAIL a/ADDRESS [d/DIAGNOSIS] [t/TAG]";
+    public static final String ADD_COMMAND_MESSAGE = "1) Add a person to the patient records: \n"
+                                                     + "Format:add n/NAME i/NRIC p/PHONE_NUMBER e/EMAIL"
+                                                     + " a/ADDRESS [d/DIAGNOSIS] [t/TAG]";
     public static final String GET_COMMAND_MESSAGE = "2) Get a patient's record: \nFormat: get i/NRIC";
-    public static final String EDIT_COMMAND_MESSAGE = "3) Edit an existing person in patient records: \nFormat: edit INDEX [n/NAME] [i/NRIC] [p/PHONE] [e/EMAIL] [a/ADDRESS] [d/DIAGNOSIS] [t/TAG]";
-    public static final String DELETE_COMMAND_MESSAGE = "4) Deletes the patient with the specified NRIC from the address book: \nFormat: delete INDEX\n";
-    public static final String FILTER_COMMAND_MESSAGE = "5) Filters patients according to a particular attribute. Eg. Medicine usage or health conditions: \nFormat: filter KEYWORD [MORE_KEYWORDS]";
+    public static final String EDIT_COMMAND_MESSAGE = "3) Edit an existing person in patient records: \n"
+                                                        + "Format: edit INDEX [n/NAME] [i/NRIC] [p/PHONE]"
+                                                        + " [e/EMAIL] [a/ADDRESS] [d/DIAGNOSIS] [t/TAG]";
+    public static final String DELETE_COMMAND_MESSAGE = "4) Deletes patient with the specified NRIC from"
+                                                        + " the address book: \nFormat: delete INDEX\n";
+    public static final String FILTER_COMMAND_MESSAGE = "5) Filters patients according to a particular attribute. "
+                                                        + "Eg. Medicine usage or health conditions: \n"
+                                                        + "Format: filter KEYWORD [MORE_KEYWORDS]";
     public static final String LIST_COMMAND_MESSAGE = "6) Lists all patients in the records system: \nFormat: list";
     public static final String CLEAR_COMMAND_MESSAGE = "7) Purge all data in records system: \nFormat: clearAll";
     public static final String NOTE_MESSAGE = "Note that square brackets signify optional arguments";
