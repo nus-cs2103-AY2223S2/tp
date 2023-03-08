2dd6cdfbf6858f627b99b756e87a08d3562bfe10
KristenChungJiaYi <kristenchung4@gmail.com>
2023-03-09 05:17:06 +0800

    Add compareTo method for RecurringEvent

--- src/main/java/seedu/address/model/event/RecurringEvent.java
+++ src/main/java/seedu/address/model/event/RecurringEvent.java
@@ -1,19 +1,67 @@
 package seedu.address.model.event;
 
+import java.time.DayOfWeek;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 
 /**
  * Represents an {@code Event} that occurs on a weekly basis.
  */
 public class RecurringEvent extends Event implements Comparable<RecurringEvent> {
     // TODO: Add String representation of RecurringEvent
-    public RecurringEvent(String eventName) {
+
+    private  DayOfWeek dayOfWeek;
+    private LocalTime startTime;
+    private LocalTime endTime;
+
+    public RecurringEvent(String eventName, String dayOfWeek, String startTime, String endTime) {
         super(eventName);
+        this.dayOfWeek = DayOfWeek.valueOf(dayOfWeek);
+        this.startTime = LocalTime.parse(startTime);
+        this.endTime = LocalTime.parse(endTime);
     }
-    // TODO: Implement compareTo by comparing the start date/time of events
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public int getDayValue () {
+        return dayOfWeek.getValue();
+    }
+
+    /**
+     * Compares the day and time between recurringEvent.
+     * @param o the object to be compared.
+     * @return -1 if the event falls before o, 1 if both event falls after o and 0 if the event
+     * conflicts with each other
+     */
     @Override
     public int compareTo(RecurringEvent o) { //TODO: Add implementation for compareTo
+
+        int oDay = o.getDayValue();
+
+        if (getDayValue() > oDay) {
+            return 1;
+        } else if (getDayValue() < oDay) {
+            return -1;
+        }
+
+        LocalTime oStart = o.getStartTime();
+        LocalTime oEnd = o.getEndTime();
+
+        if (this.startTime.isBefore(oStart) && this.endTime.isBefore(oStart)) {
+            return -1;
+        } else if (this.startTime.isAfter(oStart) && this.startTime.isAfter(oEnd)) {
+            return 1;
+        }
+
         return 0;
+
     }
 
     /**
