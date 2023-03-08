c39c82806178e8b76237bdf0b4d6939dc91f0de1
Jun-How <e0725343@u.nus.edu>
2023-03-08 20:53:44 +0800

    Update a few tests for TagCommandParser

--- src/test/java/seedu/modtrek/logic/parser/TagCommandParserTest.java
+++ src/test/java/seedu/modtrek/logic/parser/TagCommandParserTest.java
@@ -3,6 +3,7 @@ package seedu.modtrek.logic.parser;
 import static seedu.modtrek.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.modtrek.logic.commands.CommandTestUtil.INVALID_TAG_DESC;
 import static seedu.modtrek.logic.commands.CommandTestUtil.VALID_CODE_CS1101S;
+import static seedu.modtrek.logic.commands.CommandTestUtil.VALID_TAG_CS1101S;
 import static seedu.modtrek.logic.parser.CliSyntax.PREFIX_TAG;
 import static seedu.modtrek.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.modtrek.logic.parser.CommandParserTestUtil.assertParseSuccess;
@@ -42,14 +43,16 @@ class TagCommandParserTest {
 
     @Test
     public void parse_invalidValue_failure() {
-        assertParseFailure(parser, VALID_CODE_CS1101S + " include " + INVALID_TAG_DESC,
+        assertParseFailure(parser, VALID_CODE_CS1101S + " include" + INVALID_TAG_DESC,
                 Tag.MESSAGE_CONSTRAINTS); // invalid tag
     }
 
     @Test
     public void parse_success() {
-        assertParseSuccess(parser, VALID_CODE_CS1101S + " include ", new TagCommand(CS1101S.getCode(),
-                true, new HashSet<>()));
+        HashSet<Tag> tags = new HashSet<>();
+        tags.add(new Tag("Computer Science Foundation"));
+        assertParseSuccess(parser, VALID_CODE_CS1101S + " include " + PREFIX_TAG + " " + VALID_TAG_CS1101S,
+                new TagCommand(CS1101S.getCode(), true, tags));
     }
 
 }
