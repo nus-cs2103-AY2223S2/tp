fbbe31ccb7f370571fbc049a5c2b6c363458d9ff
Clyde Lhui <62978225+clydelhui@users.noreply.github.com>
2023-03-08 15:35:58 +0800

    Fix typo

--- src/test/java/seedu/address/model/event/IsolatedEventListTest.java
+++ src/test/java/seedu/address/model/event/IsolatedEventListTest.java
@@ -1,6 +1,6 @@
 package seedu.address.model.event;
 
-q1import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
