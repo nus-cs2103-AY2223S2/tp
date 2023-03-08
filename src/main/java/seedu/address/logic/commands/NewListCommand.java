9cc25deb4d5f38dd07523b364e1dd515a738a583
Varstak <e0725939@u.nus.edu>
2023-03-08 16:04:00 +0800

    Refactored List Command

---
+++ src/main/java/seedu/address/logic/commands/NewListCommand.java
@@ -0,0 +1,28 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+// import static seedu.address.model.Model.PREDICATE_SHOW_ALL_OPENINGS;
+
+
+import seedu.address.model.Model;
+
+/**
+ * Lists all openings in the address book to the user.
+ */
+
+public class NewListCommand extends Command {
+
+    public static final String COMMAND_WORD = "list";
+
+    public static final String MESSAGE_SUCCESS = "Listed all openings";
+
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+        // model.updateFilteredOpeningList(PREDICATE_SHOW_ALL_OPENINGS);
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+}
