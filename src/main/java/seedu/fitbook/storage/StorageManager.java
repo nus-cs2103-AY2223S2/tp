2e8b3d57cd3e0a71f1ba45b70975e9b1543dbb16
dfordarius <dfordarius@users.noreply.github.com>
2023-03-09 19:24:43 +0800

    Refactor address to fitbook
    Currently the package is still named "address".
    
    Lets refactor address to fitbook for consistency and clarity.

---
+++ src/main/java/seedu/fitbook/storage/StorageManager.java
@@ -0,0 +1,78 @@
+package seedu.fitbook.storage;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Optional;
+import java.util.logging.Logger;
+
+import seedu.fitbook.commons.core.LogsCenter;
+import seedu.fitbook.commons.exceptions.DataConversionException;
+import seedu.fitbook.model.ReadOnlyFitBook;
+import seedu.fitbook.model.ReadOnlyUserPrefs;
+import seedu.fitbook.model.UserPrefs;
+
+/**
+ * Manages storage of FitBook data in local storage.
+ */
+public class StorageManager implements Storage {
+
+    private static final Logger logger = LogsCenter.getLogger(StorageManager.class);
+    private FitBookStorage fitBookStorage;
+    private UserPrefsStorage userPrefsStorage;
+
+    /**
+     * Creates a {@code StorageManager} with the given {@code FitBookStorage} and {@code UserPrefStorage}.
+     */
+    public StorageManager(FitBookStorage fitBookStorage, UserPrefsStorage userPrefsStorage) {
+        this.fitBookStorage = fitBookStorage;
+        this.userPrefsStorage = userPrefsStorage;
+    }
+
+    // ================ UserPrefs methods ==============================
+
+    @Override
+    public Path getUserPrefsFilePath() {
+        return userPrefsStorage.getUserPrefsFilePath();
+    }
+
+    @Override
+    public Optional<UserPrefs> readUserPrefs() throws DataConversionException, IOException {
+        return userPrefsStorage.readUserPrefs();
+    }
+
+    @Override
+    public void saveUserPrefs(ReadOnlyUserPrefs userPrefs) throws IOException {
+        userPrefsStorage.saveUserPrefs(userPrefs);
+    }
+
+
+    // ================ FitBook methods ==============================
+
+    @Override
+    public Path getFitBookFilePath() {
+        return fitBookStorage.getFitBookFilePath();
+    }
+
+    @Override
+    public Optional<ReadOnlyFitBook> readFitBook() throws DataConversionException, IOException {
+        return readFitBook(fitBookStorage.getFitBookFilePath());
+    }
+
+    @Override
+    public Optional<ReadOnlyFitBook> readFitBook(Path filePath) throws DataConversionException, IOException {
+        logger.fine("Attempting to read data from file: " + filePath);
+        return fitBookStorage.readFitBook(filePath);
+    }
+
+    @Override
+    public void saveFitBook(ReadOnlyFitBook fitBook) throws IOException {
+        saveFitBook(fitBook, fitBookStorage.getFitBookFilePath());
+    }
+
+    @Override
+    public void saveFitBook(ReadOnlyFitBook fitBook, Path filePath) throws IOException {
+        logger.fine("Attempting to write to data file: " + filePath);
+        fitBookStorage.saveFitBook(fitBook, filePath);
+    }
+
+}
