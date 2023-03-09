170b00a071ab31fe8b3a2d29d12d2cc7b3a85c3a
Carmen <carmenang12@gmail.com>
2023-03-09 17:52:21 +0800

    remove language prefix

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -13,6 +13,5 @@ public class CliSyntax {
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
     public static final Prefix PREFIX_LANGUAGE = new Prefix("l/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
-    public static final Prefix PREFIX_LANGUAGE = new Prefix("l/");
 
 }
