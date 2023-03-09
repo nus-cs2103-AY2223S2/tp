f9a00228b4a4b98b2870f4dabce0fb1966389a58
Wei Jie <ongweijie7@gmail.com>
2023-03-09 11:22:16 +0800

    Edit some formtting for import packages which are being detected as errors`

--- src/main/java/seedu/address/model/person/AddressContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/person/AddressContainsKeywordsPredicate.java
@@ -1,7 +1,8 @@
 package seedu.address.model.person;
 
-import java.util.function.Predicate;
 import java.util.List;
+import java.util.function.Predicate;
+
 
 import seedu.address.commons.util.StringUtil;
 
