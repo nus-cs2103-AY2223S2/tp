5435e54453aaec8ea0d2ce2b7beabc9447b8bc47
swxk19 <s.wxk19@gmail.com>
2023-03-10 03:19:24 +0800

    ViewCommand: Change references of 'person' to 'student' instead

--- src/main/java/seedu/address/logic/commands/ViewCommand.java
+++ src/main/java/seedu/address/logic/commands/ViewCommand.java
@@ -8,12 +8,12 @@ import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
-import seedu.address.model.person.Person;
+import seedu.address.model.student.Student;
 
 public class ViewCommand extends Command {
 	public static final String COMMAND_WORD = "view";
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Views the person identified by the index number used in the displayed person list.\n"
+            + ": Views the student identified by the index number used in the displayed student list.\n"
             + "Parameters: INDEX (must be a positive integer)\n"
             + "Example: " + COMMAND_WORD + " 1";
 
@@ -28,14 +28,14 @@ public class ViewCommand extends Command {
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
-        List<Person> lastShownList = model.getFilteredPersonList();
+        List<Student> lastShownList = model.getFilteredStudentList();
 
         if (targetIndex.getZeroBased() >= lastShownList.size()) {
             throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
         }
 
-        Person personToView = lastShownList.get(targetIndex.getZeroBased());
-        return new CommandResult(personToView.toString());
+        Student studentToView = lastShownList.get(targetIndex.getZeroBased());
+        return new CommandResult(studentToView.toString());
     }
     @Override
     public boolean equals(Object other) {
