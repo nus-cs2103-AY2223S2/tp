134233f7f71263c81a68031cfb50ed5facf624e5
daitenshionyan <100074448+daitenshionyan@users.noreply.github.com>
2023-03-11 04:03:55 +0800

    Colorize message state

--- src/main/java/seedu/vms/ui/ResultMessageBox.java
+++ src/main/java/seedu/vms/ui/ResultMessageBox.java
@@ -8,6 +8,10 @@ import seedu.vms.logic.commands.CommandResult;
 public class ResultMessageBox extends UiPart<Region> {
     private static final String FXML_FILE = "ResultMessageBox.fxml";
 
+    private static final String STYLE_CLASS_ERROR = "result-message-error-color";
+    private static final String STYLE_CLASS_WARNING = "result-message-warning-color";
+    private static final String STYLE_CLASS_INFO = "result-message-info-color";
+
     @FXML private Label stateLabel;
     @FXML private Label messageLabel;
 
@@ -16,5 +20,23 @@ public class ResultMessageBox extends UiPart<Region> {
         super(FXML_FILE);
         stateLabel.setText(String.format("[%s]", result.getState().toString()));
         messageLabel.setText(result.getMessage());
+        String colorStyleClass;
+        switch (result.getState()) {
+
+        case ERROR:
+            colorStyleClass = STYLE_CLASS_ERROR;
+            break;
+
+        case WARNING:
+            colorStyleClass = STYLE_CLASS_WARNING;
+            break;
+
+        default:
+            colorStyleClass = STYLE_CLASS_INFO;
+            break;
+
+        }
+        stateLabel.getStyleClass().add(colorStyleClass);
+        messageLabel.getStyleClass().add(colorStyleClass);
     }
 }
