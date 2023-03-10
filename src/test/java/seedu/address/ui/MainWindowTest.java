e7627d4b958de53db0801468f962b79726ad987d
tituswe <tituslowesg@gmail.com>
2023-03-10 22:09:24 +0800

    Create MainWindowTest

---
+++ src/test/java/seedu/address/ui/MainWindowTest.java
@@ -0,0 +1,38 @@
+package seedu.address.ui;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MainWindowTest {
+
+    @Test
+    void getPrimaryStage() {
+        assertTrue(true);
+    }
+
+    @Test
+    void fillInnerParts() {
+        assertTrue(true);
+    }
+
+    @Test
+    void handleSelect() {
+        assertTrue(true);
+    }
+
+    @Test
+    void handleHelp() {
+        assertTrue(true);
+    }
+
+    @Test
+    void show() {
+        assertTrue(true);
+    }
+
+    @Test
+    void getClientListPanel() {
+        assertTrue(true);
+    }
+}
