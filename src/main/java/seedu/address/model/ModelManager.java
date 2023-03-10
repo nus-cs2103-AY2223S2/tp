5d79d05d4eaf0d321440b2d0f7b9571d81eb7a2c
davidni <davidni055@gmail.com>
2023-03-10 12:20:31 +0800

    Correct to bug

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -17,13 +17,14 @@ import seedu.address.model.person.Person;
  * Represents the in-memory model of the address book data.
  */
 public class ModelManager implements Model {
+    
+    protected int index;
     private static final Logger logger = LogsCenter.getLogger(ModelManager.class);
 
     private final AddressBook addressBook;
     private final UserPrefs userPrefs;
     private final FilteredList<Person> filteredPersons;
 
-    protected int index;
 
     /**
      * Initializes a ModelManager with the given addressBook and userPrefs.
