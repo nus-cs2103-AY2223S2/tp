011eb52ca81712f8367c1ebbf8a0b8f8b8836c19
toh-xinyi <tohxinyi10@gmail.com>
2023-03-10 13:55:30 +0800

    Refactor deleteTask command in UG and add new command class for delete task command

---
+++ src/main/java/seedu/address/logic/commands/DeleteTaskCommand.java
@@ -0,0 +1,8 @@
+package seedu.address.logic.commands;
+
+/**
+ * Deletes a task identified using it's displayed index from a specified person's task list.
+ */
+public class DeleteTaskCommand extends Command {
+
+}
