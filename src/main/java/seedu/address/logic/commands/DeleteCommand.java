ae3786cc4be98c4a8c227cb3de7b6a032eac9dac
Yaladah <jeromeseet@hotmail.com>
2023-03-10 19:01:19 +0800

    Minor changes

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -18,8 +18,7 @@ public class DeleteCommand extends Command {
     public static final String COMMAND_WORD = "delete";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Deletes the person identified by the index number "
-            + "used in the displayed task list or the person's name.\n"
+            + ": Deletes the person identified by the index number in the displayed task list or the person's name.\n"
             + "Parameters: INDEX (must be a positive integer)\n"
             + "Example: " + COMMAND_WORD + " 1";
 
