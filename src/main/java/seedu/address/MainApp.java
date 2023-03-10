9cd155e292edf7b59a58c3b127617d2c1014bbd1
tyx021 <yxting1109@gmail.com>
2023-03-10 10:45:41 +0800

    Remove unnecessary code

--- src/main/java/seedu/address/MainApp.java
+++ src/main/java/seedu/address/MainApp.java
@@ -67,11 +67,6 @@ public class MainApp extends Application {
         logic = new LogicManager(model, storage);
 
         ui = new UiManager(logic);
-
-        File file = new File("images");
-        if (!file.exists()) {
-            new File("images").mkdir();
-        }
     }
 
     /**
