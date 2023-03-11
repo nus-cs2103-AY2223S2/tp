389617928c6cc54857bec40fbb579dae67210a8b
Sean Wong <58366602+ExtraShotLatte@users.noreply.github.com>
2023-03-11 21:02:59 +0800

    Add test case to reflect different time

--- src/test/java/seedu/address/model/person/EventTest.java
+++ src/test/java/seedu/address/model/person/EventTest.java
@@ -2,10 +2,7 @@ package seedu.address.model.person;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
+import static seedu.address.logic.commands.CommandTestUtil.*;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalPersons.ALICE;
 import static seedu.address.testutil.TypicalPersons.BOB;
@@ -85,5 +82,9 @@ public class EventTest {
 
         // different tags -> returns false
         assertFalse(ALICE.hashCode() == (BOB.hashCode()));
+
+        // different time -> return false
+        editedAlice = new PersonBuilder(ALICE).withTiming(VALID_START_TIME_AMY, VALID_END_TIME_AMY).build();
+        assertFalse(ALICE.equals(editedAlice));
     }
 }
