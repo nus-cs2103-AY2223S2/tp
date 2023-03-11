e7d88f5ef4fb190c3bb0d9bbb15ecc3eaa3f674c
chuakid <chuakid@gmail.com>
2023-03-11 16:03:34 +0800

    Fix format

--- src/main/java/seedu/address/logic/commands/ViewMeetingsCommand.java
+++ src/main/java/seedu/address/logic/commands/ViewMeetingsCommand.java
@@ -1,10 +1,10 @@
 package seedu.address.logic.commands;
+import static java.util.Objects.requireNonNull;
 
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.meeting.Meeting;
 
-import static java.util.Objects.requireNonNull;
 
 
 /**
