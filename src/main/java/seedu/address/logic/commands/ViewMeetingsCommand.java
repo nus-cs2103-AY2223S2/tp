aeabe5968afb3facf2ae6c12a4837ea0b2c71a0c
chuakid <chuakid@gmail.com>
2023-03-11 15:20:58 +0800

    Implement view meetings command

--- src/main/java/seedu/address/logic/commands/ViewMeetingsCommand.java
+++ src/main/java/seedu/address/logic/commands/ViewMeetingsCommand.java
@@ -2,15 +2,33 @@ package seedu.address.logic.commands;
 
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.meeting.Meeting;
 
 import static java.util.Objects.requireNonNull;
 
+
+/**
+ * Lists all meetings in the address book to the user.
+ */
 public class ViewMeetingsCommand extends Command {
     public static final String COMMAND_WORD = "listm";
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
-        return new CommandResult("Listing meetings");
+        StringBuilder result = new StringBuilder();
+        for (Meeting m : model.getMeetingsList()) {
+            result.append(m.getTitle());
+            result.append("\n");
+            result.append("Description: ").append(m.getDescription());
+            result.append("\n");
+            result.append("At: ").append(m.getDateTime());
+            result.append("\n");
+            result.append("Attendees: ").append(m.getAttendees());
+            result.append("\n");
+            result.append("Location: ").append(m.getLocation());
+            result.append("\n\n");
+        }
+        return new CommandResult(result.toString());
     }
 }
