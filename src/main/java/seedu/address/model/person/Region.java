0bcbe188a6bd27e1efbe6862654a8274f44d5127
bokung <e0774428@u.nus.edu>
2023-03-11 14:48:00 +0800

    Fix Checkstyle

--- src/main/java/seedu/address/model/person/Region.java
+++ src/main/java/seedu/address/model/person/Region.java
@@ -10,7 +10,7 @@ package seedu.address.model.person;
  */
 public class Region {
 
-    public static final String MESSAGE_CONSTRAINTS = 
+    public static final String MESSAGE_CONSTRAINTS =
             "Region in Singapore should be either North, South, East, West or Central\n" +
             "No other regions are accepted!\n";
 
