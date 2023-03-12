d928db91697b3c0720342c859b48aafb780900e9
Samarth Verma <samarthverma@icloud.com>
2023-03-13 04:12:38 +0800

    Add birthday model

---
+++ src/main/java/seedu/address/model/person/Birthday.java
@@ -0,0 +1,55 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Birthday {
+    public static final String MESSAGE_CONSTRAINTS = "Birthday should be of the format DD/MM/YYYY";
+    public static final String VALIDATION_REGEX = "^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[012])/([0-9]{4})$";
+    public final LocalDate value;
+
+    /**
+     * Constructs an {@code Birthday}.
+     *
+     * @param birthday A valid birthday.
+     */
+    public Birthday(String birthday) {
+        requireNonNull(birthday);
+        checkArgument(isValidBirthday(birthday), MESSAGE_CONSTRAINTS);
+
+        String[] date = birthday.split("/");
+        int day = Integer.parseInt(date[0]);
+        int month = Integer.parseInt(date[1]);
+        int year = Integer.parseInt(date[2]);
+
+        value = LocalDate.of(year, month, day);
+    }
+
+    /**
+     * Returns if a given string is a valid birthday.
+     */
+    public static boolean isValidBirthday(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return value.format(DateTimeFormatter.ofPattern("MMM dd yyyy"));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Birthday // instanceof handles nulls
+                        && value.equals(((Birthday) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+}
---
+++ src/test/java/seedu/address/model/person/BirthdayTest.java
@@ -0,0 +1,35 @@
+package seedu.address.model.person;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+public class BirthdayTest {
+
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new Birthday(null));
+    }
+
+    @Test
+    public void constructor_invalidBirthday_throwsIllegalArgumentException() {
+        String invalidBirthday = "";
+        assertThrows(IllegalArgumentException.class, () -> new Birthday(invalidBirthday));
+
+        String invalidBirthdayTonight = "tonight";
+        assertThrows(IllegalArgumentException.class, () -> new Birthday(invalidBirthdayTonight));
+
+    }
+
+    @Test
+    public void constructor_works() {
+        String validBirthday = "01/01/2000";
+        assertTrue(Birthday.isValidBirthday(validBirthday));
+
+        Birthday bd = new Birthday(validBirthday);
+        assertTrue(bd.toString().equals("Jan 01 2000"));
+    }
+
+}
