28949d373ca8be7e243648b1bdccd1fc0efd62de
Wang <shaofeng1234567@gmail.com>
2023-03-13 00:16:29 +0800

    Minor Style Change

--- src/test/java/seedu/address/logic/commands/DeleteCommandsTest.java
+++ src/test/java/seedu/address/logic/commands/DeleteCommandsTest.java
@@ -1,10 +1,12 @@
 package seedu.address.logic.commands;
+
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
+
 import seedu.address.commons.core.index.Index;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
