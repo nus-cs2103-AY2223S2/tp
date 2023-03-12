164e56f6960a5e2371256474abfec88902a76cf3
RyanQiu1 <ryanqjx@gmail.com>
2023-03-12 16:40:15 +0800

    Fix checkstyle

--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -1,10 +1,14 @@
 package seedu.address.testutil;
 
-import java.nio.channels.WritableByteChannel;
 import java.util.HashSet;
 import java.util.Set;
 
-import seedu.address.model.person.*;
+import seedu.address.model.person.Address;
+import seedu.address.model.person.Email;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.Phone;
+import seedu.address.model.person.Website;
 import seedu.address.model.tag.Tag;
 import seedu.address.model.util.SampleDataUtil;
 
@@ -91,8 +95,13 @@ public class PersonBuilder {
         return this;
     }
 
+    /**
+     * Set the {@code Website} of the {@code Person} that we are building.
+     * @param website
+     * @return
+     */
     public PersonBuilder withWebsite(String website) {
-        this.website= new Website(website);
+        this.website = new Website(website);
         return this;
     }
 
