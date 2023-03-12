ad86e52d056b541e326d836ce3ac173433069e17
Saraan Kannusami <saraankannusami@gmail.com>
2023-03-13 05:53:17 +0800

    Add AddMeetingCommandParser to parse 'addm' requests and relevant tests
    This commit adds a new parser, AddMeetingCommandParser, to parse requests to add new meetings. The parser takes input in a specific format and returns a structured representation of the request that can be used by the application. Additionally, this commit includes tests to ensure that the parser is working correctly.

---
+++ src/main/java/seedu/address/logic/parser/AddMeetingCommandParser.java
@@ -0,0 +1,46 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.AddMeetingCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.meeting.DateTime;
+import seedu.address.model.meeting.Description;
+import seedu.address.model.meeting.Location;
+import seedu.address.model.meeting.Title;
+import seedu.address.model.person.Name;
+
+import java.util.Set;
+import java.util.stream.Stream;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.*;
+
+public class AddMeetingCommandParser implements Parser<AddMeetingCommand> {
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
+    }
+
+    @Override
+    public AddMeetingCommand parse(String userInput) throws ParseException {
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(userInput, PREFIX_MEETING_TITLE, PREFIX_PERSON, PREFIX_DATE,
+                        PREFIX_LOCATION, PREFIX_DESCRIPTION);
+
+        if (!arePrefixesPresent(argMultimap, PREFIX_MEETING_TITLE, PREFIX_DATE, PREFIX_PERSON, PREFIX_LOCATION,
+                PREFIX_DESCRIPTION)
+                || !argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddMeetingCommand.MESSAGE_USAGE));
+        }
+
+        Title title = ParserUtil.parseTitle(argMultimap.getValue(PREFIX_MEETING_TITLE).get());
+        Set<Name> attendeeNames = ParserUtil.parseNames(argMultimap.getAllValues(PREFIX_PERSON));
+        DateTime dateTime = ParserUtil.parseDateTime(argMultimap.getValue(PREFIX_DATE).get());
+        Location location = ParserUtil.parseLocation(argMultimap.getValue(PREFIX_LOCATION).get());
+        Description description = ParserUtil.parseDescription(argMultimap.getValue(PREFIX_DESCRIPTION).get());
+
+        return new AddMeetingCommand(title, dateTime, attendeeNames, location, description);
+    }
+}
