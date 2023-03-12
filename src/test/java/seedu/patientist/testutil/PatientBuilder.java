a7def0b2f455179a1c9e71e16c8b0989346fde80
LIN_CHIEH <linchieh@stu.comp.nus.edu.sg>
2023-03-12 13:27:10 +0800

    Revert to java 11 in build.gradle Fixes a mistaken change of java version for project to 15

--- src/test/java/seedu/patientist/testutil/PatientBuilder.java
+++ src/test/java/seedu/patientist/testutil/PatientBuilder.java
@@ -14,12 +14,9 @@ import seedu.patientist.model.util.SampleDataUtil;
  */
 public class PatientBuilder extends PersonBuilder {
 
-    public static final String DEFAULT_STATUS =
-            """
-            The patient is currently in stable condition.
-            The patient is required to attend physiotherapy 3 times a week
-            The patient has medical allergy as follows: None
-            """;
+    public static final String DEFAULT_STATUS = "The patient is currently in stable condition. \n"
+            + "The patient is required to attend physiotherapy 3 times a week. \n"
+            + "The patient has medical allergy as follows: None";
     public static final String DEFAULT_ID = "A1234567890B";
 
     private PatientStatusDetails status;
