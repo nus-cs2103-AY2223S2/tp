76b59fee02c0dfdbf42a233ae365b3d21ad9d5eb
weilin1202 <e0775018@u.nus.edu>
2023-03-12 08:33:53 +0800

    Update Index file

--- src/main/java/seedu/address/commons/core/index/Index.java
+++ src/main/java/seedu/address/commons/core/index/Index.java
@@ -51,9 +51,4 @@ public class Index {
                 || (other instanceof Index // instanceof handles nulls
                 && zeroBasedIndex == ((Index) other).zeroBasedIndex); // state check
     }
-
-    @Override
-    public String toString() {
-        return Integer.toString(zeroBasedIndex);
-    }
 }
