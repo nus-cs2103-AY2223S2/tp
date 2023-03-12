541bdc88f9f56ce08e9db9fe50d4c4bc45d95d9d
mayuanxin1234 <95178026+mayuanxin1234@users.noreply.github.com>
2023-03-12 23:22:42 +0800

    Revert "Revert "Branch add parent""

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -7,25 +7,24 @@ public class CliSyntax {
 
     /* Prefix definitions */
     public static final Prefix PREFIX_ADD = new Prefix("add");
-    public static final Prefix PREFIX_COMMENTCOMMAND = new Prefix("comment");
-    public static final Prefix PREFIX_NAME = new Prefix("n/");
-    public static final Prefix PREFIX_PHONE = new Prefix("p/");
-    public static final Prefix PREFIX_EMAIL = new Prefix("e/");
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
-    public static final Prefix PREFIX_COMMENT = new Prefix("com/");
-    public static final Prefix PREFIX_TAG = new Prefix("t/");
-    public static final Prefix PREFIX_INDEXNUMBER = new Prefix("in/");
-    public static final Prefix PREFIX_SEX = new Prefix("s/");
-    public static final Prefix PREFIX_PARENTNAME = new Prefix("pn/");
-    public static final Prefix PREFIX_AGESTUDENT = new Prefix("ageS/");
-    public static final Prefix PREFIX_IMAGESTUDENT = new Prefix("imgS/");
-    public static final Prefix PREFIX_EMAILSTUDENT = new Prefix("eS/");
-    public static final Prefix PREFIX_PHONESTUDENT = new Prefix("phS/");
-    public static final Prefix PREFIX_CCA = new Prefix("cca/");
+    public static final Prefix PREFIX_AGE = new Prefix("ageS/");
     public static final Prefix PREFIX_ATTENDANCE = new Prefix("att/");
+    public static final Prefix PREFIX_CCA = new Prefix("cca/");
+    public static final Prefix PREFIX_COMMENT = new Prefix("com/");
+    public static final Prefix PREFIX_COMMENTCOMMAND = new Prefix("comment");
+    public static final Prefix PREFIX_EMAIL = new Prefix("e/");
+    public static final Prefix PREFIX_EMAILSTUDENT = new Prefix("eS/");
     public static final Prefix PREFIX_HOMEWORK = new Prefix("hw/");
+    public static final Prefix PREFIX_IMAGESTUDENT = new Prefix("imgS/");
+    public static final Prefix PREFIX_IMAGEPARENT = new Prefix("imgP/");
+    public static final Prefix PREFIX_INDEXNUMBER = new Prefix("in/");
+    public static final Prefix PREFIX_NAME = new Prefix("n/");
+    public static final Prefix PREFIX_PARENTNAME = new Prefix("pn/");
+    public static final Prefix PREFIX_PHONESTUDENT = new Prefix("phS/");
+    public static final Prefix PREFIX_PHONE = new Prefix("p/");
+    public static final Prefix PREFIX_RELATIONSHIP = new Prefix("rls/");
+    public static final Prefix PREFIX_SEX = new Prefix("s/");
+    public static final Prefix PREFIX_TAG = new Prefix("t/");
     public static final Prefix PREFIX_TEST = new Prefix("test/");
-
-
-
 }
--- src/main/java/seedu/address/model/AddressBook.java
+++ src/main/java/seedu/address/model/AddressBook.java
@@ -7,6 +7,8 @@ import java.util.List;
 import javafx.collections.ObservableList;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.UniquePersonList;
+import seedu.address.model.person.parent.Parent;
+import seedu.address.model.person.parent.UniqueParentList;
 import seedu.address.model.person.student.Student;
 import seedu.address.model.person.student.UniqueStudentList;
 
@@ -20,6 +22,8 @@ public class AddressBook implements ReadOnlyAddressBook {
 
     private final UniqueStudentList students;
 
+    private final UniqueParentList parents;
+
     /*
      * The 'unusual' code block below is a non-static initialization block, sometimes used to avoid duplication
      * between constructors. See https://docs.oracle.com/javase/tutorial/java/javaOO/initial.html
@@ -30,6 +34,7 @@ public class AddressBook implements ReadOnlyAddressBook {
     {
         persons = new UniquePersonList();
         students = new UniqueStudentList();
+        parents = new UniqueParentList();
     }
 
     public AddressBook() {}
@@ -56,6 +61,10 @@ public class AddressBook implements ReadOnlyAddressBook {
         this.students.setStudents(students);
     }
 
+    public void setParents(List<Parent> parents) {
+        this.parents.setParents(parents);
+    }
+
     /**
      * Resets the existing data of this {@code AddressBook} with {@code newData}.
      */
@@ -78,7 +87,7 @@ public class AddressBook implements ReadOnlyAddressBook {
     /**
      * Returns boolean value true if a student with the same identity as {@code student} exists in PowerConnect.
      *
-     * @param student Student object that needs to be checked for duplication
+     * @param student Student object that needs to be checked for duplication.
      * @return Boolean value indicating whether the student already exists in PowerConnect.
      */
     public boolean hasStudent(Student student) {
@@ -87,11 +96,21 @@ public class AddressBook implements ReadOnlyAddressBook {
     }
 
     /**
+     * Returns boolean value true if a parent with the same identity as {@code parent} exists in PowerConnect.
+     *
+     * @param parent Parent object that needs to be checked for duplication.
+     * @return Boolean value indicating whether the parent already exists in PowerConnect.
+     */
+    public boolean hasParent(Parent parent) {
+        requireNonNull(parent);
+        return parents.contains(parent);
+    }
+
+    /**
      * Adds a person to the address book.
      * The person must not already exist in the address book.
      */
     public void addPerson(Person p) {
-
         persons.add(p);
     }
 
@@ -101,6 +120,10 @@ public class AddressBook implements ReadOnlyAddressBook {
      */
     public void addStudent(Student s) {
         students.add(s);
+    }
+
+    public void addParent(Parent p) {
+        parents.add(p);
     }
 
     /**
@@ -118,6 +141,11 @@ public class AddressBook implements ReadOnlyAddressBook {
         students.setStudent(target, editedStudent);
     }
 
+    public void setParent(Parent target, Parent editedParent) {
+        requireNonNull(editedParent);
+        parents.setParent(target, editedParent);
+    }
+
     /**
      * Removes {@code key} from this {@code AddressBook}.
      * {@code key} must exist in the address book.
@@ -132,6 +160,10 @@ public class AddressBook implements ReadOnlyAddressBook {
      */
     public void removeStudent(Student key) {
         students.remove(key);
+    }
+
+    public void removeParent(Parent key) {
+        parents.remove(key);
     }
     //// util methods
 
@@ -150,6 +182,10 @@ public class AddressBook implements ReadOnlyAddressBook {
     @Override
     public ObservableList<Student> getStudentList() {
         return students.asUnmodifiableObservableList();
+    }
+
+    public ObservableList<Parent> getParentList() {
+        return parents.asUnmodifiableObservableList();
     }
 
     @Override
