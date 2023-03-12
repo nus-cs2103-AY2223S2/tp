29eadc480357ee36037351b857f95ad0995bffea
Jiahua <edures@protonmail.com>
2023-03-13 02:40:36 +0800

    Add tests for group commands and parser classes

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -18,6 +18,8 @@ import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
+import seedu.address.model.group.Group;
+import seedu.address.model.group.GroupNameContainsKeywordsPredicate;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
 import seedu.address.model.person.Person;
 import seedu.address.testutil.EditPersonDescriptorBuilder;
@@ -125,6 +127,20 @@ public class CommandTestUtil {
         model.updateFilteredPersonList(new NameContainsKeywordsPredicate(Arrays.asList(splitName[0])));
 
         assertEquals(1, model.getFilteredPersonList().size());
+    }
+
+    /**
+     * Updates {@code model}'s filtered list to show only the group at the given {@code targetIndex} in the
+     * {@code model}'s address book.
+     */
+    public static void showGroupAtIndex(Model model, Index targetIndex) {
+        assertTrue(targetIndex.getZeroBased() < model.getFilteredGroupList().size());
+
+        Group group = model.getFilteredGroupList().get(targetIndex.getZeroBased());
+        final String[] splitName = group.getName().split("\\s+");
+        model.updateFilteredGroupList(new GroupNameContainsKeywordsPredicate(Arrays.asList(splitName[0])));
+
+        assertEquals(1, model.getFilteredGroupList().size());
     }
 
 }
--- src/test/java/seedu/address/logic/commands/group/GroupFindCommandTest.java
+++ src/test/java/seedu/address/logic/commands/group/GroupFindCommandTest.java
@@ -1,14 +1,77 @@
 package seedu.address.logic.commands.group;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.commons.core.Messages.MESSAGE_GROUPS_LISTED_OVERVIEW;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalGroups.CS2103_GROUP;
+import static seedu.address.testutil.TypicalGroups.FRIENDS_GROUP;
+import static seedu.address.testutil.TypicalGroups.getTypicalAddressBookWithEmptyGroups;
+
+import java.util.Arrays;
+import java.util.Collections;
+
 import org.junit.jupiter.api.Test;
 
-class GroupFindCommandTest {
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.group.GroupNameContainsKeywordsPredicate;
+
+/**
+ * Contains integration tests (interaction with the Model) for {@code FindCommand}.
+ */
+public class GroupFindCommandTest {
+
+    private Model model = new ModelManager(getTypicalAddressBookWithEmptyGroups(), new UserPrefs());
+    private Model expectedModel = new ModelManager(getTypicalAddressBookWithEmptyGroups(), new UserPrefs());
 
     @Test
-    void execute() {
+    public void equals() {
+        GroupNameContainsKeywordsPredicate firstPredicate =
+                new GroupNameContainsKeywordsPredicate(Collections.singletonList("first"));
+        GroupNameContainsKeywordsPredicate secondPredicate =
+                new GroupNameContainsKeywordsPredicate(Collections.singletonList("second"));
+
+        GroupFindCommand findFirstCommand = new GroupFindCommand(firstPredicate);
+        GroupFindCommand findSecondCommand = new GroupFindCommand(secondPredicate);
+
+        // same object -> returns true
+        assertTrue(findFirstCommand.equals(findFirstCommand));
+
+        // same values -> returns true
+        GroupFindCommand findFirstCommandCopy = new GroupFindCommand(firstPredicate);
+        assertTrue(findFirstCommand.equals(findFirstCommandCopy));
+
+        // different keyword -> returns false
+        assertFalse(findFirstCommand.equals(findSecondCommand));
     }
 
     @Test
-    void testEquals() {
+    public void execute_zeroKeywords_noGroupFound() {
+        String expectedMessage = String.format(MESSAGE_GROUPS_LISTED_OVERVIEW, 0);
+        GroupNameContainsKeywordsPredicate predicate = preparePredicate(" ");
+        GroupFindCommand command = new GroupFindCommand(predicate);
+        expectedModel.updateFilteredGroupList(predicate);
+        assertCommandSuccess(command, model, expectedMessage, expectedModel);
+        assertEquals(Collections.emptyList(), model.getFilteredGroupList());
+    }
+
+    @Test
+    public void execute_multipleKeywords_multipleGroupsFound() {
+        String expectedMessage = String.format(MESSAGE_GROUPS_LISTED_OVERVIEW, 2);
+        GroupNameContainsKeywordsPredicate predicate = preparePredicate("Friends CS2103");
+        GroupFindCommand command = new GroupFindCommand(predicate);
+        expectedModel.updateFilteredGroupList(predicate);
+        assertCommandSuccess(command, model, expectedMessage, expectedModel);
+        assertEquals(Arrays.asList(FRIENDS_GROUP, CS2103_GROUP), model.getFilteredGroupList());
+    }
+
+    /**
+     * Parses {@code userInput} into a {@code GroupNameContainsKeywordsPredicate}.
+     */
+    private GroupNameContainsKeywordsPredicate preparePredicate(String userInput) {
+        return new GroupNameContainsKeywordsPredicate(Arrays.asList(userInput.split("\\s+")));
     }
 }
