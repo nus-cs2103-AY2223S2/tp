fc6eaa39a67e297a076a518c7809912b6c5054a7
dohaduong <duonghado03@gmail.com>
2023-03-12 22:05:08 +0800

    Update TimetableCommand Missing timetable command hinders users from using timetable function easily.
    Add timetable command helps users to access timetable more easily from main window.
    
    Let's update timetable command

---
+++ src/main/java/seedu/address/logic/commands/TimetableCommand.java
@@ -0,0 +1,13 @@
+package seedu.address.logic.commands;
+
+import seedu.address.model.jobs.UniqueDeliveryList;
+
+public class TimetableCommand {
+    private final UniqueDeliveryList jobs;
+
+    {
+        jobs = new UniqueDeliveryList();
+    }
+
+
+}
