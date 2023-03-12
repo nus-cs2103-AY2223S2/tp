719c9dbecb55b24aa90188eae3eb6df612a32aaf
Jun-How <e0725343@u.nus.edu>
2023-03-13 01:10:54 +0800

    Add and update test cases to commit to codecov

--- src/test/java/seedu/modtrek/logic/commands/HelpCommandTest.java
+++ src/test/java/seedu/modtrek/logic/commands/HelpCommandTest.java
@@ -19,8 +19,44 @@ public class HelpCommandTest {
     }
 
     @Test
-    public void execute_helpValidArgs_success() {
+    public void execute_helpAdd_success() {
         CommandResult expectedCommandResult = new CommandResult(AddCommand.MESSAGE_USAGE);
         assertCommandSuccess(new HelpCommand(AddCommand.MESSAGE_USAGE), model, expectedCommandResult, expectedModel);
+    }
+
+    @Test
+    public void execute_helpEdit_success() {
+        CommandResult expectedCommandResult = new CommandResult(EditCommand.MESSAGE_USAGE);
+        assertCommandSuccess(new HelpCommand(EditCommand.MESSAGE_USAGE), model, expectedCommandResult, expectedModel);
+    }
+
+    @Test
+    public void execute_helpDelete_success() {
+        CommandResult expectedCommandResult = new CommandResult(DeleteCommand.MESSAGE_USAGE);
+        assertCommandSuccess(new HelpCommand(DeleteCommand.MESSAGE_USAGE), model, expectedCommandResult, expectedModel);
+    }
+
+    @Test
+    public void execute_helpTag_success() {
+        CommandResult expectedCommandResult = new CommandResult(TagCommand.MESSAGE_USAGE);
+        assertCommandSuccess(new HelpCommand(TagCommand.MESSAGE_USAGE), model, expectedCommandResult, expectedModel);
+    }
+
+    @Test
+    public void execute_helpList_success() {
+        CommandResult expectedCommandResult = new CommandResult(ListCommand.MESSAGE_USAGE);
+        assertCommandSuccess(new HelpCommand(ListCommand.MESSAGE_USAGE), model, expectedCommandResult, expectedModel);
+    }
+
+    @Test
+    public void execute_helpFind_success() {
+        CommandResult expectedCommandResult = new CommandResult(FindCommand.MESSAGE_USAGE);
+        assertCommandSuccess(new HelpCommand(FindCommand.MESSAGE_USAGE), model, expectedCommandResult, expectedModel);
+    }
+
+    @Test
+    public void execute_helpExit_success() {
+        CommandResult expectedCommandResult = new CommandResult(ExitCommand.MESSAGE_USAGE);
+        assertCommandSuccess(new HelpCommand(ExitCommand.MESSAGE_USAGE), model, expectedCommandResult, expectedModel);
     }
 }
--- src/test/java/seedu/modtrek/logic/parser/HelpCommandParserTest.java
+++ src/test/java/seedu/modtrek/logic/parser/HelpCommandParserTest.java
@@ -7,7 +7,13 @@ import static seedu.modtrek.logic.parser.CommandParserTestUtil.assertParseSucces
 import org.junit.jupiter.api.Test;
 
 import seedu.modtrek.logic.commands.AddCommand;
+import seedu.modtrek.logic.commands.DeleteCommand;
+import seedu.modtrek.logic.commands.EditCommand;
+import seedu.modtrek.logic.commands.ExitCommand;
+import seedu.modtrek.logic.commands.FindCommand;
 import seedu.modtrek.logic.commands.HelpCommand;
+import seedu.modtrek.logic.commands.ListCommand;
+import seedu.modtrek.logic.commands.TagCommand;
 
 public class HelpCommandParserTest {
     private HelpCommandParser parser = new HelpCommandParser();
@@ -18,8 +24,38 @@ public class HelpCommandParserTest {
     }
 
     @Test
-    public void parse_helpValidArgs_success() {
+    public void parse_helpAdd_success() {
         assertParseSuccess(parser, "add", new HelpCommand(AddCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_helpEdit_success() {
+        assertParseSuccess(parser, "edit", new HelpCommand(EditCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_helpDelete_success() {
+        assertParseSuccess(parser, "delete", new HelpCommand(DeleteCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_helpTag_success() {
+        assertParseSuccess(parser, "tag", new HelpCommand(TagCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_helpList_success() {
+        assertParseSuccess(parser, "list", new HelpCommand(ListCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_helpFind_success() {
+        assertParseSuccess(parser, "find", new HelpCommand(FindCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_helpExit_success() {
+        assertParseSuccess(parser, "exit", new HelpCommand(ExitCommand.MESSAGE_USAGE));
     }
 
     @Test
