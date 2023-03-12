fe51c1c938abf2f1af0d43c11fabcc05ebb84a3e
minosx31 <n_minghao28@hotmail.com>
2023-03-12 15:02:41 +0800

    Update test cases

--- src/test/java/seedu/address/logic/commands/CopyCommandTest.java
+++ src/test/java/seedu/address/logic/commands/CopyCommandTest.java
@@ -1,19 +1,10 @@
 package seedu.address.logic.commands;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
-import java.awt.Toolkit;
-import java.awt.datatransfer.Clipboard;
-import java.awt.datatransfer.DataFlavor;
-import java.awt.datatransfer.StringSelection;
-import java.awt.datatransfer.UnsupportedFlavorException;
-import java.io.IOException;
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.Messages;
@@ -21,36 +12,17 @@ import seedu.address.commons.core.index.Index;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
-import seedu.address.model.person.Person;
 
 public class CopyCommandTest {
 
-    private final Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
     private final Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
     private final Model expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
-    private final StringSelection emptyString = new StringSelection("");
-
-    @BeforeEach
-    public void setUp() {
-        // Clear clipboard contents
-        clipboard.setContents(emptyString, null);
-    }
 
     @Test
-    public void execute_copyValidIndex_success() throws IOException, UnsupportedFlavorException {
-        Person personToCopy = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+    public void execute_copyValidIndex_success() {
         CopyCommand copyCommand = new CopyCommand(INDEX_FIRST_PERSON);
 
         assertCommandSuccess(copyCommand, model, CopyCommand.MESSAGE_COPY_SUCCESS, expectedModel);
-
-        // Check contents of clipboard
-        try {
-            String expectedInformation = copyCommand.getInformation(personToCopy);
-            String actualInformation = (String) clipboard.getData(DataFlavor.stringFlavor);
-            assertEquals(actualInformation, expectedInformation);
-        } catch (IOException | UnsupportedFlavorException e) {
-            throw new AssertionError("Execution of command should not fail.", e);
-        }
     }
 
     @Test
