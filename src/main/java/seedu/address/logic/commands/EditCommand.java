b5347da69d5ebd34b1c0c8564f9cf2b761b195f1
LAPTOP-DOG86KJ2\pinoc <pinochoi@yahoo.com.sg>
2023-03-12 15:59:31 +0800

    EditCommand class and Model interface had checkstyle issue
    Let's edit the checkstyle issue

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -70,10 +70,6 @@ public class EditCommand extends Command {
         requireNonNull(model);
         List<Person> lastShownList = model.getFilteredPersonList();
 
-//        if (index.getZeroBased() >= lastShownList.size()) {
-//            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
-//        }
-
         if (index.getZeroBased() >= lastShownList.size()) {
             throw new CommandException(Messages.MESSAGE_INVALID_PERSON_NAME);
         }
