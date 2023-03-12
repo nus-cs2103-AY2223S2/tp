eb2eb7cba511f3fc52ca697ec469b5c9c11835a1
Saraan Kannusami <saraankannusami@gmail.com>
2023-03-13 03:32:19 +0800

    Add new Prefixes to CliSyntax
    This commit adds the following Prefixes to CliSyntax:
    - PREFIX_MEETING_TITLE - m/
    - PREFIX_DATE - d/
    - PREFIX_LOCATION - l/
    - PREFIX_DESCRIPTION - des/
    - PREFIX_PERSON - p/

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -11,5 +11,10 @@ public class CliSyntax {
     public static final Prefix PREFIX_EMAIL = new Prefix("e/");
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
+    public static final Prefix PREFIX_MEETING_TITLE = new Prefix("m/");
+    public static final Prefix PREFIX_DATE = new Prefix("d/");
+    public static final Prefix PREFIX_LOCATION = new Prefix("l/");
+    public static final Prefix PREFIX_DESCRIPTION = new Prefix("des/");
+    public static final Prefix PREFIX_PERSON = new Prefix("p/");
 
 }
