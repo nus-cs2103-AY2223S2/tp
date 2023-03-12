660a1dede9623b01e3c9284feb4d605e67f61676
toh-xinyi <tohxinyi10@gmail.com>
2023-03-12 15:17:23 +0800

    Remove unused import statements from JsonAdaptedTask

--- src/main/java/seedu/address/storage/JsonAdaptedTask.java
+++ src/main/java/seedu/address/storage/JsonAdaptedTask.java
@@ -2,9 +2,8 @@ package seedu.address.storage;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonValue;
-import seedu.address.commons.exceptions.IllegalValueException;
 
+import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.person.Name;
 import seedu.address.model.task.Task;
 
