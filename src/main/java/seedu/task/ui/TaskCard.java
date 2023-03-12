8511913c7acece0e3bd648d0919dcbd0eb147108
lywich <lywichcy@gmail.com>
2023-03-12 22:34:28 +0800

    Update TaskCard.java
    Ensure pos will be non-negative as hashcode may return negative numbers.
    
    Change text colour to black for visibility.
    Remove black background.

--- src/main/java/seedu/task/ui/TaskCard.java
+++ src/main/java/seedu/task/ui/TaskCard.java
@@ -79,9 +79,9 @@ public class TaskCard extends UiPart<Region> {
             "#911eb4", "#46f0f0", "#f032e6", "#bcf60c", "#fabebe",
             "#008080", "#e6beff", "#9a6324", "#fffac8", "#800000",
             "#aaffc3", "#808000", "#ffd8b1", "#000075", "#808080",
-            "#ffffff", "#000000"
+            "#ffffff"
         };
-        int pos = tagName.hashCode() % arr.length;
+        int pos = Math.abs(tagName.hashCode() % arr.length);
         return arr[pos];
     }
 
