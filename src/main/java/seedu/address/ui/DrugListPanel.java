1d1b775a2ece1e3631db5f964d9d5f7f0b17c96f
Shuwen <chenshuwen0210@gmail.com>
2023-03-12 09:36:42 +0800

    add additional column to the drug list panel to display selected patient details.

--- src/main/java/seedu/address/ui/DrugListPanel.java
+++ src/main/java/seedu/address/ui/DrugListPanel.java
@@ -2,20 +2,56 @@ package seedu.address.ui;
 
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.Region;
 import seedu.address.model.drug.Drug;
+import seedu.address.model.person.Patient;
 
 public class DrugListPanel extends UiPart<Region> {
     private static final String FXML = "DrugListPanel.fxml";
+
     @FXML
     private ListView<Drug> drugListView;
+
+    @FXML
+    private Label selectedTradeName;
+
+    @FXML
+    private Label selectedActiveIngredient;
+
+    @FXML
+    private Label selectedDirection;
+
+    @FXML
+    private Label selectedPurposes;
+
+    @FXML
+    private Label selectedSideEffects;
+
+    @FXML
+    private Label selectedStorageCount;
+
     public DrugListPanel(ObservableList<Drug> drugList) {
         super(FXML);
         // DRUG LIST
         drugListView.setItems(drugList);
         drugListView.setCellFactory(listView -> new DrugListViewCell());
+        displayDrugDetail();
+    }
+
+    private void displayDrugDetail() {
+        drugListView.setOnMouseClicked(event -> {
+            Drug selectedDrug = drugListView.getSelectionModel().getSelectedItem();
+            System.out.println(selectedDrug.toString());
+            selectedTradeName.setText(selectedDrug.getTradeName().tradeName);
+            selectedActiveIngredient.setText("Active Ingredient: " + selectedDrug.getActiveIngredient().value);
+            selectedDirection.setText("Phone Number: " + selectedDrug.getDirection().value);
+            selectedPurposes.setText("Address: " + selectedDrug.getPurposes().purpose);
+            selectedSideEffects.setText("Email: " + selectedDrug.getSideEffects().sideEffect);
+            selectedStorageCount.setText("Date of Birth: " + selectedDrug.getStorageCount().toString());
+        });
     }
 
     /**
