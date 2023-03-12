871c8b39023071e2b5a552fb99ddca258d6d90e1
Wang <shaofeng1234567@gmail.com>
2023-03-13 00:18:32 +0800

    Minor Style Change 2

--- src/test/java/seedu/address/logic/commands/DeleteCommandsTest.java
+++ src/test/java/seedu/address/logic/commands/DeleteCommandsTest.java
@@ -5,6 +5,8 @@ import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.index.Index;
@@ -13,14 +15,14 @@ import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.person.Person;
 
-import java.util.ArrayList;
+
 
 public class DeleteCommandsTest {
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
 
     @Test
-    public void execute_validIndexsUnfilteredList_success(){
+    public void execute_validIndexsUnfilteredList_success() {
 
         Person personToDelete1 = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
         Person personToDelete2 = model.getFilteredPersonList().get(INDEX_SECOND_PERSON.getZeroBased());
