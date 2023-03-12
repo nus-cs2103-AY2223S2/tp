fc8b9ee8d760475f65a29d3512aca04855c4c595
antonlee59 <92142956+antonlee59@users.noreply.github.com>
2023-03-13 01:28:40 +0800

    Refactor find command and user guide

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -8,6 +8,6 @@ public class Messages {
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_OPENING_DISPLAYED_INDEX = "The opening index provided is invalid";
-    public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d openings listed!";
+    public static final String MESSAGE_OPENING_LISTED_OVERVIEW = "%1$d openings listed!";
 
 }
