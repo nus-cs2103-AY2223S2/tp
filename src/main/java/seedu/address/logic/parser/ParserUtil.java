580d6804135d388f10064c9bfcca46d1604507b3
igezt <pierce.nws@gmail.com>
2023-03-13 06:44:02 +0800

    Edit for checkstyle errors

--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -65,6 +65,7 @@ public class ParserUtil {
      * @throws ParseException if the specified index is invalid (not non-zero unsigned integer).
      */
     public static Optional<Index> parseIndex(String oneBasedIndex) throws ParseException {
+
         String trimmedIndex = oneBasedIndex.trim();
         if (!StringUtil.isNonZeroUnsignedInteger(trimmedIndex)) {
             throw new ParseException(MESSAGE_INVALID_INDEX);
