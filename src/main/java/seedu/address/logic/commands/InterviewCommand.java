7cb5ba2b6a87bf43773a4ba1cedaac0edbe80cef
DESKTOP-4FAGDBQ\Lee Jia Xin <jxleejiaxin@gmail.com>
2023-03-13 14:14:39 +0800

    Add basic interview command that shows all shortlisted applicants

---
+++ src/main/java/seedu/address/logic/commands/InterviewCommand.java
@@ -0,0 +1,33 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.function.Predicate;
+
+import seedu.address.model.Model;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.Status;
+
+
+public class InterviewCommand extends Command {
+    public static final String COMMAND_WORD = "interview";
+
+    public static final Predicate<Person> shortlistedPredicate = person -> (person.getStatus() == Status.SHORTLISTED);
+
+    public static final String MESSAGE_SUCCESS_FORMAT = "Listed all shortlisted applicants!";
+
+    /**
+     * Returns a filtered list of applicants with SHORTLISTED status and sorts by earliest interview date
+     */
+    public String getSuccessMessage(Model model) {
+        model.updateFilteredPersonList(shortlistedPredicate);
+        return MESSAGE_SUCCESS_FORMAT;
+    }
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        return new CommandResult(getSuccessMessage(model));
+    }
+
+}
