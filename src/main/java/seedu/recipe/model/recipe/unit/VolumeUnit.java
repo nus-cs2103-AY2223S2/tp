5ca9155c214f078d3875a8f50f8d6c4092269fb5
ZHTang29 <e0725226@u.nus.edu>
2023-03-13 14:23:16 +0800

    Edit unit package, Add new classes which are Recipe properties

--- src/main/java/seedu/recipe/model/recipe/unit/VolumeUnit.java
+++
@@ -1,8 +0,0 @@
-package seedu.recipe.model.recipe.unit;
-
-public class VolumeUnit extends Unit{
-
-    public VolumeUnit(String unit) {
-        super(unit);
-    }
-}
--- src/main/java/seedu/recipe/model/recipe/unit/WeightUnit.java
+++
@@ -1,8 +0,0 @@
-package seedu.recipe.model.recipe.unit;
-
-public class WeightUnit extends Unit{
-
-    public WeightUnit(String unit) {
-        super(unit);
-    }
-}
