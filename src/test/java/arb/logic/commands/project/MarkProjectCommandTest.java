bf621a289a554f0068ca19caa1ff40d6cefd3c7b
John Chia Heng Kit <programmingwithjohn@gmail.com>
2023-03-13 21:24:13 +0800

    Fix checkstyle issues of MarkProjectCommandTest

--- src/test/java/arb/logic/commands/project/MarkProjectCommandTest.java
+++ src/test/java/arb/logic/commands/project/MarkProjectCommandTest.java
@@ -1,22 +1,27 @@
 package arb.logic.commands.project;
 
+import static arb.logic.commands.CommandTestUtil.assertCommandFailure;
+import static arb.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static arb.testutil.TypicalAddressBook.getTypicalAddressBook;
+import static arb.testutil.TypicalIndexes.INDEX_FIRST_CLIENT;
+import static arb.testutil.TypicalIndexes.INDEX_SECOND_CLIENT;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
 import arb.commons.core.Messages;
 import arb.commons.core.index.Index;
-import arb.logic.commands.client.DeleteClientCommand;
 import arb.model.ListType;
 import arb.model.Model;
 import arb.model.ModelManager;
 import arb.model.UserPrefs;
-import arb.model.client.Client;
 import arb.model.project.Project;
-import org.junit.jupiter.api.Test;
 
-import static arb.logic.commands.CommandTestUtil.*;
-import static arb.testutil.TypicalAddressBook.getTypicalAddressBook;
-import static arb.testutil.TypicalIndexes.INDEX_FIRST_CLIENT;
-import static arb.testutil.TypicalIndexes.INDEX_SECOND_CLIENT;
-import static org.junit.jupiter.api.Assertions.*;
-
+/**
+ * Contains integration tests (interaction with the Model) and unit tests for
+ * {@code MarkProjectCommand}.
+ */
 class MarkProjectCommandTest {
 
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
@@ -41,7 +46,6 @@ class MarkProjectCommandTest {
                 Messages.MESSAGE_INVALID_PROJECT_DISPLAYED_INDEX);
     }
 
-
     @Test
     public void equals() {
         MarkProjectCommand markFirstCommand = new MarkProjectCommand(INDEX_FIRST_CLIENT);
@@ -64,3 +68,4 @@ class MarkProjectCommandTest {
         assertFalse(markFirstCommand.equals(markSecondCommand));
     }
 }
+
