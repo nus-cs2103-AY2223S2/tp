412e0d661d67028fd0d017a6357518fd8a1c9a2a
alson001 <alsonang01@gmail.com>
2023-03-13 14:29:33 +0800

    Rest of Person refactor to Recipe

--- src/main/java/seedu/address/ui/RecipeCard.java
+++ src/main/java/seedu/address/ui/RecipeCard.java
@@ -14,7 +14,7 @@ import seedu.address.model.recipe.Recipe;
  */
 public class RecipeCard extends UiPart<Region> {
 
-    private static final String FXML = "PersonListCard.fxml";
+    private static final String FXML = "RecipeListCard.fxml";
 
     /**
      * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
@@ -42,7 +42,7 @@ public class RecipeCard extends UiPart<Region> {
     private FlowPane tags;
 
     /**
-     * Creates a {@code PersonCode} with the given {@code Recipe} and index to display.
+     * Creates a {@code RecipeCode} with the given {@code Recipe} and index to display.
      */
     public RecipeCard(Recipe recipe, int displayedIndex) {
         super(FXML);
