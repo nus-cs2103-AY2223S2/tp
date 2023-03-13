90c5f350ce845c99df3f0760ff3eb1df33fc358c
Aarnav Srinivasan <aarnav.srinivasan@gmail.com>
2023-03-13 15:20:39 +0800

    Fix parseCommand_add test error

--- src/main/java/seedu/calidr/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/calidr/logic/parser/AddCommandParser.java
@@ -5,6 +5,7 @@ import static seedu.calidr.logic.parser.CliSyntax.PREFIX_ADDRESS;
 import static seedu.calidr.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.calidr.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.calidr.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.calidr.logic.parser.CliSyntax.PREFIX_REMARK;
 import static seedu.calidr.logic.parser.CliSyntax.PREFIX_TAG;
 
 import java.util.Set;
@@ -32,9 +33,11 @@ public class AddCommandParser implements Parser<AddCommand> {
      */
     public AddCommand parse(String args) throws ParseException {
         ArgumentMultimap argMultimap =
-                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_TAG);
+                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL,
+                        PREFIX_ADDRESS, PREFIX_REMARK, PREFIX_TAG);
 
-        if (!arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_ADDRESS, PREFIX_PHONE, PREFIX_EMAIL)
+        if (!arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_ADDRESS,
+                PREFIX_REMARK, PREFIX_PHONE, PREFIX_EMAIL)
                 || !argMultimap.getPreamble().isEmpty()) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
         }
@@ -43,7 +46,7 @@ public class AddCommandParser implements Parser<AddCommand> {
         Phone phone = ParserUtil.parsePhone(argMultimap.getValue(PREFIX_PHONE).get());
         Email email = ParserUtil.parseEmail(argMultimap.getValue(PREFIX_EMAIL).get());
         Address address = ParserUtil.parseAddress(argMultimap.getValue(PREFIX_ADDRESS).get());
-        Remark remark = new Remark("");
+        Remark remark = ParserUtil.parseRemark(argMultimap.getValue(PREFIX_REMARK).get());
         Set<Tag> tagList = ParserUtil.parseTags(argMultimap.getAllValues(PREFIX_TAG));
 
         Person person = new Person(name, phone, email, address, remark, tagList);
