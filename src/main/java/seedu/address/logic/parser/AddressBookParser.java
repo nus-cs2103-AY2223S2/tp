ecd5c988cc299a8b356c727b1755a22c7855e275
FireRadical22 <97332558+FireRadical22@users.noreply.github.com>
2023-03-13 14:45:29 +0800

    Address checkstyle issues (No.7)
    Address checkstyle issues identified by GitHub CI

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -16,6 +16,7 @@ import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.commands.RemoveMeetingCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
