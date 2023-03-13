19bf6b319edab26bb01e9974389ea562817985f8
Haiqel <34855234+Acerizm@users.noreply.github.com>
2023-03-13 22:44:28 +0800

    update filterCommandParser to correctly get the prefix that the student enters

--- src/main/java/seedu/address/logic/parser/FilterCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FilterCommandParser.java
@@ -1,7 +1,6 @@
 package seedu.address.logic.parser;
 
 import seedu.address.logic.commands.FilterCommand;
-import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.ContactContainsAddressPredicate;
@@ -9,7 +8,6 @@ import seedu.address.model.person.ContactContainsEmailPredicate;
 import seedu.address.model.person.ContactContainsPhoneNumberPredicate;
 import seedu.address.model.person.ContactContainsTagPredicate;
 import seedu.address.model.person.Email;
-import seedu.address.model.person.MatchNamePredicate;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
 import seedu.address.model.person.Phone;
@@ -27,6 +25,11 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
+/**
+ * Parses input arguments and creates a new FilterCommand object
+ *
+ * @author Haiqel Bin Hanaffi
+ */
 public class FilterCommandParser implements Parser<FilterCommand> {
     /**
      * Parses the given {@code String} of arguments in the context of the FilterCommand
@@ -41,27 +44,27 @@ public class FilterCommandParser implements Parser<FilterCommand> {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, FilterCommand.MESSAGE_USAGE));
         }
 
-        if (isAnyPrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_ADDRESS, PREFIX_PHONE, PREFIX_EMAIL)) {
+        if (!isAnyPrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_ADDRESS, PREFIX_PHONE, PREFIX_EMAIL)) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, FilterCommand.MESSAGE_USAGE));
         }
 
         Prefix prefix = getPrefix(argMultimap, PREFIX_NAME, PREFIX_ADDRESS, PREFIX_PHONE, PREFIX_EMAIL,PREFIX_TAG);
 
-        if (prefix.equals("n/")) {
+        if (prefix.getPrefix().equals("n/")) {
             Name name = ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get());
             String fullName = name.toString();
             String trimmedFullName = fullName.trim();
             String[] nameKeywords = trimmedFullName.split("\\s+");
             return new FilterCommand(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
-        } else if (prefix.equals("p/")) {
+        } else if (prefix.getPrefix().equals("p/")) {
             Phone phone = ParserUtil.parsePhone(argMultimap.getValue(PREFIX_PHONE).get());
             String phoneNumber = phone.toString();
             return new FilterCommand(new ContactContainsPhoneNumberPredicate(phoneNumber));
-        } else if (prefix.equals("e/")) {
+        } else if (prefix.getPrefix().equals("e/")) {
             Email email = ParserUtil.parseEmail(argMultimap.getValue(PREFIX_EMAIL).get());
             String emailAddr = email.toString();
             return new FilterCommand(new ContactContainsEmailPredicate(emailAddr));
-        } else if (prefix.equals("d/")) {
+        } else if (prefix.getPrefix().equals("d/")) {
             Address address = ParserUtil.parseAddress(argMultimap.getValue(PREFIX_ADDRESS).get());
             String fullAddress = address.toString();
             return new FilterCommand(new ContactContainsAddressPredicate(fullAddress));
@@ -79,6 +82,10 @@ public class FilterCommandParser implements Parser<FilterCommand> {
         return Stream.of(prefixes).anyMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
     }
 
+    /**
+     * Returns the type of prefix
+     * {@code ArgumentMultimap}.
+     */
     private static Prefix getPrefix(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
         Optional<Prefix> optionalPrefix = Stream.of(prefixes)
                 .filter(prefix -> argumentMultimap.getValue(prefix).isPresent())
