79acdd8b87595f1e68b4e036799e75d003116f8d
nerdyboy98 <chewchunkiat@gmail.com>
2023-03-13 22:37:28 +0800

    Add HelpCommandParser

---
+++ src/main/java/seedu/address/logic/parser/HelpCommandParser.java
@@ -0,0 +1,27 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.HelpLabCommand;
+import seedu.address.logic.commands.HelpTutorialCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.logic.commands.HelpCommand;
+import seedu.address.logic.commands.HelpConsultationCommand;
+
+public class HelpCommandParser {
+    /**
+     * Parses the given {@code String} of arguments in the context of the HelpCommand
+     * and returns an HelpCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+
+    public HelpCommand parse(String args) {
+        if (args.equals(" tutorial")) {
+            return new HelpTutorialCommand();
+        } else if (args.equals(" lab")) {
+            return new HelpLabCommand();
+        } else if (args.equals(" consultation")) {
+            return new HelpConsultationCommand();
+        }else {
+            return new HelpCommand();
+        }
+    }
+}
