6d77552c631ed3ace831e63f3ee4526967ef19cb
linustws <linustws00@gmail.com>
2023-03-14 02:59:13 +0800

    More clearing up of AddressBook

--- src/test/java/codoc/logic/commands/EditCommandTest.java
+++ src/test/java/codoc/logic/commands/EditCommandTest.java
@@ -12,14 +12,14 @@
 //import static codoc.logic.commands.CommandTestUtil.showPersonAtIndex;
 //import static codoc.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 //import static codoc.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
-//import static codoc.testutil.TypicalPersons.getTypicalAddressBook;
+//import static codoc.testutil.TypicalPersons.getTypicalCodoc;
 //
 //import org.junit.jupiter.api.Test;
 //
 //import codoc.commons.core.Messages;
 //import codoc.commons.core.index.Index;
 //import codoc.logic.commands.EditCommand.EditPersonDescriptor;
-//import codoc.model.AddressBook;
+//import codoc.model.Codoc;
 //import codoc.model.Model;
 //import codoc.model.ModelManager;
 //import codoc.model.UserPrefs;
@@ -32,7 +32,7 @@
 // */
 //public class EditCommandTest {
 //
-//    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+//    private Model model = new ModelManager(getTypicalCodoc(), new UserPrefs());
 //
 //    @Test
 //    public void execute_allFieldsSpecifiedUnfilteredList_success() {
@@ -42,7 +42,7 @@
 //
 //        String expectedMessage = String.format(EditCommand.MESSAGE_EDIT_PERSON_SUCCESS, editedPerson);
 //
-//        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+//        Model expectedModel = new ModelManager(new Codoc(model.getCodoc()), new UserPrefs());
 //        expectedModel.setPerson(model.getFilteredPersonList().get(0), editedPerson);
 //
 //        assertCommandSuccess(editCommand, model, expectedMessage, expectedModel);
@@ -63,7 +63,7 @@
 //
 //        String expectedMessage = String.format(EditCommand.MESSAGE_EDIT_PERSON_SUCCESS, editedPerson);
 //
-//        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+//        Model expectedModel = new ModelManager(new Codoc(model.getCodoc()), new UserPrefs());
 //        expectedModel.setPerson(lastPerson, editedPerson);
 //
 //        assertCommandSuccess(editCommand, model, expectedMessage, expectedModel);
@@ -76,7 +76,7 @@
 //
 //        String expectedMessage = String.format(EditCommand.MESSAGE_EDIT_PERSON_SUCCESS, editedPerson);
 //
-//        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+//        Model expectedModel = new ModelManager(new Codoc(model.getCodoc()), new UserPrefs());
 //
 //        assertCommandSuccess(editCommand, model, expectedMessage, expectedModel);
 //    }
@@ -92,7 +92,7 @@
 //
 //        String expectedMessage = String.format(EditCommand.MESSAGE_EDIT_PERSON_SUCCESS, editedPerson);
 //
-//        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+//        Model expectedModel = new ModelManager(new Codoc(model.getCodoc()), new UserPrefs());
 //        expectedModel.setPerson(model.getFilteredPersonList().get(0), editedPerson);
 //
 //        assertCommandSuccess(editCommand, model, expectedMessage, expectedModel);
@@ -112,7 +112,7 @@
 //        showPersonAtIndex(model, INDEX_FIRST_PERSON);
 //
 //        // edit person in filtered list into a duplicate in address book
-//        Person personInList = model.getAddressBook().getPersonList().get(INDEX_SECOND_PERSON.getZeroBased());
+//        Person personInList = model.getCodoc().getPersonList().get(INDEX_SECOND_PERSON.getZeroBased());
 //        EditCommand editCommand = new EditCommand(INDEX_FIRST_PERSON,
 //                new EditPersonDescriptorBuilder(personInList).build());
 //
@@ -137,7 +137,7 @@
 //        showPersonAtIndex(model, INDEX_FIRST_PERSON);
 //        Index outOfBoundIndex = INDEX_SECOND_PERSON;
 //        // ensures that outOfBoundIndex is still in bounds of address book list
-//        assertTrue(outOfBoundIndex.getZeroBased() < model.getAddressBook().getPersonList().size());
+//        assertTrue(outOfBoundIndex.getZeroBased() < model.getCodoc().getPersonList().size());
 //
 //        EditCommand editCommand = new EditCommand(outOfBoundIndex,
 //                new EditPersonDescriptorBuilder().withName(VALID_NAME_BOB).build());
