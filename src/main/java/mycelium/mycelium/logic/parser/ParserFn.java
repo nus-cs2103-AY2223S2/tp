22de4ca0e6259731dac7e07958a9930ac35dda96
junyi <47441677+immanuelhume@users.noreply.github.com>
2023-03-13 21:40:30 +0800

    Add explanation for ParserFn interface
    To differentiate it from the Parser interface
    which already existed.

--- src/main/java/mycelium/mycelium/logic/parser/ParserFn.java
+++ src/main/java/mycelium/mycelium/logic/parser/ParserFn.java
@@ -3,8 +3,11 @@ package mycelium.mycelium.logic.parser;
 import mycelium.mycelium.logic.parser.exceptions.ParseException;
 
 /**
- * This is essentially a java.util.function.Function which might throw a {@link ParseException}. The type is made
- * explicit here so that it is more convenient to pass methods around as lambdas.
+ * This is essentially a java.util.function.Function which might throw a {@link
+ * ParseException}. The type is made explicit here so that it is more convenient
+ * to pass methods around as * lambdas. It defers from {@link Parser} in that it
+ * returns any type, not just a command. Thus, its main use is in parsing
+ * arguments within each command string.
  */
 @FunctionalInterface
 public interface ParserFn<T, U> {
