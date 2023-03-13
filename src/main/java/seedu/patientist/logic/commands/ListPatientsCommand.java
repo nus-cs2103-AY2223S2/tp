8f5bcf94acb92edcc89c5f4a8cf75b31027ab295
pzhengze <zhengze@u.nus.edu>
2023-03-13 23:04:16 +0800

    Refactor IsPatientPredicate

--- src/main/java/seedu/patientist/logic/commands/ListPatientsCommand.java
+++ src/main/java/seedu/patientist/logic/commands/ListPatientsCommand.java
@@ -4,7 +4,7 @@ import static java.util.Objects.requireNonNull;
 
 import seedu.patientist.commons.core.Messages;
 import seedu.patientist.model.Model;
-import seedu.patientist.model.person.IsPatientPredicate;
+import seedu.patientist.model.person.patient.IsPatientPredicate;
 
 /**
  * Lists all patients in the patientist book to the user.
@@ -12,9 +12,7 @@ import seedu.patientist.model.person.IsPatientPredicate;
 public class ListPatientsCommand extends Command {
     public static final String COMMAND_WORD = "lspat";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": List all patients\n"
-           + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
-           + "Example: " + COMMAND_WORD + " alice bob charlie";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Lists all patients";
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
---
+++ src/main/java/seedu/patientist/model/person/patient/IsPatientPredicate.java
@@ -0,0 +1,22 @@
+package seedu.patientist.model.person.patient;
+
+import java.util.function.Predicate;
+
+import seedu.patientist.model.person.Person;
+import seedu.patientist.model.tag.Tag;
+
+/**
+ * Test if Person object is a patients
+ */
+public class IsPatientPredicate implements Predicate<Person> {
+    @Override
+    public boolean test(Person person) {
+        return person.getTags().contains(new Tag("Patient"));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+               || (other instanceof IsPatientPredicate); // instanceof handles nulls
+    }
+}
