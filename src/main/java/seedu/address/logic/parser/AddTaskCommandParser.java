2c0d677c3122d47ef9eb3fb6973a4faf5c31a8a7
toh-xinyi <tohxinyi10@gmail.com>
2023-03-13 22:04:47 +0800

    Refactor AddTaskCommandParser to throw exception if the prefix needed is not present

--- src/main/java/seedu/address/logic/parser/AddTaskCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddTaskCommandParser.java
@@ -2,9 +2,13 @@ package seedu.address.logic.parser;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TASK_TITLE;
+import static seedu.address.logic.parser.CliSyntax.*;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_SCORE_DATE;
+
+import java.util.stream.Stream;
 
 import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.AddScoreCommand;
 import seedu.address.logic.commands.AddTaskCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.Name;
@@ -33,9 +37,21 @@ public class AddTaskCommandParser implements Parser<AddTaskCommand> {
                     AddTaskCommand.MESSAGE_USAGE), pe);
         }
 
+        if (!arePrefixesPresent(argMultimap, PREFIX_TASK_TITLE)) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddTaskCommand.MESSAGE_USAGE));
+        }
+
         Name taskName = ParserUtil.parseName(argMultimap.getValue(PREFIX_TASK_TITLE).get());
         Task task = new Task(taskName);
 
         return new AddTaskCommand(personIndex, task);
+    }
+
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
     }
 }
