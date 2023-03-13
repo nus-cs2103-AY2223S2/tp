52f9b007cf5dcf94e5ab3cf7e126fc11d1c82b7e
kwangjoo <pkj5257@gmail.com>
2023-03-13 18:08:10 +0800

    NameContainsKeywordsPredicate: Refactor person to employee

--- src/main/java/seedu/sudohr/model/employee/NameContainsKeywordsPredicate.java
+++ src/main/java/seedu/sudohr/model/employee/NameContainsKeywordsPredicate.java
@@ -16,9 +16,9 @@ public class NameContainsKeywordsPredicate implements Predicate<Employee> {
     }
 
     @Override
-    public boolean test(Employee person) {
+    public boolean test(Employee employee) {
         return keywords.stream()
-                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getName().fullName, keyword));
+                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(employee.getName().fullName, keyword));
     }
 
     @Override
