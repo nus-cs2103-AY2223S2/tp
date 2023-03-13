fe37c559fc01df663a6e9b32117632107b7ef212
Owen Yap <owen.y.yuhan@gmail.com>
2023-03-13 20:14:03 +0800

    Update src/main/java/seedu/address/storage/JsonAdaptedDoctor.java
    Co-authored-by: Praveen Krishna <84656853+praveenkrishna0512@users.noreply.github.com>

--- src/main/java/seedu/address/storage/JsonAdaptedDoctor.java
+++ src/main/java/seedu/address/storage/JsonAdaptedDoctor.java
@@ -22,7 +22,7 @@ class JsonAdaptedDoctor extends JsonAdaptedPerson {
     private final String yearsOfExperience;
 
     /**
-     * Constructs a {@code JsonAdaptedPerson} with the given doctor details.
+     * Constructs a {@code JsonAdaptedDoctor} with the given doctor details.
      */
     @JsonCreator
     public JsonAdaptedDoctor(@JsonProperty("name") String name, @JsonProperty("phone") String phone,
