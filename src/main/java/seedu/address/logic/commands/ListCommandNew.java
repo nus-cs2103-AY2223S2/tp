9f34aec20c0c538d0b015aa3f8b6a3f6e737b2a1
Varstak <e0725939@u.nus.edu>
2023-03-13 17:38:36 +0800

    Edited Checkstyle

--- src/main/java/seedu/address/logic/commands/ListCommandNew.java
+++ src/main/java/seedu/address/logic/commands/ListCommandNew.java
@@ -2,13 +2,13 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.model.ModelNew.PREDICATE_SHOW_ALL_OPENINGS;
+
 import seedu.address.model.ModelNew;
 
 /**
- * Lists all openings in the address book to the user.
+ * Lists all openings Ultron to the user.
  */
-
-public class NewListCommand extends CommandNew {
+public class ListCommandNew extends CommandNew {
 
     public static final String COMMAND_WORD = "list";
 
@@ -19,7 +19,6 @@ public class NewListCommand extends CommandNew {
     public CommandResultNew execute(ModelNew model) {
         requireNonNull(model);
         model.updateFilteredOpeningList(PREDICATE_SHOW_ALL_OPENINGS);
-         return new CommandResultNew(MESSAGE_SUCCESS);
-     }
-
+        return new CommandResultNew(MESSAGE_SUCCESS);
+    }
 }
