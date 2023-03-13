416375f3670616a41404983bfdf09f4d3a32b4db
Aarnav Srinivasan <aarnav.srinivasan@gmail.com>
2023-03-13 18:51:41 +0800

    Add wrapper class for the Location associated with a Task

---
+++ src/main/java/seedu/calidr/model/task/params/Location.java
@@ -0,0 +1,56 @@
+package seedu.calidr.model.task.params;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.calidr.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents the location associated with a Task.
+ */
+public class Location {
+
+    public static final String MESSAGE_CONSTRAINTS = "Locations can take any values, and it should not be blank";
+
+    /*
+     * The first character of the location must not be a whitespace,
+     * otherwise " " (a blank string) becomes a valid input.
+     */
+    public static final String VALIDATION_REGEX = "[^\\s].*";
+
+    public final String value;
+
+    /**
+     * Constructs an {@code Location}.
+     *
+     * @param address A valid location.
+     */
+    public Location(String address) {
+        requireNonNull(address);
+        checkArgument(isValidLocation(address), MESSAGE_CONSTRAINTS);
+        value = address;
+    }
+
+    /**
+     * Returns true if a given string is a valid title.
+     */
+    public static boolean isValidLocation(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Location // instanceof handles nulls
+                && value.equals(((Location) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+    
+}
