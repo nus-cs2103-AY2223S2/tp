53182d13599c8ce85b5d43d3827f91314069bb20
jednghk <jednghk@gmail.com>
2023-03-13 15:01:48 +0800

    Extract AddressTest and NameTest into model.attribute

--- src/test/java/seedu/loyaltylift/model/attribute/NameTest.java
+++ src/test/java/seedu/loyaltylift/model/attribute/NameTest.java
@@ -1,4 +1,4 @@
-package seedu.loyaltylift.model.customer;
+package seedu.loyaltylift.model.attribute;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
