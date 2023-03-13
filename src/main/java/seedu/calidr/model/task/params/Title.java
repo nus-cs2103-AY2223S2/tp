6268e2e0ebe227e8a2cf4ed745f8a77bf8e6601b
Aarnav Srinivasan <aarnav.srinivasan@gmail.com>
2023-03-13 18:44:21 +0800

    Add wrapper class for the Title of a Task

---
+++ src/main/java/seedu/calidr/model/task/params/Title.java
@@ -0,0 +1,58 @@
+package seedu.calidr.model.task.params;
+
+import seedu.calidr.model.person.Address;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.calidr.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents the title of a Task.
+ */
+public class Title {
+
+    public static final String MESSAGE_CONSTRAINTS = "Titles can take any values, and it should not be blank";
+
+    /*
+     * The first character of the title must not be a whitespace,
+     * otherwise " " (a blank string) becomes a valid input.
+     */
+    public static final String VALIDATION_REGEX = "[^\\s].*";
+
+    public final String value;
+
+    /**
+     * Constructs an {@code Title}.
+     *
+     * @param address A valid title.
+     */
+    public Title(String address) {
+        requireNonNull(address);
+        checkArgument(isValidTitle(address), MESSAGE_CONSTRAINTS);
+        value = address;
+    }
+
+    /**
+     * Returns true if a given string is a valid title.
+     */
+    public static boolean isValidTitle(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Title // instanceof handles nulls
+                && value.equals(((Title) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+}
