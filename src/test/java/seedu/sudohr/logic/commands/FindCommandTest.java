f53324f1ffa2c597ce1b936dbf12d4705178ec1a
kwangjoo <pkj5257@gmail.com>
2023-03-13 23:40:00 +0800

    commons: Refactor person to employee

--- src/test/java/seedu/sudohr/logic/commands/FindCommandTest.java
+++ src/test/java/seedu/sudohr/logic/commands/FindCommandTest.java
@@ -3,7 +3,7 @@ package seedu.sudohr.logic.commands;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.sudohr.commons.core.Messages.MESSAGE_PERSONS_LISTED_OVERVIEW;
+import static seedu.sudohr.commons.core.Messages.MESSAGE_EMPLOYEES_LISTED_OVERVIEW;
 import static seedu.sudohr.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.sudohr.testutil.TypicalPersons.CARL;
 import static seedu.sudohr.testutil.TypicalPersons.ELLE;
@@ -56,7 +56,7 @@ public class FindCommandTest {
 
     @Test
     public void execute_zeroKeywords_noPersonFound() {
-        String expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 0);
+        String expectedMessage = String.format(MESSAGE_EMPLOYEES_LISTED_OVERVIEW, 0);
         NameContainsKeywordsPredicate predicate = preparePredicate(" ");
         FindCommand command = new FindCommand(predicate);
         expectedModel.updateFilteredEmployeeList(predicate);
@@ -66,7 +66,7 @@ public class FindCommandTest {
 
     @Test
     public void execute_multipleKeywords_multiplePersonsFound() {
-        String expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 3);
+        String expectedMessage = String.format(MESSAGE_EMPLOYEES_LISTED_OVERVIEW, 3);
         NameContainsKeywordsPredicate predicate = preparePredicate("Kurz Elle Kunz");
         FindCommand command = new FindCommand(predicate);
         expectedModel.updateFilteredEmployeeList(predicate);
