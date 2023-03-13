b01fd734694d2ce33a2a12e0f435f935c86917ca
Vigneshwar Hariharan <vigneshwar.hariharan1@gmail.com>
2023-03-13 17:26:13 +0800

    Add test utils for Doctor and DeleteDoctorCommand

---
+++ src/test/java/seedu/address/testutil/TypicalDoctors.java
@@ -0,0 +1,52 @@
+package seedu.address.testutil;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import seedu.address.model.AddressBook;
+import seedu.address.model.person.Doctor;
+
+/**
+ * A utility class containing a list of {@code Doctor} objects to be used in tests.
+ */
+public class TypicalDoctors {
+
+    public static final Doctor ALICE = new DoctorBuilder().withName("Alice Pauline")
+            .withEmail("alice@example.com")
+            .withPhone("94351253")
+            .withTags("friends")
+            .withSpecialty("General Medicine")
+            .withYoe("5")
+            .build();
+    public static final Doctor BENSON = new DoctorBuilder().withName("Benson Meier")
+            .withEmail("johnd@example.com").withPhone("98765432").withSpecialty("General Medicine")
+            .withYoe("5").withTags("owesMoney", "friends").build();
+
+    public static final Doctor CARL = new DoctorBuilder().withName("Carl Kurz").withPhone("95352563")
+            .withEmail("carlk@example.com").withSpecialty("General Medicine").withYoe("5").build();
+
+    public static final Doctor DANIEL = new DoctorBuilder().withName("Daniel Meier").withPhone("87652533")
+            .withEmail("danielm@example.com").withSpecialty("Neurosurgery").withYoe("10")
+            .withTags("HoD").build();
+
+
+    public static final String KEYWORD_MATCHING_MEIER = "Meier"; // A keyword that matches MEIER
+
+    private TypicalDoctors() {} // prevents instantiation
+
+    /**
+     * Returns an {@code AddressBook} with all the typical doctors.
+     */
+    public static AddressBook getTypicalAddressBook() {
+        AddressBook ab = new AddressBook();
+        for (Doctor doctor : getTypicalDoctors()) {
+            ab.addDoctor(doctor);
+        }
+        return ab;
+    }
+
+    public static List<Doctor> getTypicalDoctors() {
+        return new ArrayList<>(Arrays.asList(ALICE, BENSON, CARL, DANIEL));
+    }
+}
