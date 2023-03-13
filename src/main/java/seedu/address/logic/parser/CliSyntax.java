1344197e562cf1d83b7c4aa883013ff38c1a265c
evanpy <evanleewj@gmail.com>
2023-03-14 00:35:52 +0800

    Add prefix for gender, industry, occupation, and job title

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -7,12 +7,15 @@ public class CliSyntax {
 
     /* Prefix definitions */
     public static final Prefix PREFIX_NAME = new Prefix("n/");
+    public static final Prefix PREFIX_GENDER = new Prefix("g/");
     public static final Prefix PREFIX_PHONE = new Prefix("p/");
     public static final Prefix PREFIX_EMAIL = new Prefix("e/");
     public static final Prefix PREFIX_COMPANY = new Prefix("c/");
+    public static final Prefix PREFIX_INDUSTRY = new Prefix("i/");
+    public static final Prefix PREFIX_OCCUPATION = new Prefix("o/");
+    public static final Prefix PREFIX_JOBTITLE = new Prefix("j/");
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
     public static final Prefix PREFIX_REMARK = new Prefix("r/");
-
 
 }
