25941b17e8030a2f573d48db6cbf01c9365da3c7
AngJunYang <handbombex3@gmail.com>
2023-03-14 13:20:46 +0800

    Add Entity
    This commit adds an abstract entity class to be inherited by mobs, characters and items.
    
    Extracting common behavior like Name and Tags will allow us to use polymorphism when implementing Characters, Mobs and Items.

---
+++ src/main/java/seedu/address/model/entity/Entity.java
@@ -0,0 +1,56 @@
+package seedu.address.model.entity;
+
+import seedu.address.model.person.Address;
+import seedu.address.model.person.Email;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Phone;
+import seedu.address.model.tag.Tag;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public abstract class Entity {
+    // Identity fields
+    private final Name name;
+    private final Set<Tag> tags = new HashSet<>();
+
+    public Entity(Name name) {
+        this.name = name;
+    }
+
+
+    public Name getName() {
+        return name;
+    }
+    /**
+     * Returns an immutable tag set, which throws {@code UnsupportedOperationException}
+     * if modification is attempted.
+     */
+    public Set<Tag> getTags() {
+        return Collections.unmodifiableSet(tags);
+    }
+
+    @Override
+    public int hashCode() {
+        // use this method for custom fields hashing instead of implementing your own
+        return Objects.hash(name, tags);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append(getName());
+
+        Set<Tag> tags = getTags();
+        if (!tags.isEmpty()) {
+            builder.append("; Tags: ");
+            tags.forEach(builder::append);
+        }
+        return builder.toString();
+    }
+
+
+
+}
