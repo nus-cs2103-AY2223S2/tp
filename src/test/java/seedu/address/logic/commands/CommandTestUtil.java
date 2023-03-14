33d6df8e1134808e69ce9c5aa3de728adc5e4ea6
ajjajjajjajj <angjingjie0512@gmail.com>
2023-03-14 13:37:39 +0800

    Add tests methods for StatusCommand.java

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -19,6 +19,8 @@ import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
 import seedu.address.model.person.Person;
+import seedu.address.model.person.status.LeadStatus;
+import seedu.address.model.person.status.LeadStatusName;
 import seedu.address.testutil.EditPersonDescriptorBuilder;
 
 /**
@@ -62,6 +64,9 @@ public class CommandTestUtil {
     public static final EditCommand.EditPersonDescriptor DESC_AMY;
     public static final EditCommand.EditPersonDescriptor DESC_BOB;
 
+    public static final LeadStatus VALID_LEAD_STATUS_UNCONTACTED;
+    public static final LeadStatus VALID_LEAD_STATUS_WORKING;
+
     static {
         DESC_AMY = new EditPersonDescriptorBuilder().withName(VALID_NAME_AMY)
                 .withPhone(VALID_PHONE_AMY).withEmail(VALID_EMAIL_AMY).withAddress(VALID_ADDRESS_AMY)
@@ -69,6 +74,9 @@ public class CommandTestUtil {
         DESC_BOB = new EditPersonDescriptorBuilder().withName(VALID_NAME_BOB)
                 .withPhone(VALID_PHONE_BOB).withEmail(VALID_EMAIL_BOB).withAddress(VALID_ADDRESS_BOB)
                 .withTags(VALID_TAG_HUSBAND, VALID_TAG_FRIEND).build();
+
+        VALID_LEAD_STATUS_UNCONTACTED = new LeadStatus(LeadStatusName.UNCONTACTED.getLabel());
+        VALID_LEAD_STATUS_WORKING = new LeadStatus(LeadStatusName.WORKING.getLabel());
     }
 
     /**
---
+++ src/test/java/seedu/address/logic/commands/StatusCommandTest.java
@@ -0,0 +1,63 @@
+package seedu.address.logic.commands;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.model.AddressBook;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.status.LeadStatus;
+import seedu.address.model.person.status.LeadStatusName;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_LEAD_STATUS_UNCONTACTED;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_LEAD_STATUS_WORKING;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+public class StatusCommandTest {
+
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+    @Test void execute_changeLeadStatus_success() {
+        Person toBeUpdated = getTypicalAddressBook().getPersonList().get(0);
+        assertEquals(toBeUpdated.getStatus(), VALID_LEAD_STATUS_UNCONTACTED); // default should be uncontacted
+
+        StatusCommand statusCommand = new StatusCommand(INDEX_FIRST_PERSON, VALID_LEAD_STATUS_WORKING);
+
+        String expectedMessage = String.format(StatusCommand.MESSAGE_STATUS_ASSIGN_PERSON_SUCCESS,
+                toBeUpdated.getName(), VALID_LEAD_STATUS_WORKING);
+
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+        expectedModel.setPerson(model.getFilteredPersonList().get(0), toBeUpdated);
+
+        assertCommandSuccess(statusCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_invalidPersonIndex_failure() {
+        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
+
+        // the lead status chosen should not matter as ofOfBound check occurs before lead status assignment;
+        StatusCommand statusCommand = new StatusCommand(outOfBoundIndex, VALID_LEAD_STATUS_WORKING);
+
+        assertCommandFailure(statusCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void execute_leadStatusIsSame_failure() {
+        Person firstPerson = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        LeadStatusName statusName = firstPerson.getStatus().getStatusName();
+        LeadStatus sameStatus = new LeadStatus(statusName.getLabel());
+
+        StatusCommand statusCommand = new StatusCommand(INDEX_FIRST_PERSON, sameStatus);
+
+        assertCommandFailure(statusCommand, model, StatusCommand.MESSAGE_STATUS_IS_SAME);
+    }
+
+}
