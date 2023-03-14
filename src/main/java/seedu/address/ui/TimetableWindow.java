21b260d7011451cfa5049c307c2067dcf855cb86
dohaduong <duonghado03@gmail.com>
2023-03-14 21:19:28 +0800

    Delete unused import Unused import in TimetableWindow.java is not deleted.
    Removing unused import helps to increase code quality.
    
    Let's delete unused imports

--- src/main/java/seedu/address/ui/TimetableWindow.java
+++ src/main/java/seedu/address/ui/TimetableWindow.java
@@ -13,10 +13,6 @@ import javafx.stage.Stage;
 import seedu.address.commons.core.GuiSettings;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.logic.Logic;
-import seedu.address.logic.commands.CommandResult;
-import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.ui.main.CommandBox;
 import seedu.address.ui.main.ResultDisplay;
 import seedu.address.ui.main.StatusBarFooter;
 
@@ -39,7 +35,7 @@ public class TimetableWindow extends UiPart<Stage> implements Initializable {
     private StackPane statusbarPlaceholder;
 
     @FXML
-    private TreeView monthDeliveryJobListTreeView;
+    private TreeView<String> monthDeliveryJobListTreeView;
 
     /**
      * Creates a {@code JobWindow} with the given {@code Stage} and {@code Logic}.
@@ -56,25 +52,25 @@ public class TimetableWindow extends UiPart<Stage> implements Initializable {
     }
 
     /**
-     * Shows the help window.
-     *
-     * @throws IllegalStateException <ul>
-     *                                       <li>
-     *                                           if this method is called on a thread other than the JavaFX Application Thread.
-     *                                       </li>
-     *                                       <li>
-     *                                           if this method is called during animation or layout processing.
-     *                                       </li>
-     *                                       <li>
-     *                                           if this method is called on the primary stage.
-     *                                       </li>
-     *                                       <li>
-     *                                           if {@code dialogStage} is already showing.
-     *                                       </li>
-     *                                   </ul>
+     * Shows the timetable window.
+     * @throws IllegalStateException
+     *     <ul>
+     *         <li>
+     *             if this method is called on a thread other than the JavaFX Application Thread.
+     *         </li>
+     *         <li>
+     *             if this method is called during animation or layout processing.
+     *         </li>
+     *         <li>
+     *             if this method is called on the primary stage.
+     *         </li>
+     *         <li>
+     *             if {@code dialogStage} is already showing.
+     *         </li>
+     *     </ul>
      */
     public void show() {
-        logger.fine("Showing help page about the application.");
+        logger.fine("Showing timetable.");
         getRoot().show();
         getRoot().centerOnScreen();
     }
@@ -104,17 +100,8 @@ public class TimetableWindow extends UiPart<Stage> implements Initializable {
      * Fills up all the placeholders of this window.
      */
     void fillInnerParts() {
-        //jobListPanel = new jobListPanel(logic.getFilteredjobList());
-        //jobListPanelPlaceholder.getChildren().add(jobListPanel.getRoot());
-
-     //   resultDisplay = new ResultDisplay();
-       // resultDisplayPlaceholder.getChildren().add(resultDisplay.getRoot());
-
         StatusBarFooter statusBarFooter = new StatusBarFooter(logic.getAddressBookFilePath());
         statusbarPlaceholder.getChildren().add(statusBarFooter.getRoot());
-
-    //    CommandBox commandBox = new CommandBox(this::executeCommand);
-      //  commandBoxPlaceholder.getChildren().add(commandBox.getRoot());
     }
 
     /**
@@ -136,7 +123,6 @@ public class TimetableWindow extends UiPart<Stage> implements Initializable {
 
         for (int i = 1; i < 13; i++) {
             TreeItem<String> month = new TreeItem<String>(String.format("Month %d", i));
-         //   month.getChildren().add(month);
             year.getChildren().add(month);
         }
 
