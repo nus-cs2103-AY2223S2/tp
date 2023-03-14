9492627b55f2f20fdd3058836560342a7a10dd96
hansstanley <nusstuhss@gmail.com>
2023-03-14 20:12:58 +0800

    Do checkstyle's bidding

--- src/test/java/seedu/address/logic/ui/tab/TabUtilTest.java
+++ src/test/java/seedu/address/logic/ui/tab/TabUtilTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+
 import seedu.address.commons.core.index.Index;
 
 import java.util.List;
