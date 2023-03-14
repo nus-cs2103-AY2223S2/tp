aa9e9cc895fb700ff8a58155011ce02ed73c5233
francisyzy <francisyzy@me.com>
2023-03-15 02:00:54 +0800

    Fix wrong command format displayed
    Fix #82

--- src/main/java/seedu/vms/logic/commands/patient/DeleteCommand.java
+++ src/main/java/seedu/vms/logic/commands/patient/DeleteCommand.java
@@ -19,11 +19,12 @@ import seedu.vms.model.patient.Patient;
 public class DeleteCommand extends Command {
 
     public static final String COMMAND_WORD = "delete";
+    public static final String COMMAND_GROUP = "patient";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD
+    public static final String MESSAGE_USAGE = COMMAND_GROUP + " " + COMMAND_WORD
             + ": Deletes the patient identified by the index number used in the displayed patient list.\n"
             + "Parameters: INDEX (must be a positive integer)\n"
-            + "Example: " + COMMAND_WORD + " 1";
+            + "Example: " + COMMAND_GROUP + " " + COMMAND_WORD + " 1";
 
     public static final String MESSAGE_DELETE_PATIENT_SUCCESS = "Deleted Patient: %1$s";
 
--- src/main/java/seedu/vms/logic/commands/patient/FindCommand.java
+++ src/main/java/seedu/vms/logic/commands/patient/FindCommand.java
@@ -15,11 +15,13 @@ import seedu.vms.model.patient.NameContainsKeywordsPredicate;
 public class FindCommand extends Command {
 
     public static final String COMMAND_WORD = "find";
+    public static final String COMMAND_GROUP = "patient";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all patients whose names contain any of "
+    public static final String MESSAGE_USAGE = COMMAND_GROUP + " " + COMMAND_WORD
+            + ": Finds all patients whose names contain any of "
             + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
             + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
-            + "Example: " + COMMAND_WORD + " alice bob charlie";
+            + "Example: " + COMMAND_GROUP + " " + COMMAND_WORD + " alice bob charlie";
 
     private final NameContainsKeywordsPredicate predicate;
 
