341b14d2611c2a007ad707c851fd3645677bc31d
Zong Xun <zxun2000@gmail.com>
2023-03-15 01:17:47 +0800

    Update parsers

--- src/main/java/seedu/address/logic/parser/AddVolunteerCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddVolunteerCommandParser.java
@@ -3,12 +3,14 @@ package seedu.address.logic.parser;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_AGE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_AVAILABILITY;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NRIC_VOLUNTEER;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
+import java.util.Arrays;
 import java.util.Set;
 
 import seedu.address.commons.util.PrefixUtil;
@@ -17,6 +19,7 @@ import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.Volunteer;
 import seedu.address.model.person.information.Address;
 import seedu.address.model.person.information.Age;
+import seedu.address.model.person.information.AvailableDate;
 import seedu.address.model.person.information.Email;
 import seedu.address.model.person.information.Name;
 import seedu.address.model.person.information.Nric;
@@ -34,17 +37,17 @@ public class AddVolunteerCommandParser implements Parser<AddVolunteerCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public AddVolunteerCommand parse(String args) throws ParseException {
-        ArgumentMultimap argMultimap =
-                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE,
-                        PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_NRIC_VOLUNTEER, PREFIX_AGE,
-                        PREFIX_TAG);
+        Prefix[] availablePrefixes = {PREFIX_NAME, PREFIX_PHONE,
+                    PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_NRIC_VOLUNTEER, PREFIX_AGE,
+                    PREFIX_AVAILABILITY, PREFIX_TAG};
+        Prefix[] compulsoryPrefixes = Arrays.copyOfRange(availablePrefixes, 0, 6) ;
 
-        if (!PrefixUtil.arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_ADDRESS,
-                PREFIX_PHONE, PREFIX_EMAIL, PREFIX_NRIC_VOLUNTEER, PREFIX_AGE)
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, availablePrefixes);
+
+        if (!PrefixUtil.arePrefixesPresent(argMultimap, compulsoryPrefixes)
                 || !argMultimap.getPreamble().isEmpty()) {
-            throw new ParseException(String
-                    .format(MESSAGE_INVALID_COMMAND_FORMAT,
-                            AddVolunteerCommand.MESSAGE_USAGE));
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddVolunteerCommand.MESSAGE_USAGE));
         }
 
         Name name = ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get());
@@ -53,9 +56,10 @@ public class AddVolunteerCommandParser implements Parser<AddVolunteerCommand> {
         Address address = ParserUtil.parseAddress(argMultimap.getValue(PREFIX_ADDRESS).get());
         Nric nric = ParserUtil.parseNric(argMultimap.getValue(PREFIX_NRIC_VOLUNTEER).get());
         Age age = ParserUtil.parseAge(argMultimap.getValue(PREFIX_AGE).get());
+        Set<AvailableDate> availableDates = ParserUtil.parseDateRanges(argMultimap.getAllValues(PREFIX_AVAILABILITY));
         Set<Tag> tagList = ParserUtil.parseTags(argMultimap.getAllValues(PREFIX_TAG));
 
-        Volunteer volunteer = new Volunteer(name, phone, email, address, nric, age, tagList);
+        Volunteer volunteer = new Volunteer(name, phone, email, address, nric, age, tagList, availableDates);
 
         return new AddVolunteerCommand(volunteer);
     }
