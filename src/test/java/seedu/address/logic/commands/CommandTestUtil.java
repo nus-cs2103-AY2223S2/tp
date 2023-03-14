178edb4c03fab57686e41b4e5edbac2003785750
Nicholas Tan <ntnic13@gmail.com>
2023-03-15 01:22:37 +0800

    fix codecov issue

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -79,7 +79,7 @@ public class CommandTestUtil {
      * Executes the given {@code command}, confirms that <br>
      * - a {@code CommandException} is thrown <br>
      * - the CommandException message matches {@code expectedMessage} <br>
-     * - the listing book, filtered listing list and selected listing in {@code actualModel} remain unchanged
+     * - the listing book, filtered listing book and selected listing in {@code actualModel} remain unchanged
      */
     public static void assertCommandFailure(Command command, Model actualModel, String expectedMessage) {
         // we are unable to defensively copy the model for comparison later, so we can
