4bc65c901ff8e7285793cf3de16c33d061d745b9
hansstanley <nusstuhss@gmail.com>
2023-03-14 19:32:30 +0800

    Add tests for TabCommandParser

---
+++ src/test/java/seedu/address/logic/parser/TabCommandParserTest.java
@@ -0,0 +1,24 @@
+package seedu.address.logic.parser;
+
+import org.junit.jupiter.api.Test;
+import seedu.address.logic.commands.TabCommand;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_ADDRESS_BOOK_TAB;
+
+class TabCommandParserTest {
+
+    private final TabCommandParser parser = new TabCommandParser();
+
+    @Test
+    void parse_validArgs_returnTabCommand() {
+        assertParseSuccess(parser, "1", new TabCommand(INDEX_ADDRESS_BOOK_TAB));
+    }
+
+    @Test
+    void parse_invalidArgs_throwsParseException() {
+        assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT, TabCommand.MESSAGE_USAGE));
+    }
+}
