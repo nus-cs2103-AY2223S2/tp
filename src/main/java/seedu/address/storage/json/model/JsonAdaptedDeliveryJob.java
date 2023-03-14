5eff5669588dba07fa209723a72c8b2f1e928fde
c0j0s <c0j0s@outlook.com>
2023-03-14 17:47:07 +0800

    Add sender to delivery job model

--- src/main/java/seedu/address/storage/json/model/JsonAdaptedDeliveryJob.java
+++ src/main/java/seedu/address/storage/json/model/JsonAdaptedDeliveryJob.java
@@ -12,6 +12,7 @@ public class JsonAdaptedDeliveryJob extends JsonAdapted<DeliveryJob> {
 
     private final String jobId;
     private final JsonAdaptedPerson recepient;
+    private final JsonAdaptedPerson sender;
     private final String slot;
     private final String earning;
     private final boolean isDelivered;
@@ -21,18 +22,21 @@ public class JsonAdaptedDeliveryJob extends JsonAdapted<DeliveryJob> {
      *
      * @param jobId
      * @param recepient
+     * @param sender
      * @param deliverySlot
      * @param earning
      */
     public JsonAdaptedDeliveryJob(
         @JsonProperty("jobid") String jobId,
         @JsonProperty("recepient") JsonAdaptedPerson recepient,
+        @JsonProperty("sender") JsonAdaptedPerson sender,
         @JsonProperty("slot") String deliverySlot,
         @JsonProperty("earning") String earning,
         @JsonProperty("isDelivered") boolean isDelivered
     ) {
         this.jobId = jobId;
         this.recepient = recepient;
+        this.sender = sender;
         this.slot = deliverySlot;
         this.earning = earning;
         this.isDelivered = isDelivered;
@@ -46,6 +50,7 @@ public class JsonAdaptedDeliveryJob extends JsonAdapted<DeliveryJob> {
     public JsonAdaptedDeliveryJob(DeliveryJob source) {
         this.jobId = source.getJobId();
         this.recepient = new JsonAdaptedPerson(source.getRecepient());
+        this.sender = new JsonAdaptedPerson(source.getSender());
         this.slot = source.getDeliverSlot();
         this.earning = source.getEarning().value;
         this.isDelivered = source.getDeliveredStatus();
@@ -54,7 +59,7 @@ public class JsonAdaptedDeliveryJob extends JsonAdapted<DeliveryJob> {
     @Override
     public DeliveryJob toModelType() throws IllegalValueException {
         // TODO: refine later
-        return new DeliveryJob(jobId, recepient.toModelType(), slot, earning, isDelivered);
+        return new DeliveryJob(jobId, recepient.toModelType(), sender.toModelType(), slot, earning, isDelivered);
     }
 
 }
--- src/test/java/seedu/address/testutil/TypicalDeliveryJobs.java
+++ src/test/java/seedu/address/testutil/TypicalDeliveryJobs.java
@@ -14,27 +14,33 @@ public class TypicalDeliveryJobs {
 
     public static final DeliveryJob JOBA = new DeliveryJob(
         TypicalPersons.ALICE,
+        TypicalPersons.BOB,
         "dummy slot",
         "1.0");
 
     public static final DeliveryJob JOBB = new DeliveryJob(
         TypicalPersons.BOB,
+        TypicalPersons.CARL,
         "dummy slot",
         "2.0");
     public static final DeliveryJob JOBC = new DeliveryJob(
         TypicalPersons.CARL,
+        TypicalPersons.DANIEL,
         "dummy slot",
         "3.0");
     public static final DeliveryJob JOBD = new DeliveryJob(
         TypicalPersons.DANIEL,
+        TypicalPersons.ELLE,
         "dummy slot",
         "4.0");
     public static final DeliveryJob JOBE = new DeliveryJob(
         TypicalPersons.ELLE,
+        TypicalPersons.FIONA,
         "dummy slot",
         "5.0");
     public static final DeliveryJob JOBF = new DeliveryJob(
         TypicalPersons.FIONA,
+        TypicalPersons.GEORGE,
         "dummy slot",
         "6.0");
 
