4388f1478b64116e4c2503b24f390d91e99dd840
XylusChen <chenxinyang00@gmail.com>
2023-03-14 23:49:49 +0800

    Fix checkstyle errors

--- src/main/java/seedu/address/logic/commands/FindApplicationCommand.java
+++ src/main/java/seedu/address/logic/commands/FindApplicationCommand.java
@@ -11,33 +11,32 @@ import seedu.address.model.application.NameContainsKeywordsPredicate;
  * containing any of the argument keywords. Keyword matching is case-insensitive.
  */
 public class FindApplicationCommand extends ApplicationCommand {
+    public static final String COMMAND_WORD = "find";
 
-	public static final String COMMAND_WORD = "find";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all internship applications "
+            + "which has company names containing "
+            + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
+            + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
+            + "Example: " + COMMAND_WORD + "  Goggle Mata";
 
-	public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all internship applications "
-			+ "which has company names containing "
-			+ "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
-			+ "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
-			+ "Example: " + COMMAND_WORD + "  Goggle Mata";
+    private final NameContainsKeywordsPredicate predicate;
 
-	private final NameContainsKeywordsPredicate predicate;
+    public FindApplicationCommand(NameContainsKeywordsPredicate predicate) {
+        this.predicate = predicate;
+    }
 
-	public FindApplicationCommand(NameContainsKeywordsPredicate predicate) {
-		this.predicate = predicate;
-	}
+    @Override
+    public CommandResult execute(ApplicationModel model) {
+        requireNonNull(model);
+        model.updateFilteredApplicationList(predicate);
+        return new CommandResult(String.format(Messages.MESSAGE_APPLICATIONS_LISTED_OVERVIEW,
+                model.getFilteredApplicationList().size()));
+    }
 
-	@Override
-	public CommandResult execute(ApplicationModel model) {
-		requireNonNull(model);
-		model.updateFilteredApplicationList(predicate);
-		return new CommandResult(String.format(Messages.MESSAGE_APPLICATIONS_LISTED_OVERVIEW,
-						model.getFilteredApplicationList().size()));
-	}
-
-	@Override
-	public boolean equals(Object other) {
-		return other == this // short circuit if same object
-				|| (other instanceof FindApplicationCommand // instanceof handles nulls
-				&& predicate.equals(((FindApplicationCommand) other).predicate)); // state check
-	}
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof FindApplicationCommand // instanceof handles nulls
+                && predicate.equals(((FindApplicationCommand) other).predicate)); // state check
+    }
 }
