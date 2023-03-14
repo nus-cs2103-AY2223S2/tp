2bbf086969257808c6855a4e66c983c7d689d83a
junyi <47441677+immanuelhume@users.noreply.github.com>
2023-03-14 13:30:17 +0800

    Add verification for project status

--- src/main/java/mycelium/mycelium/model/project/ProjectStatus.java
+++ src/main/java/mycelium/mycelium/model/project/ProjectStatus.java
@@ -6,5 +6,37 @@ package mycelium.mycelium.model.project;
 public enum ProjectStatus {
     NOT_STARTED,
     IN_PROGRESS,
-    DONE,
+    DONE;
+
+    /**
+     * Parses a project's status from its string representation. Throws an
+     * {@code IllegalArgumentException} if the input string is not valid. To
+     * avoid having to catch this error, consider calling {@code
+     * isValidProjectStatus} first.
+     */
+    public static ProjectStatus fromString(String s) {
+        switch (s.toLowerCase()) {
+        case "not_started":
+            return NOT_STARTED;
+        case "in_progress":
+            return IN_PROGRESS;
+        case "done":
+            return DONE;
+        default:
+            throw new IllegalArgumentException("Invalid project status: " + s);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return this == NOT_STARTED ? "not_started" : this == IN_PROGRESS ? "in_progress" : "done";
+    }
+
+    /**
+     * Checks if a string represents a valid project status.
+     */
+    public static boolean isValidProjectStatus(String test) {
+        test = test.toLowerCase();
+        return test.equals("not_started") || test.equals("in_progress") || test.equals("done");
+    }
 }
