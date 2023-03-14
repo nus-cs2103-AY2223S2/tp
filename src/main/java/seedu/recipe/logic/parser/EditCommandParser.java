10c0e4864e6c1cdaf6a42ba56f8677608473df3b
SeeuSim <seeusimong@gmail.com>
2023-03-15 00:08:27 +0800

    Refactored `Add` Command

--- src/main/java/seedu/recipe/logic/parser/EditCommandParser.java
+++ src/main/java/seedu/recipe/logic/parser/EditCommandParser.java
@@ -2,10 +2,7 @@ package seedu.recipe.logic.parser;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.recipe.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.recipe.logic.parser.CliSyntax.PREFIX_ADDRESS;
-import static seedu.recipe.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.recipe.logic.parser.CliSyntax.PREFIX_NAME;
-import static seedu.recipe.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.recipe.logic.parser.CliSyntax.PREFIX_TAG;
 
 import java.util.Collection;
