0636c89ec5a2f2c06cfef6748b455058bfb492e4
Aarnav Srinivasan <aarnav.srinivasan@gmail.com>
2023-03-14 16:44:43 +0800

    Add AddTodoCommand

---
+++ src/main/java/seedu/calidr/logic/commands/AddTodoCommand.java
@@ -0,0 +1,55 @@
+package seedu.calidr.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.calidr.logic.parser.CliSyntax.PREFIX_BY;
+import static seedu.calidr.logic.parser.CliSyntax.PREFIX_TITLE;
+
+import seedu.calidr.logic.commands.exceptions.CommandException;
+import seedu.calidr.model.Model;
+import seedu.calidr.model.task.ToDo;
+
+public class AddTodoCommand extends Command {
+
+    public static final String COMMAND_WORD = "todo";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a to-do to the task list. "
+            + "Parameters: "
+            + PREFIX_TITLE + "TITLE "
+            + PREFIX_BY + "BY DATE-TIME "
+            + "\n"
+            + "Example: " + COMMAND_WORD + " "
+            + PREFIX_TITLE + "Assignment "
+            + PREFIX_BY + "2023-05-04 2359 ";
+
+    public static final String MESSAGE_SUCCESS = "New to-do added: %1$s";
+    public static final String MESSAGE_DUPLICATE_TODO = "This to-do already exists in the task list.";
+
+    private final ToDo toAdd;
+
+    /**
+     * Creates an AddCommand to add the specified {@code ToDo}
+     */
+    public AddTodoCommand(ToDo todo) {
+        requireNonNull(todo);
+        toAdd = todo;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+
+        if (model.hasTask(toAdd)) {
+            throw new CommandException(MESSAGE_DUPLICATE_TODO);
+        }
+
+        model.addTask(toAdd);
+        return new CommandResult(String.format(MESSAGE_SUCCESS, toAdd));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof AddTodoCommand // instanceof handles nulls
+                && toAdd.equals(((AddTodoCommand) other).toAdd));
+    }
+}
