0de35c577c11f57b09a2c1e984c655f0b528f73f
nusE0726844 <92289387+nusE0726844@users.noreply.github.com>
2023-03-15 06:03:10 +0800

    Fix CustomImportOrder checkstyle error

--- src/main/java/seedu/vms/storage/appointment/JsonAdaptedAppointment.java
+++ src/main/java/seedu/vms/storage/appointment/JsonAdaptedAppointment.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import seedu.vms.commons.core.index.Index;
+
 import seedu.vms.commons.exceptions.IllegalValueException;
 import seedu.vms.model.appointment.Appointment;
 import seedu.vms.model.appointment.AppointmentBuilder;
