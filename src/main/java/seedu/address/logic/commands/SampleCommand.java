699a77461504cb587fbab4a689647a0b6a9cf10b
Hafeez <izzhafeez@gmail.com>
2023-03-14 16:44:20 +0800

    Fix merge conflicts

--- src/main/java/seedu/address/logic/commands/SampleCommand.java
+++ src/main/java/seedu/address/logic/commands/SampleCommand.java
@@ -4,6 +4,7 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 import seedu.address.commons.core.Messages;
+import seedu.address.logic.commands.results.CommandResult;
 import seedu.address.model.Model;
 import seedu.address.model.util.SampleDataUtil;
 
