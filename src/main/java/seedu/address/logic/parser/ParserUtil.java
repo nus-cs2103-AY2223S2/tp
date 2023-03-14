cec8c7dd1b14e99204480ce683c1de63f1158088
0x787af25e <74492257+0x787af25e@users.noreply.github.com>
2023-03-14 10:18:30 +0800

    Revert "Merge pull request #11 from jefrai/tutorial-adding-command"
    This reverts commit 7953f06f69406afa2bab0ea9acad781d16c8cb20, reversing
    changes made to 577cce3ea465848b2f0c76984a429851981a56c2.
    
    Revert remark command

--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -13,7 +13,6 @@ import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Phone;
-import seedu.address.model.person.Remark;
 import seedu.address.model.tag.Tag;
 
 /**
@@ -121,17 +120,5 @@ public class ParserUtil {
             tagSet.add(parseTag(tagName));
         }
         return tagSet;
-    }
-
-    /**
-     * Parses a {@code String remark} into a {@code Remark}.
-     * Leading and trailing whitespaces will be trimmed.
-     *
-     * @throws ParseException if the given {@code phone} is invalid.
-     */
-    public static Remark parseRemark(String remark) {
-        requireNonNull(remark);
-        String trimmedRemark = remark.trim();
-        return new Remark(trimmedRemark);
     }
 }
