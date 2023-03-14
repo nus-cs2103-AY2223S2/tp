ef2f38fa8745081dea0399615cba5b47a3997048
abenx162 <benxuan162@gmail.com>
2023-03-15 01:47:44 +0800

    Minor style changes

--- src/test/java/seedu/address/logic/parser/ThemeCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ThemeCommandParserTest.java
@@ -6,15 +6,8 @@ import seedu.address.logic.commands.ThemeCommand;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
-import static seedu.address.testutil.TypicalEmployeeIds.EMPLOYEE_ID_ONE;
 
-/**
- * As we are only doing white-box testing, our test cases do not cover path variations
- * outside of the DeleteCommand code. For example, inputs "1" and "1 abc" take the
- * same path through the DeleteCommand, and therefore we test only one of them.
- * The path variation for those two cases occur inside the ParserUtil, and
- * therefore should be covered by the ParserUtilTest.
- */
+
 public class ThemeCommandParserTest {
 
     private ThemeCommandParser parser = new ThemeCommandParser();
--- src/test/java/seedu/address/testutil/EmployeeUtil.java
+++ src/test/java/seedu/address/testutil/EmployeeUtil.java
@@ -7,7 +7,12 @@ import seedu.address.logic.commands.EditCommand;
 import seedu.address.model.employee.Employee;
 import seedu.address.model.tag.Tag;
 
-import static seedu.address.logic.parser.CliSyntax.*;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DEPARTMENT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 /**
  * A utility class for Employee.
