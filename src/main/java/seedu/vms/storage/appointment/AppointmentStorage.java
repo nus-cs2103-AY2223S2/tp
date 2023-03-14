334d2335022df35a9820d52deba2f91799ba9ee4
nusE0726844 <92289387+nusE0726844@users.noreply.github.com>
2023-03-14 16:54:55 +0800

    Create AppointmentStorage interface

---
+++ src/main/java/seedu/vms/storage/appointment/AppointmentStorage.java
@@ -0,0 +1,35 @@
+package seedu.vms.storage.appointment;
+
+import java.io.IOException;
+
+import seedu.vms.model.appointment.AppointmentManager;
+
+
+/**
+ * Represents the storage for {@link AppointmentManager}.
+ */
+public interface AppointmentStorage {
+    /**
+     * Loads the {@code AppointmentManager} as defined by the user.
+     *
+     * @throws IOException if an I/O error occurs.
+     */
+    public AppointmentManager loadUserAppointments() throws IOException;
+
+
+    /**
+     * Loads the {@code AppointmentManager} as defined in the application
+     * resources.
+     *
+     * @throws RuntimeException if an error occurs.
+     */
+    public AppointmentManager loadDefaultAppointments() throws RuntimeException;
+
+
+    /**
+     * Saves the specified {@code AppointmentManager} to hard disk.
+     *
+     * @throws IOException if an I/O error occurs.
+     */
+    public void saveAppointments(AppointmentManager manager) throws IOException;
+}
