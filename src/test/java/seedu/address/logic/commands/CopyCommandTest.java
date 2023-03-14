e4e0c2c49211fbc6268410f3f110bbea7a7ca902
minosx31 <n_minghao28@hotmail.com>
2023-03-14 15:39:44 +0800

    Add test case

--- src/test/java/seedu/address/logic/commands/CopyCommandTest.java
+++ src/test/java/seedu/address/logic/commands/CopyCommandTest.java
@@ -1,8 +1,11 @@
 package seedu.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import java.awt.GraphicsEnvironment;
@@ -43,4 +46,27 @@ public class CopyCommandTest {
 
         assertCommandFailure(copyCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
     }
+
+    @Test
+    public void equals() {
+        CopyCommand copyFirstCommand = new CopyCommand(INDEX_FIRST_PERSON);
+        CopyCommand copySecondCommand = new CopyCommand(INDEX_SECOND_PERSON);
+
+        // same object -> returns true
+        assertTrue(copyFirstCommand.equals(copyFirstCommand));
+
+        // same values -> returns true
+        CopyCommand copyFirstCommandCopy = new CopyCommand(INDEX_FIRST_PERSON);
+        assertTrue(copyFirstCommand.equals(copyFirstCommandCopy));
+
+        // different types -> returns false
+        assertFalse(copyFirstCommand.equals(1));
+
+        // null -> returns false
+        assertFalse(copyFirstCommand.equals(null));
+
+        // different person -> returns false
+        assertFalse(copyFirstCommand.equals(copySecondCommand));
+    }
+
 }
