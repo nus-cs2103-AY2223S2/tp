4973325fd9cc4f8004e4ed543b8e635c20c39e59
Aarnav Srinivasan <aarnav.srinivasan@gmail.com>
2023-03-15 00:12:43 +0800

    Refactor related code to work with recent changes

--- src/main/java/seedu/calidr/logic/commands/DeleteTaskCommand.java
+++ src/main/java/seedu/calidr/logic/commands/DeleteTaskCommand.java
@@ -43,7 +43,7 @@ public class DeleteTaskCommand extends Command {
         }
 
         Task taskToDelete = lastShownList.get(targetIndex.getZeroBased());
-        model.deleteTask(taskToDelete);
+        model.deleteTask(targetIndex);
         return new CommandResult(String.format(MESSAGE_DELETE_TASK_SUCCESS, taskToDelete));
     }
 
