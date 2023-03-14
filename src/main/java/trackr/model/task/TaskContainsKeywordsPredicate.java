37060a3b04e05bc1621d0dfa77848da78d73e0d3
Liu Muchen <liumuchen1@gmail.com>
2023-03-14 13:24:51 +0800

    Update TaskPredicate to use descriptor

--- src/main/java/trackr/model/task/TaskContainsKeywordsPredicate.java
+++ src/main/java/trackr/model/task/TaskContainsKeywordsPredicate.java
@@ -1,6 +1,7 @@
 package trackr.model.task;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Predicate;
 
 import trackr.commons.util.StringUtil;
@@ -8,24 +9,81 @@ import trackr.commons.util.StringUtil;
 /**
  * Tests that a {@code Task}'s {@code TaskName} matches any of the keywords given.
  */
-public class TaskContainsKeywordsPredicate implements Predicate<Task> {
-    private final List<String> keywords;
+public class TaskContainsKeywordsPredicate extends TaskDescriptor implements Predicate<Task> {
+    private List<String> taskNameKeywords;
 
-    public TaskContainsKeywordsPredicate(List<String> keywords) {
-        this.keywords = keywords;
+    public TaskContainsKeywordsPredicate() {
+        super();
+    }
+
+    /**
+     * Copy constructor.
+     */
+    public TaskContainsKeywordsPredicate(TaskContainsKeywordsPredicate toCopy) {
+        setTaskNameKeywords(toCopy.taskNameKeywords);
+        setTaskName(toCopy.getTaskName().isPresent() ? toCopy.getTaskName().get() : null);
+        setTaskDeadline(toCopy.getTaskDeadline().isPresent() ? toCopy.getTaskDeadline().get() : null);
+        setTaskStatus(toCopy.getTaskStatus().isPresent() ? toCopy.getTaskStatus().get() : null);
+    }
+
+    public void setTaskNameKeywords(List<String> taskNameKeywords) {
+        this.taskNameKeywords = taskNameKeywords;
+    }
+
+    public Optional<List<String>> getTaskNameKeywords() {
+        return Optional.ofNullable(taskNameKeywords);
     }
 
     @Override
     public boolean test(Task task) {
-        return keywords.stream()
-                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(task.getTaskName().fullTaskName, keyword));
+        boolean isTaskNameMatch;
+        boolean isTaskDeadlineMatch;
+        boolean isTaskStatusMatch;
+
+        if (taskNameKeywords != null) {
+            isTaskNameMatch = taskNameKeywords.stream()
+                    .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(task.getTaskName().fullTaskName, keyword));
+        } else {
+            // Default value since no name keyword is set
+            isTaskNameMatch = true;
+        }
+
+        if (getTaskDeadline().isPresent()) {
+            isTaskDeadlineMatch = getTaskDeadline().get().equals(task.getTaskDeadline());
+        } else {
+            // Default value since no deadline keyword is set
+            isTaskDeadlineMatch = true;
+        }
+
+        if (getTaskStatus().isPresent()) {
+            isTaskStatusMatch = getTaskStatus().get().equals(task.getTaskStatus());
+        } else {
+            // Default value since no status keyword is set
+            isTaskStatusMatch = true;
+        }
+
+        return isTaskNameMatch && isTaskDeadlineMatch && isTaskStatusMatch;
     }
 
     @Override
     public boolean equals(Object other) {
-        return other == this // short circuit if same object
-                || (other instanceof TaskContainsKeywordsPredicate // instanceof handles nulls
-                && keywords.equals(((TaskContainsKeywordsPredicate) other).keywords)); // state check
+        // short circuit if same object
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof TaskContainsKeywordsPredicate)) {
+            return false;
+        }
+
+        // state check
+        TaskContainsKeywordsPredicate predicate = (TaskContainsKeywordsPredicate) other;
+
+        return getTaskNameKeywords().equals(predicate.getTaskNameKeywords())
+                && getTaskName().equals(predicate.getTaskName())
+                && getTaskDeadline().equals(predicate.getTaskDeadline())
+                && getTaskStatus().equals(predicate.getTaskStatus());
     }
 
 }
