aac61837d38b1acb3e9f424e9ed0d94430e89d7d
ARPspoofing <mohdreyaaz99@gmail.com>
2023-03-14 17:07:27 +0800

    Add tableview

--- src/main/java/seedu/address/ui/PersonListPanel.java
+++ src/main/java/seedu/address/ui/PersonListPanel.java
@@ -2,18 +2,15 @@ package seedu.address.ui;
 
 import java.util.logging.Logger;
 
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.ListCell;
-import javafx.scene.control.ListView;
-import javafx.scene.layout.Region;
-import seedu.address.commons.core.LogsCenter;
-import seedu.address.model.person.Person;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
-
-import javafx.beans.property.SimpleStringProperty;
+import javafx.scene.layout.Region;
+import seedu.address.commons.core.LogsCenter;
+import seedu.address.model.person.Person;
 
 /**
  * Panel containing the list of persons.
@@ -23,15 +20,17 @@ public class PersonListPanel extends UiPart<Region> {
     private final Logger logger = LogsCenter.getLogger(PersonListPanel.class);
 
     @FXML
-    private ListView<Person> personListView;
-
+    private TableView<Person> table;
     @FXML
-    public TableView<Person> exampleTable;
-    public TableColumn<Person, String> name;
-    public TableColumn<Person, String> email;
-    public TableColumn<Person, String> address;
-    public TableColumn<Person, String> performance;
-    public TableColumn<Person, String> remark;
+    private TableColumn<Person, String> name;
+    @FXML
+    private TableColumn<Person, String> email;
+    @FXML
+    private TableColumn<Person, String> address;
+    @FXML
+    private TableColumn<Person, String> performance;
+    @FXML
+    private TableColumn<Person, String> remark;
 
     /**
      * Creates a {@code PersonListPanel} with the given {@code ObservableList}.
@@ -39,7 +38,6 @@ public class PersonListPanel extends UiPart<Region> {
     public PersonListPanel(ObservableList<Person> personList) {
         super(FXML);
 
-        //Adhere to Law of Demeter in next commit
         name.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getName().toString()));
         email.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getEmail().toString()));
         address.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getAddress().toString()));
@@ -48,34 +46,13 @@ public class PersonListPanel extends UiPart<Region> {
         remark.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getRemark().toString()));
 
         //to sort
-        //name.setSortable(true);
+        //
+        // name.setSortable(true);
 
-        exampleTable.setItems(personList);
-        exampleTable.setRowFactory(tableView -> {
+        table.setItems(personList);
+        table.setRowFactory(tableView -> {
             TableRow<Person> row = new TableRow<>();
             return row;
         });
-
-        //personListView.setItems(personList);
-        //personListView.setCellFactory(listView -> new PersonListViewCell());
     }
-
-    //Remove once no longer needed
-    /**
-     * Custom {@code ListCell} that displays the graphics of a {@code Person} using a {@code PersonCard}.
-     */
-    class PersonListViewCell extends ListCell<Person> {
-        @Override
-        protected void updateItem(Person person, boolean empty) {
-            super.updateItem(person, empty);
-
-            if (empty || person == null) {
-                setGraphic(null);
-                setText(null);
-            } else {
-                setGraphic(new PersonCard(person, getIndex() + 1).getRoot());
-            }
-        }
-    }
-
 }
