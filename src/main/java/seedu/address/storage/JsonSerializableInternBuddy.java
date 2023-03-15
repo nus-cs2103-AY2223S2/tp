0e1d7a95bbb13485abb0dcce10c209e3ca53f22d
DerrickSaltFish <1049347983@qq.com>
2023-03-16 01:09:26 +0800

    Update from JsonSerializableAddressBook.java to JsonSerializableInternBuddy.java

--- src/main/java/seedu/address/storage/JsonSerializableInternBuddy.java
+++ src/main/java/seedu/address/storage/JsonSerializableInternBuddy.java
@@ -9,53 +9,53 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonRootName;
 
 import seedu.address.commons.exceptions.IllegalValueException;
-import seedu.address.model.AddressBook;
-import seedu.address.model.ReadOnlyAddressBook;
+import seedu.address.model.InternBuddy;
+import seedu.address.model.ReadOnlyInternBuddy;
 import seedu.address.model.internship.Internship;
 
 /**
- * An Immutable AddressBook that is serializable to JSON format.
+ * An Immutable InternBuddy that is serializable to JSON format.
  */
-@JsonRootName(value = "addressbook")
-class JsonSerializableAddressBook {
+@JsonRootName(value = "internbuddy")
+class JsonSerializableInternBuddy {
 
     public static final String MESSAGE_DUPLICATE_INTERNSHIP = "Internships list contains duplicate internship(s).";
 
     private final List<JsonAdaptedInternship> internships = new ArrayList<>();
 
     /**
-     * Constructs a {@code JsonSerializableAddressBook} with the given internships
+     * Constructs a {@code JsonSerializableInternBuddy} with the given internships
      */
     @JsonCreator
-    public JsonSerializableAddressBook(@JsonProperty("internships") List<JsonAdaptedInternship> internships) {
+    public JsonSerializableInternBuddy(@JsonProperty("internships") List<JsonAdaptedInternship> internships) {
         this.internships.addAll(internships);
     }
 
     /**
-     * Converts a given {@code ReadOnlyAddressBook} into this class for Jackson use.
+     * Converts a given {@code ReadOnlyInternBuddy} into this class for Jackson use.
      *
-     * @param source future changes to this will not affect the created {@code JsonSerializableAddressBook}.
+     * @param source future changes to this will not affect the created {@code JsonSerializableInternBuddy}.
      */
-    public JsonSerializableAddressBook(ReadOnlyAddressBook source) {
+    public JsonSerializableInternBuddy(ReadOnlyInternBuddy source) {
         internships.addAll(source.getInternshipList().stream().map(JsonAdaptedInternship::new)
                 .collect(Collectors.toList()));
     }
 
     /**
-     * Converts this address book into the model's {@code AddressBook} object.
+     * Converts this address book into the model's {@code InternBuddy} object.
      *
      * @throws IllegalValueException if there were any data constraints violated.
      */
-    public AddressBook toModelType() throws IllegalValueException {
-        AddressBook addressBook = new AddressBook();
+    public InternBuddy toModelType() throws IllegalValueException {
+        InternBuddy internBuddy = new InternBuddy();
         for (JsonAdaptedInternship jsonAdaptedInternship : internships) {
             Internship internship = jsonAdaptedInternship.toModelType();
-            if (addressBook.hasInternship(internship)) {
+            if (internBuddy.hasInternship(internship)) {
                 throw new IllegalValueException(MESSAGE_DUPLICATE_INTERNSHIP);
             }
-            addressBook.addInternship(internship);
+            internBuddy.addInternship(internship);
         }
-        return addressBook;
+        return internBuddy;
     }
 
 }
