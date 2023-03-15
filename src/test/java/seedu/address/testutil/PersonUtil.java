57487f01dadc12513db6e3711cfc67fad2391c85
Lavanya Mishra <lavanya.m1804@gmail.com>
2023-03-15 16:15:46 +0800

    fix bug in tests

--- src/test/java/seedu/address/testutil/PersonUtil.java
+++ src/test/java/seedu/address/testutil/PersonUtil.java
@@ -64,7 +64,7 @@ public class PersonUtil {
         if (descriptor.getTags().isPresent()) {
             Set<Tag> tags = descriptor.getTags().get();
             if (tags.isEmpty()) {
-                sb.append(PREFIX_TAG);
+                sb.append(PREFIX_TAG).append(" ");
             } else {
                 tags.forEach(s -> sb.append(PREFIX_TAG).append(s.tagName).append(" "));
             }
@@ -72,7 +72,7 @@ public class PersonUtil {
         if (descriptor.getModules().isPresent()) {
             Set<Module> modules = descriptor.getModules().get();
             if (modules.isEmpty()) {
-                sb.append(PREFIX_MODULE);
+                sb.append(PREFIX_MODULE).append(" ");
             } else {
                 modules.forEach(s -> sb.append(PREFIX_MODULE).append(s.moduleName).append(" "));
             }
