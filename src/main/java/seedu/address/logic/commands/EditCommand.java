301c6cc1cb926b098b9d547a3c158651b103e42f
yyj-02 <yeohyongjie@outlook.com>
2023-03-15 14:03:06 +0800

    Add index

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -16,18 +16,19 @@ public class EditCommand extends Command {
     public static final String COMMAND_WORD = "edit";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits a listing in the listing book. "
+            + "INDEX (must be positive integer) "
             + "Parameters: "
-            + PREFIX_TITLE + "TITLE "
-            + PREFIX_DESCRIPTION + "DESCRIPTION "
-            + "[" + PREFIX_APPLICANT + "APPLICANT]...\n"
-            + "Example: " + COMMAND_WORD + " "
+            + PREFIX_TITLE + "TITLE (optional)"
+            + PREFIX_DESCRIPTION + "DESCRIPTION (optional)"
+            + "[" + PREFIX_APPLICANT + "APPLICANT (optional)]...\n"
+            + "Example: " + COMMAND_WORD + " 1 "
             + PREFIX_TITLE + "Cool job title "
             + PREFIX_DESCRIPTION + "Informative job description "
             + PREFIX_APPLICANT + "John "
             + PREFIX_APPLICANT + "Sam";
 
-    public static final String MESSAGE_SUCCESS = "New listing added: %1$s";
-    public static final String MESSAGE_DUPLICATE_LISTING = "This listing already exists in the listing book";
+    public static final String MESSAGE_SUCCESS = "Listing is changed to: %1$s";
+    public static final String MESSAGE_FAILURE = "Please provide the correct parameters.";
 
     private final Listing listingToAdd;
 
