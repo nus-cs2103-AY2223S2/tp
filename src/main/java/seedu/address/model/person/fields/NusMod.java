f37081564893ed5e9b920f8b3a17e6c45cec36c8
Shane Duggan <>
2023-03-15 16:38:25 +0800

    Add basic implementation of isValidModName

--- src/main/java/seedu/address/model/person/fields/NusMod.java
+++ src/main/java/seedu/address/model/person/fields/NusMod.java
@@ -1,11 +1,14 @@
 package seedu.address.model.person.fields;
 
+import java.io.*;
+
 /**
  * Represents a Person's singular mod taken in the address book.
  */
 public class NusMod {
 
     public static final String MESSAGE_CONSTRAINTS = "Modules should be a part of NUS' NUSMods list";
+    public static final String MODULELIST_FILE_PATH = "data/moduleList.json";
     public final String name;
 
     public NusMod(String modsString) {
@@ -14,6 +17,24 @@ public class NusMod {
 
     //todo: Update modules to only be able to include mods that are a part of NUSMods.
     public static boolean isValidModName(String trimmedTag) {
-        return true;
+        try {
+            FileInputStream inputStream = new FileInputStream("moduleListClean.json");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.contains("\"moduleCode\":")) {
+                    String moduleCode = line.split(":")[1].trim().replace("\"", "");
+                    if (moduleCode.equals(trimmedTag)) {
+                        reader.close();
+                        return true;
+                    }
+                }
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
