8891532328c0d46d67f63d4374aaa3fdb96ed9f5
DerrickSaltFish <1049347983@qq.com>
2023-03-16 01:02:04 +0800

    Update adressbook to internbuddy in UserPrefs.java and ReadOnlyUserPrefs.java

--- src/main/java/seedu/address/model/UserPrefs.java
+++ src/main/java/seedu/address/model/UserPrefs.java
@@ -14,7 +14,7 @@ import seedu.address.commons.core.GuiSettings;
 public class UserPrefs implements ReadOnlyUserPrefs {
 
     private GuiSettings guiSettings = new GuiSettings();
-    private Path addressBookFilePath = Paths.get("data" , "addressbook.json");
+    private Path internBuddyFilePath = Paths.get("data" , "addressbook.json");
 
     /**
      * Creates a {@code UserPrefs} with default values.
@@ -35,7 +35,7 @@ public class UserPrefs implements ReadOnlyUserPrefs {
     public void resetData(ReadOnlyUserPrefs newUserPrefs) {
         requireNonNull(newUserPrefs);
         setGuiSettings(newUserPrefs.getGuiSettings());
-        setAddressBookFilePath(newUserPrefs.getAddressBookFilePath());
+        setInternBuddyFilePath(newUserPrefs.getInternBuddyFilePath());
     }
 
     public GuiSettings getGuiSettings() {
@@ -46,14 +46,13 @@ public class UserPrefs implements ReadOnlyUserPrefs {
         requireNonNull(guiSettings);
         this.guiSettings = guiSettings;
     }
-
-    public Path getAddressBookFilePath() {
-        return addressBookFilePath;
+    public Path getInternBuddyFilePath() {
+        return internBuddyFilePath;
     }
 
-    public void setAddressBookFilePath(Path addressBookFilePath) {
-        requireNonNull(addressBookFilePath);
-        this.addressBookFilePath = addressBookFilePath;
+    public void setInternBuddyFilePath(Path internBuddyFilePath) {
+        requireNonNull(internBuddyFilePath);
+        this.internBuddyFilePath = internBuddyFilePath;
     }
 
     @Override
@@ -68,19 +67,19 @@ public class UserPrefs implements ReadOnlyUserPrefs {
         UserPrefs o = (UserPrefs) other;
 
         return guiSettings.equals(o.guiSettings)
-                && addressBookFilePath.equals(o.addressBookFilePath);
+                && internBuddyFilePath.equals(o.internBuddyFilePath);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(guiSettings, addressBookFilePath);
+        return Objects.hash(guiSettings, internBuddyFilePath);
     }
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append("Gui Settings : " + guiSettings);
-        sb.append("\nLocal data file location : " + addressBookFilePath);
+        sb.append("\nLocal data file location : " + internBuddyFilePath);
         return sb.toString();
     }
 
