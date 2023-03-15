f5e2b554b13f77de658d05c1b8d85264d20991fe
pkpaing <pkpaing@gmail.com>
2023-03-15 18:31:26 +0800

    Fixed CheckStyle

--- src/main/java/seedu/address/logic/commands/ImportCommand.java
+++ src/main/java/seedu/address/logic/commands/ImportCommand.java
@@ -1,22 +1,13 @@
 package seedu.address.logic.commands;
 
+import java.util.ArrayList;
 import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
-import seedu.address.model.person.*;
-import seedu.address.model.tag.Tag;
-import static seedu.address.storage.SocContacts.SOC_CONTACTS;
+import seedu.address.model.person.Person;
 import static seedu.address.storage.ChsContacts.CHS_CONTACTS;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
+import static seedu.address.storage.SocContacts.SOC_CONTACTS;
 
 /**
  * Adds a person to the address book.
