2926b658999682a124f96e4113dcad927716243f
pkpaing <pkpaing@gmail.com>
2023-03-15 18:53:43 +0800

    Fixed Checkstyle

--- src/test/java/seedu/address/logic/commands/ImportCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ImportCommandTest.java
@@ -5,6 +5,7 @@ import static seedu.address.logic.commands.ImportCommand.MESSAGE_NO_FACULTY;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
+
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
