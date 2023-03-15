310282748ba865406a10856a6f33201ae9350c17
Ai Bo <bo.ai@u.nus.edu>
2023-03-15 21:24:52 +0800

    remove unused import

--- src/main/java/seedu/address/storage/json/adapted/JsonAdaptedFlight.java
+++ src/main/java/seedu/address/storage/json/adapted/JsonAdaptedFlight.java
@@ -1,7 +1,5 @@
 package seedu.address.storage.json.adapted;
 
-
-import com.fasterxml.jackson.annotation.JacksonAnnotation;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
