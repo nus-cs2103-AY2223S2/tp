6dae5e524c93d4b2292d73d68b1fea8914de3d86
dfordarius <dfordarius@users.noreply.github.com>
2023-03-15 19:18:56 +0800

    Add config testing

---
+++ src/test/java/seedu/fitbook/commons/core/CsvConfigTest.java
@@ -0,0 +1,18 @@
+package seedu.fitbook.commons.core;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class CsvConfigTest {
+    @Test
+    public void equalsMethod() {
+        CsvConfig defaultCsvConfig = new CsvConfig();
+        assertNotNull(defaultCsvConfig);
+        assertTrue(defaultCsvConfig.equals(defaultCsvConfig));
+    }
+
+
+}
