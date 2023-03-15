a64a250eafd1a2d4d6e860d4e46497d5ce8e2053
chongweiguan <75872136+chongweiguan@users.noreply.github.com>
2023-03-15 23:40:57 +0800

    Create more test cases

--- src/test/java/trackr/model/order/OrderNameContainsKeywordPredicateTest.java
+++ src/test/java/trackr/model/order/OrderNameContainsKeywordPredicateTest.java
@@ -127,4 +127,205 @@ public class OrderNameContainsKeywordPredicateTest {
                 new OrderPredicateBuilder().withCustomerPhone("12345678").build();
         assertTrue(predicate.test(new OrderBuilder().withCustomerPhone("12345678").build()));
     }
+
+    @Test
+    public void test_oneVariableDoesNotMatch_returnsFalse() {
+        OrderNameContainsKeywordPredicate predicate;
+        Order testTask;
+
+        // Zero keywords
+        predicate =
+                new OrderPredicateBuilder().withOrderNameKeywords(Collections.emptyList()).build();
+        assertFalse(predicate.test(new OrderBuilder().withOrderName("Buy").build()));
+
+        // Non-matching keyword
+        predicate =
+                new OrderPredicateBuilder().withOrderNameKeywords(Arrays.asList("Buy")).build();
+        assertFalse(predicate.test(new OrderBuilder().withOrderName("Sort Inventory").build()));
+
+        // Keywords match deadline and status, but does not match name
+        predicate =
+                new OrderPredicateBuilder().withOrderNameKeywords(Arrays.asList("11/11/2024", "N")).build();
+        assertFalse(predicate.test(new OrderBuilder().withOrderName("Buy").withOrderDeadline("11/11/2024")
+                .withOrderStatus("N").build()));
+
+        // Non-matching deadline
+        predicate =
+                new OrderPredicateBuilder().withOrderDeadline("01/01/2023").build();
+        assertFalse(predicate.test(new OrderBuilder().withOrderDeadline("11/11/2023").build()));
+
+        // Match name and status, but not deadline
+        predicate = new OrderPredicateBuilder()
+                .withOrderNameKeywords(Arrays.asList("Buy", "Flour"))
+                .withOrderStatus("N")
+                .withOrderDeadline("01/01/2023")
+                .build();
+        testTask = new OrderBuilder()
+                .withOrderName("Buy Flour")
+                .withOrderStatus("N")
+                .withOrderDeadline("11/11/2023")
+                .build();
+        assertTrue(predicate.test(testTask));
+
+        // Non-matching status
+        predicate =
+                new OrderPredicateBuilder().withOrderStatus("N").build();
+        assertFalse(predicate.test(new OrderBuilder().withOrderStatus("D").build()));
+
+        // Match name and deadline, but not status
+        predicate = new OrderPredicateBuilder()
+                .withOrderNameKeywords(Arrays.asList("Buy", "Flour"))
+                .withOrderStatus("N")
+                .withOrderDeadline("01/01/2023")
+                .build();
+        testTask = new OrderBuilder()
+                .withOrderName("Buy Flour")
+                .withOrderStatus("D")
+                .withOrderDeadline("01/01/2023")
+                .build();
+        assertTrue(predicate.test(testTask));
+    }
+
+    @Test
+    public void test_twoVariableMatch_returnsTrue() {
+        OrderNameContainsKeywordPredicate predicate;
+
+        // Same name and deadline
+        predicate = new OrderPredicateBuilder()
+                .withOrderDeadline("01/01/2023")
+                .withOrderNameKeywords(Arrays.asList("Buy", "Food"))
+                .build();
+        assertTrue(predicate.test(new OrderBuilder().withOrderDeadline("01/01/2023").withOrderName("Buy Food").build()));
+
+        // Same deadline and status
+        predicate = new OrderPredicateBuilder()
+                .withOrderDeadline("01/01/2023")
+                .withOrderStatus("N")
+                .build();
+        assertTrue(predicate.test(new OrderBuilder().withOrderDeadline("01/01/2023").withOrderStatus("N").build()));
+
+        // Same name and status
+        predicate = new OrderPredicateBuilder()
+                .withOrderNameKeywords(Arrays.asList("Buy", "Food"))
+                .withOrderStatus("N")
+                .build();
+        Order testTask = new OrderBuilder()
+                .withOrderName("Buy Food")
+                .withOrderStatus("N")
+                .build();
+        assertTrue(predicate.test(testTask));
+    }
+
+    @Test
+    public void test_twoVariableDoesNotMatch_returnsFalse() {
+        OrderNameContainsKeywordPredicate predicate;
+        Order testTask;
+
+        // Same name, different deadline and status
+        predicate = new OrderPredicateBuilder()
+                .withOrderNameKeywords(Arrays.asList("Buy", "Food"))
+                .withOrderStatus("D")
+                .withOrderDeadline("11/11/2023")
+                .build();
+        testTask = new OrderBuilder()
+                .withOrderName("Buy Food")
+                .withOrderStatus("N")
+                .withOrderDeadline("01/01/2023")
+                .build();
+        assertTrue(predicate.test(testTask));
+
+        // Same deadline, different name and status
+        predicate = new OrderPredicateBuilder()
+                .withOrderNameKeywords(Arrays.asList("Sort", "Inventory"))
+                .withOrderStatus("D")
+                .withOrderDeadline("01/01/2023")
+                .build();
+        testTask = new OrderBuilder()
+                .withOrderName("Buy Food")
+                .withOrderStatus("N")
+                .withOrderDeadline("01/01/2023")
+                .build();
+        assertFalse(predicate.test(testTask));
+
+        // Same status, different name and deadline
+        predicate = new OrderPredicateBuilder()
+                .withOrderNameKeywords(Arrays.asList("Sort", "Inventory"))
+                .withOrderStatus("N")
+                .withOrderDeadline("01/01/2023")
+                .build();
+        testTask = new OrderBuilder()
+                .withOrderName("Buy Food")
+                .withOrderStatus("N")
+                .withOrderDeadline("01/01/2023")
+                .build();
+        assertFalse(predicate.test(testTask));
+    }
+
+    @Test
+    public void test_threeVariableMatch_returnsTrue() {
+        OrderNameContainsKeywordPredicate predicate;
+
+        // Same name, deadline and status
+        predicate = new OrderPredicateBuilder()
+                .withOrderNameKeywords(Arrays.asList("Buy", "Food"))
+                .withOrderStatus("N")
+                .withOrderDeadline("01/01/2023")
+                .build();
+        Order testTask = new OrderBuilder()
+                .withOrderName("Buy Food")
+                .withOrderStatus("N")
+                .withOrderDeadline("01/01/2023")
+                .build();
+        assertTrue(predicate.test(testTask));
+    }
+
+    @Test
+    public void test_threeVariableDoesNotMatch_returnsFalse() {
+        OrderNameContainsKeywordPredicate predicate;
+
+        // Different name, deadline and status
+        predicate = new OrderPredicateBuilder()
+                .withOrderNameKeywords(Arrays.asList("Sort", "Inventory"))
+                .withOrderStatus("D")
+                .withOrderDeadline("11/11/2023")
+                .build();
+        Order testTask = new OrderBuilder()
+                .withOrderName("Buy Food")
+                .withOrderStatus("N")
+                .withOrderDeadline("01/01/2023")
+                .build();
+        assertFalse(predicate.test(testTask));
+    }
+
+    @Test
+    public void test_atLeastOneFieldPresent_returnsTrue() {
+        OrderNameContainsKeywordPredicate predicate;
+
+        // taskNameKeywords present
+        predicate = new OrderPredicateBuilder()
+                .withOrderNameKeywords(Arrays.asList("Sort", "Inventory"))
+                .build();
+        assertTrue(predicate.isAnyFieldPresent());
+
+        // taskDeadline present
+        predicate = new OrderPredicateBuilder()
+                .withOrderDeadline("01/01/2022")
+                .build();
+        assertTrue(predicate.isAnyFieldPresent());
+
+        // taskStatus present
+        predicate = new OrderPredicateBuilder()
+                .withOrderStatus("N")
+                .build();
+        assertTrue(predicate.isAnyFieldPresent());
+    }
+
+    @Test
+    public void test_noFieldPresent_returnsFalse() {
+        OrderNameContainsKeywordPredicate predicate;
+
+        // taskNameKeywords present
+        predicate = new OrderPredicateBuilder().build();
+        assertFalse(predicate.isAnyFieldPresent());
+    }
 }
