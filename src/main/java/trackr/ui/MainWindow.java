1b4d87516cd9434ffbb64b04bbfbd009de812847
arkarsg <alxarkar@gmail.com>
2023-03-15 09:56:48 +0800

    Minor tweaks for Ui

--- src/main/java/trackr/ui/MainWindow.java
+++ src/main/java/trackr/ui/MainWindow.java
@@ -48,6 +48,9 @@ public class MainWindow extends UiPart<Stage> {
     private StackPane taskListPanelPlaceholder;
 
     @FXML
+    private StackPane orderListPanelPlaceholder;
+
+    @FXML
     private StackPane resultDisplayPlaceholder;
 
     @FXML
