9948d93392aa7bd25018299532af7f85238f4ac7
ScorpiusSigma <justinleng_@hotmail.com>
2023-03-15 19:06:12 +0800

    Add ListByTagCommand

--- src/main/java/seedu/wife/logic/parser/ListByTagCommandParser.java
+++ src/main/java/seedu/wife/logic/parser/ListByTagCommandParser.java
@@ -2,27 +2,27 @@ package seedu.wife.logic.parser;
 
 import static seedu.wife.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
-import seedu.wife.commons.core.index.Index;
-import seedu.wife.logic.commands.DeleteCommand;
+import seedu.wife.logic.commands.ListByTagCommand;
 import seedu.wife.logic.parser.exceptions.ParseException;
+import seedu.wife.model.tag.Tag;
 
 /**
- * Parses input arguments and creates a new DeleteCommand object
+ * Parses input arguments and creates a new ListByTagCommand object
  */
-public class ListByTagCommandParser implements Parser<DeleteCommand> {
+public class ListByTagCommandParser implements Parser<ListByTagCommand> {
 
     /**
-     * Parses the given {@code String} of arguments in the context of the DeleteCommand
-     * and returns a DeleteCommand object for execution.
+     * Parses the given {@code String} of arguments in the context of the ListByTagCommand
+     * and returns a ListByTagCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
-    public DeleteCommand parse(String args) throws ParseException {
+    public ListByTagCommand parse(String args) throws ParseException {
         try {
-            Index index = ParserUtil.parseIndex(args);
-            return new DeleteCommand(index);
+            Tag tag = ParserUtil.parseTag(args);
+            return new ListByTagCommand(tag);
         } catch (ParseException pe) {
             throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE), pe);
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, ListByTagCommand.MESSAGE_USAGE), pe);
         }
     }
 
