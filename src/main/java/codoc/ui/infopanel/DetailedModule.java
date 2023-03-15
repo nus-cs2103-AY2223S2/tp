0f3fe96a1b3f7eff5aa7e877a51337a545131368
harin0826 <e0411191@u.nus.edu>
2023-03-16 00:45:48 +0800

    Update CSS for theme style

--- src/main/java/codoc/ui/infopanel/DetailedModule.java
+++ src/main/java/codoc/ui/infopanel/DetailedModule.java
@@ -19,6 +19,7 @@ public class DetailedModule extends DetailedInfo {
 
     @FXML
     private ListView<Module> moduleListView;
+
     /**
      * Creates a {@code DetailedModule} tab with the given {@code protagonist}.
      */
@@ -30,12 +31,15 @@ public class DetailedModule extends DetailedInfo {
                 .forEach(module -> modules.add(module));
         moduleListView.setItems(modules);
         moduleListView.setCellFactory(listView -> new ModuleListViewCell());
+        // Took forever to get this to work but now ListView sets max height based on number of items
+        moduleListView.setPrefHeight((52 * modules.size()) + 2);
     }
 
     /**
      * Custom {@code ListCell} that displays the graphics of a {@code Module} using a {@code ModuleCard}.
      */
     class ModuleListViewCell extends ListCell<Module> {
+
         @Override
         protected void updateItem(Module module, boolean empty) {
             super.updateItem(module, empty);
