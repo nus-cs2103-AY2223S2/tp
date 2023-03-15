a37829b19e2e2639e2286aadf72e528258c6f6e6
ScorpiusSigma <justinleng_@hotmail.com>
2023-03-15 19:05:46 +0800

    Add ListByTagCommandParser

--- src/main/java/seedu/wife/logic/commands/ListByTagCommand.java
+++ src/main/java/seedu/wife/logic/commands/ListByTagCommand.java
@@ -1,9 +1,9 @@
 package seedu.wife.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.wife.model.Model.PREDICATE_SHOW_ALL_FOODS;
 
 import seedu.wife.model.Model;
+import seedu.wife.model.tag.Tag;
 
 /**
  * Lists all persons in the address book to the user.
@@ -14,16 +14,20 @@ public class ListByTagCommand extends Command {
 
     public static final String MESSAGE_SUCCESS = "Listed all food with %1$s tag";
 
-    public final Tag targetTag;
-    
-    public void ListByTagCommand(Tag targetTag) {
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Shows all food with the specific tag.\n"
+        + "Example: " + COMMAND_WORD + "dairy";
+
+    private final Tag targetTag;
+
+    public ListByTagCommand(Tag targetTag) {
         this.targetTag = targetTag;
     }
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        List<Food> lastShownList = model.updateFilteredFoodList(PREDICATE_SHOW_ALL_FOODS);
+
+        model.updateFilteredFoodList(food -> food.getTags().contains(this.targetTag));
 
         return new CommandResult(String.format(MESSAGE_SUCCESS, this.targetTag));
     }
