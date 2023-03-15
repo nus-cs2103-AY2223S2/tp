a26cf1a9fd96992ce7e5227a605f75af3d3cc97b
jnjy <jefferyng2000@gmail.com>
2023-03-15 17:53:37 +0800

    Remove remnant files

--- src/main/java/seedu/wife/commons/core/Messages.java
+++ src/main/java/seedu/wife/commons/core/Messages.java
@@ -10,5 +10,5 @@ public class Messages {
     public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
     public static final String MESSAGE_INVALID_FOOD_DISPLAYED_INDEX = "The food item index provided is invalid";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
-
+    public static final String MESSAGE_SUCCESSFUL_FOOD_TAG = "%s successfully tagged with %s";
 }
