250e276d22f83aae6e42576605a7967eef3c0c31
Tan Kai Min Russell <russelltanreg38@gmail.com>
2023-03-15 20:35:36 +0800

    Add tests for SortCommand

--- src/test/java/seedu/address/logic/parser/SortCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/SortCommandParserTest.java
@@ -1,12 +1,23 @@
 package seedu.address.logic.parser;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalPersons.getTypicalEduMate;
 
 import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.SortCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.person.ContactIndex;
 import seedu.address.model.person.Person;
 
 public class SortCommandParserTest {
@@ -69,6 +80,24 @@ public class SortCommandParserTest {
                 Comparator.comparing(Person::getModuleTags), "Module Tags: Ascending");
         testSinglePrefix(String.format(" %sd", Prefix.MODULE_TAG.getPrefix()),
                 Comparator.comparing(Person::getModuleTags), "Module Tags: Descending");
+    }
+
+    @Test
+    public void execute_sortIndexCommand_success() throws ParseException {
+        Model model = new ModelManager(getTypicalEduMate(), new UserPrefs());
+        SortCommandParser parser = new SortCommandParser();
+        parser.parse(String.format(" %s", Prefix.NAME.getPrefix()))
+                .execute(model);
+        List<Person> personList = model.getFilteredPersonList();
+        List<ContactIndex> contactIndexSortedByName = personList.stream()
+                .map(x -> x.getContactIndex()).collect(Collectors.toUnmodifiableList());
+        List<ContactIndex> sortedIndexSorted = personList.stream()
+                .map(x -> x.getContactIndex()).collect(Collectors.toUnmodifiableList());
+        assertNotEquals(contactIndexSortedByName.toArray(), sortedIndexSorted.toArray());
+        parser.parse(" index").execute(model);
+        List<ContactIndex> indexStream = personList.stream()
+                .map(x -> x.getContactIndex()).collect(Collectors.toUnmodifiableList());
+        assertArrayEquals(sortedIndexSorted.toArray(), indexStream.toArray());
     }
 
     /**
