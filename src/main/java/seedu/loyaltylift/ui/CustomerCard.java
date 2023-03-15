a33c9955a03669d708a75791f30094954e51cad1
JavonTeo <javonteo@hotmail.com>
2023-03-15 16:34:14 +0800

    - Added marked-icon.png and unmarked-icon.png for customer bookmark

--- src/main/java/seedu/loyaltylift/ui/CustomerCard.java
+++ src/main/java/seedu/loyaltylift/ui/CustomerCard.java
@@ -2,6 +2,8 @@ package seedu.loyaltylift.ui;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import javafx.scene.paint.Color;
