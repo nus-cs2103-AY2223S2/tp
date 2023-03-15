f169b902f7448ae449eb5883b8361b97d347d708
JThh <jiatong.han@u.nus.edu>
2023-03-15 21:51:06 +0800

    Fix CI

--- src/test/java/seedu/address/model/event/NoteTest.java
+++ src/test/java/seedu/address/model/event/NoteTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
