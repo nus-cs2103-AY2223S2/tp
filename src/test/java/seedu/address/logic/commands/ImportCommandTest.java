fb10bf3341f901ef2df057abaf2ebadddf3642ab
pkpaing <pkpaing@gmail.com>
2023-03-15 18:51:26 +0800

    Fixed Checkstyle

--- src/test/java/seedu/address/logic/commands/ImportCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ImportCommandTest.java
@@ -1,15 +1,13 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.ImportCommand.MESSAGE_NO_FACULTY;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
 import org.junit.jupiter.api.Test;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
-
-import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.logic.commands.ImportCommand.MESSAGE_NO_FACULTY;
-import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 public class ImportCommandTest {
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
