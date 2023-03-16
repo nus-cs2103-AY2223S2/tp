a0a31d1bc2ea1778e7535cfe110e15b5045a37d0
jweeyh <jweeyh@gmail.com>
2023-03-16 15:15:10 +0800

    Change icon for Sudo

--- src/main/java/seedu/sudohr/ui/UiPart.java
+++ src/main/java/seedu/sudohr/ui/UiPart.java
@@ -21,7 +21,7 @@ public abstract class UiPart<T> {
 
     /**
      * Constructs a UiPart with the specified FXML file URL.
-     * The FXML file must not specify the {@code fx:controller} attribute.d
+     * The FXML file must not specify the {@code fx:controller} attribute.
      */
     public UiPart(URL fxmlFileUrl) {
         loadFxmlFile(fxmlFileUrl, null);
