12842799b1dacb02f75cd1b208af9ad7f4adca8e
dohaduong <duonghado03@gmail.com>
2023-03-16 12:28:36 +0800

    Fix missing blank line at EOF error and checkstyle error

--- src/main/java/seedu/address/logic/LogicManager.java
+++ src/main/java/seedu/address/logic/LogicManager.java
@@ -32,7 +32,7 @@ public class LogicManager implements Logic {
 
     private final Model model;
     private final Storage storage;
-    private final DukeDriverParser  addressBookParser;
+    private final DukeDriverParser addressBookParser;
     private final TimetableParser timetableParser;
 
 
@@ -42,7 +42,7 @@ public class LogicManager implements Logic {
     public LogicManager(Model model, Storage storage) {
         this.model = model;
         this.storage = storage;
-        addressBookParser = new DukeDriverParser ();
+        addressBookParser = new DukeDriverParser();
         timetableParser = new TimetableParser();
     }
 
