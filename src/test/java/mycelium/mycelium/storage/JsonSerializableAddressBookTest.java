201e3a7512093c6fe4fd8db1852a7f6b803671a3
junyi <47441677+immanuelhume@users.noreply.github.com>
2023-03-16 20:59:42 +0800

    Rename test data file

--- src/test/java/mycelium/mycelium/storage/JsonSerializableAddressBookTest.java
+++ src/test/java/mycelium/mycelium/storage/JsonSerializableAddressBookTest.java
@@ -16,20 +16,20 @@ import mycelium.mycelium.testutil.TypicalPersons;
 public class JsonSerializableAddressBookTest {
 
     private static final Path TEST_DATA_FOLDER = Paths.get("src", "test", "data", "JsonSerializableAddressBookTest");
-    private static final Path TYPICAL_PERSONS_FILE = TEST_DATA_FOLDER.resolve("typicalPersonsAddressBook.json");
+    private static final Path TYPICAL_PERSONS_FILE = TEST_DATA_FOLDER.resolve("typicalAddressBook.json");
     private static final Path INVALID_PERSON_FILE = TEST_DATA_FOLDER.resolve("invalidPersonAddressBook.json");
     private static final Path DUPLICATE_PERSON_FILE = TEST_DATA_FOLDER.resolve("duplicatePersonAddressBook.json");
     private static final Path DUPLICATE_CLIENT_FILE = TEST_DATA_FOLDER.resolve("duplicateClientAddressBook.json");
     private static final Path DUPLICATE_PROJECT_FILE = TEST_DATA_FOLDER.resolve("duplicateProjectAddressBook.json");
 
     @Test
-    public void toModelType_typicalPersonsFile_success() throws Exception {
+    public void toModelType_typicalAddressBookFile_success() throws Exception {
         // TODO (jy): update the name of this test
         JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(TYPICAL_PERSONS_FILE,
             JsonSerializableAddressBook.class).get();
         AddressBook addressBookFromFile = dataFromFile.toModelType();
-        AddressBook typicalPersonsAddressBook = TypicalPersons.getTypicalAddressBook();
-        assertEquals(typicalPersonsAddressBook, addressBookFromFile);
+        AddressBook typicalAddressBook = TypicalPersons.getTypicalAddressBook();
+        assertEquals(typicalAddressBook, addressBookFromFile);
     }
 
     @Test
