f4918d7a64e1c8f993a7708a153fadfc13d767f7
junyi <47441677+immanuelhume@users.noreply.github.com>
2023-03-16 17:37:08 +0800

    Add toString for Client

--- src/test/java/mycelium/mycelium/model/client/ClientTest.java
+++ src/test/java/mycelium/mycelium/model/client/ClientTest.java
@@ -55,4 +55,10 @@ public class ClientTest {
         assertEquals(client.getSource(), Optional.empty());
         assertEquals(client.getMobileNumber(), Optional.empty());
     }
+
+    @Test
+    public void toString_works() {
+        Client client = new ClientBuilder().withName("Jamal").withEmail("jamal@hogriders.org").build();
+        assertEquals(client.toString(), "Jamal (jamal@hogriders.org)");
+    }
 }
