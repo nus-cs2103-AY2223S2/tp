7efa672b75c6ff8aaf4425901939e58f62dcd58c
chia-yh <0.chiayuhong.0@gmail.com>
2023-03-16 19:16:08 +0800

    Refactor test.data

--- src/test/java/seedu/socket/storage/JsonSerializableSocketTest.java
+++ src/test/java/seedu/socket/storage/JsonSerializableSocketTest.java
@@ -16,9 +16,9 @@ import seedu.socket.testutil.TypicalPersons;
 public class JsonSerializableSocketTest {
 
     private static final Path TEST_DATA_FOLDER = Paths.get("src", "test", "data", "JsonSerializableSocketTest");
-    private static final Path TYPICAL_PERSONS_FILE = TEST_DATA_FOLDER.resolve("typicalPersonsAddressBook.json");
+    private static final Path TYPICAL_PERSONS_FILE = TEST_DATA_FOLDER.resolve("typicalPersonsSocket.json");
     private static final Path INVALID_PERSON_FILE = TEST_DATA_FOLDER.resolve("invalidPersonSocket.json");
-    private static final Path DUPLICATE_PERSON_FILE = TEST_DATA_FOLDER.resolve("duplicatePersonAddressBook.json");
+    private static final Path DUPLICATE_PERSON_FILE = TEST_DATA_FOLDER.resolve("duplicatePersonSocket.json");
 
     @Test
     public void toModelType_typicalPersonsFile_success() throws Exception {
