c71c516ec6c94a222d8c0f1b6e8fc6b8cb779500
9fc70c892 <89006592+9fc70c892@users.noreply.github.com>
2023-03-16 08:52:12 +0800

    Revert "Add View Vehicle Customer Part"
    This reverts commit bb24038ecad5e4538e6c06045f93090a3a9650c0.

--- src/main/java/seedu/address/model/Model.java
+++ src/main/java/seedu/address/model/Model.java
@@ -98,10 +98,6 @@ public interface Model {
      */
     ObservableList<Person> getFilteredPersonList();
 
-    ObservableList<Customer> getFilteredCustomerList();
-
-    ObservableList<Vehicle> getFilteredVehicleList();
-
     /**
      * Updates the filter of the filtered person list to filter by the given {@code predicate}.
      *
@@ -174,6 +170,10 @@ public interface Model {
      * @param part Part to check against
      */
     boolean hasPart(Part part);
+
+    void updateFilteredTechnicianList(Predicate<Technician> predicate);
+
+    void updateFilteredServiceList(Predicate<Service> predicate);
 
     void updateFilteredVehicleList(Predicate<Vehicle> predicate);
 }
