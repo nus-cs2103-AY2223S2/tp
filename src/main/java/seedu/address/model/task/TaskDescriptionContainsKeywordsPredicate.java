37f5e80996a129e6b07fdd760fcefa434a964160
Yaladah <jeromeseet@hotmail.com>
2023-03-16 16:20:40 +0800

    Chore: Fix styling issues

--- src/main/java/seedu/address/model/task/TaskDescriptionContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/task/TaskDescriptionContainsKeywordsPredicate.java
@@ -1,11 +1,13 @@
 package seedu.address.model.task;
 
-import seedu.address.commons.util.StringUtil;
-import seedu.address.model.person.NameContainsKeywordsPredicate;
-
 import java.util.List;
 import java.util.function.Predicate;
 
+import seedu.address.commons.util.StringUtil;
+
+/**
+ * Tests that a {@code Task}'s {@code Description} matches any of the keywords given.
+ */
 public class TaskDescriptionContainsKeywordsPredicate implements Predicate<Task> {
     private final List<String> keywords;
 
