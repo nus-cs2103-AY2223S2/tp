977601dfdb0f1e37bdf4319e6fbb5f5f50ccf8ff
sarthak181 <0108sarthak@gmail.com>
2023-03-16 16:40:41 +0800

    Fix checkstyle errors

--- src/main/java/seedu/internship/model/internship/InternshipByPositionCompanyPredicate.java
+++ src/main/java/seedu/internship/model/internship/InternshipByPositionCompanyPredicate.java
@@ -2,10 +2,18 @@ package seedu.internship.model.internship;
 import java.util.function.Predicate;
 
 
+/**
+ * Tests whether position and company of two internships are the same
+ */
 public class InternshipByPositionCompanyPredicate implements Predicate<Internship> {
     private final Position pos;
     private final Company cmp;
 
+    /**
+     * Constructor for the class
+     * @param pos
+     * @param cmp
+     */
     public InternshipByPositionCompanyPredicate(Position pos, Company cmp) {
         this.pos = pos;
         this.cmp = cmp;
@@ -18,10 +26,10 @@ public class InternshipByPositionCompanyPredicate implements Predicate<Internshi
 
     @Override
     public boolean equals(Object other) {
-        if (other == this) {// short circuit if same object
+        if (other == this) { // short circuit if same object
             return true;
         }
-        if (other instanceof InternshipByPositionCompanyPredicate) {// instanceof handles nulls
+        if (other instanceof InternshipByPositionCompanyPredicate) { // instanceof handles nulls
             InternshipByPositionCompanyPredicate otherPred = (InternshipByPositionCompanyPredicate) other;
             return otherPred.pos.equals(this.pos) && otherPred.cmp.equals(this.cmp);
         }
