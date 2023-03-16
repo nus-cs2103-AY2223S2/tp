34c265d07e707775ac975605cb49be937465f982
Yaladah <jeromeseet@hotmail.com>
2023-03-16 16:37:50 +0800

    Chore: Fix styling issues

--- src/main/java/seedu/address/logic/parser/DeadlineCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeadlineCommandParser.java
@@ -6,7 +6,6 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_TASK;
 
 import java.util.stream.Stream;
 
-import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.DeadlineCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.task.Date;
