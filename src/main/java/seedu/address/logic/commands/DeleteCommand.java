c32672b2c9c9c88968151e1195eafc425b702b67
Huang Hao <huang_hao12@hotmail.com>
2023-03-16 12:57:33 +0800

    Fix bug on UX

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -40,6 +40,9 @@ public class DeleteCommand extends Command {
         }
 
         Person personToDelete = lastShownList.get(targetIndex.getZeroBased());
+        if (personToDelete.isSamePerson(model.getViewedPerson().get(0))) {
+            model.updateViewedPerson(null);
+        }
         model.deletePerson(personToDelete);
         return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, personToDelete));
     }
