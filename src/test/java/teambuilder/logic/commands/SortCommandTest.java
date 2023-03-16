4de618d4cdf95fe01b8a1d0c18a85a9c515231e2
swx0 <76123658+swx0@users.noreply.github.com>
2023-03-17 00:55:39 +0800

    Fix checkstyle

--- src/test/java/teambuilder/logic/commands/SortCommandTest.java
+++ src/test/java/teambuilder/logic/commands/SortCommandTest.java
@@ -1,19 +1,17 @@
 package teambuilder.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static teambuilder.testutil.TypicalPersons.getTypicalAddressBook;
+
 import org.junit.jupiter.api.Test;
+
 import teambuilder.commons.core.Messages;
-import teambuilder.commons.util.HistoryUtil;
 import teambuilder.logic.commands.exceptions.CommandException;
 import teambuilder.model.Model;
 import teambuilder.model.ModelManager;
-import teambuilder.model.TeamBuilder;
 import teambuilder.model.UserPrefs;
-import teambuilder.model.person.Person;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static teambuilder.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static teambuilder.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
-import static teambuilder.testutil.TypicalPersons.getTypicalAddressBook;
 
 public class SortCommandTest {
 
@@ -21,29 +19,29 @@ public class SortCommandTest {
 
     @Test
     public void execute_unknownOrderSpecified_failure() {
-        CommandException thrown = assertThrows(CommandException.class,
-                () -> new SortCommand("unknown", "tcount").execute(model));
+        CommandException thrown = assertThrows(CommandException.class, () ->
+                new SortCommand("unknown", "tcount").execute(model));
         assertTrue(thrown.getMessage().contentEquals(Messages.MESSAGE_INVALID_SORTING_ORDER));
     }
 
     @Test
     public void execute_emptyOrderSpecified_failure() {
-        CommandException thrown = assertThrows(CommandException.class,
-                () -> new SortCommand("", "tcount").execute(model));
+        CommandException thrown = assertThrows(CommandException.class, () ->
+                new SortCommand("", "tcount").execute(model));
         assertTrue(thrown.getMessage().contentEquals(Messages.MESSAGE_INVALID_SORTING_ORDER));
     }
 
     @Test
     public void execute_unknownSortBySpecified_failure() {
-        CommandException thrown = assertThrows(CommandException.class,
-                () -> new SortCommand("desc", "unknown").execute(model));
+        CommandException thrown = assertThrows(CommandException.class, () ->
+                new SortCommand("desc", "unknown").execute(model));
         assertTrue(thrown.getMessage().contentEquals(Messages.MESSAGE_INVALID_SORT_BY));
     }
 
     @Test
     public void execute_emptySortBySpecified_failure() {
-        CommandException thrown = assertThrows(CommandException.class,
-                () -> new SortCommand("desc", "").execute(model));
+        CommandException thrown = assertThrows(CommandException.class, () ->
+                new SortCommand("desc", "").execute(model));
         assertTrue(thrown.getMessage().contentEquals(Messages.MESSAGE_INVALID_SORT_BY));
     }
 
