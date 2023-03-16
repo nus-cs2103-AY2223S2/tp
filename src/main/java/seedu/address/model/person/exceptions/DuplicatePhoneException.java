771cb6bce95c027577a12f175aa17fe34ade7573
MrTwit99 <e0726852@u.nus.edu>
2023-03-16 11:15:56 +0800

    Fix checkstyle error

--- src/main/java/seedu/address/model/person/exceptions/DuplicatePhoneException.java
+++ src/main/java/seedu/address/model/person/exceptions/DuplicatePhoneException.java
@@ -5,8 +5,11 @@ package seedu.address.model.person.exceptions;
  * two different Parents).
  */
 public class DuplicatePhoneException extends RuntimeException {
+    /**
+     * A constructor that creates a new DuplicatePhoneException when a Phone is already being used by another Parent
+     */
     public DuplicatePhoneException() {
-        super("Unable to perform the following operation as you are trying to use " +
-                "a phone number that is already used by existing parent to create a new parent!");
+        super("Unable to perform the following operation as you are trying to use "
+                + "a phone number that is already used by existing parent to create a new parent!");
     }
 }
