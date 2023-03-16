ffb0fe43c2840112a30ed729bc6c42bea2d02fd0
shuimeihe <shuimeihe@gmail.com>
2023-03-16 09:38:06 +0800

    Add archive file path

--- src/main/java/seedu/address/model/UserPrefs.java
+++ src/main/java/seedu/address/model/UserPrefs.java
@@ -15,6 +15,7 @@ public class UserPrefs implements ReadOnlyUserPrefs {
 
     private GuiSettings guiSettings = new GuiSettings();
     private Path addressBookFilePath = Paths.get("data" , "addressbook.json");
+    private Path archiveFilePath = Paths.get("data", "archive,json");
 
     /**
      * Creates a {@code UserPrefs} with default values.
