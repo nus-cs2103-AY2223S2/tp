378a77b5e41257ab0aa1cb9a1c9950a0aeb0077c
Infrix <e0857426@u.nus.edu>
2023-03-17 16:45:37 +0800

    test: add test for AddPoints

--- src/test/java/seedu/loyaltylift/model/customer/PointsTest.java
+++ src/test/java/seedu/loyaltylift/model/customer/PointsTest.java
@@ -22,8 +22,30 @@ public class PointsTest {
         // null -> returns false
         assertFalse(points.equals(null));
 
-        // different remark -> returns false
+        // different points -> returns false
         Points differentPoints = new Points(200);
         assertFalse(points.equals(differentPoints));
+    }
+
+    @Test
+    public void AddPointsEquals() {
+        Points.AddPoints addPoints = new Points.AddPoints(100, Points.AddPoints.Modifier.PLUS);
+
+        // same object -> returns true
+        assertTrue(addPoints.equals(addPoints));
+
+        // same values -> returns true
+        Points.AddPoints addPointsCopy = new Points.AddPoints(addPoints.value, addPoints.modifier);
+        assertTrue(addPoints.equals(addPointsCopy));
+
+        // different types -> returns false
+        assertFalse(addPoints.equals(1));
+
+        // null -> returns false
+        assertFalse(addPoints.equals(null));
+
+        // different addpoints -> returns false
+        Points.AddPoints differentAddPoints = new Points.AddPoints(100, Points.AddPoints.Modifier.MINUS);
+        assertFalse(addPoints.equals(differentAddPoints));
     }
 }
