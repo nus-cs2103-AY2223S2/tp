974d490840bf418fa908f80a7348dc36c03f63f8
dfordarius <dfordarius@users.noreply.github.com>
2023-03-17 19:40:13 +0800

    fix appointment testing

--- src/test/java/seedu/fitbook/model/client/AppointmentTest.java
+++ src/test/java/seedu/fitbook/model/client/AppointmentTest.java
@@ -6,26 +6,22 @@ import static seedu.fitbook.testutil.Assert.assertThrows;
 import org.junit.jupiter.api.Test;
 
 public class AppointmentTest {
-    // to be fixed has issues
-    /*
+
     @Test
     public void constructor_null_throwsNullPointerException() {
         assertThrows(NullPointerException.class, () -> new Appointment(null));
     }
-    */
 
     @Test
-    public void constructor_invalidTagName_throwsIllegalArgumentException() {
+    public void constructor_invalidAppointment_throwsIllegalArgumentException() {
         String invalidAppointment = "1999-23-30";
         assertThrows(IllegalArgumentException.class, () -> new Appointment(invalidAppointment));
     }
-    // to be fixed has issues
-    /*
+
     @Test
-    public void isValidTagName() {
-        assertThrows(NullPointerException.class, () -> Appointment.isValidAppointment("10-10-1992"));
+    public void isValidAppointment() {
+        assertThrows(NullPointerException.class, () -> Appointment.isValidAppointment(null));
     }
-    */
 
     @Test
     public void test_equalsSymmetric() {
