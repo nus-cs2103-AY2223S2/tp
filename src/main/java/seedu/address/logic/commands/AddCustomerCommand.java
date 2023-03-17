ac2fca0c6088ad410fb7749bda09c54829fe53fd
ginloy <liyongleow@gmail.com>
2023-03-17 11:24:36 +0800

    Change MESSAGE_USAGE

--- src/main/java/seedu/address/logic/commands/AddCustomerCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCustomerCommand.java
@@ -1,6 +1,11 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
@@ -11,7 +16,20 @@ import seedu.address.model.entity.person.Customer;
  */
 public class AddCustomerCommand extends AddCommand {
     public static final String COMMAND_WORD = "addcustomer";
-    public static final String MESSAGE_USAGE = AddCommand.MESSAGE_USAGE;
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a customer to the address book. "
+            + "Parameters: "
+            + PREFIX_NAME + "NAME "
+            + PREFIX_PHONE + "PHONE "
+            + PREFIX_EMAIL + "EMAIL "
+            + PREFIX_ADDRESS + "ADDRESS "
+            + "[" + PREFIX_TAG + "TAG]...\n"
+            + "Example: " + COMMAND_WORD + " "
+            + PREFIX_NAME + "John Doe "
+            + PREFIX_PHONE + "98765432 "
+            + PREFIX_EMAIL + "johnd@example.com "
+            + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 "
+            + PREFIX_TAG + "friends "
+            + PREFIX_TAG + "owesMoney";
     public static final String MESSAGE_SUCCESS = "New customer added: %1$s";
     public static final String MESSAGE_DUPLICATE_CUSTOMER = "This customer already registered";
 
