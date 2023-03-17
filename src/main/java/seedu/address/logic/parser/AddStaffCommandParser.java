a7b497afe03ee8e7a6d6580412061cb41b8e0afe
ginloy <liyongleow@gmail.com>
2023-03-17 15:31:34 +0800

    Temporary template for AddStaffCommandParser

---
+++ src/main/java/seedu/address/logic/parser/AddStaffCommandParser.java
@@ -0,0 +1,65 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.commons.core.Messages.MESSAGE_UNSUPPORTED_COMMAND;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+
+import java.util.Set;
+import java.util.stream.Stream;
+
+import seedu.address.logic.commands.AddStaffCommand;
+import seedu.address.logic.commands.AddTechnicianCommand;
+import seedu.address.logic.idgen.IdGenerator;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.entity.person.Address;
+import seedu.address.model.entity.person.Email;
+import seedu.address.model.entity.person.Name;
+import seedu.address.model.entity.person.Phone;
+import seedu.address.model.entity.person.Staff;
+import seedu.address.model.entity.person.Technician;
+import seedu.address.model.tag.Tag;
+
+/**
+ * Parses input arguments and creates AddStaffCommand object
+ */
+public class AddStaffCommandParser implements Parser<AddStaffCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the AddStaffCommand
+     * and returns an AddStaffCommand object for execution.
+     *
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public AddStaffCommand parse(String args) throws ParseException {
+//        ArgumentMultimap argMultimap =
+//                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_TAG);
+//
+//        if (!arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_ADDRESS, PREFIX_PHONE, PREFIX_EMAIL)
+//                || !argMultimap.getPreamble().isEmpty()) {
+//            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddTechnicianCommand.MESSAGE_USAGE));
+//        }
+//
+//        Name name = ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get());
+//        Phone phone = ParserUtil.parsePhone(argMultimap.getValue(PREFIX_PHONE).get());
+//        Email email = ParserUtil.parseEmail(argMultimap.getValue(PREFIX_EMAIL).get());
+//        Address address = ParserUtil.parseAddress(argMultimap.getValue(PREFIX_ADDRESS).get());
+//        Set<Tag> tagList = ParserUtil.parseTags(argMultimap.getAllValues(PREFIX_TAG));
+//
+//        Staff technician = new Staff(IdGenerator.generateStaffId(), name, phone, email, address, tagList);
+//
+//        return new AddTechnicianCommand(technician);
+        throw new ParseException(MESSAGE_UNSUPPORTED_COMMAND);
+    }
+
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
+    }
+}
