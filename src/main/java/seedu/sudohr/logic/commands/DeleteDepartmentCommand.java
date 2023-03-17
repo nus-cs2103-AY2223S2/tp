f5bab6a7faeb05749888e3ee26309bf58026085a
kwangjoo <pkj5257@gmail.com>
2023-03-17 14:12:26 +0800

    Create DeleteDepartmentCommand

---
+++ src/main/java/seedu/sudohr/logic/commands/DeleteDepartmentCommand.java
@@ -0,0 +1,47 @@
+package seedu.sudohr.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import seedu.sudohr.logic.commands.exceptions.CommandException;
+import seedu.sudohr.model.Model;
+import seedu.sudohr.model.department.Department;
+import seedu.sudohr.model.department.DepartmentName;
+
+
+
+public class DeleteDepartmentCommand extends Command {
+
+    public static final String COMMAND_WORD = "delete";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Deletes the department identified by the department name.\n"
+            + "Parameters: DEPARTMENT_NAME (must be a string)\n"
+            + "Example: " + COMMAND_WORD + " Human Resources";
+
+    public static final String MESSAGE_DELETE_DEPARTMENT_SUCCESS = "Deleted department: %1$s";
+
+    private static final String MESSAGE_DEPARTMENT_NOT_EXIST = "The given department does not exist.";
+
+    private final DepartmentName targetDepartment;
+
+    public DeleteDepartmentCommand(DepartmentName targetDepartment) {
+        this.targetDepartment = targetDepartment;
+    }
+
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        Department departmentToDelete = model.getDepartment(targetDepartment);
+
+        if (departmentToDelete == null) {
+            throw new CommandException(MESSAGE_DEPARTMENT_NOT_EXIST);
+        }
+
+        model.removeDepartment(departmentToDelete);
+
+        return new CommandResult(String.format(MESSAGE_DELETE_DEPARTMENT_SUCCESS, departmentToDelete));
+    }
+
+
+}
