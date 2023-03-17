262b45cd1066b573c14df8117a4241badd79af99
Wang Jiefan <wng.jiefan@gmail.com>
2023-03-17 11:40:05 +0800

    Fix merge

--- src/main/java/seedu/calidr/model/task/params/Priority.java
+++ src/main/java/seedu/calidr/model/task/params/Priority.java
@@ -1,9 +1,4 @@
-<<<<<<<< HEAD:src/main/java/seedu/calidr/task/Priority.java
-package seedu.calidr.task;
-========
 package seedu.calidr.model.task.params;
->>>>>>>> 97f709a476e552e2d4a8f72843117aba9cbd4dee:src/main/java/seedu/calidr/model/task/params/Priority.java
-
 /**
  * Represents the priority associated with each task.
  */
