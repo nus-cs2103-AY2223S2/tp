22ff8876438853cd023cd51a02ec953779924f83
XuanZheng <lim_xuan_zheng@hotmail.com>
2023-03-17 13:47:55 +0800

    Update to conform to checkstyle

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -22,7 +22,6 @@ import seedu.address.model.Model;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.ReadOnlyUserPrefs;
 import seedu.address.model.person.Person;
-import seedu.address.model.task.DeadlineTask;
 import seedu.address.model.task.Task;
 import seedu.address.testutil.PersonBuilder;
 
