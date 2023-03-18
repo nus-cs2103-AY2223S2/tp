dc52aa9a27111af9878a9a577bf4d3e0b873dc71
ScorpiusSigma <justinleng_@hotmail.com>
2023-03-18 15:37:29 +0800

    Add UniqueTagList to wife and its corresponding methods

--- src/main/java/seedu/wife/model/Wife.java
+++ src/main/java/seedu/wife/model/Wife.java
@@ -7,6 +7,8 @@ import java.util.List;
 import javafx.collections.ObservableList;
 import seedu.wife.model.food.Food;
 import seedu.wife.model.food.UniqueFoodList;
+import seedu.wife.model.tag.Tag;
+import seedu.wife.model.tag.UniqueTagList;
 
 /**
  * Wraps all data at the WIFE level
@@ -15,6 +17,7 @@ import seedu.wife.model.food.UniqueFoodList;
 public class Wife implements ReadOnlyWife {
 
     private final UniqueFoodList foods;
+    private final UniqueTagList tags;
 
     /*
      * The 'unusual' code block below is a non-static initialization block, sometimes used to avoid duplication
@@ -25,12 +28,13 @@ public class Wife implements ReadOnlyWife {
      */
     {
         foods = new UniqueFoodList();
+        tags = new UniqueTagList();
     }
 
     public Wife() {}
 
     /**
-     * Creates WIFE using the Foods in the {@code toBeCopied}
+     * Creates WIFE using the Foods and Tags in the {@code toBeCopied}
      */
     public Wife(ReadOnlyWife toBeCopied) {
         this();
@@ -40,11 +44,19 @@ public class Wife implements ReadOnlyWife {
     //// list overwrite operations
 
     /**
-     * Replaces the contents of the food list with {@code persons}.
-     * {@code persons} must not contain duplicate foods.
+     * Replaces the contents of the food list with {@code foods}.
+     * {@code foods} must not contain duplicate foods.
      */
     public void setFoods(List<Food> foods) {
         this.foods.setFoods(foods);
+    }
+
+    /**
+     * Replaces the contents of the tag list with {@code tags}.
+     * {@code tags} must not contain duplicate tags.
+     */
+    public void setTags(List<Tag> foods) {
+        this.tags.setTags(foods);
     }
 
     /**
@@ -54,9 +66,47 @@ public class Wife implements ReadOnlyWife {
         requireNonNull(newData);
 
         setFoods(newData.getFoodList());
+        setTags(newData.getTagList());
     }
 
-    //// person-level operations
+    //// tag-level operations
+
+    /**
+     * Returns true if tag with the same name and expiry date as another {@code tag} exists in the WIFE.
+     */
+    public boolean hasTag(Tag tag) {
+        requireNonNull(tag);
+        return tags.contains(tag);
+    }
+
+    /**
+     * Adds tag to WIFE.
+     * The tag must not already exist in WIFE.
+     */
+    public void addTag(Tag t) {
+        tags.add(t);
+    }
+
+    /**
+     * Replaces the given tag {@code target} in the list with {@code editedTag}.
+     * {@code target} must exist in WIFE.
+     * The tag of {@code editedTag} must not be the same as another existing tag in WIFE.
+     */
+    public void setTag(Tag target, Tag editedTag) {
+        requireNonNull(editedTag);
+
+        tags.setTag(target, editedTag);
+    }
+
+    /**
+     * Removes {@code key} from {@code WIFE}.
+     * {@code key} must exist in WIFE.
+     */
+    public void removeTag(Tag key) {
+        tags.remove(key);
+    }
+
+    //// food-level operations
 
     /**
      * Returns true if food with the same name and expiry date as another {@code food} exists in the WIFE.
@@ -77,7 +127,7 @@ public class Wife implements ReadOnlyWife {
     /**
      * Replaces the given food {@code target} in the list with {@code editedFood}.
      * {@code target} must exist in WIFE.
-     * The food of {@code editedFood} must not be the same as another existing food in the address book.
+     * The food of {@code editedFood} must not be the same as another existing food in the WIFE.
      */
     public void setFood(Food target, Food editedFood) {
         requireNonNull(editedFood);
@@ -97,8 +147,9 @@ public class Wife implements ReadOnlyWife {
 
     @Override
     public String toString() {
-        return foods.asUnmodifiableObservableList().size() + " foods";
-        // TODO: refine later
+        return foods.asUnmodifiableObservableList().size()
+            + " foods \n"
+            + tags.asUnmodifiableObservableList().size() + " available tags";
     }
 
     @Override
@@ -107,14 +158,26 @@ public class Wife implements ReadOnlyWife {
     }
 
     @Override
+    public ObservableList<Tag> getTagList() {
+        return tags.asUnmodifiableObservableList();
+    }
+
+    @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
-                || (other instanceof Wife // instanceof handles nulls
-                && foods.equals(((Wife) other).foods));
+                || (
+                    other instanceof Wife // instanceof handles nulls
+                    && foods.equals(
+                        ((Wife) other).foods
+                        )
+                    && tags.equals(
+                        ((Wife) other).tags
+                    )
+                );
     }
 
     @Override
     public int hashCode() {
-        return foods.hashCode();
+        return foods.hashCode() + tags.hashCode();
     }
 }
