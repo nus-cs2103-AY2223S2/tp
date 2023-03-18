523a935ecef7ee8daf25d1c071f1051a8f450c18
Darren Chang <darrenchang2@gmail.com>
2023-03-18 22:20:53 +0800

    Fix CheckstyleTest

--- src/test/java/trackr/testutil/OrderDescriptorBuilder.java
+++ src/test/java/trackr/testutil/OrderDescriptorBuilder.java
@@ -6,7 +6,6 @@ import trackr.model.order.OrderDescriptor;
 import trackr.model.order.OrderName;
 import trackr.model.order.OrderQuantity;
 import trackr.model.order.OrderStatus;
-import trackr.model.order.customer.Customer;
 import trackr.model.order.customer.CustomerAddress;
 import trackr.model.order.customer.CustomerName;
 import trackr.model.order.customer.CustomerPhone;
