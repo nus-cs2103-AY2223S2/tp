90398f2eaf189173960857d84b01a6a8abac3b5a
iapetusbob <haoyuansayhi@gmail.com>
2023-03-18 16:12:43 +0800

    updated find command to work with address and company

--- src/main/java/seedu/address/model/person/PersonContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/person/PersonContainsKeywordsPredicate.java
@@ -1,7 +1,7 @@
 package seedu.address.model.person;
 
-import java.util.List;
 import java.util.Arrays;
+import java.util.List;
 import java.util.function.Predicate;
 
 import seedu.address.commons.util.StringUtil;
