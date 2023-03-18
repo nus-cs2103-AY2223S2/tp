25cddb45fd767e067bdadfe86c3b35d75bdea62f
jnjy <jefferyng2000@gmail.com>
2023-03-18 11:50:01 +0800

    feature: add test cases for `TagName`
    Test cases for `TagName` are:
    1. check if tag name is empty (blank name)
    2. check if tag name contains random symbol
    3. check if tag name is too lengthy (> 15 characters long)

--- src/main/java/seedu/wife/model/tag/TagName.java
+++ src/main/java/seedu/wife/model/tag/TagName.java
@@ -8,11 +8,11 @@ import static seedu.wife.commons.util.AppUtil.checkArgument;
  */
 public class TagName {
     public static final Integer TAG_NAME_MAX_LENGTH = 15;
-    public static final String VALIDATION_REGEX = "/^[a-zA-Z\\s]*$/";
+    public static final String NAME_VALIDATION_REGEX = "^[a-zA-Z\\s]*$";
     public static final String TAG_NAME_NOT_PRESENT = "Tag name should not be empty. "
             + "Please insert a name for your tag.";
     public static final String TAG_NAME_LENGTHY = "Tag name should have maximum 15 characters.";
-    public static final String INVALID_TAG_NAME = "Tags names should be alphanumeric";
+    public static final String INVALID_TAG_NAME = "Tags names should contain only letters.";
     private final String tagName;
 
     /**
@@ -31,7 +31,7 @@ public class TagName {
      * Returns true if a given string is a valid tag name.
      */
     public static boolean isValidTagName(String test) {
-        return test.matches(VALIDATION_REGEX);
+        return test.matches(NAME_VALIDATION_REGEX);
     }
 
     /**
--- src/test/java/seedu/wife/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/wife/logic/commands/CommandTestUtil.java
@@ -52,6 +52,11 @@ public class CommandTestUtil {
     public static final String INVALID_EXPIRY_DATE_DESC = " " + PREFIX_EXPIRY_DATE + "13112024"; // date not formatted
     public static final String INVALID_TAG_DESC = " " + PREFIX_TAG + "dairy*"; // '*' not allowed in tags
 
+    // test util for Tag\
+    public static final String INVALID_TAG_NAME_EMPTY_NAME = "";
+    public static final String INVALID_TAG_NAME_CONTAIN_SYMBOL = "Cre@m";
+    public static final String INVALID_TAG_NAME_TOO_LENGTHY = "VegetablesHealthy";
+
     public static final String PREAMBLE_WHITESPACE = "\t  \r  \n";
     public static final String PREAMBLE_NON_EMPTY = "NonEmptyPreamble";
 
