eef5d4f2618be3c53930eb6acbebb491900da641
hingen <1627313-hingen@users.noreply.gitlab.com>
2023-03-18 09:18:47 +0800

    Add integration tests for AddLectureCommand

---
+++ src/test/java/seedu/address/logic/commands/AddLectureCommandIntegrationTest.java
@@ -0,0 +1,75 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.commons.core.Messages.MESSAGE_MODULE_DOES_NOT_EXIST;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.lecture.Lecture;
+import seedu.address.model.module.Module;
+import seedu.address.model.module.ModuleCode;
+import seedu.address.testutil.ModuleBuilder;
+import seedu.address.testutil.TypicalLectures;
+import seedu.address.testutil.TypicalModules;
+
+
+/**
+ * Contains integration tests {interactions with the Model} for {@code AddLectureCommand}.
+ */
+public class AddLectureCommandIntegrationTest {
+
+    private Model model;
+
+    @BeforeEach
+    public void setUp() {
+        model = new ModelManager(TypicalModules.getTypicalTracker(), new UserPrefs());
+    }
+
+    @Test
+    public void execute_newLecture_success() {
+        Module module = new ModuleBuilder(TypicalModules.CS2040S).build();
+        ModuleCode moduleCode = module.getCode();
+        Lecture validLecture = TypicalLectures.ST2334_TOPIC_1;
+
+        // make sure that the module modified by the command is not the original TypicalModules.CS2040S
+        model.deleteModule(module);
+        model.addModule(module);
+
+        Model expectedModel = new ModelManager(model.getTracker(), model.getUserPrefs());
+        Module moduleCopy = new ModuleBuilder(module).build();
+
+        // make sure that validLecture is added to a copy of the module and not the same Module object that model has
+        expectedModel.deleteModule(module);
+        expectedModel.addModule(moduleCopy);
+        expectedModel.addLecture(moduleCopy, validLecture);
+
+        assertCommandSuccess(new AddLectureCommand(moduleCode, validLecture), model,
+                String.format(AddLectureCommand.MESSAGE_SUCCESS, moduleCode, validLecture), model);
+    }
+
+    @Test
+    public void execute_moduleDoesNotExist_throwsCommandException() {
+        Module module = new ModuleBuilder(TypicalModules.CS2107).build();
+        ModuleCode moduleCode = module.getCode();
+        Lecture validLecture = TypicalLectures.CS2040S_WEEK_1;
+
+        assertCommandFailure(new AddLectureCommand(moduleCode, validLecture), model,
+                String.format(MESSAGE_MODULE_DOES_NOT_EXIST, moduleCode));
+    }
+
+    @Test
+    public void execute_duplicateLecture_throwsCommandException() {
+        Module module = new ModuleBuilder(TypicalModules.CS2040S).build();
+        ModuleCode moduleCode = module.getCode();
+        Lecture lectureInList = TypicalLectures.CS2040S_WEEK_1;
+
+        assertCommandFailure(new AddLectureCommand(moduleCode, lectureInList), model,
+                String.format(AddLectureCommand.MESSAGE_DUPLICATE_LECTURE, moduleCode));
+    }
+
+}
