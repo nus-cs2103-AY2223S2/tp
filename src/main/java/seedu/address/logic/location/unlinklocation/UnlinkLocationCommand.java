f7d21f0ddf010c676183b2eaeb2d56c71b7ba556
Jishnu <e0725046@u.nus.edu>
2023-03-19 20:18:40 +0800

    Adapt unlink between location and flight

--- src/main/java/seedu/address/logic/location/unlinklocation/UnlinkLocationCommand.java
+++ src/main/java/seedu/address/logic/location/unlinklocation/UnlinkLocationCommand.java
@@ -5,37 +5,66 @@ import seedu.address.logic.core.CommandResult;
 import seedu.address.logic.core.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.flight.Flight;
-import seedu.address.model.flight.exceptions.FlightNotFoundException;
+import seedu.address.model.link.exceptions.LinkException;
+import seedu.address.model.location.FlightLocationType;
+import seedu.address.model.location.Location;
+
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * The command that unlinks flights to departure and arrival locations.
  */
 public class UnlinkLocationCommand implements Command {
-    /**
-     * The id of the light to unlink locations.
-     */
-    private final String flightId;
+    private static final String FLIGHT_NOT_FOUND_EXCEPTION =
+            "Flight with id %s is not found.";
+    private static final String LOCATION_NOT_FOUND_EXCEPTION =
+            "Location with id %s is not found.";
+    private static final String DISPLAY_MESSAGE =
+            "Unlinked %s from flight %s.";
 
     /**
-     * Creates a command that unlinks a flight to locations.
-     * @param flightId the id of the flight to unlink
+     * The id of the location
      */
-    public UnlinkLocationCommand(String flightId) {
-        this.flightId = flightId;
+    private final Map<FlightLocationType, Location> locations;
+
+    /**
+     * The id of the flight
+     */
+    private final Flight flight;
+
+    /**
+     * Creates a new unlink command.
+     *
+     * @param locations the id of the locations.
+     * @param flight the id of the flight.
+     */
+    public UnlinkLocationCommand(Map<FlightLocationType, Location> locations, Flight flight) {
+        this.locations = locations;
+        this.flight = flight;
+    }
+
+    @Override
+    public String toString() {
+        String result = locations.entrySet()
+                .stream()
+                .map((entry) -> String.format(
+                        "%s: %s",
+                        entry.getKey(),
+                        entry.getValue().getName()))
+                .collect(Collectors.joining(","));
+        return String.format(DISPLAY_MESSAGE, result, flight.getCode());
     }
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
-        Flight flightToUnlink;
         try {
-            flightToUnlink = model.getFlightById(flightId);
-        } catch (FlightNotFoundException e) {
-            return new CommandResult(String.format("Flight id %s not found. ", flightId));
+            for (Map.Entry<FlightLocationType, Location> entry : locations.entrySet()) {
+                flight.locationLink.delete(entry.getKey(), entry.getValue());
+            }
+        } catch (LinkException e) {
+            throw new CommandException(e.getMessage());
         }
-
-        model.unlinkFlightToLocations(flightToUnlink);
-        return new CommandResult(
-                String.format("Unlink departure and arrival locations from flight %s", flightId)
-        );
+        return new CommandResult(this.toString());
     }
 }
--- src/main/java/seedu/address/logic/location/unlinklocation/UnlinkLocationCommandFactory.java
+++ src/main/java/seedu/address/logic/location/unlinklocation/UnlinkLocationCommandFactory.java
@@ -1,18 +1,87 @@
 package seedu.address.logic.location.unlinklocation;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 
+import seedu.address.commons.fp.Lazy;
+import seedu.address.commons.util.GetUtil;
 import seedu.address.logic.core.CommandFactory;
 import seedu.address.logic.core.CommandParam;
 import seedu.address.logic.core.exceptions.ParseException;
+import seedu.address.logic.location.linklocation.LinkLocationCommand;
+import seedu.address.model.Model;
+import seedu.address.model.ReadOnlyItemManager;
+import seedu.address.model.flight.Flight;
+import seedu.address.model.location.FlightLocationType;
+import seedu.address.model.location.Location;
 
 /**
- * The factory that createsw {@code UnlinkLocationCommand}.
+ * The factory that creates {@code UnlinkLocationCommand}.
  */
 public class UnlinkLocationCommandFactory implements CommandFactory<UnlinkLocationCommand> {
     public static final String COMMAND_WORD = "unlink";
-    public static final String PREFIX_FLIGHT = "/flight";
+    public static final String FLIGHT_PREFIX = "/flight";
+    public static final String LOCATION_DEPARTURE_PREFIX = "/from";
+    public static final String LOCATION_ARRIVAL_PREFIX = "/to";
+    private static final String NO_LOCATION_MESSAGE =
+            "No location has been entered. Please enter /from for the departure location"
+                    + " and /to for the arrival location.";
+    private static final String NO_FLIGHT_MESSAGE =
+            "No flight has been entered. Please enter /flight for the flight.";
+
+    private final Lazy<ReadOnlyItemManager<Location>> locationManagerLazy;
+
+    private final Lazy<ReadOnlyItemManager<Flight>> flightManagerLazy;
+
+    /**
+     * Creates a new unlink command factory with the model registered.
+     */
+    public UnlinkLocationCommandFactory() {
+        this(GetUtil.getLazy(Model.class));
+    }
+
+    /**
+     * Creates a new unlink command factory with the given modelLazy.
+     *
+     * @param modelLazy the modelLazy used for the creation of the link command factory.
+     */
+    public UnlinkLocationCommandFactory(Lazy<Model> modelLazy) {
+        this(
+                modelLazy.map(Model::getLocationManager),
+                modelLazy.map(Model::getFlightManager)
+        );
+    }
+
+    /**
+     * Creates a new unlink location command factory with the given location manager
+     * lazy and the flight manager lazy.
+     *
+     * @param locationManagerLazy  the lazy instance of the location manager.
+     * @param flightManagerLazy the lazy instance of the flight manager.
+     */
+    public UnlinkLocationCommandFactory(
+            Lazy<ReadOnlyItemManager<Location>> locationManagerLazy,
+            Lazy<ReadOnlyItemManager<Flight>> flightManagerLazy
+    ) {
+        this.locationManagerLazy = locationManagerLazy;
+        this.flightManagerLazy = flightManagerLazy;
+    }
+
+    /**
+     * Creates a new unlink location command factory with the given location manager
+     * and the flight manager.
+     *
+     * @param locationManager  the location manager.
+     * @param flightManager the flight manager.
+     */
+    public UnlinkLocationCommandFactory(
+            ReadOnlyItemManager<Location> locationManager,
+            ReadOnlyItemManager<Flight> flightManager
+    ) {
+        this(Lazy.of(locationManager), Lazy.of(flightManager));
+    }
 
     @Override
     public String getCommandWord() {
@@ -21,12 +90,68 @@ public class UnlinkLocationCommandFactory implements CommandFactory<UnlinkLocati
 
     @Override
     public Optional<Set<String>> getPrefixes() {
-        return Optional.of(Set.of(PREFIX_FLIGHT));
+        return Optional.of(Set.of(
+                FLIGHT_PREFIX,
+                LOCATION_DEPARTURE_PREFIX,
+                LOCATION_ARRIVAL_PREFIX
+        ));
     }
+
+    private boolean addLocation(
+            Optional<String> locationIdOptional,
+            FlightLocationType type,
+            Map<FlightLocationType, Location> target
+    ) {
+        if (locationIdOptional.isEmpty()) {
+            return false;
+        }
+        Optional<Location> locationOptional =
+                locationManagerLazy.get().getItem(locationIdOptional.get());
+        if (locationOptional.isEmpty()) {
+            return false;
+        }
+        target.put(type, locationOptional.get());
+        return true;
+    }
+
+    private Flight getFlightOrThrow(
+            Optional<String> flightIdOptional
+    ) throws ParseException {
+        if (flightIdOptional.isEmpty()) {
+            throw new ParseException(NO_FLIGHT_MESSAGE);
+        }
+        Optional<Flight> flightOptional =
+                flightManagerLazy.get().getItem(flightIdOptional.get());
+        if (flightOptional.isEmpty()) {
+            throw new ParseException(NO_FLIGHT_MESSAGE);
+        }
+        return flightOptional.get();
+    }
+
 
     @Override
     public UnlinkLocationCommand createCommand(CommandParam param) throws ParseException {
-        final String flightId = param.getNamedValuesOrThrow(PREFIX_FLIGHT);
-        return new UnlinkLocationCommand(flightId);
+        Optional<String> locationDepartureIdOptional =
+                param.getNamedValues(LOCATION_DEPARTURE_PREFIX);
+        Optional<String> locationArrivalIdOptional =
+                param.getNamedValues(LOCATION_ARRIVAL_PREFIX);
+        Map<FlightLocationType, Location> locations = new HashMap<>();
+
+        boolean hasFoundLocation = addLocation(
+                locationDepartureIdOptional,
+                FlightLocationType.LOCATION_DEPARTURE,
+                locations
+        ) || addLocation(
+                locationArrivalIdOptional,
+                FlightLocationType.LOCATION_ARRIVAL,
+                locations
+        );
+
+        if (!hasFoundLocation) {
+            throw new ParseException(NO_LOCATION_MESSAGE);
+        }
+
+        Flight flight = getFlightOrThrow(param.getNamedValues(FLIGHT_PREFIX));
+        return new UnlinkLocationCommand(locations, flight);
     }
 }
