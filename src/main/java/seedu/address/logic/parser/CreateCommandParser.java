59934e8115a87d3e8d5075b7ce967360660321ef
Wang <shaofeng1234567@gmail.com>
2023-03-20 00:31:09 +0800

    Change the way parse take in values, Requires Start init with doctor object.

---
+++ src/main/java/seedu/address/logic/parser/CreateCommandParser.java
@@ -0,0 +1,15 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.CreateCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * The type Create command parser.
+ */
+public class CreateCommandParser implements Parser<CreateCommand> {
+
+    @Override
+    public CreateCommand parse(String userInput) throws ParseException {
+        return null;
+    }
+}
