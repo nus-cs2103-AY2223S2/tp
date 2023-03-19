a0be49b922fc6eb3a7c51c434645101e2c99d0ce
Wei Jie <ongweijie7@gmail.com>
2023-03-19 23:15:24 +0800

    Edit the format of Find command to match add command

--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -2,14 +2,10 @@ package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.*;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 import java.util.Arrays;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
-import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.*;
@@ -18,41 +14,40 @@ import seedu.address.model.person.*;
  * Parses input arguments and creates a new FindCommand object
  */
 public class FindCommandParser implements Parser<FindCommand> {
-    private static final Pattern BASIC_COMMAND_FORMAT = Pattern.compile("(?<attribute>\\S+)(?<keywords>.*)");
     /**
      * Parses the given {@code String} of arguments in the context of the FindCommand
      * and returns a FindCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
     public FindCommand parse(String args) throws ParseException {
-        Nric nric;
-        Name name;
-        Address address;
+        String nric;
+        String name;
+        String address;
         ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenize(args, PREFIX_NRIC, PREFIX_NAME, PREFIX_ADDRESS);
 
         if (isPrefixesPresent(argMultimap, PREFIX_NRIC)
                 && argMultimap.getPreamble().isEmpty()) {
-            nric = ParserUtil.parseNric(argMultimap.getValue(PREFIX_NRIC).get());
-            String[] nrics = nric.toString().split("\\s+");
+            nric = argMultimap.getValue(PREFIX_NRIC).get();
+            String[] nrics = nric.split("\\s+");
             return new FindCommand(new NricContainsKeywordsPredicate(Arrays.asList(nrics)));
 
         } else if (isPrefixesPresent(argMultimap, PREFIX_NAME)
                 && argMultimap.getPreamble().isEmpty())  {
-            name = ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get());
-            String[] names = name.toString().split("\\s+");
+            name = argMultimap.getValue(PREFIX_NAME).get();
+            String[] names = name.split("\\s+");
             return new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList(names)));
 
         } else if (isPrefixesPresent(argMultimap, PREFIX_ADDRESS)
                 && argMultimap.getPreamble().isEmpty()) {
-            address = ParserUtil.parseAddress(argMultimap.getValue(PREFIX_ADDRESS).get());
-            String[] addresses = address.toString().split("\\s+");
+            address = argMultimap.getValue(PREFIX_ADDRESS).get();
+            String[] addresses = address.split("\\s+");
             return new FindCommand(new AddressContainsKeywordsPredicate(Arrays.asList(addresses)));
         }
         else {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                                                        FindCommand.MESSAGE_USAGE));
         }
-
     }
 
     /**
