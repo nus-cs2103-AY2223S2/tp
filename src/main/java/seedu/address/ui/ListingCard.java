25d69feceec06f00e4df82528d635688fe2a464f
yyj-02 <yeohyongjie@outlook.com>
2023-03-19 23:04:04 +0800

    Remove unused imports:

--- src/main/java/seedu/address/ui/ListingCard.java
+++ src/main/java/seedu/address/ui/ListingCard.java
@@ -1,8 +1,6 @@
 package seedu.address.ui;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Stream;
