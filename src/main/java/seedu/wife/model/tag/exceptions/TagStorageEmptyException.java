9d754d918ca4b4e55c2d64ba4c99d703f28a6dff
ScorpiusSigma <justinleng_@hotmail.com>
2023-03-19 21:41:41 +0800

    Fix checkstyle

--- src/main/java/seedu/wife/model/tag/exceptions/TagStorageEmptyException.java
+++ src/main/java/seedu/wife/model/tag/exceptions/TagStorageEmptyException.java
@@ -1,11 +1,14 @@
 package seedu.wife.model.tag.exceptions;
 
+/**
+ * Signals that the operation is unable to execute on empty tag storage.
+ */
 public class TagStorageEmptyException extends RuntimeException {
 
-  /**
-   * Constructor
-   */
-  public TagStorageEmptyException() {
-    super("Tag storage is empty.");
-  }
+    /**
+     * Constructor
+     */
+    public TagStorageEmptyException() {
+        super("Tag storage is empty.");
+    }
 }
