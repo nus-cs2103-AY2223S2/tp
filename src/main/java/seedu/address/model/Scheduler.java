82ff75027ca07c8a82631edefca0384fa65912ac
JrmCkh <Hhelloogkart@gmail.com>
2023-03-19 15:37:36 +0800

    Update checkstyle

--- src/main/java/seedu/address/model/Scheduler.java
+++ src/main/java/seedu/address/model/Scheduler.java
@@ -22,7 +22,8 @@ public class Scheduler implements ReadOnlyScheduler {
      *
      * Note that non-static init blocks are not recommended to use. There are other ways to avoid duplication
      *   among constructors.
-     */ {
+     */
+    {
         events = new UniqueEventList();
     }
 
--- src/main/java/seedu/address/storage/JsonSerializableScheduler.java
+++ src/main/java/seedu/address/storage/JsonSerializableScheduler.java
@@ -9,8 +9,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonRootName;
 
 import seedu.address.commons.exceptions.IllegalValueException;
-import seedu.address.model.Scheduler;
 import seedu.address.model.ReadOnlyScheduler;
+import seedu.address.model.Scheduler;
 import seedu.address.model.event.Event;
 
 /**
