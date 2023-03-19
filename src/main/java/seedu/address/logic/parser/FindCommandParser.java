f8fb66664f8dc26ce59ad25c3f9d7d2b2a82ea11
Wei Jie <ongweijie7@gmail.com>
2023-03-19 20:41:13 +0800

    Edited the synax for Find command and remove Find /all command.

--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -1,16 +1,18 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.*;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
+import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.person.AddressContainsKeywordsPredicate;
-import seedu.address.model.person.NameContainsKeywordsPredicate;
-import seedu.address.model.person.NricContainsKeywordsPredicate;
+import seedu.address.model.person.*;
 
 /**
  * Parses input arguments and creates a new FindCommand object
@@ -23,38 +25,42 @@ public class FindCommandParser implements Parser<FindCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public FindCommand parse(String args) throws ParseException {
-        args = args.replace("\n", "").trim();
-        final Matcher matcher = BASIC_COMMAND_FORMAT.matcher(args);
+        Nric nric;
+        Name name;
+        Address address;
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_NRIC, PREFIX_NAME, PREFIX_ADDRESS);
 
-        if (!matcher.matches()) {
-            throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+        if (isPrefixesPresent(argMultimap, PREFIX_NRIC)
+                && argMultimap.getPreamble().isEmpty()) {
+            nric = ParserUtil.parseNric(argMultimap.getValue(PREFIX_NRIC).get());
+            String[] nrics = nric.toString().split("\\s+");
+            return new FindCommand(new NricContainsKeywordsPredicate(Arrays.asList(nrics)));
+
+        } else if (isPrefixesPresent(argMultimap, PREFIX_NAME)
+                && argMultimap.getPreamble().isEmpty())  {
+            name = ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get());
+            String[] names = name.toString().split("\\s+");
+            return new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList(names)));
+
+        } else if (isPrefixesPresent(argMultimap, PREFIX_ADDRESS)
+                && argMultimap.getPreamble().isEmpty()) {
+            address = ParserUtil.parseAddress(argMultimap.getValue(PREFIX_ADDRESS).get());
+            String[] addresses = address.toString().split("\\s+");
+            return new FindCommand(new AddressContainsKeywordsPredicate(Arrays.asList(addresses)));
         }
-
-        final String attribute = matcher.group("attribute").trim();
-        final String trimmedKeywords = matcher.group("keywords").trim();
-
-        if (trimmedKeywords.isEmpty()) {
-            if (args.equals("/all")) {
-                return new FindCommand(null);
-            } else {
-                throw new ParseException(
-                        String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
-            }
-        }
-
-        String[] keywords = trimmedKeywords.split("\\s+");
-
-        if (attribute.equals("/a")) {
-            return new FindCommand(new AddressContainsKeywordsPredicate(Arrays.asList(keywords)));
-        } else if (attribute.equals("/n")) {
-            return new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList(keywords)));
-        } else if (attribute.equals("/nric")) {
-            return new FindCommand(new NricContainsKeywordsPredicate(Arrays.asList(keywords)));
-        } else {
+        else {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
         }
 
+    }
+
+    /**
+     * Returns true if the given prefixes does not contain empty
+     *  {@code Optional} values in the given {@code ArgumentMultimap}.
+     */
+    private static boolean isPrefixesPresent(ArgumentMultimap argumentMultimap, Prefix prefixes) {
+        return Stream.of(prefixes).anyMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
     }
 
 }
