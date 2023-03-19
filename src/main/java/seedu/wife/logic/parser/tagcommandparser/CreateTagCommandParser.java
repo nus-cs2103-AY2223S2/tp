542c6987000665d47be797d71b56428a062fdc8a
ScorpiusSigma <justinleng_@hotmail.com>
2023-03-19 20:49:21 +0800

    Change name to CreateTagCommandParser

--- src/main/java/seedu/wife/logic/parser/tagcommandparser/CreateTagCommandParser.java
+++ src/main/java/seedu/wife/logic/parser/tagcommandparser/CreateTagCommandParser.java
@@ -3,11 +3,8 @@ package seedu.wife.logic.parser.tagcommandparser;
 import static java.util.Objects.requireNonNull;
 import static seedu.wife.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.wife.logic.parser.CliSyntax.PREFIX_NAME;
-import static seedu.wife.logic.parser.CliSyntax.PREFIX_TAG;
 
-import java.util.Set;
-
-import seedu.wife.logic.commands.tagcommands.AddTagCommand;
+import seedu.wife.logic.commands.tagcommands.CreateTagCommand;
 import seedu.wife.logic.parser.ArgumentMultimap;
 import seedu.wife.logic.parser.ArgumentTokenizer;
 import seedu.wife.logic.parser.Parser;
@@ -17,25 +14,24 @@ import seedu.wife.model.tag.Tag;
 import seedu.wife.model.tag.TagName;
 
 /**
- * Parses input arguments and create a new AddTagCommand object
+ * Parses input arguments and create a new CreateTagCommand object
  */
-public class AddTagCommandParser implements Parser<AddTagCommand> {
+public class CreateTagCommandParser implements Parser<CreateTagCommand> {
     @Override
-    public AddTagCommand parse(String args) throws ParseException {
+    public CreateTagCommand parse(String args) throws ParseException {
         requireNonNull(args);
         ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenize(args, PREFIX_NAME);
 
         if (!ParserUtil.arePrefixesPresent(argMultimap, PREFIX_NAME)
                 || !argMultimap.getPreamble().isEmpty()) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddTagCommand.MESSAGE_USAGE));
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, CreateTagCommand.MESSAGE_USAGE));
         }
 
         TagName tagName = ParserUtil.parseTagName(argMultimap.getValue(PREFIX_NAME).get());
-        Set<Tag> tagList = ParserUtil.parseTags(argMultimap.getAllValues(PREFIX_TAG));
 
         Tag tag = new Tag(tagName.toString());
 
-        return new AddTagCommand(tag);
+        return new CreateTagCommand(tag);
     }
 }
