1d28e492a3c2c966c7b90e472895dcc4bb5ca87c
kennethk-1201 <kiangjiaming@gmail.com>
2023-03-19 23:36:38 +0800

    Fix checkstyle

--- src/test/java/seedu/sudohr/logic/parser/department/RemoveEmployeeFromDepartmentCommandParserTest.java
+++ src/test/java/seedu/sudohr/logic/parser/department/RemoveEmployeeFromDepartmentCommandParserTest.java
@@ -1,7 +1,12 @@
 package seedu.sudohr.logic.parser.department;
 
 import static seedu.sudohr.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.sudohr.logic.commands.CommandTestUtil.*;
+import static seedu.sudohr.logic.commands.CommandTestUtil.DEPARTMENT_NAME_DESC_ENGINEERING;
+import static seedu.sudohr.logic.commands.CommandTestUtil.ID_DESC_AMY;
+import static seedu.sudohr.logic.commands.CommandTestUtil.INVALID_DEPARTMENT_NAME_DESC;
+import static seedu.sudohr.logic.commands.CommandTestUtil.INVALID_ID_DESC;
+import static seedu.sudohr.logic.commands.CommandTestUtil.NAME_DESC_AMY;
+import static seedu.sudohr.logic.commands.CommandTestUtil.PREAMBLE_WHITESPACE;
 import static seedu.sudohr.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.sudohr.logic.parser.CommandParserTestUtil.assertParseSuccess;
 
