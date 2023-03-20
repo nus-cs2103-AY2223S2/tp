0b12ae3ff5b682700a22f6fce7d4d1761bdb51bb
John Chia Heng Kit <programmingwithjohn@gmail.com>
2023-03-21 01:59:15 +0800

    Update tests in JsonSeralizableAddressBook and StorageManagerTest
    JSON files in test/data were created

--- src/test/java/arb/storage/JsonSerializableAddressBookTest.java
+++ src/test/java/arb/storage/JsonSerializableAddressBookTest.java
@@ -11,7 +11,9 @@ import org.junit.jupiter.api.Test;
 import arb.commons.exceptions.IllegalValueException;
 import arb.commons.util.JsonUtil;
 import arb.model.AddressBook;
+import arb.testutil.TypicalAddressBook;
 import arb.testutil.TypicalClients;
+import arb.testutil.TypicalProjects;
 
 public class JsonSerializableAddressBookTest {
 
@@ -19,7 +21,11 @@ public class JsonSerializableAddressBookTest {
     private static final Path TYPICAL_CLIENTS_FILE = TEST_DATA_FOLDER.resolve("typicalClientsAddressBook.json");
     private static final Path INVALID_CLIENT_FILE = TEST_DATA_FOLDER.resolve("invalidClientAddressBook.json");
     private static final Path DUPLICATE_CLIENT_FILE = TEST_DATA_FOLDER.resolve("duplicateClientAddressBook.json");
-
+    private static final Path TYPICAL_PROJECT_FILE = TEST_DATA_FOLDER.resolve("typicalProjectsAddressBook.json");
+    private static final Path INVALID_PROJECT_FILE = TEST_DATA_FOLDER.resolve("invalidProjectAddressBook.json");
+    private static final Path DUPLICATE_PROJECT_FILE = TEST_DATA_FOLDER.resolve("duplicateProjectAddressBook.json");
+    private static final Path TYPICAL_CLIENT_AND_PROJECT_FILE =
+            TEST_DATA_FOLDER.resolve("typicalClientsAndProjectsAddressBook.json");
     @Test
     public void toModelType_typicalClientsFile_success() throws Exception {
         JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(TYPICAL_CLIENTS_FILE,
@@ -30,12 +36,34 @@ public class JsonSerializableAddressBookTest {
     }
 
     @Test
+    public void toModelType_typicalProjectsFile_success() throws Exception {
+        JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(TYPICAL_PROJECT_FILE,
+                JsonSerializableAddressBook.class).get();
+        AddressBook addressBookFromFile = dataFromFile.toModelType();
+        AddressBook typicalProjectsAddressBook = TypicalProjects.getTypicalAddressBook();
+        assertEquals(addressBookFromFile, typicalProjectsAddressBook);
+    }
+
+    @Test
+    public void toModelType_typicalClientsAndProjectsFile_success() throws Exception {
+        JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(TYPICAL_CLIENT_AND_PROJECT_FILE,
+                JsonSerializableAddressBook.class).get();
+        AddressBook addressBookFromFile = dataFromFile.toModelType();
+        AddressBook typicalProjectsAddressBook = TypicalAddressBook.getTypicalAddressBook();
+        assertEquals(addressBookFromFile, typicalProjectsAddressBook);
+    }
+    @Test
     public void toModelType_invalidClientFile_throwsIllegalValueException() throws Exception {
         JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(INVALID_CLIENT_FILE,
                 JsonSerializableAddressBook.class).get();
         assertThrows(IllegalValueException.class, dataFromFile::toModelType);
     }
-
+    @Test
+    public void toModelType_invalidProjectFile_throwsIllegalValueException() throws Exception {
+        JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(INVALID_PROJECT_FILE,
+                JsonSerializableAddressBook.class).get();
+        assertThrows(IllegalValueException.class, dataFromFile::toModelType);
+    }
     @Test
     public void toModelType_duplicateClients_throwsIllegalValueException() throws Exception {
         JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(DUPLICATE_CLIENT_FILE,
@@ -43,5 +71,11 @@ public class JsonSerializableAddressBookTest {
         assertThrows(IllegalValueException.class, JsonSerializableAddressBook.MESSAGE_DUPLICATE_CLIENT,
                 dataFromFile::toModelType);
     }
-
+    @Test
+    public void toModelType_duplicateProjects_throwsIllegalValueException() throws Exception {
+        JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(DUPLICATE_PROJECT_FILE,
+                JsonSerializableAddressBook.class).get();
+        assertThrows(IllegalValueException.class, JsonSerializableAddressBook.MESSAGE_DUPLICATE_PROJECT,
+                dataFromFile::toModelType);
+    }
 }
