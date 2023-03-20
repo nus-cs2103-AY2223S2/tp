906be7e86c1d3c4b559ccd8218fe0e4fb589d9e3
XylusChen <chenxinyang00@gmail.com>
2023-03-20 17:25:25 +0800

    Add clear applications command

--- src/main/java/seedu/address/logic/commands/ClearApplicationCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearApplicationCommand.java
@@ -2,22 +2,22 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
-import seedu.address.model.AddressBook;
-import seedu.address.model.Model;
+import seedu.address.model.ApplicationModel;
+import seedu.address.model.InternshipBook;
+
 
 /**
  * Clears the address book.
  */
-public class ClearCommand extends Command {
+public class ClearApplicationCommand extends ApplicationCommand {
 
     public static final String COMMAND_WORD = "clear";
-    public static final String MESSAGE_SUCCESS = "Address book has been cleared!";
-
+    public static final String MESSAGE_SUCCESS = "Internship Book has been cleared!";
 
     @Override
-    public CommandResult execute(Model model) {
+    public CommandResult execute(ApplicationModel model) {
         requireNonNull(model);
-        model.setAddressBook(new AddressBook());
+        model.setInternshipBook(new InternshipBook());
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
