403ff6ee33a5760f923c32a67b6b06777c6768d2
Shuwen <chenshuwen0210@gmail.com>
2023-03-20 16:09:44 +0800

    add unit test for add and clear patient command

--- src/test/java/seedu/careflow/logic/commands/patientcommands/AddCommandTest.java
+++ src/test/java/seedu/careflow/logic/commands/patientcommands/AddCommandTest.java
@@ -1,14 +1,253 @@
 package seedu.careflow.logic.commands.patientcommands;
 
+import static java.util.Objects.requireNonNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.careflow.testutil.Assert.assertThrows;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.function.Predicate;
+
 import org.junit.jupiter.api.Test;
+
+import javafx.collections.ObservableList;
+import seedu.careflow.commons.core.GuiSettings;
+import seedu.careflow.logic.commands.CommandResult;
+import seedu.careflow.logic.commands.exceptions.CommandException;
+import seedu.careflow.model.CareFlowModel;
+import seedu.careflow.model.PatientRecord;
+import seedu.careflow.model.ReadOnlyUserPrefs;
+import seedu.careflow.model.drug.Drug;
+import seedu.careflow.model.hospital.Hospital;
+import seedu.careflow.model.person.Patient;
+import seedu.careflow.model.readonly.ReadOnlyDrugInventory;
+import seedu.careflow.model.readonly.ReadOnlyHospitalRecords;
+import seedu.careflow.model.readonly.ReadOnlyPatientRecord;
+import seedu.careflow.testutil.PatientBuilder;
 
 class AddCommandTest {
 
     @Test
-    void testEquals() {
+    public void constructor_nullPatient_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new AddCommand(null));
     }
 
     @Test
-    void execute() {
+    public void execute_patientAcceptedByModel_addSuccessful() throws Exception {
+        ModelStubAcceptingPatientAdded modelStub = new ModelStubAcceptingPatientAdded();
+        Patient  validPatient  = new PatientBuilder().build();
+
+        CommandResult commandResult = new AddCommand(validPatient).execute(modelStub);
+
+        assertEquals(String.format(AddCommand.MESSAGE_SUCCESS, validPatient), commandResult.getFeedbackToUser());
+        assertEquals(Arrays.asList(validPatient), modelStub.patientsAdded);
+    }
+    @Test
+    public void execute_duplicatePatient_throwsCommandException() {
+        Patient validPatient = new PatientBuilder().build();
+        AddCommand addCommand = new AddCommand(validPatient);
+        ModelStub modelStub = new ModelStubWithPatient(validPatient);
+
+        assertThrows(CommandException.class, AddCommand.MESSAGE_DUPLICATE_PATIENT, () -> addCommand.execute(modelStub));
+    }
+
+    @Test
+    public void equals() {
+        Patient alice = new PatientBuilder().withName("Alice").build();
+        Patient bob = new PatientBuilder().withName("Bob").build();
+        AddCommand addAliceCommand = new AddCommand(alice);
+        AddCommand addBobCommand = new AddCommand(bob);
+
+        // same object -> returns true
+        assertTrue(addAliceCommand.equals(addAliceCommand));
+
+        // same values -> returns true
+        AddCommand addAliceCommandCopy = new AddCommand(alice);
+        assertTrue(addAliceCommand.equals(addAliceCommandCopy));
+
+        // different types -> returns false
+        assertFalse(addAliceCommand.equals(1));
+
+        // null -> returns false
+        assertFalse(addAliceCommand.equals(null));
+
+        // different person -> returns false
+        assertFalse(addAliceCommand.equals(addBobCommand));
+    }
+
+    private class ModelStub implements CareFlowModel {
+        @Override
+        public void setUserPrefs(ReadOnlyUserPrefs userPrefs) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ReadOnlyUserPrefs getUserPrefs() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public GuiSettings getGuiSettings() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setGuiSettings(GuiSettings guiSettings) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public Path getPatientRecordFilePath() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public Path getDrugInventoryFilePath() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setPatientRecordFilePath(Path patientRecordFilePath) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setDrugInventoryFilePath(Path drugInventoryFilePath) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setPatientRecord(ReadOnlyPatientRecord patientRecord) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setDrugInventory(ReadOnlyDrugInventory drugInventory) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ReadOnlyPatientRecord getPatientRecord() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ReadOnlyDrugInventory getDrugInventory() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ReadOnlyHospitalRecords getHospitalRecords() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public boolean hasPatient(Patient patient) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public boolean hasDrug(Drug drug) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void deletePatient(Patient target) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void deleteDrug(Drug target) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void addPatient(Patient patient) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void addDrug(Drug drug) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setPatient(Patient target, Patient editedPatient) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setDrug(Drug target, Drug editedDrug) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ObservableList<Patient> getFilteredPatientList() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ObservableList<Drug> getFilteredDrugList() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ObservableList<Hospital> getHospitalList() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void updateFilteredPatientList(Predicate<Patient> predicate) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void updateFilteredDrugList(Predicate<Drug> predicate) {
+            throw new AssertionError("This method should not be called.");
+        }
+    }
+
+    /**
+     * A Model stub that contains a single patient.
+     */
+    private class ModelStubWithPatient extends ModelStub {
+        private final Patient patient;
+
+        ModelStubWithPatient(Patient patient) {
+            requireNonNull(patient);
+            this.patient = patient;
+        }
+
+        @Override
+        public boolean hasPatient (Patient patient) {
+            requireNonNull(patient);
+            return this.patient.isSamePatient(patient);
+        }
+    }
+
+    /**
+     * A Model stub that always accept the patient being added.
+     */
+    private class ModelStubAcceptingPatientAdded extends ModelStub {
+        final ArrayList<Patient> patientsAdded = new ArrayList<>();
+
+        @Override
+        public boolean hasPatient (Patient patient) {
+            requireNonNull(patient);
+            return patientsAdded.stream().anyMatch(patient::isSamePatient );
+        }
+
+        @Override
+        public void addPatient (Patient patient) {
+            requireNonNull(patient);patientsAdded.add(patient);
+        }
+
+        @Override
+        public ReadOnlyPatientRecord getPatientRecord() {
+            return new PatientRecord();
+        }
     }
 }
--- src/test/java/seedu/careflow/logic/commands/patientcommands/ClearCommandTest.java
+++ src/test/java/seedu/careflow/logic/commands/patientcommands/ClearCommandTest.java
@@ -1,10 +1,35 @@
 package seedu.careflow.logic.commands.patientcommands;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.careflow.logic.commands.patientcommands.CommandTestUtil.assertCommandSuccess;
+import static seedu.careflow.testutil.TypicalDrugs.getTypicalDrugInventory;
+import static seedu.careflow.testutil.TypicalPatients.getTypicalPatientRecord;
+
+
 import org.junit.jupiter.api.Test;
 
-class ClearCommandTest {
+import seedu.careflow.model.CareFlowModel;
+import seedu.careflow.model.CareFlowModelManager;
+import seedu.careflow.model.PatientRecord;
+import seedu.careflow.model.UserPrefs;
+
+public class ClearCommandTest {
 
     @Test
-    void execute() {
+    public void execute_emptyCareFlowPatientRecord_success() {
+        CareFlowModel model = new CareFlowModelManager();
+        CareFlowModel expectedCareFlowModel = new CareFlowModelManager();
+        assertCommandSuccess(new ClearCommand(), model, ClearCommand.MESSAGE_SUCCESS, expectedCareFlowModel);
     }
+
+    @Test
+    public void execute_nonEmptyAddressBook_success() {
+        CareFlowModel model = new CareFlowModelManager(getTypicalPatientRecord(), getTypicalDrugInventory(), new UserPrefs());
+        CareFlowModel expectedCareFlowModel = new CareFlowModelManager(getTypicalPatientRecord(),
+                getTypicalDrugInventory(), new UserPrefs());
+        expectedCareFlowModel.setPatientRecord(new PatientRecord());
+
+        assertCommandSuccess(new ClearCommand(), model, ClearCommand.MESSAGE_SUCCESS, expectedCareFlowModel);
+    }
+
 }
