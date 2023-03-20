619df553dd3bcdae9d651d3b82018b7332444162
Gloria Zhang Xinhui <glozxi@gmail.com>
2023-03-20 22:00:00 +0800

    Fix checkstyle

--- src/test/java/seedu/address/model/event/StartTimeWithinDaysPredicateTest.java
+++ src/test/java/seedu/address/model/event/StartTimeWithinDaysPredicateTest.java
@@ -4,10 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.TypicalLocalDateTimes.TIME_NOW;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 import org.junit.jupiter.api.Test;
 
 import seedu.address.testutil.EventBuilder;
