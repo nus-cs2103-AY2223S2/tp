5ea80c17bcdf2a75a883b58318029ed579f25f36
ruiyigan <85063215+ruiyigan@users.noreply.github.com>
2023-03-20 18:00:22 +0800

    Save changes

---
+++ src/main/java/seedu/address/logic/commands/TagCardDuringReviewCommand.java
@@ -0,0 +1,26 @@
+package seedu.address.logic.commands;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+
+import static java.util.Objects.requireNonNull;
+
+public class TagCardDuringReviewCommand extends Command {
+    public static final String COMMAND_WORD = "tag";
+
+    public static final String MESSAGE_SUCCESS = "Card is tagged with %1$s!";
+
+    private final String tagName;
+
+    public TagCardDuringReviewCommand(String tagName) {
+        requireNonNull(tagName);
+        this.tagName = tagName;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        model.tagCurrentCardInReview(this.tagName);
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+}
---
+++ src/main/java/seedu/address/logic/parser/TagCardDuringReviewCommandParser.java
@@ -0,0 +1,11 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.TagCardDuringReviewCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+public class TagCardDuringReviewCommandParser implements Parser<TagCardDuringReviewCommand> {
+
+    public TagCardDuringReviewCommand parse(String args) throws ParseException {
+
+    }
+}
