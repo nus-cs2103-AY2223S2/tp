6bd2c48d0c6473a31903fdf13159e266396a344f
TariqDroid <vegita2009@hotmail.com>
2023-03-20 15:23:34 +0800

    Modify FindCommand Modified find command such that it can search for names as well as type. Moreover, it can now search for substrings. example: find CS3263 Or find tutorial remember to use list command afterwards to get back to the original list

--- src/main/java/seedu/address/model/module/NameContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/module/NameContainsKeywordsPredicate.java
@@ -6,7 +6,7 @@ import java.util.function.Predicate;
 import seedu.address.commons.util.StringUtil;
 
 /**
- * Tests that a {@code Module}'s {@code Name} matches any of the keywords given.
+ * Tests that a {@code Module}'s {@code Name} {@code Tag} matches any of the keywords given.
  */
 public class NameContainsKeywordsPredicate implements Predicate<Module> {
     private final List<String> keywords;
@@ -18,7 +18,13 @@ public class NameContainsKeywordsPredicate implements Predicate<Module> {
     @Override
     public boolean test(Module module) {
         return keywords.stream()
-                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(module.getName().fullName, keyword));
+                .anyMatch(keyword -> containsSubstringIgnoreCase(module.getName().fullName, keyword)
+                        || module.getTags().stream()
+                        .anyMatch(tag -> containsSubstringIgnoreCase(tag.tagName, keyword)));
+    }
+
+    private boolean containsSubstringIgnoreCase(String text, String substring) {
+        return text.toLowerCase().contains(substring.toLowerCase());
     }
 
     @Override
