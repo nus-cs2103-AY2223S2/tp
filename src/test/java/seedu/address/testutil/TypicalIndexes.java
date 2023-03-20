7b7ddc3c637fcc1494e21af7170404bc02d09f47
yyj-02 <yeohyongjie@outlook.com>
2023-03-20 15:05:43 +0800

    Add first test case

--- src/test/java/seedu/address/testutil/TypicalIndexes.java
+++ src/test/java/seedu/address/testutil/TypicalIndexes.java
@@ -1,6 +1,8 @@
 package seedu.address.testutil;
 
+import java.util.ArrayList;
 import seedu.address.commons.core.index.Index;
+import seedu.address.model.listing.Listing;
 
 /**
  * A utility class containing a list of {@code Index} objects to be used in tests.
@@ -9,4 +11,7 @@ public class TypicalIndexes {
     public static final Index INDEX_FIRST_LISTING = Index.fromOneBased(1);
     public static final Index INDEX_SECOND_LISTING = Index.fromOneBased(2);
     public static final Index INDEX_THIRD_LISTING = Index.fromOneBased(3);
+    public static final Index INDEX_LAST_LISTING (ArrayList<Listing> listing) {
+        return Index.fromOneBased(listing.size());
+    }
 }
