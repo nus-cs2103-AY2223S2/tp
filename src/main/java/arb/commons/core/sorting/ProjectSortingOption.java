04615a61e7a28a11b2b9b85924e29f6bc1f66812
zrei <88964793+zrei@users.noreply.github.com>
2023-03-20 10:57:17 +0800

    Implement aliases for sorting options

--- src/main/java/arb/commons/core/sorting/ProjectSortingOption.java
+++ src/main/java/arb/commons/core/sorting/ProjectSortingOption.java
@@ -3,39 +3,46 @@ package arb.commons.core.sorting;
 import static arb.model.Model.PROJECT_DEADLINE_COMPARATOR;
 import static arb.model.Model.PROJECT_TITLE_COMPARATOR;
 
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.EnumSet;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Set;
 
 import arb.model.project.Project;
 
 /** Represents a sorting option to sort the project list by. */
 public enum ProjectSortingOption {
-    DEADLINE("deadline", "Sorted all projects by deadline!", PROJECT_DEADLINE_COMPARATOR),
-    NAME("name", "Sorted all projects by name!", PROJECT_TITLE_COMPARATOR);
+    DEADLINE("deadline", "d", "Sorted all projects by deadline!", PROJECT_DEADLINE_COMPARATOR),
+    NAME("name", "n", "Sorted all projects by name!", PROJECT_TITLE_COMPARATOR);
 
     public static final String MESSAGE_CONSTRAINTS =
             "Sorting options should be either \'deadline\' or \'name\'";
     private static final EnumSet<ProjectSortingOption> sortingOptions = EnumSet.allOf(ProjectSortingOption.class);
 
-    private final String optionString;
+    private final Set<String> OPTION_STRINGS;
     private final String successMessage;
     private final Comparator<Project> comparator;
 
     /**
      * ProjectSortingOption can only be created by calling {@link ProjectSortingOption#getSortingOption(String)}.
      */
-    private ProjectSortingOption(String optionString, String successMessage, Comparator<Project> comparator) {
-        this.optionString = optionString;
+    private ProjectSortingOption(String optionString, String aliasOptionString, String successMessage, Comparator<Project> comparator) {
+        this.OPTION_STRINGS = new HashSet<>(Arrays.asList(optionString, aliasOptionString));
         this.successMessage = successMessage;
         this.comparator = comparator;
+    }
+
+    private boolean isOptionString(String optionString) {
+        return this.OPTION_STRINGS.contains(optionString);
     }
 
     /**
      * Returns if a given string is a valid project sorting option.
      */
     public static boolean isValidSortingOption(String optionString) {
-        return sortingOptions.stream().anyMatch(e -> e.optionString.equals(optionString));
+        return sortingOptions.stream().anyMatch(e -> e.isOptionString(optionString));
     }
 
     /**
@@ -45,7 +52,7 @@ public enum ProjectSortingOption {
         Iterator<ProjectSortingOption> iterator = sortingOptions.iterator();
         while (iterator.hasNext()) {
             ProjectSortingOption currentSortingOption = iterator.next();
-            if (currentSortingOption.optionString.equals(optionString)) {
+            if (currentSortingOption.isOptionString(optionString)) {
                 return currentSortingOption;
             }
         }
