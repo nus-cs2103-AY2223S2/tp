7e361b8250d3f344564fe0a8e309e820e6efbf25
DESKTOP-401ETA8\USER <adrielkee@gmail.com>
2023-03-20 22:18:55 +0800

    Add parser class for company

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -9,6 +9,7 @@ import java.util.regex.Pattern;
 import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.CompanyCommand;
 import seedu.address.logic.commands.DeadlineCommand;
 import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.commands.EditCommand;
@@ -75,6 +76,9 @@ public class AddressBookParser {
 
         case SalaryCommand.COMMAND_WORD:
             return new SalaryCommandParser().parse(arguments);
+
+        case CompanyCommand.COMMAND_WORD:
+            return new CompanyCommandParser().parse(arguments);
 
         default:
             throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
