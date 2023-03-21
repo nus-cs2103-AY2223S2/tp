ff48788b40c3afe64b19d222523561636843ffe5
ARPspoofing <mohdreyaaz99@gmail.com>
2023-03-21 09:37:34 +0800

    Change performance prefix from per to score

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -11,7 +11,7 @@ public class CliSyntax {
     public static final Prefix PREFIX_EMAIL = new Prefix("e/");
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
     public static final Prefix PREFIX_REMARK = new Prefix("r/");
-    public static final Prefix PREFIX_PERFORMANCE = new Prefix("per/");
+    public static final Prefix PREFIX_PERFORMANCE = new Prefix("score/");
     public static final Prefix PREFIX_PHOTO = new Prefix("photo/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
     public static final Prefix PREFIX_TUTORIAL = new Prefix("Tutorial/");
