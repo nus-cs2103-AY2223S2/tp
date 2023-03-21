3be60cea4f2f90740e09e4b7f3895c16501dce39
securespider <e0725031@u.nus.edu>
2023-03-22 03:17:31 +0800

    Added attendance command

--- src/main/java/seedu/address/model/person/student/Attendance.java
+++ src/main/java/seedu/address/model/person/student/Attendance.java
@@ -1,5 +1,7 @@
 package seedu.address.model.person.student;
 
+import java.time.LocalDate;
+
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
@@ -7,41 +9,77 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
  * Represents the Attendance of a Student object.
  */
 public class Attendance {
-    public static final String MESSAGE_CONSTRAINTS = "Attendance must be T or F";
+    public static final String MESSAGE_CONSTRAINTS = "Attendance must be a date in the format of dd/mm/yyyy";
 
-    /*
-     * The first character of the address must not be a whitespace,
-     * otherwise " " (a blank string) becomes a valid input.
-     */
-    public static final String VALIDATION_REGEX = "[TF]";
+    public static final String VALIDATION_REGEX = "\\d{2}/\\d{2}/\\d{4}";
+    public static final String VALIDATION_REGEX2 = "[TF]";
+    private String value;
+    private LocalDate date;
 
-    public final String value;
 
     /**
      * Constructs an {@code Address}.
      *
-     * @param attendance A valid student image file destination.
+     * @param value A string representation of a date
      */
-    public Attendance(String attendance) {
-        requireNonNull(attendance);
-        checkArgument(isValidAttendance(attendance), MESSAGE_CONSTRAINTS);
-        value = attendance;
+    public Attendance(String value) {
+        requireNonNull(value);
+        checkArgument(isValidAttendance(value), MESSAGE_CONSTRAINTS);
+        this.value = value;
+        if (value.equals("T")) {
+            this.date = LocalDate.now();
+        } else if (!value.equals("F")) {
+            this.date = LocalDate.parse(value, java.time.format.DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        }
     }
 
     /**
-     * Returns true if a given string is a valid email.
+     * Constructs an {@code Address}.
+     *
+     * @param value A LocalDate representation of an attendance
+     */
+    public Attendance(LocalDate value) {
+        requireNonNull(value);
+        this.value = value.format(java.time.format.DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        this.date = value;
+    }
+
+    /**
+     * Returns true if a given string is a valid date
      */
     public static boolean isValidAttendance(String test) {
+        // Default address
         if (test.equals("Insert student attendance here!")) {
             return true;
         }
 
-        return test.matches(VALIDATION_REGEX);
+        return test.matches(VALIDATION_REGEX) || test.matches(VALIDATION_REGEX2);
+    }
+
+    /**
+     * Returns true if the attendance is present on the given date.
+     * @param currDate The date to check against
+     * @return boolean value to indicate if the attendance is present on the given date.
+     */
+    public boolean isPresent(LocalDate currDate) {
+        if (value.equals("T"))
+            return true;
+        if (value.equals("F"))
+            return false;
+        return currDate.isEqual(date);
+    }
+
+    /**
+     * Notes that the student is absent
+     * @return Attendance object with value "F"
+     */
+    public boolean isAbsent() {
+        return value.equals("F");
     }
 
     @Override
     public String toString() {
-        return value;
+        return this.value;
     }
 
     @Override
