eccfe2df558a89f2743bac51dd2751cb18b19b3f
chia-yh <0.chiayuhong.0@gmail.com>
2023-03-21 20:22:11 +0800

    Resolve checkstyle errors

--- src/main/java/seedu/socket/model/project/Project.java
+++ src/main/java/seedu/socket/model/project/Project.java
@@ -69,7 +69,10 @@ public class Project {
     public ProjectDeadline getDeadline() {
         return deadline;
     }
-    public ProjectMeeting getMeeting() { return meeting; }
+
+    public ProjectMeeting getMeeting() {
+        return meeting;
+    }
 
     /**
      * Returns an immutable {@code Person} set, which throws {@code UnsupportedOperationException}
