daf941066e8722cf5748e355629c7380dea578dc
Tan Kai Min Russell <russelltanreg38@gmail.com>
2023-03-21 23:57:03 +0800

    Test git push for upstream branch

--- src/main/java/seedu/address/Main.java
+++ src/main/java/seedu/address/Main.java
@@ -21,6 +21,7 @@ import javafx.application.Application;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hi!");
+        System.out.println("Bye!");
         Application.launch(MainApp.class, args);
     }
 }
