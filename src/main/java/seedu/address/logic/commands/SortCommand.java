e1af65a7ea8a85b9a39d3561ea8eb9e2eebafeb9
evanpy <evanleewj@gmail.com>
2023-03-22 00:42:31 +0800

    Add sort command class

--- src/main/java/seedu/address/logic/commands/SortCommand.java
+++ src/main/java/seedu/address/logic/commands/SortCommand.java
@@ -11,11 +11,10 @@ import seedu.address.model.Model;
 public class SortCommand extends Command {
     public static final String COMMAND_WORD = "sort";
 
-    //NEEDS TO BE CHANGEDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons whose names contain any of "
-            + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
-            + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
-            + "Example: " + COMMAND_WORD + " alice bob charlie";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Sorts all persons based on a specific attribute "
+            + "the specified attribute (case-insensitive) and displays them as a list with index numbers.\n"
+            + "Parameters: ATTRIBUTE \n"
+            + "Example: " + COMMAND_WORD + " name";
     private final String attribute;
 
     public SortCommand(String attribute) {
@@ -27,7 +26,8 @@ public class SortCommand extends Command {
         requireNonNull(model);
         model.sortPersonList(attribute);
         return new CommandResult(
-                String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()));
+                String.format(
+                        Messages.MESSAGE_PERSONS_SORTED_OVERVIEW, model.getFilteredPersonList().size(), attribute));
     }
 
     @Override
