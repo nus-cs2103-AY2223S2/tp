8f4c9a5fdb2eadff50bfec9688094e51f89e93d5
securespider <e0725031@u.nus.edu>
2023-03-22 04:12:51 +0800

    Fixed bug where the storage could not work

--- src/main/java/seedu/address/storage/academics/JsonAdaptedAttendance.java
+++ src/main/java/seedu/address/storage/academics/JsonAdaptedAttendance.java
@@ -1,15 +1,15 @@
 package seedu.address.storage.academics;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.person.student.Assignment;
 import seedu.address.model.person.student.Attendance;
 import seedu.address.storage.JsonAdapted;
 
-import java.time.format.DateTimeFormatter;
 
 /**
- * Jackson-friendly version of {@link Assignment}.
+ * Jackson-friendly version of {@link Attendance}.
  */
 public class JsonAdaptedAttendance implements JsonAdapted<Attendance> {
     private final String attendance;
@@ -22,15 +22,29 @@ public class JsonAdaptedAttendance implements JsonAdapted<Attendance> {
         this.attendance = attendance;
     }
 
+    /**
+     * Converts a given {@code Attendance} into this class for Jackson use.
+     * @param source future changes to this will not affect the created JsonAdaptedAttendance
+     */
     public JsonAdaptedAttendance(Attendance source) {
         this.attendance = source.toString();
     }
 
+    /**
+     * Converts this Jackson-friendly adapted tag object into the model's {@code Attendance} object.
+     * @return the model's Attendance object
+     * @throws IllegalValueException if there were any data constraints violated in the adapted Attendance.
+     */
     @Override
     public Attendance toModelType() throws IllegalValueException {
         return new Attendance(attendance);
     }
 
+    /**
+     * Returns the attendance of the student.
+     * @return the attendance of the student
+     */
+    @JsonValue
     public String getAttendance() {
         return attendance;
     }
