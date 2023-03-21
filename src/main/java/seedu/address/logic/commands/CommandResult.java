47e6f81b0d8dc88088384b6e0f3e29b5e406f7de
kayyenl <yenyenlee9@gmail.com>
2023-03-21 14:46:11 +0800

    Edit equals method of commandresult

--- src/main/java/seedu/address/logic/commands/CommandResult.java
+++ src/main/java/seedu/address/logic/commands/CommandResult.java
@@ -78,7 +78,8 @@ public class CommandResult {
         CommandResult otherCommandResult = (CommandResult) other;
         return feedbackToUser.equals(otherCommandResult.feedbackToUser)
                 && showHelp == otherCommandResult.showHelp
-                && exit == otherCommandResult.exit;
+                && exit == otherCommandResult.exit
+                && showQuickstart == otherCommandResult.showQuickstart;
     }
 
     @Override
