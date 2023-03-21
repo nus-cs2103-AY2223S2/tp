7bf415b7fcc4cefab9d8c94a2f5c485d235f2507
Joy Ng Jing Ru <e0943052@u.nus.edu>
2023-03-21 19:54:35 +0800

    Fix deadline bug that ignores deadlines on the day plan is generated

--- src/main/java/seedu/task/model/task/DeadlineList.java
+++ src/main/java/seedu/task/model/task/DeadlineList.java
@@ -43,7 +43,7 @@ public class DeadlineList extends CategoricalTaskList {
         if (t.isDeadline()) {
             Deadline d = (Deadline) t;
 
-            if (today.isBefore(d.getDeadline().getDate())) {
+            if (today.isBefore(d.getDeadline().getDate()) || today.isBefore(d.getDeadline().getDate())) {
                 taskList.add(d);
                 return true;
             }
