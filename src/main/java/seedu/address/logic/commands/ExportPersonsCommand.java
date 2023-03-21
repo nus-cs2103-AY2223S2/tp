817983fa5be08dd4333c2b4b41743d139a999f53
chuakid <chuakid@gmail.com>
2023-03-21 17:46:03 +0800

    Change export command to use JsonAdaptedPerson instead

--- src/main/java/seedu/address/logic/commands/ExportPersonsCommand.java
+++ src/main/java/seedu/address/logic/commands/ExportPersonsCommand.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.commands;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 
@@ -9,6 +10,7 @@ import seedu.address.commons.util.JsonUtil;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.person.Person;
+import seedu.address.storage.JsonAdaptedPerson;
 
 /**
  * This class represents a command for exporting of persons
@@ -30,7 +32,12 @@ public class ExportPersonsCommand extends Command {
     public CommandResult execute(Model model) throws CommandException {
         try {
             List<Person> personList = model.getPersonsByIndexes(indexList);
-            return new CommandResult(JsonUtil.toJsonString(personList));
+            List<JsonAdaptedPerson> adapted =
+                    personList
+                            .stream()
+                            .map(JsonAdaptedPerson::new)
+                            .collect(Collectors.toList());
+            return new CommandResult(JsonUtil.toJsonString(adapted));
         } catch (IndexOutOfBoundsException e) {
             throw new CommandException(INDEX_NOT_FOUND);
         } catch (JsonProcessingException e) {
