4bcdeab0504607f47e76d280356cad709b53f3d5
jinbesan <e0505781@u.nus.edu>
2023-03-21 22:08:55 +0800

    Update for checkstyle

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -5,7 +5,6 @@ import static java.util.Objects.requireNonNull;
 import seedu.address.commons.core.Messages;
 import seedu.address.model.Model;
 import seedu.address.model.expense.ExpenseContainsKeywordsPredicate;
-import seedu.address.model.person.NameContainsKeywordsPredicate;
 
 /**
  * Finds and lists all persons in address book whose name contains any of the argument keywords.
