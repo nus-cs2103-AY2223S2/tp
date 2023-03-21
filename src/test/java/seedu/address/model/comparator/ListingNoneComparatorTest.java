55d1b5cb1e3fdbc3e06703f61f2ef3829207d133
yyj-02 <yeohyongjie@outlook.com>
2023-03-21 23:15:23 +0800

    Check style

--- src/test/java/seedu/address/model/comparator/ListingNoneComparatorTest.java
+++ src/test/java/seedu/address/model/comparator/ListingNoneComparatorTest.java
@@ -1,9 +1,12 @@
 package seedu.address.model.comparator;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.ArrayList;
 import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
+
 import seedu.address.model.listing.Listing;
 import seedu.address.testutil.ApplicantBuilder;
 import seedu.address.testutil.ListingBuilder;
