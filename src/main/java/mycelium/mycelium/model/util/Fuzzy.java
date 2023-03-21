03278d3c53be1c160c5c46ba1d3784f45749d66b
junyi <47441677+immanuelhume@users.noreply.github.com>
2023-03-21 23:20:36 +0800

    Add basic function for fuzzy matching

---
+++ src/main/java/mycelium/mycelium/model/util/Fuzzy.java
@@ -0,0 +1,65 @@
+package mycelium.mycelium.model.util;
+
+/**
+ * A utility class for fuzzy string matching.
+ */
+public class Fuzzy {
+
+    /**
+     * Computes a "delta-score" between two strings. The lower the score, the more similar the strings are. A score
+     * of zero means the strings are completely identical.
+     */
+    public static int delta(String a, String b) {
+        // The intuitive way to do this is through a m-by-n matrix. Here we are only
+        // using one array and a temp variable.
+        int m = a.length(), n = b.length();
+        if (m > n) {
+            String tmp = a;
+            a = b;
+            b = tmp;
+
+            int _tmp = m;
+            m = n;
+            n = _tmp;
+        }
+        if (m == 0) {
+            return n;
+        }
+        if (m == 1) {
+            char c = a.charAt(0);
+            for (int i = 0; i < n; i++) {
+                if (b.charAt(i) == c) {
+                    return n - 1;
+                }
+            }
+            return n;
+        }
+
+        int[] prevRow = new int[b.length() + 1];
+
+        for (int j = 0; j < n + 1; j++) {
+            prevRow[j] = j;
+        }
+
+        int grave, ifDelete, ifInsert, tmp;
+        for (int i = 0; i < m; i++) {
+            grave = i; // grave refers to the upper-left tile (from the matrix POV)
+            prevRow[0] = i + 1; // the first entry is always i+1
+
+            // now fill in 1..n+1 entries, writing to index j+1 for each iteration
+            for (int j = 0; j < n; j++) {
+                tmp = grave;
+                if (a.charAt(i) != b.charAt(j)) {
+                    ifDelete = prevRow[j + 1];
+                    ifInsert = prevRow[j]; // this is technically "currentRow[j]"
+                    tmp = ifInsert < grave ? ifInsert : grave;
+                    tmp = ifDelete < tmp ? ifDelete : tmp;
+                    tmp += 1;
+                }
+                grave = prevRow[j + 1];
+                prevRow[j + 1] = tmp;
+            }
+        }
+        return prevRow[n];
+    }
+}
---
+++ src/test/java/mycelium/mycelium/model/util/FuzzyTest.java
@@ -0,0 +1,48 @@
+package mycelium.mycelium.model.util;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+import mycelium.mycelium.testutil.Pair;
+
+class FuzzyTest {
+
+    /**
+     * A test case for the delta function.
+     */
+    private static class Case {
+        String a;
+        String b;
+        int expected;
+        String desc;
+
+        Case(String a, String b, int expected, String desc) {
+            this.a = a;
+            this.b = b;
+            this.expected = expected;
+            this.desc = desc;
+        }
+    }
+
+    @Test
+    void delta() {
+        Case[] tests = {
+            new Case("uwu", "owo", 2, "replace only"),
+            new Case("horse", "ros", 3, "replace with insert"),
+            new Case("intention", "execution", 5, "replace with insert"),
+            new Case("uwu", "", 3, "RHS empty"),
+            new Case("", "uwu", 3, "LHS empty"),
+            new Case("   ", "uwu", 3, "whitespace"),
+            new Case("uwu", "uwu", 0, "same strings")
+        };
+
+        for (Case test : tests) {
+            int actual = Fuzzy.delta(test.a, test.b);
+            assertEquals(test.expected, actual, "While testing case: " + test.desc);
+        }
+    }
+
+}
