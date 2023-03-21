a9704b9d25d26856dd211e2030d1c3279247db32
William Chau Wei Xuan <55905659+WillCWX@users.noreply.github.com>
2023-03-21 13:50:30 +0800

    Update HistoryUtil to better apply Demeter's Law
    Concrete Memento has a string description that the UndoCommand uses to
    provide feedback to user. However, a memento cannot have a string description
    when made as it is simply a state of the originator and has no knowledge
    of time and change. Hence, the memento's description setter is used by
    the modifying Commands to descibe the change occured.
    
    This is problematic as this violates Demeter's Law. The Commands use a
    method from the memento from the method from model to set a description.
    Furthermore, this complicates the Commands' relationship with Memento as
    Commands now directly rely on the Concrete implementations of Memento to
    set descriptions.
    
    Let's move the descriptions out of the memento and into HistoryUtil. We
    will now store the description of the change, along with the memento, into
    HistoryUtil instead. HistoryUtil should directly interface with memento,
    while Commands should directly interface with model and HistoryUtil instead.
    
    This will resolve Demeter's Law as a memento's methods are no longer used
    by the Commands. Command and Memento classes are also decoupled for better
    extension in accordance to the open-close principle.
    
    Misc.
    - Changed Momento to Memento (spelling).
    - Changed HistoryUtil.store to HistoryUtil.storePast (better name).
    - Updated Tests to reflect above changes.
    - Updated Commands to reflect above changes.

--- src/main/java/teambuilder/commons/util/HistoryUtil.java
+++ src/main/java/teambuilder/commons/util/HistoryUtil.java
@@ -1,6 +1,10 @@
 package teambuilder.commons.util;
 
-import teambuilder.commons.core.Momento;
+import static java.util.Objects.requireNonNull;
+
+import java.util.Optional;
+
+import teambuilder.commons.core.Memento;
 
 /**
  * Represents the history of states in the app.
@@ -9,12 +13,13 @@ public class HistoryUtil {
     private static HistoryUtil singleton = new HistoryUtil();
 
     private static final int MAX_STATE = 10;
-    private Momento[] history;
+    private DescribedMemento[] history;
     private int currentNum = -1;
-    private String descriptionUndo;
+    private DescribedMemento[] redoFuture;
 
     private HistoryUtil() {
-        history = new Momento[MAX_STATE];
+        history = new DescribedMemento[MAX_STATE];
+        redoFuture = new DescribedMemento[MAX_STATE];
     }
 
     private void incrementCurrentNum() {
@@ -45,42 +50,94 @@ public class HistoryUtil {
     }
 
     /**
+     * Stores a past memento of the app and a description of the memento. 
+     * Maximumn of 10 stored momentos at any time.
+     *
+     * @param memo The momento to be stored.
+     * @param desc The description of the memo.
+     */
+    public void storePast(Memento memo, String desc) {
+        requireNonNull(memo);
+        requireNonNull(desc);
+
+        DescribedMemento memoWithDesc = new DescribedMemento(memo, desc);
+        incrementCurrentNum();
+        history[currentNum] = memoWithDesc;
+        redoFuture = new DescribedMemento[MAX_STATE];
+    }
+
+    /**
      * Restores the app to its previous state.
      *
-     * @return true if successful, false otherwise
+     * @return An Optional containing a string of the description of the state restored if successful,
+     * contains null otherwise.
      */
-    public boolean undo() {
+    public Optional<String> undo() {
         if (currentNum <= -1) {
-            return false;
+            return Optional.ofNullable(null);
         }
         if (history[currentNum] == null) {
             currentNum = -1;
-            return false;
+            return Optional.ofNullable(null);
         }
 
-        boolean isSuccessful = history[currentNum].restore();
-        descriptionUndo = history[currentNum].toString();
+        DescribedMemento mementoForUndo = history[currentNum];
+        DescribedMemento mementoForRedo = mementoForUndo.getUpdatedMemento();
+        boolean isSuccessful = mementoForUndo.restore();
+        String descriptionUndo = mementoForUndo.desc;
         history[currentNum] = null;
+        redoFuture[currentNum] = mementoForRedo;
+
         decreaseCurrentNum();
-        return isSuccessful;
+        return isSuccessful ? Optional.of(descriptionUndo) : Optional.ofNullable(null);
     }
 
     /**
-     * Retrieves the description of the last successful undo.
+     * Restores the app to its state before an undo.
      *
-     * @return The string containing the description of the last successful undo
+     * @return An Optional containing a string of the description of the state restored if successful,
+     * contains null otherwise.
      */
-    public String getLastUndoDescription() {
-        return descriptionUndo == null ? "" : descriptionUndo;
-    }
+    public Optional<String> redo() {
+        if (currentNum <= -1) {
+            return Optional.ofNullable(null);
+        }
 
-    /**
-     * Stores a momento of the app. Maximumn of 10 stored momentos at any time.
-     *
-     * @param memo The momento to be stored.
-     */
-    public void store(Momento memo) {
         incrementCurrentNum();
-        history[currentNum] = memo;
+
+        if (redoFuture[currentNum] == null) {
+            decreaseCurrentNum();
+            return Optional.ofNullable(null);
+        }
+
+        DescribedMemento mementoForRedo = history[currentNum];
+        boolean isSuccessful = mementoForRedo.restore();
+        String descriptionRedo = mementoForRedo.desc;
+        history[currentNum] = mementoForRedo;
+        redoFuture[currentNum] = null;
+
+        return isSuccessful ? Optional.of(descriptionRedo) : Optional.ofNullable(null);
+    }
+
+    /**
+     * Wrapper class for the Memento and its description.
+     */
+    private class DescribedMemento {
+        private Memento memento;
+        private String desc;
+
+        DescribedMemento(Memento memento, String desc) {
+            this.memento = memento;
+            this.desc = desc;
+        }
+
+        private boolean restore() {
+            return memento.restore();
+        }
+
+        private DescribedMemento getUpdatedMemento() {
+            return new DescribedMemento(memento.getUpdatedMemento(), desc);
+        }
+
     }
 }
