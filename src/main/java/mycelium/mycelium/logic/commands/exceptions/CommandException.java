b7ef2ef04d9b9e861e87dc953e38d3e697bd97d5
StanleyNeoh <95394480+StanleyNeoh@users.noreply.github.com>
2023-03-21 15:26:15 +0800

    Fix trailing whitespace

--- src/main/java/mycelium/mycelium/logic/commands/exceptions/CommandException.java
+++ src/main/java/mycelium/mycelium/logic/commands/exceptions/CommandException.java
@@ -1,10 +1,10 @@
 package mycelium.mycelium.logic.commands.exceptions;
 
+import static java.util.Objects.requireNonNull;
+
 import mycelium.mycelium.ui.MainWindow;
 import mycelium.mycelium.ui.action.DoNothingAction;
 import mycelium.mycelium.ui.action.UiAction;
-
-import static java.util.Objects.requireNonNull;
 
 /**
  * Represents an error which occurs during execution of a {@link Command}.
@@ -12,11 +12,36 @@ import static java.util.Objects.requireNonNull;
 public class CommandException extends Exception {
     private UiAction action;
 
+    /**
+     * Constructs a new {@code CommandException} with the
+     * specified detail {@code message} and {@code action} to be executed.
+     * {@code action} cannot be null.
+     *
+     * @param message the detail message.
+     * @param action the action to be executed.
+     */
     public CommandException(String message, UiAction action) {
         super(message);
         this.action = requireNonNull(action);
     }
 
+    /**
+     * Constructs a new {@code CommandException} with the
+     * specified detail {@code message} and {@code cause}.
+     *
+     * @param message the detail message.
+     * @param cause the throwable that caused this exception.
+     */
+    public CommandException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    /**
+     * Constructs a new {@code CommandException} with the
+     * specified detail {@code message} and a {@code DoNothingAction}.
+     *
+     * @param message the detail message.
+     */
     public CommandException(String message) {
         this(message, new DoNothingAction());
     }
@@ -25,10 +50,4 @@ public class CommandException extends Exception {
         action.execute(mainWindow);
     }
 
-    /**
-     * Constructs a new {@code CommandException} with the specified detail {@code message} and {@code cause}.
-     */
-    public CommandException(String message, Throwable cause) {
-        super(message, cause);
-    }
 }
