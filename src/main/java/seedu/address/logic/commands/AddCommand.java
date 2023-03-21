34de03acd185ac7c6b7f6222c6674ced98b26903
tingyic <e0735450@u.nus.edu>
2023-03-21 12:32:38 +0800

    General restructuring of add and edit command

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -5,9 +5,11 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_DEADLINE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_REMARK;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_RESOURCE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TEACHER;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TIMESLOT;
+
 
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
@@ -26,6 +28,7 @@ public class AddCommand extends Command {
             + PREFIX_TAG + "TAG (Lecture, Tutorial, Lab) "
             + "[" + PREFIX_TIMESLOT + "TIMESLOT] "
             + "[" + PREFIX_ADDRESS + "VENUE] "
+            + "[" + PREFIX_RESOURCE + "RESOURCE] "
             + "[" + PREFIX_TEACHER + "TEACHER] "
             + "[" + PREFIX_DEADLINE + "DEADLINES] "
             + "[" + PREFIX_REMARK + "REMARKS]\n"
