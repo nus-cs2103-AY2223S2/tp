ba000eed3ab4492942b572fa8af05e0eab26b605
zrei <88964793+zrei@users.noreply.github.com>
2023-03-21 16:24:30 +0800

    Fix list tag command

--- src/main/java/arb/logic/commands/tag/ListTagCommand.java
+++ src/main/java/arb/logic/commands/tag/ListTagCommand.java
@@ -2,6 +2,12 @@ package arb.logic.commands.tag;
 
 import static java.util.Objects.requireNonNull;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import arb.logic.commands.Command;
 import arb.logic.commands.CommandResult;
 import arb.model.ListType;
@@ -12,7 +18,10 @@ import arb.model.Model;
  */
 public class ListTagCommand extends Command {
 
-    public static final String COMMAND_WORD = "list-tag";
+    private static final String MAIN_COMMAND_WORD = "list-tag";
+    private static final String ALIAS_COMMAND_WORD = "lt";
+    private static final Set<String> COMMAND_WORDS =
+            new HashSet<>(Arrays.asList(MAIN_COMMAND_WORD, ALIAS_COMMAND_WORD));
 
     public static final String MESSAGE_SUCCESS = "Listed all tags";
 
@@ -20,5 +29,13 @@ public class ListTagCommand extends Command {
     public CommandResult execute(Model model, ListType currentListBeingShown) {
         requireNonNull(model);
         return new CommandResult(MESSAGE_SUCCESS, ListType.TAG);
+    }
+
+    public static boolean isCommandWord(String commandWord) {
+        return COMMAND_WORDS.contains(commandWord);
+    }
+
+    public static List<String> getCommandWords() {
+        return new ArrayList<>(COMMAND_WORDS);
     }
 }
