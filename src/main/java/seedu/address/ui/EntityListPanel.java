30a7baaeb1bf9ece4aa8d0ab69e630141a54d717
roultitude <51879825+roultitude@users.noreply.github.com>
2023-03-22 23:42:08 +0800

    Add custom theme for entity details panel
    Current design choices were somewhat garish and primitive.
    
    A clean and somewhat brighter design will benefit users by not making their eyes bleed.
    
    Adjusted font choices for default dark theme.

--- src/main/java/seedu/address/ui/EntityListPanel.java
+++ src/main/java/seedu/address/ui/EntityListPanel.java
@@ -15,25 +15,25 @@ import seedu.address.model.entity.Entity;
  */
 public class EntityListPanel extends UiPart<Region> {
 
-    private static final String FXML = "PersonListPanel.fxml";
+    private static final String FXML = "EntityListPanel.fxml";
     private final Logger logger = LogsCenter.getLogger(EntityListPanel.class);
 
     @FXML
-    private ListView<Entity> personListView;
+    private ListView<Entity> entityListView;
 
     /**
      * Creates a {@code PersonListPanel} with the given {@code ObservableList}.
      */
     public EntityListPanel(ObservableList<Entity> entityList) {
         super(FXML);
-        personListView.setItems(entityList);
-        personListView.setCellFactory(listView -> new PersonListViewCell());
+        entityListView.setItems(entityList);
+        entityListView.setCellFactory(listView -> new EntityListViewCell());
     }
 
     /**
      * Custom {@code ListCell} that displays the graphics of a {@code Person} using a {@code PersonCard}.
      */
-    class PersonListViewCell extends ListCell<Entity> {
+    class EntityListViewCell extends ListCell<Entity> {
 
         @Override
         protected void updateItem(Entity entity, boolean empty) {
