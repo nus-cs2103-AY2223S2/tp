b85e07aac93fb1f2f44c805df2cbfebc2bd153c1
zrei <88964793+zrei@users.noreply.github.com>
2023-03-22 09:44:30 +0800

    Update LogicManager tests

--- src/test/java/arb/logic/LogicManagerTest.java
+++ src/test/java/arb/logic/LogicManagerTest.java
@@ -97,6 +97,11 @@ public class LogicManagerTest {
         assertThrows(UnsupportedOperationException.class, () -> logic.getSortedProjectList().remove(0));
     }
 
+    @Test
+    public void getTagMappingList_modifyList_throwsUnsupportedOperationException() {
+        assertThrows(UnsupportedOperationException.class, () -> logic.getTagMappingList().remove(0));
+    }
+
     /**
      * Executes the command and confirms that
      * - no exceptions are thrown <br>
