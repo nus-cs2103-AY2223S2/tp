b93cfbae9eb4f3d2957420507c3c24a3f149a83e
EvitanRelta <shauntanzongzhi@gmail.com>
2023-03-22 13:26:05 +0800

    Refactor all 'Person' constants in 'TypicalPersons' class

--- src/test/java/seedu/address/testutil/TypicalPersons.java
+++ src/test/java/seedu/address/testutil/TypicalPersons.java
@@ -23,43 +23,109 @@ import seedu.address.model.person.Person;
  */
 public class TypicalPersons {
 
-    public static final Person ALICE = new PersonBuilder().withName("Alice Pauline")
-            .withAddress("123, Jurong West Ave 6, #08-111").withEmail("alice@example.com")
-            .withPhone("94351253").withRemark("She likes aardvarks.")
+    public static final Person ALICE = new PersonBuilder()
+            .withName("Alice Pauline")
+            .withPhone("94351253")
+            .withEmail("alice@example.com")
+            .withAddress("123, Jurong West Ave 6, #08-111")
+            .withRemark("She likes aardvarks.")
             .withSubjects("Mathematics")
-            .withTags("friends").build();
-    public static final Person BENSON = new PersonBuilder().withName("Benson Meier")
-            .withAddress("311, Clementi Ave 2, #02-25").withRemark("He can't take beer!")
-            .withEmail("johnd@example.com").withPhone("98765432")
+            .withTags("friends")
+            .build();
+    public static final Person BENSON = new PersonBuilder()
+            .withName("Benson Meier")
+            .withPhone("98765432")
+            .withEmail("johnd@example.com")
+            .withAddress("311, Clementi Ave 2, #02-25")
+            .withRemark("He can't take beer!")
             .withSubjects("English", "Science")
-            .withTags("owesMoney", "friends").build();
-    public static final Person CARL = new PersonBuilder().withName("Carl Kurz").withPhone("95352563")
-            .withEmail("heinz@example.com").withAddress("wall street").withRemark("Needs help with algebra.").build();
-    public static final Person DANIEL = new PersonBuilder().withName("Daniel Meier").withPhone("87652533")
-            .withEmail("cornelia@example.com").withAddress("10th street").withRemark("Needs help with compo.")
-            .withTags("friends").build();
-    public static final Person ELLE = new PersonBuilder().withName("Elle Meyer").withPhone("9482224")
-            .withEmail("werner@example.com").withAddress("michegan ave")
-            .withRemark("Needs more time on limits").build();
-    public static final Person FIONA = new PersonBuilder().withName("Fiona Kunz").withPhone("9482427")
-            .withEmail("lydia@example.com").withAddress("little tokyo").withRemark("Is good at algebra").build();
-    public static final Person GEORGE = new PersonBuilder().withName("George Best").withPhone("9482442")
-            .withEmail("anna@example.com").withAddress("4th street")
-            .withRemark("Is bad at argumentative essays").build();
+            .withTags("owesMoney", "friends")
+            .build();
+    public static final Person CARL = new PersonBuilder()
+            .withName("Carl Kurz")
+            .withPhone("95352563")
+            .withEmail("heinz@example.com")
+            .withAddress("wall street")
+            .withRemark("Needs help with algebra.")
+            .withSubjects()
+            .withTags()
+            .build();
+    public static final Person DANIEL = new PersonBuilder()
+            .withName("Daniel Meier")
+            .withPhone("87652533")
+            .withEmail("cornelia@example.com")
+            .withAddress("10th street")
+            .withRemark("Needs help with compo.")
+            .withSubjects()
+            .withTags("friends")
+            .build();
+    public static final Person ELLE = new PersonBuilder()
+            .withName("Elle Meyer")
+            .withPhone("9482224")
+            .withEmail("werner@example.com")
+            .withAddress("michegan ave")
+            .withRemark("Needs more time on limits")
+            .withSubjects()
+            .withTags()
+            .build();
+    public static final Person FIONA = new PersonBuilder()
+            .withName("Fiona Kunz")
+            .withPhone("9482427")
+            .withEmail("lydia@example.com")
+            .withAddress("little tokyo")
+            .withRemark("Is good at algebra")
+            .withSubjects()
+            .withTags()
+            .build();
+    public static final Person GEORGE = new PersonBuilder()
+            .withName("George Best")
+            .withPhone("9482442")
+            .withEmail("anna@example.com")
+            .withAddress("4th street")
+            .withRemark("Is bad at argumentative essays")
+            .withSubjects()
+            .withTags()
+            .build();
 
     // Manually added
-    public static final Person HOON = new PersonBuilder().withName("Hoon Meier").withPhone("8482424")
-            .withEmail("stefan@example.com").withAddress("little india").withRemark("Needs help with trigo").build();
-    public static final Person IDA = new PersonBuilder().withName("Ida Mueller").withPhone("8482131")
-            .withEmail("hans@example.com").withAddress("chicago ave").withRemark("Needs help with angles").build();
+    public static final Person HOON = new PersonBuilder()
+            .withName("Hoon Meier")
+            .withPhone("8482424")
+            .withEmail("stefan@example.com")
+            .withAddress("little india")
+            .withRemark("Needs help with trigo")
+            .withSubjects()
+            .withTags()
+            .build();
+    public static final Person IDA = new PersonBuilder()
+            .withName("Ida Mueller")
+            .withPhone("8482131")
+            .withEmail("hans@example.com")
+            .withAddress("chicago ave")
+            .withRemark("Needs help with angles")
+            .withSubjects()
+            .withTags()
+            .build();
 
     // Manually added - Person's details found in {@code CommandTestUtil}
-    public static final Person AMY = new PersonBuilder().withName(VALID_NAME_AMY).withPhone(VALID_PHONE_AMY)
-            .withEmail(VALID_EMAIL_AMY).withAddress(VALID_ADDRESS_AMY).withRemark(null).withTags(VALID_TAG_FRIEND)
+    public static final Person AMY = new PersonBuilder()
+            .withName(VALID_NAME_AMY)
+            .withPhone(VALID_PHONE_AMY)
+            .withEmail(VALID_EMAIL_AMY)
+            .withAddress(VALID_ADDRESS_AMY)
+            .withRemark(null)
+            .withSubjects()
+            .withTags(VALID_TAG_FRIEND)
             .build();
-    public static final Person BOB = new PersonBuilder().withName(VALID_NAME_BOB).withPhone(VALID_PHONE_BOB)
-            .withEmail(VALID_EMAIL_BOB).withAddress(VALID_ADDRESS_BOB).withRemark(null)
-            .withTags(VALID_TAG_HUSBAND, VALID_TAG_FRIEND).build();
+    public static final Person BOB = new PersonBuilder()
+            .withName(VALID_NAME_BOB)
+            .withPhone(VALID_PHONE_BOB)
+            .withEmail(VALID_EMAIL_BOB)
+            .withAddress(VALID_ADDRESS_BOB)
+            .withRemark(null)
+            .withSubjects()
+            .withTags(VALID_TAG_HUSBAND, VALID_TAG_FRIEND)
+            .build();
 
     public static final String KEYWORD_MATCHING_MEIER = "Meier"; // A keyword that matches MEIER
 
