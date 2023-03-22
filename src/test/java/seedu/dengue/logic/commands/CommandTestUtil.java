1a2f326510a53540bb1f3f38221503c252178ff6
LAPTOP-EG15LSJU\hungy <e0726431@u.nus.edu>
2023-03-23 01:50:23 +0800

    rename PersonContainsNamesPredicate to FilterPredicate

--- src/test/java/seedu/dengue/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/dengue/logic/commands/CommandTestUtil.java
@@ -18,7 +18,7 @@ import seedu.dengue.logic.commands.exceptions.CommandException;
 import seedu.dengue.model.DengueHotspotTracker;
 import seedu.dengue.model.Model;
 import seedu.dengue.model.person.Person;
-import seedu.dengue.model.person.PersonContainsKeywordsPredicate;
+import seedu.dengue.model.person.FilterPredicate;
 import seedu.dengue.testutil.EditPersonDescriptorBuilder;
 
 /**
@@ -121,7 +121,7 @@ public class CommandTestUtil {
 
         Person person = model.getFilteredPersonList().get(targetIndex.getZeroBased());
         final String[] splitName = person.getName().fullName.split("\\s+");
-        model.updateFilteredPersonList(new PersonContainsKeywordsPredicate(Arrays.asList(splitName[0])));
+        model.updateFilteredPersonList(new FilterPredicate(Arrays.asList(splitName[0])));
 
         assertEquals(1, model.getFilteredPersonList().size());
     }
