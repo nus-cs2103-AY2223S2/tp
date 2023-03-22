07a605709274f7455b297b75d498ca9f2594cbb4
Ritika Joshi <ritikajoshi0114@gmail.com>
2023-03-22 20:53:13 +0800

    Complete HelpCommandParser test cases
    Added equals method to command to compare objects

--- src/test/java/seedu/wife/logic/parser/HelpCommandParserTest.java
+++ src/test/java/seedu/wife/logic/parser/HelpCommandParserTest.java
@@ -1,40 +1,39 @@
 package seedu.wife.logic.parser;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static seedu.wife.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.wife.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.wife.logic.parser.CommandParserTestUtil.assertParseSuccess;
-
-import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
 import seedu.wife.commons.core.HelpMenu;
-import seedu.wife.logic.commands.Command;
 import seedu.wife.logic.commands.HelpCommand;
-import seedu.wife.logic.parser.exceptions.ParseException;
-import seedu.wife.model.food.NameContainsKeywordsPredicate;
 
 public class HelpCommandParserTest {
 
     private HelpCommandParser parser = new HelpCommandParser();
 
     @Test
-    public void parse_invalidArg_returnsInvalidArgMessage() throws ParseException {
-        //invalid string
-        Command command = parser.parse("sdkjksjks");
-        Command expectedCommand = new HelpCommand(HelpMenu.INVALID.getCommandUsage());
+    public void parse_emptyArgs_returnsGeneralHelpCommand() {
+        //empty string
+        HelpCommand expectedCommand = new HelpCommand(HelpMenu.getGeneralHelp());
+        assertParseSuccess(parser, "", expectedCommand);
 
-        assertEquals(expectedCommand, command);
+        //string with whitespace
+        assertParseSuccess(parser, "        ", expectedCommand);
     }
 
-//    @Test
-//    public void parse_validArgs_returnsFindCommand() {
-//        // no leading and trailing whitespaces
-//        FindCommand expectedFindCommand =
-//                new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList("Meiji", "Chocolate")));
-//        assertParseSuccess(parser, "Meiji Chocolate", expectedFindCommand);
-//
-//        // multiple whitespaces between keywords
-//        assertParseSuccess(parser, " \n Meiji \n \t Chocolate  \t", expectedFindCommand);
-//    }
+    @Test
+    public void parse_validCommand_returnsCommandSpecificHelpCommand() {
+        //normal input
+        HelpCommand expectedCommand = new HelpCommand(HelpMenu.ADD.getCommandUsage());
+        assertParseSuccess(parser, "add", expectedCommand);
+
+        //input with whitespaces
+        assertParseSuccess(parser, "    add     ", expectedCommand);
+    }
+
+    @Test
+    public void parse_invalidArg_returnsInvalidArgMessage() {
+        //invalid string
+        HelpCommand expectedCommand = new HelpCommand(HelpMenu.INVALID.getCommandUsage());
+        assertParseSuccess(parser, "abcdef", expectedCommand);
+    }
 }
