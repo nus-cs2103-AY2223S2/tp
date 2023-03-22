99a6addfec549405fe6d62e1c6ce491dd996937b
Tan Jun Da <e0726893@u.nus.edu>
2023-03-22 12:50:48 +0800

    Update some test and sampledata issues
    Current test and sampledata has some errors.
    
    Required to fix these issues to pass the tests.
    
    Let's fix these issues in the code itself.
    
    Fixing these issues is the only way to pass the tests.
    
    Some tests have not been fixed. Committing these files first to change
    branch.

--- src/main/java/seedu/fitbook/model/util/SampleDataUtil.java
+++ src/main/java/seedu/fitbook/model/util/SampleDataUtil.java
@@ -33,27 +33,27 @@ public class SampleDataUtil {
             new Client(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
                 new Address("Blk 30 Geylang Street 29, #06-40"), getAppointmentSet("12-12-2019 18:30"),
                 new Weight("20"), new Gender("M"), new Calorie("1200"), new Goal("lose weight"),
-                getTagSet("friends"), getRoutineNameSet()),
+                getTagSet("friends"), getRoutineSet()),
             new Client(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
                 new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"), getAppointmentSet("12-12-2019 18:30"),
                 new Weight("20"), new Gender("M"), new Calorie("1200"), new Goal("lose weight"),
-                getTagSet("colleagues", "friends"), getRoutineNameSet()),
+                getTagSet("colleagues", "friends"), getRoutineSet()),
             new Client(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
                 new Address("Blk 11 Ang Mo Kio Street 74, #11-04"), getAppointmentSet("12-12-2019 18:30"),
                 new Weight("30"), new Gender("M"), new Calorie("1100"), new Goal("lose weight"),
-                getTagSet("neighbours"), getRoutineNameSet()),
+                getTagSet("neighbours"), getRoutineSet()),
             new Client(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
                 new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
                 getAppointmentSet("12-12-2019 18:30"), new Weight("40"), new Gender("M"), new Calorie("1200"),
-                new Goal("lose weight"), getTagSet("family"), getRoutineNameSet()),
+                new Goal("lose weight"), getTagSet("family"), getRoutineSet()),
             new Client(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
                 new Address("Blk 47 Tampines Street 20, #17-35"), getAppointmentSet("12-12-2019 18:30"),
                 new Weight("50"), new Gender("M"), new Calorie("1202"), new Goal("lose weight"),
-                getTagSet("classmates"), getRoutineNameSet()),
+                getTagSet("classmates"), getRoutineSet()),
             new Client(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
                 new Address("Blk 45 Aljunied Street 85, #11-31"), getAppointmentSet("12-12-2019 18:30"),
                 new Weight("60"), new Gender("M"), new Calorie("2356"), new Goal("lose weight"),
-                getTagSet("colleagues"), getRoutineNameSet())
+                getTagSet("colleagues"), getRoutineSet())
         };
     }
 
@@ -86,7 +86,7 @@ public class SampleDataUtil {
     /**
      * Returns a routine name set containing the list of strings given.
      */
-    public static Set<Routine> getRoutineNameSet(String... strings) {
+    public static Set<Routine> getRoutineSet(String... strings) {
         Set<Routine> routines = new HashSet<>();
         for (String routineNameStr : strings) {
             RoutineName routineName = new RoutineName(routineNameStr);
