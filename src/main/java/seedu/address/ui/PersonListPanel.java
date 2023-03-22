6024c99651d0c61442403adbcf421b1202e21aa2
yanlings <e0774088@u.nus.edu>
2023-03-22 18:28:30 +0800

    Update files such that ExecutivePro has a clicking function

--- src/main/java/seedu/address/ui/PersonListPanel.java
+++ src/main/java/seedu/address/ui/PersonListPanel.java
@@ -2,14 +2,18 @@ package seedu.address.ui;
 
 import java.util.logging.Logger;
 
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Region;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.model.person.Person;
+import seedu.address.logic.Logic;
 
 
 
@@ -25,17 +29,57 @@ public class PersonListPanel extends UiPart<Region> {
     @FXML
     private Label nameCard;
 
+    @FXML
+    private Label name;
+    @FXML
+    private Label id;
+    @FXML
+    private Label employeeId;
+    @FXML
+    private Label phone;
+    @FXML
+    private Label address;
+    @FXML
+    private Label email;
+    @FXML
+    private Label department;
+    @FXML
+    private FlowPane tags;
+
     /**
      * Creates a {@code PersonListPanel} with the given {@code ObservableList}.
      */
-    public PersonListPanel(ObservableList<Person> personList) {
+    public PersonListPanel(ObservableList<Person> personList, Logic logic) {
         super(FXML);
         personListView.setItems(personList);
         personListView.setCellFactory(listView -> new PersonListViewCell());
-        nameCard.setText("Welcome to ExecutivePro v1.2");
+        //nameCard.setText("Welcome to ExecutivePro v1.2");
+        setClick();
+        logic.getFilteredPersonList().addListener(new ListChangeListener<Person>() {
+            @Override
+            public void onChanged(Change<? extends Person> change) {
+                Person person = personListView.getSelectionModel().getSelectedItem();
+                setInformation(person);
+            }
+        });
+
 
     }
 
+    public void setClick() {
+        personListView.setOnMouseClicked(mouseEvent -> {
+            Person person = personListView.getSelectionModel().getSelectedItem();
+            setInformation(person);
+        });
+    }
+    public void setInformation(Person person) {
+        name.setText("Name: " + person.getName().fullName);
+        employeeId.setText("EmployeeId: " + person.getEmployeeId().value);
+        phone.setText("Phone: " + person.getPhone().value);
+        address.setText("Address: " + person.getAddress().value);
+        email.setText("Email: " + person.getEmail().value);
+        department.setText("Department: " + person.getDepartment().value);
+    }
     /**
      * Custom {@code ListCell} that displays the graphics of a {@code Person} using a {@code PersonCard}.
      */
