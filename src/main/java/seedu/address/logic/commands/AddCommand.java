e3724940c0797707e590236f4c1c0f950f9e5ec9
RyanQiu1 <ryanqjx@gmail.com>
2023-03-22 14:11:08 +0800

    Update DG

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -63,7 +63,6 @@ public class AddCommand extends Command {
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
-
         if (model.hasRole(toAdd)) {
             throw new CommandException(MESSAGE_DUPLICATE_PERSON);
         }
