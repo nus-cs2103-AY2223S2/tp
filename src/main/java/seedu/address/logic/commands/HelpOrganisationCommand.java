3753ffba1c0ac084ffeefea7fccbf211cb5d70bf
nerdyboy98 <chewchunkiat@gmail.com>
2023-03-23 00:38:23 +0800

    Add Help category classes

--- src/main/java/seedu/address/logic/commands/HelpOrganisationCommand.java
+++ src/main/java/seedu/address/logic/commands/HelpOrganisationCommand.java
@@ -1,2 +1,18 @@
-package seedu.address.logic.commands;public class HelpOrganisationCommand {
+package seedu.address.logic.commands;
+
+import seedu.address.model.Model;
+
+/**
+ * Displays the different Organisation helps available
+ */
+public class HelpOrganisationCommand extends HelpCommand {
+
+    public static final String COMMAND_WORD = "organisation";
+
+    public static final String ORGANISATION_SYNTAX = "alphabetica? chronlogical?";
+
+    @Override
+    public CommandResult execute(Model model) {
+        return new CommandResult(ORGANISATION_SYNTAX);
+    }
 }
--- src/main/java/seedu/address/logic/commands/HelpStudentCommand.java
+++ src/main/java/seedu/address/logic/commands/HelpStudentCommand.java
@@ -1,2 +1,18 @@
-package seedu.address.logic.commands;public class HelpStudentCommand {
+package seedu.address.logic.commands;
+
+import seedu.address.model.Model;
+
+/**
+ * Displays the different Student helps available
+ */
+public class HelpStudentCommand extends HelpCommand {
+
+    public static final String COMMAND_WORD = "student";
+
+    public static final String STUDENT_CATEGORIES = "blabla";
+
+    @Override
+    public CommandResult execute(Model model) {
+        return new CommandResult(STUDENT_CATEGORIES);
+    }
 }
