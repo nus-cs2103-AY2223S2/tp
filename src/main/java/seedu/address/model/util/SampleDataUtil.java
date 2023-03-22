2f6ff2c2131bdb2a3bfd71e990589b4d1e0c57cc
Dylan Sng <dsja612@gmail.com>
2023-03-22 18:15:13 +0800

    Add expired dates in SampleDataUtil

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -28,18 +28,18 @@ public class SampleDataUtil {
         return new Role[] {
             new Role(new Name("SWE"), new Phone("87438807"), new Email("google@example.com"),
                 new Company("Google"), new JobDescription("Penultimate year preferred"),
-                getTagSet("Tech"), new Website("www.gohome.com"),
+                getTagSet("Java", "Golang"), new Website("www.gohome.com"),
                 new Salary("8000"), new Deadline(LocalDate.now().plusWeeks(3).toString()),
                 new Experience("Javascript - 1 Year")),
             new Role(new Name("Data Analyst"), new Phone("99272758"), new Email("facebook@example.com"),
                 new Company("Facebook"),
                 new JobDescription("Open to non-CS majors"),
-                getTagSet("Tech"), new Website("www.gohome.com"),
-                new Salary("10000"), new Deadline(LocalDate.now().plusYears(1).toString()),
+                getTagSet("FrontEnd", "React"), new Website("www.gohome.com"),
+                new Salary("10000"), new Deadline(LocalDate.now().minusDays(3).toString()),
                 new Experience("C - 1 Year")),
             new Role(new Name("Cloud Architect"), new Phone("93210283"), new Email("Amazon@example.com"),
                 new Company("Amazon"),
-                new JobDescription("Value contribution to open source"), getTagSet("Tech"),
+                new JobDescription("Value contribution to open source"), getTagSet("AWS"),
                 new Website("www.gohome.com"), new Salary("9000"),
                 new Deadline(LocalDate.now().plusDays(30).toString()), new Experience("Python - 1 Year"))
         };
