835d9b3280daa786c4c67278504159351962a550
William Gu <williamhaiweigu@gmail.com>
2023-03-22 20:09:33 +0800

    Add -dorce flag to parser

--- src/main/java/taa/logic/parser/ImportCommandParser.java
+++ src/main/java/taa/logic/parser/ImportCommandParser.java
@@ -3,17 +3,25 @@ package taa.logic.parser;
 import java.io.File;
 
 import taa.commons.core.Messages;
+import taa.commons.util.StringUtil;
 import taa.logic.commands.ImportCommand;
 import taa.logic.parser.exceptions.ParseException;
 
 /** Parses import command. Format: import FILE_NAME. */
 public class ImportCommandParser implements Parser<ImportCommand> {
+    private static final String FORCE_FLAG ="-force";
+    private static final String SYNTAX_ERR_MSG_FULL=
+            String.format(Messages.MESSAGE_INVALID_COMMAND_FORMAT, ImportCommand.MSG_USAGE);
     @Override
     public ImportCommand parse(String args) throws ParseException {
-        args = args.trim();
-        if (args.isEmpty()) {
-            throw new ParseException(String.format(Messages.MESSAGE_INVALID_COMMAND_FORMAT, ImportCommand.MSG_USAGE));
+        final String sanitizedArgs = StringUtil.rmAdjSpace(args).trim();
+        if (sanitizedArgs.isEmpty()) {
+            throw new ParseException(SYNTAX_ERR_MSG_FULL);
         }
-        return new ImportCommand(new File(args));
+        final String[] tokens=sanitizedArgs.split(" ");
+        final int fileIdx= FORCE_FLAG.equals(tokens[0])?1:0;
+        if(tokens.length>fileIdx+1)
+            throw new ParseException(SYNTAX_ERR_MSG_FULL);
+        return new ImportCommand(new File(tokens[fileIdx]));
     }
 }
