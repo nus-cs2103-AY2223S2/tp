0e31ca4c9cd8e2f99d74e4c6c929281e227576e3
YuanXun9834 <lollin9834@gmail.com>
2023-03-22 16:30:34 +0800

    Added Remind Command

---
+++ src/main/java/seedu/address/logic/commands/RemindCommand.java
@@ -0,0 +1,38 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.model.Model;
+import seedu.address.model.pet.DeadlineWithinThreeDaysPredicate;
+import seedu.address.model.pet.Pet;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ * Lists all pets in the pet list to the user.
+ */
+public class RemindCommand extends Command {
+
+    public static final String COMMAND_WORD = "remind";
+
+    public static final String MESSAGE_SUCCESS = "Here are all the upcoming reminders!";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all pets whose deadline is within 2 days "
+            + "and displays them as a list with index numbers.\n"
+            + "Example: " + COMMAND_WORD + " ";
+
+    private final DeadlineWithinThreeDaysPredicate predicate;
+
+    public RemindCommand(DeadlineWithinThreeDaysPredicate predicate) {
+        this.predicate = predicate;
+    }
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        model.updateFilteredPetList(predicate);
+
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+}
