dad8b80702d890bc80be0ee13b26f9123074d7e0
daitenshionyan <100074448+daitenshionyan@users.noreply.github.com>
2023-03-22 17:52:11 +0800

    Add limits to VaxTypeManager

--- src/main/java/seedu/vms/model/vaccination/VaxTypeManager.java
+++ src/main/java/seedu/vms/model/vaccination/VaxTypeManager.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableMap;
+import seedu.vms.commons.exceptions.LimitExceededException;
 
 
 /**
@@ -12,14 +13,31 @@ import javafx.collections.ObservableMap;
  * <p>The uniqueness of {@code VaxType} names are maintained.
  */
 public class VaxTypeManager {
+    public static final int DEFAULT_LIMIT = 30;
+
+    private static final String ERROR_FORMAT_LIMIT_EXCEEDED = "Limit of %d exceeded";
+
+    private final int limit;
+
     private final ObservableMap<String, VaxType> typeMap;
     private final ObservableMap<String, VaxType> unmodifiableTypeMap;
 
 
     /**
-     * Constructs an empty {@code VaxTypeManager}.
+     * Constructs a {@code VaxTypeManager} with the default limit.
      */
     public VaxTypeManager() {
+        this(DEFAULT_LIMIT);
+    }
+
+
+    /**
+     * Constructs a {@code VaxTypeManager}.
+     *
+     * @param limit - the size limit of this {@code VaxTypeManager}.
+     */
+    public VaxTypeManager(int limit) {
+        this.limit = limit;
         typeMap = FXCollections.observableHashMap();
         unmodifiableTypeMap = FXCollections.unmodifiableObservableMap(typeMap);
     }
@@ -29,8 +47,14 @@ public class VaxTypeManager {
      * Resets the data of this {@code VaxTypeManager} to the specified manager.
      *
      * @param manager - the state of the manager to reset to.
+     * @throws LimitExceededException if the size of the given manager is
+     *      larger than the set limit of this manager.
      */
     public void resetData(VaxTypeManager manager) {
+        if (manager.size() > limit) {
+            throw new LimitExceededException(String.format(ERROR_FORMAT_LIMIT_EXCEEDED,
+                    limit));
+        }
         typeMap.clear();
         typeMap.putAll(manager.typeMap);
     }
@@ -41,8 +65,14 @@ public class VaxTypeManager {
      * with the same name as the specified, that {@code VaxType} is replaced.
      *
      * @param vaxType - the {@code VaxType} to add.
+     * @throws LimitExceededException if the set limit will be exceeded after
+     *      this addition.
      */
     public void add(VaxType vaxType) {
+        if (typeMap.size() >= limit) {
+            throw new LimitExceededException(String.format(ERROR_FORMAT_LIMIT_EXCEEDED,
+                    limit));
+        }
         typeMap.put(vaxType.getName(), vaxType);
     }
 
@@ -83,6 +113,14 @@ public class VaxTypeManager {
      */
     public boolean contains(String name) {
         return typeMap.containsKey(name);
+    }
+
+
+    /**
+     * Returns the number of {@code VaxType} stored within this manager.
+     */
+    public int size() {
+        return typeMap.size();
     }
 
 
