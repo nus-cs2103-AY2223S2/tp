ca6259405eb74a2ceb2689c58c3577b8e0402c97
Zong Xun <zxun2000@gmail.com>
2023-03-23 20:11:14 +0800

    Fix checkstyle

--- src/main/java/seedu/address/logic/parser/StatsCommandParser.java
+++ src/main/java/seedu/address/logic/parser/StatsCommandParser.java
@@ -3,6 +3,9 @@ package seedu.address.logic.parser;
 import seedu.address.logic.commands.StatsCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
+/**
+ * Parses input arguments and creates a new StatsCommand object.
+ */
 public class StatsCommandParser implements Parser<StatsCommand> {
     /**
      * Parses {@code userInput} into a command and returns it.
