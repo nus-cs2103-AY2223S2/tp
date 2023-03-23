0dfc64550334ff5426d50002a9eb6c92fea4eaea
slackernoob <87632389+slackernoob@users.noreply.github.com>
2023-03-23 21:10:54 +0800

    Fix naming issue

--- src/main/java/seedu/vms/model/keyword/VaccinationKeyword.java
+++ src/main/java/seedu/vms/model/keyword/VaccinationKeyword.java
@@ -1,7 +1,7 @@
 package seedu.vms.model.keyword;
 
 public class VaccinationKeyword extends Keyword {
-    private static final String MAIN_KEY_NAME = "appointment";
+    private static final String MAIN_KEY_NAME = "vaccination";
 
     public VaccinationKeyword(String keyword) {
         super(keyword, MAIN_KEY_NAME);
