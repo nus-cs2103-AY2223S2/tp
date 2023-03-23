b548eeb41582f732fe6674c4e0110a9056f1b590
Yu Heng Lee <lee2000yh@gmail.com>
2023-03-23 13:46:14 +0800

    Bug Fix for AlertCommandParser
    fixed inconsistent behavior where parameter should be able to take in more than 1 occurrence and only the last occurrence should be taken.
    
    Added test cases.
    
    addresses #108

--- src/main/java/seedu/task/logic/parser/AlertCommandParser.java
+++ src/main/java/seedu/task/logic/parser/AlertCommandParser.java
@@ -5,6 +5,8 @@ import static seedu.task.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import seedu.task.logic.commands.AlertCommand;
 import seedu.task.logic.parser.exceptions.ParseException;
 
+import java.util.Arrays;
+
 /**
  * Parses input arguments and creates a new AlertCommand object
  */
@@ -20,11 +22,31 @@ public class AlertCommandParser implements Parser<AlertCommand> {
             if (args.isBlank()) {
                 return new AlertCommand();
             }
-            int timeframe = ParserUtil.parseTimeFrame(args);
+            // assume that args might have multiple integers, of which we should take the last one
+            // first check if any of the arguments are non-integers
+            if (!onlyDigits(args)) {
+                throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AlertCommand.MESSAGE_USAGE));
+            }
+            String[] timeframes = args.split(" ");
+            int timeframe = ParserUtil.parseTimeFrame(timeframes[timeframes.length - 1].trim());
             return new AlertCommand(timeframe);
         } catch (ParseException pe) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, AlertCommand.MESSAGE_USAGE), pe);
         }
+    }
+
+    /**
+     * Returns true if the input is only whitespaces and digits, else false.
+     */
+    public boolean onlyDigits(String input) {
+        String[] inputs = input.split(" ");
+        for (String i: inputs) {
+            if (i.trim().chars().allMatch(Character::isDigit)) {
+                continue;
+            }
+            return false;
+        }
+        return true;
     }
 }
