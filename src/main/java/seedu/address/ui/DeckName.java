2ddadd3d65cc58ede3cc2d10a80e67902285df07
junianob <junianob@gmail.com>
2023-03-23 22:28:10 +0800

    Style Deck Title

---
+++ src/main/java/seedu/address/ui/DeckName.java
@@ -0,0 +1,30 @@
+package seedu.address.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Region;
+import javafx.util.Pair;
+
+/**
+ * Individual element of the review panel
+ */
+public class DeckName extends UiPart<Region> {
+    private static final String FXML = "DeckName.fxml";
+
+    @FXML
+    private Label title;
+
+    @FXML
+    private Label description;
+
+    /**
+     * Creates a {@code ReviewStat} with the given {@code pair}.
+     */
+    public DeckName(Pair<String, String> pair) {
+        super(FXML);
+        title.setText(pair.getKey());
+        description.setText(pair.getValue());
+    }
+
+}
+
--- src/main/java/seedu/address/ui/DeckNamePanel.java
+++ src/main/java/seedu/address/ui/DeckNamePanel.java
@@ -4,6 +4,7 @@ import java.util.logging.Logger;
 
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.Region;
 import javafx.util.Pair;
@@ -25,8 +26,22 @@ public class DeckNamePanel extends UiPart<Region> {
     public DeckNamePanel(ObservableList<Pair<String, String> > deckNameList) {
         super(FXML);
         deckNameView.setItems(deckNameList);
-        deckNameView.setStyle("-fx-background-color: #FFFFFF;"
-                + "-fx-background-radius: 30; -fx-border-radius: 30; -fx-border-width:5;");
+        deckNameView.setCellFactory(listView -> new DeckNamePanel.DeckNameViewCell());
+        deckNameView.setStyle("-fx-background-color: transparent");
     }
 
+    class DeckNameViewCell extends ListCell<Pair<String, String>> {
+        @Override
+        protected void updateItem(Pair<String, String> pair, boolean empty) {
+            super.updateItem(pair, empty);
+            if (empty || pair == null) {
+                setGraphic(null);
+                setText(null);
+
+            } else {
+                setGraphic(new DeckName(pair).getRoot());
+                setStyle("fx-background-color:transparent");
+            }
+        }
+    }
 }
