ad218662a04ad3fe4ed6700a3307c62d26f5f96a
nerdyboy98 <chewchunkiat@gmail.com>
2023-03-24 00:10:35 +0800

    Update displayed help messages

--- src/main/java/seedu/address/logic/commands/HelpOrganisationCommand.java
+++ src/main/java/seedu/address/logic/commands/HelpOrganisationCommand.java
@@ -9,7 +9,7 @@ public class HelpOrganisationCommand extends HelpCommand {
 
     public static final String COMMAND_WORD = "organisation";
 
-    public static final String ORGANISATION_SYNTAX = "alphabetica? chronlogical?";
+    public static final String ORGANISATION_SYNTAX = "alphabetica? chronlogical? Update coming soon~";
 
     @Override
     public CommandResult execute(Model model) {
