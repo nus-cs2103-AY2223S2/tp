43ecd588f21e9781c6ce87c4bb71af9f07ea1d72
nerdyboy98 <chewchunkiat@gmail.com>
2023-03-23 16:14:03 +0800

    Split MESSAGE_USAGE into Syntax & Example

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -19,13 +19,10 @@ public class RemarkCommand extends Command {
 
     public static final String COMMAND_WORD = "remark";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Edits the remark of the person identified "
-            + "by the index number used in the last person listing. "
-            + "Existing remark will be overwritten by the input.\n"
-            + "Parameters: INDEX (must be a positive integer) "
-            + "r/ [REMARK]\n"
-            + "Example: " + COMMAND_WORD + " 1 "
+    public static final String MESSAGE_USAGE = "Remark Syntax: INDEX (must be a positive integer) "
+            + "r/ [REMARK]";
+
+    public static final String REMARK_EXAMPLE = "Example: " + COMMAND_WORD + " 1 "
             + "r/ Likes to swim.";
 
     public static final String MESSAGE_NOT_IMPLEMENTED_YET =
