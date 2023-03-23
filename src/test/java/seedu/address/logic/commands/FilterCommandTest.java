2c5120aad3554ea4745208f27eee6de22443f171
ezeAng <65171471+ezeAng@users.noreply.github.com>
2023-03-23 22:38:03 +0800

    Add Test Coverage

--- src/test/java/seedu/address/logic/commands/FilterCommandTest.java
+++ src/test/java/seedu/address/logic/commands/FilterCommandTest.java
@@ -7,7 +7,6 @@ import static seedu.address.commons.core.Messages.MESSAGE_PERSONS_LISTED_OVERVIE
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalPersons.ALICE;
 import static seedu.address.testutil.TypicalPersons.BENSON;
-
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import java.util.Arrays;
