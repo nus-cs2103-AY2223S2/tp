4881025cc3a34db3205e86284964c686e6fa237b
Elvern <elvern18@gmail.com>
2023-03-23 16:29:03 +0800

    create tests for DeleteMultipleIndexCommand

---
+++ src/test/java/seedu/address/logic/commands/DeleteMultipleIndexCommandTest.java
@@ -0,0 +1,40 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.model.AddressBook;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.person.Person;
+
+class DeleteMultipleIndexCommandTest {
+
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+    @Test
+    void execute_deleteMultipleValidIndex_success() {
+        List<Person> list = model.getFilteredPersonList();
+        int size = list.size();
+        ArrayList<Index> indexes = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            indexes.add(Index.fromZeroBased(i));
+        }
+        DeleteMultipleIndexCommand deleteCommand = new DeleteMultipleIndexCommand(indexes);
+
+        String expectedMessage = String.format(DeleteMultipleIndexCommand.MESSAGE_DELETE_PERSON_SUCCESS);
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+        expectedModel.deleteMultiplePersons(new ArrayList<>(list));
+
+        assertCommandSuccess(deleteCommand, model, expectedMessage, expectedModel);
+    }
+}
