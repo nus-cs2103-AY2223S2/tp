59c9edebeff4a9967cb32c3018d8b20371b36745
pzaiming <zaiming.pan@gmail.com>
2023-03-23 15:44:57 +0800

    Updated checkstyle issues

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -11,7 +11,6 @@ import java.util.logging.Logger;
 
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
-import seedu.address.MainApp;
 import seedu.address.commons.core.GuiSettings;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.model.job.Order;
