cc162d632b956f2e934ea34d36b574f373e0c037
leeyi45 <leeyi45@gmail.com>
2023-03-24 03:06:16 +0800

    Modify mark command to take multiple dates

--- src/main/java/seedu/address/logic/commands/MarkCommand.java
+++ src/main/java/seedu/address/logic/commands/MarkCommand.java
@@ -5,6 +5,7 @@ import static java.util.Objects.requireNonNull;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
@@ -19,22 +20,22 @@ import seedu.address.model.tutee.fields.Attendance;
  */
 public class MarkCommand extends Command {
     public static final String COMMAND_WORD = "mark";
-    public static final String EXPECTED_DATE_FORMAT = "YYYY-MM-DD";
+    public static final String EXPECTED_DATE_FORMAT = "yyyy-MM-dd";
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Marks the tutee identified "
                 + "by the index number used in the last tutee listing as present on a given date.\n"
                 + "If no date is provided for this command, then the current date will be used\n"
                 + "Example: " + COMMAND_WORD + " 1 2020-01-02";
 
     private final Index index;
-    private final LocalDate date;
+    private final List<LocalDate> dates;
 
     /**
      * Creates a MarkCommand for the tutee at the specified index and date.
      */
-    public MarkCommand(Index tuteeIndex, LocalDate date) {
+    public MarkCommand(Index tuteeIndex, List<LocalDate> date) {
         requireNonNull(tuteeIndex);
         this.index = tuteeIndex;
-        this.date = date;
+        this.dates = date;
     }
 
     @Override
@@ -49,12 +50,25 @@ public class MarkCommand extends Command {
         Tutee toMarkAttendance = lastShownList.get(index.getZeroBased());
         TuteeBuilder modified = new TuteeBuilder(toMarkAttendance);
         Attendance attendance = toMarkAttendance.getAttendance();
-        modified.withAttendance(attendance.markAttendance(date));
+
+        for (LocalDate date : dates) {
+            attendance = attendance.markAttendance(date);
+        }
+        modified.withAttendance(attendance);
         model.setTutee(toMarkAttendance, modified.build());
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(EXPECTED_DATE_FORMAT);
         return new CommandResult(
-            String.format("Marked %s's attendance for %s as present",
-                toMarkAttendance.getName(),
-                date.format(DateTimeFormatter.ofPattern(EXPECTED_DATE_FORMAT))
+            dates.stream()
+                .map(formatter::format)
+                .collect(Collectors.joining(
+                    ", ",
+                    String.format(
+                        "Marked %s's attendance as present for the following dates: ",
+                        toMarkAttendance.getName()
+                    ),
+                    ""
+                )
             )
         );
     }
