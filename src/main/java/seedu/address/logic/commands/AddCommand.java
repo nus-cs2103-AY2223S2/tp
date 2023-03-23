6edcc3450df45c3a4452a44ab9aecb1eaf03791c
Niu BoQian <e0774823@u.nus.edu>
2023-03-23 23:31:22 +0800

    fix the bug where tags does not allow spaces to exist.

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -34,8 +34,8 @@ public class AddCommand extends Command {
             + PREFIX_PHONE + "98765432 "
             + PREFIX_EMAIL + "johnd@example.com "
             + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 "
-            + PREFIX_SCHOOL + "AngloChinese "
-            + PREFIX_GRADELEVEL + "Sec3 "
+            + PREFIX_SCHOOL + "Anglo Chinese "
+            + PREFIX_GRADELEVEL + "Secondary 3 "
             + PREFIX_TAG + "friends "
             + PREFIX_TAG + "owesMoney";
 
