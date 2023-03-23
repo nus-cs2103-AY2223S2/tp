98e73003cf048e586ef64dd9503470313defa2fa
zhoyx <zhouyx2000@gmail.com>
2023-03-24 02:45:48 +0800

    Update validation regex for Genre to match predefined values

--- src/main/java/seedu/library/model/bookmark/Genre.java
+++ src/main/java/seedu/library/model/bookmark/Genre.java
@@ -3,6 +3,9 @@ package seedu.library.model.bookmark;
 import static java.util.Objects.requireNonNull;
 import static seedu.library.commons.util.AppUtil.checkArgument;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Represents a Bookmark's genre in the library.
  * Guarantees: immutable; is valid as declared in {@link #isValidGenre(String)}
@@ -19,6 +22,9 @@ public class Genre {
      */
     public static final String VALIDATION_REGEX = "[^\\s].*";
 
+    private static final List<String> VALID_GENRES = Arrays.asList("Action", "Adventure", "Comedy", "Drama",
+            "Fantasy", "Historical", "Horror", "Martial Arts", "Sci-Fi", "Mystery", "Romance", "Sports", "Others");
+
     public final String value;
 
     /**
@@ -33,10 +39,19 @@ public class Genre {
     }
 
     /**
+     * Constructs a regex string to match valid genres.
+     * @return Regex string.
+     */
+    private static String getValidationRegex() {
+        String validGenreOptions = String.join("|", VALID_GENRES);
+        return "\\b(" + validGenreOptions + ")\\b";
+    }
+    /**
      * Returns true if a given string is a valid genre.
      */
     public static boolean isValidGenre(String test) {
-        return test.matches(VALIDATION_REGEX);
+        String validationRegex = getValidationRegex();
+        return test.matches(validationRegex);
     }
 
     @Override
