5d9f90e1b31fbc0cfcc863d57a1a9676ce8110ba
ChickenChiang <96032660+ChickenChiang@users.noreply.github.com>
2023-03-23 12:33:58 +0800

    Fix behaviour for AddCommandParser and EditCommand parser
    Allow AddCommandParser to accept inputs with no phone number. This is done by removing `PREFIX_PHONE` as an argument from the `arePrefixesPresent()` method. An additional check then has to be done to check if `PREFIX_PHONE` is found in the input. If it is, we parse the argument with `parsePhone` method from `PaserUtil` class. If not, we set the phone to be the default empty phone from the phone class. The default empty phone is a final static field, and calling the method `getEmptyPhone` will return the same instance of the empty phone.
    Additionally, if an empty string or a string containing only white space is passed as an argument to `parsePhone` method, it will also return the default emptyPhone from the phone class.

--- src/main/java/teambuilder/logic/parser/AddCommandParser.java
+++ src/main/java/teambuilder/logic/parser/AddCommandParser.java
@@ -37,13 +37,15 @@ public class AddCommandParser implements Parser<AddCommand> {
                 ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL,
                         PREFIX_ADDRESS, PREFIX_MAJOR, PREFIX_TAG);
 
-        if (!arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_ADDRESS, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_MAJOR)
+        if (!arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_ADDRESS, PREFIX_EMAIL, PREFIX_MAJOR)
                 || !argMultimap.getPreamble().isEmpty()) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
         }
 
         Name name = ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get());
-        Phone phone = ParserUtil.parsePhone(argMultimap.getValue(PREFIX_PHONE).get());
+        Phone phone = argMultimap.getValue(PREFIX_PHONE).isPresent()
+                ? ParserUtil.parsePhone(argMultimap.getValue(PREFIX_PHONE).get())
+                : Phone.getEmptyPhone();
         Email email = ParserUtil.parseEmail(argMultimap.getValue(PREFIX_EMAIL).get());
         Address address = ParserUtil.parseAddress(argMultimap.getValue(PREFIX_ADDRESS).get());
         Major major = ParserUtil.parseMajor(argMultimap.getValue(PREFIX_MAJOR).get());
