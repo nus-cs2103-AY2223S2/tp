0f3c35e638d987ccd885c37591ab84c02a5f790a
WindyCall <E0902032@U.NUS.EDU>
2023-03-23 20:44:33 +0800

    Complete MarkAppointment

--- src/main/java/seedu/address/logic/commands/MarkAppointmentCommand.java
+++ src/main/java/seedu/address/logic/commands/MarkAppointmentCommand.java
@@ -1,11 +1,15 @@
 package seedu.address.logic.commands;
 
 import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
-import seedu.address.model.person.Person;
+import seedu.address.model.person.*;
+import seedu.address.model.tag.Tag;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
@@ -14,9 +18,49 @@ public class MarkAppointmentCommand extends Command {
 
     public static final String COMMAND_WORD = "markApp";
     public static final String MESSAGE_SUCCESS = "Appointment successfully marked";
+    public static final String NO_APPOINTMENT = "This patient does not have appointment yet";
+
+    private final Index index;
+
+    public MarkAppointmentCommand(Index index) {
+        this.index = index;
+    }
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        List<Person> lastShownList = model.getFilteredPersonList();
+
+        if (index.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+
+        Person personToEdit = lastShownList.get(index.getZeroBased());
+        Person editedPerson = createEditedPerson(personToEdit, null);
+
+        if (!personToEdit.hasAppointment()) {
+            throw new CommandException(NO_APPOINTMENT);
+        }
+
+        model.setPerson(personToEdit, editedPerson);
+        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
         return new CommandResult(MESSAGE_SUCCESS);
+
+    }
+
+    private static Person createEditedPerson(Person personToEdit, Appointment appointmentToAdd) {
+        assert personToEdit != null;
+
+        Name updatedName = personToEdit.getName();
+        Phone updatedPhone = personToEdit.getPhone();
+        Email updatedEmail = personToEdit.getEmail();
+        Address updatedAddress = personToEdit.getAddress();
+        Set<Tag> updatedTags = personToEdit.getTags();
+        Age updatedAge = personToEdit.getAge();
+        LocalDateTime updatedTime = personToEdit.getTime();
+        MedicalCondition updatedMedicalCondition = personToEdit.getMedicalCondition();
+
+        return new Person(updatedName, updatedPhone, updatedEmail, updatedAddress, updatedAge,
+                updatedTags, updatedTime, updatedMedicalCondition, appointmentToAdd);
     }
 }
--- src/main/java/seedu/address/logic/parser/MarkAppointmentCommandParser.java
+++ src/main/java/seedu/address/logic/parser/MarkAppointmentCommandParser.java
@@ -1,11 +1,23 @@
 package seedu.address.logic.parser;
 
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.commands.MarkAppointmentCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
 public class MarkAppointmentCommandParser implements Parser<MarkAppointmentCommand> {
 
+    // notation: markApp {index}
     public MarkAppointmentCommand parse(String args) throws ParseException {
-        return new MarkAppointmentCommand();
+        String trimmedArgs = args.trim();
+        try {
+            Index index = ParserUtil.parseIndex(trimmedArgs);
+            return new MarkAppointmentCommand(index);
+        } catch (ParseException pe) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE), pe);
+        }
     }
 }
