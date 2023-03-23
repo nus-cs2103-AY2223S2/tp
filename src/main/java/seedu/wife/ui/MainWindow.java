239ca2b01d068a4d42ba8f6169bb7df71b4113e7
Ritika Joshi <ritikajoshi0114@gmail.com>
2023-03-23 20:38:18 +0800

    Fix checkstyle

--- src/main/java/seedu/wife/ui/MainWindow.java
+++ src/main/java/seedu/wife/ui/MainWindow.java
@@ -130,10 +130,10 @@ public class MainWindow extends UiPart<Stage> {
         commandBoxPlaceholder.setBackground(new Background(new BackgroundFill(
                 Color.valueOf("D9D9D9"), new CornerRadii(5), null)));
 
-//        todo: link enter button and change colour and corner radius
-//        enterButton.onKeyPressedProperty().
-//        enterButton.setBackground(new Background(new BackgroundFill(
-//                Color.valueOf("006994"), new CornerRadii(5), null)));
+        //      link enter button and change colour and corner radius
+        //      enterButton.onKeyPressedProperty().
+        //      enterButton.setBackground(new Background(new BackgroundFill(
+        //      Color.valueOf("006994"), new CornerRadii(5), null)));
     }
 
     /**
