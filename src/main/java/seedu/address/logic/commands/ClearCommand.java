5c2378e3776afdcfc4fb0f4d48c278a7bc7a63c5
Nathan Tran <80334013+rockman007372@users.noreply.github.com>
2023-03-24 12:49:47 +0800

    Update ClearCommand.java

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -5,9 +5,9 @@ import static java.util.Objects.requireNonNull;
 import seedu.address.model.Model;
 
 /**
- * Clears the master deck.
+ * Asks for user confirmation before clearing the master deck.
  */
-public class ClearCommand extends Command { //todo: this command is dangerous and should warn users in the future
+public class ClearCommand extends Command {
 
     public static final String COMMAND_WORD = "clear";
     public static final String MESSAGE_SUCCESS = "Please confirm before continue!";
