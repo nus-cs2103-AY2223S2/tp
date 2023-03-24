8a52ce537ca299628067694daef8dbad0de0da1c
chuakid <chuakid@gmail.com>
2023-03-25 00:42:28 +0800

    Update import meeting usage message

--- src/main/java/seedu/address/logic/commands/ImportMeetingsCommand.java
+++ src/main/java/seedu/address/logic/commands/ImportMeetingsCommand.java
@@ -14,7 +14,7 @@ public class ImportMeetingsCommand extends Command {
     public static final String COMMAND_WORD = "importm";
     static final String DUPLICATE_MEETING = "Duplicate meeting found";
     static final String SUCCESS = "Meetings imported";
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Imports meetings in JSON format\n" + "Parameters: `-f "
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Imports meetings in JSON format\n" + "Parameters: -f "
             + " to force imports regardless of duplicates";
     private final List<Meeting> meetings;
     private final boolean isForced;
