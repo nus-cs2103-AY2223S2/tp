86eccc1cd3f1ce15c2e2092b1a0b8b9b89a07d2e
Vigneshwar Hariharan <88077703+vigonometry@users.noreply.github.com>
2023-03-24 13:25:02 +0800

    Update src/main/java/seedu/address/logic/commands/FindPatientCommand.java

--- src/main/java/seedu/address/logic/commands/FindPatientCommand.java
+++ src/main/java/seedu/address/logic/commands/FindPatientCommand.java
@@ -27,7 +27,7 @@ public class FindPatientCommand extends Command {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds a patient in the address book. "
             + "Parameters: "
-            + "(" + PREFIX_NAME + "NAME) "
+            + "[" + PREFIX_NAME + "NAME] "
             + "(" + PREFIX_PHONE + "PHONE) "
             + "(" + PREFIX_EMAIL + "EMAIL) "
             + "(" + PREFIX_HEIGHT + "HEIGHT) "
