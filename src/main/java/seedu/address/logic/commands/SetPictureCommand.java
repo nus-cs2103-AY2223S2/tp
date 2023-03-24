f0f880443d32f5fd820a83afbc96517000b59735
abenx162 <benxuan162@gmail.com>
2023-03-24 16:47:07 +0800

    Add functionality to copy image from file explorer.

---
+++ src/main/java/seedu/address/logic/commands/SetPictureCommand.java
@@ -0,0 +1,106 @@
+package seedu.address.logic.commands;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.employee.Employee;
+import seedu.address.model.employee.EmployeeId;
+
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.List;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.filechooser.FileView;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Sets the picture of an employee, identified using it's displayed index from the address book.
+ */
+public class SetPictureCommand extends Command {
+    public static final String COMMAND_WORD = "setpicture";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Sets the picture of an employee, identified by the EMPLOYEE_ID used in the displayed employee list.\n"
+            + "Parameters: EMPLOYEE_ID (must be a positive integer)\n"
+            + "Example: " + COMMAND_WORD + " 1";
+
+    public static final String MESSAGE_SET_PICTURE_SUCCESS = "Set picture for Employee: %1$s";
+    public static final String MESSAGE_SET_PICTURE_FAILURE = "Failed to set picture.";
+    public static final String MESSAGE_IO_ERROR = "I/O error occurred.";
+
+    private final EmployeeId employeeId;
+
+    public SetPictureCommand(EmployeeId employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        List<Employee> lastShownList = model.getFilteredEmployeeList();
+        Employee employeeToSetPicture;
+
+        // https://stackoverflow.com/questions/4096433/making-jfilechooser-show-image-thumbnails
+        class ThumbNailView extends FileView {
+            public Icon getIcon(File f){
+                Icon icon = createImageIcon(f.getPath());
+                return icon;
+            }
+
+            private ImageIcon createImageIcon(String path) {
+                ImageIcon icon = new ImageIcon(path);
+                Image image = icon.getImage();
+                Image scaledImage = image.getScaledInstance( 16, 16,  Image.SCALE_SMOOTH ) ;
+                return new ImageIcon(scaledImage);
+            } 
+        }
+
+        for (Employee employee : lastShownList) {
+            if (employee.getEmployeeId().equals(employeeId)) {
+                employeeToSetPicture = employee;
+
+                JFileChooser chooser = new JFileChooser();
+                FileNameExtensionFilter filter = new FileNameExtensionFilter(
+                        "JPG, JPEG, or PNG images", "jpg", "jpeg", "png");
+                ThumbNailView thumbNailView = new ThumbNailView();
+                chooser.setFileFilter(filter);
+                chooser.setFileView(thumbNailView);
+                chooser.setDialogTitle("Choose input file");
+
+                int returnVal = chooser.showOpenDialog(null);
+                if (returnVal != JFileChooser.APPROVE_OPTION) {
+                    throw new CommandException(MESSAGE_SET_PICTURE_FAILURE);
+                }
+                Path sourcePath = chooser.getSelectedFile().toPath();
+                Path destPath = new File("src/main/resources/employeepictures/"
+                        + employeeToSetPicture.getName().fullName + ".png").toPath();
+                try {
+                    Files.copy(sourcePath, destPath, StandardCopyOption.REPLACE_EXISTING);
+                    System.out.println(destPath);
+                } catch (IOException e) {
+                    throw new CommandException(MESSAGE_IO_ERROR);
+                }
+
+                return new CommandResult(String.format(MESSAGE_SET_PICTURE_SUCCESS, employeeToSetPicture));
+            }
+        }
+        throw new CommandException(Messages.MESSAGE_INVALID_EMPLOYEE_DISPLAYED_INDEX);
+    }
+
+
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof SetPictureCommand // instanceof handles nulls
+                && employeeId.equals(((SetPictureCommand) other).employeeId)); // state check
+    }
+}
---
+++ src/main/java/seedu/address/logic/parser/SetPictureCommandParser.java
@@ -0,0 +1,30 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.DeleteCommand;
+import seedu.address.logic.commands.SetPictureCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.employee.EmployeeId;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+/**
+ * Parses input arguments and creates a new DeleteCommand object
+ */
+public class SetPictureCommandParser implements Parser<SetPictureCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the DeleteCommand
+     * and returns a DeleteCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public SetPictureCommand parse(String args) throws ParseException {
+        try {
+            EmployeeId index = ParserUtil.parseEmployeeId(args);
+            return new SetPictureCommand(index);
+        } catch (ParseException pe) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, SetPictureCommand.MESSAGE_USAGE), pe);
+        }
+    }
+
+}
