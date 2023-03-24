f0b484e2ece991118c7c13adeaa8e304593738ee
gitsac <isaacchai99@gmail.com>
2023-03-25 00:44:46 +0800

    Move imports in test file to fix checkstyle

--- src/test/java/seedu/address/logic/commands/EditExpenseCommandTest.java
+++ src/test/java/seedu/address/logic/commands/EditExpenseCommandTest.java
@@ -1,9 +1,9 @@
 package seedu.address.logic.commands;
 
-import java.time.LocalDate;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.time.LocalDate;
 
 import org.junit.jupiter.api.Test;
 
