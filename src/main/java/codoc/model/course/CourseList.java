bff5f538f029632d26043709b47eeff48af6ccb8
anchengyang <anchengyang72@gmail.com>
2023-03-25 01:22:52 +0800

    add left panel for course list

--- src/main/java/codoc/model/course/CourseList.java
+++ src/main/java/codoc/model/course/CourseList.java
@@ -16,4 +16,13 @@ public class CourseList {
                     "Computer Engineering"
             )
     );
+
+    @Override
+    public String toString() {
+        String result = "";
+        for (int i = 0; i < COURSE_LIST.size(); i++) {
+            result = result + String.format("\n%d. %s", i + 1, COURSE_LIST.get(i));
+        }
+        return result;
+    }
 }
