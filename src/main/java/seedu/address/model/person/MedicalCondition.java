c0724a8162ebcd102eb3b4f1647f355bdf5571f0
Teo Weide <teoweide1999@gmail.com>
2023-03-25 01:52:49 +0800

    Add test cases for ParseUtilTest

--- src/main/java/seedu/address/model/person/MedicalCondition.java
+++ src/main/java/seedu/address/model/person/MedicalCondition.java
@@ -9,6 +9,21 @@ public class MedicalCondition {
         this.value = tagName;
     }
 
+    /**
+     * Returns true if the value is valid medical condition
+     */
+    public boolean isValidCondition(String value) {
+        if (value == "") {
+            return true;
+        }
+        return false;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        return obj == this || (obj instanceof MedicalCondition
+                && obj.equals(((MedicalCondition) obj).value));
+    }
+
     @Override
     public String toString() {
         return value;
