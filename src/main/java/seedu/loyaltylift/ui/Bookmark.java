9a0a3212aa9e8ad83a2f9d57707ce5cb3920598b
JavonTeo <javonteo@hotmail.com>
2023-03-24 16:44:52 +0800

    Added test for Bookmark.

--- src/main/java/seedu/loyaltylift/ui/Bookmark.java
+++ src/main/java/seedu/loyaltylift/ui/Bookmark.java
@@ -5,6 +5,9 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import seedu.loyaltylift.model.customer.Marked;
 
+/**
+ * A small icon that indicates whether the customer is bookmarked.
+ */
 public class Bookmark extends UiPart<StackPane> {
 
     private static final String FXML = "Bookmark.fxml";
@@ -12,14 +15,26 @@ public class Bookmark extends UiPart<StackPane> {
     @FXML
     private ImageView markedIcon;
 
+    /**
+     * Creates a {@code Bookmark} based on the marked status of the customer.
+     * @param marked The marked status of the customer.
+     */
     private Bookmark(Marked marked) {
         super(FXML);
-
 
         markedIcon.setVisible(marked.value);
     }
 
+    /**
+     * Constructs a {@code Bookmark} based on the marked status of the customer.
+     * @param marked The marked status of the customer.
+     * @return An instance of the Bookmark.
+     */
     public static Bookmark createBookmarkIcon(Marked marked) {
-        return new Bookmark(marked);
+        if (marked instanceof Marked) {
+            return new Bookmark(marked);
+        } else {
+            throw new NullPointerException();
+        }
     }
 }
