4c22d93672ddb74cd855c6bdc46504baf060c23d
CarrieLi1015 <e0775592@u.nus.edu>
2023-03-24 15:09:11 +0800

    Add chart "tooltip"
    Added in chart "tooltip", when mouse
    hover over a certain node, the respective information
    will be displayed, including exam label and exact mark.

--- src/main/java/seedu/address/ui/ScoreListPanel.java
+++ src/main/java/seedu/address/ui/ScoreListPanel.java
@@ -2,14 +2,21 @@ package seedu.address.ui;
 
 import java.util.logging.Logger;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.Cursor;
+import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.chart.Axis;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.model.person.Person;
 import seedu.address.model.score.Score;
@@ -29,7 +36,7 @@ public class ScoreListPanel extends UiPart<Region> {
     @FXML
     private Label nameChart;
     @FXML
-    private LineChart scoreChart;
+    private LineChart<String, Double> scoreChart;
     @FXML
     private Axis xAxis;
     @FXML
@@ -44,30 +51,85 @@ public class ScoreListPanel extends UiPart<Region> {
         name.setText("No student being checked now");
         nameChart.setText("No student being checked now");
 
-        if (person != null) {
-            scoreListView.setItems(person.getScoreList().getInternalList());
-        }
         scoreListView.setCellFactory(listView -> new ScoreListPanel.ScoreListViewCell());
 
         if (person != null) {
+            scoreListView.setItems(person.getScoreList().getInternalList());
             if (person.getScoreList().getInternalList().size() != 0) {
-                name.setText("Score history for " + person.getName().fullName);
-                nameChart.setText("Chart for " + person.getName().fullName);
-                scoreChart.setVisible(true);
-                xAxis.setLabel("Date");
-                yAxis.setLabel("Score");
-                XYChart.Series series = new XYChart.Series();
-                for (int i = 0; i < person.getScoreList().getInternalList().size() && i < 5; i++) {
-                    //String label = person.getScoreList().getInternalList().get(i).getLabel().toString();
-                    String date = person.getScoreList().getInternalList().get(i).getDate().toString();
-                    Double value = person.getScoreList().getInternalList().get(i).getValue().value;
-                    series.getData().add(new XYChart.Data<>(date, value));
-                }
-                scoreChart.getData().add(series);
+                newChart(person);
             } else {
                 name.setText("No score history found for " + person.getName().fullName);
                 nameChart.setText("No score chart for " + person.getName().fullName);
             }
+        }
+    }
+
+    private void newChart(Person person) {
+        name.setText("Score history for " + person.getName().fullName);
+        nameChart.setText("Chart for " + person.getName().fullName);
+        xAxis.setLabel("Date");
+        yAxis.setLabel("Score");
+        scoreChart.setVisible(true);
+        XYChart.Series<String, Double> series = new XYChart.Series<>();
+
+        Region chartContent = (Region) scoreChart.lookup(".chart-content");
+        for (Node node: chartContent.getChildrenUnmodifiable()) {
+            if (node instanceof Group) {
+                node.toFront();
+                Group plotArea = (Group) node;
+                plotArea.setClip(null);
+            }
+        }
+
+        for (int i = 0; i < person.getScoreList().getInternalList().size() && i < 5; i++) {
+            String date = person.getScoreList().getInternalList().get(i).getDate().toString();
+            Double value = person.getScoreList().getInternalList().get(i).getValue().value;
+            String label = person.getScoreList().getInternalList().get(i).getLabel().toString();
+            XYChart.Data<String, Double> data = new XYChart.Data<>(date, value);
+            data.setNode(new HoveredThresholdNode(data.getYValue(), label));
+            series.getData().add(data);
+        }
+
+        scoreChart.getData().add(series);
+    }
+
+    private class HoveredThresholdNode extends StackPane {
+        HoveredThresholdNode(Double scoreValue, String examLabel) {
+            final Label label = createDataThresholdLabel(scoreValue, examLabel);
+
+            setOnMouseEntered(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent mouseEvent) {
+                    getChildren().setAll(label);
+                    setCursor(Cursor.NONE);
+                    toFront();
+                }
+            });
+
+            setOnMouseExited(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent mouseEvent) {
+                    getChildren().clear();
+                    setCursor(Cursor.CROSSHAIR);
+                }
+            });
+        }
+
+        private Label createDataThresholdLabel(Double scoreValue, String examLabel) {
+            final Label label = new Label(examLabel + ": " + scoreValue);
+            label.getStyleClass().addAll("default-color0", "chart-line-symbol", "chart-series-line");
+            label.setStyle("-fx-font-size: 14; -fx-font-weight: bold; -fx-font-family: \"SansSerif\";");
+
+            if (scoreValue >= 80) {
+                label.setTextFill(Color.GREEN);
+            } else if (50 <= scoreValue && scoreValue < 80) {
+                label.setTextFill(Color.YELLOW);
+            } else {
+                label.setTextFill(Color.RED);
+            }
+
+            label.setMinSize(Label.USE_PREF_SIZE, Label.USE_PREF_SIZE);
+            return label;
         }
     }
 
