b885456fcf2093c0ced6dcf031691656beba24c5
chuakid <chuakid@gmail.com>
2023-03-25 00:05:42 +0800

    Refactor import persons json parsing into ImportPersonsParser Added tests for ImportPersonsParser

--- src/main/java/seedu/address/logic/commands/ImportPersonsCommand.java
+++ src/main/java/seedu/address/logic/commands/ImportPersonsCommand.java
@@ -1,63 +1,66 @@
 package seedu.address.logic.commands;
 
-import java.io.IOException;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
-import seedu.address.commons.exceptions.IllegalValueException;
-import seedu.address.commons.util.JsonUtil;
+import java.util.List;
+
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.person.Person;
 import seedu.address.model.person.exceptions.DuplicatePersonException;
-import seedu.address.storage.JsonAdaptedPerson;
 
 /**
  * This class represents a command for importing of persons
  */
 public class ImportPersonsCommand extends Command {
     public static final String COMMAND_WORD = "import";
-    static final String MALFORMED_JSON = "JSON input malformed";
     static final String DUPLICATE_PERSON = "Duplicate person found";
     static final String SUCCESS = "Persons imported";
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Imports persons in JSON format\n" + "Parameters: f/ "
-            + "true: to force imports regardless of duplicates";
-    private final String json;
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Imports persons in JSON format\n" + "Parameters: -f"
+            + " to force imports regardless of duplicates";
+    private final List<Person> people;
     private final boolean isForced;
 
     /**
      * Creates a new ImportPersonsCommand
      *
-     * @param json     person to be imported
+     * @param people   people to import
      * @param isForced whether to force imports regardless of duplicates
      */
-    public ImportPersonsCommand(String json, boolean isForced) {
-        this.json = json;
+    public ImportPersonsCommand(List<Person> people, boolean isForced) {
+        this.people = people;
         this.isForced = isForced;
     }
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
         try {
-            JsonAdaptedPerson[] personList = JsonUtil.fromJsonString(json, JsonAdaptedPerson[].class);
             if (isForced) {
-                for (JsonAdaptedPerson jsonAdaptedPerson : personList) {
-                    if (!model.hasPerson(jsonAdaptedPerson.toModelType())) {
-                        model.addPerson(jsonAdaptedPerson.toModelType());
+                for (Person person : people) {
+                    if (!model.hasPerson(person)) {
+                        model.addPerson(person);
                     }
                 }
             } else {
-                for (JsonAdaptedPerson jsonAdaptedPerson : personList) {
-                    if (model.hasPerson(jsonAdaptedPerson.toModelType())) {
+                for (Person person : people) {
+                    if (model.hasPerson(person)) {
                         throw new DuplicatePersonException();
                     }
                 }
-                for (JsonAdaptedPerson p : personList) {
-                    model.addPerson(p.toModelType());
+                for (Person person : people) {
+                    model.addPerson(person);
                 }
             }
             return new CommandResult(SUCCESS);
-        } catch (IOException | IllegalValueException e) {
-            throw new CommandException(MALFORMED_JSON);
         } catch (DuplicatePersonException e) {
-            throw new CommandException(DUPLICATE_PERSON);
+            throw new CommandException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, DUPLICATE_PERSON));
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return o == this // short circuit if same object
+                || (o instanceof ImportPersonsCommand
+                && people.equals(((ImportPersonsCommand) o).people));
     }
 }
