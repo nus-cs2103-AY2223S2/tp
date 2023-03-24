ceed9f68124c196ff97f0120e6f97a6b2271e42a
chongweiguan <75872136+chongweiguan@users.noreply.github.com>
2023-03-24 12:46:58 +0800

    commit main

--- src/main/java/trackr/MainApp.java
+++ src/main/java/trackr/MainApp.java
@@ -55,7 +55,7 @@ public class MainApp extends Application {
         logger.info("=============================[ Initializing Trackr ]===========================");
         super.init();
 
-        assert false;
+        //assert false;
 
         AppParameters appParameters = AppParameters.parse(getParameters());
         config = initConfig(appParameters.getConfigPath());
