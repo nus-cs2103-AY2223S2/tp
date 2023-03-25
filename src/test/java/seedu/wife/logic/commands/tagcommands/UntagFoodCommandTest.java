3caa2a778f98e7b37ae59dc525628a78c9f28f6e
Ritika Joshi <ritikajoshi0114@gmail.com>
2023-03-26 00:09:34 +0800

    Update UntagFoodCommandTest test cases

--- src/test/java/seedu/wife/logic/commands/tagcommands/UntagFoodCommandTest.java
+++ src/test/java/seedu/wife/logic/commands/tagcommands/UntagFoodCommandTest.java
@@ -1,7 +1,27 @@
 package seedu.wife.logic.commands.tagcommands;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static seedu.wife.logic.commands.CommandTestUtil.VALID_TAG_DAIRY;
+import static seedu.wife.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.wife.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.wife.testutil.TypicalIndex.INDEX_FIRST_FOOD;
+import static seedu.wife.testutil.TypicalIndex.INDEX_THIRD_FOOD;
+import static seedu.wife.testutil.TypicalWife.getTypicalWife;
+import static seedu.wife.testutil.TypicalWife.getTypicalWifeWithoutFoodTag;
 
+import org.junit.jupiter.api.Test;
+import seedu.wife.logic.commands.generalcommands.ExitCommand;
+import seedu.wife.model.Model;
+import seedu.wife.model.ModelManager;
+import seedu.wife.model.UserPrefs;
+import seedu.wife.model.Wife;
+import seedu.wife.model.food.Food;
+import seedu.wife.model.tag.Tag;
+import seedu.wife.testutil.FoodBuilder;
+import seedu.wife.testutil.TagBuilder;
+import seedu.wife.testutil.TypicalTag;
 
 
 public class UntagFoodCommandTest {
@@ -11,98 +31,87 @@ public class UntagFoodCommandTest {
 
     @Test
     public void execute_untagFood_success() {
-    //        final Model model = new ModelManager(getTypicalWife(), new UserPrefs());
-    //
-    //        // Creating a copy of first Food of model and adding a dairy tag
-    //        Food foodWithTag = new FoodBuilder(model.getFilteredFoodList().get(0))
-    //                .withTags(VALID_TAG_DAIRY)
-    //                .build();
-    //
-    //        Food foodWithoutTag = new FoodBuilder(model.getFilteredFoodList().get(0))
-    //                .build();
-    //        Tag tag = new TagBuilder()
-    //                .withTagName(VALID_TAG_DAIRY)
-    //                .build();
-    //
-    //        // Creating an expected model for comparison -> should not have tag
-    //        Model expectedModel = new ModelManager(new Wife(model.getWife()), new UserPrefs());
-    //
-    //        // Setting expected model's first Food to the tagged first Food
-    //        // expectedModel.createTag(tag);
-    //        expectedModel.setFood(expectedModel.getFilteredFoodList().get(0), foodWithoutTag);
-    //
-    //        // tag food on original model
-    //        model.createTag(tag);
-    //        model.setFood(model.getFilteredFoodList().get(0), foodWithTag);
-    //        UntagFoodCommand untagFoodCommand = new UntagFoodCommand(tag.getTagName(), INDEX_FIRST_FOOD);
-    //
-    //        String expectedMessage = String.format(EXPECTED_ERROR_TAG_NOT_FOUND, tag.getTagName());
-    //        assertCommandSuccess(untagFoodCommand, model, expectedMessage, expectedModel);
-    //        //      assertFalse(model.getFilteredFoodList().get(0).getTags().contains(tag));
-    //    }
+        final Model model = new ModelManager(getTypicalWife(), new UserPrefs());
 
-    //    @Test
-    //    public void execute_tagFoodWithoutExistingTagInModel_throwsCommandException() {
-    //        final Model model = new ModelManager(getTypicalWifeWithoutFoodTag(), new UserPrefs());
-    //        Tag tag = new TagBuilder().withTagName(VALID_TAG_DAIRY).build();
-    //        TagFoodCommand tagFoodCommand = new TagFoodCommand(tag.getTagName(), INDEX_FIRST_FOOD);
-    //
-    //        String expectedMessage = String.format(EXPECTED_ERROR_TAG_NOT_FOUND, tag.getTagName());
-    //        assertCommandFailure(tagFoodCommand, model, expectedMessage);
-    //    }
-    //
-    //    @Test
-    //    public void execute_foodIndexNotFound_throwsCommandException() {
-    //        final Model model = new ModelManager(getTypicalWifeWithoutFoodTag(), new UserPrefs());
-    //        Tag tag = new TagBuilder().withTagName(VALID_TAG_DAIRY).build();
-    //        model.createTag(tag);
-    //        TagFoodCommand tagFoodCommand = new TagFoodCommand(tag.getTagName(), INDEX_THIRD_FOOD);
-    //
-    //        assertCommandFailure(tagFoodCommand, model, EXPECTED_ERROR_FOOD_NOT_FOUND);
-    //    }
+        // Creating a copy of first Food of model and adding a dairy tag
+        Food foodWithTag = new FoodBuilder(model.getFilteredFoodList().get(0))
+                .withTags(VALID_TAG_DAIRY)
+                .build();
+        Food foodWithoutTag = new FoodBuilder(model.getFilteredFoodList().get(0))
+                .build();
+        Tag tag = new TagBuilder()
+                .withTagName(VALID_TAG_DAIRY)
+                .build();
 
-    //    @Test
-    //    public void execute_duplicateTagInFood_throwsCommandException() {
-    //        final Model model = new ModelManager(getTypicalWifeWithoutFoodTag(), new UserPrefs());
-    //        Tag tag = new TagBuilder().withTagName(VALID_TAG_DAIRY).build();
-    //        model.createTag(tag);
-    //
-    //        Food food = model.getFilteredFoodList().get(0);
-    //        Set<Tag> tagSet = food.getCurrentTags();
-    //        tagSet.add(tag);
-    //        model.setFood(food, food.createNewFoodWithNewTags(food, tagSet));
-    //
-    //        TagFoodCommand tagFoodCommand = new TagFoodCommand(tag.getTagName(), INDEX_FIRST_FOOD);
-    //
-    //        String expectedMessage = String.format(EXPECTED_ERROR_DUPLICATE_TAG, food.getName(), tag.getTagName());
-    //        assertCommandFailure(tagFoodCommand, model, expectedMessage);
-    //    }
+        // Creating an expected model for comparison -> should not have tag
+        Model expectedModel = new ModelManager(new Wife(model.getWife()), new UserPrefs());
 
-    //    @Test
-    //    public void equals() {
-    //        Tag dairyTag = TypicalTag.DAIRY_TAG;
-    //        Tag chocolateTag = TypicalTag.CHOCOLATE_TAG;
-    //
-    //        TagFoodCommand tagFoodWithDairyCommand = new TagFoodCommand(dairyTag.getTagName(), INDEX_FIRST_FOOD);
-    //        TagFoodCommand tagFoodWithChocolateCommand = new TagFoodCommand(chocolateTag.getTagName(),
-        //        INDEX_FIRST_FOOD);
-    //
-    //        // same object -> returns true
-    //        assertEquals(tagFoodWithDairyCommand, tagFoodWithDairyCommand);
-    //
-    //        // same tags and same food -> returns true
-    //        Tag dairyTagCopy = new TagBuilder().withTagName(VALID_TAG_DAIRY).build();
-    //        TagFoodCommand tagFoodWithDairyCommandCopy = new TagFoodCommand(dairyTagCopy.getTagName(),
-        //        INDEX_FIRST_FOOD);
-    //        assertEquals(tagFoodWithDairyCommand, tagFoodWithDairyCommandCopy);
-    //
-    //        // different commands -> returns false
-    //        assertNotEquals(tagFoodWithDairyCommand, new ExitCommand());
-    //
-    //        // null -> returns false
-    //        assertNotEquals(tagFoodWithDairyCommand, null);
-    //
-    //        // different tag command -> returns false
-    //        assertNotEquals(tagFoodWithDairyCommand, tagFoodWithChocolateCommand);
+        // Setting expected model's first Food to the untagged first Food
+        expectedModel.createTag(tag);
+        expectedModel.setFood(expectedModel.getFilteredFoodList().get(0), foodWithoutTag);
+
+        // tag food on original model
+        model.createTag(tag);
+        model.setFood(model.getFilteredFoodList().get(0), foodWithTag);
+        UntagFoodCommand untagFoodCommand = new UntagFoodCommand(tag.getTagName(), INDEX_FIRST_FOOD);
+
+        String expectedMessage = String.format(EXPECTED_ERROR_TAG_NOT_FOUND, tag.getTagName());
+//        assertCommandSuccess(untagFoodCommand, model, expectedMessage, expectedModel);
+        assertFalse(model.getFilteredFoodList().get(0).getTags().contains(tag));
+    }
+
+    @Test
+    public void execute_untagFoodWithoutExistingTag_throwsCommandException() {
+        final Model model = new ModelManager(getTypicalWifeWithoutFoodTag(), new UserPrefs());
+        Tag dairyTag = new TagBuilder().withTagName(VALID_TAG_DAIRY).build();
+        UntagFoodCommand untagFoodCommand = new UntagFoodCommand(dairyTag.getTagName(), INDEX_FIRST_FOOD);
+
+        //tag does not exist in model
+        String expectedMessage = String.format(EXPECTED_ERROR_TAG_NOT_FOUND, dairyTag.getTagName());
+        assertCommandFailure(untagFoodCommand, model, expectedMessage);
+
+        //tag name is not currently tagged to item(does not exist in food item tag set)
+        //but tag does exist in model
+        model.createTag(dairyTag);
+        String expectedMessageToo = String.format(EXPECTED_ERROR_TAG_NOT_FOUND, dairyTag.getTagName());
+        assertCommandFailure(untagFoodCommand, model, expectedMessageToo);
+    }
+
+    @Test
+    public void execute_foodIndexNotFound_throwsCommandException() {
+        final Model model = new ModelManager(getTypicalWifeWithoutFoodTag(), new UserPrefs());
+        Tag tag = new TagBuilder().withTagName(VALID_TAG_DAIRY).build();
+        model.createTag(tag);
+        UntagFoodCommand untagFoodCommand = new UntagFoodCommand(tag.getTagName(), INDEX_THIRD_FOOD);
+
+        assertCommandFailure(untagFoodCommand, model, EXPECTED_ERROR_FOOD_NOT_FOUND);
+    }
+
+    @Test
+    public void equals() {
+        Tag dairyTag = TypicalTag.DAIRY_TAG;
+        Tag chocolateTag = TypicalTag.CHOCOLATE_TAG;
+
+        UntagFoodCommand untagFoodDairy = new UntagFoodCommand(dairyTag.getTagName(), INDEX_FIRST_FOOD);
+        UntagFoodCommand untagFoodChocolate = new UntagFoodCommand(chocolateTag.getTagName(),
+            INDEX_FIRST_FOOD);
+
+        // same object -> returns true
+        assertEquals(untagFoodDairy, untagFoodDairy);
+
+        // same tags and same food -> returns true
+        Tag dairyTagCopy = new TagBuilder().withTagName(VALID_TAG_DAIRY).build();
+        UntagFoodCommand untagFoodDairyCopy = new UntagFoodCommand(dairyTagCopy.getTagName(),
+            INDEX_FIRST_FOOD);
+        assertEquals(untagFoodDairy, untagFoodDairyCopy);
+
+        // different commands -> returns false
+        assertNotEquals(untagFoodDairy, new ExitCommand());
+
+        // null -> returns false
+        assertNotEquals(untagFoodDairy, null);
+
+        // different tag command -> returns false
+        assertNotEquals(untagFoodDairy, untagFoodChocolate);
     }
 }
