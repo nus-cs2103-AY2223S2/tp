31626404de41750434c7f2eaf702bf54c319f8dc
Ng Jia Yi <92777033+gremmyz@users.noreply.github.com>
2023-03-25 23:12:51 +0800

    update files

--- src/main/java/seedu/connectus/logic/commands/ExitCommand.java
+++ src/main/java/seedu/connectus/logic/commands/ExitCommand.java
@@ -9,6 +9,10 @@ public class ExitCommand extends Command {
 
     public static final String COMMAND_WORD = "exit";
 
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Exits the program.\n"
+            + "Example: " + COMMAND_WORD;
+
     public static final String MESSAGE_EXIT_ACKNOWLEDGEMENT = "Exiting ConnectUS as requested ...";
 
     @Override
--- src/main/java/seedu/connectus/logic/commands/HelpCommand.java
+++ src/main/java/seedu/connectus/logic/commands/HelpCommand.java
@@ -10,12 +10,34 @@ public class HelpCommand extends Command {
     public static final String COMMAND_WORD = "help";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Shows program usage instructions.\n"
-            + "Example: " + COMMAND_WORD;
+            + "Entering '" + COMMAND_WORD + "' alone returns a separate window with the link to the User Guide.\n"
+            + "Entering '" + COMMAND_WORD + " [COMMAND]' will return the usage instructions for the specified command.\n"
+            + "Example: '" + COMMAND_WORD + " add";
 
+    private final boolean isHelpWindowShown;
+    private final String helpMessage;
     public static final String SHOWING_HELP_MESSAGE = "Opened help window.";
+
+    public HelpCommand() {
+        this.isHelpWindowShown = true;
+        this.helpMessage = SHOWING_HELP_MESSAGE;
+    }
+
+    public HelpCommand(String command) {
+        this.isHelpWindowShown = false;
+        this.helpMessage = command;
+    }
 
     @Override
     public CommandResult execute(Model model) {
-        return new CommandResult(SHOWING_HELP_MESSAGE, true, false);
+        return new CommandResult(helpMessage, isHelpWindowShown, false);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof HelpCommand // instanceof handles nulls
+                && isHelpWindowShown == ((HelpCommand) other).isHelpWindowShown
+                && helpMessage.equals(((HelpCommand) other).helpMessage));
     }
 }
