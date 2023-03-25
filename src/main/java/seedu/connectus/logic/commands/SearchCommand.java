f08eae76a9fffe23fa716493bc0c5a78f32177f5
Ng Jia Yi <92777033+gremmyz@users.noreply.github.com>
2023-03-25 21:35:59 +0800

    Update SearchCommand.java

--- src/main/java/seedu/connectus/logic/commands/SearchCommand.java
+++ src/main/java/seedu/connectus/logic/commands/SearchCommand.java
@@ -7,17 +7,17 @@ import seedu.connectus.model.Model;
 import seedu.connectus.model.person.FieldsContainKeywordsPredicate;
 
 /**
- * Finds and lists all persons in ConnectUS whose name contains any of the argument keywords.
+ * Finds and lists all persons in ConnectUS whose fields contains any of the argument keywords.
  * Keyword matching is case insensitive.
  */
 public class SearchCommand extends Command {
 
     public static final String COMMAND_WORD = "search";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons whose names contain any of "
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons whose fields contain any of "
             + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
             + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
-            + "Example: " + COMMAND_WORD + " alice bob charlie";
+            + "Example: " + COMMAND_WORD + " alex may CS2103T";
 
     private final FieldsContainKeywordsPredicate predicate;
 
