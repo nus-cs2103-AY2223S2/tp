99892bf9492abae17c516b8142cc48de149e6bc1
Filbert Phang <fillberth@gmail.com>
2023-03-26 01:20:03 +0800

    Add tests for new Find command functionality
    - Update tests for finding by name
    - Add tests for finding by tags

--- src/test/java/seedu/recipe/logic/commands/FindCommandTest.java
+++ src/test/java/seedu/recipe/logic/commands/FindCommandTest.java
@@ -4,6 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static seedu.recipe.commons.core.Messages.MESSAGE_RECIPES_LISTED_OVERVIEW;
 import static seedu.recipe.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.recipe.testutil.TypicalRecipes.BLUEBERRY_PANCAKES;
+import static seedu.recipe.testutil.TypicalRecipes.CACIO_E_PEPE;
+import static seedu.recipe.testutil.TypicalRecipes.FISH_AND_CHIPS;
 import static seedu.recipe.testutil.TypicalRecipes.getTypicalRecipeBook;
 
 import java.util.Arrays;
@@ -16,7 +19,9 @@ import seedu.recipe.model.Model;
 import seedu.recipe.model.ModelManager;
 import seedu.recipe.model.UserPrefs;
 import seedu.recipe.model.recipe.Name;
+import seedu.recipe.model.recipe.PropertyCollectionContainsKeywordsPredicate;
 import seedu.recipe.model.recipe.PropertyNameContainsKeywordsPredicate;
+import seedu.recipe.model.tag.Tag;
 
 /**
  * Contains integration tests (interaction with the Model) for {@code FindCommand}.
@@ -35,9 +40,14 @@ public class FindCommandTest {
             new PropertyNameContainsKeywordsPredicate<Name>(Collections.singletonList("second"),
                 FindUtil.GET_NAME_FROM_RECIPE,
                 FindUtil.GET_NAME_STRING);
+        PropertyNameContainsKeywordsPredicate<Name> thirdPredicate =
+            new PropertyNameContainsKeywordsPredicate<Name>(Collections.singletonList("second"),
+                FindUtil.GET_NAME_FROM_RECIPE,
+                FindUtil.GET_NAME_STRING);
 
         FindCommand findFirstCommand = new FindCommand(firstPredicate);
         FindCommand findSecondCommand = new FindCommand(secondPredicate);
+        FindCommand findThirdCommand = new FindCommand(thirdPredicate);
 
         // same object -> returns true
         assertEquals(findFirstCommand, findFirstCommand);
@@ -46,20 +56,23 @@ public class FindCommandTest {
         FindCommand findFirstCommandCopy = new FindCommand(firstPredicate);
         assertEquals(findFirstCommand, findFirstCommandCopy);
 
+        // different but equal values -> returns true
+        assertEquals(findSecondCommand, findThirdCommand);
+
         // different types -> returns false
         assertNotEquals(1, findFirstCommand);
 
         // null -> returns false
         assertNotEquals(null, findFirstCommand);
 
-        // different recipe -> returns false
+        // different predicate -> returns false
         assertNotEquals(findFirstCommand, findSecondCommand);
     }
 
     @Test
-    public void execute_zeroKeywords_noRecipeFound() {
+    public void execute_namePropertyZeroKeywords_noRecipeFound() {
         String expectedMessage = String.format(MESSAGE_RECIPES_LISTED_OVERVIEW, 0);
-        PropertyNameContainsKeywordsPredicate<Name> predicate = preparePredicate(" ");
+        PropertyNameContainsKeywordsPredicate<Name> predicate = prepareNamePredicate(" ");
         FindCommand command = new FindCommand(predicate);
         expectedModel.updateFilteredRecipeList(predicate);
         assertCommandSuccess(command, model, expectedMessage, expectedModel);
@@ -67,21 +80,37 @@ public class FindCommandTest {
     }
 
     @Test
-    public void execute_multipleKeywords_multipleRecipesFound() {
-        String expectedMessage = String.format(MESSAGE_RECIPES_LISTED_OVERVIEW, 3);
-        PropertyNameContainsKeywordsPredicate<Name> predicate = preparePredicate("Kurz Elle Kunz");
+    public void execute_namePropertyMultipleKeywords_multipleRecipesFound() {
+        String expectedMessage = String.format(MESSAGE_RECIPES_LISTED_OVERVIEW, 2);
+        PropertyNameContainsKeywordsPredicate<Name> predicate = prepareNamePredicate("American Fish");
         FindCommand command = new FindCommand(predicate);
         expectedModel.updateFilteredRecipeList(predicate);
-        // assertCommandSuccess(command, model, expectedMessage, expectedModel);
-        //        assertEquals(Arrays.asList(CARL, ELLE, FIONA), model.getFilteredRecipeList());
+        assertCommandSuccess(command, model, expectedMessage, expectedModel);
+        assertEquals(Arrays.asList(BLUEBERRY_PANCAKES, FISH_AND_CHIPS), model.getFilteredRecipeList());
+    }
+
+    @Test
+    public void execute_tagPropertyMultipleKeywords_multipleRecipesFound() {
+        String expectedMessage = String.format(MESSAGE_RECIPES_LISTED_OVERVIEW, 2);
+        PropertyCollectionContainsKeywordsPredicate<Tag> predicate = prepareTagPredicate("American Italian");
+        FindCommand command = new FindCommand(predicate);
+        expectedModel.updateFilteredRecipeList(predicate);
+        assertCommandSuccess(command, model, expectedMessage, expectedModel);
+        assertEquals(Arrays.asList(BLUEBERRY_PANCAKES, CACIO_E_PEPE), model.getFilteredRecipeList());
     }
 
     /**
      * Parses {@code userInput} into a {@code PropertyNameContainsKeywordsPredicate<Name>}.
      */
-    private PropertyNameContainsKeywordsPredicate<Name> preparePredicate(String userInput) {
+    private PropertyNameContainsKeywordsPredicate<Name> prepareNamePredicate(String userInput) {
         return new PropertyNameContainsKeywordsPredicate<Name>(Arrays.asList(userInput.split("\\s+")),
             FindUtil.GET_NAME_FROM_RECIPE,
             FindUtil.GET_NAME_STRING);
+    }
+
+    private PropertyCollectionContainsKeywordsPredicate<Tag> prepareTagPredicate(String userInput) {
+        return new PropertyCollectionContainsKeywordsPredicate<Tag>(Arrays.asList(userInput.split("\\s+")),
+            FindUtil.GET_TAGS_FROM_RECIPE,
+            FindUtil.GET_TAG_STRING);
     }
 }
