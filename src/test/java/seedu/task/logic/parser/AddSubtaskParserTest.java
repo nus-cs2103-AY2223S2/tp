52bfee68d06eb8279a47a1235a49b951ab97e440
HAO ZEYU <88484192+PROGRAMMERHAO@users.noreply.github.com>
2023-03-25 13:40:56 +0800

    Update AddSubtaskParserTest.java

--- src/test/java/seedu/task/logic/parser/AddSubtaskParserTest.java
+++ src/test/java/seedu/task/logic/parser/AddSubtaskParserTest.java
@@ -1,10 +1,10 @@
 package seedu.task.logic.parser;
 
 import static seedu.task.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.task.logic.commands.CommandTestUtil.BLANK_DESCRIPTION_DESC;
 import static seedu.task.logic.commands.CommandTestUtil.DESCRIPTION_DESC_AMY;
 import static seedu.task.logic.commands.CommandTestUtil.DESCRIPTION_DESC_BOB;
 import static seedu.task.logic.commands.CommandTestUtil.INDEX_1;
-import static seedu.task.logic.commands.CommandTestUtil.BLANK_DESCRIPTION_DESC;
 import static seedu.task.logic.commands.CommandTestUtil.INVALID_NAME_DESC;
 import static seedu.task.logic.commands.CommandTestUtil.NAME_DESC_BOB;
 import static seedu.task.logic.commands.CommandTestUtil.PREAMBLE_WHITESPACE;
