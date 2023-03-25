814e79611f1e3af12a92f2a60903635fa7f9c759
ScorpiusSigma <justinleng_@hotmail.com>
2023-03-25 22:28:10 +0800

    Edit removing tag in predefine tags will remove tags in food

--- src/main/java/seedu/wife/logic/commands/tagcommands/DeleteTagCommand.java
+++ src/main/java/seedu/wife/logic/commands/tagcommands/DeleteTagCommand.java
@@ -1,13 +1,17 @@
 package seedu.wife.logic.commands.tagcommands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.wife.model.Model.PREDICATE_SHOW_ALL_FOODS;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import seedu.wife.logic.commands.Command;
 import seedu.wife.logic.commands.CommandResult;
 import seedu.wife.logic.commands.exceptions.CommandException;
 import seedu.wife.model.Model;
+import seedu.wife.model.food.Food;
 import seedu.wife.model.tag.Tag;
 
 /**
@@ -20,12 +24,22 @@ public class DeleteTagCommand extends Command {
             + "Example: " + COMMAND_WORD + " n/vegetable";
     public static final String TAG_DELETE_SUCCESS_MESSAGE = "Tag successfully deleted:";
     public static final String TAG_DELETE_UNSUCCESS_MESSAGE = "The tag you are trying to delete does not exist.";
-    private Tag toDelete;
+    private Set<Tag> toDelete;
 
     /**
      * Constructor to create a new DeleteTagCommand object.
      */
     public DeleteTagCommand(Tag tag) {
+        requireNonNull(tag);
+        HashSet<Tag> taglist = new HashSet<Tag>();
+        taglist.add(tag);
+        this.toDelete = taglist;
+    }
+
+    /**
+     * Constructor to create a new DeleteTagCommand object.
+     */
+    public DeleteTagCommand(Set<Tag> tag) {
         requireNonNull(tag);
         this.toDelete = tag;
     }
@@ -33,15 +47,26 @@ public class DeleteTagCommand extends Command {
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
-        List<Tag> lastShownList = List.copyOf(model.getTagList());
+        List<Tag> lastShownTagList = List.copyOf(model.getTagList());
+        List<Food> lastShownFoodList = List.copyOf(model.getFoodList());
         String deletedTagSuccessMessage = TAG_DELETE_SUCCESS_MESSAGE;
 
-        for (Tag tag : lastShownList) {
-            if (model.hasTag(this.toDelete)) {
-                model.deleteTag(this.toDelete);
+        // Deletes tag from food
+        for (Tag tag : toDelete) {
+            for (Food food : lastShownFoodList) {
+                food.removeTag(tag);
+            }
+        }
+
+        // Deletes tag from WIFE
+        for (Tag tag : toDelete) {
+            if (model.hasTag(tag)) {
+                model.deleteTag(tag);
                 deletedTagSuccessMessage += "\n" + this.toDelete;
             }
         }
+
+        model.updateFilteredFoodList(PREDICATE_SHOW_ALL_FOODS);
 
         return new CommandResult(deletedTagSuccessMessage);
     }
