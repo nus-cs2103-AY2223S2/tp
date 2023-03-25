6aed172af8b5f715e1a76884a785048d78b79c35
abenx162 <benxuan162@gmail.com>
2023-03-25 18:28:31 +0800

    Add PicturePath tests

--- src/test/java/seedu/address/model/employee/PicturePathTest.java
+++ src/test/java/seedu/address/model/employee/PicturePathTest.java
@@ -4,6 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.junit.jupiter.api.Test;
 
 public class PicturePathTest {
@@ -37,5 +40,33 @@ public class PicturePathTest {
         assertTrue(PicturePath.isValidPicturePath("src/main/resources/employeepictures/anotherfolder/default.png"));
         // file does not exist, but still valid
         assertTrue(PicturePath.isValidPicturePath("src/main/resources/employeepictures/doesnotexist.png"));
+    }
+
+    @Test
+    public void toPath() {
+        PicturePath picturePath = new PicturePath("src/main/resources/employeepictures/default.png");
+        Path expectedPath = Paths.get("src", "main", "resources", "employeepictures", "default.png");
+        assertTrue(expectedPath.equals(picturePath.toPath()));
+    }
+
+    @Test
+    public void equals() {
+        // same values -> returns true
+        PicturePath defaultPicturePath = new PicturePath("src/main/resources/employeepictures/default.png");
+        PicturePath defaultPicturePathCopy = new PicturePath("src/main/resources/employeepictures/default.png");
+        assertTrue(defaultPicturePath.equals(defaultPicturePathCopy));
+
+        // same object -> returns true
+        assertTrue(defaultPicturePath.equals(defaultPicturePath));
+
+        // null -> returns false
+        assertFalse(defaultPicturePath.equals(null));
+
+        // different type -> returns false
+        assertFalse(defaultPicturePath.equals("src/main/resources/employeepictures/default.png"));
+
+        // different employee -> returns false
+        PicturePath anotherPicturePath = new PicturePath("src/main/resources/employeepictures/another.png");
+        assertFalse(defaultPicturePath.equals(anotherPicturePath));
     }
 }
