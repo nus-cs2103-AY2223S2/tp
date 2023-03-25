8abc0b43940fbf11f1099da28726186980c45f67
SeanChinJunKai <e0774037@u.nus.edu>
2023-03-25 22:29:29 +0800

    Fixed checkstyle

--- src/test/java/seedu/address/logic/parser/ViewCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ViewCommandParserTest.java
@@ -8,7 +8,6 @@ import static seedu.address.logic.commands.CommandTestUtil.VALID_NRIC_BOB;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 
-
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.ViewCommand;
