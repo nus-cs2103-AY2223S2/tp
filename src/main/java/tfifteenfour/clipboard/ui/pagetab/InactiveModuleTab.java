a3e57c5fbb6fa429dcbcbe2405a0d8a03382265d
afiq <afiqzuhairi@gmail.com>
2023-03-26 00:16:13 +0800

    Add page tab GUI

---
+++ src/main/java/tfifteenfour/clipboard/ui/pagetab/InactiveModuleTab.java
@@ -0,0 +1,22 @@
+package tfifteenfour.clipboard.ui.pagetab;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Region;
+import tfifteenfour.clipboard.ui.UiPart;
+
+/**
+ * A UI for the module tab.
+ */
+public class InactiveModuleTab extends UiPart<Region> {
+
+    private static final String FXML = "InactiveModuleTab.fxml";
+
+    @FXML
+    private Label name;
+
+    public InactiveModuleTab() {
+        super(FXML);
+    }
+
+}
