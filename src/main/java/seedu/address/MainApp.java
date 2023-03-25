5afbd27c1a10b1b8b8be73be7f0854cdeaf1d74a
Xiuxuan Wang <36021591+wxxedu@users.noreply.github.com>
2023-03-25 10:52:04 +0800

    lmao

--- src/main/java/seedu/address/MainApp.java
+++ src/main/java/seedu/address/MainApp.java
@@ -13,6 +13,7 @@ import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.core.Version;
 import seedu.address.commons.exceptions.DataConversionException;
 import seedu.address.commons.util.ConfigUtil;
+import seedu.address.commons.util.FileUtil;
 import seedu.address.commons.util.GetUtil;
 import seedu.address.commons.util.StringUtil;
 import seedu.address.logic.Logic;
@@ -57,7 +58,8 @@ public class MainApp extends Application {
 
     @Override
     public void init() throws Exception {
-        logger.info("=============================[ Initializing Wingman ]===========================");
+        logger.info(
+                "=============================[ Initializing Wingman ]===========================");
         super.init();
         initFields();
         configureServiceLocator();
@@ -66,7 +68,7 @@ public class MainApp extends Application {
     /**
      * Initialises respective fields from storage
      */
-    private void initFields() {
+    private void initFields() throws IOException {
         AppParameters appParameters = AppParameters.parse(getParameters());
         config = initConfig(appParameters.getConfigPath());
         UserPrefsStorage userPrefsStorage = new JsonUserPrefsStorage(config.getUserPrefsFilePath());
@@ -81,7 +83,6 @@ public class MainApp extends Application {
     /**
      * To allow for convenient access to the services in the specified packages,
      * without disrupting effective isolation between classes
-     *
      */
     private void configureServiceLocator() {
         GetUtil.put(Config.class, config);
@@ -92,14 +93,15 @@ public class MainApp extends Application {
 
     /**
      * Read a specified manager from data
+     *
      * @param managerSupplier the data from which the manager is read
-     * @param managerName name of the manager
-     * @param <T> the type of object that the manager manages
+     * @param managerName     name of the manager
+     * @param <T>             the type of object that the manager manages
      * @return a read only version of the item manager
      */
     private <T extends Item> ReadOnlyItemManager<T> readManager(
-        Callable<Optional<? extends ReadOnlyItemManager<T>>> managerSupplier,
-        String managerName
+            Callable<Optional<? extends ReadOnlyItemManager<T>>> managerSupplier,
+            String managerName
     ) {
         Optional<? extends ReadOnlyItemManager<T>> managerOptional;
         try {
@@ -108,18 +110,21 @@ public class MainApp extends Application {
                 return managerOptional.get();
             }
             logger.info(
-                String.format("Data file for %s is not found.", managerName)
+                    String.format("Data file for %s is not found.", managerName)
             );
             return new ItemManager<>();
         } catch (DataConversionException | IOException e) {
             logger.info(String.format(
-                "Exception caught while reading data file for %s: %s",
-                managerName,
-                e.toString()
+                    "Exception caught while reading data file for %s: %s",
+                    managerName,
+                    e.toString()
             ));
             return new ItemManager<>();
         } catch (Exception e) {
-            logger.info(String.format("Unknown Exception: %s.", e.getMessage()));
+            logger.info(String.format(
+                    "Unknown Exception: %s.",
+                    e.getMessage()
+            ));
             return new ItemManager<>();
         }
     }
@@ -129,44 +134,61 @@ public class MainApp extends Application {
      * The data from the sample address book will be used instead if {@code storage}'s address book is not found,
      * or an empty address book will be used instead if errors occur when reading {@code storage}'s address book.
      */
-    private Model initModelManager(Storage storage,
-        ReadOnlyUserPrefs userPrefs) {
+    private Model initModelManager(
+            Storage storage,
+            ReadOnlyUserPrefs userPrefs
+    ) {
         ReadOnlyItemManager<Pilot> pilotManager =
-            readManager(storage::readPilotManager, "PilotManager");
+                readManager(storage::readPilotManager, "PilotManager");
         ReadOnlyItemManager<Location> locationManager =
-            readManager(storage::readLocationManager, "LocationManager");
+                readManager(storage::readLocationManager, "LocationManager");
         ReadOnlyItemManager<Crew> crewManager =
-            readManager(storage::readCrewManager, "CrewManager");
+                readManager(storage::readCrewManager, "CrewManager");
         ReadOnlyItemManager<Plane> planeManager =
-            readManager(storage::readPlaneManager, "PlaneManager");
+                readManager(storage::readPlaneManager, "PlaneManager");
         ReadOnlyItemManager<Flight> flightManager =
-            readManager(storage::readFlightManager, "FlightManager");
+                readManager(storage::readFlightManager, "FlightManager");
 
         return new ModelManager(userPrefs, pilotManager, locationManager,
-            crewManager, planeManager, flightManager);
+                crewManager, planeManager, flightManager
+        );
     }
 
     /**
      * Initialises storages for specified object types and user preferences
-     * @param userPrefs user preferences
+     *
+     * @param userPrefs        user preferences
      * @param userPrefsStorage storage destination for user preferences
      * @return StorageManager object that manages the storages for specified object types and user preferences
      */
-    private Storage initStorage(UserPrefs userPrefs,
-        UserPrefsStorage userPrefsStorage) {
+    private Storage initStorage(
+            UserPrefs userPrefs,
+            UserPrefsStorage userPrefsStorage
+    ) throws IOException {
+        FileUtil.createParentDirsOfFile(userPrefs.getPilotManagerFilePath());
+        FileUtil.createParentDirsOfFile(userPrefs.getCrewManagerFilePath());
+        FileUtil.createParentDirsOfFile(userPrefs.getPlaneManagerFilePath());
+        FileUtil.createParentDirsOfFile(userPrefs.getLocationManagerFilePath());
+        FileUtil.createParentDirsOfFile(userPrefs.getFlightManagerFilePath());
+        FileUtil.createIfMissing(userPrefs.getPilotManagerFilePath());
+        FileUtil.createIfMissing(userPrefs.getCrewManagerFilePath());
+        FileUtil.createIfMissing(userPrefs.getPlaneManagerFilePath());
+        FileUtil.createIfMissing(userPrefs.getLocationManagerFilePath());
+        FileUtil.createIfMissing(userPrefs.getFlightManagerFilePath());
         ItemStorage<Pilot> pilotStorage =
-            new JsonPilotManagerStorage(userPrefs.getPilotManagerFilePath());
+                new JsonPilotManagerStorage(userPrefs.getPilotManagerFilePath());
         ItemStorage<Location> locationStorage =
-            new JsonLocationManagerStorage(userPrefs.getLocationManagerFilePath());
+                new JsonLocationManagerStorage(userPrefs.getLocationManagerFilePath());
         ItemStorage<Crew> crewStorage =
-            new JsonCrewManagerStorage(userPrefs.getCrewManagerFilePath());
+                new JsonCrewManagerStorage(userPrefs.getCrewManagerFilePath());
         ItemStorage<Plane> planeStorage =
-            new JsonPlaneManagerStorage(userPrefs.getPlaneManagerFilePath());
+                new JsonPlaneManagerStorage(userPrefs.getPlaneManagerFilePath());
         ItemStorage<Flight> flightStorage =
-            new JsonFlightManagerStorage(userPrefs.getFlightManagerFilePath());
+                new JsonFlightManagerStorage(userPrefs.getFlightManagerFilePath());
         return new StorageManager(userPrefsStorage, pilotStorage,
-            locationStorage,
-            crewStorage, planeStorage, flightStorage);
+                locationStorage,
+                crewStorage, planeStorage, flightStorage
+        );
     }
 
     private void initLogging(Config config) {
@@ -192,11 +214,12 @@ public class MainApp extends Application {
         logger.info("Using config file : " + configFilePathUsed);
 
         try {
-            Optional<Config> configOptional = ConfigUtil.readConfig(configFilePathUsed);
+            Optional<Config> configOptional = ConfigUtil.readConfig(
+                    configFilePathUsed);
             initializedConfig = configOptional.orElse(new Config());
         } catch (DataConversionException e) {
             logger.warning("Config file at " + configFilePathUsed + " is not in the correct format. "
-                               + "Using default config properties");
+                                   + "Using default config properties");
             initializedConfig = new Config();
         }
 
@@ -204,7 +227,8 @@ public class MainApp extends Application {
         try {
             ConfigUtil.saveConfig(initializedConfig, configFilePathUsed);
         } catch (IOException e) {
-            logger.warning("Failed to save config file : " + StringUtil.getDetails(e));
+            logger.warning("Failed to save config file : " + StringUtil.getDetails(
+                    e));
         }
         return initializedConfig;
     }
@@ -224,10 +248,11 @@ public class MainApp extends Application {
             initializedPrefs = prefsOptional.orElse(new UserPrefs());
         } catch (DataConversionException e) {
             logger.warning("UserPrefs file at " + prefsFilePath + " is not in the correct format. "
-                               + "Using default user prefs");
+                                   + "Using default user prefs");
             initializedPrefs = new UserPrefs();
         } catch (IOException e) {
-            logger.warning("Problem while reading from the file. Will be starting with an empty AddressBook");
+            logger.warning(
+                    "Problem while reading from the file. Will be starting with an empty AddressBook");
             initializedPrefs = new UserPrefs();
         }
 
@@ -235,7 +260,8 @@ public class MainApp extends Application {
         try {
             storage.saveUserPrefs(initializedPrefs);
         } catch (IOException e) {
-            logger.warning("Failed to save config file : " + StringUtil.getDetails(e));
+            logger.warning("Failed to save config file : " + StringUtil.getDetails(
+                    e));
         }
 
         return initializedPrefs;
@@ -249,11 +275,13 @@ public class MainApp extends Application {
 
     @Override
     public void stop() {
-        logger.info("============================ [ Stopping Wingman ] =============================");
+        logger.info(
+                "============================ [ Stopping Wingman ] =============================");
         try {
             storage.saveUserPrefs(model.getUserPrefs());
         } catch (IOException e) {
-            logger.severe("Failed to save preferences " + StringUtil.getDetails(e));
+            logger.severe("Failed to save preferences " + StringUtil.getDetails(
+                    e));
         }
     }
 }
