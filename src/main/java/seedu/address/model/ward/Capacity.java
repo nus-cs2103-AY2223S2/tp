d92eb45a25307d3e71d33f0d8eb0bfed0246aa56
nramapurath <nramapurath@gmail.com>
2023-03-25 16:06:51 +0800

    Add method to check if entered valid capacity

--- src/main/java/seedu/address/model/ward/Capacity.java
+++ src/main/java/seedu/address/model/ward/Capacity.java
@@ -4,14 +4,34 @@ package seedu.address.model.ward;
  * Represents the capacity and occupancy of a ward.
  */
 public class Capacity {
+
+    public static final String MESSAGE_CONSTRAINTS = "Capacity should be a valid integer"
+            + " and it should not be blank";
+
     private int value;
 
     public Capacity(int capacity) {
         value = capacity;
     }
 
+    public Capacity(String capacity) {
+        value = Integer.parseInt(capacity);
+    }
+
     public void setCapacity(int capacity) {
         value = capacity;
+    }
+
+    /**
+     * Returns true if a given string is a valid capacity.
+     */
+    public static boolean isValidCapacity(String test) {
+        try {
+            Integer.parseInt(test);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 
     public int getValue() {
