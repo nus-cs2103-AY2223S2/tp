765938c7794577b4093a45857ded8e1e20a4af08
CedricPei <wenqi_pei@u.nus.edu>
2023-03-25 23:25:21 +0800

    Fix the add hint message error.

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -35,6 +35,7 @@ public class AddCommand extends Command {
             + PREFIX_PHONE + "98765432 "
             + PREFIX_EMAIL + "johnd@example.com "
             + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 "
+            + PREFIX_DRUG_ALLERGY + "NKDA "
             + PREFIX_TAG + "friends "
             + PREFIX_TAG + "owesMoney";
 
