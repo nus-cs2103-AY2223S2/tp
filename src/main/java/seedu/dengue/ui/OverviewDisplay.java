b7c0061a7d970b24ecbd2f5d2d62bc2ed379abfc
axmszr <e0727266@u.nus.edu>
2023-03-27 02:07:23 +0800

    Allow OverviewDisplay to be updated with an Overview instance.

--- src/main/java/seedu/dengue/ui/OverviewDisplay.java
+++ src/main/java/seedu/dengue/ui/OverviewDisplay.java
@@ -5,30 +5,52 @@ import static java.util.Objects.requireNonNull;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.Region;
+import seedu.dengue.ui.overview.Overview;
 
 /**
  * A ui for the current overview that is displayed on the right of the application.
  */
 public class OverviewDisplay extends UiPart<Region> {
-
     private static final String FXML = "OverviewDisplay.fxml";
 
     @FXML
     private TextArea overviewTitle;
     @FXML
+    private TextArea overviewSubtitle;
+    @FXML
     private TextArea overviewContent;
 
+    /**
+     * Constructs a new blank {@code OverviewDisplay} instance.
+     */
     public OverviewDisplay() {
         super(FXML);
     }
 
-    public void setOverviewTitle(String newOverviewTitle) {
+    private void setOverviewTitle(String newOverviewTitle) {
         requireNonNull(newOverviewTitle);
         overviewTitle.setText(newOverviewTitle);
     }
 
-    public void setOverviewContent(String newOverviewContent) {
+    private void setOverviewSubtitle(String newOverviewSubtitle) {
+        requireNonNull(newOverviewSubtitle);
+        overviewSubtitle.setText(newOverviewSubtitle);
+    }
+
+    private void setOverviewContent(String newOverviewContent) {
         requireNonNull(newOverviewContent);
         overviewContent.setText(newOverviewContent);
+    }
+
+    /**
+     * Updates the OverviewDisplay with the given {@code Overview}.
+     *
+     * @param overview The overview to display.
+     */
+    public void setOverview(Overview overview) {
+        requireNonNull(overview);
+        setOverviewTitle(overview.getOverviewTitle());
+        setOverviewSubtitle(overview.getOverviewSubtitle());
+        setOverviewContent(overview.getOverviewContent());
     }
 }
