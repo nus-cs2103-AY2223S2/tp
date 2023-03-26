b634fcf716eba9e3cf6547cc4e472d8dd5fe2695
ajjajjajjajj <angjingjie0512@gmail.com>
2023-03-26 20:59:24 +0800

    Remove task attributes from PersonCard temporarily

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -70,7 +70,6 @@ public class PersonCard extends UiPart<Region> {
         address.setText(person.getAddress().value);
         email.setText(person.getEmail().value);
         remark.setText(person.getRemark().value);
-        task.setText(person.getTask().value);
         person.getTags().stream()
                 .sorted(Comparator.comparing(tag -> tag.tagName))
                 .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
