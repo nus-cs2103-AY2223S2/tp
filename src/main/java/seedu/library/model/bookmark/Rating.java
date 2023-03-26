68146feaa06d4cefc4ce666df5b917e8c53571b7
zhoyx <zhouyx2000@gmail.com>
2023-03-27 01:40:10 +0800

    Create Rating class

---
+++ src/main/java/seedu/library/model/bookmark/Rating.java
@@ -0,0 +1,52 @@
+package seedu.library.model.bookmark;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.library.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents the rating of a book in the library.
+ * Guarantees: immutable; is valid as declared in {@link #isValidTitle(String)}
+ */
+public class Rating {
+
+    public static final String MESSAGE_CONSTRAINTS =
+            "Rating should be integer value from 1 to 5(inclusive)";
+    public static final String VALIDATION_REGEX = "[1-5]";
+
+    public final String value;
+
+    /**
+     * Constructs a {@code Rating}.
+     * @param rating
+     */
+    public Rating(String rating) {
+        requireNonNull(rating);
+        checkArgument(isRating(rating), MESSAGE_CONSTRAINTS);
+        value = rating;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+    /**
+     * Returns true if a given string is a valid url.
+     */
+    public static boolean isRating(String rating) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Rating // instanceof handles nulls
+                && value.equals(((Rating) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+
+}
