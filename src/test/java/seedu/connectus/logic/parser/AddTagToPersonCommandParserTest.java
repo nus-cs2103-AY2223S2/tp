b2daccef9d05346bcc7de1226bd136d6477f5457
salty-flower <95509524+salty-flower@users.noreply.github.com>
2023-03-26 22:59:07 +0800

    Add to testcases

--- src/test/java/seedu/connectus/logic/parser/AddTagToPersonCommandParserTest.java
+++ src/test/java/seedu/connectus/logic/parser/AddTagToPersonCommandParserTest.java
@@ -18,7 +18,7 @@ public class AddTagToPersonCommandParserTest {
     private final AddTagToPersonCommandParser parser = new AddTagToPersonCommandParser();
 
     @Test
-    public void parse_validArgs_returnsDeleteCommand() {
+    public void parse_validArgs_returnsCommand() {
         var tags = new HashSet<Tag>();
         tags.add(new Tag("tagg"));
         var modules = new HashSet<Module>();
@@ -28,8 +28,38 @@ public class AddTagToPersonCommandParserTest {
     }
 
     @Test
-    public void parse_invalidArgs_throwsParseException() {
+    public void parse_unrelatedArgs_throwsParseException() {
         assertParseFailure(parser, "a",
+            String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddTagToPersonCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_noTagSpecified_throwsParseException() {
+        assertParseFailure(parser, "1 t/",
+            String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddTagToPersonCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "1 t/ ",
+            String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddTagToPersonCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "1 t/\t",
+            String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddTagToPersonCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_noModuleSpecified_throwsParseException() {
+        assertParseFailure(parser, "1 mod/",
+            String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddTagToPersonCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "1 mod/ ",
+            String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddTagToPersonCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "1 mod/\t",
+            String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddTagToPersonCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_nothingSpecified_throwsParseException() {
+        assertParseFailure(parser, "1 mod/ t/",
+            String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddTagToPersonCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "1 mod/  t/ ",
+            String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddTagToPersonCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "1 mod/\t t/\t",
             String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddTagToPersonCommand.MESSAGE_USAGE));
     }
 }
--- src/test/java/seedu/connectus/logic/parser/DeleteTagFromPersonCommandParserTest.java
+++ src/test/java/seedu/connectus/logic/parser/DeleteTagFromPersonCommandParserTest.java
@@ -16,15 +16,45 @@ public class DeleteTagFromPersonCommandParserTest {
     private final DeleteTagFromPersonCommandParser parser = new DeleteTagFromPersonCommandParser();
 
     @Test
-    public void parse_validArgs_returnsDeleteCommand() {
+    public void parse_validArgs_returnsCommand() {
         assertParseSuccess(parser, "1 t/1 mod/1", new DeleteTagFromPersonCommand(INDEX_FIRST_PERSON,
             Index.fromZeroBased(0), Index.fromZeroBased(0)
         ));
     }
 
     @Test
-    public void parse_invalidArgs_throwsParseException() {
+    public void parse_unrelatedArgs_throwsParseException() {
         assertParseFailure(parser, "a",
+            String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteTagFromPersonCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_noTagSpecified_throwsParseException() {
+        assertParseFailure(parser, "1 t/",
+            String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteTagFromPersonCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "1 t/ ",
+            String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteTagFromPersonCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "1 t/\t",
+            String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteTagFromPersonCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_noModuleSpecified_throwsParseException() {
+        assertParseFailure(parser, "1 mod/",
+            String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteTagFromPersonCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "1 mod/ ",
+            String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteTagFromPersonCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "1 mod/\t",
+            String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteTagFromPersonCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_nothingSpecified_throwsParseException() {
+        assertParseFailure(parser, "1 mod/ t/",
+            String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteTagFromPersonCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "1 mod/  t/ ",
+            String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteTagFromPersonCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "1 mod/\t t/\t",
             String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteTagFromPersonCommand.MESSAGE_USAGE));
     }
 }
