51654da131847b8caead48e0b475878183c99fb6
ScorpiusSigma <justinleng_@hotmail.com>
2023-03-26 18:22:57 +0800

    clean up checkstyle and addressbook traces

--- src/test/java/seedu/wife/storage/JsonSerializableWifeTest.java
+++ src/test/java/seedu/wife/storage/JsonSerializableWifeTest.java
@@ -20,7 +20,6 @@ public class JsonSerializableWifeTest {
     private static final Path INVALID_FOOD_FILE = TEST_DATA_FOLDER.resolve("invalidFoodWife.json");
     private static final Path DUPLICATE_FOOD_FILE = TEST_DATA_FOLDER.resolve("duplicateFoodWife.json");
 
-    
     @Test
     public void toModelType_typicalFoodFile_success() throws Exception {
         JsonSerializableWife dataFromFile = JsonUtil
@@ -31,19 +30,19 @@ public class JsonSerializableWifeTest {
 
         Wife wifeFromFile = dataFromFile.toModelType();
         Wife typicalWife = TypicalWife.getTypicalWife();
-        
+
         assertEquals(wifeFromFile, typicalWife);
     }
-    
+
     @Test
-    public void toModelType_invalidPersonFile_throwsIllegalValueException() throws Exception {
+    public void toModelType_invalidFoodFile_throwsIllegalValueException() throws Exception {
         JsonSerializableWife dataFromFile = JsonUtil.readJsonFile(INVALID_FOOD_FILE,
                 JsonSerializableWife.class).get();
         assertThrows(IllegalValueException.class, dataFromFile::toModelType);
     }
 
     @Test
-    public void toModelType_duplicatePersons_throwsIllegalValueException() throws Exception {
+    public void toModelType_duplicateFoods_throwsIllegalValueException() throws Exception {
         JsonSerializableWife dataFromFile = JsonUtil
             .readJsonFile(DUPLICATE_FOOD_FILE, JsonSerializableWife.class).get();
         assertThrows(
