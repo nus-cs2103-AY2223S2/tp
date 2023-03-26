9252c4efe86ae3d3bf29c9a8243358825d6b26b8
Nathan Tran <80334013+rockman007372@users.noreply.github.com>
2023-03-26 20:28:56 +0800

    Update TagTest.java

--- src/test/java/seedu/address/model/tag/TagTest.java
+++ src/test/java/seedu/address/model/tag/TagTest.java
@@ -1,6 +1,11 @@
 package seedu.address.model.tag;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
@@ -11,17 +16,21 @@ public class TagTest {
         assertThrows(NullPointerException.class, () -> new Tag(null));
     }
 
-    /*
-    @Test
-    public void constructor_invalidTagName_throwsIllegalArgumentException() {
-        String invalidTagName = "";
-        assertThrows(IllegalArgumentException.class, () -> new Tag(invalidTagName));
-    } */
-
     @Test
     public void isValidTagName() {
         // null tag name
         assertThrows(NullPointerException.class, () -> Tag.isValidTagName(null));
+
+        // valid tag name
+        List<String> validTagNameTestList = List.of("easy", "medium", "hard", "easY", "MediUm", "HARD");
+        for (String name : validTagNameTestList) {
+            assertTrue(Tag.isValidTagName(name));
+        }
+
+        // invalid tag name
+        assertFalse(Tag.isValidTagName("random"));
+        assertFalse(Tag.isValidTagName("easyyy"));
+        assertFalse(Tag.isValidTagName("easy medium"));
     }
 
 }
