5afd925688d3898da6afca6c1840f5e6b08d19ac
StanleyNeoh <95394480+StanleyNeoh@users.noreply.github.com>
2023-03-27 23:29:43 +0800

    Update Help URL

--- src/main/java/mycelium/mycelium/ui/helpwindow/HelpWindow.java
+++ src/main/java/mycelium/mycelium/ui/helpwindow/HelpWindow.java
@@ -16,7 +16,7 @@ import mycelium.mycelium.ui.UiPart;
  */
 public class HelpWindow extends UiPart<Stage> {
 
-    public static final String USERGUIDE_URL = "https://se-education.org/addressbook-level3/UserGuide.html";
+    public static final String USERGUIDE_URL = "https://ay2223s2-cs2103t-w14-1.github.io/tp/UserGuide.html";
     public static final String HELP_MESSAGE = "Refer to the user guide: " + USERGUIDE_URL;
 
     private static final Logger logger = LogsCenter.getLogger(HelpWindow.class);
