47dbe1df9962dce1561f548053bcba4fee79ef34
Tohtoroo <e0725512@u.nus.edu>
2023-03-27 13:51:07 +0800

    Add PersonContainsTagPredicate

--- src/main/java/seedu/dengue/model/predicate/PersonContainsTagPredicate.java
+++ src/main/java/seedu/dengue/model/predicate/PersonContainsTagPredicate.java
@@ -1,24 +1,26 @@
-//package seedu.dengue.model.predicate;
-//
-//import java.util.function.Predicate;
-//
-//import seedu.dengue.commons.util.StringUtil;
-//import seedu.dengue.model.person.Person;
-//import seedu.dengue.model.variant.Variant;
-//
-//public class PersonContainsTagPredicate implements Predicate<Person> {
-//
-//    private final Variant variant;
-//
-//    public PersonContainsTagPredicate(Variant variant) {
-//        this.variant = variant;
-//    }
-//
-//    @Override
-//    public boolean test(Person person) {
-//        Predicate<String> containsVariant =
-//                keyword -> StringUtil.containsWordIgnoreCase(person.getPostal().value, keyword);
-//
-//        return containsVariant.test;
-//    }
-//}
+package seedu.dengue.model.predicate;
+
+import java.util.function.Predicate;
+
+import seedu.dengue.commons.util.StringUtil;
+import seedu.dengue.model.person.Person;
+import seedu.dengue.model.variant.Variant;
+
+public class PersonContainsTagPredicate implements Predicate<Person> {
+
+    private final Variant variant;
+
+    public PersonContainsTagPredicate(Variant variant) {
+        this.variant = variant;
+    }
+    @Override
+    public boolean test(Person person) {
+        boolean hasVariant = false;
+        for (Variant personVariant : person.getVariants()) {
+            if (StringUtil.containsWordIgnoreCase(personVariant.toString(), this.variant.toString())) {
+                hasVariant = true;
+            }
+        }
+        return hasVariant;
+    }
+}
