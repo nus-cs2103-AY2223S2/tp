a11dfb5b92467bdb888806135db490a37c8723a3
Hafeez <izzhafeez@gmail.com>
2023-03-27 15:44:35 +0800

    Fix checkstyle errors

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -5,7 +5,6 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
