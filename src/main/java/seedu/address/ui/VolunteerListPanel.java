1b72b314547f770ff451a5ac4764d7f5bae0368d
Zong Xun <zxun2000@gmail.com>
2023-03-27 15:31:41 +0800

    Fix typo on volunteer label

--- src/main/java/seedu/address/ui/VolunteerListPanel.java
+++ src/main/java/seedu/address/ui/VolunteerListPanel.java
@@ -47,7 +47,7 @@ public class VolunteerListPanel extends UiPart<Region> {
         imageView.setImage(image);
 
         emptyBox.getChildren().add(imageView);
-        Label label = new Label("Add an elderly to see something here!");
+        Label label = new Label("Add a volunteer to see something here!");
         emptyBox.getChildren().add(label);
 
         emptyBox.setAlignment(Pos.CENTER);
