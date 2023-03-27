0fe5c946a48f77c87837f55ff569e8e565c1df40
axmszr <e0727266@u.nus.edu>
2023-03-28 03:36:52 +0800

    Update SampleDataUtil to match new postal code restrictions

--- src/main/java/seedu/dengue/model/util/SampleDataUtil.java
+++ src/main/java/seedu/dengue/model/util/SampleDataUtil.java
@@ -19,24 +19,24 @@ import seedu.dengue.model.variant.Variant;
 public class SampleDataUtil {
     public static Person[] getSamplePersons() {
         return new Person[] {
-            new Person(new Name("Alex Yeoh"), new Postal("674388"), new Date("2000-11-11"),
-                new Age("111"),
-                getVariantSet("DENV1")),
-            new Person(new Name("Bernice Yu"), new Postal("992727"), new Date("2000-11-11"),
-                new Age("11"),
-                getVariantSet("DENV2", "DENV4")),
-            new Person(new Name("Charlotte Oliveiro"), new Postal("932283"),
-                    new Date("2000-11-11"), new Age("0"),
-                getVariantSet("DENV3")),
-            new Person(new Name("David Li"), new Postal("910312"), new Date("2000-11-11"),
-                new Age("1"),
-                getVariantSet("DENV1")),
-            new Person(new Name("Irfan Ibrahim"), new Postal("924921"), new Date("2000-11-11"),
-                new Age("199"),
-                getVariantSet("DENV2")),
-            new Person(new Name("Roy Balakrishnan"), new Postal("926247"), new Date("2000-11-11"),
-                new Age("2"),
-                getVariantSet("DENV4"))
+            new Person(new Name("Alex Yeoh"), new Postal("664388"), new Date("2000-11-11"),
+                    new Age("111"),
+                    getVariantSet("DENV1")),
+            new Person(new Name("Bernice Yu"), new Postal("112727"), new Date("2000-11-11"),
+                    new Age("11"),
+                    getVariantSet("DENV2", "DENV4")),
+            new Person(new Name("Charlotte Oliveiro"), new Postal("602283"), new Date("2000-11-11"),
+                    new Age("0"),
+                    getVariantSet("DENV3")),
+            new Person(new Name("David Li"), new Postal("600312"), new Date("2000-11-11"),
+                    new Age("1"),
+                    getVariantSet("DENV1")),
+            new Person(new Name("Irfan Ibrahim"), new Postal("654921"), new Date("2000-11-11"),
+                    new Age("199"),
+                    getVariantSet("DENV2")),
+            new Person(new Name("Roy Balakrishnan"), new Postal("666247"), new Date("2000-11-11"),
+                    new Age("2"),
+                    getVariantSet("DENV4"))
         };
     }
 
