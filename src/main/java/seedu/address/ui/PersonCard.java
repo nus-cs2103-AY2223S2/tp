4d2e6920479ac88005390e54515f8c0fc52d3f73
CedricPei <wenqi_pei@u.nus.edu>
2023-03-27 15:26:58 +0800

    Fix Checkstyle.

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -1,15 +1,14 @@
 package seedu.address.ui;
 
-import javafx.event.EventHandler;
-import javafx.scene.input.MouseEvent;
 import java.util.Comparator;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
-import seedu.address.logic.commands.ViewCommand;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.Person;
