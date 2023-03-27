ebb9029735c830cbf3853a25e2daf55c83ccdf5d
JamesLiuZX <liuzixin@comp.nus.edu.sg>
2023-03-28 00:59:54 +0800

    Change addformcommand

--- src/main/java/seedu/recipe/logic/commands/AddFormCommand.java
+++ src/main/java/seedu/recipe/logic/commands/AddFormCommand.java
@@ -1,8 +1,14 @@
 package seedu.recipe.logic.commands;
 
+import static java.util.Objects.requireNonNull;
+
+import seedu.recipe.logic.commands.exceptions.CommandException;
+import seedu.recipe.logic.parser.AddCommandParser;
+import seedu.recipe.logic.util.RecipeDescriptor;
 import seedu.recipe.model.Model;
-import seedu.recipe.ui.CommandBox.CommandExecutor;
-import seedu.recipe.ui.RecipeForm;
+import seedu.recipe.model.recipe.Recipe;
+import seedu.recipe.ui.AddRecipeForm;
+
 
 /**
  * Opens a new RecipeForm instance.
@@ -11,18 +17,28 @@ public class AddFormCommand extends Command {
 
     public static final String COMMAND_WORD = "addf";
     public static final String MESSAGE_SUCCESS = "Opened a new RecipeForm instance.";
-    private final CommandExecutor commandExecutor;
+    public static final String MESSAGE_DUPLICATE_RECIPE = "This recipe already exists in the recipe book";
+    private RecipeDescriptor toAdd;
 
-    public AddFormCommand(CommandExecutor commandExecutor) {
-        this.commandExecutor = commandExecutor;
+    public AddFormCommand(RecipeDescriptor recipe) {
+        requireNonNull(recipe);
+        toAdd = recipe;
     }
 
     @Override
-    public CommandResult execute(Model model) {
-        // Implement the logic for opening a new RecipeForm instance here
-        RecipeForm recipeForm = new RecipeForm(null, -1,  commandExecutor);
-        recipeForm.display();
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
 
-        return new CommandResult(MESSAGE_SUCCESS);
+        AddRecipeForm recipeForm = new AddRecipeForm();
+        recipeForm.display();
+        String commandString = recipeForm.saveRecipe();
+        toAdd = AddCommandParser.parseToAddCommand(commandString);
+        Recipe recipeToAdd = toAdd.toRecipe();
+
+        if (model.hasRecipe(recipeToAdd)) {
+            throw new CommandException(MESSAGE_DUPLICATE_RECIPE);
+        }
+        model.addRecipe(recipeToAdd);
+        return new CommandResult(String.format(MESSAGE_SUCCESS, recipeToAdd));
     }
 }
