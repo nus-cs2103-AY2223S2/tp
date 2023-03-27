712060353767a7824806852263bff5230f0a4066
ARPspoofing <mohdreyaaz99@gmail.com>
2023-03-27 10:04:30 +0800

    Add telegram handle instead of phone

--- src/main/java/seedu/address/model/person/Phone.java
+++ src/main/java/seedu/address/model/person/Phone.java
@@ -11,8 +11,11 @@ public class Phone {
 
     public static final String NULL_PHONE = "NO_PHONE";
     public static final String MESSAGE_CONSTRAINTS =
-            "Phone numbers should only contain numbers, and it should be at least 3 digits long";
-    public static final String VALIDATION_REGEX = "\\d{3,}";
+            "Telegram handle should only contain numbers, letters, _ and it should be at" +
+                    "least 5 characters long and at most 32 characters long";
+    //public static final String VALIDATION_REGEX = "\\d{3,}";
+    public static final String VALIDATION_REGEX = "^[a-zA-Z0-9]{5,32}$";
+
     public final String value;
 
     public Phone() {
