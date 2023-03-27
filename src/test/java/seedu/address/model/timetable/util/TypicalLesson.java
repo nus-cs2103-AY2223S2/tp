35663be3fce077c5d04822636e1b799a3f2e8aca
Hafeez <izzhafeez@gmail.com>
2023-03-27 21:33:02 +0800

    Improve test coverage

--- src/test/java/seedu/address/model/timetable/util/TypicalLesson.java
+++ src/test/java/seedu/address/model/timetable/util/TypicalLesson.java
@@ -168,3 +168,49 @@ public class TypicalLesson {
             Day.FRIDAY,
             new Location("NUS", 1.341, 103.71));
 }
+
+public class LessonBuilder {
+    public static final String DEFAULT_MODULE_CODE = "CS2103T";
+    public static final LocalTime DEFAULT_START_TIME = new LocalTime(10, 0);
+    public static final LocalTime DEFAULT_END_TIME = new LocalTime(12, 0);
+    public static final Day DEFAULT_DAY = Day.TUESDAY;
+
+    private String moduleCode;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private Day day;
+
+    public LessonBuilder() {
+        moduleCode = DEFAULT_MODULE_CODE;
+        startTime = DEFAULT_START_TIME;
+        endTime = DEFAULT_END_TIME;
+        day = DEFAULT_DAY;
+    }
+
+    public LessonBuilder(Lesson lesson) {
+        moduleCode = lesson.getModuleCode();
+        startTime = lesson.getStartTime();
+        endTime = lesson.getEndTime();
+        day = lesson.getDay();
+    }
+
+    public LessonBuilder withModuleCode(String moduleCode) {
+        this.moduleCode = moduleCode;
+        return this;
+    }
+
+    public LessonBuilder withStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+        return this;
+    }
+
+    public LessonBuilder withDay(Day day) {
+        this.day = day;
+        return this;
+    }
+
+    public Lesson build() {
+        return new Lesson(moduleCode, startTime,
+                endTime, day, Location.NUS);
+    }
+}
