867efa05169f1bec730aa2dc851fc8287c6c978c
lhy-hoyin <88828097+lhy-hoyin@users.noreply.github.com>
2023-03-27 11:36:11 +0800

    Enhance sort feature to auto-sort when events are changed
    Sorting requires user to enter a SortCommand.
    
    This can be a hassle to the user, and may produced unexpected,
    inconsistent results. Eg. user thinks that it had been sorted,
    when in actual fact it isnt, thus possibly causing the user to
    miss an event.
    
    Let's automatically sort the list of events whenever an event is
    added, edited or deleted, to produce a consistent ordering of the
    list of events. This is done via a listener.
    
    A listener is a clean way to check for list changes, and can cater
    to any future commands that also modify the list.
    
    Subsequently, the SortCommand shall be removed.

--- src/main/java/ezschedule/model/event/UniqueEventList.java
+++ src/main/java/ezschedule/model/event/UniqueEventList.java
@@ -9,6 +9,7 @@ import ezschedule.commons.util.CollectionUtil;
 import ezschedule.model.event.exceptions.DuplicateEventException;
 import ezschedule.model.event.exceptions.EventNotFoundException;
 import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 
 /**
@@ -28,6 +29,21 @@ public class UniqueEventList implements Iterable<Event> {
     private final ObservableList<Event> internalList = FXCollections.observableArrayList();
     private final ObservableList<Event> internalUnmodifiableList =
             FXCollections.unmodifiableObservableList(internalList);
+
+    /**
+     * Constructor of UniqueEventList.
+     * Attaches a listener to sort the list whenever its changed.
+     */
+    public UniqueEventList() {
+        // Auto-sort whenever a list is changed
+        internalList.addListener((ListChangeListener<Event>) c -> {
+            while (c.next()) {
+                if (!c.wasPermutated()) {
+                    FXCollections.sort(internalList);
+                }
+            }
+        });
+    }
 
     /**
      * Returns true if the list contains an equivalent event as the given argument.
