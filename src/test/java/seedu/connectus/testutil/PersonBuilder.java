b5253cad7aea48c1590fa50da6eb1070d2062353
Ng Jia Yi <92777033+gremmyz@users.noreply.github.com>
2023-03-28 22:19:38 +0800

    Update PersonBuilder.java

--- src/test/java/seedu/connectus/testutil/PersonBuilder.java
+++ src/test/java/seedu/connectus/testutil/PersonBuilder.java
@@ -195,12 +195,12 @@ public class PersonBuilder {
         }
 
         if (ccas != null) {
-            p.setModules(modules);
+            p.setCcas(ccas);
 
         }
 
         if (ccaPositions != null) {
-            p.setModules(modules);
+            p.setCcaPositions(ccaPositions);
 
         }
 
