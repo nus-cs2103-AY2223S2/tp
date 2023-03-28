a76611a2e3351bf3f5283e081f9c5b2ffd05093a
panavdua <114715893+panavdua@users.noreply.github.com>
2023-03-28 15:34:29 +0800

    Fix checkstyle

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -2,15 +2,17 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import java.util.function.Predicate;
+
 import seedu.address.commons.core.Messages;
 import seedu.address.model.Model;
 import seedu.address.model.employee.Employee;
 
-import java.util.function.Predicate;
+
 
 /**
  * Finds and lists all employees in address book whose name contains any of the argument keywords.
- * Keyword matching is case insensitive.
+ * Keyword matching is case-insensitive.
  */
 public class FindCommand extends Command {
 
