4fff338cb3fdea19de8939efc4510b1f1e2e79fb
francisyzy <francisyzy@me.com>
2023-03-28 20:31:17 +0800

    Add display DetailCommand.MESSAGE_USAGE

--- src/main/java/seedu/vms/logic/parser/patient/DetailCommandParser.java
+++ src/main/java/seedu/vms/logic/parser/patient/DetailCommandParser.java
@@ -1,5 +1,7 @@
 package seedu.vms.logic.parser.patient;
 
+import static seedu.vms.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
 import seedu.vms.commons.core.index.Index;
 import seedu.vms.logic.commands.patient.DetailCommand;
 import seedu.vms.logic.parser.ArgumentMultimap;
@@ -13,7 +15,12 @@ import seedu.vms.logic.parser.exceptions.ParseException;
 public class DetailCommandParser implements CommandParser {
     @Override
     public DetailCommand parse(ArgumentMultimap argsMap) throws ParseException {
-        Index index = ParserUtil.parseIndex(argsMap.getPreamble());
-        return new DetailCommand(index);
+        try {
+            Index index = ParserUtil.parseIndex(argsMap.getPreamble());
+            return new DetailCommand(index);
+        } catch (ParseException pe) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, DetailCommand.MESSAGE_USAGE), pe);
+        }
     }
 }
