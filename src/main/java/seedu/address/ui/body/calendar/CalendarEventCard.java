36c6b3316e8188f67b95043e12ea69a9e549ea2f
hansstanley <nusstuhss@gmail.com>
2023-03-28 17:04:01 +0800

    Add placeholder people tags in events

--- src/main/java/seedu/address/ui/body/calendar/CalendarEventCard.java
+++ src/main/java/seedu/address/ui/body/calendar/CalendarEventCard.java
@@ -3,13 +3,18 @@ package seedu.address.ui.body.calendar;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import seedu.address.commons.core.index.Index;
 import seedu.address.model.event.Event;
 import seedu.address.model.event.fields.DateTime;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.fields.Name;
 import seedu.address.ui.UiPart;
 
 /**
@@ -30,6 +35,8 @@ public class CalendarEventCard extends UiPart<Region> {
     private Label recurrence;
     @FXML
     private Label indexTag;
+    @FXML
+    private HBox attendees;
 
     /**
      * Creates a {@code CalendarEventCard} with the given {@code indexedEvent}.
@@ -47,6 +54,21 @@ public class CalendarEventCard extends UiPart<Region> {
         endDateTime.setText(effectiveEnd.toString(FORMATTER));
         recurrence.setText(event.getRecurrence().toString());
         indexTag.setText(String.format("Index %d", index.getOneBased()));
+
+        // TODO: replace with actual list of people
+        Set<Person> people = Set.of(
+                new Person(new Name("Jane1 Appleseed")),
+                new Person(new Name("Jane2 Appleseed")),
+                new Person(new Name("Jane3 Appleseed")),
+                new Person(new Name("Jane4 Appleseed")),
+                new Person(new Name("Jane5 Appleseed")),
+                new Person(new Name("Jeremy Oppenheimer Jeremy Oppenheimer Jeremy Oppenheimer Jeremy Oppenheimer")),
+                new Person(new Name("Johnny"))
+        );
+        attendees.getChildren().addAll(people.stream()
+                .map(CalendarPersonTag::new)
+                .map(UiPart::getRoot)
+                .collect(Collectors.toList()));
 
         if (effectiveEnd.getDateTime().isBefore(LocalDateTime.now())) {
             getRoot().setOpacity(0.5);
