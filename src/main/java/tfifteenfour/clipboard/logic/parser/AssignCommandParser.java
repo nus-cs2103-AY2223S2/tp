629bccaf9f339a09639cd720f169f988bd91b49f
Isabel Chong <IsabelChong.CYJI@gmail.com>
2023-03-28 22:35:55 +0800

    Update AssignCommandParser to handle validation of grade range

--- src/main/java/tfifteenfour/clipboard/logic/parser/AssignCommandParser.java
+++ src/main/java/tfifteenfour/clipboard/logic/parser/AssignCommandParser.java
@@ -4,7 +4,6 @@ import static tfifteenfour.clipboard.commons.core.Messages.MESSAGE_INVALID_COMMA
 
 import tfifteenfour.clipboard.commons.core.index.Index;
 import tfifteenfour.clipboard.logic.commands.taskcommand.AssignCommand;
-import tfifteenfour.clipboard.logic.commands.taskcommand.TaskCommand;
 import tfifteenfour.clipboard.logic.parser.exceptions.ParseException;
 
 /**
@@ -18,9 +17,21 @@ public class AssignCommandParser implements Parser<AssignCommand> {
      */
     public AssignCommand parse(String args) throws ParseException {
         try {
+            String[] tokens = ArgumentTokenizer.tokenizeString(args);
+            if (tokens.length != 3) {
+                throw new ParseException("Invalid number of arguments");
+            }
+
             args = args.trim();
+
             Index index = ParserUtil.parseIndex(args.split(" ",2)[0]);
             int grade = Integer.parseInt(args.split(" ",2)[1]);
+
+            if (grade < 0 || grade > 100) {
+                throw new ParseException(
+                        String.format(MESSAGE_INVALID_COMMAND_FORMAT, AssignCommand.MESSAGE_USAGE));
+            }
+
             return new AssignCommand(index, grade);
         } catch (ParseException pe) {
             throw new ParseException(
