3e491fc4adb813885f8e764b4414008ab7d4ebc9
Oliver Loo <oliverloo09@gmail.com>
2023-03-28 22:37:46 +0800

    Edit group functionality

--- src/main/java/seedu/address/logic/commands/DisplayGroupCommand.java
+++ src/main/java/seedu/address/logic/commands/DisplayGroupCommand.java
@@ -14,15 +14,15 @@ import seedu.address.model.Model;
 public class DisplayGroupCommand extends Command {
     public static final String COMMAND_WORD = "display";
 
-    public static final String MESSAGE_SUCCESS = "Listed all sessions";
+    public static final String MESSAGE_SUCCESS = "Listed all groups";
 
-    public static final String MESSAGE_TEMPORARY_OUTPUT = "Here are all existing sessions: ";
+    public static final String MESSAGE_TEMPORARY_OUTPUT = "Here are all existing groups: ";
 
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        String results = model.getAddressBook().getSessionList().toString();
+        String results = model.getAddressBook().getTagList().toString();
         return new CommandResult(MESSAGE_TEMPORARY_OUTPUT + results);
     }
 }
--- src/main/java/seedu/address/logic/parser/EditCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditCommandParser.java
@@ -32,8 +32,7 @@ public class EditCommandParser implements Parser<EditCommand> {
     public EditCommand parse(String args) throws ParseException {
         requireNonNull(args);
         ArgumentMultimap argMultimap =
-                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_ADDRESS, PREFIX_PAY_RATE,
-                        PREFIX_TAG);
+                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_ADDRESS, PREFIX_PAY_RATE);
 
         Index index;
 
@@ -56,28 +55,12 @@ public class EditCommandParser implements Parser<EditCommand> {
         if (argMultimap.getValue(PREFIX_PAY_RATE).isPresent()) {
             editPersonDescriptor.setPayRate(ParserUtil.parsePayRate(argMultimap.getValue(PREFIX_PAY_RATE).get()));
         }
-        parseTagsForEdit(argMultimap.getAllValues(PREFIX_TAG)).ifPresent(editPersonDescriptor::setTags);
 
         if (!editPersonDescriptor.isAnyFieldEdited()) {
             throw new ParseException(EditCommand.MESSAGE_NOT_EDITED);
         }
 
         return new EditCommand(index, editPersonDescriptor);
-    }
-
-    /**
-     * Parses {@code Collection<String> tags} into a {@code Set<Tag>} if {@code tags} is non-empty.
-     * If {@code tags} contain only one element which is an empty string, it will be parsed into a
-     * {@code Set<Tag>} containing zero tags.
-     */
-    private Optional<Set<Tag>> parseTagsForEdit(Collection<String> tags) throws ParseException {
-        assert tags != null;
-
-        if (tags.isEmpty()) {
-            return Optional.empty();
-        }
-        Collection<String> tagSet = tags.size() == 1 && tags.contains("") ? Collections.emptySet() : tags;
-        return Optional.of(ParserUtil.parseTags(tagSet));
     }
 
 }
