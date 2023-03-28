aabace1362a0f0a6e238716e67ccfd96a54524e7
cetigerlily <cetigerlily@gmail.com>
2023-03-28 23:02:49 +0800

    Edit style

--- src/main/java/seedu/address/logic/plane/checkplane/CheckPlaneCommand.java
+++ src/main/java/seedu/address/logic/plane/checkplane/CheckPlaneCommand.java
@@ -16,6 +16,7 @@ public class CheckPlaneCommand implements Command {
     private static final String INVALID_INDEX_VALUE_MESSAGE =
             "%s is an invalid value.\n"
                     + "Please try using an integer instead.";
+
     /**
      * The UUID of the plane whose availability is to be checked.
      */
