d484ff6df50b717f8796c515b94bf319486019e4
Isabel Chong <IsabelChong.CYJI@gmail.com>
2023-03-28 13:46:42 +0800

    Update task name to allow spaces

--- src/main/java/tfifteenfour/clipboard/logic/parser/AddCommandParser.java
+++ src/main/java/tfifteenfour/clipboard/logic/parser/AddCommandParser.java
@@ -106,7 +106,9 @@ public class AddCommandParser implements Parser<AddCommand> {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddSessionCommand.MESSAGE_USAGE));
         }
 
-        Task task = ParserUtil.parseTask(tokens[2]);
+        String taskName = args.split(" ",2)[1].split(" ", 2)[1];
+
+        Task task = ParserUtil.parseTask(taskName);
         return task;
     }
 
