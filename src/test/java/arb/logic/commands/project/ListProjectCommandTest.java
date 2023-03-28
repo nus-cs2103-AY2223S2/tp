b07ebd22f8f969e3e62e44c2ad10dedeb392201b
John Chia Heng Kit <programmingwithjohn@gmail.com>
2023-03-28 23:33:57 +0800

    Update tests

--- src/test/java/arb/logic/commands/project/ListProjectCommandTest.java
+++ src/test/java/arb/logic/commands/project/ListProjectCommandTest.java
@@ -16,43 +16,39 @@ import arb.model.UserPrefs;
 public class ListProjectCommandTest {
     private Model model;
     private Model expectedModel;
-
-    private String message;
+    private String expectedModelProjectsMessage;
 
     @BeforeEach
     public void setUp() {
         model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
         expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
-        String message = String.format(ListProjectCommand.MESSAGE_PROJECTS_CONTENT, model.getProjectsContent());
+        expectedModelProjectsMessage = String.format(ListProjectCommand.MESSAGE_PROJECTS_CONTENT,
+                expectedModel.getProjectsContent());
     }
 
     @Test
     public void execute_listIsNotFiltered_showsSameList() {
-        String message = String.format(ListProjectCommand.MESSAGE_PROJECTS_CONTENT, model.getProjectsContent());
         assertCommandSuccess(new ListProjectCommand(), ListType.PROJECT, ListType.PROJECT, model,
-                ListProjectCommand.MESSAGE_SUCCESS + "\n" + message, expectedModel);
+                ListProjectCommand.MESSAGE_SUCCESS + "\n" + expectedModelProjectsMessage, expectedModel);
     }
 
     @Test
     public void execute_currentListShownClient_success() {
-        String message = String.format(ListProjectCommand.MESSAGE_PROJECTS_CONTENT, model.getProjectsContent());
         assertCommandSuccess(new ListProjectCommand(), ListType.CLIENT, ListType.PROJECT, model,
-                ListProjectCommand.MESSAGE_SUCCESS + "\n" + message, expectedModel);
+                ListProjectCommand.MESSAGE_SUCCESS + "\n" + expectedModelProjectsMessage, expectedModel);
     }
 
     @Test
     public void execute_currentListShownTag_success() {
-        String message = String.format(ListProjectCommand.MESSAGE_PROJECTS_CONTENT, model.getProjectsContent());
         assertCommandSuccess(new ListProjectCommand(), ListType.TAG, ListType.PROJECT, model,
-                ListProjectCommand.MESSAGE_SUCCESS + "\n" + message, expectedModel);
+                ListProjectCommand.MESSAGE_SUCCESS + "\n" + expectedModelProjectsMessage, expectedModel);
     }
 
     @Test
     public void execute_listIsFiltered_showsEverything() {
         showProjectAtIndex(model, INDEX_FIRST);
-        String message = String.format(ListProjectCommand.MESSAGE_PROJECTS_CONTENT, model.getProjectsContent());
         assertCommandSuccess(new ListProjectCommand(), ListType.PROJECT, ListType.PROJECT, model,
-                ListProjectCommand.MESSAGE_SUCCESS + "\n" + message, expectedModel);
+                ListProjectCommand.MESSAGE_SUCCESS + "\n" + expectedModelProjectsMessage, expectedModel);
     }
 }
 
