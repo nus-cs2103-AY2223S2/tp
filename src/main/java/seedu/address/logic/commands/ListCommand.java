fffc6290e06b32663ab666ff53eeba1e419c22fa
Gibson <gibsonyip@u.nus.edu>
2023-03-28 20:09:35 +0800

    Update ListCommand.java

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -3,10 +3,10 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
+import java.util.List;
+
 import seedu.address.model.Model;
 import seedu.address.model.person.Person;
-
-import java.util.List;
 
 /**
  * Lists all persons in the address book to the user.
