5ca906bcf184d07cbf7f89869ca4d09264f55379
Wei Jie <ongweijie7@gmail.com>
2023-03-29 05:31:08 +0800

    Edit test case

--- src/main/java/seedu/address/model/person/Email.java
+++ src/main/java/seedu/address/model/person/Email.java
@@ -8,7 +8,7 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
  */
 public class Email {
 
-    public static final String EMPTY_EMAIL = "^//s*(-)//s*$";
+    public static final String EMPTY_EMAIL = "^\\s*(-)\\s*$";
     private static final String SPECIAL_CHARACTERS = "+_.-";
     public static final String MESSAGE_CONSTRAINTS = "Emails should be of the format local-part@domain "
             + "and adhere to the following constraints:\n"
