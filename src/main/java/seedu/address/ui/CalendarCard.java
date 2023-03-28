66155883928e08e6de934436977ef5a417431d4d
Jayanth Balasubramanian <jayanth.balasubramanian1@gmail.com>
2023-03-28 23:12:17 +0800

    Fix failing repository-wide tests

--- src/main/java/seedu/address/ui/CalendarCard.java
+++ src/main/java/seedu/address/ui/CalendarCard.java
@@ -1,4 +1,9 @@
 package seedu.address.ui;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.logging.Logger;
+
 import com.calendarfx.model.Calendar;
 import com.calendarfx.model.CalendarSource;
 import com.calendarfx.model.Entry;
@@ -9,21 +14,23 @@ import javafx.scene.layout.Region;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.model.ReadOnlyAppointmentList;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-
-import java.util.logging.Logger;
-
-// class to display calendar using CalendarFX
+/**
+ * A Calendar card that displays appointments in the appointment list.
+ * Adapted from `CalendarPanel.java` written by ChoChihTun, from
+ * <a href="https://github.com/CS2103JAN2018-T11-B1/main/blob/master/src/main/java/seedu/address/ui/CalendarPanel.java">
+ *     Tuition Connect </a>.
+ */
 public class CalendarCard extends UiPart<Region> {
-
-    private static final String FXML = "CalendarCard.fxml";
-    private final Logger logger = LogsCenter.getLogger(CalendarCard.class);
     private static final CalendarSource source = new CalendarSource("Schedule");
     private static Calendar calendar = new Calendar("Task");
     @FXML
     private static YearMonthView calendarView = new YearMonthView();
+    private static final String FXML = "CalendarCard.fxml";
+    private final Logger logger = LogsCenter.getLogger(CalendarCard.class);
 
+    /**
+     * Creates a {@code CalendarCard}.
+     */
     public CalendarCard() {
         super(FXML);
         calendarView.setRequestedTime(LocalTime.now());
@@ -31,13 +38,12 @@ public class CalendarCard extends UiPart<Region> {
         source.getCalendars().add(calendar);
         calendarView.getCalendarSources().add(source);
         calendarView.setClickBehaviour(YearMonthView.ClickBehaviour.SHOW_DETAILS);
-//        calendarView.setShowAddCalendarButton(false);
-//        calendarView.setShowPrintButton(false);
-//        calendarView.setShowPageToolBarControls(false);
-//        calendarView.setShowSearchField(false);
-//        calendarView.showDayPage();
     }
 
+    /**
+     * Adds appointments to the calendar.
+     * @param appointmentList The appointment list to be added to the calendar.
+     */
     public static void addAppointmentsToCalendar(ReadOnlyAppointmentList appointmentList) {
         for (int i = 0; i < appointmentList.getAppointmentList().size(); i++) {
             Entry<String> entry = new Entry<>(appointmentList.getAppointmentList().get(i).getDescription().description);
@@ -51,6 +57,4 @@ public class CalendarCard extends UiPart<Region> {
     public YearMonthView getRoot() {
         return calendarView;
     }
-
-
 }
