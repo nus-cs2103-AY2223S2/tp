70e1448b2fc8a994b9e9d0b6af4836bafb9b7214
Wei Jie <ongweijie7@gmail.com>
2023-03-29 05:01:35 +0800

    Edit help window to be updated in line with UG

--- src/main/java/seedu/address/ui/HelpWindow.java
+++ src/main/java/seedu/address/ui/HelpWindow.java
@@ -16,24 +16,66 @@ import seedu.address.commons.core.LogsCenter;
 public class HelpWindow extends UiPart<Stage> {
 
     public static final String USERGUIDE_URL = "https://ay2223s2-cs2103t-t11-4.github.io/tp/UserGuide.html";
-    public static final String HELP_MESSAGE = "Refer to the user guide for more in-depth information: " + USERGUIDE_URL;
-    public static final String ADD_COMMAND_MESSAGE = "1) Add a person to the patient records: \n"
-                                                     + "Format: add n/NAME i/NRIC p/PHONE_NUMBER e/EMAIL"
-                                                     + " a/ADDRESS [d/DIAGNOSIS] [t/TAG]";
-    public static final String GET_COMMAND_MESSAGE = "2) Get a patient's record: \nFormat: get i/NRIC";
-    public static final String EDIT_COMMAND_MESSAGE = "3) Edit an existing person in patient records: \n"
+    public static final String HELP_MESSAGE = "Refer to the user guide for more in-depth information: FK" + USERGUIDE_URL;
+
+    public static final String UNDO_COMMAND_MESSAGE = "1) Reverts the patient records to the state"
+                                                        + " before the previous command was executed:\n"
+                                                        + "Format: undo";
+
+    public static final String REDO_COMMAND_MESSAGE = "2) Reverts the patient records to the state"
+                                                        + " before the previous undo was executed:\n"
+                                                        + "Format: redo";
+
+    public static final String ADD_COMMAND_MESSAGE = "3) Add a person to the patient records:\n"
+                                                        + "Format: add n/NAME i/NRIC p/PHONE_NUMBER a/ADDRESS"
+                                                        +  " d/DRUG ALLERGIES g/GENDER"
+                                                        + " [e/EMAIL] [t/TAG] [m/MEDICINE]";
+
+    public static final String EDIT_COMMAND_MESSAGE = "4) Edit an existing person in patient records:\n"
                                                         + "Format: edit i/NRIC [n/NAME] [p/PHONE]"
-                                                        + " [e/EMAIL] [a/ADDRESS] [d/DIAGNOSIS] [t/TAG]";
-    public static final String DELETE_COMMAND_MESSAGE = "4) Deletes patient with the specified NRIC from"
-                                                        + " the address book: \nFormat: delete i/NRIC";
-    public static final String FILTER_COMMAND_MESSAGE = "5) Filters patients according to a particular attribute. "
-                                                        + "Eg. Medicine usage or health conditions: \n"
-                                                        + "Format: filter KEYWORD [MORE_KEYWORDS]";
-    public static final String LIST_COMMAND_MESSAGE = "6) Lists all patients in the records system: \nFormat: list";
-    public static final String SAVE_COMMAND_MESSAGE = "7) Save current data to specified save file: \n"
-                                                      + "Format: save FILE_NO";
-    public static final String CLEAR_COMMAND_MESSAGE = "8) Purge all data in records system: \nFormat: clear";
-    public static final String NOTE_MESSAGE = "Note that square brackets signify optional arguments";
+                                                        + " [a/ADDRESS] [d/DRUG ALLERGIES g/GENDER"
+                                                        + " [e/EMAIL] [t/TAG] [m/MEDICINE]";
+
+    public static final String DELETE_COMMAND_MESSAGE = "5) Deletes patient with the specified NRIC from"
+                                                        + " the address book:\n"
+                                                        + "Format: delete i/NRIC";
+
+    public static final String LIST_COMMAND_MESSAGE = "6) Lists all patients in the records system:\n"
+                                                        + "Format: list";
+
+    public static final String VIEW_COMMAND_MESSAGE = "7) View a patient's record:\n"
+                                                        + "Format: get i/NRIC";
+
+    public static final String FIND_COMMAND_MESSAGE = "8) Find patients according to a particular attribute:\n"
+                                                        + "Format: find (i/NRIC | n/NAME | t/TAG | m/MEDICINE)";
+
+    public static final String BACKUP_COMMAND_MESSAGE = "9) Backs up the patient recors to specified"
+                                                        + " slot represented by an index:\n"
+                                                        + " Format: backup INDEX_NO";
+
+    public static final String LOAD_DATA_COMMAND_MESSAGE = "10) Loads the data from a specified slot represented"
+                                                        + " by an index.\n"
+                                                        + " Format: load INDEX_NO";
+
+    public static final String VIEW_BACKUP_COMMAND_MESSAGE = "11) Shows all the backups available:\n"
+                                                        + "Format: viewbackup";
+
+    public static final String DELETE_BACKUP_COMMAND_MESSAGE = "12) Deletes the data from a specified slot"
+                                                        + " represented by an index.\n"
+                                                        + "Format: deletebackup INDEX_NO";
+
+    public static final String CLEAR_COMMAND_MESSAGE = "13) Purge all data in records system:\n"
+                                                        + "Format: clear";
+
+    public static final String LIGHT_MODE_COMMAND_MESSAGE = "14) Switch to light mode:\n"
+                                                        + "Format: light";
+
+    public static final String DARK_MODE_COMMAND_MESSAGE = "15) Switch to dark mode:\n"
+                                                        + "Format: dark";
+
+    public static final String NOTE_MESSAGE = "Note that square brackets signify optional arguments.\n"
+                                                + "Parentheses indicate that at least one of the arguments"
+                                                + " must be present.";
     private static final Logger logger = LogsCenter.getLogger(HelpWindow.class);
     private static final String FXML = "HelpWindow.fxml";
 
@@ -44,10 +86,16 @@ public class HelpWindow extends UiPart<Stage> {
     private Label helpMessage;
 
     @FXML
+    private Label undoMessage;
+
+    @FXML
+    private Label redoMessage;
+
+    @FXML
     private Label addMessage;
 
     @FXML
-    private Label getMessage;
+    private Label viewMessage;
 
     @FXML
     private Label editMessage;
@@ -56,7 +104,7 @@ public class HelpWindow extends UiPart<Stage> {
     private Label deleteMessage;
 
     @FXML
-    private Label filterMessage;
+    private Label findMessage;
 
     @FXML
     private Label listMessage;
@@ -65,7 +113,22 @@ public class HelpWindow extends UiPart<Stage> {
     private Label noteMessage;
 
     @FXML
-    private Label saveMessage;
+    private Label backupMessage;
+
+    @FXML
+    private Label loadBackupMessage;
+
+    @FXML
+    private Label viewBackupMessage;
+
+    @FXML
+    private Label deleteBackupMessage;
+
+    @FXML
+    private Label lightModeMessage;
+
+    @FXML
+    private Label darkModeMessage;
 
     @FXML
     private Label clearMessage;
@@ -79,15 +142,23 @@ public class HelpWindow extends UiPart<Stage> {
     public HelpWindow(Stage root) {
         super(FXML, root);
         helpMessage.setText(HELP_MESSAGE);
+        undoMessage.setText(UNDO_COMMAND_MESSAGE);
+        redoMessage.setText(REDO_COMMAND_MESSAGE);
         addMessage.setText(ADD_COMMAND_MESSAGE);
-        getMessage.setText(GET_COMMAND_MESSAGE);
         editMessage.setText(EDIT_COMMAND_MESSAGE);
         deleteMessage.setText(DELETE_COMMAND_MESSAGE);
-        filterMessage.setText(FILTER_COMMAND_MESSAGE);
         listMessage.setText(LIST_COMMAND_MESSAGE);
-        saveMessage.setText(SAVE_COMMAND_MESSAGE);
+        viewMessage.setText(VIEW_COMMAND_MESSAGE);
+        findMessage.setText(FIND_COMMAND_MESSAGE);
+        loadBackupMessage.setText(LOAD_DATA_COMMAND_MESSAGE);
+        viewBackupMessage.setText(VIEW_BACKUP_COMMAND_MESSAGE);
+        deleteBackupMessage.setText(DELETE_BACKUP_COMMAND_MESSAGE);
+        backupMessage.setText(BACKUP_COMMAND_MESSAGE);
         clearMessage.setText(CLEAR_COMMAND_MESSAGE);
+        lightModeMessage.setText(LIGHT_MODE_COMMAND_MESSAGE);
+        darkModeMessage.setText(DARK_MODE_COMMAND_MESSAGE);
         noteMessage.setText(NOTE_MESSAGE);
+
     }
 
     /**
