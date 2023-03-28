f5052eae8d7edd803cef7177c064b1f037cb8c89
liankokhai <unfazing@gmail.com>
2023-03-29 01:10:39 +0800

    Add javadoc

--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -114,6 +114,12 @@ public class ParserUtil {
         }
     }
 
+    /**
+     * Parses a String {@code userInput} and returns an empty string if input is empty. Leading and trailing whitespaces
+     * will be trimmed.
+     *
+     * @throws ParseException if the user input contains any characters (excluding whitespaces)
+     */
     public static String parseEmptyInput(String userInput) throws ParseException {
         String trimmedUserInput = userInput.trim();
         if (trimmedUserInput.isEmpty()) {
