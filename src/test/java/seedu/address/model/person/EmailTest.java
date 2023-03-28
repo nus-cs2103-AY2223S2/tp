2badd520a5af7f79d975141f8fb8909ee1586cad
Wei Jie <ongweijie7@gmail.com>
2023-03-29 02:48:17 +0800

    Fix format issues

--- src/test/java/seedu/address/model/person/EmailTest.java
+++ src/test/java/seedu/address/model/person/EmailTest.java
@@ -2,9 +2,10 @@ package seedu.address.model.person;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 import static seedu.address.testutil.Assert.assertThrows;
 
 public class EmailTest {
