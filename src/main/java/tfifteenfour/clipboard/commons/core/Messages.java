0ec9f7a5de2c6d95a2277482ec17ae5dc4a23c82
Isabel Chong <IsabelChong.CYJI@gmail.com>
2023-03-28 17:41:14 +0800

    Add new incorrect index message for task

--- src/main/java/tfifteenfour/clipboard/commons/core/Messages.java
+++ src/main/java/tfifteenfour/clipboard/commons/core/Messages.java
@@ -11,6 +11,7 @@ public class Messages {
     public static final String MESSAGE_INVALID_GROUP_DISPLAYED_INDEX = "The group index provided is invalid";
     public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The student index provided is invalid";
     public static final String MESSAGE_INVALID_SESSION_DISPLAYED_INDEX = "The session index provided is invalid";
+    public static final String MESSAGE_INVALID_TASK_DISPLAYED_INDEX = "The task index provided is invalid";
 
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
 
