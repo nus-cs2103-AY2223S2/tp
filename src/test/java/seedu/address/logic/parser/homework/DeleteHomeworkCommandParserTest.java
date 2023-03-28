62ac024dd91495d869d7e939e944c8a3fd658efb
Muhammad Fahim Tajwar <fahim.tajwar@u.nus.edu>
2023-03-28 21:55:27 +0800

    Remove redundant helper methods in DeleteHomeworkCommand parser.

--- src/test/java/seedu/address/logic/parser/homework/DeleteHomeworkCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/homework/DeleteHomeworkCommandParserTest.java
@@ -17,53 +17,8 @@ public class DeleteHomeworkCommandParserTest {
     public static final String VALID_DEADLINE = "2027-04-31T12:00";
     private AddressBookParser mainParser = new AddressBookParser();
     private DeleteHomeworkCommandParser parser = new DeleteHomeworkCommandParser();
-
-    public void addStudent() {
-        try {
-            mainParser.parseCommand("new-student name/" + VALID_NAME_AMY
-                                            + " phone/" + VALID_PHONE_AMY
-                                            + " email/" + VALID_EMAIL_AMY
-                                            + " address/" + VALID_ADDRESS_AMY
-            );
-        } catch (ParseException e) {
-            //The above command will not throw any ParseException.
-            throw new RuntimeException(e);
-        }
-    }
-
-    public void addHomework(int idx) {
-        String input = "new-homework"
-                + " name/" + VALID_NAME_AMY
-                + " homework/" + "Biology Chapter " + idx
-                + " deadline/" + VALID_DEADLINE;
-        try {
-            mainParser.parseCommand(input);
-        } catch (ParseException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public void clearTutorPro() {
-        try {
-            mainParser.parseCommand("clear");
-        } catch (ParseException e) {
-            //Clear command will not cause a ParseException
-            throw new RuntimeException(e);
-        }
-    }
-
-
     @Test
     public void parse_indexIsNonPositive_failure() {
-        clearTutorPro();
-        addStudent();
-
-        //Add 4 homeworks
-        for (int i = 1; i < 5; i++) {
-            addHomework(i);
-        }
-
-
         //Negative index
         String input1 = " name/" + VALID_NAME_AMY
                 + " index/-1";
