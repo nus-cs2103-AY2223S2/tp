d370e0c97ee5394d8888cbe718588ab2ba7bb683
swxk19 <s.wxk19@gmail.com>
2023-03-29 03:35:15 +0800

    Update UniqueTasksList to extend UniqueList

--- src/main/java/tfifteenfour/clipboard/model/task/UniqueTasksList.java
+++ src/main/java/tfifteenfour/clipboard/model/task/UniqueTasksList.java
@@ -6,9 +6,7 @@ import static tfifteenfour.clipboard.commons.util.CollectionUtil.requireAllNonNu
 import java.util.Iterator;
 import java.util.List;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.collections.transformation.FilteredList;
+import tfifteenfour.clipboard.model.UniqueList;
 import tfifteenfour.clipboard.model.task.exceptions.DuplicateTaskException;
 import tfifteenfour.clipboard.model.task.exceptions.TaskNotFoundException;
 
@@ -23,16 +21,12 @@ import tfifteenfour.clipboard.model.task.exceptions.TaskNotFoundException;
  *
  * @see Task#isSameTask(Task)
  */
-public class UniqueTasksList implements Iterable<Task> {
-
-    private final ObservableList<Task> internalList = FXCollections.observableArrayList();
-    private final ObservableList<Task> internalUnmodifiableList =
-            FXCollections.unmodifiableObservableList(internalList);
-    private final FilteredList<Task> filteredTasks = new FilteredList<>(internalList);
+public class UniqueTasksList extends UniqueList<Task> {
 
     /**
      * Returns true if the list contains an equivalent Task as the given argument.
      */
+    @Override
     public boolean contains(Task toCheck) {
         requireNonNull(toCheck);
         return internalList.stream().anyMatch(toCheck::isSameTask);
@@ -42,6 +36,7 @@ public class UniqueTasksList implements Iterable<Task> {
      * Adds a Task to the list.
      * The Task must not already exist in the list.
      */
+    @Override
     public void add(Task toAdd) {
         requireNonNull(toAdd);
         if (contains(toAdd)) {
@@ -55,7 +50,8 @@ public class UniqueTasksList implements Iterable<Task> {
      * {@code target} must exist in the list.
      * The Task identity of {@code editedTask} must not be the same as another existing Task in the list.
      */
-    public void setTask(Task target, Task editedTask) {
+    @Override
+    public void set(Task target, Task editedTask) {
         requireAllNonNull(target, editedTask);
 
         int index = internalList.indexOf(target);
@@ -68,53 +64,6 @@ public class UniqueTasksList implements Iterable<Task> {
         }
 
         internalList.set(index, editedTask);
-    }
-
-    /**
-     * Removes the equivalent task from the list.
-     * The task must exist in the list.
-     */
-    public void remove(Task toRemove) {
-        requireNonNull(toRemove);
-        if (!internalList.remove(toRemove)) {
-            throw new TaskNotFoundException();
-        }
-    }
-
-    public void setTasks(UniqueTasksList replacement) {
-        requireNonNull(replacement);
-        internalList.setAll(replacement.internalList);
-    }
-
-    /**
-     * Replaces the contents of this list with {@code Tasks}.
-     * {@code Tasks} must not contain duplicate tasks.
-     */
-    public void setSessions(List<Task> tasks) {
-        requireAllNonNull(tasks);
-        if (!tasksAreUnique(tasks)) {
-            throw new DuplicateTaskException();
-        }
-
-        internalList.setAll(tasks);
-    }
-
-    /**
-     * Returns the backing list as an unmodifiable {@code ObservableList}.
-     */
-    public ObservableList<Task> asUnmodifiableObservableList() {
-        return internalUnmodifiableList;
-    }
-
-    public ObservableList<Task> asUnmodifiableFilteredList() {
-        return FXCollections.unmodifiableObservableList(filteredTasks);
-    }
-
-    /**
-     * Returns the backing list as a modifiable {@code ObservableList}.
-     */
-    public ObservableList<Task> asModifiableObservableList() {
-        return internalList;
     }
 
     @Override
@@ -137,7 +86,7 @@ public class UniqueTasksList implements Iterable<Task> {
     /**
      * Returns true if {@code Sessions} contains only unique Sessions.
      */
-    private boolean tasksAreUnique(List<Task> tasks) {
+    protected boolean elementsAreUnique(List<Task> tasks) {
         for (int i = 0; i < tasks.size() - 1; i++) {
             for (int j = i + 1; j < tasks.size(); j++) {
                 if (tasks.get(i).isSameTask(tasks.get(j))) {
