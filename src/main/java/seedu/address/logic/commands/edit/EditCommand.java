16332afcb5f4d85e3f9731a269ca629d08278f3c
hingen <1627313-hingen@users.noreply.gitlab.com>
2023-03-28 22:38:37 +0800

    EditCommand class: update usage message

--- src/main/java/seedu/address/logic/commands/edit/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/edit/EditCommand.java
@@ -4,6 +4,9 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_CODE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_LECTURE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_MODULE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_UNWATCH;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_WATCH;
 
 import seedu.address.logic.commands.Command;
 
@@ -14,32 +17,42 @@ public abstract class EditCommand extends Command {
 
     public static final String COMMAND_WORD = "edit";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": (1) Edits the details of a module in the tracker. "
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ":\n"
+            + "(1) Edits the details of a module in the tracker. "
             + "Parameters: "
             + "{module_code} "
-            + "[" + PREFIX_CODE + " {new_code}] "
-            + "[" + PREFIX_NAME + " {new_name}] "
+            + "[" + PREFIX_CODE + " {updated_code}] "
+            + "[" + PREFIX_NAME + " {updated_name}] "
+            + "[" + PREFIX_TAG + " {tag_1}, [{tag_2}, ...]] "
             + "Example: " + COMMAND_WORD + " CS2040S "
             + PREFIX_CODE + " CS2040 "
-            + PREFIX_NAME + " Data Structures and Algorithms\n"
+            + PREFIX_NAME + " Data Structures and Algorithms "
+            + PREFIX_TAG + " Heavy, Math, Analysis\n"
             + "(2) Edits the details of a lecture in a module. "
             + "Parameters: "
             + "{lecture_name} "
             + "[" + PREFIX_MODULE + " {module_code}] "
-            + "[" + PREFIX_NAME + " {new_name}] "
-            + "Example: " + COMMAND_WORD + " Lecture 01 "
+            + "[" + PREFIX_NAME + " {updated_name}] "
+            + "[" + PREFIX_TAG + " {tag_1}, [{tag_2}, ...]] "
+            + "Example: " + COMMAND_WORD + " Week 1 "
             + PREFIX_MODULE + " CS2040S "
-            + PREFIX_NAME + " Lecture 01 Introduction\n"
+            + PREFIX_NAME + " Week 01 Introduction "
+            + PREFIX_TAG + " Intro, Important\n"
             + "(3) Edits the details of a video in a lecture. "
             + "Parameters: "
             + "{video_name} "
             + "[" + PREFIX_MODULE + " {module_code}] "
             + "[" + PREFIX_LECTURE + " {lecture_name}] "
-            + "[" + PREFIX_NAME + " {new_name}] "
-            + "Example: " + COMMAND_WORD + " Video 01 "
+            + "[" + PREFIX_NAME + " {updated_name}] "
+            + "[" + PREFIX_WATCH + "] "
+            + "[" + PREFIX_UNWATCH + "] "
+            + "[" + PREFIX_TAG + " {tag_1}, [{tag_2}, ...]] "
+            + "Example: " + COMMAND_WORD + " Video 1 "
             + PREFIX_MODULE + " CS2040S "
-            + PREFIX_LECTURE + " Lecture 01 "
-            + PREFIX_NAME + " Video 01 Grade Breakdown";
+            + PREFIX_LECTURE + " Week 1 "
+            + PREFIX_NAME + " Video 01 Grade Breakdown "
+            + PREFIX_WATCH + " "
+            + PREFIX_TAG + " Intro, Short";
 
     public static final String MESSAGE_NOT_EDITED = "At least one field to edit must be provided.";
 
