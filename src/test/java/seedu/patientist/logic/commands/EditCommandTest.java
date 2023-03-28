d58a54cc6026b4c49a889f7cd895545cb259e7db
pzhengze <zhengze@u.nus.edu>
2023-03-28 23:49:24 +0800

    Resolve merge conflicts

--- src/test/java/seedu/patientist/logic/commands/EditCommandTest.java
+++ src/test/java/seedu/patientist/logic/commands/EditCommandTest.java
@@ -1,6 +1,5 @@
 package seedu.patientist.logic.commands;
 
-<<<<<<< HEAD
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.patientist.logic.commands.CommandTestUtil.DESC_AMY;
@@ -30,41 +29,11 @@ import seedu.patientist.model.person.staff.Staff;
 import seedu.patientist.testutil.EditPersonDescriptorBuilder;
 import seedu.patientist.testutil.PatientBuilder;
 import seedu.patientist.testutil.StaffBuilder;
-=======
-//import static org.junit.jupiter.api.Assertions.assertFalse;
-//import static org.junit.jupiter.api.Assertions.assertTrue;
-//import static seedu.patientist.logic.commands.CommandTestUtil.DESC_AMY;
-//import static seedu.patientist.logic.commands.CommandTestUtil.DESC_BOB;
-//import static seedu.patientist.logic.commands.CommandTestUtil.VALID_NAME_BOB;
-//import static seedu.patientist.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
-//import static seedu.patientist.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
-//import static seedu.patientist.logic.commands.CommandTestUtil.assertCommandFailure;
-//import static seedu.patientist.logic.commands.CommandTestUtil.assertCommandSuccess;
-//import static seedu.patientist.logic.commands.CommandTestUtil.showPersonAtIndex;
-//import static seedu.patientist.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
-//import static seedu.patientist.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
-//import static seedu.patientist.testutil.TypicalWards.getTypicalPatientist;
-//
-//import org.junit.jupiter.api.Test;
-//
-//import seedu.patientist.commons.core.Messages;
-//import seedu.patientist.commons.core.index.Index;
-//import seedu.patientist.logic.commands.EditCommand.EditPersonDescriptor;
-//import seedu.patientist.model.Model;
-//import seedu.patientist.model.ModelManager;
-//import seedu.patientist.model.Patientist;
-//import seedu.patientist.model.UserPrefs;
-//import seedu.patientist.model.person.Person;
-//import seedu.patientist.testutil.EditPersonDescriptorBuilder;
-//import seedu.patientist.testutil.PersonBuilder;
->>>>>>> master
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for EditCommand.
  */
 public class EditCommandTest {
-<<<<<<< HEAD
-
     private Model model = new ModelManager(getTypicalPatientist(), new UserPrefs());
 
     @Test
@@ -187,146 +156,4 @@ public class EditCommandTest {
         // different descriptor -> returns false
         assertFalse(standardCommand.equals(new EditCommand(INDEX_FIRST_PERSON, DESC_BOB)));
     }
-=======
-//
-//    private Model model = new ModelManager(getTypicalPatientist(), new UserPrefs());
-//
-//    @Test
-//    public void execute_allFieldsSpecifiedUnfilteredList_success() {
-//        Person editedPerson = null; //new PersonBuilder().build(); TODO: person is now abstract. use patient or staff.
-//        EditPersonDescriptor descriptor = new EditPersonDescriptorBuilder(editedPerson).build();
-//        EditCommand editCommand = new EditCommand(INDEX_FIRST_PERSON, descriptor);
-//
-//        String expectedMessage = String.format(EditCommand.MESSAGE_EDIT_PERSON_SUCCESS, editedPerson);
-//
-//        Model expectedModel = new ModelManager(new Patientist(model.getPatientist()), new UserPrefs());
-//        //expectedModel.setPerson(model.getFilteredPersonList().get(0), editedPerson); TODO: refer to API for new sets
-//
-//        assertCommandSuccess(editCommand, model, expectedMessage, expectedModel);
-//    }
-//
-//    @Test
-//    public void execute_someFieldsSpecifiedUnfilteredList_success() {
-//        Index indexLastPerson = Index.fromOneBased(model.getFilteredPersonList().size());
-//        Person lastPerson = model.getFilteredPersonList().get(indexLastPerson.getZeroBased());
-//
-//        PersonBuilder personInList = null; //new PersonBuilder(lastPerson); TODO: person is now abstract. pat or staff
-//        Person editedPerson = personInList.withName(VALID_NAME_BOB).withPhone(VALID_PHONE_BOB)
-//                .withTags(VALID_TAG_HUSBAND).build();
-//
-//        EditPersonDescriptor descriptor = new EditPersonDescriptorBuilder().withName(VALID_NAME_BOB)
-//                .withPhone(VALID_PHONE_BOB).withTags(VALID_TAG_HUSBAND).build();
-//        EditCommand editCommand = new EditCommand(indexLastPerson, descriptor);
-//
-//        String expectedMessage = String.format(EditCommand.MESSAGE_EDIT_PERSON_SUCCESS, editedPerson);
-//
-//        Model expectedModel = new ModelManager(new Patientist(model.getPatientist()), new UserPrefs());
-//        //expectedModel.setPerson(lastPerson, editedPerson); TODO: refer to API for new sets
-//
-//        assertCommandSuccess(editCommand, model, expectedMessage, expectedModel);
-//    }
-//
-//    @Test
-//    public void execute_noFieldSpecifiedUnfilteredList_success() {
-//        EditCommand editCommand = new EditCommand(INDEX_FIRST_PERSON, new EditPersonDescriptor());
-//        Person editedPerson = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
-//
-//        String expectedMessage = String.format(EditCommand.MESSAGE_EDIT_PERSON_SUCCESS, editedPerson);
-//
-//        Model expectedModel = new ModelManager(new Patientist(model.getPatientist()), new UserPrefs());
-//
-//        assertCommandSuccess(editCommand, model, expectedMessage, expectedModel);
-//    }
-//
-//    @Test
-//    public void execute_filteredList_success() {
-//        showPersonAtIndex(model, INDEX_FIRST_PERSON);
-//
-//        Person personInFilteredList = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
-//        Person editedPerson = null; //new PersonBuilder(personInFilteredList).withName(VALID_NAME_BOB).build();
-//        //TODO: person is now abstract. use staff or patient.
-//        EditCommand editCommand = new EditCommand(INDEX_FIRST_PERSON,
-//                new EditPersonDescriptorBuilder().withName(VALID_NAME_BOB).build());
-//
-//        String expectedMessage = String.format(EditCommand.MESSAGE_EDIT_PERSON_SUCCESS, editedPerson);
-//
-//        Model expectedModel = new ModelManager(new Patientist(model.getPatientist()), new UserPrefs());
-//        //expectedModel.setPerson(model.getFilteredPersonList().get(0), editedPerson); TODO: refer to API for new sets
-//
-//        assertCommandSuccess(editCommand, model, expectedMessage, expectedModel);
-//    }
-//
-//    @Test
-//    public void execute_duplicatePersonUnfilteredList_failure() {
-//        Person firstPerson = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
-//        EditPersonDescriptor descriptor = new EditPersonDescriptorBuilder(firstPerson).build();
-//        EditCommand editCommand = new EditCommand(INDEX_SECOND_PERSON, descriptor);
-//
-//        assertCommandFailure(editCommand, model, EditCommand.MESSAGE_DUPLICATE_PERSON);
-//    }
-//
-//    @Test
-//    public void execute_duplicatePersonFilteredList_failure() {
-//        showPersonAtIndex(model, INDEX_FIRST_PERSON);
-//
-//        // edit person in filtered list into a duplicate in patientist book
-//        Person personInList = model.getPatientist().getPersonList().get(INDEX_SECOND_PERSON.getZeroBased());
-//        EditCommand editCommand = new EditCommand(INDEX_FIRST_PERSON,
-//                new EditPersonDescriptorBuilder(personInList).build());
-//
-//        assertCommandFailure(editCommand, model, EditCommand.MESSAGE_DUPLICATE_PERSON);
-//    }
-//
-//    @Test
-//    public void execute_invalidPersonIndexUnfilteredList_failure() {
-//        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
-//        EditPersonDescriptor descriptor = new EditPersonDescriptorBuilder().withName(VALID_NAME_BOB).build();
-//        EditCommand editCommand = new EditCommand(outOfBoundIndex, descriptor);
-//
-//        assertCommandFailure(editCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
-//    }
-//
-//    /**
-//     * Edit filtered list where index is larger than size of filtered list,
-//     * but smaller than size of patientist book
-//     */
-//    @Test
-//    public void execute_invalidPersonIndexFilteredList_failure() {
-//        showPersonAtIndex(model, INDEX_FIRST_PERSON);
-//        Index outOfBoundIndex = INDEX_SECOND_PERSON;
-//        // ensures that outOfBoundIndex is still in bounds of patientist book list
-//        assertTrue(outOfBoundIndex.getZeroBased() < model.getPatientist().getPersonList().size());
-//
-//        EditCommand editCommand = new EditCommand(outOfBoundIndex,
-//                new EditPersonDescriptorBuilder().withName(VALID_NAME_BOB).build());
-//
-//        assertCommandFailure(editCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
-//    }
-//
-//    @Test
-//    public void equals() {
-//        final EditCommand standardCommand = new EditCommand(INDEX_FIRST_PERSON, DESC_AMY);
-//
-//        // same values -> returns true
-//        EditPersonDescriptor copyDescriptor = new EditPersonDescriptor(DESC_AMY);
-//        EditCommand commandWithSameValues = new EditCommand(INDEX_FIRST_PERSON, copyDescriptor);
-//        assertTrue(standardCommand.equals(commandWithSameValues));
-//
-//        // same object -> returns true
-//        assertTrue(standardCommand.equals(standardCommand));
-//
-//        // null -> returns false
-//        assertFalse(standardCommand.equals(null));
-//
-//        // different types -> returns false
-//        assertFalse(standardCommand.equals(new ClearCommand()));
-//
-//        // different index -> returns false
-//        assertFalse(standardCommand.equals(new EditCommand(INDEX_SECOND_PERSON, DESC_AMY)));
-//
-//        // different descriptor -> returns false
-//        assertFalse(standardCommand.equals(new EditCommand(INDEX_FIRST_PERSON, DESC_BOB)));
-//    }
->>>>>>> master
-
 }
