9c99fe1fc4e6443af5d7092d2eab575733be3a72
Gibson <gibsonyip@u.nus.edu>
2023-03-29 21:44:12 +0800

    Update AddCommand.java

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -52,12 +52,12 @@ public class AddCommand extends Command {
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
+        model.resetPersonHiddenStatus();
         if (model.hasPerson(toAdd)) {
             throw new CommandException(MESSAGE_DUPLICATE_PERSON);
         }
 
         model.addPerson(toAdd);
-        model.resetPersonHiddenStatus();
         return new CommandResult(String.format(MESSAGE_SUCCESS, toAdd));
     }
 
