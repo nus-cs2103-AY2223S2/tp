898c413d2478a469663593e4ebc8133e06f16faf
nerdyboy98 <chewchunkiat@gmail.com>
2023-03-30 03:58:50 +0800

    Change Student help-command header

--- src/main/java/seedu/address/logic/commands/HelpStudentCommand.java
+++ src/main/java/seedu/address/logic/commands/HelpStudentCommand.java
@@ -9,11 +9,12 @@ public class HelpStudentCommand extends HelpCommand {
 
     public static final String COMMAND_WORD = "student";
 
-    public static final String STUDENT_CATEGORIES = "blabla";
+    public static final String HEADER_1 = "----- Student Control Commands -----\n";
 
     @Override
     public CommandResult execute(Model model) {
-        return new CommandResult(AddCommand.MESSAGE_USAGE + "\n"
+        return new CommandResult(HEADER_1
+                + AddCommand.MESSAGE_USAGE + "\n"
                 + DeleteCommand.MESSAGE_USAGE + "\n"
                 + EditCommand.MESSAGE_USAGE + "\n"
                 + FindCommand.MESSAGE_USAGE + "\n"
