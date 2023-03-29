544d8a3dcd09284bd7e8d7b96b50369d72d335ee
UnwilledTangent <tinsean.work@gmail.com>
2023-03-29 19:57:48 +0800

    Add code coverage

--- src/main/java/seedu/roles/logic/parser/TagCommandParser.java
+++ src/main/java/seedu/roles/logic/parser/TagCommandParser.java
@@ -18,14 +18,19 @@ public class TagCommandParser implements Parser<TagCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public TagCommand parse(String args) throws ParseException {
-        String trimmedArgs = args.trim();
-        if (trimmedArgs.isEmpty()) {
+        try {
+            String trimmedArgs = args.trim();
+            if (trimmedArgs.isEmpty()) {
+                throw new ParseException(
+                        String.format(MESSAGE_INVALID_COMMAND_FORMAT, TagCommand.MESSAGE_USAGE));
+            }
+
+            String[] nameKeywords = trimmedArgs.split("\\s+");
+
+            return new TagCommand(new TagContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+        } catch (ParseException pe) {
             throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, TagCommand.MESSAGE_USAGE));
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, TagCommand.MESSAGE_USAGE), pe);
         }
-
-        String[] nameKeywords = trimmedArgs.split("\\s+");
-
-        return new TagCommand(new TagContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
     }
 }
