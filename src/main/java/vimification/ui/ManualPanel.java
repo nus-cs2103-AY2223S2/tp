6c8b6ac1759825bd1f136baf617947a8861f82f6
Ethan <ethanchen2009@hotmail.com>
2023-03-29 22:55:36 +0800

    Update build.gradle and implement ManualPanel

--- src/main/java/vimification/ui/ManualPanel.java
+++ src/main/java/vimification/ui/ManualPanel.java
@@ -1,6 +1,11 @@
 package vimification.ui;
 
+import java.io.File;
+
+import javafx.fxml.FXML;
 import javafx.scene.layout.VBox;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 
 /**
  * An UI component that displays the manual page for Vimification.
@@ -9,8 +14,18 @@ public class ManualPanel extends UiPart<VBox> {
 
     private static final String FXML = "ManualPanel.fxml";
 
+    @FXML
+    private WebView webView;
+
     public ManualPanel() {
         super(FXML);
+        // TODO: Change to Path
+        File manualHtml = new File("./src/main/resources/view/ManualPanel.html");
+        System.out.println(manualHtml.exists());
+        WebEngine webEngine = webView.getEngine();
+
+        webEngine.load(manualHtml.toURI().toString());
+
     }
 
 }
