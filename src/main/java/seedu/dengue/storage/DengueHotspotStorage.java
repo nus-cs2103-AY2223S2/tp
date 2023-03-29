a45a1f1aa01d0691a654d3209e2b315fec9f3097
markusyeo <62845336+markusyeo@users.noreply.github.com>
2023-03-29 19:21:47 +0800

    Update src/main/java/seedu/dengue/storage/DengueHotspotStorage.java
    Co-authored-by: axmszr <72402917+axmszr@users.noreply.github.com>

--- src/main/java/seedu/dengue/storage/DengueHotspotStorage.java
+++ src/main/java/seedu/dengue/storage/DengueHotspotStorage.java
@@ -26,7 +26,7 @@ public interface DengueHotspotStorage {
      * Returns {@code Optional.empty()} if storage file is not found.
      *
      * @throws DataConversionException if the data in storage is not in the expected format.
-     * @throws IOException             if there was any problem when reading from the storage.
+     * @throws IOException if there was any problem when reading from the storage.
      */
     Optional<ReadOnlyDengueHotspotTracker> readDengueHotspotTracker() throws DataConversionException, IOException;
 
