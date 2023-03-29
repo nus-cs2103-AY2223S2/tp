7692f91c49f697b5175e6162f4db7c47d5d4a90a
0x787af25e <74492257+0x787af25e@users.noreply.github.com>
2023-03-29 12:57:15 +0800

    Add import to fix code

--- src/main/java/seedu/address/logic/parser/MassOpCommandParser.java
+++ src/main/java/seedu/address/logic/parser/MassOpCommandParser.java
@@ -14,6 +14,7 @@ import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
 
 import seedu.address.logic.commands.MassOpCommand;
+import seedu.address.logic.commands.TagCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.FieldsMatchRegexPredicate;
 import seedu.address.model.tag.Tag;
