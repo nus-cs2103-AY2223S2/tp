83d6f04c31a48c8eae1c00b500a208f936546f6a
LeonardYam <yamthesmall@gmail.com>
2023-03-29 19:52:59 +0800

    Refactor Template to extend from Character class

--- src/main/java/seedu/address/experimental/model/util/BasicTemplates.java
+++ src/main/java/seedu/address/experimental/model/util/BasicTemplates.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import java.util.List;
 
 import seedu.address.model.entity.Name;
+import seedu.address.model.entity.Stats;
 import seedu.address.model.entity.Template;
 
 /**
@@ -15,9 +16,12 @@ public class BasicTemplates {
 
     public static List<Template> getBasicTemplates() {
         templates.clear();
-        Template orc = new Template(new Name("orc"), 0.8f, 0.1f, 0.1f, new HashSet<>());
-        Template elf = new Template(new Name("elf"), 0.15f, 0.5f, 0.3f, new HashSet<>());
-        Template human = new Template(new Name("human"), 0.2f, 0.2f, 0.8f, new HashSet<>());
+        Stats orcStats = new Stats(15, 6, 1);
+        Stats elfStats = new Stats(6, 10, 10);
+        Stats humanStats = new Stats(7, 9, 9);
+        Template orc = new Template(new Name("orc"), orcStats);
+        Template elf = new Template(new Name("elf"), elfStats);
+        Template human = new Template(new Name("human"), humanStats);
         templates.add(orc);
         templates.add(elf);
         templates.add(human);
--- src/main/java/seedu/address/model/entity/Template.java
+++ src/main/java/seedu/address/model/entity/Template.java
@@ -1,9 +1,7 @@
 package seedu.address.model.entity;
 
-import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+import static java.util.Objects.requireNonNull;
 
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.Set;
 
 import seedu.address.model.tag.Tag;
@@ -11,72 +9,29 @@ import seedu.address.model.tag.Tag;
 /**
  * A template class to create a new Character.
  */
-public class Template {
-
-    private static final int BASE_LEVEL = 1;
-    private static final int BASE_XP = 0;
-    private static final int BASE_POINTS = 10;
-
-    private final Name name;
-    private final float strWeight;
-    private final float dexWeight;
-    private final float intWeight;
-
-    private final Set<Tag> tags = new HashSet<>();
-
-    // May have default inventory.
-
+public class Template extends Character {
     /**
-     * Constructor for Template
+     * Minimal constructor with just name and stats
+     * @param name name of template
+     * @param stats stats of template
      */
-    public Template(Name name, float strWeight, float dexWeight, float intWeight, Set<Tag> tags) {
-        requireAllNonNull(name, tags);
-        this.name = name;
-        this.dexWeight = dexWeight;
-        this.strWeight = strWeight;
-        this.intWeight = intWeight;
-        this.tags.addAll(tags);
-        // The template always tags its own name
-        // this.tags.add(new Tag(name.toString()));
+    public Template(Name name, Stats stats) {
+        super(name, stats);
     }
 
     /**
-     * Returns name
+     * Full constructor for template with all fields inputted
      */
-    public Name getName() {
-        return name;
-    }
-
-    public float getDexWeight() {
-        return dexWeight;
-    }
-
-    public float getStrWeight() {
-        return strWeight;
-    }
-
-    public float getIntWeight() {
-        return intWeight;
-    }
-
-    public Set<Tag> getTags() {
-        return Collections.unmodifiableSet(tags);
+    public Template(Name name, Stats stats, int level, int xp, Inventory inventory, Set<Tag> tags) {
+        super(name, stats, level, xp, inventory, tags);
     }
 
     /**
      * Primitive version of generating character from template.
      */
     public Character generateCharacter(Name characterName) {
-        int dexterity = (int) (BASE_POINTS * dexWeight);
-        int strength = (int) (BASE_POINTS * intWeight);
-        int intelligence = (int) (BASE_POINTS * dexWeight);
-        if (dexterity + strength + intelligence < BASE_POINTS) {
-            // just allocate somewhere...
-            dexterity += BASE_POINTS - dexterity - strength - intelligence;
-        }
-        Stats stats = new Stats(strength, dexterity, intelligence);
-
-        return new Character(characterName, stats, BASE_LEVEL, BASE_XP, tags);
+        requireNonNull(characterName);
+        return new Character(characterName, getStats(), getLevel(), getXP(), getInventory(), getTags());
     }
 
     /**
