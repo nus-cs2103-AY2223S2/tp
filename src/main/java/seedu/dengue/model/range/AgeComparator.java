48f1a4f0b404b985ed7b6759f8a2cdfca00980c8
axmszr <72402917+axmszr@users.noreply.github.com>
2023-03-29 19:53:02 +0800

    Fix line length

--- src/main/java/seedu/dengue/model/range/AgeComparator.java
+++ src/main/java/seedu/dengue/model/range/AgeComparator.java
@@ -11,7 +11,8 @@ public class AgeComparator extends GeneralComparator<Age> {
      * Compares two {@code Age} objects.
      * @param a1 the first object to be compared.
      * @param a2 the second object to be compared.
-     * @return A negative integer, zero, or a positive integer as the first argument is less than, equal to, or greater than the second.
+     * @return A negative integer, zero, or a positive integer if the first argument is
+     *         less than, equal to, or greater than the second.
      */
     public int compare(Age a1, Age a2) {
         int firstAge = Integer.valueOf(a1.value);
