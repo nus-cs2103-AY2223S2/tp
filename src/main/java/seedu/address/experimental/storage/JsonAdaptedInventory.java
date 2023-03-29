40f21d70a5578f8ccc536f57d8f33919ddc61cd4
Jiexuan <81137599+botcon02@users.noreply.github.com>
2023-03-29 15:25:40 +0800

    Fix checkstyle

--- src/main/java/seedu/address/experimental/storage/JsonAdaptedInventory.java
+++ src/main/java/seedu/address/experimental/storage/JsonAdaptedInventory.java
@@ -1,14 +1,15 @@
 package seedu.address.experimental.storage;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import seedu.address.commons.exceptions.IllegalValueException;
-import seedu.address.model.entity.Inventory;
-import seedu.address.model.entity.Item;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.entity.Inventory;
+import seedu.address.model.entity.Item;
 
 /**
  * Jackson friendly version of {@link Inventory}
