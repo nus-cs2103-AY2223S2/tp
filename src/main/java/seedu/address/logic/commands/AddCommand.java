576d7929b9506ec1e718f78c799db8b2f9c28aa2
FredericChow00 <ruihui00.chow@gmail.com>
2023-03-29 20:17:28 +0800

    Replace example with GUI pictures

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -7,6 +7,7 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NOTE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
@@ -58,7 +59,6 @@ public class AddCommand extends Command {
         }
 
         model.addPerson(toAdd);
-        model.updateFilteredPersonList();
         return new CommandResult(String.format(MESSAGE_SUCCESS, toAdd));
     }
 
