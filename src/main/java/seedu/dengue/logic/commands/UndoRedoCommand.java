917a610725ee8caf556449e0c2aac812df64667b
LAPTOP-EG15LSJU\hungy <e0726431@u.nus.edu>
2023-03-29 14:28:40 +0800

    fix javadocs

--- src/main/java/seedu/dengue/logic/commands/UndoRedoCommand.java
+++ src/main/java/seedu/dengue/logic/commands/UndoRedoCommand.java
@@ -3,8 +3,19 @@ package seedu.dengue.logic.commands;
 import seedu.dengue.logic.commands.exceptions.CommandException;
 import seedu.dengue.model.Model;
 
+/**
+ * A command class that handles common operations between {@link UndoCommand} and @{link RedoCommand}.
+ */
 abstract class UndoRedoCommand extends Command {
-    abstract public CommandResult execute(Model model) throws CommandException;
+    public abstract CommandResult execute(Model model) throws CommandException;
+
+    /**
+     * Undo or redo actions by the user for a maximum number of times possible.
+     * @param model The model to undo or redo.
+     * @param numOfSteps An upper bound on the number of steps to undo or redo.
+     * @param isUndo Boolean value that checks to undo or redo.
+     * @return The maximum number of undo or redo operations possible that is less than {@param numOfSteps}.
+     */
     public int undoOrRedoAtMost(Model model, int numOfSteps, boolean isUndo) {
         int counts;
         if (isUndo) {
