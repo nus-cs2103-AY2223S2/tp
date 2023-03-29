7b87452aaa07c19c51aad30038b5bda59699cacc
SeeuSim <seeusimong@gmail.com>
2023-03-29 20:21:58 +0800

    Add fix for import menu kb shortcut

--- src/main/java/seedu/recipe/ui/MainWindow.java
+++ src/main/java/seedu/recipe/ui/MainWindow.java
@@ -102,7 +102,7 @@ public class MainWindow extends UiPart<Stage> {
      */
     private void setAccelerators() {
         setAccelerator(helpMenuItem, KeyCombination.valueOf("F1"));
-        //setAccelerator(importMenuItem, KeyCombination.valueOf("F2"));
+        setAccelerator(importMenuItem, KeyCombination.valueOf("F3"));
     }
 
     /**
