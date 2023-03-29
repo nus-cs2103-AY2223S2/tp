18dd5c704193f9abecf8798f5b2fdda171eed919
Yaladah <jeromeseet@hotmail.com>
2023-03-29 13:49:34 +0800

    Feat: Add edittask command

--- src/test/java/seedu/address/logic/commands/DeadlineCommandTest.java
+++ src/test/java/seedu/address/logic/commands/DeadlineCommandTest.java
@@ -221,6 +221,12 @@ public class DeadlineCommandTest {
         }
 
         @Override
+        public void setTask(Task target, Task editedTask) {
+            // TODO Auto-generated method stub
+            throw new UnsupportedOperationException("Unimplemented method 'setTask'");
+        }
+
+        @Override
         public void deleteTask(Task target) {
             // TODO Auto-generated method stub
             throw new UnsupportedOperationException("Unimplemented method 'deleteTask'");
