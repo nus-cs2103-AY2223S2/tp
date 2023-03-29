0c1a47601ac1e6b8e6e19cf7b57ff4004693e7ac
Jiexuan <81137599+botcon02@users.noreply.github.com>
2023-03-29 15:17:40 +0800

    Implement storage support for Inventory

--- src/main/java/seedu/address/experimental/storage/JsonAdaptedMob.java
+++ src/main/java/seedu/address/experimental/storage/JsonAdaptedMob.java
@@ -10,6 +10,7 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.entity.Inventory;
 import seedu.address.model.entity.Mob;
 import seedu.address.model.entity.Name;
 import seedu.address.model.entity.Stats;
@@ -22,6 +23,7 @@ public class JsonAdaptedMob {
 
     private final String name;
     private final JsonAdaptedStats stats;
+    private final JsonAdaptedInventory inventory;
     private final float challengeRating;
     private final boolean isLegendary;
     private final List<JsonAdaptedTag> tagged = new ArrayList<>();
@@ -31,11 +33,13 @@ public class JsonAdaptedMob {
      */
     @JsonCreator
     JsonAdaptedMob(@JsonProperty("name") String name, @JsonProperty("stats") JsonAdaptedStats stats,
+            @JsonProperty("inventory") JsonAdaptedInventory inventory,
             @JsonProperty("challengeRating") float challengeRating,
             @JsonProperty("isLegendary") boolean isLegendary,
             @JsonProperty("tagged") List<JsonAdaptedTag> tagged) {
         this.name = name;
         this.stats = stats;
+        this.inventory = inventory;
         this.challengeRating = challengeRating;
         this.isLegendary = isLegendary;
         if (tagged != null) {
@@ -49,6 +53,7 @@ public class JsonAdaptedMob {
     public JsonAdaptedMob(Mob source) {
         name = source.getName().fullName;
         stats = new JsonAdaptedStats(source.getStats());
+        inventory = new JsonAdaptedInventory(source.getInventory());
         challengeRating = source.getChallengeRating();
         isLegendary = source.getLegendaryStatus();
         tagged.addAll(source.getTags().stream()
@@ -64,12 +69,13 @@ public class JsonAdaptedMob {
     public Mob toModelType() throws IllegalValueException {
         // dont care
         Stats stat = stats.toModalType();
+        Inventory inventory = this.inventory.toModelType();
         final List<Tag> tags = new ArrayList<>();
         for (JsonAdaptedTag tag : tagged) {
             tags.add(tag.toModelType());
         }
 
         final Set<Tag> modelTags = new HashSet<>(tags);
-        return new Mob(new Name(name), stat, challengeRating, isLegendary, modelTags);
+        return new Mob(new Name(name), stat, challengeRating, isLegendary, modelTags, inventory);
     }
 }
