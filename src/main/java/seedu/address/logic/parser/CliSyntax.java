ce9fff6d443cf3ce81659b9fbb4e402f304001de
Jedidiah Cheng <chengjedidiah@gmail.com>
2023-03-29 20:46:49 +0800

    Add /r root prefix that allows commands to specify root context

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -17,8 +17,9 @@ public class CliSyntax {
     public static final Prefix PREFIX_EMAIL = new Prefix("e/");
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
 
+
+    public static final Prefix PREFIX_ROOT = new Prefix("/r");
     public static final Prefix PREFIX_MODULE = new Prefix("/mod");
     public static final Prefix PREFIX_LECTURE = new Prefix("/lec");
     public static final Prefix PREFIX_VIDEO = new Prefix("/vid");
-
 }
