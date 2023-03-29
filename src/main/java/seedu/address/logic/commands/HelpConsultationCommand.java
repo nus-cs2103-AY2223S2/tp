04a2fdcf6f34a2c3616e9df158ad7ab2c663cc89
nerdyboy98 <chewchunkiat@gmail.com>
2023-03-30 01:38:55 +0800

    Add 2nd Header, Edit & Delete help commands for Consultation

--- src/main/java/seedu/address/logic/commands/HelpConsultationCommand.java
+++ src/main/java/seedu/address/logic/commands/HelpConsultationCommand.java
@@ -9,22 +9,28 @@ public class HelpConsultationCommand extends HelpCommand {
 
     public static final String COMMAND_WORD = "help consultation";
 
-    public static final String HEADER = "Consultation Input Format:\n";
+    public static final String HEADER_1 = "Consultation Basic Inputs:\n";
+
+    public static final String HEADER_2 = "\nOther Inputs:\n";
 
     public static final String ADD_CONSULTATION = "Add: mkdir Consultation/CONSULTATION_NAME";
 
     public static final String DELETE_CONSULTATION = "Delete: delete Consultation/INDEX";
 
-    public static final String EDIT_CONSULTATION = "Edit: coming soon~";
+    public static final String EDIT_CONSULTATION = "Edit: editEvent EVENT_INDEX Consultation/NEW_NAME -date NEW_DATE -file NEW_FILEPATH";
 
     public static final String ADD_STUDENT = "Add Student: addStudent INDEX Consultation/CONSULTATION_NAME";
 
+    public static final String DELETE_STUDENT = "Delete Student: deleteStudent STUDENT_INDEX_IN_LIST Consultation/CONSULTATION_NAME";
+
     @Override
     public CommandResult execute(Model model) {
-        return new CommandResult(HEADER
+        return new CommandResult(HEADER_1
                 + ADD_CONSULTATION + "\n"
                 + DELETE_CONSULTATION + "\n"
+                + HEADER_2
                 + EDIT_CONSULTATION + "\n"
-                + ADD_STUDENT);
+                + ADD_STUDENT + "\n"
+                + DELETE_STUDENT);
     }
 }
