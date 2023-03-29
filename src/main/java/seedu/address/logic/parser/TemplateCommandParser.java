6a9856dfbd1f34cc6c44137bbf262270f43dfa50
LeonardYam <yamthesmall@gmail.com>
2023-03-29 22:34:36 +0800

    Implement TemplateCommandParser

--- src/main/java/seedu/address/logic/parser/TemplateCommandParser.java
+++ src/main/java/seedu/address/logic/parser/TemplateCommandParser.java
@@ -4,8 +4,13 @@ import seedu.address.logic.commands.TemplateCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.entity.Name;
 
+import java.util.regex.Pattern;
+
+// TODO: Improve code quality
 public class TemplateCommandParser implements Parser<TemplateCommand> {
     public TemplateCommand parse(String args) throws ParseException {
-        return new TemplateCommand(new Name("orc"), new Name("test"));
+        boolean isValidCommand = Pattern.matches("^(\\s+[\\w]+)+\\|(\\s+[\\w]+)+$", args.trim());
+        String[] split = args.trim().split("\\|");
+        return new TemplateCommand(new Name(split[0]), new Name(split[1]));
     }
 }
