4c8df49b37361fb8bc2540ee4815f71e79899b4d
gwynethguo <e0905362@u.nus.edu>
2023-03-29 11:46:25 +0800

    Delete unused method

--- src/main/java/seedu/address/logic/parser/ExportProgressCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ExportProgressCommandParser.java
@@ -37,12 +37,4 @@ public class ExportProgressCommandParser implements Parser<ExportProgressCommand
         return new ExportProgressCommand(index, filePath);
     }
 
-    /**
-     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
-     * {@code ArgumentMultimap}.
-     */
-    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
-        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
-    }
-
 }
