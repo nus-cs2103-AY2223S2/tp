af7f71ec7f053204b390d0d358b5ec464f346338
tyx021 <yxting1109@gmail.com>
2023-03-30 02:10:22 +0800

    Implemented viewing attendance based on student - `attendance` under Student Page will show number of sessions attended

--- src/main/java/tfifteenfour/clipboard/logic/commands/attendancecommand/AttendanceCommand.java
+++ src/main/java/tfifteenfour/clipboard/logic/commands/attendancecommand/AttendanceCommand.java
@@ -2,6 +2,7 @@ package tfifteenfour.clipboard.logic.commands.attendancecommand;
 
 import static java.util.Objects.requireNonNull;
 
+import java.util.List;
 import java.util.Map;
 
 import tfifteenfour.clipboard.logic.CurrentSelection;
@@ -13,6 +14,8 @@ import tfifteenfour.clipboard.model.Model;
 import tfifteenfour.clipboard.model.course.Course;
 import tfifteenfour.clipboard.model.course.Group;
 import tfifteenfour.clipboard.model.course.Session;
+import tfifteenfour.clipboard.model.course.UniqueSessionsList;
+import tfifteenfour.clipboard.model.student.SessionWithAttendance;
 import tfifteenfour.clipboard.model.student.Student;
 
 
@@ -22,9 +25,8 @@ import tfifteenfour.clipboard.model.student.Student;
 public class AttendanceCommand extends Command {
     public static final String COMMAND_WORD = "attendance";
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Views the attendance of current session.\n"
+            + ": Views the overall attendance of current session or student.\n"
             + "Example: " + COMMAND_WORD;
-
 
     public AttendanceCommand() {
         super(false);
@@ -44,21 +46,51 @@ public class AttendanceCommand extends Command {
 
         if (currentSelection.getCurrentPage() == PageType.SESSION_PAGE) {
             throw new CommandException("Please select a session to view overall attendance.");
-        } else if (currentSelection.getCurrentPage() != PageType.SESSION_STUDENT_PAGE) {
-            throw new CommandException("Wrong page. Navigate to session page and select a session to view attendance.");
+        } else if (currentSelection.getCurrentPage() == PageType.STUDENT_PAGE && currentSelection.getSelectedStudent() == CurrentSelection.NON_EXISTENT_STUDENT) {
+            throw new CommandException("Please select a student to view his / her overall attendance.");
+        } else if (currentSelection.getCurrentPage() != PageType.SESSION_STUDENT_PAGE &&
+                currentSelection.getCurrentPage() != PageType.STUDENT_PAGE) {
+            throw new CommandException("Wrong page. "
+                    + "Navigate to session page and select a session to view overall attendance of the session."
+                    + "Navigate to student page and select a student to view overall attendance of the student.");
         }
 
         Course course = currentSelection.getSelectedCourse();
         Group group = currentSelection.getSelectedGroup();
-        Session session = currentSelection.getSelectedSession();
-        Map<Student, Integer> attendance = session.getAttendance();
 
-        int numOfTotalStudents = attendance.keySet().size();
-        int numOfPresentStudents = (int) attendance.values().stream().filter(x -> x == 1).count();
 
-        return new CommandResult(this, String.format(
-                "Attendance for %s %s %s:\n %d / %d",
-                course, group, session, numOfPresentStudents, numOfTotalStudents), willModifyState);
+        if (currentSelection.getCurrentPage() == PageType.SESSION_STUDENT_PAGE) {
+            Session session = currentSelection.getSelectedSession();
+            Map<Student, Integer> attendance = session.getAttendance();
+
+            int numOfTotalStudents = attendance.keySet().size();
+            int numOfPresentStudents = (int) attendance.values().stream().filter(x -> x == 1).count();
+
+            return new CommandResult(this, String.format(
+                    "Attendance for %s %s %s:\n %d / %d",
+                    course, group, session, numOfPresentStudents, numOfTotalStudents), willModifyState);
+        } else if (currentSelection.getCurrentPage() == PageType.STUDENT_PAGE) {
+            Student student = currentSelection.getSelectedStudent();
+            List<Session> sessions = group.getUnmodifiableSessionList();
+            int numOfTotalSession = sessions.size();
+            int numOfAttendedSession = 0;
+            UniqueSessionsList sessionsWithAttendance = new UniqueSessionsList();
+
+            for (Session session : sessions) {
+                if (session.getAttendance().get(student) == 1) {
+                    numOfAttendedSession++;
+                    sessionsWithAttendance.add(new SessionWithAttendance(session, 1));
+                } else {
+                    sessionsWithAttendance.add(new SessionWithAttendance(session, 0));
+                }
+            }
+
+            return new CommandResult(this, String.format(
+                    "Attendance for %s in %s %s:\n %d / %d",
+                    student.getName() , course, group, numOfAttendedSession, numOfTotalSession), willModifyState);
+        } else {
+            throw new CommandException("should not reach here!");
+        }
     }
 
 }
