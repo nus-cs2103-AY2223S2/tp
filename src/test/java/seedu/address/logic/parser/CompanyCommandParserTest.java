f1bda5cc948a567c502f298c4295a8d79d3ba0a0
LAPTOP-KMN3PILB\adrie <adrielkee@gmail.com>
2023-03-29 15:04:58 +0800

    Fix long sentences in code

--- src/test/java/seedu/address/logic/parser/CompanyCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/CompanyCommandParserTest.java
@@ -17,7 +17,8 @@ public class CompanyCommandParserTest {
 
     @Test
     public void parse_emptyArg_throwsParseException() {
-        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, CompanyCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                CompanyCommand.MESSAGE_USAGE));
     }
 
     @Test
