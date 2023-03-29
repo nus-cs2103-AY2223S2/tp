3ca854ed44bc71ce717865543540bb5e5ce7decd
YF <e0773591@u.nus.edu>
2023-03-29 23:46:50 +0800

    Update Messages

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -20,7 +20,7 @@ public class Messages {
     public static final String MESSAGE_HOMEWORK_DELETED_SUCCESS = "Homework : %s. %s\n"
             + "Deleted from the %s\n";
     public static final String MESSAGE_LESSON_ADDED_SUCCESS = "New lesson added: \n%s \n"
-        + "To the following students: \n%s";
+            + "To the following students: \n%s";
     public static final String MESSAGE_HOMEWORK_ALREADY_MARKED_AS_DONE =
             "Homework %s of student %s is already marked as done\n";
     public static final String MESSAGE_HOMEWORK_MARKED_AS_DONE = "Homework %s of student %s is marked as done\n";
@@ -32,12 +32,12 @@ public class Messages {
     public static final String MESSAGE_ALL_LESSONS_LISTED_OVERVIEW = "%d lessons from all students listed:\n%s";
     public static final String MESSAGE_LESSONS_LISTED_OVERVIEW = "%d lessons from %d students listed: \n%s";
     public static final String MESSAGE_LESSON_DELETED_SUCCESS = "Lesson: %s, %s\n"
-        + "Deleted from the %s\n";
+            + "Deleted from the %s\n";
     public static final String MESSAGE_INVALID_LESSON_DISPLAYED_INDEX = "The lesson index provided is invalid";
 
     public static final String MESSAGE_INVALID_EXAM_DISPLAYED_INDEX = "The exam index provided is invalid";
     public static final String MESSAGE_EXAM_DELETED_SUCCESS = "Exam: %s, %s\n"
-        + "Deleted from the %s\n";
+            + "Deleted from the %s\n";
 
     public static final String MESSAGE_EXAM_ADDED_SUCCESS = "New exam added: \n%s \n"
             + "To the following students: \n%s";
@@ -48,10 +48,30 @@ public class Messages {
             + "Homework name: %s\n"
             + "Deadline: %s\n";
     public static final String MESSAGE_LESSON_UPDATED_SUCCESS = "Lesson %s of student %s is updated to:\n"
-        + "Lesson name: %s\n"
-        + "Start Time: %s\n"
-        + "End Time: %s\n";
+            + "Lesson name: %s\n"
+            + "Start Time: %s\n"
+            + "End Time: %s\n";
     public static final String MESSAGE_HAS_DUPLICATE_NAMES = "Duplicate names detected for **%s**."
             + "\nPlease enter full name(s)";
     public static final String MESSAGE_NO_SUCH_STUDENT = "No student found: **%s**.\nPlease check the name entered";
+
+    public static final String MESSAGE_INVALID_LESSON_TIME = "Start time cannot be after end time";
+    public static final String MESSAGE_INVALID_LESSON_DURATION =
+            "The lesson duration is too short(< 30 min)/long(> 3 hours)";
+    public static final String MESSAGE_INVALID_EXAM_TIME = "Exam start time cannot be after exam end time";
+    public static final String MESSAGE_INVALID_EXAM_DURATION =
+            "The exam duration is too short(< 30 min)/long(> 3 hours)";
+
+    public static final String MESSAGE_DEADLINE_IN_PAST = "Deadline cannot be in the past!";
+    public static final String MESSAGE_ONLY_ONE_STUDENT = "Only one student name is allowed!";
+    public static final String MESSAGE_EMPTY_STUDENT = "Student name cannot be empty!";
+    public static final String MESSAGE_ONLY_ONE_HOMEWORK = "Only one homework name is allowed!";
+    public static final String MESSAGE_EMPTY_HOMEWORK = "Homework name cannot be empty!";
+    public static final String MESSAGE_ONLY_ONE_INDEX = "Only one index is allowed!";
+    public static final String MESSAGE_EMPTY_INDEX = "Index cannot be empty!";
+    public static final String MESSAGE_ONLY_ONE_DEADLINE = "Only one deadline is allowed!";
+    public static final String MESSAGE_EMPTY_DEADLINE = "Deadline cannot be empty!";
+    public static final Object MESSAGE_MULTIPLE_STATUS = "Only one status is allowed!";
+    public static final Object MESSAGE_EMPTY_STATUS = "Status cannot be empty!";
+
 }
