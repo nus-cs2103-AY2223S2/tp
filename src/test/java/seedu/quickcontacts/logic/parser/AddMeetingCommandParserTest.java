00fd023fe15622f50baf5c8dc5c182e6ee688c54
Jia Wei Lee <b33b33o11@gmail.com>
2023-03-29 21:47:11 +0800

    Fix `AddMeetingCommandParserTest`

--- src/test/java/seedu/quickcontacts/logic/parser/AddMeetingCommandParserTest.java
+++ src/test/java/seedu/quickcontacts/logic/parser/AddMeetingCommandParserTest.java
@@ -1,11 +1,24 @@
 package seedu.quickcontacts.logic.parser;
 
 import static seedu.quickcontacts.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.quickcontacts.logic.commands.CommandTestUtil.MEETING_DATETIME_DESC;
+import static seedu.quickcontacts.logic.commands.CommandTestUtil.MEETING_DESCRIPTION_DESC;
+import static seedu.quickcontacts.logic.commands.CommandTestUtil.MEETING_LOCATION_DESC;
+import static seedu.quickcontacts.logic.commands.CommandTestUtil.MEETING_PERSON_ALICE_DESC;
+import static seedu.quickcontacts.logic.commands.CommandTestUtil.MEETING_PERSON_BENSON_DESC;
+import static seedu.quickcontacts.logic.commands.CommandTestUtil.MEETING_TITLE_DESC;
+import static seedu.quickcontacts.logic.commands.CommandTestUtil.NAME_DESC_ALICE;
+import static seedu.quickcontacts.logic.commands.CommandTestUtil.PREAMBLE_WHITESPACE;
+import static seedu.quickcontacts.logic.commands.CommandTestUtil.VALID_MEETING_DATETIME;
+import static seedu.quickcontacts.logic.commands.CommandTestUtil.VALID_MEETING_DESCRIPTION;
+import static seedu.quickcontacts.logic.commands.CommandTestUtil.VALID_MEETING_LOCATION;
+import static seedu.quickcontacts.logic.commands.CommandTestUtil.VALID_MEETING_TITLE;
+import static seedu.quickcontacts.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.quickcontacts.logic.parser.CommandParserTestUtil.assertParseSuccess;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.quickcontacts.logic.commands.AddMeetingCommand;
-import seedu.quickcontacts.logic.commands.CommandTestUtil;
 import seedu.quickcontacts.model.meeting.Meeting;
 import seedu.quickcontacts.testutil.MeetingBuilder;
 import seedu.quickcontacts.testutil.TypicalPersons;
@@ -15,40 +28,35 @@ public class AddMeetingCommandParserTest {
 
     @Test
     public void parse_allFieldsPresent_success() {
-        Meeting expectedMeetingWithOnePerson = new MeetingBuilder().withTitle(CommandTestUtil.VALID_MEETING_TITLE)
-            .withDateTime(CommandTestUtil.VALID_MEETING_DATETIME).withAttendees(TypicalPersons.ALICE)
-            .withLocation(CommandTestUtil.VALID_MEETING_LOCATION)
-            .withDescription(CommandTestUtil.VALID_MEETING_DESCRIPTION).build();
-        Meeting expectedMeetingWithTwoPersons = new MeetingBuilder().withTitle(CommandTestUtil.VALID_MEETING_TITLE)
-            .withDateTime(CommandTestUtil.VALID_MEETING_DATETIME)
-            .withAttendees(TypicalPersons.ALICE, TypicalPersons.BENSON)
-            .withLocation(CommandTestUtil.VALID_MEETING_LOCATION)
-            .withDescription(CommandTestUtil.VALID_MEETING_DESCRIPTION).build();
+        Meeting expectedMeetingWithOnePerson = new MeetingBuilder().withTitle(VALID_MEETING_TITLE)
+                .withDateTime(VALID_MEETING_DATETIME).withAttendees(TypicalPersons.ALICE)
+                .withLocation(VALID_MEETING_LOCATION)
+                .withDescription(VALID_MEETING_DESCRIPTION).build();
+        Meeting expectedMeetingWithTwoPersons = new MeetingBuilder().withTitle(VALID_MEETING_TITLE)
+                .withDateTime(VALID_MEETING_DATETIME).withAttendees(TypicalPersons.ALICE, TypicalPersons.BENSON)
+                .withLocation(VALID_MEETING_LOCATION).withDescription(VALID_MEETING_DESCRIPTION).build();
 
         // whitespace only preamble
-        CommandParserTestUtil.assertParseSuccess(parser, CommandTestUtil.PREAMBLE_WHITESPACE
-                + CommandTestUtil.MEETING_TITLE_DESC + CommandTestUtil.MEETING_DATETIME_DESC
-                + CommandTestUtil.MEETING_PERSON_ALICE_DESC + CommandTestUtil.MEETING_LOCATION_DESC
-                + CommandTestUtil.MEETING_DESCRIPTION_DESC, new AddMeetingCommand(expectedMeetingWithOnePerson));
+        assertParseSuccess(parser, PREAMBLE_WHITESPACE + MEETING_TITLE_DESC + MEETING_DATETIME_DESC
+                        + MEETING_PERSON_ALICE_DESC + MEETING_LOCATION_DESC + MEETING_DESCRIPTION_DESC,
+                new AddMeetingCommand(expectedMeetingWithOnePerson));
 
         // no whitespace preamble
-        CommandParserTestUtil.assertParseSuccess(parser, CommandTestUtil.MEETING_TITLE_DESC
-                + CommandTestUtil.MEETING_DATETIME_DESC + CommandTestUtil.MEETING_PERSON_ALICE_DESC
-                + CommandTestUtil.MEETING_LOCATION_DESC + CommandTestUtil.MEETING_DESCRIPTION_DESC,
-            new AddMeetingCommand(expectedMeetingWithOnePerson));
+        assertParseSuccess(parser, MEETING_TITLE_DESC + MEETING_DATETIME_DESC
+                        + MEETING_PERSON_ALICE_DESC + MEETING_LOCATION_DESC + MEETING_DESCRIPTION_DESC,
+                new AddMeetingCommand(expectedMeetingWithOnePerson));
 
         // whitespace only preamble and multiple persons
-        CommandParserTestUtil.assertParseSuccess(parser, CommandTestUtil.PREAMBLE_WHITESPACE
-                + CommandTestUtil.MEETING_TITLE_DESC + CommandTestUtil.MEETING_DATETIME_DESC
-                + CommandTestUtil.MEETING_PERSON_ALICE_DESC + CommandTestUtil.MEETING_PERSON_BENSON_DESC
-                + CommandTestUtil.MEETING_LOCATION_DESC + CommandTestUtil.MEETING_DESCRIPTION_DESC,
-            new AddMeetingCommand(expectedMeetingWithTwoPersons));
+        assertParseSuccess(parser, PREAMBLE_WHITESPACE + MEETING_TITLE_DESC + MEETING_DATETIME_DESC
+                        + MEETING_PERSON_ALICE_DESC + MEETING_PERSON_BENSON_DESC + MEETING_LOCATION_DESC
+                        + MEETING_DESCRIPTION_DESC,
+                new AddMeetingCommand(expectedMeetingWithTwoPersons));
 
         // no whitespace only preamble and multiple persons
-        CommandParserTestUtil.assertParseSuccess(parser, CommandTestUtil.MEETING_TITLE_DESC
-                + CommandTestUtil.MEETING_DATETIME_DESC + CommandTestUtil.MEETING_PERSON_ALICE_DESC
-                + CommandTestUtil.MEETING_PERSON_BENSON_DESC + CommandTestUtil.MEETING_LOCATION_DESC
-                + CommandTestUtil.MEETING_DESCRIPTION_DESC, new AddMeetingCommand(expectedMeetingWithTwoPersons));
+        assertParseSuccess(parser, MEETING_TITLE_DESC + MEETING_DATETIME_DESC
+                        + MEETING_PERSON_ALICE_DESC + MEETING_PERSON_BENSON_DESC + MEETING_LOCATION_DESC
+                        + MEETING_DESCRIPTION_DESC,
+                new AddMeetingCommand(expectedMeetingWithTwoPersons));
     }
 
     @Test
@@ -56,33 +64,15 @@ public class AddMeetingCommandParserTest {
         String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddMeetingCommand.MESSAGE_USAGE);
 
         // missing title prefix
-        CommandParserTestUtil.assertParseFailure(parser, CommandTestUtil.VALID_MEETING_TITLE
-            + CommandTestUtil.MEETING_DATETIME_DESC + CommandTestUtil.MEETING_PERSON_ALICE_DESC
-            + CommandTestUtil.MEETING_LOCATION_DESC + CommandTestUtil.MEETING_DESCRIPTION_DESC, expectedMessage);
+        assertParseFailure(parser, VALID_MEETING_TITLE + MEETING_DATETIME_DESC + MEETING_PERSON_ALICE_DESC
+                + MEETING_LOCATION_DESC + MEETING_DESCRIPTION_DESC, expectedMessage);
 
         // missing datetime prefix
-        CommandParserTestUtil.assertParseFailure(parser, CommandTestUtil.MEETING_TITLE_DESC
-            + CommandTestUtil.VALID_MEETING_DATETIME + CommandTestUtil.MEETING_PERSON_ALICE_DESC
-            + CommandTestUtil.MEETING_LOCATION_DESC + CommandTestUtil.MEETING_DESCRIPTION_DESC, expectedMessage);
-
-        // missing person prefix
-        CommandParserTestUtil.assertParseFailure(parser, CommandTestUtil.MEETING_TITLE_DESC
-            + CommandTestUtil.MEETING_DATETIME_DESC + CommandTestUtil.NAME_DESC_AMY
-            + CommandTestUtil.MEETING_LOCATION_DESC + CommandTestUtil.MEETING_DESCRIPTION_DESC, expectedMessage);
-
-        // missing location prefix
-        CommandParserTestUtil.assertParseFailure(parser, CommandTestUtil.MEETING_TITLE_DESC
-            + CommandTestUtil.MEETING_DATETIME_DESC + CommandTestUtil.MEETING_PERSON_ALICE_DESC
-            + CommandTestUtil.VALID_MEETING_LOCATION + CommandTestUtil.MEETING_DESCRIPTION_DESC, expectedMessage);
-
-        // missing description prefix
-        CommandParserTestUtil.assertParseFailure(parser, CommandTestUtil.MEETING_TITLE_DESC
-            + CommandTestUtil.MEETING_DATETIME_DESC + CommandTestUtil.MEETING_PERSON_ALICE_DESC
-            + CommandTestUtil.MEETING_LOCATION_DESC + CommandTestUtil.VALID_MEETING_DESCRIPTION, expectedMessage);
+        assertParseFailure(parser, MEETING_TITLE_DESC + VALID_MEETING_DATETIME + MEETING_PERSON_ALICE_DESC
+                + MEETING_LOCATION_DESC + MEETING_DESCRIPTION_DESC, expectedMessage);
 
         // all prefixes missing
-        CommandParserTestUtil.assertParseFailure(parser, CommandTestUtil.VALID_MEETING_TITLE
-            + CommandTestUtil.VALID_MEETING_DATETIME + CommandTestUtil.NAME_DESC_ALICE
-            + CommandTestUtil.VALID_MEETING_LOCATION + CommandTestUtil.VALID_MEETING_DESCRIPTION, expectedMessage);
+        assertParseFailure(parser, VALID_MEETING_TITLE + VALID_MEETING_DATETIME + NAME_DESC_ALICE
+                + VALID_MEETING_LOCATION + VALID_MEETING_DESCRIPTION, expectedMessage);
     }
 }
