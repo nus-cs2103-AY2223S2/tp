fe66cd41bb59600ecedd889fe05c51c4beccc5d1
nerdyboy98 <chewchunkiat@gmail.com>
2023-03-30 01:32:05 +0800

    Add 2nd Header, Edit & Delete help commands for Lab

--- src/main/java/seedu/address/logic/commands/HelpLabCommand.java
+++ src/main/java/seedu/address/logic/commands/HelpLabCommand.java
@@ -9,22 +9,28 @@ public class HelpLabCommand extends HelpCommand {
 
     public static final String COMMAND_WORD = "help lab";
 
-    public static final String HEADER = "Labs Input Format:\n";
+    public static final String HEADER_1 = "Labs Basic Inputs:\n";
+
+    public static final String HEADER_2 = "\nOther Inputs:\n";
 
     public static final String ADD_LAB = "Add: vim Lab/LAB_NAME";
 
     public static final String DELETE_LAB = "Delete: delete Lab/INDEX";
 
-    public static final String EDIT_LAB = "Edit: coming soon~";
+    public static final String EDIT_LAB = "Edit: editEvent EVENT_INDEX Lab/NEW_NAME -date NEW_DATE -file NEW_FILEPATH";
 
     public static final String ADD_STUDENT = "Add Student: addStudent INDEX Lab/LAB_NAME";
 
+    public static final String DELETE_STUDENT = "Delete Student: deleteStudent STUDENT_INDEX_IN_LIST Lab/LAB_NAME";
+
     @Override
     public CommandResult execute(Model model) {
-        return new CommandResult(HEADER
+        return new CommandResult(HEADER_1
                 + ADD_LAB + "\n"
                 + DELETE_LAB + "\n"
+                + HEADER_2
                 + EDIT_LAB + "\n"
-                + ADD_STUDENT);
+                + ADD_STUDENT + "\n"
+                + DELETE_STUDENT);
     }
 }
