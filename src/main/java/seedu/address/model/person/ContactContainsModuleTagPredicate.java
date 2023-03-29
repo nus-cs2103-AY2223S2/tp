7d3a586fc18fbda31be5b2be1fc85a3ef1d7025c
joellow88 <joellow88@gmail.com>
2023-03-29 16:29:44 +0800

    Update filter command

---
+++ src/main/java/seedu/address/model/person/ContactContainsModuleTagPredicate.java
@@ -0,0 +1,31 @@
+package seedu.address.model.person;
+
+import seedu.address.model.tag.ModuleTag;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.Predicate;
+
+public class ContactContainsModuleTagPredicate implements Predicate<Person> {
+    private final Set<ModuleTag> tagSet;
+
+    public ContactContainsModuleTagPredicate(Set<ModuleTag> tagSet) {
+        this.tagSet = tagSet;
+    }
+
+    @Override
+    public boolean test(Person person) {
+        // perform filtering based on the tag attribute
+        Set<ModuleTag> personTags = person.getModuleTags();
+        Set<ModuleTag> tempTagsSet = new HashSet<>(this.tagSet);
+        tempTagsSet.retainAll(personTags);
+        return !tempTagsSet.isEmpty();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof ContactContainsModuleTagPredicate // instanceof handles nulls
+                && tagSet.equals(((ContactContainsModuleTagPredicate) other).tagSet)); // state check
+    }
+}
