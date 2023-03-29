dc5a76757dc83ec22e0d52211bab5370e3becb6f
Shi Jiaao <e0372500@u.nus.edu>
2023-03-29 19:52:05 +0800

    Add method into AddCommandTest's ModelStub

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -149,6 +149,11 @@ public class AddCommandTest {
         }
 
         @Override
+        public void deleteStudentFromEvent(Index index, String eventName, String eventType) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
         public void setAddressBook(ReadOnlyAddressBook newData) {
             throw new AssertionError("This method should not be called.");
         }
