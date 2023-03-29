97cfffca0ee08185e530e13336a68a937bb40549
DESKTOP-76Q8Q8L\Owner <e0772512@u.nus.edu>
2023-03-29 10:45:43 +0800

    Fixed validation regex for business size and past transaction count classes

--- src/main/java/seedu/address/model/person/BusinessSize.java
+++ src/main/java/seedu/address/model/person/BusinessSize.java
@@ -9,7 +9,7 @@ public class BusinessSize {
 
     public static final String MESSAGE_CONSTRAINTS =
             "Business Size can take any number greater than or equal to 0, and it should not be blank";
-    public static final String VALIDATION_REGEX = "[^\\\\s].*"; //todo: change to take in number >= 0
+    public static final String VALIDATION_REGEX = "^[0-9]\\d*$";
     public final String value;
 
     /**
