d105c19385fe9d4584e88e96e8f1ed5ea27de431
noahxinjie <noahxinjie@gmail.com>
2023-03-29 18:06:56 +0800

    Change focus to role for AlphabeticalComparator

--- src/main/java/seedu/address/model/application/AlphabeticalComparator.java
+++ src/main/java/seedu/address/model/application/AlphabeticalComparator.java
@@ -17,20 +17,22 @@ public class AlphabeticalComparator implements Comparator<Application> {
 
     /**
      * Compares two applications such that the application ahead in terms
-     * of alphabetical order will be considered smaller than the other application.
+     * of alphabetical order for the role will be considered smaller
+     * than the other application.
      * @param appOne the first application to be compared.
      * @param appTwo the second application, to be compared to the first one.
      * @return an integer that represents whether the first application is smaller
      *      than the second.
      */
     public int compareAscending(Application appOne, Application appTwo) {
-        if (appOne.getCompanyName().toString().compareToIgnoreCase(appTwo.getCompanyName().toString()) > 0) {
+        if (appOne.getRole().toString().compareToIgnoreCase(appTwo.getRole().toString()) > 0) {
             return 1;
-        } else if (appOne.getCompanyName().toString().compareToIgnoreCase(appTwo.getCompanyName().toString()) < 0) {
+        } else if (appOne.getRole().toString().compareToIgnoreCase(appTwo.getRole().toString()) < 0) {
             return -1;
         } else {
-            // use role as tiebreaker
-            return Integer.compare(appOne.getRole().toString().compareToIgnoreCase(appTwo.getRole().toString()), 0);
+            // use company name as tiebreaker
+            return Integer.compare(appOne.getCompanyName().toString()
+                    .compareToIgnoreCase(appTwo.getCompanyName().toString()), 0);
         }
     }
 
