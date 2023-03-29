06848cfd80fa717e4e46bc5d22797751f59ebe59
Wong-Jia-Jun <xxjiajun@gmail.com>
2023-03-29 21:39:56 +0800

    update testclass checkstyle

--- src/test/java/seedu/library/logic/commands/ViewCommandTest.java
+++ src/test/java/seedu/library/logic/commands/ViewCommandTest.java
@@ -5,19 +5,21 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.library.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.library.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.library.logic.commands.CommandTestUtil.showBookmarkAtIndex;
+import static seedu.library.testutil.TypicalBookmarks.getTypicalLibrary;
 import static seedu.library.testutil.TypicalIndexes.INDEX_FIRST_BOOKMARK;
 import static seedu.library.testutil.TypicalIndexes.INDEX_SECOND_BOOKMARK;
-import static seedu.library.testutil.TypicalBookmarks.getTypicalLibrary;
+
 
 import org.junit.jupiter.api.Test;
 
 import seedu.library.commons.core.Messages;
 import seedu.library.commons.core.index.Index;
+import seedu.library.model.bookmark.Bookmark;
 import seedu.library.model.Model;
 import seedu.library.model.ModelManager;
 import seedu.library.model.Tags;
 import seedu.library.model.UserPrefs;
-import seedu.library.model.bookmark.Bookmark;
+
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for
