3cdbc3531790db3b395f50cbaa8d97c00b41d63a
nerdyboy98 <chewchunkiat@gmail.com>
2023-03-30 01:26:32 +0800

    Add 2nd Header, Edit & Delete help commands for Tutorial

--- src/main/java/seedu/address/logic/commands/HelpTutorialCommand.java
+++ src/main/java/seedu/address/logic/commands/HelpTutorialCommand.java
@@ -9,22 +9,28 @@ public class HelpTutorialCommand extends HelpCommand {
 
     public static final String COMMAND_WORD = "help tutorial";
 
-    public static final String HEADER = "Tutorial Input Format:\n";
+    public static final String HEADER_1 = "Tutorial Basic Inputs:\n";
+
+    public static final String HEADER_2 = "\nOther Inputs:\n";
 
     public static final String ADD_TUTORIAL = "Add: touch Tutorial/TUTORIAL_NAME";
 
     public static final String DELETE_TUTORIAL = "Delete: delete Tutorial/INDEX";
 
-    public static final String EDIT_TUTORIAL = "Edit: coming soon~";
+    public static final String EDIT_TUTORIAL = "Edit: editEvent EVENT_INDEX Tutorial/NEW_NAME -date NEW_DATE -file NEW_FILEPATH";
 
     public static final String ADD_STUDENT = "Add Student: addStudent INDEX Tutorial/TUTORIAL_NAME";
 
+    public static final String DELETE_STUDENT = "Delete Student: deleteStudent STUDENT_INDEX_IN_LIST Tutorial/TUTORIAL_NAME";
+
     @Override
     public CommandResult execute(Model model) {
-        return new CommandResult(HEADER
+        return new CommandResult(HEADER_1
                 + ADD_TUTORIAL + "\n"
                 + DELETE_TUTORIAL + "\n"
+                + HEADER_2
                 + EDIT_TUTORIAL + "\n"
-                + ADD_STUDENT);
+                + ADD_STUDENT + "\n"
+                + DELETE_STUDENT);
     }
 }
