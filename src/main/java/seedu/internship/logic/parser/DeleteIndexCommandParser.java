6c47b4e124d71a6fe6094d7222de090abc077c68
potty10 <christophertan8664@gmail.com>
2023-03-29 10:41:23 +0800

    Improve codecov

--- src/main/java/seedu/internship/logic/parser/DeleteIndexCommandParser.java
+++ src/main/java/seedu/internship/logic/parser/DeleteIndexCommandParser.java
@@ -34,40 +34,4 @@ public class DeleteIndexCommandParser implements Parser<DeleteIndexCommand> {
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteIndexCommand.MESSAGE_USAGE), pe);
         }
     }
-
-    private List<String> parseCompanyNames(List<String> unparsedNames) throws ParseException {
-        List<String> parsedNames = ParserUtil.parseCompanyNames(unparsedNames).stream()
-                .map(name -> name.fullCompanyName)
-                .collect(Collectors.toList());
-        return parsedNames;
-    }
-
-    private List<String> parseRoles(List<String> unparsedRoles) throws ParseException {
-        List<String> parsedRoles = ParserUtil.parseRoles(unparsedRoles).stream()
-                .map(role -> role.fullRole)
-                .collect(Collectors.toList());
-        return parsedRoles;
-    }
-
-    private List<String> parseStatuses(List<String> unparsedStatuses) throws ParseException {
-        List<String> parsedStatuses = ParserUtil.parseStatuses(unparsedStatuses).stream()
-                .map(status -> status.fullStatus)
-                .collect(Collectors.toList());
-        return parsedStatuses;
-    }
-
-    private List<String> parseDates(List<String> unparsedDates) throws ParseException {
-        List<String> parsedDates = ParserUtil.parseDates(unparsedDates).stream()
-                .map(date -> date.fullDate)
-                .collect(Collectors.toList());
-        return parsedDates;
-    }
-
-    private List<String> parseTags(List<String> unparsedTags) throws ParseException {
-        List<String> parsedTags = ParserUtil.parseTags(unparsedTags).stream()
-                .map(tag -> tag.tagName)
-                .collect(Collectors.toList());
-        return parsedTags;
-    }
-
 }
