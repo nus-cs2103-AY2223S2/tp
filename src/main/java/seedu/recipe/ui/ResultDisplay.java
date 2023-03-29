1d903719a70a1a21bf0ff640cbd07e516c8eeab6
SeeuSim <seeusimong@gmail.com>
2023-03-29 20:29:47 +0800

    Add CQ fix for ResultDisplay

--- src/main/java/seedu/recipe/ui/ResultDisplay.java
+++ src/main/java/seedu/recipe/ui/ResultDisplay.java
@@ -7,19 +7,25 @@ import javafx.scene.control.TextArea;
 import javafx.scene.layout.Region;
 
 /**
- * A ui for the status bar that is displayed at the header of the application.
+ * Represents the UI component for the command results that is displayed within the header of the application.
  */
 public class ResultDisplay extends UiPart<Region> {
-
     private static final String FXML = "ResultDisplay.fxml";
 
     @FXML
     private TextArea resultDisplay;
 
+    /**
+     * Generates an instance of this UI Component.
+     */
     public ResultDisplay() {
         super(FXML);
     }
 
+    /**
+     * Sets the text value to be displayed back to the user.
+     * @param feedbackToUser The result of the command, as retrieved from the backend of the app.
+     */
     public void setFeedbackToUser(String feedbackToUser) {
         requireNonNull(feedbackToUser);
         resultDisplay.setText(feedbackToUser);
