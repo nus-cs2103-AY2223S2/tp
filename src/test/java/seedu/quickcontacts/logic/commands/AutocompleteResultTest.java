0363f220cf4f6a3884cee2a8808754c27e2a57df
jiawei <b33b33o11@gmail.com>
2023-03-30 13:07:03 +0800

    Update test case for `AutocompleteResult`

--- src/test/java/seedu/quickcontacts/logic/commands/AutocompleteResultTest.java
+++ src/test/java/seedu/quickcontacts/logic/commands/AutocompleteResultTest.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Optional;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import seedu.quickcontacts.logic.parser.Prefix;
@@ -15,13 +14,16 @@ public class AutocompleteResultTest {
     @Test
     public void getPrefix() {
         Prefix prefix = new Prefix("m/");
-        Assertions.assertEquals(new AutocompleteResult(prefix, false).getPrefix(), Optional.of(prefix));
-        assertEquals(new AutocompleteResult(null, false).getPrefix(), Optional.empty());
+        assertEquals(new AutocompleteResult(prefix, false).getResult(), Optional.of(prefix.getPrefix()));
+        assertEquals(new AutocompleteResult().getResult(), Optional.empty());
     }
 
     @Test
     public void isReplaceCurrent() {
-        assertTrue(new AutocompleteResult(null, true).isReplaceCurrent());
-        assertFalse(new AutocompleteResult(new Prefix("m/"), false).isReplaceCurrent());
+        assertFalse(new AutocompleteResult().isReplaceCurrent());
+
+        Prefix prefix = new Prefix("m/");
+        assertFalse(new AutocompleteResult(prefix, false).isReplaceCurrent());
+        assertTrue(new AutocompleteResult(prefix, true).isReplaceCurrent());
     }
 }
