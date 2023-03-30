69f9d0eac4e2cd6cebb80e64bd4fac5f7ab39b1d
jerrrren <cheejeren@yahoo.com.sg>
2023-03-30 21:44:58 +0800

    Modify Employee toString()

--- src/main/java/seedu/sudohr/model/employee/Employee.java
+++ src/main/java/seedu/sudohr/model/employee/Employee.java
@@ -154,20 +154,9 @@ public class Employee {
     public String toString() {
         final StringBuilder builder = new StringBuilder();
         builder.append(getName())
-                .append("; Employee ID: ")
+                .append(" (Employee ID: ")
                 .append(getId())
-                .append("; Phone: ")
-                .append(getPhone())
-                .append("; Email: ")
-                .append(getEmail())
-                .append("; Address: ")
-                .append(getAddress());
-
-        Set<Tag> tags = getTags();
-        if (!tags.isEmpty()) {
-            builder.append("; Tags: ");
-            tags.forEach(builder::append);
-        }
+                .append(")");
         return builder.toString();
     }
 }
