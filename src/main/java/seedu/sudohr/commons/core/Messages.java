489f1243e9fc4b07eea35d7242dfb4c235dc0d5a
4ndrelim <andre_lin@u.nus.edu>
2023-03-31 02:09:12 +0800

    Implement find employee by id command

--- src/main/java/seedu/sudohr/commons/core/Messages.java
+++ src/main/java/seedu/sudohr/commons/core/Messages.java
@@ -10,13 +10,14 @@ public class Messages {
 
     public static final String MESSAGE_INVALID_DATE_FORMAT = "Dates should only be of the form YYYY-MM-DD";
 
-    public static final String MESSAGE_EMPLOYEE_TO_DELETE_NOT_FOUND = "Employee to delete from SudoHR is not found.";
-    public static final String MESSAGE_EMPLOYEE_TO_EDIT_NOT_FOUND = "Employee to edit is not found in SudoHR.";
+    public static final String MESSAGE_EMPLOYEE_FOUND = "Employee found!";
+    public static final String MESSAGE_EMPLOYEE_TO_DELETE_NOT_FOUND = "Employee to delete from SudoHR is not found!";
+    public static final String MESSAGE_EMPLOYEE_TO_EDIT_NOT_FOUND = "Employee to edit is not found in SudoHR!";
     public static final String MESSAGE_EMPLOYEES_LISTED_OVERVIEW = "%1$d employees listed!";
     public static final String MESSAGE_DEPARTMENTS_LISTED_OVERVIEW = "%1$d departments listed!";
 
     public static final String MESSAGE_DEPARTMENT_LISTED_OVERVIEW = "%1$d departments listed!";
 
-    public static final String MESSAGE_DEPARTMENT_NOT_FOUND = "The department to edit does not exist in SudoHR.";
-    public static final String MESSAGE_EMPLOYEE_NOT_FOUND = "The given employee does not exist in SudoHR.";
+    public static final String MESSAGE_DEPARTMENT_NOT_FOUND = "The department to edit does not exist in SudoHR!";
+    public static final String MESSAGE_EMPLOYEE_NOT_FOUND = "The employee does not exist in SudoHR!";
 }
