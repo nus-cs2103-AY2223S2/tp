9dea288d6f82933fbd19a2e1c166d899537dbbe2
Saraan Kannusami <saraankannusami@gmail.com>
2023-03-30 20:31:58 +0800

    Update MESSAGE_USAGE in AddCommand and AddMeetingCommand

--- src/main/java/seedu/quickcontacts/logic/commands/AddCommand.java
+++ src/main/java/seedu/quickcontacts/logic/commands/AddCommand.java
@@ -18,10 +18,9 @@ public class AddCommand extends Command {
 
     public static final String COMMAND_WORD = "add";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a person to the address book. "
-            + "Parameters: "
-            + PREFIX_NAME + "NAME "
-            + PREFIX_PHONE + "PHONE "
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a person to the address book. \n"
+            + "Required Parameters: " + PREFIX_NAME + "NAME " + PREFIX_PHONE + "PHONE \n"
+            + "Optional Parameters: "
             + PREFIX_EMAIL + "EMAIL "
             + PREFIX_ADDRESS + "ADDRESS "
             + "[" + PREFIX_TAG + "TAG]...\n"
