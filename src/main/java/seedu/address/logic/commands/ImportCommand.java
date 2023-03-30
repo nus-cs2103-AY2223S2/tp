0bbca43f746687555efb7ce2a7ddbce47b8781d1
Lennoxtr <huykhang.tranp@gmail.com>
2023-03-31 05:09:29 +0800

    Fixed checkstyle errors

--- src/main/java/seedu/address/logic/commands/ImportCommand.java
+++ src/main/java/seedu/address/logic/commands/ImportCommand.java
@@ -1,9 +1,7 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-
 import static seedu.address.logic.parser.CliSyntax.PREFIX_MODULE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
