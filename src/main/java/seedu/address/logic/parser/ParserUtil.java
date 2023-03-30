e54499b39823c8131dbff865da7879ed1919e21b
Brian Q <astraxquek@gmail.com>
2023-03-31 02:46:59 +0800

    :bug: fix: future date handling for score
    - add color variables for scorelist ui

--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -219,8 +219,14 @@ public class ParserUtil {
         if (!Date.isValidDate(trimmedDate)) {
             throw new ParseException(Date.MESSAGE_CONSTRAINTS);
         }
+
+        if (!Date.isFutureDate(trimmedDate)) {
+            throw new ParseException(Date.MESSAGE_INVALID_DATE);
+        }
         return new Date(trimmedDate);
     }
+
+
 
     /**
      * Parses a file path.
