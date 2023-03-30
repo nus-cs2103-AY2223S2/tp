a140ca95be85c49c0bd5c50dfbe41034c63b7fd8
Gibson <gibsonyip@u.nus.edu>
2023-03-30 12:45:16 +0800

    Update AddressBookParser.java

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -84,8 +84,10 @@ public class AddressBookParser {
 
         case RedoCommand.COMMAND_WORD:
             return new RedoCommand();
+
         case ExportCommand.COMMAND_WORD:
             return new ExportCommandParser().parse(arguments);
+
         default:
             throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
         }
