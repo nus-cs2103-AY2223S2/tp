50557b8861465c0bbab860a00b05652a24bb4185
Lee Jia Sheng <thegreyjs@gmail.com>
2023-03-31 01:41:17 +0800

    Remove unused import

--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -5,7 +5,6 @@ import static java.util.Objects.requireNonNull;
 import java.time.LocalDate;
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.Locale;
 import java.util.Set;
 
 import seedu.address.commons.core.index.Index;
