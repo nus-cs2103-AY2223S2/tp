4cc3b30e011228aad1628d71f568d87fe1a8b60f
LAPTOP-UOT6M2SH\manas <manasijsaha9616@gmail.com>
2023-03-30 18:11:45 +0800

    checkstyle fixes

--- src/main/java/seedu/connectus/model/tag/Tag.java
+++ src/main/java/seedu/connectus/model/tag/Tag.java
@@ -7,7 +7,7 @@ import static seedu.connectus.commons.util.AppUtil.checkArgument;
  * Represents a Tag in the ConnectUS.
  * Guarantees: immutable; name is valid as declared in {@link #isValidTagName(String)}
  */
-public class  Tag implements Comparable<Tag> {
+public class Tag implements Comparable<Tag> {
 
     public static final String MESSAGE_CONSTRAINTS = "Tags names should be alphanumeric";
     public static final String VALIDATION_REGEX = "[\\p{Alnum} ]+#?[\\p{Alnum}\\s]+$";
