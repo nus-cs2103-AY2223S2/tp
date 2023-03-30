1c2e119ac13a69fcab4e3578f1129756a23e94d1
sembcorpp <e0772507@u.nus.edu>
2023-03-31 03:26:23 +0800

    hi

--- src/main/java/seedu/address/logic/commands/FindMeeting.java
+++ src/main/java/seedu/address/logic/commands/FindMeeting.java
@@ -5,36 +5,42 @@ import static java.util.Objects.requireNonNull;
 import seedu.address.commons.core.Messages;
 import seedu.address.model.Model;
 import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.model.person.NameContainsKeywordsPredicate;
-import seedu.address.model.person.Person;
-import seedu.address.model.person.UniquePersonList;
+import seedu.address.model.person.*;
+
+
+import java.time.LocalDateTime;
+
+import java.util.function.Predicate;
+
 
 public class FindMeeting extends Command{
     public static final String COMMAND_WORD = "meeting find";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all meetings with "
-        + "the specified date (case-insensitive) or person index (optional) and displays them as a list with index numbers.\n"
-        + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
-        + "Example: " + COMMAND_WORD + " 12/02/2023";
+            + "the specified date (case-insensitive) or person index (optional) and displays them as a list with index numbers.\n"
+            + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
+            + "Example: " + COMMAND_WORD + " 12/02/2023";
 
-    private final NameContainsKeywordsPredicate predicate;
+    private final LocalDateTime meetingStart;
 
-    public FindMeeting(NameContainsKeywordsPredicate predicate) {
-        this.predicate = predicate;
+    public FindMeeting(LocalDateTime meetingStart) {
+        requireNonNull(meetingStart);
+        this.meetingStart = meetingStart;
     }
 
     public CommandResult execute(Model model) throws CommandException {
+
         requireNonNull(model);
-        model.updateFilteredPersonList(predicate);
+        model.updateFilteredMeetingList((Predicate<MeetingWithPerson>) new MeetingStartDatePredicate(meetingStart));
         return new CommandResult(
-            String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()));
+                String.format(Messages.MESSAGE_MEETINGS_LISTED_OVERVIEW, model.getFilteredMeetingList().size()));
     }
+
 
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
-            || (other instanceof FindMeeting // instanceof handles nulls
-            && predicate.equals(((FindMeeting) other).predicate)); // state check
+                || (other instanceof FindMeeting // instanceof handles nulls
+                && meetingStart.equals(((FindMeeting) other).meetingStart)); // state check
     }
 }
-
