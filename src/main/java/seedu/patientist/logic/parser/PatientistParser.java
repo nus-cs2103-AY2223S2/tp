e69dcb1f37fb60e62b4e0d6a1b2b4172ef9036d8
LIN_CHIEH <linchieh@stu.comp.nus.edu.sg>
2023-03-30 18:36:57 +0800

    Implement add patient todo in UI

--- src/main/java/seedu/patientist/logic/parser/PatientistParser.java
+++ src/main/java/seedu/patientist/logic/parser/PatientistParser.java
@@ -6,29 +6,7 @@ import static seedu.patientist.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import seedu.patientist.logic.commands.AddPatientCommand;
-import seedu.patientist.logic.commands.AddPatientStatusCommand;
-import seedu.patientist.logic.commands.AddStaffCommand;
-import seedu.patientist.logic.commands.AddWardCommand;
-import seedu.patientist.logic.commands.ClearCommand;
-import seedu.patientist.logic.commands.Command;
-import seedu.patientist.logic.commands.DeleteCommand;
-import seedu.patientist.logic.commands.DeletePatientCommand;
-import seedu.patientist.logic.commands.DeletePatientStatusCommand;
-import seedu.patientist.logic.commands.DeleteStaffCommand;
-import seedu.patientist.logic.commands.DeleteWardCommand;
-import seedu.patientist.logic.commands.EditCommand;
-import seedu.patientist.logic.commands.ExitCommand;
-import seedu.patientist.logic.commands.FindCommand;
-import seedu.patientist.logic.commands.FindPatientCommand;
-import seedu.patientist.logic.commands.FindStaffCommand;
-import seedu.patientist.logic.commands.HelpCommand;
-import seedu.patientist.logic.commands.ListCommand;
-import seedu.patientist.logic.commands.ListPatientsCommand;
-import seedu.patientist.logic.commands.ListStaffCommand;
-import seedu.patientist.logic.commands.ListWardPatientsCommand;
-import seedu.patientist.logic.commands.ListWardsCommand;
-import seedu.patientist.logic.commands.ViewCommand;
+import seedu.patientist.logic.commands.*;
 import seedu.patientist.logic.parser.exceptions.ParseException;
 
 /**
@@ -111,6 +89,9 @@ public class PatientistParser {
 
         case AddPatientStatusCommand.COMMAND_WORD:
             return new AddPatientStatusCommandParser().parse(arguments);
+
+        case AddPatientToDoCommand.COMMAND_WORD:
+            return new AddPatientToDoCommandParser().parse(arguments);
 
         case DeletePatientStatusCommand.COMMAND_WORD:
             return new DeletePatientStatusCommandParser().parse(arguments);
