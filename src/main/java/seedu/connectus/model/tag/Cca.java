e00b04afb18ee2a3be805af41b52d613b05e5784
LAPTOP-UOT6M2SH\manas <manasijsaha9616@gmail.com>
2023-03-30 18:06:19 +0800

    checkstyle fixes

--- src/main/java/seedu/connectus/model/tag/Cca.java
+++ src/main/java/seedu/connectus/model/tag/Cca.java
@@ -6,7 +6,8 @@ package seedu.connectus.model.tag;
  */
 public class Cca extends Tag {
 
-    public static final String MESSAGE_CONSTRAINTS = "CCA names and CCA Positions should be alphanumeric and may have spaces";
+    public static final String MESSAGE_CONSTRAINTS = "CCA names and CCA Positions should be " +
+            "alphanumeric and may have spaces";
     public final String ccaName;
     public final String ccaPositionName;
     public final String coupledCcaName;
@@ -30,9 +31,12 @@ public class Cca extends Tag {
         decoupledCcaName = decoupled;
     }
 
+    /**
+     * Decouples command into desired tag name.
+     */
     public void decoupleCcaName() {
         String[] arr = coupledCcaName.split("#");
-        if(arr.length == 2) {
+        if (arr.length == 2) {
             cca = arr[0];
             position = arr[1];
             decoupled = cca + "-" + position;
