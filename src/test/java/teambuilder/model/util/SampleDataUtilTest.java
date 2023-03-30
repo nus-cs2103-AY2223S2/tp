a4d7d0f46399f881a1918385bcdec260ef20bb62
chen-jerry-junior <jrchenjiarui@163.com>
2023-03-31 06:19:55 +0800

    update codecov patch

--- src/test/java/teambuilder/model/util/SampleDataUtilTest.java
+++ src/test/java/teambuilder/model/util/SampleDataUtilTest.java
@@ -2,11 +2,19 @@ package teambuilder.model.util;
 
 import org.junit.jupiter.api.Test;
 
+import teambuilder.model.person.Name;
 import teambuilder.model.person.Person;
+
+import java.util.Set;
 
 public class SampleDataUtilTest {
     @Test
     public void samplePersons_samplePersons_isValid() {
         Person[] persons = SampleDataUtil.getSamplePersons();
+    }
+
+    @Test
+    public void sampleNames_samplePersons_isValid() {
+        Set<Name> persons = SampleDataUtil.getNameSet();
     }
 }
