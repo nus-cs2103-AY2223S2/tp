1b297047351a38652af4286fd44ebaad44f86d5a
anthonynmh <anthonyneomh@gmail.com>
2023-03-31 06:33:10 +0800

    Fix bug where exceptions not properly catched

--- src/main/java/ezschedule/logic/parser/RecurCommandParser.java
+++ src/main/java/ezschedule/logic/parser/RecurCommandParser.java
@@ -2,8 +2,11 @@ package ezschedule.logic.parser;
 
 import static java.util.Objects.requireNonNull;
 
+import java.util.stream.Stream;
+
 import ezschedule.commons.core.Messages;
 import ezschedule.commons.core.index.Index;
+import ezschedule.logic.commands.AddCommand;
 import ezschedule.logic.commands.RecurCommand;
 import ezschedule.logic.parser.exceptions.ParseException;
 import ezschedule.model.event.Date;
@@ -13,6 +16,14 @@ import ezschedule.model.event.RecurFactor;
  * Parses input arguments and creates a new RecurCommand object.
  */
 public class RecurCommandParser implements Parser<RecurCommand> {
+
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values
+     * in the given {@code ArgumentMultimap}.
+     */
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
+    }
 
     /**
      * Parses the given {@code String} of arguments in the context of the RecurCommand
@@ -26,17 +37,17 @@ public class RecurCommandParser implements Parser<RecurCommand> {
                 ArgumentTokenizer.tokenize(args, CliSyntax.PREFIX_DATE, CliSyntax.PREFIX_EVERY);
 
         Index index;
+        Date endDate;
+        RecurFactor recurFactor;
 
-        try {
-            index = ParserUtil.parseIndex(argMultimap.getPreamble());
-        } catch (ParseException pe) {
-            throw new ParseException(
-                    String.format(Messages.MESSAGE_INVALID_COMMAND_FORMAT, RecurCommand.MESSAGE_USAGE),
-                    pe);
+        if (!arePrefixesPresent(argMultimap, CliSyntax.PREFIX_DATE, CliSyntax.PREFIX_EVERY)) {
+            throw new ParseException(String.format(Messages.MESSAGE_INVALID_COMMAND_FORMAT,
+                    RecurCommand.MESSAGE_USAGE));
         }
 
-        Date endDate = ParserUtil.parseDate(argMultimap.getValue(CliSyntax.PREFIX_DATE).get());
-        RecurFactor recurFactor = ParserUtil.parseRecurFactor(argMultimap.getValue(CliSyntax.PREFIX_EVERY).get());
+        index = ParserUtil.parseIndex(argMultimap.getPreamble());
+        endDate = ParserUtil.parseDate(argMultimap.getValue(CliSyntax.PREFIX_DATE).get());
+        recurFactor = ParserUtil.parseRecurFactor(argMultimap.getValue(CliSyntax.PREFIX_EVERY).get());
 
         return new RecurCommand(index, endDate, recurFactor);
     }
