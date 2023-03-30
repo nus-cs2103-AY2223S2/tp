12a4dc3cb3c189bcbe8dbf295394bad526485207
SeeuSim <seeusimong@gmail.com>
2023-03-30 21:12:58 +0800

    Add adaptor for IngredientBuilder, hook up to form

--- src/main/java/seedu/recipe/model/recipe/IngredientBuilder.java
+++ src/main/java/seedu/recipe/model/recipe/IngredientBuilder.java
@@ -2,13 +2,21 @@ package seedu.recipe.model.recipe;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.recipe.commons.util.AppUtil.checkArgument;
+import static seedu.recipe.model.recipe.ingredient.IngredientParser.AMOUNT_PREFIX;
+import static seedu.recipe.model.recipe.ingredient.IngredientParser.COMMON_NAME_PREFIX;
+import static seedu.recipe.model.recipe.ingredient.IngredientParser.ESTIMATE_PREFIX;
 import static seedu.recipe.model.recipe.ingredient.IngredientParser.NAME_PREFIX;
+import static seedu.recipe.model.recipe.ingredient.IngredientParser.REMARK_PREFIX;
+import static seedu.recipe.model.recipe.ingredient.IngredientParser.SUBSTITUTION_PREFIX;
 import static seedu.recipe.model.recipe.ingredient.IngredientParser.parse;
 
 import java.util.HashMap;
 import java.util.List;
 
 import seedu.recipe.logic.parser.Prefix;
+import seedu.recipe.model.recipe.ingredient.Ingredient;
+import seedu.recipe.model.recipe.ingredient.IngredientInformation;
+import seedu.recipe.model.recipe.ingredient.IngredientParser;
 
 /**
  * Represents a recipe's ingredient in the recipe book.
@@ -38,6 +46,56 @@ public class IngredientBuilder {
         checkArgument(tokens.containsKey(NAME_PREFIX), MESSAGE_CONSTRAINTS);
         this.name = name;
         this.arguments = tokens;
+    }
+
+    /**
+     * Constructs an {@code IngredientBuilder} instance, from a Recipe instance's Ingredient Key-Value pair.
+     * This is used for UI conversion for the Edit form functionality.
+     * @param mainIngredient The Recipe instance's valid mainIngredient key.
+     * @param information The Recipe instance's valid IngredientInformation value assigned to the Ingredient key.
+     */
+    public IngredientBuilder(Ingredient mainIngredient, IngredientInformation information) {
+        StringBuilder commandString = new StringBuilder();
+
+        //Quantity
+        information.getQuantity()
+                .map(v -> AMOUNT_PREFIX.getPrefix() + " " + v + " ")
+                .ifPresent(commandString::append);
+        //Estimated Amount
+        information.getEstimatedQuantity()
+                .map(v -> ESTIMATE_PREFIX.getPrefix() + " " + v + " ")
+                .ifPresent(commandString::append);
+        //Name
+        commandString.append(NAME_PREFIX)
+                .append(" ")
+                .append(mainIngredient.getName())
+                .append(" ");
+        //Common Name
+        if (!mainIngredient.getCommonName().isEmpty()) {
+            commandString.append(COMMON_NAME_PREFIX.getPrefix())
+                    .append(" ")
+                    .append(mainIngredient.getCommonName())
+                    .append(" ");
+        }
+        //Remarks
+        information.getRemarks().forEach(remark ->
+                commandString.append(REMARK_PREFIX.getPrefix())
+                        .append(" ")
+                        .append(remark)
+                        .append(" ")
+        );
+        //Substitutions
+        information.getSubstitutions().forEach(ingredient ->
+                commandString.append(SUBSTITUTION_PREFIX.getPrefix())
+                        .append(" ")
+                        .append(ingredient.getName())
+                        .append(" ")
+        );
+
+        HashMap<Prefix, List<String>> tokens = IngredientParser.parse(commandString.toString());
+        checkArgument(tokens.containsKey(NAME_PREFIX), MESSAGE_CONSTRAINTS);
+        this.arguments = tokens;
+        this.name = commandString.toString();
     }
 
     /**
