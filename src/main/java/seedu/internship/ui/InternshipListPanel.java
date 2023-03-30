1144ef8a487dc7d9b74859ee04fac5cdd97e46bc
potty10 <christophertan8664@gmail.com>
2023-03-30 22:42:42 +0800

    Update right panel after clicking

--- src/main/java/seedu/internship/ui/InternshipListPanel.java
+++ src/main/java/seedu/internship/ui/InternshipListPanel.java
@@ -2,12 +2,15 @@ package seedu.internship.ui;
 
 import java.util.logging.Logger;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.Region;
 import seedu.internship.commons.core.LogsCenter;
+import seedu.internship.model.Model;
 import seedu.internship.model.internship.Internship;
 
 /**
@@ -17,16 +20,26 @@ public class InternshipListPanel extends UiPart<Region> {
     private static final String FXML = "InternshipListPanel.fxml";
     private final Logger logger = LogsCenter.getLogger(InternshipListPanel.class);
 
+    private Model model;
+
+    private MainWindow mainWindow;
     @FXML
     private ListView<Internship> internshipListView;
 
     /**
      * Creates a {@code InternshipListPanel} with the given {@code ObservableList}.
      */
-    public InternshipListPanel(ObservableList<Internship> internshipList) {
+    public InternshipListPanel(ObservableList<Internship> internshipList, Model selectedModel,
+                               MainWindow selectedMainWindow) {
         super(FXML);
         internshipListView.setItems(internshipList);
         internshipListView.setCellFactory(listView -> new InternshipListViewCell());
+        model = selectedModel;
+        mainWindow = selectedMainWindow;
+        internshipListView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            model.updateSelectedInternship(newValue);
+            mainWindow.updateRightPanel();
+        });
     }
 
     /**
