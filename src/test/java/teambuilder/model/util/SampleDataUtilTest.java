23dd487d5b91199470b718f2f8dae4fd793cfa50
chen-jerry-junior <jrchenjiarui@163.com>
2023-03-31 06:26:32 +0800

    update checkstyle

--- src/test/java/teambuilder/model/util/SampleDataUtilTest.java
+++ src/test/java/teambuilder/model/util/SampleDataUtilTest.java
@@ -1,11 +1,11 @@
 package teambuilder.model.util;
 
+import java.util.Set;
+
 import org.junit.jupiter.api.Test;
 
 import teambuilder.model.person.Name;
 import teambuilder.model.person.Person;
-
-import java.util.Set;
 
 public class SampleDataUtilTest {
     @Test
