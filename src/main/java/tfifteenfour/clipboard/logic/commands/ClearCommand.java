e98e36e85e52dcfa8d25f9c3b7bd76549a541c58
tyx021 <yxting1109@gmail.com>
2023-03-30 11:27:34 +0800

    Clears different entities based on current page

--- src/main/java/tfifteenfour/clipboard/logic/commands/ClearCommand.java
+++ src/main/java/tfifteenfour/clipboard/logic/commands/ClearCommand.java
@@ -2,9 +2,18 @@ package tfifteenfour.clipboard.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import java.util.List;
+
 import tfifteenfour.clipboard.logic.CurrentSelection;
+import tfifteenfour.clipboard.logic.PageType;
+import tfifteenfour.clipboard.logic.commands.exceptions.CommandException;
 import tfifteenfour.clipboard.model.Model;
 import tfifteenfour.clipboard.model.Roster;
+import tfifteenfour.clipboard.model.course.Course;
+import tfifteenfour.clipboard.model.course.Group;
+import tfifteenfour.clipboard.model.course.Session;
+import tfifteenfour.clipboard.model.student.Student;
+import tfifteenfour.clipboard.model.task.Task;
 
 /**
  * Clears the address book.
@@ -19,9 +28,47 @@ public class ClearCommand extends Command {
     }
 
     @Override
-    public CommandResult execute(Model model, CurrentSelection currentSelection) {
+    public CommandResult execute(Model model, CurrentSelection currentSelection) throws CommandException {
         requireNonNull(model);
-        model.setRoster(new Roster());
+
+        PageType currentPage = currentSelection.getCurrentPage();
+        Course selectedCourse = currentSelection.getSelectedCourse();
+        Group selectedGroup = currentSelection.getSelectedGroup();
+
+
+        switch (currentPage) {
+        case COURSE_PAGE:
+            model.setRoster(new Roster());
+            break;
+        case GROUP_PAGE:
+            List<Group> groups = selectedCourse.getUnmodifiableFilteredGroupList();
+            for (Group group : groups) {
+                selectedCourse.deleteGroup(group);
+            }
+            break;
+        case SESSION_PAGE:
+            List<Session> sessions = selectedGroup.getUnmodifiableFilteredSessionList();
+            for (Session session : sessions) {
+                selectedGroup.deleteSession(session);
+            }
+            break;
+        case TASK_PAGE:
+            List<Task> tasks = selectedGroup.getUnmodifiableFilteredTaskList();
+            for (Task task : tasks) {
+                selectedGroup.deleteTask(task);
+            }
+            break;
+        case STUDENT_PAGE:
+            List<Student> students = selectedGroup.getUnmodifiableFilteredStudentList();
+            for (Student student : students) {
+                selectedGroup.deleteStudent(student);
+            }
+            break;
+        default:
+            throw new CommandException("Can't clear current page!\n"
+                    + "Navigate to the following pages to use clear command:"
+                    + "Course page, Group page, Session page, Task page, Student page");
+        }
         return new CommandResult(this, MESSAGE_SUCCESS, willModifyState);
     }
 }
