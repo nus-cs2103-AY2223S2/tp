fd19f3bf7c4a9fa06a93679c34c121eaab048dd8
Shi Jiaao <e0372500@u.nus.edu>
2023-03-31 00:36:11 +0800

    Update Help command to match new add/deleteStudent

--- src/main/java/seedu/address/logic/commands/HelpLabCommand.java
+++ src/main/java/seedu/address/logic/commands/HelpLabCommand.java
@@ -28,10 +28,10 @@ public class HelpLabCommand extends HelpCommand {
             + "editEvent EVENT_INDEX Lab/NEW_NAME -date NEW_DATE -file NEW_FILEPATH";
 
     public static final String ADD_STUDENT = "Add Student:            "
-            + "addStudent INDEX Lab/LAB_NAME";
+            + "addStudent STUDENT_INDEX_IN_LIST Lab/LAB_INDEX_IN_LAB_LIST";
 
     public static final String DELETE_STUDENT = "Delete Student:        "
-            + "deleteStudent STUDENT_INDEX_IN_LIST Lab/LAB_NAME";
+            + "deleteStudent STUDENT_INDEX_IN_LIST Lab/LAB_INDEX_IN_LAB_LIST";
 
     public static final String ADD_NOTE = "Add Note:         "
             + "add-note -content NOTE ?????";
