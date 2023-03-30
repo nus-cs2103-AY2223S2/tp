6faf9915b6a7d0040623e4f0c333f481f85624f9
Ng Jia Yi <92777033+gremmyz@users.noreply.github.com>
2023-03-31 02:08:47 +0800

    test fix

--- src/test/java/seedu/connectus/logic/commands/SearchCommandTest.java
+++ src/test/java/seedu/connectus/logic/commands/SearchCommandTest.java
@@ -76,7 +76,7 @@ public class SearchCommandTest {
     }
 
     /**
-     * Parses {@code userInput} into a {@code NameContainsKeywordsPredicate}.
+     * Parses {@code userInput} into a {@code FieldsContainKeywordsPredicate}.
      */
     private FieldsContainKeywordsPredicate preparePredicate(String userInput) {
         return new FieldsContainKeywordsPredicate(Arrays.asList(userInput.split("\\s+")));
