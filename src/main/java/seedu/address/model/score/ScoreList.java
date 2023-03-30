f9831a75bfdc4632403215ed9a7e6ea8c4228eb8
Brian Q <astraxquek@gmail.com>
2023-03-31 01:03:53 +0800

    :hammer: fix: ci/cd

--- src/main/java/seedu/address/model/score/ScoreList.java
+++ src/main/java/seedu/address/model/score/ScoreList.java
@@ -4,7 +4,6 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
 import java.text.DecimalFormat;
-import java.util.Comparator;
 import java.util.DoubleSummaryStatistics;
 import java.util.Iterator;
 import java.util.List;
