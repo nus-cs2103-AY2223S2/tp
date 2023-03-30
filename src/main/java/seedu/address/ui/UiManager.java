f020e03a01226fdc53cf86268cd5f0756f707d26
jayhee3 <jiayuanhee@gmail.com>
2023-03-30 19:01:50 +0800

    Change UI to light blue themed

--- src/main/java/seedu/address/ui/UiManager.java
+++ src/main/java/seedu/address/ui/UiManager.java
@@ -44,7 +44,7 @@ public class UiManager implements Ui {
             mainWindow = new MainWindow(primaryStage, logic);
             mainWindow.show(); //This should be called before creating other UI parts
             mainWindow.fillInnerParts();
-            showWorkToday(logic.getWorkToday());
+            //showWorkToday(logic.getWorkToday());
 
         } catch (Throwable e) {
             logger.severe(StringUtil.getDetails(e));
@@ -67,7 +67,7 @@ public class UiManager implements Ui {
     private static void showAlertDialogAndWait(Stage owner, AlertType type, String title, String headerText,
                                                String contentText) {
         final Alert alert = new Alert(type);
-        alert.getDialogPane().getStylesheets().add("view/DarkTheme.css");
+        alert.getDialogPane().getStylesheets().add("view/LightTheme.css");
         alert.initOwner(owner);
         alert.setTitle(title);
         alert.setHeaderText(headerText);
