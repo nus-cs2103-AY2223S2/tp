84bddae1aad6768e1a7b0de382b1bb4308746a11
nramapurath <nramapurath@gmail.com>
2023-03-31 03:54:49 +0800

    Update MainApp version number

--- src/main/java/seedu/medinfo/MainApp.java
+++ src/main/java/seedu/medinfo/MainApp.java
@@ -32,7 +32,7 @@ import seedu.medinfo.ui.UiManager;
  */
 public class MainApp extends Application {
 
-    public static final Version VERSION = new Version(0, 2, 0, true);
+    public static final Version VERSION = new Version(1, 3, 0, true);
 
     private static final Logger logger = LogsCenter.getLogger(MainApp.class);
 
