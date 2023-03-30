83689e5416f0cfb938248ea220e0fc3a72872635
Lyndon Lim <hnghng127@gmail.com>
2023-03-30 20:20:05 +0800

    Fix UI bugs
    - Accommodate for lengthy names, groups, tags, email and address

--- src/main/java/seedu/address/model/event/RecurringEventList.java
+++ src/main/java/seedu/address/model/event/RecurringEventList.java
@@ -198,7 +198,7 @@ public class RecurringEventList {
 
     @Override
     public String toString() {
-        StringBuilder output = new StringBuilder("");
+        StringBuilder output = new StringBuilder("Recurring Events\n");
         int count = 1;
         for (RecurringEvent re : recurringEvents) {
             output.append(count).append(". ").append(re.toString()).append("\n");
