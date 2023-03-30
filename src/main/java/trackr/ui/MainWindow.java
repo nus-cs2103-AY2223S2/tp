d260817290c6574697e8d1dddd70fabe27daeeef
Liu Muchen <liumuchen1@gmail.com>
2023-03-30 10:52:39 +0800

    Fix Checkstyle

--- src/main/java/trackr/ui/MainWindow.java
+++ src/main/java/trackr/ui/MainWindow.java
@@ -12,7 +12,6 @@ import javafx.scene.control.TextInputControl;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import trackr.commons.core.GuiSettings;
