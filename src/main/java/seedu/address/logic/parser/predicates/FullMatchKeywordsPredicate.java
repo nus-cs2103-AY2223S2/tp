0f933760c404c5046941f1e2fa2ef5ba87b69fb8
Benny <benny.ong348@gmail.com>
2023-03-31 03:05:08 +0800

    Removed old lines of code

--- src/main/java/seedu/address/logic/parser/predicates/FullMatchKeywordsPredicate.java
+++ src/main/java/seedu/address/logic/parser/predicates/FullMatchKeywordsPredicate.java
@@ -99,7 +99,6 @@ public class FullMatchKeywordsPredicate implements Predicate<Person> {
             List<String> personModuleNames = personModules.stream()
                     .map(module -> module.moduleName)
                     .collect(Collectors.toList());
-            hasMatching = hasMatching || values.stream().anyMatch(subject -> personSubjectNames.contains(subject));
             hasMatching = hasMatching && values.stream().anyMatch(subject -> personModuleNames.contains(subject));
         }
         //@@author
