97bb9c2063d4514ad62ec622e4288e697b8f1671
Brian Q <astraxquek@gmail.com>
2023-03-31 01:57:18 +0800

    :bug: fix: scorelist percentage
    - gives a nan result if the old value.

--- src/main/java/seedu/address/model/score/ScoreList.java
+++ src/main/java/seedu/address/model/score/ScoreList.java
@@ -149,8 +149,10 @@ public class ScoreList implements Iterable<Score> {
         Double average = scoreSummary.getAverage();
         Double maxValue = scoreSummary.getMax();
         Double minValue = scoreSummary.getMin();
-        Double percentage = (recentScoreList.get(recentScoreList.size() - 1).getValue().value
-                - recentScoreList.get(0).getValue().value) / recentScoreList.get(0).getValue().value * 100;
+        Double oldValue = recentScoreList.get(0).getValue().value;
+        Double newValue = recentScoreList.get(recentScoreList.size() - 1).getValue().value;
+        Double percentage = newValue - oldValue == 0 ? 0 : oldValue == 0 ? (newValue - oldValue) / 1   : (newValue - oldValue) / oldValue * 100; 
+        System.out.println("new value " + newValue.toString() + " old value " + oldValue.toString() + " percentage " + percentage.toString());
         average = Double.valueOf(df.format(average));
         percentage = Double.valueOf(df.format(percentage));
         ScoreSummary ss = new ScoreSummary(maxValue, minValue, average, percentage);
