64a7a145990b2fab8ed21c0268251e8c7e529db6
Darren Sim <e0725166@u.nus.edu>
2023-03-30 20:50:09 +0800

    Refactor edit command parser tests

--- src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
@@ -1,35 +1,35 @@
-//package seedu.address.logic.parser;
-//
-//import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
-//import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
-//import static seedu.ultron.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-//
-//import java.util.Arrays;
-//
-//import org.junit.jupiter.api.Test;
-//
-//import seedu.ultron.logic.commands.FindCommand;
-//import seedu.ultron.logic.parser.FindCommandParser;
-//import seedu.ultron.model.person.NameContainsKeywordsPredicate;
-//
-//public class FindCommandParserTest {
-//
-//    private FindCommandParser parser = new FindCommandParser();
-//
-//    @Test
-//    public void parse_emptyArg_throwsParseException() {
-//        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
-//    }
-//
-//    @Test
-//    public void parse_validArgs_returnsFindCommand() {
-//        // no leading and trailing whitespaces
-//        FindCommand expectedFindCommand =
-//                new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList("Alice", "Bob")));
-//        assertParseSuccess(parser, "Alice Bob", expectedFindCommand);
-//
-//        // multiple whitespaces between keywords
-//        assertParseSuccess(parser, " \n Alice \n \t Bob  \t", expectedFindCommand);
-//    }
-//
-//}
+package seedu.address.logic.parser;
+
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.ultron.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.ultron.logic.commands.FindCommand;
+import seedu.ultron.logic.parser.FindCommandParser;
+import seedu.ultron.model.opening.CompanyOrPositionContainsKeywordsPredicate;
+
+public class FindCommandParserTest {
+
+    private FindCommandParser parser = new FindCommandParser();
+
+    @Test
+    public void parse_emptyArg_throwsParseException() {
+        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_validArgs_returnsFindCommand() {
+        // no leading and trailing whitespaces
+        FindCommand expectedFindCommand =
+                new FindCommand(new CompanyOrPositionContainsKeywordsPredicate(Arrays.asList("Google", "Shopee")));
+        assertParseSuccess(parser, "Google Shopee", expectedFindCommand);
+
+        // multiple whitespaces between keywords
+        assertParseSuccess(parser, " \n Google \n \t Shopee  \t", expectedFindCommand);
+    }
+
+}
