c8ffc74b0afd6759be0e4bd28beb73e1bb2f077e
zhacatomn <zhacatomn@gmail.com>
2023-03-30 20:55:27 +0800

    Remove ClearCommand

--- src/test/java/seedu/address/logic/commands/CommandRecommendationEngineTest.java
+++ src/test/java/seedu/address/logic/commands/CommandRecommendationEngineTest.java
@@ -14,7 +14,6 @@ import seedu.address.logic.parser.AddPairCommandParser;
 import seedu.address.logic.parser.AddVolunteerCommandParser;
 import seedu.address.logic.parser.ArgumentMultimap;
 import seedu.address.logic.parser.ArgumentTokenizer;
-import seedu.address.logic.parser.ClearCommandParser;
 import seedu.address.logic.parser.DeleteElderlyCommandParser;
 import seedu.address.logic.parser.DeletePairCommandParser;
 import seedu.address.logic.parser.DeleteVolunteerCommandParser;
@@ -48,8 +47,8 @@ public class CommandRecommendationEngineTest {
             actual = commandRecommendationEngine.recommendCommand("ex");
             assertEquals(expected, actual);
 
-            expected = "clear";
-            actual = commandRecommendationEngine.recommendCommand("clea");
+            expected = "auto_pair";
+            actual = commandRecommendationEngine.recommendCommand("auto_p");
             assertEquals(expected, actual);
         } catch (CommandException e) {
             fail();
@@ -83,11 +82,6 @@ public class CommandRecommendationEngineTest {
                 .COMMAND_INFO_MAP.get(AddPairCommand.COMMAND_WORD).getCmdPrompts().keySet()
                 .toArray(new Prefix[]{}));
         assertFalse(AddPairCommandParser.validate(argumentMultimap));
-
-        argumentMultimap = ArgumentTokenizer.tokenize(userArgs, CommandRecommendationEngine
-                .COMMAND_INFO_MAP.get(ClearCommand.COMMAND_WORD).getCmdPrompts().keySet()
-                .toArray(new Prefix[]{}));
-        assertFalse(ClearCommandParser.validate(argumentMultimap));
 
         argumentMultimap = ArgumentTokenizer.tokenize(userArgs, CommandRecommendationEngine
                 .COMMAND_INFO_MAP.get(ExitCommand.COMMAND_WORD).getCmdPrompts().keySet()
@@ -156,10 +150,10 @@ public class CommandRecommendationEngineTest {
             String autocomplete = commandRecommendationEngine.autocompleteCommand(userInput, recommended);
             assertEquals("add_volunteer", autocomplete);
 
-            userInput = "clea";
+            userInput = "auto_p";
             recommended = commandRecommendationEngine.recommendCommand(userInput);
             autocomplete = commandRecommendationEngine.autocompleteCommand(userInput, recommended);
-            assertEquals("clear", autocomplete);
+            assertEquals("auto_pair", autocomplete);
 
             userInput = "add_elderly";
             recommended = commandRecommendationEngine.recommendCommand(userInput);
