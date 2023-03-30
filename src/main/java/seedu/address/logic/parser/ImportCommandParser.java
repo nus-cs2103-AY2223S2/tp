db833eece8a3212318d5982e09a0e570968e6e29
winsonheng <winsonh5147@gmail.com>
2023-03-30 10:32:10 +0800

    Allow optional keyword shown for export and all for import

--- src/main/java/seedu/address/logic/parser/ImportCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ImportCommandParser.java
@@ -10,9 +10,11 @@ import seedu.address.logic.parser.exceptions.ParseException;
  */
 public class ImportCommandParser implements Parser<ImportCommand> {
     private static final String RESET_KEYWORD = "reset";
+    private static final String COMBINE_KEYWORD = "combine";
 
     /**
      * Creates a ImportCommand where isResetEnabled depends on the parsed user input.
+     * If keyword is "combine" or no keyword is present, combine the imported dataset with the existing one.
      *
      * @param args The input from user.
      * @return ImportCommand with isResetEnabled set to true if the keyword "reset" is in the user input.
@@ -20,7 +22,7 @@ public class ImportCommandParser implements Parser<ImportCommand> {
      */
     public ImportCommand parse(String args) throws ParseException {
         String trimmedArgs = args.trim().toLowerCase();
-        if (trimmedArgs.isEmpty()) {
+        if (trimmedArgs.isEmpty() || trimmedArgs.equals(COMBINE_KEYWORD)) {
             return new ImportCommand(false);
         } else if (trimmedArgs.equals(RESET_KEYWORD)) {
             return new ImportCommand(true);
