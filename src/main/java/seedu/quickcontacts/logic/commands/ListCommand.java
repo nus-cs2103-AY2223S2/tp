aaa7bf19e11b4197132fb183d50d3b296508b130
Saraan Kannusami <saraankannusami@gmail.com>
2023-03-30 20:28:09 +0800

    Add MESSAGE_USAGE for use with HelpCommand

--- src/main/java/seedu/quickcontacts/logic/commands/ListCommand.java
+++ src/main/java/seedu/quickcontacts/logic/commands/ListCommand.java
@@ -13,6 +13,8 @@ public class ListCommand extends Command {
     public static final String COMMAND_WORD = "list";
 
     public static final String MESSAGE_SUCCESS = "Listed all persons";
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Lists all persons in the address book.\n" + "Example: " + COMMAND_WORD;
 
 
     @Override
