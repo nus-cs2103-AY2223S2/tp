00b7ec6222097d876c1a685e8c074773b1e27ffd
Hafeez <izzhafeez@gmail.com>
2023-03-31 06:19:19 +0800

    Fix unresponsive add, edit, tag command

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -105,6 +105,7 @@ public class EditCommand extends Command {
 
         model.setPerson(personToEdit, editedPerson);
         model.updateObservablePersonList();
+
         return new ViewCommandResult(String.format(MESSAGE_EDIT_PERSON_SUCCESS, editedPerson), editedPerson);
     }
 
