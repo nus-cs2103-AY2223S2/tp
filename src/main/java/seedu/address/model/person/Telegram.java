dc128ab17ac0f1df0cc97fba4cfa1e14fb49a546
Wen Li <79928462+wendy0107@users.noreply.github.com>
2023-03-31 13:37:54 +0800

    Update Telegram.java
    Cannot change regex

--- src/main/java/seedu/address/model/person/Telegram.java
+++ src/main/java/seedu/address/model/person/Telegram.java
@@ -16,7 +16,7 @@ public class Telegram {
      * The first character of the address must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
-    public static final String VALIDATION_REGEX = "@[a-zA-Z0-9_]{5,}";
+    public static final String VALIDATION_REGEX = "[^\\s].*";
 
     public final String value;
 
