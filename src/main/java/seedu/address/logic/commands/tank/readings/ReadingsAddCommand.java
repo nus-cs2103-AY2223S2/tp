470b94c1795ea84771d23b0552bc411aaab1e8be
bokuanT <bokuan2000@gmail.com>
2023-03-31 09:48:47 +0800

    Fix error messages

--- src/main/java/seedu/address/logic/commands/tank/readings/ReadingsAddCommand.java
+++ src/main/java/seedu/address/logic/commands/tank/readings/ReadingsAddCommand.java
@@ -2,7 +2,9 @@ package seedu.address.logic.commands.tank.readings;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_AMMONIA_LEVEL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PH;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TANK;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TEMPERATURE;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.CommandResult;
@@ -26,9 +28,13 @@ public class ReadingsAddCommand extends TankCommand {
             + "Parameters: "
             + PREFIX_TANK + "INDEX \n"
             + PREFIX_AMMONIA_LEVEL + "AMMONIA LEVEL \n"
+            + PREFIX_PH + "PH \n"
+            + PREFIX_TEMPERATURE + "TEMPERATURE \n"
             + "Example: " + COMMAND_WORD + " " + TANK_COMMAND_WORD + " "
             + PREFIX_TANK + "1 "
-            + PREFIX_AMMONIA_LEVEL + "1.2 ";
+            + PREFIX_AMMONIA_LEVEL + "1.2 "
+            + PREFIX_PH + "7 "
+            + PREFIX_TEMPERATURE + "28 ";
 
     public static final String MESSAGE_SUCCESS = "New set of readings added: %1$s, %2$s, %3$s";
     public static final String MESSAGE_MISSING_TANK = "The tank index specified does not exist";
