79086531b45e2d550e9530f1cc2e025436b65f67
SeeuSim <seeusimong@gmail.com>
2023-04-01 15:10:52 +0800

    Begin adding tests for Commons package

--- src/main/java/seedu/recipe/commons/core/Messages.java
+++ src/main/java/seedu/recipe/commons/core/Messages.java
@@ -4,7 +4,6 @@ package seedu.recipe.commons.core;
  * Container for user visible messages.
  */
 public class Messages {
-
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_EMPTY_KEYWORDS_FIND = "Keywords for the find command cannot be empty!";
