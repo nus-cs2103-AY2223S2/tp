1c1ef4aec882c80c48339dcc0d3d797839400c23
SeeuSim <seeusimong@gmail.com>
2023-04-01 17:10:13 +0800

    Add tests for commons.exceptions

---
+++ src/test/java/seedu/recipe/commons/exceptions/CommonsExceptionsTest.java
@@ -0,0 +1,32 @@
+package seedu.recipe.commons.exceptions;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class CommonsExceptionsTest {
+    @Test
+    public void testDataConversionException_constructor() {
+        //Null value
+        assertDoesNotThrow(() -> new DataConversionException(null));
+
+        //Construct with test message, test Message and Cause
+        String testMessage = "Test message";
+        Exception wrappedException = new Exception(testMessage);
+        DataConversionException e = new DataConversionException(wrappedException);
+        assertEquals("java.lang.Exception: " + testMessage, e.getMessage());
+        assertEquals(wrappedException, e.getCause());
+    }
+
+    @Test
+    public void testIllegalValueException_constructor() {
+        String testMessage = "Test message";
+        assertEquals(testMessage, new IllegalValueException(testMessage).getMessage());
+
+        Exception wrappedException = new Exception(testMessage);
+        assertEquals(testMessage, new IllegalValueException(testMessage, wrappedException).getMessage());
+        assertEquals(wrappedException, new IllegalValueException(testMessage, wrappedException).getCause());
+    }
+}
--- src/test/java/seedu/recipe/commons/util/FileUtilTest.java
+++ src/test/java/seedu/recipe/commons/util/FileUtilTest.java
@@ -4,9 +4,19 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.recipe.testutil.Assert.assertThrows;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.junit.jupiter.api.Test;
 
 public class FileUtilTest {
+    private static final Path TEST_DATA_FOLDER = Paths.get("src", "test", "data", "JsonSerializableRecipeBookTest");
+    private static final Path VALID_PATH = TEST_DATA_FOLDER.resolve("typicalRecipeRecipeBook.json");
+
+    @Test
+    public void isFileExists() {
+        assertTrue(FileUtil.isFileExists(VALID_PATH));
+    }
 
     @Test
     public void isValidPath() {
