8a9ee2c11f20be57c141ae760613583d2397d20a
Dylan Sng <dsja612@gmail.com>
2023-04-01 09:55:35 +0800

    Change logging information to better fit TechTrack

--- src/main/java/seedu/techtrack/logic/commands/ExitCommand.java
+++ src/main/java/seedu/techtrack/logic/commands/ExitCommand.java
@@ -9,7 +9,7 @@ public class ExitCommand extends Command {
 
     public static final String COMMAND_WORD = "exit";
 
-    public static final String MESSAGE_EXIT_ACKNOWLEDGEMENT = "Exiting Company Book as requested ...";
+    public static final String MESSAGE_EXIT_ACKNOWLEDGEMENT = "Exiting TechTrack as requested ...";
 
     @Override
     public CommandResult<String> execute(Model model) {
