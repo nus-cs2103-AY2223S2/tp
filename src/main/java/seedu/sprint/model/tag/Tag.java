d51286942aa876da3c3375fcfa1d35007d93f90a
XylusChen <chenxinyang00@gmail.com>
2023-04-01 18:28:42 +0800

    Update error message for tags

--- src/main/java/seedu/sprint/model/tag/Tag.java
+++ src/main/java/seedu/sprint/model/tag/Tag.java
@@ -10,7 +10,8 @@ import static seedu.sprint.commons.util.AppUtil.checkArgument;
 public class Tag {
 
     public static final String MESSAGE_CONSTRAINTS = "Tag names should be alphanumeric, "
-            + "and it should not be left blank.";
+            + "and it should not contain spaces.\n"
+            + "Tag names should not be left blank unless you intend to remove all existing Tags.";
     public static final String VALIDATION_REGEX = "\\p{Alnum}+";
 
     public final String tagName;
