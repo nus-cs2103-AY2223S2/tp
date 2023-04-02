e18977d58bd0c86d9702c3c33d15e1506af64264
swx0 <76123658+swx0@users.noreply.github.com>
2023-04-03 02:59:12 +0800

    Fix checkstyle

--- src/test/java/teambuilder/logic/parser/SortCommandParserTest.java
+++ src/test/java/teambuilder/logic/parser/SortCommandParserTest.java
@@ -1,15 +1,15 @@
 package teambuilder.logic.parser;
 
-import org.junit.jupiter.api.Test;
-
-import teambuilder.logic.commands.CreateCommand;
-import teambuilder.logic.commands.SortCommand;
-
-
 import static teambuilder.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static teambuilder.logic.commands.CommandTestUtil.*;
+import static teambuilder.logic.commands.CommandTestUtil.PREAMBLE_WHITESPACE;
+import static teambuilder.logic.commands.CommandTestUtil.VALID_ORDER;
+import static teambuilder.logic.commands.CommandTestUtil.VALID_SORT_BY;
 import static teambuilder.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static teambuilder.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+import org.junit.jupiter.api.Test;
+
+import teambuilder.logic.commands.SortCommand;
 
 public class SortCommandParserTest {
 
@@ -35,7 +35,7 @@ public class SortCommandParserTest {
         String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE);
 
         // missing order
-        assertParseFailure(parser,  VALID_SORT_BY, expectedMessage);
+        assertParseFailure(parser, VALID_SORT_BY, expectedMessage);
 
         // missing sort by
         assertParseFailure(parser, VALID_ORDER, expectedMessage);
