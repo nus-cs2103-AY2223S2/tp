7441c7dccc007a62660d34930c4f79bc279b93db
Ng Jia Yi <92777033+gremmyz@users.noreply.github.com>
2023-04-02 19:13:05 +0800

    add test cases for JsonAdaptedAddress

--- src/main/java/seedu/connectus/storage/JsonAdaptedAddress.java
+++ src/main/java/seedu/connectus/storage/JsonAdaptedAddress.java
@@ -61,5 +61,4 @@ class JsonAdaptedAddress {
             return Optional.empty();
         }
     }
-
 }
---
+++ src/test/java/seedu/connectus/storage/JsonAdaptedAddressTest.java
@@ -0,0 +1,31 @@
+package seedu.connectus.storage;
+
+import org.junit.jupiter.api.Test;
+import seedu.connectus.commons.exceptions.IllegalValueException;
+import seedu.connectus.model.person.Address;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class JsonAdaptedAddressTest {
+    @Test
+    public void testGetAddress() {
+        JsonAdaptedAddress address = new JsonAdaptedAddress("123 Main Street");
+        assertEquals("123 Main Street", address.getAddress());
+
+        JsonAdaptedAddress addressWithoutValue = new JsonAdaptedAddress();
+        assertNull(addressWithoutValue.getAddress());
+    }
+
+    @Test
+    public void toModelType_withValidAddress_returnsOptionalOfAddress() throws IllegalValueException {
+        String validAddressString = "Blk 123, Clementi Street 14, #12-04";
+        JsonAdaptedAddress jsonAdaptedAddress = new JsonAdaptedAddress(validAddressString);
+        Optional<Address> optionalAddress = jsonAdaptedAddress.toModelType();
+        assertTrue(optionalAddress.isPresent());
+        assertEquals(validAddressString, optionalAddress.get().getValue());
+    }
+}
