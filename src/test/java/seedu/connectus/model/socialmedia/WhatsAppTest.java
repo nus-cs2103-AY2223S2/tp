ad01148f921acbf5284d5dbbd14dbc5fa1ea57c2
Ng Jia Yi <92777033+gremmyz@users.noreply.github.com>
2023-04-02 18:50:08 +0800

    add whatsapp tests

--- src/test/java/seedu/connectus/model/socialmedia/WhatsAppTest.java
+++ src/test/java/seedu/connectus/model/socialmedia/WhatsAppTest.java
@@ -1,10 +1,13 @@
 package seedu.connectus.model.socialmedia;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.connectus.testutil.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+import seedu.connectus.model.person.Name;
+import seedu.connectus.model.person.Person;
 
 public class WhatsAppTest {
 
@@ -36,5 +39,66 @@ public class WhatsAppTest {
         assertTrue(WhatsApp.isValidWhatsApp("911")); // exactly 3 numbers
         assertTrue(WhatsApp.isValidWhatsApp("93121534"));
         assertTrue(WhatsApp.isValidWhatsApp("124293842033123")); // long phone numbers
+    }
+
+    @Test
+    public void getUserLinkSingapore_WhatsApp_returnsExpectedLink() {
+        String expectedLink = "whatsapp://send?phone=6591234567";
+        WhatsApp whatsapp = new WhatsApp("91234567");
+
+        String actualLink = whatsapp.getUserLink();
+
+        assertEquals(expectedLink, actualLink);
+    }
+
+    @Test
+    public void getUserLinkForeign_WhatsApp_returnsExpectedLink() {
+        String expectedLink = "whatsapp://send?phone=60123456789";
+        WhatsApp whatsapp = new WhatsApp("60123456789");
+
+        String actualLink = whatsapp.getUserLink();
+
+        assertEquals(expectedLink, actualLink);
+    }
+
+    @Test
+    public void getUserLinkSingapore_withUser_returnsExpectedLink() {
+        String expectedLink = "whatsapp://send?phone=6591234567";
+        Person person = new Person(new Name("John Doe"));
+        person.setSocialMedia(new SocialMedia(null, null, WhatsApp.of("91234567")));
+
+        String actualLink = WhatsApp.getUserLink(person);
+
+        assertEquals(expectedLink, actualLink);
+    }
+
+    @Test
+    public void getUserLinkForeign_withUser_returnsExpectedLink() {
+        String expectedLink = "whatsapp://send?phone=60123456789";
+        Person person = new Person(new Name("John Doe"));
+        person.setSocialMedia(new SocialMedia(null, null, WhatsApp.of("60123456789")));
+
+        String actualLink = WhatsApp.getUserLink(person);
+
+        assertEquals(expectedLink, actualLink);
+    }
+
+    @Test
+    public void getUserLink_withNullUser_returnsEmptyString() {
+        Person person = new Person(new Name("John Doe"));
+
+        String actualLink = WhatsApp.getUserLink(person);
+
+        assertEquals("", actualLink);
+    }
+
+    @Test
+    public void getUserLink_withNullSocialMedia_returnsEmptyString() {
+        Person person = new Person(new Name("John Doe"));
+        person.setSocialMedia(new SocialMedia(null, null, null));
+
+        String actualLink = WhatsApp.getUserLink(person);
+
+        assertEquals("", actualLink);
     }
 }
