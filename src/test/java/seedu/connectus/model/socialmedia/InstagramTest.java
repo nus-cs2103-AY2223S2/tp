b24a0c84224c5df32a6260b77ea824ce473a4ee8
Ng Jia Yi <92777033+gremmyz@users.noreply.github.com>
2023-04-02 18:58:28 +0800

    fix checkstyle

--- src/test/java/seedu/connectus/model/socialmedia/InstagramTest.java
+++ src/test/java/seedu/connectus/model/socialmedia/InstagramTest.java
@@ -1,6 +1,8 @@
 package seedu.connectus.model.socialmedia;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.connectus.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
