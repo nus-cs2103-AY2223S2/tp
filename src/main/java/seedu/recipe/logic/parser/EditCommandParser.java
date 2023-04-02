3ac2e70b6991087594285887d23bf3f19ce7f802
Filbert Phang <fillberth@gmail.com>
2023-04-02 23:14:38 +0800

    Fix trivial stuff

--- src/main/java/seedu/recipe/logic/parser/EditCommandParser.java
+++ src/main/java/seedu/recipe/logic/parser/EditCommandParser.java
@@ -37,7 +37,6 @@ public class EditCommandParser implements Parser<EditCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public EditCommand parse(String args) throws ParseException {
-        System.out.println("args: " + args);
         requireNonNull(args);
         ArgumentMultimap argMultimap =
             ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_DURATION, PREFIX_PORTION,
