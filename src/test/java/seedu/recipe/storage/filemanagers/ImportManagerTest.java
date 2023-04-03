b2f8d2f7c08a772762b4bb4d20c0d2e6ead1dbdd
SeeuSim <seeusimong@gmail.com>
2023-04-04 06:54:01 +0800

    Fix checkstyle

--- src/test/java/seedu/recipe/storage/filemanagers/ImportManagerTest.java
+++ src/test/java/seedu/recipe/storage/filemanagers/ImportManagerTest.java
@@ -2,7 +2,7 @@ package seedu.recipe.storage.filemanagers;
 
 import org.testfx.framework.junit5.ApplicationTest;
 
-import javafx.stage.Stage;
+//import javafx.stage.Stage;
 
 public class ImportManagerTest extends ApplicationTest {
 
