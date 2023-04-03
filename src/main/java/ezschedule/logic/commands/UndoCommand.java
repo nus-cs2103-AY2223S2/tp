a0eb1db4b50d1cccdd5b67344cc061422392c544
lhy-hoyin <88828097+lhy-hoyin@users.noreply.github.com>
2023-04-04 00:18:56 +0800

    Undo command to print appropriate feedback to user accordingly
    Resolves #112, #135, #154 (duplicate issues).

--- src/main/java/ezschedule/logic/commands/UndoCommand.java
+++ src/main/java/ezschedule/logic/commands/UndoCommand.java
@@ -14,42 +14,53 @@ import ezschedule.model.event.Event;
 public class UndoCommand extends Command {
     public static final String COMMAND_WORD = "undo";
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Undo the most recently deleted event and adds to the scheduler \n"
+            + ": Undo the most recent valid action. \n"
             + "Example: " + COMMAND_WORD;
-    public static final String MESSAGE_DELETE_EVENT_SUCCESS = "Retrieved most recent deleted event: %1$s";
+    public static final String MESSAGE_UNDONE_SUCCESS = "Action undone: %1$s";
+
     @Override
     public String commandWord() {
         return COMMAND_WORD;
     }
+
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
-        Command prevCommand = model.recentCommand().get(0);
-        switch (prevCommand.commandWord()) {
+
+        Command prevCommand = model.recentCommands().get(0);
+        String actionToBeUndone = prevCommand.commandWord();
+
+        switch (actionToBeUndone) {
         case "add":
             Event preEvent = model.recentEvent().get(0);
             model.deleteEvent(preEvent);
             break;
+
         case "edit":
             Event toEditEvent = model.recentEvent().get(0);
             Event editedEvent = model.recentEvent().get(1);
             model.setEvent(editedEvent, toEditEvent);
             break;
+
         case "delete":
             ArrayList<Event> deletedEventList = model.recentEvent();
             for (Event event : deletedEventList) {
                 model.addEvent(event);
             }
             break;
+
         case "recur":
             ArrayList<Event> recurEventList = model.recentEvent();
             for (Event event : recurEventList) {
                 model.deleteEvent(event);
             }
             break;
+
         default:
-            break;
+            return new CommandResult("The previous command cannot be undone.");
         }
-        return new CommandResult(String.format(MESSAGE_DELETE_EVENT_SUCCESS, "Empty string for Undo action now"));
+
+
+        return new CommandResult(String.format(MESSAGE_UNDONE_SUCCESS, actionToBeUndone));
     }
 }
