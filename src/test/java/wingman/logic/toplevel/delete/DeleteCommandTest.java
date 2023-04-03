7369fa5f7cb1ad15faee4f066a81ebaf252b8a96
Xiuxuan Wang <36021591+wxxedu@users.noreply.github.com>
2023-04-03 21:49:07 +0800

    fix test issue

--- src/test/java/wingman/logic/toplevel/delete/DeleteCommandTest.java
+++ src/test/java/wingman/logic/toplevel/delete/DeleteCommandTest.java
@@ -10,6 +10,8 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import org.mockito.Mock;
 import org.mockito.Mockito;
 
 import org.junit.jupiter.api.Test;
@@ -28,40 +30,40 @@ import wingman.model.item.Item;
  */
 @ExtendWith(MockitoExtension.class)
 public class DeleteCommandTest {
+
+    @Mock
+    DeleteFunction<Item> deleteFunction;
+
+    @Mock
+    GetManagerFunction<Item> getManagerFunction;
+
+    @Mock
+    ReadOnlyItemManager<Item> itemManager;
+
+    @Mock
+    Item item;
+
+    @Mock
+    Model model;
+
     @Test
     public void testExecute_doesNotThrow() throws CommandException {
-        DeleteFunction<Item> deleteFunction = mock(DeleteFunction.class);
-        GetManagerFunction<Item> getManagerFunction = mock(GetManagerFunction.class);
-        ReadOnlyItemManager<Item> itemManager = mock(ReadOnlyItemManager.class);
-
-        Item item = mock(Item.class);
-        Model model = mock(Model.class);
-
         when(model.isIndexValid(1, itemManager)).thenReturn(true);
         when(getManagerFunction.get(model)).thenReturn(itemManager);
-
+        when(itemManager.getItem(0)).thenReturn(item);
         DeleteCommand<Item> deleteCommand = new DeleteCommand<>(
                 1,
                 getManagerFunction,
                 deleteFunction
         );
-        Mockito.lenient().when(deleteFunction).delete(eq(model), eq(item));
 
         CommandResult result = deleteCommand.execute(model);
         verify(deleteFunction).delete(eq(model), eq(item));
-
         assertTrue(result.getFeedbackToUser().contains("Deleted"));
     }
 
     @Test
     public void testExecute_throwOutOfBoundsException() throws CommandException {
-        DeleteFunction<Item> deleteFunction = mock(DeleteFunction.class);
-        GetManagerFunction<Item> getManagerFunction = mock(GetManagerFunction.class);
-        ReadOnlyItemManager<Item> itemManager = mock(ReadOnlyItemManager.class);
-
-        Item item = mock(Item.class);
-        Model model = mock(Model.class);
-
         when(model.isIndexValid(1, itemManager)).thenReturn(false);
         when(getManagerFunction.get(model)).thenReturn(itemManager);
 
@@ -71,7 +73,10 @@ public class DeleteCommandTest {
                 deleteFunction
         );
 
-        assertThrows(CommandException.class, () -> deleteCommand.execute(model));
+        assertThrows(
+                CommandException.class,
+                () -> deleteCommand.execute(model)
+        );
 
         verify(getManagerFunction, times(1)).get(model);
     }
