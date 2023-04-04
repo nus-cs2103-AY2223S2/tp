700d355c39cb9e25b41edc34db1694d466b6e614
QIU QIANHUI <672271700@qq.com>
2023-04-04 15:46:41 +0800

    Some minor changes

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -8,7 +8,7 @@ public class Messages {
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_STUDENT_DISPLAYED_INDEX = "The student index provided is invalid";
-    public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d students listed!";
+    public static final String MESSAGE_STUDENTS_LISTED_OVERVIEW = "%1$d students listed!";
     public static final String MESSAGE_INVALID_TASK_DISPLAYED_INDEX = "This task index provided is invalid";
     public static final String MESSAGE_INVALID_SCORE_DISPLAYED_INDEX = "This score index provided is invalid";
     public static final String MESSAGE_INVALID_DIRECTORY = "Path to directory is invalid!";
