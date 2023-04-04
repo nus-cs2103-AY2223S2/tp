6c7c90f02c9a85ac48940b7bfd866c17efecbb51
arkarsg <alxarkar@gmail.com>
2023-04-04 16:19:08 +0800

    Refactors OrderListTest

--- src/test/java/trackr/model/OrderListTest.java
+++ src/test/java/trackr/model/OrderListTest.java
@@ -3,24 +3,24 @@ package trackr.model;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-//import static trackr.logic.commands.CommandTestUtil.VALID_ORDER_STATUS_DONE;
+import static trackr.logic.commands.CommandTestUtil.VALID_ORDER_STATUS_DONE;
 import static trackr.testutil.Assert.assertThrows;
-import static trackr.testutil.TypicalOrders.CHEESE_CAKES;
-import static trackr.testutil.TypicalOrders.CHOCOLATE_COOKIES;
-//import static trackr.testutil.TypicalOrders.getTypicalOrderList;
+import static trackr.testutil.TypicalOrders.CUPCAKE_O;
+import static trackr.testutil.TypicalOrders.CHOCOLATE_COOKIES_O;
+import static trackr.testutil.TypicalOrders.getTypicalOrderList;
 
-//import java.util.Arrays;
-//import java.util.Collection;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
-//import java.util.List;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
-//import javafx.collections.FXCollections;
-//import javafx.collections.ObservableList;`
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import trackr.model.item.exceptions.DuplicateItemException;
-//import trackr.model.order.Order;
-//import trackr.testutil.OrderBuilder;
+import trackr.model.order.Order;
+import trackr.testutil.OrderBuilder;
 
 public class OrderListTest {
 
@@ -36,22 +36,21 @@ public class OrderListTest {
         assertThrows(NullPointerException.class, () -> orderList.resetData(null));
     }
 
-    //    @Test
-    //    public void resetData_withValidReadOnlyOrderList_replacesData() {
-    //        OrderList newData = getTypicalOrderList();
-    //        orderList.resetData(newData);
-    //        assertEquals(newData, orderList);
-    //    }
-    //
-    //    @Test
-    //    public void resetData_withDuplicateOrders_throwsDuplicateOrderException() {
-    //        Order editedOrder = new OrderBuilder(CHOCOLATE_COOKIES)
-    //                .withOrderStatus(VALID_ORDER_STATUS_DONE).build();
-    //        List<Order> newOrders = Arrays.asList(CHOCOLATE_COOKIES, editedOrder);
-    //        OrderListStub newData = new OrderListStub(newOrders);
-    //
-    //        assertThrows(DuplicateItemException.class, () -> orderList.resetData(newData));
-    //    }
+    @Test
+    public void resetData_withValidReadOnlyOrderList_replacesData() {
+        OrderList newData = getTypicalOrderList();
+        orderList.resetData(newData);
+        assertEquals(newData, orderList);
+    }
+
+    @Test
+    public void resetData_withDuplicateOrders_throwsDuplicateOrderException() {
+        Order editedOrder = new OrderBuilder(CHOCOLATE_COOKIES_O)
+                .withOrderStatus(VALID_ORDER_STATUS_DONE).build();
+        List<Order> newOrders = Arrays.asList(CHOCOLATE_COOKIES_O, editedOrder);
+        OrderListStub newData = new OrderListStub(newOrders);
+        assertThrows(DuplicateItemException.class, () -> orderList.resetData(newData));
+    }
 
     @Test
     public void hasOrder_nullOrder_throwsNullPointerException() {
@@ -60,49 +59,49 @@ public class OrderListTest {
 
     @Test
     public void hasOrder_orderNotInOrderList_returnsFalse() {
-        assertFalse(orderList.hasItem(CHOCOLATE_COOKIES));
+        assertFalse(orderList.hasItem(CHOCOLATE_COOKIES_O));
     }
 
     @Test
     public void hasOrder_orderInOrderList_returnsTrue() {
-        orderList.addItem(CHOCOLATE_COOKIES);
-        assertTrue(orderList.hasItem(CHOCOLATE_COOKIES));
+        orderList.addItem(CHOCOLATE_COOKIES_O);
+        assertTrue(orderList.hasItem(CHOCOLATE_COOKIES_O));
     }
 
-    //    @Test
-    //    public void hasOrder_orderWithSameIdentityFieldsInOrderList_returnsTrue() {
-    //        orderList.addItem(CHOCOLATE_COOKIES);
-    //        Order editedOrder = new OrderBuilder(CHOCOLATE_COOKIES)
-    //                .withOrderStatus(VALID_ORDER_STATUS_DONE).build();
-    //        assertTrue(orderList.hasItem(editedOrder));
-    //    }
+    @Test
+    public void hasOrder_orderWithSameIdentityFieldsInOrderList_returnsTrue() {
+        orderList.addItem(CHOCOLATE_COOKIES_O);
+        Order editedOrder = new OrderBuilder(CHOCOLATE_COOKIES_O)
+                .withOrderStatus(VALID_ORDER_STATUS_DONE).build();
+        assertTrue(orderList.hasItem(editedOrder));
+    }
 
     @Test
     public void setOrderList_nullOrder_throwsNullPointerException() {
-        assertThrows(NullPointerException.class, () -> orderList.setItem(null, CHOCOLATE_COOKIES));
+        assertThrows(NullPointerException.class, () -> orderList.setItem(null, CHOCOLATE_COOKIES_O));
     }
 
     @Test
     public void setOrderList_nullEditedOrder_throwsNullPointerException() {
-        orderList.addItem(CHOCOLATE_COOKIES);
-        assertThrows(NullPointerException.class, () -> orderList.setItem(CHOCOLATE_COOKIES, null));
+        orderList.addItem(CHOCOLATE_COOKIES_O);
+        assertThrows(NullPointerException.class, () -> orderList.setItem(CHOCOLATE_COOKIES_O, null));
     }
 
     @Test
     public void setOrderList_withDuplicateOrders_throwsDuplicateOrderException() {
-        orderList.addItem(CHOCOLATE_COOKIES);
-        orderList.addItem(CHEESE_CAKES);
+        orderList.addItem(CHOCOLATE_COOKIES_O);
+        orderList.addItem(CUPCAKE_O);
         assertThrows(DuplicateItemException.class, () ->
-                orderList.setItem(CHEESE_CAKES, CHOCOLATE_COOKIES));
+                orderList.setItem(CUPCAKE_O, CHOCOLATE_COOKIES_O));
     }
 
     @Test
     public void setOrderList_withDifferentOrder_success() {
         OrderList expectedOrderList = new OrderList();
-        expectedOrderList.addItem(CHEESE_CAKES);
+        expectedOrderList.addItem(CUPCAKE_O);
 
-        orderList.addItem(CHOCOLATE_COOKIES);
-        orderList.setItem(CHOCOLATE_COOKIES, CHEESE_CAKES);
+        orderList.addItem(CHOCOLATE_COOKIES_O);
+        orderList.setItem(CHOCOLATE_COOKIES_O, CUPCAKE_O);
 
         assertEquals(expectedOrderList, orderList);
     }
@@ -114,13 +113,13 @@ public class OrderListTest {
 
     @Test
     public void equals() {
-        orderList.addItem(CHOCOLATE_COOKIES);
+        orderList.addItem(CHOCOLATE_COOKIES_O);
 
         OrderList differentOrderList = new OrderList();
-        differentOrderList.addItem(CHEESE_CAKES);
+        differentOrderList.addItem(CUPCAKE_O);
 
         OrderList sameOrderList = new OrderList();
-        sameOrderList.addItem(CHOCOLATE_COOKIES);
+        sameOrderList.addItem(CHOCOLATE_COOKIES_O);
 
         assertTrue(orderList.equals(orderList)); //same object
         assertTrue(orderList.equals(sameOrderList)); //contains the same tasks
@@ -130,16 +129,15 @@ public class OrderListTest {
         assertFalse(orderList.equals(1)); //different objects
     }
 
-    //    private static class OrderListStub implements ReadOnlyOrderList {
-    //        private final ObservableList<Order> orders = FXCollections.observableArrayList();
-    //
-    //        OrderListStub(Collection<Order> orders) {
-    //            this.orders.setAll(orders);
-    //        }
-    //
-    //        @Override
-    //        public ObservableList<Order> getItemList() {
-    //            return orders;
-    //        }
-    //    }
+    private static class OrderListStub implements ReadOnlyOrderList {
+        private final ObservableList<Order> orders = FXCollections.observableArrayList();
+        OrderListStub(Collection<Order> orders) {
+            this.orders.setAll(orders);
+        }
+
+        @Override
+        public ObservableList<Order> getItemList() {
+            return orders;
+        }
+    }
 }
