92886a9d46e9569c37be3fe3da23981507b506e7
arkarsg <alxarkar@gmail.com>
2023-04-05 01:56:29 +0800

    Passes `OrderTest`

--- src/test/java/trackr/model/order/OrderTest.java
+++ src/test/java/trackr/model/order/OrderTest.java
@@ -1,89 +1,91 @@
-//package trackr.model.order;
-//
-//import static org.junit.jupiter.api.Assertions.assertFalse;
-//import static org.junit.jupiter.api.Assertions.assertTrue;
-//import static trackr.logic.commands.CommandTestUtil.VALID_ORDER_NAME_CHOCOLATE_COOKIES;
-//import static trackr.logic.commands.CommandTestUtil.VALID_ORDER_STATUS_NOT_DONE;
-//import static trackr.testutil.TypicalOrders.CHEESE_CAKES;
-//import static trackr.testutil.TypicalOrders.CHOCOLATE_COOKIES;
-//
-//import org.junit.jupiter.api.Test;
-//
-//import trackr.testutil.OrderBuilder;
-//
-//public class OrderTest {
-//
-//    @Test
-//    public void isSameOrder() {
-//        // same object -> returns true
-//        assertTrue(CHOCOLATE_COOKIES.isSameItem(CHOCOLATE_COOKIES));
-//
-//        // null -> returns false
-//        assertFalse(CHOCOLATE_COOKIES.isSameItem(null));
-//
-//        // same name, same deadline all other attributes different -> returns true
-//        Order editedTask = new OrderBuilder(CHOCOLATE_COOKIES)
-//                .withOrderStatus(VALID_ORDER_STATUS_NOT_DONE).build();
-//        assertTrue(CHOCOLATE_COOKIES.isSameItem(editedTask));
-//
-//        // different name, all other attributes same -> returns false
-//        editedTask = new OrderBuilder(CHOCOLATE_COOKIES)
-//                .withOrderName("CHEESE CAKE").build();
-//        assertFalse(CHOCOLATE_COOKIES.isSameItem(editedTask));
-//
-//        // different deadline, all other attributes same -> returns false
-//        editedTask = new OrderBuilder(CHOCOLATE_COOKIES)
-//                .withOrderDeadline("02/01/2024").build();
-//        assertFalse(CHOCOLATE_COOKIES.isSameItem(editedTask));
-//
-//        // name differs in case, all other attributes same -> returns false
-//        Order editedChocolateCookies = new OrderBuilder(CHOCOLATE_COOKIES)
-//                .withOrderName(VALID_ORDER_NAME_CHOCOLATE_COOKIES.toLowerCase()).build();
-//        assertFalse(CHOCOLATE_COOKIES.isSameItem(editedChocolateCookies));
-//
-//        // name has trailing spaces, all other attributes same -> returns false
-//        String taskNameWithTrailingSpaces = VALID_ORDER_NAME_CHOCOLATE_COOKIES + " ";
-////        editedChocolateCookies = new OrderBuilder(CHOCOLATE_COOKIES)
-//                .withOrderName(taskNameWithTrailingSpaces).build();
-//        assertFalse(CHOCOLATE_COOKIES.isSameItem(editedChocolateCookies));
-//    }
-//
-//    @Test
-//    public void equals() {
-//        // same values -> returns true
-//        Order aliceCopy = new OrderBuilder(CHOCOLATE_COOKIES).build();
-//        assertTrue(CHOCOLATE_COOKIES.equals(aliceCopy));
-//
-//        // same object -> returns true
-//        assertTrue(CHOCOLATE_COOKIES.equals(CHOCOLATE_COOKIES));
-//
-//        // null -> returns false
-//        assertFalse(CHOCOLATE_COOKIES.equals(null));
-//
-//        // different type -> returns false
-//        assertFalse(CHOCOLATE_COOKIES.equals(5));
-//
-//        // different person -> returns false
-//        assertFalse(CHOCOLATE_COOKIES.equals(CHEESE_CAKES));
-//
-//        // different name -> returns false
-//        Order editedAlice = new OrderBuilder(CHOCOLATE_COOKIES).withOrderName("CHEESE CAKES").build();
-//        assertFalse(CHOCOLATE_COOKIES.equals(editedAlice));
-//
-//        // different phone -> returns false
-//        editedAlice = new OrderBuilder(CHOCOLATE_COOKIES).withOrderDeadline("02/02/2025").build();
-//        assertFalse(CHOCOLATE_COOKIES.equals(editedAlice));
-//
-//        // different email -> returns false
-//        editedAlice = new OrderBuilder(CHOCOLATE_COOKIES).withOrderQuantity("444").build();
-//        assertFalse(CHOCOLATE_COOKIES.equals(editedAlice));
-//
-//        // different address -> returns false
-//        editedAlice = new OrderBuilder(CHOCOLATE_COOKIES).withOrderStatus("I").build();
-//        assertFalse(CHOCOLATE_COOKIES.equals(editedAlice));
-//
-//        assertTrue(CHOCOLATE_COOKIES.toString().equals(CHOCOLATE_COOKIES.toString()));
-//    }
-//
-//}
-//
+package trackr.model.order;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static trackr.logic.commands.CommandTestUtil.VALID_ORDER_NAME_CHOCOLATE_COOKIES;
+import static trackr.logic.commands.CommandTestUtil.VALID_ORDER_STATUS_NOT_DONE;
+import static trackr.testutil.TypicalMenuItems.CUPCAKE_M;
+import static trackr.testutil.TypicalOrders.CHOCOLATE_COOKIES_O;
+import static trackr.testutil.TypicalOrders.CUPCAKE_O;
+
+import org.junit.jupiter.api.Test;
+
+import trackr.testutil.MenuItemBuilder;
+import trackr.testutil.OrderBuilder;
+
+public class OrderTest {
+
+    @Test
+    public void isSameOrder() {
+        // same object -> returns true
+        assertTrue(CHOCOLATE_COOKIES_O.isSameItem(CHOCOLATE_COOKIES_O));
+
+        // null -> returns false
+        assertFalse(CHOCOLATE_COOKIES_O.isSameItem(null));
+
+        // same name, same deadline all other attributes different -> returns true
+        Order editedOrder = new OrderBuilder(CHOCOLATE_COOKIES_O)
+                .withOrderStatus(VALID_ORDER_STATUS_NOT_DONE).build();
+        assertTrue(CHOCOLATE_COOKIES_O.isSameItem(editedOrder));
+
+        // different order item, all other attributes same -> returns false
+        editedOrder = new OrderBuilder(CHOCOLATE_COOKIES_O)
+                .withOrderItem(CUPCAKE_M).build();
+        assertFalse(CHOCOLATE_COOKIES_O.isSameItem(editedOrder));
+
+        // different deadline, all other attributes same -> returns false
+        editedOrder = new OrderBuilder(CHOCOLATE_COOKIES_O)
+                .withOrderDeadline("02/01/2024").build();
+        assertFalse(CHOCOLATE_COOKIES_O.isSameItem(editedOrder));
+
+        // name differs in case, all other attributes same -> returns false
+        Order editedChocolateCookies = new OrderBuilder(CHOCOLATE_COOKIES_O)
+                .withOrderItem(new MenuItemBuilder().withItemName("chocolate cookies").build()).build();
+        assertFalse(CHOCOLATE_COOKIES_O.isSameItem(editedChocolateCookies));
+
+        // Order item name has trailing spaces, all other attributes same -> returns false
+        String orderItemWithTrailingSpaces = VALID_ORDER_NAME_CHOCOLATE_COOKIES + " ";
+        editedChocolateCookies = new OrderBuilder(CHOCOLATE_COOKIES_O)
+                .withOrderItem(new MenuItemBuilder().withItemName(orderItemWithTrailingSpaces).build())
+                .build();
+        assertFalse(CHOCOLATE_COOKIES_O.isSameItem(editedChocolateCookies));
+    }
+
+    @Test
+    public void equals() {
+        // same values -> returns true
+        Order aliceCopy = new OrderBuilder(CHOCOLATE_COOKIES_O).build();
+        assertTrue(CHOCOLATE_COOKIES_O.equals(aliceCopy));
+
+        // same object -> returns true
+        assertTrue(CHOCOLATE_COOKIES_O.equals(CHOCOLATE_COOKIES_O));
+
+        // null -> returns false
+        assertFalse(CHOCOLATE_COOKIES_O.equals(null));
+
+        // different type -> returns false
+        assertFalse(CHOCOLATE_COOKIES_O.equals(5));
+
+        // different person -> returns false
+        assertFalse(CHOCOLATE_COOKIES_O.equals(CUPCAKE_O));
+
+        // different item -> returns false
+        Order editedAlice = new OrderBuilder(CHOCOLATE_COOKIES_O).withOrderItem(CUPCAKE_M).build();
+        assertFalse(CHOCOLATE_COOKIES_O.equals(editedAlice));
+
+        // different deadline -> returns false
+        editedAlice = new OrderBuilder(CHOCOLATE_COOKIES_O).withOrderDeadline("02/02/2025").build();
+        assertFalse(CHOCOLATE_COOKIES_O.equals(editedAlice));
+
+        // different email -> returns false
+        editedAlice = new OrderBuilder(CHOCOLATE_COOKIES_O).withOrderQuantity("444").build();
+        assertFalse(CHOCOLATE_COOKIES_O.equals(editedAlice));
+
+        // different address -> returns false
+        editedAlice = new OrderBuilder(CHOCOLATE_COOKIES_O).withOrderStatus("I").build();
+        assertFalse(CHOCOLATE_COOKIES_O.equals(editedAlice));
+
+        assertTrue(CHOCOLATE_COOKIES_O.toString().equals(CHOCOLATE_COOKIES_O.toString()));
+    }
+
+}
