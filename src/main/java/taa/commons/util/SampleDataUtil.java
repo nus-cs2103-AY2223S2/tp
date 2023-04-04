c7ab91c0c9543e592ece1c2035b7cf52b5b3b1a5
bojie3 <xubojie2013@gmail.com>
2023-04-04 12:11:06 +0800

    Update documentation

--- src/main/java/taa/commons/util/SampleDataUtil.java
+++ src/main/java/taa/commons/util/SampleDataUtil.java
@@ -20,23 +20,23 @@ import taa.model.tag.Tag;
 public class SampleDataUtil {
     public static Student[] getSampleStudents() {
         return new Student[] {
-            new Student(new Name("Alex Yeoh"), "0,0,0,0,0,0,0,0,0,0,0,0",
-                    "-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1", new ArrayList<String>(),
+            new Student(new Name("Alex Yeoh"), "0;0;0;0;0;0;0;0;0;0;0;0",
+                    "-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1", new ArrayList<String>(),
                 getTagSet("Tut_T01")),
-            new Student(new Name("Bernice Yu"), "0,0,0,0,0,0,0,0,0,0,0,0",
-                    "-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1", new ArrayList<String>(),
+            new Student(new Name("Bernice Yu"), "0;0;0;0;0;0;0;0;0;0;0;0",
+                    "-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1", new ArrayList<String>(),
                 getTagSet("Tut_T01", "Lab_L01")),
-            new Student(new Name("Charlotte Oliveiro"), "0,0,0,0,0,0,0,0,0,0,0,0",
-                    "-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1", new ArrayList<String>(),
+            new Student(new Name("Charlotte Oliveiro"), "0;0;0;0;0;0;0;0;0;0;0;0",
+                    "-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1", new ArrayList<String>(),
                 getTagSet("Tut_T02")),
-            new Student(new Name("David Li"), "0,0,0,0,0,0,0,0,0,0,0,0",
-                    "-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1", new ArrayList<String>(),
+            new Student(new Name("David Li"), "0;0;0;0;0;0;0;0;0;0;0;0",
+                    "-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1", new ArrayList<String>(),
                 getTagSet("Lab_L02")),
-            new Student(new Name("Irfan Ibrahim"), "0,0,0,0,0,0,0,0,0,0,0,0",
-                    "-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1", new ArrayList<String>(),
+            new Student(new Name("Irfan Ibrahim"), "0;0;0;0;0;0;0;0;0;0;0;0",
+                    "-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1", new ArrayList<String>(),
                 getTagSet("Rec_R03")),
-            new Student(new Name("Roy Balakrishnan"), "0,0,0,0,0,0,0,0,0,0,0,0",
-                    "-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1", new ArrayList<String>(),
+            new Student(new Name("Roy Balakrishnan"), "0;0;0;0;0;0;0;0;0;0;0;0",
+                    "-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1", new ArrayList<String>(),
                 getTagSet("Lab_L12"))
         };
     }
