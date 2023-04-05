9a50d10f078d5715302143e199ea9ff4ba624ff7
KevinEyo1 <eyokaiwenkevin@gmail.com>
2023-04-05 22:57:37 +0800

    Fix ped

--- src/main/java/seedu/ultron/model/opening/Keydate.java
+++ src/main/java/seedu/ultron/model/opening/Keydate.java
@@ -3,6 +3,9 @@ package seedu.ultron.model.opening;
 import static java.util.Objects.requireNonNull;
 import static seedu.ultron.commons.util.AppUtil.checkArgument;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
@@ -15,11 +18,11 @@ public class Keydate implements Comparable<Keydate> {
     public static final String MESSAGE_CONSTRAINTS = "Keydates should be of the format key@date "
             + "and adhere to the following constraints:\n"
             + "1. The key should only contain alphabetic characters and must not be blank or made of only whitespace.\n"
-            + "2. This is followed by a '@' and then a date. The date should only be in the format yyyy-MM-dd.\n"
+            + "2. This is followed by a '@' and then a date. The date should only be in the format YYYY-MM-DD.\n"
             + "eg. CodingAssignment@2023-01-01\n";
     private static final String DATE_ERROR = "Date should be valid.";
     private static final DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-
+    private static final String VALIDATION_REGEX_KEY = "^[A-Za-z\\s]*$";
 
     public final String fullDate;
     public final String fullKey;
@@ -44,10 +47,18 @@ public class Keydate implements Comparable<Keydate> {
         if (test.length != 2 || test[0] == null || test[1] == null) {
             return false;
         }
+        if (test[0].strip().isEmpty()) {
+            return false;
+        }
         try {
             LocalDate.parse(test[1], dateFormat);
-            return true;
+            DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+            df.setLenient(false);
+            df.parse(test[1]);
+            return true && test[0].matches(VALIDATION_REGEX_KEY);
         } catch (DateTimeParseException e) {
+            return false;
+        } catch (ParseException e) {
             return false;
         }
     }
