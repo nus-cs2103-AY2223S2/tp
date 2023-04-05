f6ebe3c9eb565a77e8f18153e70f95610d23ce1a
JrmCkh <Hhelloogkart@gmail.com>
2023-04-05 17:53:31 +0800

    Add equals() test method to Name

--- src/test/java/ezschedule/model/event/NameTest.java
+++ src/test/java/ezschedule/model/event/NameTest.java
@@ -1,5 +1,7 @@
 package ezschedule.model.event;
 
+import static ezschedule.logic.commands.CommandTestUtil.VALID_NAME_A;
+import static ezschedule.logic.commands.CommandTestUtil.VALID_NAME_B;
 import static ezschedule.testutil.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -36,5 +38,26 @@ public class NameTest {
         assertTrue(Name.isValidName("peter the 2nd")); // alphanumeric characters
         assertTrue(Name.isValidName("Capital Tan")); // with capital letters
         assertTrue(Name.isValidName("David Roger Jackson Ray Jr 2nd")); // long names
+    }
+
+    @Test
+    public void equals() {
+        // same object -> returns true
+        Name name = new Name(VALID_NAME_A);
+        assertTrue(name.equals(name));
+
+        // same object -> returns true
+        Name nameCopy = new Name(VALID_NAME_A);
+        assertTrue(name.equals(nameCopy));
+
+        // null -> returns false
+        assertFalse(name.equals(null));
+
+        // different type -> returns false
+        assertFalse(name.equals(5));
+
+        // different name -> returns false
+        Name differentName = new Name(VALID_NAME_B);
+        assertFalse(name.equals(differentName));
     }
 }
