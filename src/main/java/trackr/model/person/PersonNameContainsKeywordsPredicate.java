0445b5c29e2eb14a8239deddb6cc83cf34a50878
Darren Chang <darrenchang2@gmail.com>
2023-04-05 14:16:02 +0800

    Revert PersonNameContainsKeywordsPredicate

--- src/main/java/trackr/model/person/PersonNameContainsKeywordsPredicate.java
+++ src/main/java/trackr/model/person/PersonNameContainsKeywordsPredicate.java
@@ -1,7 +1,6 @@
 package trackr.model.person;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.function.Predicate;
 
 import trackr.commons.util.StringUtil;
@@ -10,72 +9,30 @@ import trackr.model.item.Item;
 /**
  * Tests that a {@code Suppliers}'s {@code Name} matches any of the keywords given.
  */
-public class PersonNameContainsKeywordsPredicate extends PersonDescriptor implements Predicate<Item> {
-    private List<String> personNameKeywords;
+public class PersonNameContainsKeywordsPredicate implements Predicate<Item> {
+    private final List<String> keywords;
 
-    public PersonNameContainsKeywordsPredicate() {
-        super();
-    }
-
-    /**
-     * Copy constructor.
-     */
-    public PersonNameContainsKeywordsPredicate(PersonNameContainsKeywordsPredicate toCopy) {
-        setPersonNameKeywords(toCopy.personNameKeywords);
-    }
-
-    public void setPersonNameKeywords(List<String> personNameKeywords) {
-        this.personNameKeywords = personNameKeywords;
-    }
-
-
-    public Optional<List<String>> getPersonNameKeywords() {
-        return Optional.ofNullable(personNameKeywords);
-    }
-
-    public boolean isAnyFieldPresent() {
-        return isAnyFieldEdited() || personNameKeywords != null;
+    public PersonNameContainsKeywordsPredicate(List<String> keywords) {
+        this.keywords = keywords;
     }
 
     @Override
     public boolean test(Item item) {
-        if (!(item instanceof Person)) {
+        if (!(item instanceof Supplier)) {
             return false;
         }
 
-        Person person = (Person) item;
+        Supplier supplier = (Supplier) item;
 
-        boolean isPersonNameMatch;
-
-        if (personNameKeywords != null) {
-            isPersonNameMatch = personNameKeywords.stream()
-                    .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getPersonName().getName(), keyword));
-        } else {
-            // Default value since no name keyword is set
-            isPersonNameMatch = true;
-        }
-
-        return isPersonNameMatch;
+        return keywords.stream()
+                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(supplier.getPersonName().getName(), keyword));
     }
 
     @Override
     public boolean equals(Object other) {
-        // short circuit if same object
-        if (other == this) {
-            return true;
-        }
-
-        // instanceof handles nulls
-        if (!(other instanceof PersonNameContainsKeywordsPredicate)) {
-            return false;
-        }
-
-        // state check
-        PersonNameContainsKeywordsPredicate predicate = (PersonNameContainsKeywordsPredicate) other;
-
-        return getPersonNameKeywords().equals(predicate.getPersonNameKeywords());
+        return other == this // short circuit if same object
+                || (other instanceof PersonNameContainsKeywordsPredicate // instanceof handles nulls
+                && keywords.equals(((PersonNameContainsKeywordsPredicate) other).keywords)); // state check
     }
-
-
 
 }
