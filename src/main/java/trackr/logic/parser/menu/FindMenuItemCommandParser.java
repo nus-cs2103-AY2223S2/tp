5a7cf388efacb19eb18af31e5c87a97ea8fe841a
Darren Chang <darrenchang2@gmail.com>
2023-04-05 14:25:53 +0800

    Revert the Find item and supplier parser

--- src/main/java/trackr/logic/parser/menu/FindMenuItemCommandParser.java
+++ src/main/java/trackr/logic/parser/menu/FindMenuItemCommandParser.java
@@ -1,13 +1,10 @@
 package trackr.logic.parser.menu;
 
-import static java.util.Objects.requireNonNull;
-import static trackr.logic.parser.CliSyntax.PREFIX_NAME;
+import static trackr.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import java.util.Arrays;
 
 import trackr.logic.commands.menu.FindMenuItemCommand;
-import trackr.logic.parser.ArgumentMultimap;
-import trackr.logic.parser.ArgumentTokenizer;
 import trackr.logic.parser.Parser;
 import trackr.logic.parser.exceptions.ParseException;
 import trackr.model.menu.ItemNameContainsKeywordsPredicate;
@@ -18,22 +15,19 @@ import trackr.model.menu.ItemNameContainsKeywordsPredicate;
 public class FindMenuItemCommandParser implements Parser<FindMenuItemCommand> {
 
     /**
-     * Parses the given {@code String} of arguments in the context of the FindTaskCommand
+     * Parses the given {@code String} of arguments in the context of the FindMenuItemCommand
      * and returns a FindMenuItemCommand object for execution.
-     *
      * @throws ParseException if the user input does not conform the expected format
      */
     public FindMenuItemCommand parse(String args) throws ParseException {
-        requireNonNull(args);
-        ArgumentMultimap argMultimap =
-                ArgumentTokenizer.tokenize(args, PREFIX_NAME);
-
-        ItemNameContainsKeywordsPredicate predicate = new ItemNameContainsKeywordsPredicate();
-        if (argMultimap.getValue(PREFIX_NAME).isPresent()) {
-            String[] itemNameKeywords = argMultimap.getValue(PREFIX_NAME).get().trim().split("\\s+");
-            predicate.setItemNameKeywords(Arrays.asList(itemNameKeywords));
+        String trimmedArgs = args.trim();
+        if (trimmedArgs.isEmpty()) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindMenuItemCommand.MESSAGE_USAGE));
         }
-        return new FindMenuItemCommand(predicate);
-    }
 
+        String[] nameKeywords = trimmedArgs.split("\\s+");
+
+        return new FindMenuItemCommand(new ItemNameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+    }
 }
--- src/main/java/trackr/logic/parser/supplier/FindSupplierCommandParser.java
+++ src/main/java/trackr/logic/parser/supplier/FindSupplierCommandParser.java
@@ -1,13 +1,10 @@
 package trackr.logic.parser.supplier;
 
-import static java.util.Objects.requireNonNull;
-import static trackr.logic.parser.CliSyntax.PREFIX_NAME;
+import static trackr.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import java.util.Arrays;
 
 import trackr.logic.commands.supplier.FindSupplierCommand;
-import trackr.logic.parser.ArgumentMultimap;
-import trackr.logic.parser.ArgumentTokenizer;
 import trackr.logic.parser.Parser;
 import trackr.logic.parser.exceptions.ParseException;
 import trackr.model.person.PersonNameContainsKeywordsPredicate;
@@ -18,24 +15,20 @@ import trackr.model.person.PersonNameContainsKeywordsPredicate;
 public class FindSupplierCommandParser implements Parser<FindSupplierCommand> {
 
     /**
-     * Parses the given {@code String} of arguments in the context of the FindTaskCommand
-     * and returns a FindTaskCommand object for execution.
-     *
+     * Parses the given {@code String} of arguments in the context of the FindSupplierCommand
+     * and returns a FindSupplierCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
     public FindSupplierCommand parse(String args) throws ParseException {
-        requireNonNull(args);
-        ArgumentMultimap argMultimap =
-                ArgumentTokenizer.tokenize(args, PREFIX_NAME);
-
-        PersonNameContainsKeywordsPredicate predicate = new PersonNameContainsKeywordsPredicate();
-        if (argMultimap.getValue(PREFIX_NAME).isPresent()) {
-            String[] supplierNameKeywords = argMultimap.getValue(PREFIX_NAME).get().trim().split("\\s+");
-            predicate.setPersonNameKeywords(Arrays.asList(supplierNameKeywords));
+        String trimmedArgs = args.trim();
+        if (trimmedArgs.isEmpty()) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindSupplierCommand.MESSAGE_USAGE));
         }
 
+        String[] nameKeywords = trimmedArgs.split("\\s+");
 
-        return new FindSupplierCommand(predicate);
+        return new FindSupplierCommand(new PersonNameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
     }
 
 }
