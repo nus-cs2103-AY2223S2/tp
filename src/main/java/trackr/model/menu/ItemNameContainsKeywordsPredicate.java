38b70bf7af3780b6e8481e1437eefd451025354a
Darren Chang <darrenchang2@gmail.com>
2023-04-05 14:22:29 +0800

    Revert ItemNameContainsKeywordsPredicate

--- src/main/java/trackr/model/menu/ItemNameContainsKeywordsPredicate.java
+++ src/main/java/trackr/model/menu/ItemNameContainsKeywordsPredicate.java
@@ -1,77 +1,38 @@
 package trackr.model.menu;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.function.Predicate;
 
 import trackr.commons.util.StringUtil;
 import trackr.model.item.Item;
 
 /**
- * Tests that a {@code Suppliers}'s {@code Name} matches any of the keywords given.
+ * Tests that a {@code MenuItem}'s {@code ItemName} matches any of the keywords given.
  */
-public class ItemNameContainsKeywordsPredicate extends MenuItemDescriptor implements Predicate<Item> {
-    private List<String> itemNameKeywords;
+public class ItemNameContainsKeywordsPredicate implements Predicate<Item> {
+    private final List<String> keywords;
 
-    public ItemNameContainsKeywordsPredicate() {
-        super();
-    }
-
-    /**
-     * Copy constructor.
-     */
-    public ItemNameContainsKeywordsPredicate(ItemNameContainsKeywordsPredicate toCopy) {
-        setItemNameKeywords(toCopy.itemNameKeywords);
-    }
-
-    public void setItemNameKeywords(List<String> itemNameKeywords) {
-        this.itemNameKeywords = itemNameKeywords;
-    }
-
-    public Optional<List<String>> getItemNameKeywords() {
-        return Optional.ofNullable(itemNameKeywords);
-    }
-
-    public boolean isAnyFieldPresent() {
-        return isAnyFieldNonNull() || itemNameKeywords != null;
+    public ItemNameContainsKeywordsPredicate(List<String> keywords) {
+        this.keywords = keywords;
     }
 
     @Override
     public boolean test(Item item) {
-        if (!(item instanceof Item)) {
+        if (!(item instanceof MenuItem)) {
             return false;
         }
 
         MenuItem menuItem = (MenuItem) item;
 
-        boolean isItemNameMatch;
-
-        if (itemNameKeywords != null) {
-            isItemNameMatch = itemNameKeywords.stream()
-                    .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(menuItem.getItemName().getName(), keyword));
-        } else {
-            // Default value since no name keyword is set
-            isItemNameMatch = true;
-        }
-
-        return isItemNameMatch;
+        return keywords.stream()
+                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(menuItem.getItemName().getName(), keyword));
     }
 
     @Override
     public boolean equals(Object other) {
-        // short circuit if same object
-        if (other == this) {
-            return true;
-        }
-
-        // instanceof handles nulls
-        if (!(other instanceof ItemNameContainsKeywordsPredicate)) {
-            return false;
-        }
-
-        // state check
-        ItemNameContainsKeywordsPredicate predicate = (ItemNameContainsKeywordsPredicate) other;
-
-        return getItemNameKeywords().equals(predicate.getItemNameKeywords());
+        return other == this // short circuit if same object
+                || (other instanceof ItemNameContainsKeywordsPredicate // instanceof handles nulls
+                && keywords.equals(((ItemNameContainsKeywordsPredicate) other).keywords)); // state check
     }
+
 }
