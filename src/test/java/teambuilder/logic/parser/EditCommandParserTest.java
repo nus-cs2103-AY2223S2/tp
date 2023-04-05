83ebea51e22c65f2b904f0bba193f1c9c710ecbe
ChickenChiang <potatoeschiang@gmail.com>
2023-04-05 17:55:23 +0800

    Update test cases to check agaisnt new error message

--- src/test/java/teambuilder/logic/parser/EditCommandParserTest.java
+++ src/test/java/teambuilder/logic/parser/EditCommandParserTest.java
@@ -46,7 +46,8 @@ public class EditCommandParserTest {
     private static final String TAG_EMPTY = " " + PREFIX_TAG;
 
     private static final String MESSAGE_INVALID_FORMAT =
-            String.format(MESSAGE_INVALID_COMMAND_FORMAT, EditCommand.MESSAGE_USAGE);
+            String.format(ParserUtil.MESSAGE_INVALID_INDEX + "\n"
+                    + MESSAGE_INVALID_COMMAND_FORMAT, EditCommand.MESSAGE_USAGE);
 
     private EditCommandParser parser = new EditCommandParser();
 
