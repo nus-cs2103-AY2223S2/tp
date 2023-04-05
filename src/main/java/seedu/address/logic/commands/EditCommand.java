982c2e90f700b25b6779156cd6303ee87bf79139
Muhammad Fahim Tajwar <fahim.tajwar@u.nus.edu>
2023-04-05 21:13:58 +0800

    Rename edit command to update-info

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -31,7 +31,7 @@ import seedu.address.model.tag.Tag;
  */
 public class EditCommand extends Command {
 
-    public static final String COMMAND_WORD = "edit";
+    public static final String COMMAND_WORD = "update-info";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the details of the person identified "
             + "by the index number used in the displayed person list. "
