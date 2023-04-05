5ed42e21ecc59f656bff1bbf17067c1682ac490f
Eric Leow <ericleowyuquan@gmail.com>
2023-04-05 21:24:56 +0800

    Add tests for filter and sort methods

--- src/test/java/seedu/address/logic/commands/SortStudentCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SortStudentCommandTest.java
@@ -14,16 +14,30 @@ import seedu.address.model.person.Person;
 import seedu.address.testutil.PersonBuilder;
 
 /**
- * Contains integration tests (interaction with the Model) and unit tests for RemarkCommand.
+ * Contains integration tests (interaction with the Model) and unit tests for SortStudentCommand.
  */
 public class SortStudentCommandTest {
 
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+    public static final String COMMAND_WORD = "sort-student";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Sorts all address book students.\n "
+            + "Parameters: The group of students you wish to sort (either all, lab, tutorial or consultation), "
+            + "the metric to be sorted (either address, email, name, performance or remark), "
+            + "and the desired order (either reverse or nonreverse)\n"
+            + "For example: 'sort-student all name reverse' command will order all students in reverse-alphabetical "
+            + "ordering of their names";
 
     @Test
     public void execute() throws CommandException {
         Person validPerson = new PersonBuilder().build();
         CommandResult commandResult = new AddCommand(validPerson).execute(model);
         assertEquals(new SortStudentCommand("temp", "temp", true).execute(model), new CommandResult(MESSAGE_SUCCESS));
+    }
+
+    @Test
+    public void check_attributes() throws CommandException {
+        SortStudentCommand sortCommand = new SortStudentCommand("all", "name", true);
+        assertEquals(sortCommand.MESSAGE_USAGE, MESSAGE_USAGE);
     }
 }
