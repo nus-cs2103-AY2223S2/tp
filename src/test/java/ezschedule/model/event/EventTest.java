319dfac9816d5772285242a9d3fe62ca60e4ef54
JrmCkh <Hhelloogkart@gmail.com>
2023-04-05 15:51:56 +0800

    Add test cases for Event class

--- src/test/java/ezschedule/model/event/EventTest.java
+++ src/test/java/ezschedule/model/event/EventTest.java
@@ -1,5 +1,9 @@
 package ezschedule.model.event;
 
+import static ezschedule.logic.commands.CommandTestUtil.INVALID_DATE;
+import static ezschedule.logic.commands.CommandTestUtil.INVALID_END_TIME;
+import static ezschedule.logic.commands.CommandTestUtil.INVALID_NAME;
+import static ezschedule.logic.commands.CommandTestUtil.INVALID_START_TIME;
 import static ezschedule.logic.commands.CommandTestUtil.VALID_DATE_B;
 import static ezschedule.logic.commands.CommandTestUtil.VALID_END_TIME_B;
 import static ezschedule.logic.commands.CommandTestUtil.VALID_NAME_B;
@@ -10,9 +14,12 @@ import static ezschedule.testutil.TypicalEvents.EVENT_B;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.Assertions;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import org.junit.jupiter.api.Test;
 
 import ezschedule.testutil.EventBuilder;
@@ -20,83 +27,119 @@ import ezschedule.testutil.EventBuilder;
 public class EventTest {
 
     @Test
-    public void isSameEvent() {
-        // same object -> returns true
-        assertEquals(EVENT_A, EVENT_A);
+    public void isCompleted() {
+        LocalDate today = LocalDate.now();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String formattedTodayDate = today.format(dateTimeFormatter);
 
-        // null -> returns false
-        assertNotEquals(null, EVENT_A);
+        Event pastEvent = new EventBuilder(ART).withDate("2020-01-01").withEndTime("00:00").build();
+        Event futureEvent = new EventBuilder(ART).withDate("2999-01-01").withEndTime("00:00").build();
+        Event pastTodayEvent = new EventBuilder(ART).withDate(formattedTodayDate).withEndTime("00:00").build();
+        Event futureTodayEvent = new EventBuilder(ART).withDate(formattedTodayDate).withEndTime("23:59").build();
 
-        // same name, all other attributes different -> returns false
-        Event editedA = new EventBuilder(EVENT_A).withDate(VALID_DATE_B)
-                .withStartTime(VALID_START_TIME_B).withEndTime(VALID_END_TIME_B).build();
-        assertNotEquals(EVENT_A, editedA);
+        assertTrue(pastEvent.isCompleted());
+        assertFalse(futureEvent.isCompleted());
+        assertTrue(pastTodayEvent.isCompleted());
+        assertFalse(futureTodayEvent.isCompleted());
+    }
 
-        // different name, all other attributes same -> returns false
-        editedA = new EventBuilder(EVENT_A).withName(VALID_NAME_B).build();
-        assertNotEquals(EVENT_A, editedA);
+    @Test
+    public void isEventOverlap() {
+        Event event = new EventBuilder().build();
 
-        // name differs in case, all other attributes same -> returns false
-        Event editedB = new EventBuilder(EVENT_A).withName(VALID_NAME_B.toLowerCase()).build();
-        assertNotEquals(EVENT_B, editedB);
+        // different date -> returns false
+        Event otherEvent = new EventBuilder().withDate("2025-01-01").build();
+        assertFalse(event.isEventOverlap(otherEvent));
 
-        // name has trailing spaces, all other attributes same -> returns false
-        String nameWithTrailingSpaces = VALID_NAME_B + " ";
-        editedB = new EventBuilder(EVENT_B).withName(nameWithTrailingSpaces).build();
-        assertNotEquals(EVENT_B, editedB);
+        // same date, earlier time -> returns false
+        otherEvent = new EventBuilder().withStartTime("00:00").withEndTime("00:00").build();
+        assertFalse(event.isEventOverlap(otherEvent));
+
+        // same date, later time -> returns false
+        otherEvent = new EventBuilder().withStartTime("23:59").withEndTime("23:59").build();
+        assertFalse(event.isEventOverlap(otherEvent));
+
+        // same date, same start time and end time -> returns true
+        otherEvent = new EventBuilder().build();
+        assertTrue(event.isEventOverlap(otherEvent));
+
+        // same date, within event time -> returns true
+        otherEvent = new EventBuilder().withStartTime("08:01").withEndTime("09:59").build();
+        assertTrue(event.isEventOverlap(otherEvent));
+
+        // same date, overlaps start time -> returns true
+        otherEvent = new EventBuilder().withStartTime("07:59").withEndTime("08:01").build();
+        assertTrue(event.isEventOverlap(otherEvent));
+
+        // same date, overlaps end time -> returns true
+        otherEvent = new EventBuilder().withStartTime("09:59").withEndTime("10:01").build();
+        assertTrue(event.isEventOverlap(otherEvent));
+
+        // same date, earlier start time and same end time -> returns true
+        otherEvent = new EventBuilder().withStartTime("09:59").build();
+        assertTrue(event.isEventOverlap(otherEvent));
+
+        // same date, same start time and later end time -> returns true
+        otherEvent = new EventBuilder().withEndTime("10:01").build();
+        assertTrue(event.isEventOverlap(otherEvent));
+
+        // same date, earlier start time and later end time -> returns true
+        otherEvent = new EventBuilder().withStartTime("07:59").withEndTime("10:01").build();
+        assertTrue(event.isEventOverlap(otherEvent));
     }
 
     @Test
     public void equals() {
+        // same object -> returns true
+        assertEquals(EVENT_A, EVENT_A);
+
         // same values -> returns true
         Event aCopy = new EventBuilder(EVENT_A).build();
-        assertTrue(EVENT_A.equals(aCopy));
-
-        // same object -> returns true
-        assertTrue(EVENT_A.equals(EVENT_A));
+        assertEquals(EVENT_A, aCopy);
 
         // null -> returns false
-        assertFalse(EVENT_A.equals(null));
+        assertNotEquals(null, EVENT_A);
 
         // different type -> returns false
-        assertFalse(EVENT_A.equals(5));
+        assertNotEquals(5, EVENT_A);
 
         // different event -> returns false
-        assertFalse(EVENT_A.equals(EVENT_B));
+        assertNotEquals(EVENT_A, EVENT_B);
 
-        // different name -> returns false
+        // different name, all other attributes same -> returns false
         Event editedA = new EventBuilder(EVENT_A).withName(VALID_NAME_B).build();
-        assertFalse(EVENT_A.equals(editedA));
+        assertNotEquals(EVENT_A, editedA);
 
-        // different date -> returns false
+        // different date, all other attributes same -> returns false
         editedA = new EventBuilder(EVENT_A).withDate(VALID_DATE_B).build();
-        Assertions.assertFalse(EVENT_A.equals(editedA));
+        assertNotEquals(EVENT_A, editedA);
 
-        // different start time -> returns false
+        // different start time, all other attributes same -> returns false
         editedA = new EventBuilder(EVENT_A).withStartTime(VALID_START_TIME_B).build();
-        Assertions.assertFalse(EVENT_A.equals(editedA));
+        assertNotEquals(EVENT_A, editedA);
 
-        // different end time -> returns false
+        // different end time, all other attributes same -> returns false
         editedA = new EventBuilder(EVENT_A).withEndTime(VALID_END_TIME_B).build();
-        Assertions.assertFalse(EVENT_A.equals(editedA));
-    }
+        assertNotEquals(EVENT_A, editedA);
 
-    @Test
-    public void compareToItself() {
-        Event event = new EventBuilder(ART).build();
-        assertEquals(0, event.compareTo(event));
-    }
+        // invalid name -> throw IllegalArgumentException
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () ->
+                new EventBuilder(EVENT_A).withName(INVALID_NAME).build());
+        assertEquals(Name.MESSAGE_CONSTRAINTS, exception.getMessage());
 
-    @Test
-    public void getCompletedStatus() {
-        Event pastEventMorningTime = new EventBuilder(ART).withDate("2020-01-01").withEndTime("00:00").build();
-        Event pastEventNightTime = new EventBuilder(ART).withDate("2020-01-01").withEndTime("23:59").build();
-        Event futureEventMorningTime = new EventBuilder(ART).withDate("2999-01-01").withEndTime("00:00").build();
-        Event futureEventNightTime = new EventBuilder(ART).withDate("2999-01-01").withEndTime("23:59").build();
+        // invalid date -> throw IllegalArgumentException
+        exception = assertThrows(IllegalArgumentException.class, () ->
+                new EventBuilder(EVENT_A).withDate(INVALID_DATE).build());
+        assertEquals(Date.MESSAGE_CONSTRAINTS, exception.getMessage());
 
-        assertTrue(pastEventMorningTime.isCompleted());
-        assertTrue(pastEventNightTime.isCompleted());
-        assertFalse(futureEventMorningTime.isCompleted());
-        assertFalse(futureEventNightTime.isCompleted());
+        // invalid start time -> throw IllegalArgumentException
+        exception = assertThrows(IllegalArgumentException.class, () ->
+                new EventBuilder(EVENT_A).withStartTime(INVALID_START_TIME).build());
+        assertEquals(Time.MESSAGE_CONSTRAINTS, exception.getMessage());
+
+        // invalid end time -> throw IllegalArgumentException
+        exception = assertThrows(IllegalArgumentException.class, () ->
+                new EventBuilder(EVENT_A).withStartTime(INVALID_END_TIME).build());
+        assertEquals(Time.MESSAGE_CONSTRAINTS, exception.getMessage());
     }
 }
