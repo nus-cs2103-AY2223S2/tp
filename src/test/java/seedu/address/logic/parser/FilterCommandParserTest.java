dbce22da9284c3e7392b4a15ae76d7a16068ad2a
Eric Leow <ericleowyuquan@gmail.com>
2023-04-05 22:17:44 +0800

    Fix checkstyle

--- src/test/java/seedu/address/logic/parser/FilterCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FilterCommandParserTest.java
@@ -11,7 +11,6 @@ import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.FilterCommand;
 import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
@@ -21,11 +20,11 @@ import seedu.address.model.UserPrefs;
  * Contains integration tests (interaction with the Model) and unit tests for FilterCommand.
  */
 public class FilterCommandParserTest {
-    public static final String errorMessage = "Invalid command format! \n" +
-            "filter: Filters all address book students.\n " +
-            "Parameters: the metric to be sorted (performance or urgency), " +
-            "and the desired threshold value (0 to 100)\n" +
-            "For example, 'filter performance 40' is a command you can type";
+    public static final String ERROR_MESSAGE = "Invalid command format! \n"
+            + "filter: Filters all address book students.\n "
+            + "Parameters: the metric to be sorted (performance or urgency), "
+            + "and the desired threshold value (0 to 100)\n"
+            + "For example, 'filter performance 40' is a command you can type";
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
     @Test
     public void check_attributes() throws CommandException {
@@ -41,14 +40,16 @@ public class FilterCommandParserTest {
     public void execute() {
         FilterCommandParser filterParser = new FilterCommandParser();
 
-        assertParseFailure(filterParser, "sda", errorMessage);
-        assertParseFailure(filterParser, "filter all performance 40", errorMessage);
-        assertParseFailure(filterParser, "filter performance 101", errorMessage);
-        assertParseFailure(filterParser, "filter performance 40.0", errorMessage);
-        assertParseFailure(filterParser, "filter performance -1", errorMessage);
-        assertParseFailure(filterParser, "filter performance 40.5", errorMessage);
-        assertParseFailure(filterParser, "filter performanc 40", errorMessage);
-        assertParseFailure(filterParser, "filter name 40", errorMessage);
+        assertParseFailure(filterParser, "sda", ERROR_MESSAGE);
+        assertParseFailure(filterParser, "filter all performance 40", ERROR_MESSAGE);
+        assertParseFailure(filterParser, "filter performance 101", ERROR_MESSAGE);
+        assertParseFailure(filterParser, "filter performance 40.0", ERROR_MESSAGE);
+        assertParseFailure(filterParser, "filter performance -1", ERROR_MESSAGE);
+        assertParseFailure(filterParser, "filter performance 40.5", ERROR_MESSAGE);
+        assertParseFailure(filterParser, "filter performanc 40", ERROR_MESSAGE);
+        assertParseFailure(filterParser, "filter name 40", ERROR_MESSAGE);
+        assertParseFailure(filterParser, "filter 40", ERROR_MESSAGE);
+        assertParseFailure(filterParser, "filter 40 performance", ERROR_MESSAGE);
 
         FilterCommand filterCommand = new FilterCommand("performance", 40);
         assertParseSuccess(filterParser, "filter performance 40", filterCommand);
