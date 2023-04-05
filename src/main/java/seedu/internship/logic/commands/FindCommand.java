d0670473311ff8159f2bca6d340c5435751ba166
sarthak181 <0108sarthak@gmail.com>
2023-04-05 23:47:50 +0800

    Fix FindCommand

--- src/main/java/seedu/internship/logic/commands/FindCommand.java
+++ src/main/java/seedu/internship/logic/commands/FindCommand.java
@@ -61,7 +61,7 @@ public class FindCommand extends Command {
 
         if (filterInternshipDescriptor.getTags().isPresent()) {
             filterStat = x -> filterInternshipDescriptor.getTags().get().isEmpty()
-                    ? false
+                    ? x.getTags().isEmpty()
                     :x.getTags().containsAll(filterInternshipDescriptor.getTags().get());
         }
         Predicate<Internship> finalFilterPos = filterPos;
