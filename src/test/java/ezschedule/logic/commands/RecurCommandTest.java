1401fbb0cdbc8b0464a144a875bb9d1b1a172c52
anthonynmh <anthonyneomh@gmail.com>
2023-04-05 16:24:11 +0800

    Add RecurCommandTest

--- src/test/java/ezschedule/logic/commands/RecurCommandTest.java
+++ src/test/java/ezschedule/logic/commands/RecurCommandTest.java
@@ -1,170 +1,245 @@
 package ezschedule.logic.commands;
 
-import static ezschedule.commons.core.Messages.MESSAGE_INVALID_EVENT_DISPLAYED_INDEX;
-import static ezschedule.logic.commands.CommandTestUtil.*;
+import static ezschedule.logic.commands.CommandTestUtil.VALID_DATE_A;
+import static ezschedule.logic.commands.CommandTestUtil.VALID_DATE_B;
+import static ezschedule.logic.commands.CommandTestUtil.VALID_RECUR_FACTOR_DAY;
+import static ezschedule.logic.commands.CommandTestUtil.VALID_RECUR_FACTOR_MONTH;
+import static ezschedule.logic.commands.CommandTestUtil.VALID_RECUR_FACTOR_WEEK;
+import static ezschedule.logic.commands.CommandTestUtil.assertCommandFailure;
+import static ezschedule.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static ezschedule.testutil.TypicalEvents.getTypicalScheduler;
 import static ezschedule.testutil.TypicalIndexes.INDEX_FIRST_EVENT;
 import static ezschedule.testutil.TypicalIndexes.INDEX_SECOND_EVENT;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import ezschedule.model.event.Date;
-import ezschedule.model.event.RecurFactor;
 import org.junit.jupiter.api.Test;
 
-import ezschedule.commons.core.Messages;
 import ezschedule.commons.core.index.Index;
-import ezschedule.logic.commands.EditCommand.EditEventDescriptor;
 import ezschedule.model.Model;
 import ezschedule.model.ModelManager;
 import ezschedule.model.Scheduler;
 import ezschedule.model.UserPrefs;
+import ezschedule.model.event.Date;
 import ezschedule.model.event.Event;
-import ezschedule.testutil.EditEventDescriptorBuilder;
-import ezschedule.testutil.EventBuilder;
+import ezschedule.model.event.RecurFactor;
 
 public class RecurCommandTest {
 
     private final Model model = new ModelManager(getTypicalScheduler(), new UserPrefs());
 
     @Test
-    public void execute_allFieldsSpecified_success() {
+    public void execute_withRecurFactorDay_success() {
         Index targetIndex = INDEX_FIRST_EVENT;
-        Date endDate = new Date(VALID_DATE_B);
-        RecurFactor rf = new RecurFactor(VALID_RECUR_FACTOR_MONTH);
+        Date endDate = new Date("2023-05-15"); // recur from 2023-05-01 till 2023-05-15
+        RecurFactor rf = new RecurFactor(VALID_RECUR_FACTOR_DAY);
+        RecurCommand recurCommand = new RecurCommand(targetIndex, endDate, rf);
 
         Event eventToRecur = model.getFilteredEventList().get(0);
-
-        RecurCommand recurCommand = new RecurCommand(targetIndex, endDate, rf);
         String expectedMessage = String.format(RecurCommand.MESSAGE_SUCCESS, eventToRecur);
         Model expectedModel = new ModelManager(new Scheduler(model.getScheduler()), new UserPrefs());
 
-        Date newDate = new Date(eventToRecur.getDate().date.plusMonths(1).toString());
-        Event nextEventToRecur =
-                new Event(eventToRecur.getName(), newDate,
-                        eventToRecur.getStartTime(), eventToRecur.getEndTime());
+        // 14 days to recur
+        for (int i = 0; i < 14; i++) {
+            Date newDate = new Date(eventToRecur.getDate().date.plusDays(1).toString());
+            eventToRecur =
+                    new Event(eventToRecur.getName(), newDate,
+                            eventToRecur.getStartTime(), eventToRecur.getEndTime());
 
-        expectedModel.addEvent(nextEventToRecur);
+            expectedModel.addEvent(eventToRecur);
+        }
 
         assertCommandSuccess(recurCommand, model, expectedMessage, expectedModel);
     }
 
-    // here
-
     @Test
-    public void execute_someFieldsSpecifiedUnfilteredList_success() {
-        Index indexLastEvent = Index.fromOneBased(model.getFilteredEventList().size());
-        Event lastEvent = model.getFilteredEventList().get(indexLastEvent.getZeroBased());
+    public void execute_withRecurFactorWeek_success() {
+        Index targetIndex = INDEX_FIRST_EVENT;
+        Date endDate = new Date("2023-12-01"); // recur from 2023-05-01 till 2023-12-01
+        RecurFactor rf = new RecurFactor(VALID_RECUR_FACTOR_WEEK);
+        RecurCommand recurCommand = new RecurCommand(targetIndex, endDate, rf);
 
-        EventBuilder eventInList = new EventBuilder(lastEvent);
-        Event editedEvent = eventInList.withName(VALID_NAME_B).withDate(VALID_DATE_B).build();
-
-        EditEventDescriptor descriptor = new EditEventDescriptorBuilder()
-                .withName(VALID_NAME_B).withDate(VALID_DATE_B).build();
-        EditCommand editCommand = new EditCommand(indexLastEvent, descriptor);
-
-        String expectedMessage = String.format(EditCommand.MESSAGE_EDIT_EVENT_SUCCESS, editedEvent);
-
-        Model expectedModel = new ModelManager(new Scheduler(model.getScheduler()), new UserPrefs());
-        expectedModel.setEvent(lastEvent, editedEvent);
-
-        assertCommandSuccess(editCommand, model, expectedMessage, expectedModel);
-    }
-
-    @Test
-    public void execute_noFieldSpecifiedUnfilteredList_success() {
-        EditCommand editCommand = new EditCommand(INDEX_FIRST_EVENT, new EditEventDescriptor());
-        Event editedEvent = model.getFilteredEventList().get(INDEX_FIRST_EVENT.getZeroBased());
-
-        String expectedMessage = String.format(EditCommand.MESSAGE_EDIT_EVENT_SUCCESS, editedEvent);
-
+        Event eventToRecur = model.getFilteredEventList().get(0);
+        String expectedMessage = String.format(RecurCommand.MESSAGE_SUCCESS, eventToRecur);
         Model expectedModel = new ModelManager(new Scheduler(model.getScheduler()), new UserPrefs());
 
-        assertCommandSuccess(editCommand, model, expectedMessage, expectedModel);
+        // 30 weeks to recur
+        for (int i = 0; i < 30; i++) {
+            Date newDate = new Date(eventToRecur.getDate().date.plusWeeks(1).toString());
+            eventToRecur =
+                    new Event(eventToRecur.getName(), newDate,
+                            eventToRecur.getStartTime(), eventToRecur.getEndTime());
+
+            expectedModel.addEvent(eventToRecur);
+        }
+
+        assertCommandSuccess(recurCommand, model, expectedMessage, expectedModel);
     }
 
     @Test
-    public void execute_filteredList_success() {
-        showEventAtIndex(model, INDEX_FIRST_EVENT);
+    public void execute_withRecurFactorMonth_success() {
+        Index targetIndex = INDEX_FIRST_EVENT;
+        Date endDate = new Date("2023-06-01"); // recur from 2023-05-01 till 2023-06-01
+        RecurFactor rf = new RecurFactor(VALID_RECUR_FACTOR_MONTH);
+        RecurCommand recurCommand = new RecurCommand(targetIndex, endDate, rf);
 
-        Event eventInFilteredList = model.getFilteredEventList().get(INDEX_FIRST_EVENT.getZeroBased());
-        Event editedEvent = new EventBuilder(eventInFilteredList).withName(VALID_NAME_B).build();
-        EditCommand editCommand = new EditCommand(INDEX_FIRST_EVENT,
-                new EditEventDescriptorBuilder().withName(VALID_NAME_B).build());
+        Event eventToRecur = model.getFilteredEventList().get(0);
+        String expectedMessage = String.format(RecurCommand.MESSAGE_SUCCESS, eventToRecur);
+        Model expectedModel = new ModelManager(new Scheduler(model.getScheduler()), new UserPrefs());
 
-        String expectedMessage = String.format(EditCommand.MESSAGE_EDIT_EVENT_SUCCESS, editedEvent);
+        // 1 month to recur
+        for (int i = 0; i < 1; i++) {
+            Date newDate = new Date(eventToRecur.getDate().date.plusMonths(1).toString());
+            eventToRecur =
+                    new Event(eventToRecur.getName(), newDate,
+                            eventToRecur.getStartTime(), eventToRecur.getEndTime());
+
+            expectedModel.addEvent(eventToRecur);
+        }
+
+        assertCommandSuccess(recurCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_withRecurFactorDayExceedLimit_failure() {
+
+        int maxDaysInMonth = 30;
+
+        Index targetIndex = INDEX_FIRST_EVENT;
+        Date endDate = new Date("2023-06-01"); // recur from 2023-05-01 till 2023-06-01
+        RecurFactor rf = new RecurFactor(VALID_RECUR_FACTOR_DAY);
+        RecurCommand recurCommand = new RecurCommand(targetIndex, endDate, rf);
+
+        Event eventToRecur = model.getFilteredEventList().get(0);
 
         Model expectedModel = new ModelManager(new Scheduler(model.getScheduler()), new UserPrefs());
-        expectedModel.setEvent(model.getFilteredEventList().get(0), editedEvent);
 
-        assertCommandSuccess(editCommand, model, expectedMessage, expectedModel);
+        // 31 days to recur
+        for (int i = 0; i < 31; i++) {
+            Date newDate = new Date(eventToRecur.getDate().date.plusDays(1).toString());
+            eventToRecur =
+                    new Event(eventToRecur.getName(), newDate,
+                            eventToRecur.getStartTime(), eventToRecur.getEndTime());
+
+            expectedModel.addEvent(eventToRecur);
+        }
+
+        assertCommandFailure(recurCommand, model, String.format(
+                RecurCommand.MESSAGE_RECUR_FACTOR_CAP, rf, maxDaysInMonth, rf));
     }
 
     @Test
-    public void execute_duplicateEventUnfilteredList_failure() {
-        Event firstEvent = model.getFilteredEventList().get(INDEX_FIRST_EVENT.getZeroBased());
-        EditEventDescriptor descriptor = new EditEventDescriptorBuilder(firstEvent).build();
-        EditCommand editCommand = new EditCommand(INDEX_SECOND_EVENT, descriptor);
+    public void execute_withRecurFactorWeekExceedLimit_failure() {
 
-        assertCommandFailure(editCommand, model, EditCommand.MESSAGE_DUPLICATE_EVENT);
+        int maxWeeksInYear = 52;
+
+        Index targetIndex = INDEX_FIRST_EVENT;
+        Date endDate = new Date("2024-06-01"); // recur from 2023-05-01 till 2024-06-01
+        RecurFactor rf = new RecurFactor(VALID_RECUR_FACTOR_WEEK);
+        RecurCommand recurCommand = new RecurCommand(targetIndex, endDate, rf);
+
+        Event eventToRecur = model.getFilteredEventList().get(0);
+
+        Model expectedModel = new ModelManager(new Scheduler(model.getScheduler()), new UserPrefs());
+
+        // 56 weeks to recur
+        for (int i = 0; i < 56; i++) {
+            Date newDate = new Date(eventToRecur.getDate().date.plusWeeks(1).toString());
+            eventToRecur =
+                    new Event(eventToRecur.getName(), newDate,
+                            eventToRecur.getStartTime(), eventToRecur.getEndTime());
+
+            expectedModel.addEvent(eventToRecur);
+        }
+
+        assertCommandFailure(recurCommand, model, String.format(
+                RecurCommand.MESSAGE_RECUR_FACTOR_CAP, rf, maxWeeksInYear, rf));
     }
 
     @Test
-    public void execute_duplicateEventFilteredList_failure() {
-        showEventAtIndex(model, INDEX_FIRST_EVENT);
+    public void execute_withRecurFactorMonthExceedLimit_failure() {
 
-        // edit event in filtered list into a duplicate in scheduler
-        Event eventInList = model.getScheduler().getEventList().get(INDEX_SECOND_EVENT.getZeroBased());
-        EditCommand editCommand = new EditCommand(INDEX_FIRST_EVENT,
-                new EditEventDescriptorBuilder(eventInList).build());
+        int maxMonthInYear = 12;
 
-        assertCommandFailure(editCommand, model, EditCommand.MESSAGE_DUPLICATE_EVENT);
+        Index targetIndex = INDEX_FIRST_EVENT;
+        Date endDate = new Date("2024-06-01"); // recur from 2023-05-01 till 2024-06-01
+        RecurFactor rf = new RecurFactor(VALID_RECUR_FACTOR_MONTH);
+        RecurCommand recurCommand = new RecurCommand(targetIndex, endDate, rf);
+
+        Event eventToRecur = model.getFilteredEventList().get(0);
+
+        Model expectedModel = new ModelManager(new Scheduler(model.getScheduler()), new UserPrefs());
+
+        // 13 month to recur
+        for (int i = 0; i < 13; i++) {
+            Date newDate = new Date(eventToRecur.getDate().date.plusMonths(1).toString());
+            eventToRecur =
+                    new Event(eventToRecur.getName(), newDate,
+                            eventToRecur.getStartTime(), eventToRecur.getEndTime());
+
+            expectedModel.addEvent(eventToRecur);
+        }
+
+        assertCommandFailure(recurCommand, model, String.format(
+                RecurCommand.MESSAGE_RECUR_FACTOR_CAP, rf, maxMonthInYear, rf));
     }
 
     @Test
-    public void execute_eventEndTimeEarlierThanStartTime_failure() {
-        EditCommand editCommand = new EditCommand(INDEX_FIRST_EVENT,
-                new EditEventDescriptorBuilder().withStartTime("11:00").withEndTime("00:00").build());
+    public void execute_validIntToStringMonths_success() {
+        Index targetIndex = INDEX_FIRST_EVENT;
+        Date endDate = new Date("2023-06-01"); // recur from 2023-05-01 till 2023-06-01
+        RecurFactor rf = new RecurFactor(VALID_RECUR_FACTOR_MONTH);
+        RecurCommand recurCommand = new RecurCommand(targetIndex, endDate, rf);
 
-        assertCommandFailure(editCommand, model, Messages.MESSAGE_EVENT_END_TIME_EARLIER_THAN_START_TIME);
+        // valid months [1, 12]
+        String expectedMonthJan = "January";
+        String actualMonthJan = recurCommand.intToStringMonth(1);
+
+        String expectedMonthJun = "June";
+        String actualMonthJun = recurCommand.intToStringMonth(6);
+
+        String expectedMonthDec = "December";
+        String actualMonthDec = recurCommand.intToStringMonth(12);
+
+        assertEquals(expectedMonthJan, actualMonthJan);
+        assertEquals(expectedMonthJun, actualMonthJun);
+        assertEquals(expectedMonthDec, actualMonthDec);
     }
 
     @Test
-    public void execute_invalidEventIndexUnfilteredList_failure() {
-        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredEventList().size() + 1);
-        EditEventDescriptor descriptor = new EditEventDescriptorBuilder().withName(VALID_NAME_B).build();
-        EditCommand editCommand = new EditCommand(outOfBoundIndex, descriptor);
+    public void execute_invalidIntToStringMonths_success() {
+        Index targetIndex = INDEX_FIRST_EVENT;
+        Date endDate = new Date("2023-06-01"); // recur from 2023-05-01 till 2023-06-01
+        RecurFactor rf = new RecurFactor(VALID_RECUR_FACTOR_MONTH);
+        RecurCommand recurCommand = new RecurCommand(targetIndex, endDate, rf);
 
-        assertCommandFailure(editCommand, model, String.format(
-                MESSAGE_INVALID_EVENT_DISPLAYED_INDEX, model.getFilteredEventList().size() + 1));
-    }
+        String invalidMessage = "No such month";
 
-    /**
-     * Edit filtered list where index is larger than size of filtered list,
-     * but smaller than size of scheduler
-     */
-    @Test
-    public void execute_invalidEventIndexFilteredList_failure() {
-        showEventAtIndex(model, INDEX_FIRST_EVENT);
-        Index outOfBoundIndex = INDEX_SECOND_EVENT;
-        // ensures that outOfBoundIndex is still in bounds of scheduler list
-        assertTrue(outOfBoundIndex.getZeroBased() < model.getScheduler().getEventList().size());
+        // invalid months [..., 0]
+        String actualMonthZero = recurCommand.intToStringMonth(0);
+        String actualMonthNegative = recurCommand.intToStringMonth(-10);
 
-        EditCommand editCommand = new EditCommand(outOfBoundIndex,
-                new EditEventDescriptorBuilder().withName(VALID_NAME_B).build());
+        // invalid months [13, ...]
+        String actualMonthThirteen = recurCommand.intToStringMonth(13);
+        String actualMonthHundred = recurCommand.intToStringMonth(100);
 
-        assertCommandFailure(editCommand, model, String.format(
-                MESSAGE_INVALID_EVENT_DISPLAYED_INDEX, outOfBoundIndex.getZeroBased() + 1));
+
+        assertEquals(invalidMessage, actualMonthZero);
+        assertEquals(invalidMessage, actualMonthNegative);
+        assertEquals(invalidMessage, actualMonthThirteen);
+        assertEquals(invalidMessage, actualMonthHundred);
     }
 
     @Test
-    public void equals() {
-        final EditCommand standardCommand = new EditCommand(INDEX_FIRST_EVENT, DESC_A);
+    public void equals_success() {
+        final RecurCommand standardCommand =
+                new RecurCommand(INDEX_FIRST_EVENT, new Date(VALID_DATE_A),
+                        new RecurFactor(VALID_RECUR_FACTOR_DAY));
 
         // same values -> returns true
-        EditEventDescriptor copyDescriptor = new EditEventDescriptor(DESC_A);
-        EditCommand commandWithSameValues = new EditCommand(INDEX_FIRST_EVENT, copyDescriptor);
+        RecurCommand commandWithSameValues =
+                new RecurCommand(INDEX_FIRST_EVENT, standardCommand.getEndDate(),
+                        standardCommand.getFactor());
         assertEquals(standardCommand, commandWithSameValues);
 
         // same object -> returns true
@@ -177,10 +252,18 @@ public class RecurCommandTest {
         assertNotEquals(standardCommand, new ClearCommand());
 
         // different index -> returns false
-        assertNotEquals(standardCommand, new EditCommand(INDEX_SECOND_EVENT, DESC_A));
+        assertNotEquals(standardCommand,
+                new RecurCommand(INDEX_SECOND_EVENT, new Date(VALID_DATE_A),
+                        new RecurFactor(VALID_RECUR_FACTOR_DAY)));
 
-        // different descriptor -> returns false
-        assertNotEquals(standardCommand, new EditCommand(INDEX_FIRST_EVENT, DESC_B));
+        // different date -> returns false
+        assertNotEquals(standardCommand,
+                new RecurCommand(INDEX_FIRST_EVENT, new Date(VALID_DATE_B),
+                        new RecurFactor(VALID_RECUR_FACTOR_DAY)));
+
+        // different factor -> returns false
+        assertNotEquals(standardCommand,
+                new RecurCommand(INDEX_FIRST_EVENT, new Date(VALID_DATE_A),
+                        new RecurFactor(VALID_RECUR_FACTOR_MONTH)));
     }
-
 }
