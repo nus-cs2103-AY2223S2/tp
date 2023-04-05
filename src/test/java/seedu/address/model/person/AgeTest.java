be098993230e3f1915f81641f53db98892ca83cb
MrTwit99 <e0726852@u.nus.edu>
2023-04-05 18:40:29 +0800

    Fix checkstyle error

--- src/test/java/seedu/address/model/person/AgeTest.java
+++ src/test/java/seedu/address/model/person/AgeTest.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
-import seedu.address.model.person.parent.Relationship;
 
 public class AgeTest {
     private String validAge1 = "0";
