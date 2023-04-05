d0b5d7db23b4148d781abf168e715fa5911373f3
evanpy <evanleewj@gmail.com>
2023-04-06 04:13:39 +0800

    Replace lead term with contact in messages

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -7,9 +7,9 @@ public class Messages {
 
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
-    public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The lead's index provided is invalid";
-    public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d leads listed!";
-    public static final String MESSAGE_PERSONS_SORTED_OVERVIEW = "Leads sorted by %1$s";
+    public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The contact's index provided is invalid";
+    public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d contacts listed!";
+    public static final String MESSAGE_PERSONS_SORTED_OVERVIEW = "Contact list sorted by %1$s";
 
     public static final String MESSAGE_INVALID_TASK_DISPLAYED_INDEX = "The task's index provided is invalid";
 
