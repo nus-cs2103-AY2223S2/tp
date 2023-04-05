2f5edfb54de31f035ecf587fea9e98b4692bf579
hansstanley <nusstuhss@gmail.com>
2023-04-05 18:09:01 +0800

    Add tests for event fields

---
+++ src/test/java/seedu/address/model/event/enums/IntervalTest.java
@@ -0,0 +1,27 @@
+package seedu.address.model.event.enums;
+
+import org.junit.jupiter.api.Test;
+import seedu.address.model.event.fields.Recurrence;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class IntervalTest {
+
+    @Test
+    void getValue() {
+        assertEquals(Recurrence.NONE_CASE, Interval.NONE.getValue());
+        assertEquals(Recurrence.DAILY_CASE, Interval.DAILY.getValue());
+        assertEquals(Recurrence.WEEKLY_CASE, Interval.WEEKLY.getValue());
+        assertEquals(Recurrence.MONTHLY_CASE, Interval.MONTHLY.getValue());
+        assertEquals(Recurrence.YEARLY_CASE, Interval.YEARLY.getValue());
+    }
+
+    @Test
+    void testToString() {
+        assertEquals("one-time", Interval.NONE.toString());
+        assertEquals(Recurrence.DAILY_CASE, Interval.DAILY.toString());
+        assertEquals(Recurrence.WEEKLY_CASE, Interval.WEEKLY.toString());
+        assertEquals(Recurrence.MONTHLY_CASE, Interval.MONTHLY.toString());
+        assertEquals(Recurrence.YEARLY_CASE, Interval.YEARLY.toString());
+    }
+}
