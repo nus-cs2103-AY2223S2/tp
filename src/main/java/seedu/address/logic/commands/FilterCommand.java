7c44ded013b2dfa6dfdb1e8dd40c124ba93ef756
Eric Leow <ericleowyuquan@gmail.com>
2023-04-05 19:35:19 +0800

    Fix instructions for filter command

--- src/main/java/seedu/address/logic/commands/FilterCommand.java
+++ src/main/java/seedu/address/logic/commands/FilterCommand.java
@@ -15,10 +15,9 @@ public class FilterCommand extends Command {
     public static final String MESSAGE_SUCCESS = "Filter students accordingly.";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Filters all address book students.\n "
-            + "Parameters: The group of students you wish to sort (either all, lab, tutorial or consultation), "
-            + "the metric to be sorted (performance or urgency), "
+            + "Parameters: the metric to be sorted (performance or urgency), "
             + "and the desired threshold value (0 to 100)\n"
-            + "For example, 'filter all performance 40' is a command you can type";
+            + "For example, 'filter performance 40' is a command you can type";
 
     private static String metric;
     private static int threshold;
@@ -53,22 +52,6 @@ public class FilterCommand extends Command {
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
-        /*
-        switch (this.group) {
-        case "all":
-            model.updateSortAllPersonList(this.metric, this.increasingOrder);
-            break;
-        case "lab":
-            model.updateSortLabPersonList(this.metric, this.increasingOrder);
-            break;
-        case "tutorial":
-            model.updateSortTutorialPersonList(this.metric, this.increasingOrder);
-            break;
-        case "consultation":
-            model.updateSortConsultationPersonList(this.metric, this.increasingOrder);
-            break;
-        }
-         */
 
         model.updateFilteredPersonList((person) -> filterMetric(person));
         return new CommandResult(MESSAGE_SUCCESS);
