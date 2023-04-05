fb0af24f1521b45375c0ce123b701c3814bbb40e
Xiuxuan Wang <36021591+wxxedu@users.noreply.github.com>
2023-04-05 14:52:07 +0800

    fix #46

--- src/main/java/wingman/logic/core/exceptions/ParseException.java
+++ src/main/java/wingman/logic/core/exceptions/ParseException.java
@@ -1,5 +1,7 @@
 package wingman.logic.core.exceptions;
 
+import java.util.Objects;
+
 import wingman.commons.exceptions.IllegalValueException;
 
 /**
@@ -13,5 +15,12 @@ public class ParseException extends IllegalValueException {
 
     public ParseException(String message, Throwable cause) {
         super(message, cause);
+    }
+
+    public static ParseException formatted(String message, Object... objects) {
+        return new ParseException(String.format(
+                message,
+                objects
+        ));
     }
 }
--- src/test/java/wingman/logic/core/CommandParamTest.java
+++ src/test/java/wingman/logic/core/CommandParamTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static wingman.testutil.Assert.assertThrows;
 
 import java.util.ArrayDeque;
 import java.util.Arrays;
@@ -17,6 +18,7 @@ import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import wingman.logic.core.exceptions.CommandException;
 import wingman.logic.core.exceptions.ParseException;
 import wingman.testutil.Assert;
 
@@ -30,7 +32,12 @@ public class CommandParamTest {
     private static final String PREFIX_C = "/c";
     private static final String PREFIX_D = "/d";
 
-    private final Set<String> prefixes = Set.of(PREFIX_A, PREFIX_B, PREFIX_C, PREFIX_D);
+    private final Set<String> prefixes = Set.of(
+            PREFIX_A,
+            PREFIX_B,
+            PREFIX_C,
+            PREFIX_D
+    );
 
     private Deque<String> tokens;
 
@@ -40,7 +47,7 @@ public class CommandParamTest {
     }
 
     @Test
-    void from_emptyTokenEmptyPrefixes_returnsEmpty() {
+    void from_emptyTokenEmptyPrefixes_returnsEmpty() throws ParseException {
         // execute
         CommandParam param = CommandParam.from(tokens, Optional.empty());
 
@@ -50,7 +57,7 @@ public class CommandParamTest {
     }
 
     @Test
-    void from_emptyTokenNonEmptyPrefixes_returnsEmpty() {
+    void from_emptyTokenNonEmptyPrefixes_returnsEmpty() throws ParseException {
         // execute
         CommandParam param = CommandParam.from(tokens, Optional.of(prefixes));
 
@@ -60,7 +67,16 @@ public class CommandParamTest {
     }
 
     @Test
-    void from_nonEmptyTokenEmptyPrefixes_returnsUnnamedValue() {
+    void from_duplicatedKey_throwsParseException() {
+        tokens.addAll(List.of(PREFIX_A, "Hello", PREFIX_A, "World", PREFIX_A));
+        assertThrows(ParseException.class, () -> CommandParam.from(
+                tokens,
+                Optional.of(prefixes)
+        ));
+    }
+
+    @Test
+    void from_nonEmptyTokenEmptyPrefixes_returnsUnnamedValue() throws ParseException {
         // setup
         tokens.addAll(List.of("token", "token2"));
 
@@ -74,17 +90,20 @@ public class CommandParamTest {
     }
 
     @Test
-    void from_nonEmptyTokenNonEmptyPrefixes_returnsNamedValues() {
+    void from_nonEmptyTokenNonEmptyPrefixes_returnsNamedValues() throws ParseException {
         // setup
         tokens.addAll(List.of("token1", "token2", PREFIX_A, "value1", "value2",
-            PREFIX_B, "value2", PREFIX_C, "value3"));
+                PREFIX_B, "value2", PREFIX_C, "value3"
+        ));
         // execute
         CommandParam param = CommandParam.from(tokens, Optional.of(prefixes));
         // verify
         assertTrue(param.getUnnamedValue().isPresent());
         assertEquals("token1 token2", param.getUnnamedValue().get());
         assertTrue(param.getNamedValues().isPresent());
-        Map<String, Optional<String>> namedValues = param.getNamedValues().get();
+        Map<String, Optional<String>> namedValues = param
+                                                            .getNamedValues()
+                                                            .get();
         assertEquals(4, namedValues.size());
         for (String prefix : prefixes) {
             assertTrue(namedValues.containsKey(prefix));
@@ -96,97 +115,12 @@ public class CommandParamTest {
     }
 
     @Test
-    void parseUnnamedValue_emptyToken_returnEmptyResult() {
-        // execute
-        Optional<String> result = CommandParam.parseUnnamedValue(tokens, prefixes);
-
-        // verify
-        assertFalse(result.isPresent());
-    }
-
-    @Test
-    void parseUnnamedValue_nonEmptyTokenNoPrefix_returnFullString() {
-        // setup
-        final String expected = "hello world how are you";
-        tokens.addAll(Arrays.asList(expected.split(" ")));
-
-        // execute
-        Optional<String> result = CommandParam.parseUnnamedValue(tokens, prefixes);
-
-        // verify
-        assertTrue(result.isPresent());
-        assertEquals(expected, result.get());
-    }
-
-    @Test
-    void parseUnnamedValue_nonEmptyTokenWithPrefix_returnStringBeforePrefix() {
-        // setup
-        final String expected = "hello world how are you";
-        tokens.addAll(Arrays.asList(expected.split(" ")));
-        tokens.addAll(List.of(PREFIX_A, "value1", "value2"));
-
-        // execute
-        Optional<String> result = CommandParam.parseUnnamedValue(tokens, prefixes);
-
-        // verify
-        assertTrue(result.isPresent());
-        assertEquals(expected, result.get());
-    }
-
-    @Test
-    void parseNamedValues_emptyTokenNonEmptyPrefixes_returnResultOfSizePrefixes() {
-        // execute
-        Map<String, Optional<String>> result = CommandParam.parseNamedValues(tokens, prefixes);
-
-        // verify
-        assertFalse(result.isEmpty());
-        assertEquals(prefixes.size(), result.size());
-        for (String prefix : prefixes) {
-            assertTrue(result.containsKey(prefix));
-            assertFalse(result.get(prefix).isPresent());
-        }
-    }
-
-    @Test
-    void parseNamedValues_nonEmptyTokenNonEmptyPrefixes_returnResultOfSizePrefixes() {
-        // setup
-        tokens.addAll(List.of(PREFIX_A, "value1", "value2", PREFIX_B, "value2", PREFIX_C, "value3"));
-        // execute
-        Map<String, Optional<String>> result = CommandParam.parseNamedValues(tokens, prefixes);
-        // verify
-        assertFalse(result.isEmpty());
-        assertEquals(prefixes.size(), result.size());
-        for (String prefix : prefixes) {
-            assertTrue(result.containsKey(prefix));
-        }
-        assertEquals(Optional.of("value1 value2"), result.get(PREFIX_A));
-        assertEquals(Optional.of("value2"), result.get(PREFIX_B));
-        assertEquals(Optional.of("value3"), result.get(PREFIX_C));
-        assertEquals(Optional.empty(), result.get(PREFIX_D));
-    }
-
-    @Test
-    void parseNamedValues_unnamedNotRemoved_shouldRemoveUnnamedAndReturnCorrectValues() {
-        tokens.addAll(List.of("hello", "world", PREFIX_A, "value1", "value2", PREFIX_B, "value2", PREFIX_C, "value3"));
-        // execute
-        Map<String, Optional<String>> result = CommandParam.parseNamedValues(tokens, prefixes);
-
-        // verify
-        assertFalse(result.isEmpty());
-        assertEquals(prefixes.size(), result.size());
-        for (String prefix : prefixes) {
-            assertTrue(result.containsKey(prefix));
-        }
-        assertEquals(Optional.of("value1 value2"), result.get(PREFIX_A));
-        assertEquals(Optional.of("value2"), result.get(PREFIX_B));
-        assertEquals(Optional.of("value3"), result.get(PREFIX_C));
-        assertEquals(Optional.empty(), result.get(PREFIX_D));
-    }
-
-    @Test
     void getUnnamedValue_emptyToken_returnEmpty() {
         // setup
-        CommandParam param = new CommandParam(Optional.empty(), Optional.empty());
+        CommandParam param = new CommandParam(
+                Optional.empty(),
+                Optional.empty()
+        );
         // execute
         Optional<String> result = param.getUnnamedValue();
 
@@ -210,15 +144,21 @@ public class CommandParamTest {
     @Test
     void getUnnamedValueOrThrow_emptyToken_throwsException() {
         // setup
-        CommandParam param = new CommandParam(Optional.empty(), Optional.empty());
+        CommandParam param = new CommandParam(
+                Optional.empty(),
+                Optional.empty()
+        );
         // execute
-        Assert.assertThrows(ParseException.class, param::getUnnamedValueOrThrow);
+        assertThrows(ParseException.class, param::getUnnamedValueOrThrow);
     }
 
     @Test
     void getNamedValues_emptyToken_returnEmpty() {
         // setup
-        CommandParam param = new CommandParam(Optional.empty(), Optional.empty());
+        CommandParam param = new CommandParam(
+                Optional.empty(),
+                Optional.empty()
+        );
         // execute
         Optional<Map<String, Optional<String>>> result = param.getNamedValues();
 
@@ -242,34 +182,54 @@ public class CommandParamTest {
     @Test
     void getNamedValuesOrThrow_emptyToken_throwsException() {
         // setup
-        CommandParam param = new CommandParam(Optional.empty(), Optional.empty());
-        CommandParam param2 = new CommandParam(Optional.of("token"),
-            Optional.of(Map.of(PREFIX_A, Optional.empty())));
+        CommandParam param = new CommandParam(
+                Optional.empty(),
+                Optional.empty()
+        );
+        CommandParam param2 = new CommandParam(
+                Optional.of("token"),
+                Optional.of(Map.of(PREFIX_A, Optional.empty()))
+        );
         // execute
-        Assert.assertThrows(ParseException.class, () -> param.getNamedValuesOrThrow(PREFIX_A));
-        Assert.assertThrows(ParseException.class, () -> param2.getNamedValuesOrThrow(PREFIX_A));
+        assertThrows(
+                ParseException.class,
+                () -> param.getNamedValuesOrThrow(PREFIX_A)
+        );
+        assertThrows(
+                ParseException.class,
+                () -> param2.getNamedValuesOrThrow(PREFIX_A)
+        );
     }
 
     @Test
     void getUnnamedIntOrThrow_emptyToken_throwsException() {
         // setup
-        CommandParam param = new CommandParam(Optional.empty(), Optional.empty());
+        CommandParam param = new CommandParam(
+                Optional.empty(),
+                Optional.empty()
+        );
         // execute
-        Assert.assertThrows(ParseException.class, param::getUnnamedIntOrThrow);
+        assertThrows(ParseException.class, param::getUnnamedIntOrThrow);
     }
 
     @Test
     void getUnnamedIntOrThrow_nonEmptyTokenNonInt_throwsException() {
         // setup
-        CommandParam param = new CommandParam(Optional.of("token"), Optional.empty());
+        CommandParam param = new CommandParam(
+                Optional.of("token"),
+                Optional.empty()
+        );
         // execute
-        Assert.assertThrows(ParseException.class, param::getUnnamedIntOrThrow);
+        assertThrows(ParseException.class, param::getUnnamedIntOrThrow);
     }
 
     @Test
     void getUnnamedIntOrThrow_nonEmptyTokenInt_returnInt() throws ParseException {
         // setup
-        CommandParam param = new CommandParam(Optional.of("1"), Optional.empty());
+        CommandParam param = new CommandParam(
+                Optional.of("1"),
+                Optional.empty()
+        );
         // execute
         int result = param.getUnnamedIntOrThrow();
         // verify
@@ -279,25 +239,38 @@ public class CommandParamTest {
     @Test
     void getNamedIntOrThrow_emptyToken_throwsException() {
         // setup
-        CommandParam param = new CommandParam(Optional.empty(), Optional.empty());
+        CommandParam param = new CommandParam(
+                Optional.empty(),
+                Optional.empty()
+        );
         // execute
-        Assert.assertThrows(ParseException.class, () -> param.getNamedIntOrThrow(PREFIX_A));
+        assertThrows(
+                ParseException.class,
+                () -> param.getNamedIntOrThrow(PREFIX_A)
+        );
     }
 
     @Test
     void getNamedIntOrThrow_nonEmptyTokenNonInt_throwsException() {
         // setup
-        CommandParam param = new CommandParam(Optional.empty(),
-            Optional.of(Map.of(PREFIX_A, Optional.of("token"))));
+        CommandParam param = new CommandParam(
+                Optional.empty(),
+                Optional.of(Map.of(PREFIX_A, Optional.of("token")))
+        );
         // execute
-        Assert.assertThrows(ParseException.class, () -> param.getNamedIntOrThrow(PREFIX_A));
+        assertThrows(
+                ParseException.class,
+                () -> param.getNamedIntOrThrow(PREFIX_A)
+        );
     }
 
     @Test
     void getNamedIntOrThrow_nonEmptyTokenInt_returnInt() throws ParseException {
         // setup
-        CommandParam param = new CommandParam(Optional.empty(),
-            Optional.of(Map.of(PREFIX_A, Optional.of("1"))));
+        CommandParam param = new CommandParam(
+                Optional.empty(),
+                Optional.of(Map.of(PREFIX_A, Optional.of("1")))
+        );
         // execute
         int result = param.getNamedIntOrThrow(PREFIX_A);
         // verify
@@ -307,8 +280,10 @@ public class CommandParamTest {
     @Test
     void equals_sameObject_returnTrue() {
         // setup
-        CommandParam param = new CommandParam(Optional.of("token"),
-            Optional.of(Map.of(PREFIX_A, Optional.of("value"))));
+        CommandParam param = new CommandParam(
+                Optional.of("token"),
+                Optional.of(Map.of(PREFIX_A, Optional.of("value")))
+        );
         // verify
         assertEquals(param, param);
     }
@@ -316,8 +291,10 @@ public class CommandParamTest {
     @Test
     void equals_nullObject_returnFalse() {
         // setup
-        CommandParam param = new CommandParam(Optional.of("token"),
-            Optional.of(Map.of(PREFIX_A, Optional.of("value"))));
+        CommandParam param = new CommandParam(
+                Optional.of("token"),
+                Optional.of(Map.of(PREFIX_A, Optional.of("value")))
+        );
         // verify
         assertNotEquals(param, null);
     }
@@ -325,8 +302,10 @@ public class CommandParamTest {
     @Test
     void equals_differentClass_returnFalse() {
         // setup
-        CommandParam param = new CommandParam(Optional.of("token"),
-            Optional.of(Map.of(PREFIX_A, Optional.of("value"))));
+        CommandParam param = new CommandParam(
+                Optional.of("token"),
+                Optional.of(Map.of(PREFIX_A, Optional.of("value")))
+        );
         // verify
         assertNotEquals(param, new Object());
     }
@@ -334,10 +313,14 @@ public class CommandParamTest {
     @Test
     void equals_sameTokenDifferentInstance_returnTrue() {
         // setup
-        CommandParam param = new CommandParam(Optional.of("token"),
-            Optional.of(Map.of(PREFIX_A, Optional.of("value"))));
-        CommandParam param2 = new CommandParam(Optional.of("token"),
-            Optional.of(Map.of(PREFIX_A, Optional.of("value"))));
+        CommandParam param = new CommandParam(
+                Optional.of("token"),
+                Optional.of(Map.of(PREFIX_A, Optional.of("value")))
+        );
+        CommandParam param2 = new CommandParam(
+                Optional.of("token"),
+                Optional.of(Map.of(PREFIX_A, Optional.of("value")))
+        );
         // verify
         assertEquals(param, param2);
     }
