3a116606e91bdc9f5509010185a03d091b03944a
pkpaing <pkpaing@gmail.com>
2023-04-06 14:30:39 +0800

    Codecov for imported contacts

---
+++ src/test/java/seedu/address/storage/ChsContactsTest.java
@@ -0,0 +1,24 @@
+package seedu.address.storage;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.storage.ChsContacts.CHS_CONTACTS;
+
+
+import seedu.address.model.person.Person;
+
+public class ChsContactsTest {
+    @Test
+    public void equals() {
+        Person chngShuSin = CHS_CONTACTS.get(0);
+        Person chewFookTim = CHS_CONTACTS.get(1);
+
+        // same object -> returns true
+        assertTrue(chngShuSin.isSamePerson(chngShuSin));
+
+        // different person -> returns false
+        assertFalse(chngShuSin.isSamePerson(chewFookTim));
+    }
+}
