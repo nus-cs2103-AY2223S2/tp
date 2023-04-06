d1f673609e86eef57e6713aa00061c2fc0d09eb1
Huang Hao <huang_hao12@hotmail.com>
2023-04-06 18:36:48 +0800

    Fix duplicate

--- src/main/java/seedu/socket/model/project/exceptions/ProjectNotFoundException 2.java
+++
@@ -1,6 +0,0 @@
-package seedu.socket.model.project.exceptions;
-
-/**
- * Signals that the operation is unable to find the specified project.
- */
-public class ProjectNotFoundException extends RuntimeException {}
