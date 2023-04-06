7b1df3295d752b6bf0ce504154133d6e0a22ba50
pzhengze <zhengze@u.nus.edu>
2023-04-06 17:28:28 +0800

    Fix broken tests

--- src/test/java/seedu/patientist/logic/commands/ExitCommandTest.java
+++ src/test/java/seedu/patientist/logic/commands/ExitCommandTest.java
@@ -14,7 +14,8 @@ public class ExitCommandTest {
 
     @Test
     public void execute_exit_success() {
-        CommandResult expectedCommandResult = new CommandResult(MESSAGE_EXIT_ACKNOWLEDGEMENT, -1, false, false, true);
+        CommandResult expectedCommandResult =
+                new CommandResult(MESSAGE_EXIT_ACKNOWLEDGEMENT, -1, 0, false, true);
         assertCommandSuccess(new ExitCommand(), model, expectedCommandResult, expectedModel);
     }
 }
