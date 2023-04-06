d1a260c0a6c8e63983e00e72b88a9809480af040
CarrieLi1015 <e0775592@u.nus.edu>
2023-04-06 16:19:10 +0800

    Add filter command parser test

---
+++ src/test/java/seedu/address/logic/parser/FilterCommandParserTest.java
@@ -0,0 +1,34 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.FilterCommand;
+import seedu.address.model.tag.TagContainsKeywordsPredicate;
+
+public class FilterCommandParserTest {
+
+    private FilterCommandParser parser = new FilterCommandParser();
+
+    @Test
+    public void parse_emptyArg_throwsParseException() {
+        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, FilterCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_validArgs_returnsFindCommand() {
+        // no leading and trailing whitespaces
+        FilterCommand expectedFilterCommand =
+                new FilterCommand(new TagContainsKeywordsPredicate(Arrays.asList("friends", "owesMoney")));
+        assertParseSuccess(parser, "friends owesMoney", expectedFilterCommand);
+
+        // multiple whitespaces between keywords
+        assertParseSuccess(parser, " \n friends \n \t owesMoney  \t", expectedFilterCommand);
+    }
+
+}
