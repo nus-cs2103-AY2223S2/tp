f9276685df4d86cae3e2ebd90abfa697717e9cd4
tituswe <tituslowesg@gmail.com>
2023-04-06 16:38:12 +0800

    Fix name length

--- src/main/java/seedu/address/model/client/Name.java
+++ src/main/java/seedu/address/model/client/Name.java
@@ -10,7 +10,8 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 public class Name {
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Names should only contain alphanumeric characters and spaces, and it should not be blank";
+            "Names should only contain alphanumeric characters and spaces, should be less than 50 characters, and it " +
+                    "should not be blank";
 
     /*
      * The first character of the address must not be a whitespace,
@@ -35,7 +36,7 @@ public class Name {
      * Returns true if a given string is a valid name.
      */
     public static boolean isValidName(String test) {
-        return test.matches(VALIDATION_REGEX);
+        return test.matches(VALIDATION_REGEX) && test.length() < 50;
     }
 
 
