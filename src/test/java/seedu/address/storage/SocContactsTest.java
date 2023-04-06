d5125ff6e186ef3dbe543c2521971dcf5180efa8
pkpaing <pkpaing@gmail.com>
2023-04-06 14:41:43 +0800

    Checkstyle

--- src/test/java/seedu/address/storage/SocContactsTest.java
+++ src/test/java/seedu/address/storage/SocContactsTest.java
@@ -2,7 +2,6 @@ package seedu.address.storage;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import static seedu.address.storage.SocContacts.SOC_CONTACTS;
 
 import org.junit.jupiter.api.Test;
