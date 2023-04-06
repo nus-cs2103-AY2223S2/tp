6ce5cb3225859f47296943c2d840d9726e71456e
SeeuSim <seeusimong@gmail.com>
2023-04-06 12:31:05 +0800

    Add CliSyntax TC

--- src/test/java/seedu/recipe/logic/parser/ArgumentTokenizerTest.java
+++ src/test/java/seedu/recipe/logic/parser/ArgumentTokenizerTest.java
@@ -2,7 +2,6 @@ package seedu.recipe.logic.parser;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
