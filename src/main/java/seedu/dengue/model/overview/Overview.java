953da5e5e91f74d45e6969125cfa5f9acbc64fc6
axmszr <e0727266@u.nus.edu>
2023-04-08 06:59:53 +0800

    Remove retired imports, methods and variables. They are no longer used since the change in OverviewDisplay's implementation.

--- src/main/java/seedu/dengue/model/overview/Overview.java
+++ src/main/java/seedu/dengue/model/overview/Overview.java
@@ -3,7 +3,6 @@ package seedu.dengue.model.overview;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -15,9 +14,6 @@ import seedu.dengue.model.person.Person;
  * The API of the Overview list.
  */
 public abstract class Overview {
-    protected static final int MAX_INDEX_LEN = 4; // Length of " 28."
-    protected static final int MAX_BIN_NAME_LENGTH = 17; // Hong Leong Garden
-    protected static final String GAP = "     ";
 
     /**
      * Returns the title {@code String} of the overview.
@@ -49,40 +45,6 @@ public abstract class Overview {
      * @param personsList The new list of persons to analyse.
      */
     public abstract void update(List<Person> personsList);
-
-    /**
-     * Returns a standardized {@code String} representation of a given bin,
-     * including information like its name and size,
-     * such that the format matches other bins in the same overview.
-     *
-     * @param bin The input bin to format into a {@code String}.
-     * @return The string representation of the bin.
-     */
-    public String makeBinFormat(DataBin bin) {
-        int maxSizeLen = String.valueOf(getAnalyst().getTotal()).length();
-        return String.format("%s" + GAP + "%" + maxSizeLen + "d",
-                padBinName(bin.getName()), bin.getSize());
-    }
-
-    protected static String makeWhitespace(int length) {
-        if (length <= 0) {
-            return "";
-        }
-
-        return Stream.iterate(" ", x -> x).limit(length).collect(Collectors.joining());
-    }
-
-    protected static String padBinName(String binName) {
-        int paddingNeeded = MAX_BIN_NAME_LENGTH - binName.length();
-        String whitespace = makeWhitespace(paddingNeeded / 2);
-        return whitespace + binName + whitespace;
-    }
-
-    private String makeBinPrefix(int x) {
-        String index = x + ".";
-        int paddingNeeded = MAX_INDEX_LEN - index.length();
-        return makeWhitespace(paddingNeeded) + index;
-    }
 
     /**
      * Returns a {@code String} of all the bins recorded in the overview.
