63e02104efcb3c7ffe3e02cd625a8e98577aada5
ginloy <liyongleow@gmail.com>
2023-04-07 14:47:17 +0800

    Use stream instead of collection

--- src/main/java/seedu/address/model/DeepCopy.java
+++ src/main/java/seedu/address/model/DeepCopy.java
@@ -2,6 +2,7 @@ package seedu.address.model;
 
 import java.util.Collection;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Any class implementing this interface can be deeply copied
@@ -15,15 +16,14 @@ public interface DeepCopy<T> {
     T copy();
 
     /**
-     * Copies a collection of objects that implement DeepCopy
+     * Copies a stream of objects that implement DeepCopy
      *
-     * @param collection Collection of objects to copy
+     * @param stream Collection of objects to copy
      * @param <T> Type of object to copy
      * @return A new collection of copied objects
      */
-    static <T> Collection<T> copyCollection(Collection<? extends DeepCopy<? extends T>> collection) {
-        return collection.stream()
-            .map(DeepCopy::copy)
-            .collect(Collectors.toList());
+    static <T> Stream<T> copyCollection(Stream<? extends DeepCopy<? extends T>> stream) {
+        return stream
+            .map(DeepCopy::copy);
     }
 }
