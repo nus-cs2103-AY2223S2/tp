760d8cd096e4cd653a921f48dc95a18843b8fe35
Liu Muchen <liumuchen1@gmail.com>
2023-04-07 10:39:44 +0800

    Simplify MenuItemNameTest

--- src/test/java/trackr/model/menu/MenuItemNameTest.java
+++ src/test/java/trackr/model/menu/MenuItemNameTest.java
@@ -1,7 +1,5 @@
 package trackr.model.menu;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static trackr.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
@@ -9,45 +7,12 @@ import org.junit.jupiter.api.Test;
 public class MenuItemNameTest {
 
     @Test
-    public void constructor_null_throwsNullPointerException() {
+    public void constructor_invalidName_throwsException() {
         assertThrows(NullPointerException.class, () -> new ItemName(null));
+
+        assertThrows(IllegalArgumentException.class, () -> new ItemName(""));
+
+        assertThrows(IllegalArgumentException.class, () -> new ItemName("*!@#"));
     }
 
-    @Test
-    public void constructor_invalidName_throwsIllegalArgumentException() {
-        String invalidName = "";
-        assertThrows(IllegalArgumentException.class, () -> new ItemName(invalidName));
-    }
-
-    @Test
-    public void isValidName() {
-        // null name
-        assertThrows(NullPointerException.class, () -> ItemName.isValidName(null));
-
-        // invalid name
-        assertFalse(ItemName.isValidName(""));
-        assertFalse(ItemName.isValidName(" "));
-        assertFalse(ItemName.isValidName("^"));
-        assertFalse(ItemName.isValidName("cookie*"));
-
-        // valid name
-        assertTrue(ItemName.isValidName("chocolate cookies"));
-        assertTrue(ItemName.isValidName("Chocolate Cookies"));
-        assertTrue(ItemName.isValidName("12345"));
-        assertTrue(ItemName.isValidName("2nd Best Burger"));
-        assertTrue(ItemName.isValidName("Maggie Goreng with egg and chicken"));
-    }
-
-    @Test
-    public void equals() {
-        ItemName name = new ItemName("Bread");
-        ItemName differentName = new ItemName("Pudding");
-
-        assertTrue(name.equals(name));
-        assertTrue(name.equals(new ItemName("Bread")));
-
-        assertFalse(name.equals(null));
-        assertFalse(name.equals(differentName));
-        assertFalse(name.equals(1));
-    }
 }
