3d9cb938a63bbc548005825c52123d9e83f1f2a8
Elijah Ku <elijahku716@gmail.com>
2023-04-07 16:34:09 +0800

    Refactor file locations

---
+++ src/test/java/seedu/address/model/RerollTest.java
@@ -0,0 +1,34 @@
+package seedu.address.model;
+
+import org.junit.jupiter.api.Test;
+
+class RerollTest {
+
+    @Test
+    void getItems() {
+    }
+
+    @Test
+    void getCharacters() {
+    }
+
+    @Test
+    void getMobs() {
+    }
+
+    @Test
+    void getAllList() {
+    }
+
+    @Test
+    void getCharacterList() {
+    }
+
+    @Test
+    void getItemList() {
+    }
+
+    @Test
+    void getMobList() {
+    }
+}
