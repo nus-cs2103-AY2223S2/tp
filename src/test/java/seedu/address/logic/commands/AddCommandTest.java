d745b27d7eb4549557a5686c7f2408ba14380090
homuntan02 <homuntan02@gmail.com>
2023-04-07 16:40:59 +0800

    fix testing

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -161,6 +161,11 @@ public class AddCommandTest {
         public boolean hasPreviousState() {
             throw new AssertionError("This method should not be called.");
         }
+
+        @Override
+        public void commitListingBook() {
+            throw new AssertionError("This method should not be called.");
+        }
     }
 
     /**
