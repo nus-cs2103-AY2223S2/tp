b3ef5596a808c9520ba0154f89318b5abec88e07
9fc70c892 <89006592+9fc70c892@users.noreply.github.com>
2023-04-08 19:12:52 +0800

    Complete DeleteCommand for Customer and check cascading

--- src/test/java/seedu/address/logic/commands/AutoM8DeleteCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AutoM8DeleteCommandTest.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 //import static org.junit.jupiter.api.Assertions.assertFalse;
 //import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.AutoM8CommandTestUtil.assertFailure;
+//import static seedu.address.logic.commands.AutoM8CommandTestUtil.assertSuccess;
+import static seedu.address.logic.commands.DeleteCustomerCommand.MESSAGE_DELETE_CUSTOMER_SUCCESS;
 
 import org.junit.jupiter.api.Test;
 
@@ -13,6 +15,7 @@ import seedu.address.model.entity.person.Customer;
 import seedu.address.model.entity.shop.exception.AppointmentNotFoundException;
 import seedu.address.model.entity.shop.exception.CustomerNotFoundException;
 import seedu.address.model.entity.shop.exception.PartNotFoundException;
+import seedu.address.model.entity.shop.exception.ServiceNotFoundException;
 import seedu.address.model.entity.shop.exception.TechnicianNotFoundException;
 import seedu.address.model.entity.shop.exception.VehicleNotFoundException;
 import seedu.address.testutil.TypicalShop;
@@ -22,14 +25,14 @@ import seedu.address.testutil.TypicalShop;
  * {@code DeleteCommand}.
  */
 public class AutoM8DeleteCommandTest {
+
+    /**
+     * Attempt to delete based on non-existing values in a typical shop
+     */
     @Test
     public void invalidValues_failure() {
-        /**
-         * Attempt to delete based on non-existing values in a typical shop
-         */
 
         Model model = new TypicalShop().getTypicalModel();
-
 
         assertFailure(new DeleteCustomerCommand(-1), model, new CustomerNotFoundException(-1));
         assertFailure(new DeleteCustomerCommand(9), model, new CustomerNotFoundException(9));
@@ -51,24 +54,53 @@ public class AutoM8DeleteCommandTest {
         /**
          * Attempt to delete customer and check for cascading.
          */
-        System.out.println("deleteCustomer_success");
 
         Model model = new TypicalShop().getTypicalModel();
 
-        Customer markForDeleteCustomer = model.getFilteredCustomerList().get(1);
-        DeleteCustomerCommand deleteCustomerCommand = new DeleteCustomerCommand(markForDeleteCustomer.getId());
-
-        CommandResult result = null;
-
         try {
-            result = deleteCustomerCommand.execute(model);
-        } catch (CommandException ce) {
-            throw new CommandException("JUnitTest: deleteCustomer_success - Failed. Message: " + ce.getMessage());
+            int id = 1;
+            Customer markForDeleteCustomer = AutoM8CommandTestUtil.getCustomer(id, model);
+            DeleteCustomerCommand deleteCustomerCommand = new DeleteCustomerCommand(markForDeleteCustomer.getId());
+            CommandResult result = deleteCustomerCommand.execute(model);
+
+            assertEquals(result, new CommandResult(String.format(MESSAGE_DELETE_CUSTOMER_SUCCESS, id), Tab.CUSTOMERS));
+
+            try {
+                AutoM8CommandTestUtil.getCustomer(id, model);
+                throw new AssertionError("Command reported customer delete success but customer exists!");
+            } catch (CustomerNotFoundException e) {
+                assertEquals(e.getMessage(), new CustomerNotFoundException(id).getMessage());
+            }
+
+            id = 2;
+            try {
+                AutoM8CommandTestUtil.getVehicle(id, model);
+                throw new AssertionError("Delete Customer command not cascading to vehicle deletes!");
+            } catch (VehicleNotFoundException e) {
+                assertEquals(e.getMessage(), new VehicleNotFoundException(id).getMessage());
+            }
+
+            id = 3;
+
+            try {
+                AutoM8CommandTestUtil.getService(id, model);
+                throw new AssertionError("Delete Customer command not cascading to service deletes!");
+            } catch (ServiceNotFoundException e) {
+                assertEquals(e.getMessage(), new ServiceNotFoundException(id).getMessage());
+            }
+
+            id = 4;
+            try {
+                AutoM8CommandTestUtil.getAppointment(id, model);
+                throw new AssertionError("Delete Customer command not cascading to service deletes!");
+            } catch (AppointmentNotFoundException e) {
+                assertEquals(e.getMessage(), new AppointmentNotFoundException(id).getMessage());
+            }
+
+        } catch (CustomerNotFoundException | CommandException e) {
+            throw new CommandException("JUnitTest: deleteCustomer_success - Failed. Message: " + e.getMessage());
         }
-
-//        System.out.println(result);
-
-//        assertEquals(result, );
-
     }
+
+
 }
