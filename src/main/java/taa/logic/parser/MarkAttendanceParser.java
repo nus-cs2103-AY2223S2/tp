4a8a03b953541762e961c79d2e39ecb47d77291b
bojie3 <xubojie2013@gmail.com>
2023-04-08 17:02:19 +0800

    Update to include test cases for mark,unmark and insert participation

--- src/main/java/taa/logic/parser/MarkAttendanceParser.java
+++ src/main/java/taa/logic/parser/MarkAttendanceParser.java
@@ -41,19 +41,4 @@ public class MarkAttendanceParser implements Parser<MarkAttendanceCommand> {
 
         return new MarkAttendanceCommand(index, Index.fromOneBased(week));
     }
-
-    /**
-     * Parses {@code Collection<String> tags} into a {@code Set<Tag>} if {@code tags} is non-empty.
-     * If {@code tags} contain only one element which is an empty string, it will be parsed into a
-     * {@code Set<Tag>} containing zero tags.
-     */
-    private Optional<Set<Tag>> parseTagsForEdit(Collection<String> tags) throws ParseException {
-        assert tags != null;
-
-        if (tags.isEmpty()) {
-            return Optional.empty();
-        }
-        Collection<String> tagSet = tags.size() == 1 && tags.contains("") ? Collections.emptySet() : tags;
-        return Optional.of(ParserUtil.parseTags(tagSet));
-    }
 }
---
+++ src/test/java/taa/logic/parser/MarkAttendanceParserTest.java
@@ -0,0 +1,14 @@
+package taa.logic.parser;
+
+import org.junit.jupiter.api.Test;
+import taa.model.student.Attendance;
+
+public class MarkAttendanceParserTest {
+    private static final String MISSING_WEEK = " 1";
+
+    @Test
+    public void parse() {
+        MarkAttendanceParser parser = new MarkAttendanceParser();
+        CommandParserTestUtil.assertParseFailure(parser, MISSING_WEEK, Attendance.WEEK_ERROR_MSG);
+    }
+}
