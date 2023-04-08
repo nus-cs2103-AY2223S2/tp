979a893de1c98e901c8d701a4cb66e101ff30151
sumhungyee <113227987+sumhungyee@users.noreply.github.com>
2023-04-08 16:27:44 +0800

    Update src/test/java/seedu/dengue/model/person/FindPredicateTest.java

--- src/test/java/seedu/dengue/model/person/FindPredicateTest.java
+++ src/test/java/seedu/dengue/model/person/FindPredicateTest.java
@@ -1,6 +1,5 @@
 package seedu.dengue.model.person;
 
-//import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
