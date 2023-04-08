035e6f8d020c157159a08a1b0aef52feeb9a8669
Tohtoroo <e0725512@u.nus.edu>
2023-04-09 03:53:56 +0800

    Make CircleCI Happy pt. 2

--- src/test/java/seedu/dengue/storage/TemporaryMemoryTest.java
+++ src/test/java/seedu/dengue/storage/TemporaryMemoryTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.ArrayList;
-import java.util.Random;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
