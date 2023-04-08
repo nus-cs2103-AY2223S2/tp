bc8eb105ef413860e99f347e3ac43768951c900b
nusE0726844 <92289387+nusE0726844@users.noreply.github.com>
2023-04-08 18:39:26 +0800

    Add test code for JsonAdaptedAppointment

---
+++ src/test/java/seedu/vms/storage/appointment/JsonAdaptedAppointmentTest.java
@@ -0,0 +1,42 @@
+package seedu.vms.storage.appointment;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import seedu.vms.commons.core.index.Index;
+import seedu.vms.commons.exceptions.IllegalValueException;
+import seedu.vms.model.GroupName;
+import seedu.vms.model.appointment.Appointment;
+
+class JsonAdaptedAppointmentTest {;
+
+    private final Index patientId = Index.fromOneBased(1);
+    private final LocalDateTime startTime = LocalDateTime.now();
+    private final LocalDateTime endTime = LocalDateTime.now().plusHours(1);
+    private final GroupName vaccine = new GroupName("Test Vaccine");
+    private final Boolean isCompleted = false;
+
+    private Appointment appointment;
+    private JsonAdaptedAppointment jsonAdaptedAppointment;
+
+    @BeforeEach
+    void setUp() {
+        appointment = new Appointment(patientId, startTime, endTime, vaccine, isCompleted);
+        jsonAdaptedAppointment = new JsonAdaptedAppointment(appointment);
+    }
+
+    @Test
+    void toModelType() throws IllegalValueException {
+        Appointment loadedAppointment = jsonAdaptedAppointment.toModelType();
+
+        assertEquals(appointment.getPatient(), loadedAppointment.getPatient());
+        assertEquals(appointment.getAppointmentTime(), loadedAppointment.getAppointmentTime());
+        assertEquals(appointment.getAppointmentEndTime(), loadedAppointment.getAppointmentEndTime());
+        assertEquals(appointment.getVaccination(), loadedAppointment.getVaccination());
+        assertEquals(appointment.getStatus(), loadedAppointment.getStatus());
+    }
+}
