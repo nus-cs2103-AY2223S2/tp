0c81ca2f8005e979930997f902da887d2932c164
Anshumaan Tyagi <anshumaantyagi0@gmail.com>
2023-04-08 23:53:40 +0800

    Add EventParser Tests

--- src/test/java/seedu/internship/logic/commands/event/EventDeleteCommandTest.java
+++ src/test/java/seedu/internship/logic/commands/event/EventDeleteCommandTest.java
@@ -1,7 +1,9 @@
 package seedu.internship.logic.commands.event;
 
+import static seedu.internship.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.internship.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.internship.logic.commands.event.EventDeleteCommand.MESSAGE_DELETE_EVENT_SUCCESS;
+import static seedu.internship.testutil.Assert.assertThrows;
 import static seedu.internship.testutil.TypicalEvents.getTypicalEventCatalogue;
 import static seedu.internship.testutil.TypicalIndexes.INDEX_FIRST_INTERNSHIP;
 import static seedu.internship.testutil.TypicalInternships.getTypicalInternshipCatalogue;
@@ -9,8 +11,11 @@ import static seedu.internship.testutil.TypicalInternships.getTypicalInternshipC
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import seedu.internship.commons.core.Messages;
+import seedu.internship.commons.core.index.Index;
 import seedu.internship.logic.commands.CommandResult;
 import seedu.internship.logic.commands.ResultType;
+import seedu.internship.logic.commands.exceptions.CommandException;
 import seedu.internship.model.Model;
 import seedu.internship.model.ModelManager;
 import seedu.internship.model.UserPrefs;
@@ -44,7 +49,26 @@ public class EventDeleteCommandTest {
         assertCommandSuccess(eventDeleteCommand, model, expectedCommandResult, expectedModel);
     }
 
+    @Test
+    public void execute_eventWithoutSelecting_throwsCommandException() {
+        EventDeleteCommand deleteCommand = new EventDeleteCommand(INDEX_FIRST_INTERNSHIP);
+        ModelManager expectedModel = new ModelManager(model.getInternshipCatalogue(), model.getEventCatalogue(),
+                new UserPrefs());
+        assertThrows(CommandException.class, EventDeleteCommand.MESSAGE_NO_INTERNSHIP_SELECTED, () ->
+                deleteCommand.execute(expectedModel));
+    }
 
+    @Test
+    public void execute_invalidIndexUnfilteredList_throwsCommandException() {
+        ModelManager expectedModel = new ModelManager(model.getInternshipCatalogue(), model.getEventCatalogue(),
+                new UserPrefs());
+        Event eventToDelete = expectedModel.getFilteredEventList().get(INDEX_FIRST_INTERNSHIP.getZeroBased());
+        expectedModel.updateSelectedInternship(eventToDelete.getInternship());
+        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredEventList().size() + 1);
+        EventDeleteCommand eventDeleteCommand = new EventDeleteCommand(outOfBoundIndex);
+
+        assertCommandFailure(eventDeleteCommand, expectedModel, Messages.MESSAGE_INVALID_EVENT_DISPLAYED_INDEX);
+    }
 
 
 }
