f45452f219f4b37fe2ad13d29b65194f8e65d7eb
Pang Rui Wei <>
2023-04-08 20:03:17 +0800

    Add test cases for Appointment

--- src/test/java/seedu/address/model/client/appointment/AppointmentTest.java
+++ src/test/java/seedu/address/model/client/appointment/AppointmentTest.java
@@ -1,15 +1,22 @@
 package seedu.address.model.client.appointment;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalClients.ALICE;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.address.model.client.policy.CustomDate;
+import seedu.address.testutil.AppointmentBuilder;
+import seedu.address.testutil.ClientBuilder;
 
 public class AppointmentTest {
 
+    private final Appointment DISCUSSION = new AppointmentBuilder().build();
+    private final Appointment EMPTY = new AppointmentBuilder().emptyBuild();
 
     @Test
     public void constructor_null_throwsNullPointerException() {
@@ -22,5 +29,46 @@ public class AppointmentTest {
         assertThrows(IllegalArgumentException.class, () -> new MeetupDate(invalidDate));
     }
 
+    @Test
+    public void equals() {
+        Appointment discCpy = new AppointmentBuilder(DISCUSSION).build();
 
+        // same values -> returns true
+        assertEquals(DISCUSSION, discCpy);
+
+        // same object - > returns true
+        assertEquals(DISCUSSION, DISCUSSION);
+
+        // null -> returns false
+        assertNotEquals(null, DISCUSSION);
+
+        // different object -> returns false
+        assertNotEquals(new ClientBuilder(ALICE).build(), DISCUSSION);
+
+        // different appointment name -> returns false
+        Appointment editedAppointment = new AppointmentBuilder(DISCUSSION).withAppointmentName("review goals").build();
+        assertNotEquals(DISCUSSION, editedAppointment);
+
+        // different meetup date -> returns false
+        editedAppointment = new AppointmentBuilder(DISCUSSION).withMeetupDate("04.04.2050").build();
+        assertNotEquals(DISCUSSION, editedAppointment);
+
+
+    }
+
+
+    @Test
+    public void testHashCode() {
+        Appointment appt = new AppointmentBuilder(DISCUSSION).build();
+        assertEquals(appt.hashCode(), DISCUSSION.hashCode());
+    }
+
+    @Test
+    public void testToString() {
+        String expectedMessage = "No appointment set";
+        assertEquals(EMPTY.toString(), expectedMessage);
+
+        expectedMessage = "Meetup discussion; Meetup Date: 01.01.2024";
+        assertEquals(DISCUSSION.toString(), expectedMessage);
+    }
 }
