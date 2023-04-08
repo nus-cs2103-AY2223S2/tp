b193523bda3b79767174453067b29337db2e6c68
mandykqh <84360725+mandykqh@users.noreply.github.com>
2023-04-08 11:53:02 +0800

    Remove unused imports

--- src/main/java/seedu/address/ui/PersonViewPanel.java
+++ src/main/java/seedu/address/ui/PersonViewPanel.java
@@ -10,7 +10,6 @@ import javafx.scene.control.ListView;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import seedu.address.model.appointment.Appointment;
-import seedu.address.model.person.Doctor;
 import seedu.address.model.person.Patient;
 import seedu.address.model.person.Person;
 import seedu.address.model.prescription.Prescription;
