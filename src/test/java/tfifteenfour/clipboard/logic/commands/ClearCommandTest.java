c0f0f7ee45179b45ad824c99622bd0af42ae8633
tyx021 <yxting1109@gmail.com>
2023-04-09 03:56:41 +0800

    add testing for clear command

--- src/test/java/tfifteenfour/clipboard/logic/commands/ClearCommandTest.java
+++ src/test/java/tfifteenfour/clipboard/logic/commands/ClearCommandTest.java
@@ -1,32 +1,56 @@
-// package tfifteenfour.clipboard.logic.commands;
+ package tfifteenfour.clipboard.logic.commands;
 
-// import static tfifteenfour.clipboard.logic.commands.CommandTestUtil.assertCommandSuccess;
-// import static tfifteenfour.clipboard.testutil.TypicalStudents.getTypicalRoster;
+ import static tfifteenfour.clipboard.logic.commands.CommandTestUtil.assertCommandSuccess;
 
-// import org.junit.jupiter.api.Test;
+ import org.junit.jupiter.api.BeforeEach;
+ import org.junit.jupiter.api.Test;
 
-// import tfifteenfour.clipboard.model.Model;
-// import tfifteenfour.clipboard.model.ModelManager;
-// import tfifteenfour.clipboard.model.Roster;
-// import tfifteenfour.clipboard.model.UserPrefs;
+ import tfifteenfour.clipboard.logic.CurrentSelection;
+ import tfifteenfour.clipboard.logic.PageType;
+ import tfifteenfour.clipboard.model.Model;
+ import tfifteenfour.clipboard.model.ModelManager;
+ import tfifteenfour.clipboard.model.Roster;
+ import tfifteenfour.clipboard.model.course.Course;
+ import tfifteenfour.clipboard.model.course.Group;
+ import tfifteenfour.clipboard.model.course.Session;
+ import tfifteenfour.clipboard.model.student.Student;
+ import tfifteenfour.clipboard.testutil.TypicalModel;
 
-// public class ClearCommandTest {
+ public class ClearCommandTest {
+     private Model model;
+     private Model expectedModel;
+     private Course selectedCourse;
+     private Group selectedGroup;
+     private Session selectedSession;
+     private Student selectedStudent;
+     private CurrentSelection actualSelection;
 
-//     @Test
-//     public void execute_emptyRoster_success() {
-//         Model model = new ModelManager();
-//         Model expectedModel = new ModelManager();
+     @BeforeEach
+     public void setUp() {
+         this.model = new TypicalModel().getTypicalModel();
+         selectedCourse = model.getCurrentSelection().getSelectedCourse();
+         selectedGroup = model.getCurrentSelection().getSelectedGroup();
+         selectedSession = model.getCurrentSelection().getSelectedSession();
+         selectedStudent = model.getCurrentSelection().getSelectedStudent();
 
-//         assertCommandSuccess(new ClearCommand(), model, ClearCommand.MESSAGE_SUCCESS, expectedModel);
-//     }
+         actualSelection = this.model.getCurrentSelection();
+         actualSelection.setCurrentPage(PageType.SESSION_STUDENT_PAGE);
+     }
 
-//     @Test
-//     public void execute_nonEmptyRoster_success() {
-//         Model model = new ModelManager(getTypicalRoster(), new UserPrefs());
-//         Model expectedModel = new ModelManager(getTypicalRoster(), new UserPrefs());
-//         expectedModel.setRoster(new Roster());
+     @Test
+     public void execute_emptyRoster_success() {
+         Model model = new ModelManager();
+         Model expectedModel = new ModelManager();
 
-//         assertCommandSuccess(new ClearCommand(), model, ClearCommand.MESSAGE_SUCCESS, expectedModel);
-//     }
+         assertCommandSuccess(new ClearCommand(), model, ClearCommand.MESSAGE_SUCCESS, expectedModel);
+     }
 
-// }
+     @Test
+     public void execute_nonEmptyRoster_success() {
+         expectedModel.setRoster(new Roster());
+         String expectedMessage = (ClearCommand.MESSAGE_SUCCESS)
+
+         assertCommandSuccess(new ClearCommand(), model, ClearCommand.MESSAGE_SUCCESS, expectedModel);
+     }
+
+ }
