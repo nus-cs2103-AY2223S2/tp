8afedf20abb952c864ebac9fc14e3ea3b98469f9
JunHao247 <onlyjunhao247@gmail.com>
2023-04-09 02:41:29 +0800

    Update BatchAddCommandTest with batchadd success

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -45,6 +45,16 @@ public class ModelManager implements Model {
         this(new ExecutiveProDb(), new UserPrefs());
     }
 
+    public ModelManager(Model model) {
+        requireAllNonNull(model);
+        userPrefs = new UserPrefs(model.getUserPrefs());
+        executiveProDb = new ExecutiveProDb(model.getExecutiveProDb());
+        filteredEmployees = new FilteredList<>(this.executiveProDb.getEmployeeList());
+        allEmployees = new FilteredList<>(model.getFullEmployeeList());
+    }
+
+
+
     //=========== UserPrefs ==================================================================================
 
     @Override
--- src/test/java/seedu/address/logic/commands/BatchAddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/BatchAddCommandTest.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.commands;
 
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalEmployees.getTypicalExecutiveProDb;
 
 import java.nio.file.Path;
@@ -8,6 +9,7 @@ import java.nio.file.Paths;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.model.ExecutiveProDb;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
@@ -33,14 +35,36 @@ class BatchAddCommandTest {
         assertCommandFailure(batchAddCommand, model, String.format("%s does not have any data", "emptyFile.csv"));
     }
 
-     @Test
-     void execute_batchAddFileWithDuplicates_throwsCommandException() {
+    @Test
+    void execute_batchAddFileWithDuplicates_throwsCommandException() {
         Path testData = Paths.get("src", "test", "data", "BatchAddTest", "duplicates.csv");
         BatchAddCommand batchAddCommand = new BatchAddCommand("duplicates.csv");
         batchAddCommand.setFilePath(testData);
         assertCommandFailure(batchAddCommand, model, "One person in the list is found to be a duplicate. "
                 + "Call aborted");
-     }
+    }
+
+    @Test
+    void execute_batchAddFileMissingFields_throwsCommandException() {
+        Path testData = Paths.get("src", "test", "data", "BatchAddTest", "missingFields.csv");
+        BatchAddCommand batchAddCommand = new BatchAddCommand("missingFields.csv");
+        batchAddCommand.setFilePath(testData);
+        assertCommandFailure(batchAddCommand, model, BatchAddCommand.MESSAGE_MISSING_NEEDED_FIELDS);
+    }
+
+    @Test
+    void execute_batchAdd_success() {
+        Model emptyDatabase = new ModelManager(new ExecutiveProDb(), new UserPrefs());
+        Path testData = Paths.get("src", "test", "data", "BatchAddTest", "validEmployees.csv");
+        BatchAddCommand batchAddCommand = new BatchAddCommand("validEmployees.csv");
+        batchAddCommand.setFilePath(testData);
+
+        // Update the expectedMessage with the correct number of employees in the "validEmployees.csv" file
+        String expectedMessage = String.format(BatchAddCommand.MESSAGE_WORKS, 2);
+        Model expectedModel = emptyDatabase;
+
+        assertCommandSuccess(batchAddCommand, emptyDatabase, expectedMessage, expectedModel);
+    }
 
 
 }
