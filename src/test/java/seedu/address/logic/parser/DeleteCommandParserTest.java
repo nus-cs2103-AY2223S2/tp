09c926fdbf16e81707a6eaa745c8e20646328437
EvitanRelta <shauntanzongzhi@gmail.com>
2023-04-08 17:36:31 +0800

    Added more tests for 'DeleteCommandParserTest'

--- src/test/java/seedu/address/logic/parser/DeleteCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/DeleteCommandParserTest.java
@@ -1,5 +1,6 @@
 package seedu.address.logic.parser;
 
+import static seedu.address.commons.core.Messages.MESSAGE_DUPLICATE_PERSONS_INDEX;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
@@ -22,12 +23,23 @@ import seedu.address.logic.commands.DeleteCommand;
  */
 public class DeleteCommandParserTest {
 
+    private static final String INVALID_DELETE_FORMAT_MESSAGE =
+            String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE);
+
     private DeleteCommandParser parser = new DeleteCommandParser();
+
+    @Test
+    public void parse_noArgs_throwsParseException() {
+        assertParseFailure(parser, "", INVALID_DELETE_FORMAT_MESSAGE);
+        assertParseFailure(parser, "   ", INVALID_DELETE_FORMAT_MESSAGE);
+    }
 
     @Test
     public void parse_validArgs_returnsDeleteCommand() {
         assertParseSuccess(parser, "1", new DeleteCommand(List.of(INDEX_FIRST_PERSON)));
         assertParseSuccess(parser, "2", new DeleteCommand(List.of(INDEX_SECOND_PERSON)));
+        assertParseSuccess(parser, "  1  ", new DeleteCommand(List.of(INDEX_FIRST_PERSON)));
+        assertParseSuccess(parser, "  2  ", new DeleteCommand(List.of(INDEX_SECOND_PERSON)));
     }
 
     @Test
@@ -40,10 +52,35 @@ public class DeleteCommandParserTest {
     @Test
     public void parse_multipleValidArgs_returnsDeleteCommand() {
         assertParseSuccess(parser, "1 2", new DeleteCommand(List.of(INDEX_FIRST_PERSON, INDEX_SECOND_PERSON)));
+        assertParseSuccess(parser, "2 1", new DeleteCommand(List.of(INDEX_SECOND_PERSON, INDEX_FIRST_PERSON)));
+        assertParseSuccess(parser, "  1   2  ", new DeleteCommand(List.of(INDEX_FIRST_PERSON, INDEX_SECOND_PERSON)));
+        assertParseSuccess(parser, "  2   1  ", new DeleteCommand(List.of(INDEX_SECOND_PERSON, INDEX_FIRST_PERSON)));
     }
 
     @Test
     public void parse_multipleInvalidArgs_throwsParseException() {
+        assertParseFailure(parser, "a 1", MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
         assertParseFailure(parser, "0 1", MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        assertParseFailure(parser, "-1 1", MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        assertParseFailure(parser, "1 a", MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        assertParseFailure(parser, "1 0", MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        assertParseFailure(parser, "1 -1", MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        assertParseFailure(parser, "   a  1   ", MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        assertParseFailure(parser, "   0  1   ", MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        assertParseFailure(parser, "   -1  1   ", MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        assertParseFailure(parser, "  1   a  ", MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        assertParseFailure(parser, "  1  0  ", MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        assertParseFailure(parser, "  1  -1  ", MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
     }
+
+    @Test
+    public void parse_duplicateIndexes_throwsParseException() {
+        assertParseFailure(parser, "1 1", MESSAGE_DUPLICATE_PERSONS_INDEX);
+        assertParseFailure(parser, "2 2 2", MESSAGE_DUPLICATE_PERSONS_INDEX);
+        assertParseFailure(parser, "1 2 2", MESSAGE_DUPLICATE_PERSONS_INDEX);
+        assertParseFailure(parser, "  1   1 ", MESSAGE_DUPLICATE_PERSONS_INDEX);
+        assertParseFailure(parser, "  2   2   2  ", MESSAGE_DUPLICATE_PERSONS_INDEX);
+        assertParseFailure(parser, "  1   2   2  ", MESSAGE_DUPLICATE_PERSONS_INDEX);
+    }
+
 }
