4659ba881802a607bd37d42a77ee5b9107e3291a
dunliang0513 <dunliang014@gmail.com>
2023-04-08 13:30:32 +0800

    Implement new test cases for Skill Command

--- src/test/java/seedu/address/logic/commands/SkillCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SkillCommandTest.java
@@ -1,30 +1,30 @@
 package seedu.address.logic.commands;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_THIRD_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.function.Predicate;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.commons.core.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
-import seedu.address.model.person.Person;
 import seedu.address.model.person.NoteContainsKeywordsPredicate;
+import seedu.address.model.person.Person;
 import seedu.address.testutil.PersonBuilder;
 
 public class SkillCommandTest {
 
-    private Model model = new ModelManager((getTypicalAddressBook()), new UserPrefs());
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
     private Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
     @Test
@@ -57,7 +57,7 @@ public class SkillCommandTest {
     }
 
     @Test
-    public void execute_zero_keywords_errorThrown() {
+    public void execute_zeroKeyword_errorThrown() {
         Person personToSkill = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getOneBased());
         PersonBuilder personInList = new PersonBuilder(personToSkill);
 
@@ -74,8 +74,68 @@ public class SkillCommandTest {
 
         System.out.println(exceptionThrown.getMessage());
 
-//        assertEquals(String.format(MESSAGE_INVALID_COMMAND_FORMAT, SkillCommand.MESSAGE_USAGE),
-//                exceptionThrown.getMessage());
-        System.out.println(exceptionThrown.getMessage());
+        assertEquals(String.format(MESSAGE_INVALID_COMMAND_FORMAT, SkillCommand.MESSAGE_USAGE),
+                exceptionThrown.getMessage());
+    }
+
+    @Test
+    public void execute_oneKeyword_showOneApplicant() {
+        Person personToSkill = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getOneBased());
+        PersonBuilder personInList = new PersonBuilder(personToSkill);
+
+        Person skillPerson = personInList
+                .withNotes("Java")
+                .build();
+
+        Predicate<Person> predicate = x -> true;
+        predicate = predicate.and(new NoteContainsKeywordsPredicate(skillPerson.getNotesString()));
+
+        SkillCommand skillCommand = new SkillCommand(predicate);
+
+        model.setPerson(personToSkill, skillPerson);
+        model.updateFilteredPersonList(predicate);
+        String expectedMessage = String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW,
+                model.getFilteredPersonList().size());
+
+        CommandResult result = skillCommand.execute(model);
+        CommandResult expectedResult = new CommandResult(expectedMessage);
+
+        assertEquals(expectedResult, result);
+
+    }
+
+    @Test
+    public void execute_oneKeyword_showMultipleApplicants() {
+        Person personToSkill1 = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getOneBased());
+        PersonBuilder person1InList = new PersonBuilder(personToSkill1);
+
+        Person skillPerson1 = person1InList
+                .withNotes("Python")
+                .build();
+
+        Person personToSkill2 = model.getFilteredPersonList().get(INDEX_THIRD_PERSON.getOneBased());
+        PersonBuilder person2InList = new PersonBuilder(personToSkill2);
+
+        Person skillPerson2 = person2InList
+                .withNotes("Python")
+                .build();
+
+        Predicate<Person> predicate = x -> true;
+        predicate = predicate.and(new NoteContainsKeywordsPredicate(skillPerson1.getNotesString()));
+
+        SkillCommand skillCommand = new SkillCommand(predicate);
+
+        model.setPerson(personToSkill1, skillPerson1);
+        model.setPerson(personToSkill2, skillPerson2);
+        model.updateFilteredPersonList(predicate);
+
+        String expectedMessage = String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW,
+                model.getFilteredPersonList().size());
+
+        CommandResult result = skillCommand.execute(model);
+        CommandResult expectedResult = new CommandResult(expectedMessage);
+
+        assertEquals(expectedResult, result);
+
     }
 }
--- src/test/java/seedu/address/logic/parser/SkillCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/SkillCommandParserTest.java
@@ -1,6 +1,5 @@
 package seedu.address.logic.parser;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_NOTE_FRIEND;
@@ -8,15 +7,11 @@ import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_AMY;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 
+import java.util.function.Predicate;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.function.Predicate;
-
-import seedu.address.logic.commands.Command;
-import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.commands.SkillCommand;
-import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.NoteContainsKeywordsPredicate;
 import seedu.address.model.person.Person;
 import seedu.address.testutil.PersonBuilder;
@@ -34,19 +29,20 @@ public class SkillCommandParserTest {
                 .withPhone(VALID_PHONE_AMY)
                 .withNotes(VALID_NOTE_FRIEND)
                 .build();
+
         Predicate<Person> predicate = x -> true;
 
-        predicate = predicate.and(new NoteContainsKeywordsPredicate(amy.getNotes().toString()));
-
-//        NoteContainsKeywordsPredicate predicate = new NoteContainsKeywordsPredicate(amy.getNotes().toString());
+        predicate = predicate.and(new NoteContainsKeywordsPredicate(amy.getNotesString()));
         SkillCommand expectedSkillCommand = new SkillCommand(predicate);
 
-        assertParseSuccess(parser, "friends", expectedSkillCommand);
-        //assertParseSuccess(parser, " \n friends \n \t ", expectedSkillCommand);
+        // Cannot pass these two test cases
+        assertParseSuccess(parser, " friends", expectedSkillCommand);
+        assertParseSuccess(parser, " \n friends \n \t ", expectedSkillCommand);
     }
 
     @Test
     public void parse_emptyArg_throwsParseException() {
-        assertParseFailure(parser, "       ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, SkillCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "       ", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                SkillCommand.MESSAGE_USAGE));
     }
 }
