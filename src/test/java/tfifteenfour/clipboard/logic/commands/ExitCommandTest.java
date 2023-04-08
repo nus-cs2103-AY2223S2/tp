092d0b0f94639ae17f128e6f973cf3d7d5b168ed
tyx021 <yxting1109@gmail.com>
2023-04-08 17:57:17 +0800

    add testing for exit command

--- src/test/java/tfifteenfour/clipboard/logic/commands/ExitCommandTest.java
+++ src/test/java/tfifteenfour/clipboard/logic/commands/ExitCommandTest.java
@@ -2,19 +2,10 @@
 package tfifteenfour.clipboard.logic.commands;
 
 import static tfifteenfour.clipboard.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static tfifteenfour.clipboard.logic.commands.ExitCommand.MESSAGE_EXIT_ACKNOWLEDGEMENT;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import tfifteenfour.clipboard.logic.CurrentSelection;
-import tfifteenfour.clipboard.logic.PageType;
 import tfifteenfour.clipboard.model.Model;
-import tfifteenfour.clipboard.model.ModelManager;
-import tfifteenfour.clipboard.model.course.Course;
-import tfifteenfour.clipboard.model.course.Group;
-import tfifteenfour.clipboard.model.course.Session;
-import tfifteenfour.clipboard.model.student.Student;
 import tfifteenfour.clipboard.testutil.TypicalModel;
 
 public class ExitCommandTest {
@@ -27,8 +18,10 @@ public class ExitCommandTest {
 
     @Test
     public void execute_exit_success() {
-        CommandResult expectedCommandResult = new CommandResult(new ExitCommand(), MESSAGE_EXIT_ACKNOWLEDGEMENT, false);
-        assertCommandSuccess(new ExitCommand(), model, expectedCommandResult, expectedModel);
+        ExitCommand exitCommand = new ExitCommand();
+        CommandResult expectedCommandResult = new CommandResult(exitCommand, ExitCommand.MESSAGE_EXIT_ACKNOWLEDGEMENT, false);
+        assertCommandSuccess(exitCommand, model, expectedCommandResult, model);
     }
+
 }
 
