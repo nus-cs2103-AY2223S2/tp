beed2923019395ed2cf0c597cafac7dda60bada3
nusE0726844 <92289387+nusE0726844@users.noreply.github.com>
2023-04-09 11:47:28 +0800

    Add test code for EndTimePredicate

---
+++ src/test/java/seedu/vms/model/appointment/predicates/EndTimePredicateTest.java
@@ -0,0 +1,54 @@
+package seedu.vms.model.appointment.predicates;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import seedu.vms.commons.core.index.Index;
+import seedu.vms.model.GroupName;
+import seedu.vms.model.appointment.Appointment;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EndTimePredicateTest {
+
+    private LocalDateTime now;
+    private EndTimePredicate endTimePredicate;
+
+    @BeforeEach
+    void setUp() {
+        now = LocalDateTime.now();
+        endTimePredicate = new EndTimePredicate(now.plusHours(1));
+    }
+
+    @Test
+    void test() {
+        Index patientId = Index.fromOneBased(1);
+        LocalDateTime startTime = now.plusMinutes(5);
+        LocalDateTime endTime = now.plusHours(1);
+        GroupName vaccine = new GroupName("Test Vaccine");
+        Boolean isCompleted = false;
+        Appointment appointment = new Appointment(patientId, startTime, endTime, vaccine, isCompleted);
+
+        assertTrue(endTimePredicate.test(appointment));
+
+        endTime = endTime.plusMinutes(5);
+        appointment = new Appointment(patientId, startTime, endTime, vaccine, isCompleted);
+
+        assertFalse(endTimePredicate.test(appointment));
+    }
+
+    @Test
+    void equals() {
+        assertEquals(endTimePredicate, endTimePredicate);
+        assertNotEquals(null, endTimePredicate);
+
+        EndTimePredicate otherStartTimePredicate;
+
+        otherStartTimePredicate = new EndTimePredicate(now.plusHours(1));
+        assertEquals(endTimePredicate, otherStartTimePredicate);
+
+        otherStartTimePredicate = new EndTimePredicate(now.plusMinutes(5));
+        assertNotEquals(endTimePredicate, otherStartTimePredicate);
+    }
+}
