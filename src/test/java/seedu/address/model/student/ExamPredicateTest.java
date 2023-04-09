7d038ad77384614d7bdb229a08d04667abb60e26
szejiancheng <sze2001jc@gmail.com>
2023-04-10 01:10:32 +0800

    Add testcases for ExamPredicate

--- src/test/java/seedu/address/model/student/ExamPredicateTest.java
+++ src/test/java/seedu/address/model/student/ExamPredicateTest.java
@@ -2,15 +2,38 @@ package seedu.address.model.student;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ExamPredicateTest {
+    private final String validExamName = "valid name";
+    private final ExamPredicate validPredicate = new ExamPredicate(validExamName);
+    private final Exam validExam = new Exam(validExamName, LocalDateTime.now(), LocalDateTime.now(), null,
+            null);
+    private final Exam anotherExam = new Exam("another name", LocalDateTime.now(), LocalDateTime.now(),
+            null,null);
 
     @Test
-    void test() {
+    public void constructor_validParams_success() throws Exception {
+        try {
+            new ExamPredicate(validExamName);
+        } catch (Exception e) {
+            assert(false);
+        }
     }
 
     @Test
-    void testEquals() {
+    void test_checkIfExamMatches_returnsTrue() {
+        assertTrue(validPredicate.test(validExam));
+    }
+    @Test
+    void test_checkIfDifferentExamMatches_returnsFalse() {
+        assertFalse(validPredicate.test(anotherExam));
+    }
+
+    @Test
+    void equals_checkEqualsItself_returnsTrue() {
+        assertEquals(validPredicate, validPredicate);
     }
 }
