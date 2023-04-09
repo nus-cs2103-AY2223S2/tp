f30a69f8c347c42c2e4aca4bc23636da7d2395dd
Pang Rui Wei <>
2023-04-09 13:52:49 +0800

    Fix checkstyle

--- src/main/java/seedu/address/logic/parser/AddPolicyCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddPolicyCommandParser.java
@@ -3,7 +3,6 @@ package seedu.address.logic.parser;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_POLICY_FREQUENCY;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_POLICY_INDEX;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_POLICY_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_POLICY_PREMIUM;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_POLICY_START_DATE;
