c7119809898928d3b56fedff2a92600e4a7941a7
DerenC <chengderen953@gmail.com>
2023-04-10 00:46:11 +0800

    Fix failed testPredicate & testComparator unit tests for UiTaskList

--- src/test/java/vimification/model/UiTaskListTest.java
+++ src/test/java/vimification/model/UiTaskListTest.java
@@ -13,16 +13,28 @@ public class UiTaskListTest {
     @Test
     public void testPredicate() {
         UiTaskList utl = new TaskList();
-        Predicate<Task> pred = task -> task.getTitle().equals("Buy");
-        utl.setPredicate(pred);
-        assertEquals(utl.getPredicate(), pred);
+        Predicate<Task> expectedPred = task -> task.getTitle().equals("Buy");
+
+        utl.setPredicate(expectedPred);
+        Predicate<? super Task> actualPred = utl.getPredicate();
+
+        Task task1 = new Task("Buy milk");
+        Task task2 = new Task("Do essay");
+        assertEquals(actualPred.test(task1), expectedPred.test(task1));
+        assertEquals(actualPred.test(task2), expectedPred.test(task2));
     }
 
     @Test
     public void testComparator() {
         UiTaskList utl = new TaskList();
-        Comparator<Task> cmp = (task1, task2) -> task2.getTitle().compareTo(task1.getTitle());
-        utl.setComparator(cmp);
-        assertEquals(utl.getPredicate(), cmp);
+        Comparator<Task> expectedCmp =
+                (task1, task2) -> task2.getTitle().compareTo(task1.getTitle());
+
+        utl.setComparator(expectedCmp);
+        Comparator<? super Task> actualCmp = utl.getComparator();
+
+        Task task1 = new Task("Buy milk");
+        Task task2 = new Task("Do essay");
+        assertEquals(expectedCmp.compare(task1, task2), actualCmp.compare(task1, task2));
     }
 }
