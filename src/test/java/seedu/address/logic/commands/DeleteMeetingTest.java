e6b4b8fc59f09bef93049039786ccd5edf0e6ef7
thedesalizes <altaria99@gmail.com>
2023-04-09 12:23:45 +0800

    Added test for DeleteMeetingCommand

---
+++ src/test/java/seedu/address/logic/commands/DeleteMeetingTest.java
@@ -0,0 +1,32 @@
+package seedu.address.logic.commands;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
+import static seedu.address.testutil.TypicalAddressBooks.getTypicalAddressBook;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
+
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.meeting.Meeting;
+import seedu.address.model.person.Person;
+
+/**
+ * Contains integration tests (interaction with the Model) and unit tests for
+ * {@code DeleteMeetingCommand}.
+ */
+public class DeleteMeetingTest {
+
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+//    Meeting meetingToDelete = model.getMeetingsList().get(index.getZeroBased())
+}
