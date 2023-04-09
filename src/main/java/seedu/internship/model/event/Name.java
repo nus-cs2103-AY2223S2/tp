c827f8ae831927ffcceea3ec97adb2a9561464b5
Anshumaan Tyagi <anshumaantyagi0@gmail.com>
2023-04-09 16:09:21 +0800

    Fix Clash Warning for Deadlines

--- src/main/java/seedu/internship/model/event/Name.java
+++ src/main/java/seedu/internship/model/event/Name.java
@@ -40,7 +40,7 @@ public class Name {
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof Name // instanceof handles nulls
-                && name.toLowerCase().contains(((Name) other).name.toLowerCase())); // state check
+                && name.toLowerCase().equals(((Name) other).name.toLowerCase())); // state check
     }
 
     @Override
