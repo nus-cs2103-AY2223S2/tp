07f8e35493ae8492d840c39b8f19baf7776f3471
Pang Rui Wei <>
2023-04-10 02:32:20 +0800

    Fix checkstyle

--- src/test/java/seedu/address/logic/commands/AddAppointmentCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddAppointmentCommandTest.java
@@ -3,9 +3,7 @@ package seedu.address.logic.commands;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalClients.getTypicalAddressBook;
-import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_CLIENT;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,7 +13,6 @@ import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
-import seedu.address.model.client.Client;
 import seedu.address.model.client.appointment.Appointment;
 import seedu.address.testutil.AppointmentBuilder;
 
