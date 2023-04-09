b2ecbc7b6f26a365123d2d0a9ca10d3936437a0a
shuimeihe <shuimeihe@gmail.com>
2023-04-09 15:41:08 +0800

    Add ArchiveSequenceDiagram Add ArchiveActivityDiagram

---
+++ src/test/java/seedu/address/logic/commands/MarkCommandTest.java
@@ -0,0 +1,8 @@
+package seedu.address.logic.commands;
+
+/**
+ * Contains integration tests (interaction with the Model) and unit tests for
+ * {@code MarkCommand}.
+ */
+public class MarkCommand {
+}
