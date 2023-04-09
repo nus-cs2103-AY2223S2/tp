4225fb1c3d329fb9b39fd1c2a1899b8e017d94fd
nusE0726844 <92289387+nusE0726844@users.noreply.github.com>
2023-04-09 10:56:12 +0800

    Improve test code for JsonSerializableAppointmentManagerTest

--- src/test/java/seedu/vms/storage/appointment/JsonSerializableAppointmentManagerTest.java
+++ src/test/java/seedu/vms/storage/appointment/JsonSerializableAppointmentManagerTest.java
@@ -1,5 +1,6 @@
 package seedu.vms.storage.appointment;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static seedu.vms.testutil.Assert.assertThrows;
 
 import java.io.IOException;
@@ -13,12 +14,18 @@ import seedu.vms.commons.util.JsonUtil;
 class JsonSerializableAppointmentManagerTest {
 
     private static final Path APPOINTMENT_FILEPATH = Paths.get("src", "test", "data", "appointment");
-    private static final Path INVALID_APPOINTMENT_FILE = APPOINTMENT_FILEPATH
-            .resolve("invalidAppointmentManager.json");
+    private static final Path VALID_APPOINTMENT_FILE = APPOINTMENT_FILEPATH.resolve("validAppointmentManager.json");
+    private static final Path INVALID_APPOINTMENT_FILE = APPOINTMENT_FILEPATH.resolve("invalidAppointmentManager.json");
 
     @Test
     void toModelType() throws IOException {
-        JsonSerializableAppointmentManager dataFromFile = JsonUtil.deserializeFromFile(INVALID_APPOINTMENT_FILE,
+        JsonSerializableAppointmentManager dataFromFile;
+
+        dataFromFile = JsonUtil.deserializeFromFile(VALID_APPOINTMENT_FILE,
+                JsonSerializableAppointmentManager.class);
+        assertDoesNotThrow(dataFromFile::toModelType);
+
+        dataFromFile = JsonUtil.deserializeFromFile(INVALID_APPOINTMENT_FILE,
                 JsonSerializableAppointmentManager.class);
         assertThrows(IllegalArgumentException.class, dataFromFile::toModelType);
     }
