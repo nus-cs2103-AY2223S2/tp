36dea0315154f6da72e8e48a0551c04279fb58f0
benedict733 <wbenedict733@gmail.com>
2023-04-09 20:49:23 +0800

    Update for checkstyle

--- src/main/java/seedu/address/logic/commands/ClearTaskCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearTaskCommand.java
@@ -10,7 +10,6 @@ import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.person.Person;
-import seedu.address.model.person.TaskList;
 
 /**
  * Clears the tasks of an existing person in the address book.
