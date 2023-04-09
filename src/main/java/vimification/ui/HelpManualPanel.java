36666fe23c30419626af1e2365ec167be5a77e1a
DerenC <chengderen953@gmail.com>
2023-04-09 10:15:16 +0800

    Rename ManualPanel to HelpManualPanel

---
+++ src/main/java/vimification/ui/HelpManualPanel.java
@@ -0,0 +1,42 @@
+package vimification.ui;
+
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+
+/**
+ * An UI component that displays the manual page for Vimification.
+ */
+public class HelpManualPanel extends UiPart<VBox> {
+
+    private static final String FXML = "HelpManualPanel.fxml";
+
+    @FXML
+    private WebView webView;
+
+    public HelpManualPanel() {
+        super(FXML);
+        Path manualHtml = Path.of(FXML_FILE_FOLDER, "HelpManualPanel.html");
+        System.out.println(manualHtml.toString());
+        WebEngine webEngine = webView.getEngine();
+        webEngine.load(manualHtml.toString());
+        String manualHtmlPath;
+        try {
+            manualHtmlPath =
+                    getClass().getResource(FXML_FILE_FOLDER + "HelpManualPanel.html").toURI()
+                            .toString();
+            webEngine.load(manualHtmlPath);
+        } catch (URISyntaxException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        webView.prefWidthProperty().bind(this.getRoot().widthProperty());
+        webView.prefHeightProperty().bind(this.getRoot().heightProperty());
+    }
+
+}
