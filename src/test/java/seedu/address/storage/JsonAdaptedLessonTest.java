a73a3b3e573617375a5c544d5c46ec8cb9145266
szejiancheng <sze2001jc@gmail.com>
2023-04-10 00:15:19 +0800

    Add JsonAdaptedLesson testcases

--- src/test/java/seedu/address/storage/JsonAdaptedLessonTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedLessonTest.java
@@ -1,24 +1,48 @@
 package seedu.address.storage;
 
 import org.junit.jupiter.api.Test;
+import seedu.address.model.student.Exam;
+import seedu.address.model.student.Lesson;
+
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class JsonAdaptedLessonTest {
+    private final String validTitle = "valid title";
+    private final LocalDateTime validDateTime = LocalDateTime.now();
+    private final Lesson validLesson = new Lesson(validTitle, validDateTime, validDateTime);
+    private final JsonAdaptedLesson validJsonLesson = new JsonAdaptedLesson(validLesson);
 
     @Test
-    void getTitle() {
+    public void constructor_validLessonDetails_success() throws Exception {
+        try {
+            new JsonAdaptedLesson(validTitle, validDateTime, validDateTime);
+            new JsonAdaptedLesson(validLesson);
+        } catch (Exception e) {
+            assert(false);
+        }
+    }
+    @Test
+    public void toModelType_validJsonAdaptedLesson_success() throws Exception {
+        try {
+            validJsonLesson.toModelType();
+        } catch (Exception e) {
+            assert false;
+        }
     }
 
     @Test
-    void getStartTime() {
+    public void toModelType_CheckReturnedLessonEqualsOriginal_returnsTrue() throws Exception {
+        Lesson returnedValidLesson = validJsonLesson.toModelType();
+        assertEquals(returnedValidLesson, validLesson);
     }
 
     @Test
-    void getEndTime() {
-    }
-
-    @Test
-    void toModelType() {
+    public void toModelType_CheckReturnedLessonFields_success() throws Exception {
+        JsonAdaptedLesson adaptedHomework = new JsonAdaptedLesson(validLesson);
+        assertEquals(adaptedHomework.getTitle(), validLesson.getTitle());
+        assertEquals(adaptedHomework.getStartTime(), validLesson.getStartTime());
+        assertEquals(adaptedHomework.getEndTime(), validLesson.getEndTime());
     }
 }
