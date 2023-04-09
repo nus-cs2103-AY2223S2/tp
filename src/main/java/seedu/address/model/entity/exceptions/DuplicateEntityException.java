485bd381099dd2fd8e94f4ed645a0db492841877
Jiexuan <81137599+botcon02@users.noreply.github.com>
2023-04-09 17:02:47 +0800

    Fix checkstyle

--- src/main/java/seedu/address/model/entity/exceptions/DuplicateEntityException.java
+++ src/main/java/seedu/address/model/entity/exceptions/DuplicateEntityException.java
@@ -1,8 +1,8 @@
 package seedu.address.model.entity.exceptions;
 
 /**
- * Signals that the operation will result in duplicate Entities (Entities are considered duplicates if they have the same
- * identity).
+ * Signals that the operation will result in duplicate Entities,
+ * Entities are considered duplicates if they have the same identity.
  */
 public class DuplicateEntityException extends RuntimeException {
     public DuplicateEntityException() {
