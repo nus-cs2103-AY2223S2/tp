ab1646126a59116c1cbe3266428af5db8e18172c
thedesalizes <altaria99@gmail.com>
2023-04-09 12:47:46 +0800

    Add test cases for DeleteMeetingTest

--- src/test/java/seedu/address/logic/commands/DeleteMeetingTest.java
+++ src/test/java/seedu/address/logic/commands/DeleteMeetingTest.java
@@ -4,10 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
 import static seedu.address.testutil.TypicalAddressBooks.getTypicalAddressBook;
-import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
-import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_MEETING;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_MEETING;
 
 
 import org.junit.jupiter.api.Test;
@@ -18,7 +17,6 @@ import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.meeting.Meeting;
-import seedu.address.model.person.Person;
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for
@@ -28,5 +26,45 @@ public class DeleteMeetingTest {
 
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
-//    Meeting meetingToDelete = model.getMeetingsList().get(index.getZeroBased())
+    @Test
+    public void execute_deleteMeeting_success() {
+        Meeting meetingToDelete = model.getMeetingsList().get(INDEX_FIRST_MEETING.getZeroBased());
+        DeleteMeetingCommand deleteMeetingCommand = new DeleteMeetingCommand(INDEX_FIRST_MEETING);
+
+        String expectedMessage = String.format(DeleteMeetingCommand.MESSAGE_SUCCESS, meetingToDelete);
+
+        ModelManager expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        expectedModel.removeMeeting(meetingToDelete);
+
+        assertCommandSuccess(deleteMeetingCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_invalidMeetingIndex_throwsCommandException() {
+        Index outOfBoundIndex = Index.fromOneBased(model.getMeetingsList().size() + 1);
+        DeleteMeetingCommand deleteMeetingCommand = new DeleteMeetingCommand(outOfBoundIndex);
+
+        assertCommandFailure(deleteMeetingCommand, model, Messages.MESSAGE_INVALID_MEETING_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void equals() {
+        DeleteMeetingCommand deleteFirstMeetingCommand = new DeleteMeetingCommand(INDEX_FIRST_MEETING);
+        DeleteMeetingCommand deleteSecondMeetingCommand = new DeleteMeetingCommand(INDEX_SECOND_MEETING);
+
+        // the same object should return true
+        assertTrue(deleteFirstMeetingCommand.equals(deleteFirstMeetingCommand));
+
+        // command object with same index should return true
+        assertTrue(deleteFirstMeetingCommand.equals(new DeleteMeetingCommand(INDEX_FIRST_MEETING)));
+
+        // different object type should return false
+        assertFalse(deleteFirstMeetingCommand.equals(1));
+
+        // null returns false
+        assertFalse(deleteFirstMeetingCommand.equals(null));
+
+        // different person should return false
+        assertFalse(deleteFirstMeetingCommand.equals(deleteSecondMeetingCommand));
+    }
 }
