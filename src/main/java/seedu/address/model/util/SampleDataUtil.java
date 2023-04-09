b6620a69522b0c9a608e2bfb62a9d29a7fb87c4b
bokung <e0774428@u.nus.edu>
2023-04-09 15:08:49 +0800

    Update sample data

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -1,5 +1,6 @@
 package seedu.address.model.util;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Set;
@@ -9,6 +10,7 @@ import seedu.address.model.AddressBook;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
+import seedu.address.model.person.Meeting;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
@@ -20,23 +22,35 @@ import seedu.address.model.tag.PolicyTag;
 public class SampleDataUtil {
 
     public static Person[] getSamplePersons() {
+        Meeting introMeeting = new Meeting("Policy introduction with Jun Jie", 
+                    LocalDateTime.of(2023, 4, 14, 14, 0),
+                    LocalDateTime.of(2023, 4, 14, 16, 0));
+        Meeting followupMeeting = new Meeting("Policy follow up with Jun Jie", 
+                    LocalDateTime.of(2023, 5, 20, 14, 0),
+                    LocalDateTime.of(2023, 5, 20, 16, 0));
+
+        ArrayList<Meeting> junjieMeetingList = new ArrayList<>();
+        junjieMeetingList.add(introMeeting);
+        junjieMeetingList.add(followupMeeting);
+
         return new Person[] {
-            new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
+            new Person(new Name("Lim Jun Jie"), new Phone("87438807"), new Email("limjunjie@gmail.com"),
                 new Address("Blk 30 Geylang Street 29, #06-40"),
-                getTagSet("Insurance"), new ArrayList<>()),
-            new Person(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
+                getTagSet("Insurance"), 
+                junjieMeetingList),
+            new Person(new Name("Tan Jia Qi"), new Phone("99272758"), new Email("tanjq@gmail.com"),
                 new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
                 getTagSet("Investment", "Insurance"), new ArrayList<>()),
-            new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
+            new Person(new Name("Fariq Ahmad"), new Phone("93210283"), new Email("ahmadfariq@hotmail.com"),
                 new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
                 getTagSet("Retirement"), new ArrayList<>()),
-            new Person(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
+            new Person(new Name("Rohit Sharma"), new Phone("91031282"), new Email("sharma@outlook.com"),
                 new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
                 getTagSet("Investment"), new ArrayList<>()),
-            new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
+            new Person(new Name("Irfan Iskandar"), new Phone("92492021"), new Email("irfan@gmail.com"),
                 new Address("Blk 47 Tampines Street 20, #17-35"),
                 getTagSet("DebtManagement"), new ArrayList<>()),
-            new Person(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
+            new Person(new Name("Joe Sanders"), new Phone("92624417"), new Email("joesanders@yahoo.com"),
                 new Address("Blk 45 Aljunied Street 85, #11-31"),
                 getTagSet("AssetManagement", "Investment"), new ArrayList<>())
         };
