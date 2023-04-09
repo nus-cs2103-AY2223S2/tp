68ec4dc3f3784acb3cf00a40fbb0ab73ce3c1d9b
Lee Jia Sheng <thegreyjs@gmail.com>
2023-04-10 02:27:18 +0800

    Remove irrelevant test cases

--- src/test/java/seedu/address/storage/StorageManagerTest.java
+++ src/test/java/seedu/address/storage/StorageManagerTest.java
@@ -2,7 +2,7 @@
 //
 //import static org.junit.jupiter.api.Assertions.assertEquals;
 //import static org.junit.jupiter.api.Assertions.assertNotNull;
-//import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+//import static seedu.address.testutil.TypicalInternships.getTypicalAddressBook;
 //
 //import java.nio.file.Path;
 //
