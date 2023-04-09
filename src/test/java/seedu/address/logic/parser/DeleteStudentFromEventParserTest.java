ced0a62d066e60b272ba9f5508370b6ddd23e427
Shi Jiaao <e0372500@u.nus.edu>
2023-04-10 03:47:36 +0800

    Add tests for deleteStudent parser

--- src/test/java/seedu/address/logic/parser/DeleteStudentFromEventParserTest.java
+++ src/test/java/seedu/address/logic/parser/DeleteStudentFromEventParserTest.java
@@ -1,10 +1,63 @@
 package seedu.address.logic.parser;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.Test;
 
+import seedu.address.logic.commands.DeleteStudentFromEventCommand;
+import seedu.address.logic.commands.DeleteStudentFromEventCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
 class DeleteStudentFromEventParserTest {
+    public static final String VALID_COMMAND_TUTORIAL = "1 Tutorial/1";
+    public static final String VALID_COMMAND_LAB = "1 Lab/1";
+    public static final String VALID_COMMAND_CONSULTATION = "1 Consultation/1";
+    public static final String INVALID_EVENT_FIELD_COMMAND = "1 Leb/1";
+    public static final String TOO_MANY_EVENT_FIELDS_COMMAND = "1 Lab/1 Tutorial/1";
+    public static final String INVALID_STUDENT_INDEX_COMMAND = "1 1 1 Tutorial/1";
+    public static final String INVALID_EVENT_INDEX_COMMAND = "1 Tutorial/1 1 1";
+    public static final String EMPTY_EVENT_COMMAND = "1";
 
     @Test
-    void parse() {
+    void parse_validTutorial_success() throws ParseException {
+        assert(new DeleteStudentFromEventParser().parse(VALID_COMMAND_TUTORIAL)
+                instanceof DeleteStudentFromEventCommand);
+    }
+
+    @Test
+    void parse_validLab_success() throws ParseException {
+        assert(new DeleteStudentFromEventParser().parse(VALID_COMMAND_LAB)
+                instanceof DeleteStudentFromEventCommand);
+    }
+
+    @Test
+    void parse_validConsultation_success() throws ParseException {
+        assert(new DeleteStudentFromEventParser().parse(VALID_COMMAND_CONSULTATION)
+                instanceof DeleteStudentFromEventCommand);
+    }
+
+    @Test
+    void parse_invalidEventField_throwsParseException() {
+        assertThrows(ParseException.class, () -> new DeleteStudentFromEventParser().parse(INVALID_EVENT_FIELD_COMMAND));
+    }
+
+    @Test
+    void parse_tooManyEventFields_throwsParseException() {
+        assertThrows(ParseException.class, () -> new DeleteStudentFromEventParser().parse(TOO_MANY_EVENT_FIELDS_COMMAND));
+    }
+
+    @Test
+    void parse_invalidStudentIndex_throwsParseException() {
+        assertThrows(ParseException.class, () -> new DeleteStudentFromEventParser().parse(INVALID_STUDENT_INDEX_COMMAND));
+    }
+
+    @Test
+    void parse_invalidEventIndex_throwsParseException() {
+        assertThrows(ParseException.class, () -> new DeleteStudentFromEventParser().parse(INVALID_EVENT_INDEX_COMMAND));
+    }
+
+    @Test
+    void parse_emptyEvent_throwsParseException() {
+        assertThrows(ParseException.class, () -> new DeleteStudentFromEventParser().parse(EMPTY_EVENT_COMMAND));
     }
 }
