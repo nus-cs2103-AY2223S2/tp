63163e3d5038d8a86e0893b736f4323fffba3c67
ruiyigan <85063215+ruiyigan@users.noreply.github.com>
2023-04-09 11:08:52 +0800

    Add for previous card

--- src/test/java/seedu/address/logic/commands/reviewcommands/PreviousCardCommandTest.java
+++ src/test/java/seedu/address/logic/commands/reviewcommands/PreviousCardCommandTest.java
@@ -2,7 +2,6 @@ package seedu.address.logic.commands.reviewcommands;
 
 import org.junit.jupiter.api.Test;
 import seedu.address.logic.commands.commandresult.CommandResult;
-import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
@@ -15,22 +14,22 @@ import static seedu.address.testutil.TypicalCards.getTypicalMasterDeck;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIFTH;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST;
 
-public class NextCardCommandTest {
+public class PreviousCardCommandTest {
     private Model model = new ModelManager(getTypicalMasterDeck(), new UserPrefs());
 
     @Test
-    public void execute_nextCard_success() {
+    public void execute_previousCard_success() {
         model.reviewDeck(INDEX_FIRST, List.of(new Tag.TagName[]{Tag.TagName.HARD}));
-
-        CommandResult expectedCommandResult = new CommandResult(NextCardCommand.MESSAGE_SUCCESS);
-        assertEquals(expectedCommandResult, new NextCardCommand().execute(model));
+        model.goToNextCard();
+        CommandResult expectedCommandResult = new CommandResult(PreviousCardCommand.MESSAGE_SUCCESS);
+        assertEquals(expectedCommandResult, new PreviousCardCommand().execute(model));
     }
 
     @Test
-    public void execute_noMoreNextCard_throwsCommandException() {
+    public void execute_noMorePreviousCard_throwsCommandException() {
         model.reviewDeck(INDEX_FIFTH, List.of(new Tag.TagName[]{Tag.TagName.UNTAGGED}));
 
-        CommandResult expectedCommandResult = new CommandResult(NextCardCommand.MESSAGE_NO_MORE_NEXT_CARD);
-        assertEquals(expectedCommandResult, new NextCardCommand().execute(model));
+        CommandResult expectedCommandResult = new CommandResult(PreviousCardCommand.MESSAGE_NO_MORE_PREV_CARD);
+        assertEquals(expectedCommandResult, new PreviousCardCommand().execute(model));
     }
 }
