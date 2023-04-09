30b22a46242e692590ac439b2717aad2efd91b73
Jishnu <e0725046@u.nus.edu>
2023-04-09 17:06:56 +0800

    Fix more style errors

--- src/test/java/wingman/model/flight/FlightTest.java
+++ src/test/java/wingman/model/flight/FlightTest.java
@@ -1,6 +1,5 @@
 package wingman.model.flight;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -9,6 +8,8 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
+
+import org.junit.jupiter.api.Test;
 
 import wingman.model.link.exceptions.LinkException;
 import wingman.model.location.FlightLocationType;
--- src/test/java/wingman/model/flight/exceptions/FlightNotFoundExceptionTest.java
+++ src/test/java/wingman/model/flight/exceptions/FlightNotFoundExceptionTest.java
@@ -1,8 +1,10 @@
 package wingman.model.flight.exceptions;
 
-import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
 
 public class FlightNotFoundExceptionTest {
     @Test
