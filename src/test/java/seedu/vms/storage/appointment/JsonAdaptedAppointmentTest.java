885192f85b58f3a2b3f31c98c1b5f71648a546a3
nusE0726844 <92289387+nusE0726844@users.noreply.github.com>
2023-04-09 10:23:36 +0800

    Improve test coverage for JsonAdaptedAppointment.java

--- src/test/java/seedu/vms/storage/appointment/JsonAdaptedAppointmentTest.java
+++ src/test/java/seedu/vms/storage/appointment/JsonAdaptedAppointmentTest.java
@@ -1,16 +1,19 @@
 package seedu.vms.storage.appointment;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.time.LocalDateTime;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import org.junit.jupiter.api.function.Executable;
 import seedu.vms.commons.core.index.Index;
 import seedu.vms.commons.exceptions.IllegalValueException;
 import seedu.vms.model.GroupName;
 import seedu.vms.model.appointment.Appointment;
+import seedu.vms.storage.JsonAdaptedGroupName;
 
 class JsonAdaptedAppointmentTest {
 
@@ -38,5 +41,29 @@ class JsonAdaptedAppointmentTest {
         assertEquals(appointment.getAppointmentEndTime(), loadedAppointment.getAppointmentEndTime());
         assertEquals(appointment.getVaccination(), loadedAppointment.getVaccination());
         assertEquals(appointment.getStatus(), loadedAppointment.getStatus());
+
+        Integer patientId = this.patientId.getOneBased();
+        LocalDateTime startTime = this.startTime;
+        LocalDateTime endTime = this.endTime;
+        JsonAdaptedGroupName vaccine = new JsonAdaptedGroupName(this.vaccine.getName());
+        Boolean isCompleted = this.isCompleted;
+
+        jsonAdaptedAppointment = new JsonAdaptedAppointment(null, startTime, endTime, vaccine, isCompleted);
+        assertThrows(IllegalValueException.class, jsonAdaptedAppointment::toModelType);
+
+        jsonAdaptedAppointment = new JsonAdaptedAppointment(0, startTime, endTime, vaccine, isCompleted);
+        assertThrows(IllegalValueException.class, jsonAdaptedAppointment::toModelType);
+
+        jsonAdaptedAppointment = new JsonAdaptedAppointment(patientId, null, endTime, vaccine, isCompleted);
+        assertThrows(IllegalValueException.class, jsonAdaptedAppointment::toModelType);
+
+        jsonAdaptedAppointment = new JsonAdaptedAppointment(patientId, startTime, null, vaccine, isCompleted);
+        assertThrows(IllegalValueException.class, jsonAdaptedAppointment::toModelType);
+
+        jsonAdaptedAppointment = new JsonAdaptedAppointment(patientId, startTime, endTime, null, isCompleted);
+        assertThrows(IllegalValueException.class, jsonAdaptedAppointment::toModelType);
+
+        jsonAdaptedAppointment = new JsonAdaptedAppointment(patientId, startTime, endTime, vaccine, null);
+        assertThrows(IllegalValueException.class, jsonAdaptedAppointment::toModelType);
     }
 }
