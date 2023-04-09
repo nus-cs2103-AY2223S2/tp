fa6992a314ac99fc753c72ea69649e19c2f32292
jmestxr <tan_xing_rong@hotmail.com>
2023-04-09 11:02:48 +0800

    Add edge cases for DegreeProgressionDataTest

--- src/test/java/seedu/modtrek/model/DegreeProgressionDataTest.java
+++ src/test/java/seedu/modtrek/model/DegreeProgressionDataTest.java
@@ -7,17 +7,27 @@ import static seedu.modtrek.testutil.TypicalModules.getTypicalDegreeProgression;
 
 import java.util.Map;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import seedu.modtrek.model.degreedata.DegreeProgressionData;
+import seedu.modtrek.model.module.Module;
+import seedu.modtrek.testutil.ModuleBuilder;
 
 public class DegreeProgressionDataTest {
 
-    // Using Empty Degree Progression as test
-    private DegreeProgressionData emptyData = initEmptyData();
+    private DegreeProgressionData populatedData;
+    private DegreeProgressionData emptyData;
 
-    // Using Typical Degree Progression as test
-    private DegreeProgressionData populatedData = addModuleAndInit();
+    @BeforeEach
+    public void setUp() {
+        DegreeProgression populatedProgression = getTypicalDegreeProgression();
+        populatedProgression.addModule(MA2002);
+        populatedData = populatedProgression.getProgressionData();
+
+        emptyData = getEmptyDegreeProgression().getProgressionData();
+    }
+
 
     @Test
     public void getCompletedCreditForEmptyData() {
@@ -83,15 +93,37 @@ public class DegreeProgressionDataTest {
         assertEquals(4.70, populatedData.getGpa());
     }
 
-    private DegreeProgressionData initEmptyData() {
-        DegreeProgression test = getEmptyDegreeProgression();
-        return test.getProgressionData();
+    @Test
+    public void getCreditForAbsurdData() {
+        DegreeProgression absurdProgression = getEmptyDegreeProgression();
+        final Module cs0000 = new ModuleBuilder().withCode("CS0000")
+                .withCredit("99")
+                .withSemYear("Y1S1")
+                .withGrade("A+")
+                .withTags("COMPUTER SCIENCE BREADTH AND DEPTH")
+                .build();
+        absurdProgression.addModule(cs0000);
+        DegreeProgressionData absurdData = absurdProgression.getProgressionData();
+
+        assertEquals(99, absurdData.getCompletedCredit());
+        assertEquals(40, absurdData.getMeaningfulCredit());
     }
 
-    private DegreeProgressionData addModuleAndInit() {
-        DegreeProgression test = getTypicalDegreeProgression();
-        test.addModule(MA2002);
-        return test.getProgressionData();
+    @Test
+    public void getCreditForDataWithDoubleTaggedModules() {
+        DegreeProgression progression = getTypicalDegreeProgression();
+        final Module cs2106 = new ModuleBuilder().withCode("CS2106")
+                .withCredit("4")
+                .withSemYear("Y2S2")
+                .withGrade("A+")
+                .withTags("COMPUTER SCIENCE FOUNDATION", "COMPUTER SCIENCE BREADTH AND DEPTH")
+                .build();
+        progression.addModule(cs2106);
+        DegreeProgressionData progressionData = progression.getProgressionData();
+
+        assertEquals(164, progressionData.getTotalWithDuplicatedCredit());
+        assertEquals(16, progressionData.getCompletedCredit());
+        assertEquals(16, progressionData.getMeaningfulCredit());
     }
 
 }
