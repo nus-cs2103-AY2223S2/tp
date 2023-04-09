a7cb63c99ae7433dabb3585fa105268aa97e971c
Arkobrata Chaudhuri <arkobrata.2002@gmail.com>
2023-04-09 17:00:01 +0800

    Add tests for add appointment command

---
+++ src/test/java/seedu/address/logic/commands/AddAppointmentCommandTest.java
@@ -0,0 +1,307 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+//import java.util.Arrays;
+import java.util.function.Predicate;
+
+import org.junit.jupiter.api.Test;
+
+import javafx.collections.ObservableList;
+import seedu.address.commons.core.GuiSettings;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.AddressBook;
+import seedu.address.model.AppointmentList;
+import seedu.address.model.Model;
+import seedu.address.model.ReadOnlyAppointmentList;
+import seedu.address.model.ReadOnlyPatientList;
+import seedu.address.model.ReadOnlyUserPrefs;
+import seedu.address.model.appointment.Appointment;
+import seedu.address.model.patient.Name;
+import seedu.address.model.patient.Patient;
+import seedu.address.testutil.AppointmentBuilder;
+//import seedu.address.testutil.PatientBuilder;
+
+public class AddAppointmentCommandTest {
+
+    @Test
+    public void constructor_nullAppointment_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new AddAppointmentCommand(null));
+    }
+
+    //    @Test
+    //    public void execute_appointmentAcceptedByModel_addSuccessful() throws Exception {
+    //        ModelStubAcceptingAppointmentAdded modelStub = new ModelStubAcceptingAppointmentAdded();
+    //        Appointment validAppointment = new AppointmentBuilder().build();
+    //        modelStub.addPatient(new PatientBuilder().withName(validAppointment.getPatientName().toString()).build());
+    //
+    //        CommandResult commandResult = new AddAppointmentCommand(validAppointment).execute(modelStub);
+    //
+    //        assertEquals(String.format(AddAppointmentCommand.MESSAGE_SUCCESS, validAppointment),
+    //                commandResult.getFeedbackToUser());
+    //        assertEquals(Arrays.asList(validAppointment), modelStub.appointmentsAdded);
+    //    }
+
+    @Test
+    public void execute_duplicateAppointment_throwsCommandException() {
+        Appointment validAppointment = new AppointmentBuilder().build();
+        AddAppointmentCommand addAppointmentCommand = new AddAppointmentCommand(validAppointment);
+        ModelStub modelStub = new ModelStubWithAppointment(validAppointment);
+        assertThrows(CommandException.class,
+                AddAppointmentCommand.MESSAGE_TIMESLOT_OCCUPIED, () -> addAppointmentCommand.execute(modelStub));
+    }
+
+    @Test
+    public void equals() {
+        Appointment alice = new AppointmentBuilder().withPatientName("Alice").build();
+        Appointment bob = new AppointmentBuilder().withPatientName("Bob").build();
+        AddAppointmentCommand addAliceCommand = new AddAppointmentCommand(alice);
+        AddAppointmentCommand addBobCommand = new AddAppointmentCommand(bob);
+
+        // same object -> returns true
+        assertTrue(addAliceCommand.equals(addAliceCommand));
+
+        // same values -> returns true
+        AddAppointmentCommand addAliceCommandCopy = new AddAppointmentCommand(alice);
+        assertTrue(addAliceCommand.equals(addAliceCommandCopy));
+
+        // different types -> returns false
+        assertFalse(addAliceCommand.equals(1));
+
+        // null -> returns false
+        assertFalse(addAliceCommand.equals(null));
+
+        // different appointment -> returns false
+        assertFalse(addAliceCommand.equals(addBobCommand));
+    }
+
+    /**
+     * A default model stub that has all of its methods failing.
+     */
+    private class ModelStub implements Model {
+        @Override
+        public void setUserPrefs(ReadOnlyUserPrefs userPrefs) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ReadOnlyUserPrefs getUserPrefs() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public GuiSettings getGuiSettings() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setGuiSettings(GuiSettings guiSettings) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public Path getAddressBookFilePath() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setAddressBookFilePath(Path addressBookFilePath) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public Path getAppointmentListPath() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setAppointmentListPath(Path appointmentListPath) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void addPatient(Patient patient) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setAddressBook(ReadOnlyPatientList newData) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ReadOnlyPatientList getAddressBook() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public boolean hasPatient(Patient patient) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public boolean hasPatientName(Name patientName) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void deletePatient(Patient target) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setPatient(Patient target, Patient editedPatient) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ObservableList<Patient> getFilteredPatientList() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void updateFilteredPatientList(Predicate<Patient> predicate) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setAppointmentList(ReadOnlyAppointmentList appointmentList) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ReadOnlyAppointmentList getAppointmentList() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public boolean hasAppointment(Appointment appointment) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public boolean hasOverlap(Appointment appointment) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void deleteAppointment(Appointment appointment) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void addAppointment(Appointment appointment) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setAppointment(Appointment target, Appointment editedAppointment) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ObservableList<Appointment> getFilteredAppointmentList() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void updateFilteredAppointmentList(Predicate<Appointment> predicate) {
+            throw new AssertionError("This method should not be called.");
+        }
+    }
+
+    /**
+     * A Model stub that contains a single appointment.
+     */
+    private class ModelStubWithAppointment extends ModelStub {
+        private final Name name;
+        private final Appointment appointment;
+
+        ModelStubWithAppointment(Appointment appointment) {
+            requireNonNull(appointment);
+            this.name = appointment.getPatientName();
+            this.appointment = appointment;
+        }
+
+        @Override
+        public boolean hasPatient(Patient patient) {
+            requireNonNull(patient);
+            return this.name.equals(patient.getName());
+        }
+
+        @Override
+        public boolean hasPatientName(Name name) {
+            requireNonNull(name);
+            return this.name.equals(name);
+        }
+
+        @Override
+        public boolean hasAppointment(Appointment appointment) {
+            requireNonNull(appointment);
+            return this.appointment.isSameAppointment(appointment);
+        }
+    }
+
+    private class ModelStubAcceptingAppointmentAdded extends ModelStub {
+        final ArrayList<Name> patientsAdded = new ArrayList<>();
+        final ArrayList<Appointment> appointmentsAdded = new ArrayList<>();
+
+        @Override
+        public boolean hasAppointment(Appointment appointment) {
+            requireNonNull(appointment);
+            return appointmentsAdded.stream().anyMatch(appointment::isSameAppointment);
+        }
+
+        @Override
+        public void addAppointment(Appointment appointment) {
+            requireNonNull(appointment);
+            patientsAdded.add(appointment.getPatientName());
+            appointmentsAdded.add(appointment);
+        }
+
+        @Override
+        public boolean hasPatient(Patient patient) {
+            requireNonNull(patient);
+            return patientsAdded.stream().anyMatch(patientName -> patient.getName().equals(patientName));
+        }
+
+        @Override
+        public boolean hasPatientName(Name name) {
+            requireNonNull(name);
+            return patientsAdded.stream().anyMatch(patientName -> patientName.equals(name));
+        }
+
+        @Override
+        public boolean hasOverlap(Appointment appointment) {
+            requireNonNull(appointment);
+            for (Appointment appt : appointmentsAdded) {
+                if (appt.hasOverlap(appointment.getTimeslot())) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        @Override
+        public ReadOnlyAppointmentList getAppointmentList() {
+            return new AppointmentList();
+        }
+
+        @Override
+        public void addPatient(Patient patient) {
+            requireNonNull(patient);
+            patientsAdded.add(patient.getName());
+        }
+
+        @Override
+        public ReadOnlyPatientList getAddressBook() {
+            return new AddressBook();
+        }
+    }
+}
