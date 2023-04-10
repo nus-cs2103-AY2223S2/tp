e978716463175be83a734d9b303d5fbac7f4f248
swxk19 <s.wxk19@gmail.com>
2023-04-10 19:39:54 +0800

    Remove unused instances of tag

--- src/main/java/tfifteenfour/clipboard/model/ElementNotFoundException.java
+++ src/main/java/tfifteenfour/clipboard/model/ElementNotFoundException.java
@@ -1,6 +1,10 @@
 package tfifteenfour.clipboard.model;
 
 /**
- * Signals that the operation is unable to find the specified course.
+ * Signals that the operation is unable to find the specified element.
  */
-public class ElementNotFoundException extends RuntimeException {}
+public class ElementNotFoundException extends RuntimeException {
+	public ElementNotFoundException() {
+        super("Can't find such element in list.");
+    }
+}
--- src/main/java/tfifteenfour/clipboard/model/student/exceptions/StudentNotFoundException.java
+++ src/main/java/tfifteenfour/clipboard/model/student/exceptions/StudentNotFoundException.java
@@ -3,4 +3,8 @@ package tfifteenfour.clipboard.model.student.exceptions;
 /**
  * Signals that the operation is unable to find the specified student.
  */
-public class StudentNotFoundException extends RuntimeException {}
+public class StudentNotFoundException extends RuntimeException {
+	public StudentNotFoundException() {
+        super("Can't find such student in current group.");
+    }
+}
