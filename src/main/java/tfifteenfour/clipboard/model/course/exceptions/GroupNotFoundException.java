d3bb313b7dfa460a8e2d73705f06975e5a6dcae8
swxk19 <s.wxk19@gmail.com>
2023-04-10 19:25:14 +0800

    Add missing bodies in exception classes

--- src/main/java/tfifteenfour/clipboard/model/course/exceptions/GroupNotFoundException.java
+++ src/main/java/tfifteenfour/clipboard/model/course/exceptions/GroupNotFoundException.java
@@ -3,4 +3,8 @@ package tfifteenfour.clipboard.model.course.exceptions;
 /**
  * Signals that the operation is unable to find the specified Group.
  */
-public class GroupNotFoundException extends RuntimeException {}
+public class GroupNotFoundException extends RuntimeException {
+	public GroupNotFoundException() {
+        super("Can't find such group in this course.");
+    }
+}
--- src/main/java/tfifteenfour/clipboard/model/course/exceptions/SessionNotFoundException.java
+++ src/main/java/tfifteenfour/clipboard/model/course/exceptions/SessionNotFoundException.java
@@ -3,4 +3,7 @@ package tfifteenfour.clipboard.model.course.exceptions;
 /**
  * Signals that the operation is unable to find the specified session.
  */
-public class SessionNotFoundException extends RuntimeException {}
+public class SessionNotFoundException extends RuntimeException {
+	public SessionNotFoundException() {
+		super("Can't find such session in current group.");
+}	}
