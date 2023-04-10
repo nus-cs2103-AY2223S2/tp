30368aa9eedf6068bf111d4659b16ef06186277e
Jayanth Balasubramanian <jayanth.balasubramanian1@gmail.com>
2023-04-10 13:33:02 +0800

    Make clear command clear the appointment list

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -3,7 +3,9 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 
 import seedu.address.model.AddressBook;
+import seedu.address.model.AppointmentList;
 import seedu.address.model.Model;
+import seedu.address.ui.CalendarCard;
 
 /**
  * Clears the address book.
@@ -19,6 +21,8 @@ public class ClearCommand extends Command {
     public CommandResult execute(Model model) {
         requireNonNull(model);
         model.setAddressBook(new AddressBook());
+        model.setAppointmentList(new AppointmentList());
+        CalendarCard.addAppointmentsToCalendar(model.getFilteredAppointmentList());
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
