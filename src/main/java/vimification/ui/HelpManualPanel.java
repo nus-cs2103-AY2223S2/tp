11715ef61b4e2c9c18a17e1df90d6b21748b3500
Ethan <ethanchen2009@hotmail.com>
2023-04-10 23:24:53 +0800

    Fix HelpManualPage

--- src/main/java/vimification/ui/HelpManualPanel.java
+++ src/main/java/vimification/ui/HelpManualPanel.java
@@ -1,23 +1,16 @@
 package vimification.ui;
 
-import java.net.URISyntaxException;
-import java.nio.file.Path;
-import java.util.logging.Logger;
-
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.layout.VBox;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
-import vimification.common.core.LogsCenter;
-import vimification.common.util.StringUtil;
 
 /**
  * An UI component that displays the manual page for Vimification.
  */
 public class HelpManualPanel extends UiPart<VBox> {
 
-    private static final Logger LOGGER = LogsCenter.getLogger(HelpManualPanel.class);
     private static final String FXML = "HelpManualPanel.fxml";
 
     @FXML
@@ -28,19 +21,8 @@ public class HelpManualPanel extends UiPart<VBox> {
      */
     public HelpManualPanel() {
         super(FXML);
-        Path manualHtml = Path.of(FXML_FILE_FOLDER, "HelpManualPanel.html");
-        LOGGER.info(manualHtml.toString());
         WebEngine webEngine = webView.getEngine();
-        webEngine.load(manualHtml.toString());
-        String manualHtmlPath;
-        try {
-            manualHtmlPath = getClass()
-                    .getResource(FXML_FILE_FOLDER + "HelpManualPanel.html").toURI()
-                    .toString();
-            webEngine.load(manualHtmlPath);
-        } catch (URISyntaxException ex) {
-            LOGGER.info(StringUtil.getDetails(ex));
-        }
+        webEngine.load("https://ay2223s2-cs2103t-t15-3.github.io/tp/UserGuide.html");
         webView.prefWidthProperty().bind(this.getRoot().widthProperty());
         webView.prefHeightProperty().bind(this.getRoot().heightProperty());
     }
