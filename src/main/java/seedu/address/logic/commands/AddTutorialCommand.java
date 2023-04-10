0cb88acd02fe38f458f84a9182ec07941e565f48
ARPspoofing <mohdreyaaz99@gmail.com>
2023-04-10 17:47:22 +0800

    Modify example

--- src/main/java/seedu/address/logic/commands/AddTutorialCommand.java
+++ src/main/java/seedu/address/logic/commands/AddTutorialCommand.java
@@ -17,7 +17,7 @@ public class AddTutorialCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a tutorial to the address book. "
             + "Parameters: "
             + PREFIX_TUTORIAL + "TUTORIAL_NAME "
-            + "Restrictions: Not allowed to add tutorial and student with the same command! \n"
+            + "Restrictions: Name has a maximum of 20 characters \n"
             + "Example: " + COMMAND_WORD + " Tutorial/Dijkstra -date 10/10/2024 10:00";
 
     public static final String MESSAGE_SUCCESS = "New tutorial added: %1$s";
