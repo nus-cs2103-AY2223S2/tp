b798d69760e0abc6798feba8e63977e6f9f1a651
Teo Weide <teoweide1999@gmail.com>
2023-04-10 16:43:00 +0800

    Amend Userguide

--- src/main/java/seedu/address/model/person/MedicalCondition.java
+++ src/main/java/seedu/address/model/person/MedicalCondition.java
@@ -23,13 +23,6 @@ public class MedicalCondition {
      * Returns true if the value is valid medical condition
      */
     public static boolean isValidCondition(String value) {
-        char[] numbers = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-
-        for (int i = 0; i < numbers.length; i++) {
-            if (value.indexOf(numbers[i]) != -1) {
-                return false;
-            }
-        }
         if (value.equals("")) {
             return true;
         } else if (value.trim().equals("")) {
