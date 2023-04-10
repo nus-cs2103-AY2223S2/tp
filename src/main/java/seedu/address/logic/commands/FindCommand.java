9e5ebcda7fc7e0a17c644d7a70ad68482a361378
Elijah Ku <69575234+Zeli0@users.noreply.github.com>
2023-04-10 11:31:42 +0800

    Fix find entity error message

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -30,7 +30,7 @@ public class FindCommand extends Command {
         requireNonNull(model);
         model.updateFilteredEntityList(predicate);
         return new CommandResult(
-            String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredEntityList().size()));
+            String.format(Messages.MESSAGE_ENTITIES_LISTED_OVERVIEW, model.getFilteredEntityList().size()));
     }
 
     @Override
