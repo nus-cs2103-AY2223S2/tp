67792e432fcb372a7a5542f1ba51f8070fdf5f78
DerenC <chengderen953@gmail.com>
2023-04-10 14:45:31 +0800

    Change return type of fromInt() to Optional for Priority & Status

--- src/test/java/vimification/model/task/PriorityTest.java
+++ src/test/java/vimification/model/task/PriorityTest.java
@@ -1,23 +1,18 @@
 package vimification.model.task;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 
 public class PriorityTest {
     @Test
     public void testFromInt() {
-        assertEquals(Priority.fromInt(1), Priority.VERY_URGENT);
-        assertEquals(Priority.fromInt(2), Priority.URGENT);
-        assertEquals(Priority.fromInt(3), Priority.NOT_URGENT);
-        assertEquals(Priority.fromInt(0), Priority.UNKNOWN);
-    }
-
-    @Test
-    public void testFromIntException() {
-        assertThrows(IllegalArgumentException.class, () -> Priority.fromInt(-1));
-        assertThrows(IllegalArgumentException.class, () -> Priority.fromInt(4));
+        assertEquals(Priority.fromInt(1), Optional.of(Priority.VERY_URGENT));
+        assertEquals(Priority.fromInt(2), Optional.of(Priority.URGENT));
+        assertEquals(Priority.fromInt(3), Optional.of(Priority.NOT_URGENT));
+        assertEquals(Priority.fromInt(0), Optional.of(Priority.UNKNOWN));
     }
 
     @Test
