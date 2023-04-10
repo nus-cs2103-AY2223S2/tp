121b269facdab392b243a25a2a74c750b75d0864
Yufannnn <e0773591@u.nus.edu>
2023-04-10 16:04:50 +0800

    Minor Update

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -36,9 +36,7 @@ public class AddCommand extends Command {
             + PREFIX_EMAIL + "johnd@example.com "
             + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 "
             + PREFIX_SCHOOL + "Anglo Chinese "
-            + PREFIX_GRADELEVEL + "Secondary 3 "
-            + PREFIX_TAG + "friends "
-            + PREFIX_TAG + "owesMoney";
+            + PREFIX_GRADELEVEL + "Secondary 3 ";
 
     public static final String MESSAGE_SUCCESS = "New student added: %1$s";
     public static final String MESSAGE_DUPLICATE_STUDENT = "This student already exists in the address book";
