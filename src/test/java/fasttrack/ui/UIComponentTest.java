30e2e5c80722bc7554e75b4b3d35ad27223860ae
Shirshajit Sen Gupta <shirshajit@gmail.com>
2023-04-10 20:41:00 +0800

    Add component test

--- src/test/java/fasttrack/ui/UIComponentTest.java
+++ src/test/java/fasttrack/ui/UIComponentTest.java
@@ -1,6 +1,9 @@
 package fasttrack.ui;
 
 import static fasttrack.testutil.TypicalCategories.FOOD;
+import static fasttrack.testutil.TypicalCategories.TECH;
+import static fasttrack.testutil.TypicalExpenses.APPLE;
+import static fasttrack.testutil.TypicalExpenses.CHERRY;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.fail;
@@ -15,20 +18,29 @@ import org.junit.jupiter.api.Test;
 import org.opentest4j.AssertionFailedError;
 
 import fasttrack.model.category.Category;
+import fasttrack.model.expense.Expense;
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 
 public class UiComponentTest {
 
     private Category category;
     private int displayedIndex;
     private int associatedExpenseCount;
+    private CategoryListPanel categoryListPanel;
+    private ObservableList<Category> categories;
+    private ObservableList<Expense> expenses;
 
     @BeforeEach
     public void setUp() throws InterruptedException {
         category = FOOD;
         displayedIndex = 1;
         associatedExpenseCount = 3;
+        categories = FXCollections.observableArrayList(FOOD, TECH);
+        expenses = FXCollections.observableArrayList(APPLE, CHERRY);
     }
 
     @BeforeAll
@@ -78,6 +90,49 @@ public class UiComponentTest {
                 assertEquals(categoryCard1, categoryCard1);
                 assertNotEquals(categoryCard1, categoryCard2);
                 assertEquals(categoryCard2, categoryCard3);
+                future.complete(null);
+            } catch (AssertionFailedError e) {
+                future.completeExceptionally(e);
+            }
+        });
+        try {
+            future.get();
+        } catch (InterruptedException | ExecutionException e) {
+            fail("Assertion error thrown in Platform.runLater thread: " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void categoryListView_validCategories_countEqual() {
+        CompletableFuture<Void> future = new CompletableFuture<>();
+        categoryListPanel = new CategoryListPanel(categories, expenses);
+        Platform.runLater(() -> {
+            try {
+                // Test that the number of categories is correct
+                ListView<?> categoryListView = (ListView<?>) categoryListPanel.getRoot().lookup("#categoryListView");
+                assertEquals(categories.size(), categoryListView.getItems().size());
+                future.complete(null);
+            } catch (AssertionFailedError e) {
+                future.completeExceptionally(e);
+            }
+        });
+        try {
+            future.get();
+        } catch (InterruptedException | ExecutionException e) {
+            fail("Assertion error thrown in Platform.runLater thread: " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void categoryListView_emptyList_countZero() {
+        categories = FXCollections.observableArrayList();
+        expenses = FXCollections.observableArrayList();
+        CompletableFuture<Void> future = new CompletableFuture<>();
+        categoryListPanel = new CategoryListPanel(categories, expenses);
+        Platform.runLater(() -> {
+            try {
+                ListView<?> categoryListView = (ListView<?>) categoryListPanel.getRoot().lookup("#categoryListView");
+                assertEquals(0, categoryListView.getItems().size());
                 future.complete(null);
             } catch (AssertionFailedError e) {
                 future.completeExceptionally(e);
