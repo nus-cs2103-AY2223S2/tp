3295bd4dea28fbea2f2b40592ffe1e191d07e3bf
arkarsg <alxarkar@gmail.com>
2023-04-10 17:58:16 +0800

    Adds additional Test cases for UploadCsvCommand

--- src/test/java/trackr/logic/commands/UploadCsvCommandTest.java
+++ src/test/java/trackr/logic/commands/UploadCsvCommandTest.java
@@ -105,6 +105,20 @@ public class UploadCsvCommandTest {
                         AddSupplierCommand.MESSAGE_USAGE), () -> command.execute(model));
     }
 
+    @Test
+    public void execute_uploadCsv_withDuplicate_addsOtherValidItems_success() throws Exception {
+        model = new ModelStubWithItems();
+        model.addItem(CUPCAKE_M, ModelEnum.MENUITEM);
+
+        // first item is a duplicate
+        List<String> csvInput = SampleCommands.withMultipleValidCommand(SampleCommands.VALID_ADD_M,
+                                                                        SampleCommands.VALID_ADD_O);
+
+        CommandResult result = new UploadCsvCommand(csvInput).execute(model);
+
+        assertEquals(UploadCsvCommand.MESSAGE_SUCCESS, result.getFeedbackToUser());
+        assertEquals(Arrays.asList(CUPCAKE_M, CHOCOLATE_COOKIES_O), model.itemsAdded);
+    }
     /**
      * A Model stub that always accept Orders, Suppliers, Task and MenuItems being added.
      */
