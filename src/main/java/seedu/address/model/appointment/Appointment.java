b645bb7d26523fdd0ee9ddc7474400e061c6f5f6
mandykqh <84360725+mandykqh@users.noreply.github.com>
2023-04-10 16:03:07 +0800

    SLAP for appointment related classes

--- src/main/java/seedu/address/model/appointment/Appointment.java
+++ src/main/java/seedu/address/model/appointment/Appointment.java
@@ -48,17 +48,23 @@ public class Appointment {
             return true;
         }
 
-        String thisAppointmentDate = getBooking().toString();
-        String otherAppointmentDate = otherAppointment.getBooking().toString();
-        String thisAppointmentPatient = getPatientNric().toString();
-        String otherAppointmentPatient = otherAppointment.getPatientNric().toString();
-        String thisAppointmentDr = getDrNric().toString();
-        String otherAppointmentDr = otherAppointment.getDrNric().toString();
+        String thisAppointmentDate = retrieveAppmtDate(this);
+        String otherAppointmentDate = retrieveAppmtDate(otherAppointment);
+        String thisAppointmentPatient = retrievePatientNric(this);
+        String otherAppointmentPatient = retrievePatientNric(otherAppointment);
+        String thisAppointmentDr = retrieveDrNric(this);
+        String otherAppointmentDr = retrieveDrNric(otherAppointment);
 
         return thisAppointmentDate.equals(otherAppointmentDate)
                 && thisAppointmentPatient.equals(otherAppointmentPatient)
                 && thisAppointmentDr.equals(otherAppointmentDr);
     }
+
+    private String retrieveDrNric(Appointment appointment) {
+        String thisAppointmentDr = appointment.getDrNric().toString();
+        return thisAppointmentDr;
+    }
+
 
     /**
      * Checks if this appointment slot and the patient is the same as another appointment.
@@ -66,13 +72,23 @@ public class Appointment {
      * @return true if is the same, false otherwise
      */
     public boolean isSamePatientAppointmentTime(Appointment otherAppointment) {
-        String thisAppointmentDate = getBooking().toString();
-        String otherAppointmentDate = otherAppointment.getBooking().toString();
-        String thisAppointmentPatient = getPatientNric().toString();
-        String otherAppointmentPatient = otherAppointment.getPatientNric().toString();
+        String thisAppointmentDate = retrieveAppmtDate(this);
+        String otherAppointmentDate = retrieveAppmtDate(otherAppointment);
+        String thisAppointmentPatient = retrievePatientNric(this);
+        String otherAppointmentPatient = retrievePatientNric(otherAppointment);
 
         return thisAppointmentDate.equals(otherAppointmentDate)
                 && thisAppointmentPatient.equals(otherAppointmentPatient);
+    }
+
+    private String retrievePatientNric(Appointment appointment) {
+        String thisAppointmentPatient = appointment.getPatientNric().toString();
+        return thisAppointmentPatient;
+    }
+
+    private String retrieveAppmtDate(Appointment appointment) {
+        String thisAppointmentDate = appointment.getBooking().toString();
+        return thisAppointmentDate;
     }
 
     /**
@@ -96,4 +112,6 @@ public class Appointment {
                 + "; Doctor: " + getDrNric();
         return builder;
     }
+
+
 }
