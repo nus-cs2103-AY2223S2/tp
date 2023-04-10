e204e26263cf01195bc44a963d80d6a9c30aa889
ginloy <liyongleow@gmail.com>
2023-04-10 17:24:07 +0800

    Fix checkstyle

--- src/test/java/seedu/address/model/entity/shop/ShopTest.java
+++ src/test/java/seedu/address/model/entity/shop/ShopTest.java
@@ -1,6 +1,8 @@
 package seedu.address.model.entity.shop;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalDateTime;
 import java.util.HashSet;
