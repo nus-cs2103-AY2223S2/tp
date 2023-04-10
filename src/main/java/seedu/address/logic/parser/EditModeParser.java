a9d5ade30450761d12af3bab9d8db040c8795c0f
Elijah Ku <69575234+Zeli0@users.noreply.github.com>
2023-04-10 15:13:15 +0800

    Remove commented exception

--- src/main/java/seedu/address/logic/parser/EditModeParser.java
+++ src/main/java/seedu/address/logic/parser/EditModeParser.java
@@ -266,9 +266,6 @@ public class EditModeParser {
             } else {
                 throw new ParseException(MESSAGE_ENTITY_NONEXISTENT);
             }
-        } else {
-            //throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-            // MESSAGE_INVALID_INVENTORY_COMMAND));
-        }
+        } 
     }
 }
