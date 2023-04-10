d18d3ea464821958b4251a6ab0e86ba66de75769
swxk19 <s.wxk19@gmail.com>
2023-04-10 21:13:13 +0800

    Remove unused test files

--- src/test/java/tfifteenfour/clipboard/storage/JsonUserPrefsStorageTest.java
+++ src/test/java/tfifteenfour/clipboard/storage/JsonUserPrefsStorageTest.java
@@ -64,18 +64,6 @@ public class JsonUserPrefsStorageTest {
         assertEquals(new UserPrefs(), actual);
     }
 
-    /*
-    @Test
-    public void readUserPrefs_extraValuesInFile_extraValuesIgnored() throws DataConversionException {
-        UserPrefs expected = getTypicalUserPrefs();
-        UserPrefs actual = readUserPrefs("ExtraValuesUserPref.json").get();
-
-        assertEquals(expected, actual);
-    }
-     */
-
-
-
     private UserPrefs getTypicalUserPrefs() {
         UserPrefs userPrefs = new UserPrefs();
         userPrefs.setGuiSettings(new GuiSettings(1000, 500, 300, 100));
